fae04e7958ee3f5b219a4fe4d4e67670abddd544
謝桂綱 <xie.guigang@gmail.com>
2016-01-18 04:34:57 +0800

    add a method for gets matches

---
+++ Microsoft.VisualBasic.Architecture.Framework/Extensions/StringHelpers/Compares/LevExtensions.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module LevExtensions
+
+    <Extension> Public Sub GetMatches(Of T)(edits As DistResult, ref As T(), hyp As T(), ByRef refOUT As T(), ByRef hypOUT As T())
+        Dim len As Integer = edits.DistEdits.Count("m"c)
+        Dim idx As Integer = -1
+        Dim iiiii As Integer = 0
+
+        refOUT = New T(len - 1) {}
+        hypOUT = New T(len - 1) {}
+
+        For j As Integer = 0 To hyp.Length   ' 参照subject画列
+            For i As Integer = -1 To ref.Length - 1 ' 参照query画行
+                If edits.IsPath(i, j) Then
+                    Dim ch As String = edits.DistEdits.Get(idx.MoveNext)
+
+                    If ch = "m"c Then
+                        refOUT(iiiii) = ref(i)
+                        hypOUT(iiiii) = hyp(j)
+                    End If
+                End If
+            Next
+        Next
+    End Sub
+End Module
