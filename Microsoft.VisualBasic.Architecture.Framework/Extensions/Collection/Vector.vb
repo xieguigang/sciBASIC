ca0be5c390850d01b43e4ddf5cb75422ada6ad75
謝桂綱 <xie.guigang@gmail.com>
2016-01-17 17:58:05 +0800

    add a string mid like function for the generic vector operations

---
+++ Microsoft.VisualBasic.Architecture.Framework/Extensions/Collection/Vector.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module Vector
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <typeparam name="T"></typeparam>
+    ''' <param name="source"></param>
+    ''' <param name="start">0 base</param>
+    ''' <param name="length"></param>
+    ''' <returns></returns>
+    <Extension> Public Function Midv(Of T)(source As IEnumerable(Of T), start As Integer, length As Integer) As T()
+        If source.IsNullOrEmpty Then
+            Return New T() {}
+        ElseIf source.Count < length Then
+            Return source.ToArray
+        End If
+
+        Dim array As T() = source.ToArray
+        Dim ends As Integer = start + length
+
+        If ends > array.Length Then
+            length -= array.Length - ends
+        End If
+
+        Dim buf As T() = New T(length - 1) {}
+        Call System.Array.ConstrainedCopy(array, start, buf, Scan0, buf.Length)
+        Return buf
+    End Function
+
+End Module
