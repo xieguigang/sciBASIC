77da642e0b81e6c1ee4c951da37ca47bb7013285
謝桂綱 <xie.guigang@gmail.com>
2016-02-20 14:21:12 +0800

    lost some nodes?

--- Datavisualization/Datavisualization.Network/Datavisualization.Network/TreeAPI/Operations.vb
+++ Datavisualization/Datavisualization.Network/Datavisualization.Network/TreeAPI/Operations.vb
@@ -102,9 +102,56 @@ Namespace TreeAPI
             Return hash
         End Function
 
-        <Extension> Private Function __addCluster(node As TreeNode(Of NodeTypes), hash As Dictionary(Of String, String())) As Dictionary(Of String, String())
+        <Extension> Private Function __addCluster(node As TreeNode(Of NodeTypes), ByRef hash As Dictionary(Of String, String())) As Dictionary(Of String, String())
+            Dim list As New List(Of String)
 
+            For Each x In node.GetEnumerator
+                If x.__hashLeaf Then
+                    Dim leafs As New List(Of String)
+                    Call x.__continuteCluster(hash, leafs)
+                    Call list.AddRange(leafs)
+                Else
+                    Call x.__addCluster(hash)
+                End If
+            Next
 
+            If list.Count > 0 Then
+                Call hash.Add(node.Name, list.Distinct.ToArray)
+            End If
+
+            Return hash
+        End Function
+
+        <Extension> Private Sub __continuteCluster(node As TreeNode(Of NodeTypes), ByRef hash As Dictionary(Of String, String()), ByRef list As List(Of String))
+            For Each x In node.GetEnumerator
+                If TypeOf x Is Leaf OrElse TypeOf x Is LeafX Then
+                    Dim leafs As String() = DirectCast(x, TreeNode).GetEntities
+                    Call list.AddRange(leafs)
+                ElseIf x.__hashLeaf Then
+                    Call x.__continuteCluster(hash, list)
+                Else
+                    Call x.__addCluster(hash)
+                End If
+            Next
+        End Sub
+
+        ''' <summary>
+        ''' 最远只允许隔着一层Path，这些就可以看作为一个cluster
+        ''' </summary>
+        ''' <param name="node"></param>
+        ''' <returns></returns>
+        <Extension> Private Function __hashLeaf(node As TreeNode(Of NodeTypes)) As Boolean
+            For Each x In node.GetEnumerator
+                If TypeOf x Is Leaf OrElse TypeOf x Is LeafX Then
+                    Return True
+                End If
+                For Each y In x.GetEnumerator
+                    If TypeOf y Is Leaf OrElse TypeOf x Is LeafX Then
+                        Return True
+                    End If
+                Next
+            Next
+            Return False
         End Function
     End Module
 End Namespace
