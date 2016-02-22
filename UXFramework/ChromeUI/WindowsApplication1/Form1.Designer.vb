7ec6809cfe47e1106373a0ccf2a34b7c25869e93
謝桂綱 <xie.guigang@gmail.com>
2016-02-23 00:59:12 +0800

    renderer
    chrome menu style

---
+++ UXFramework/ChromeUI/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.BackToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ForwardToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReloadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.SaveAsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TranslateToEnglishToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.DownloadAllLinksWithIDMToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ViewPageSourceToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InspectToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(659, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BackToolStripMenuItem, Me.ForwardToolStripMenuItem, Me.ReloadToolStripMenuItem, Me.ToolStripMenuItem1, Me.SaveAsToolStripMenuItem, Me.PrintToolStripMenuItem, Me.TranslateToEnglishToolStripMenuItem, Me.ToolStripMenuItem2, Me.DownloadAllLinksWithIDMToolStripMenuItem, Me.ToolStripMenuItem3, Me.ViewPageSourceToolStripMenuItem, Me.InspectToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(222, 220)
+        '
+        'BackToolStripMenuItem
+        '
+        Me.BackToolStripMenuItem.Name = "BackToolStripMenuItem"
+        Me.BackToolStripMenuItem.Size = New System.Drawing.Size(221, 22)
+        Me.BackToolStripMenuItem.Text = "Back"
+        '
+        'ForwardToolStripMenuItem
+        '
+        Me.ForwardToolStripMenuItem.Name = "ForwardToolStripMenuItem"
+        Me.ForwardToolStripMenuItem.Size = New System.Drawing.Size(221, 22)
+        Me.ForwardToolStripMenuItem.Text = "Forward"
+        '
+        'ReloadToolStripMenuItem
+        '
+        Me.ReloadToolStripMenuItem.Name = "ReloadToolStripMenuItem"
+        Me.ReloadToolStripMenuItem.Size = New System.Drawing.Size(221, 22)
+        Me.ReloadToolStripMenuItem.Text = "Reload"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(218, 6)
+        '
+        'SaveAsToolStripMenuItem
+        '
+        Me.SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        Me.SaveAsToolStripMenuItem.Size = New System.Drawing.Size(221, 22)
+        Me.SaveAsToolStripMenuItem.Text = "Save as..."
+        '
+        'PrintToolStripMenuItem
+        '
+        Me.PrintToolStripMenuItem.Name = "PrintToolStripMenuItem"
+        Me.PrintToolStripMenuItem.Size = New System.Drawing.Size(221, 22)
+        Me.PrintToolStripMenuItem.Text = "Print..."
+        '
+        'TranslateToEnglishToolStripMenuItem
+        '
+        Me.TranslateToEnglishToolStripMenuItem.Name = "TranslateToEnglishToolStripMenuItem"
+        Me.TranslateToEnglishToolStripMenuItem.Size = New System.Drawing.Size(221, 22)
+        Me.TranslateToEnglishToolStripMenuItem.Text = "Translate to English"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(218, 6)
+        '
+        'DownloadAllLinksWithIDMToolStripMenuItem
+        '
+        Me.DownloadAllLinksWithIDMToolStripMenuItem.Name = "DownloadAllLinksWithIDMToolStripMenuItem"
+        Me.DownloadAllLinksWithIDMToolStripMenuItem.Size = New System.Drawing.Size(221, 22)
+        Me.DownloadAllLinksWithIDMToolStripMenuItem.Text = "Download all links with IDM"
+        '
+        'ToolStripMenuItem3
+        '
+        Me.ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        Me.ToolStripMenuItem3.Size = New System.Drawing.Size(218, 6)
+        '
+        'ViewPageSourceToolStripMenuItem
+        '
+        Me.ViewPageSourceToolStripMenuItem.Name = "ViewPageSourceToolStripMenuItem"
+        Me.ViewPageSourceToolStripMenuItem.Size = New System.Drawing.Size(221, 22)
+        Me.ViewPageSourceToolStripMenuItem.Text = "View page source"
+        '
+        'InspectToolStripMenuItem
+        '
+        Me.InspectToolStripMenuItem.Name = "InspectToolStripMenuItem"
+        Me.InspectToolStripMenuItem.Size = New System.Drawing.Size(221, 22)
+        Me.InspectToolStripMenuItem.Text = "Inspect"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(659, 478)
+        Me.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents BackToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ForwardToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReloadToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PrintToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TranslateToEnglishToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripSeparator
+    Friend WithEvents DownloadAllLinksWithIDMToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripSeparator
+    Friend WithEvents ViewPageSourceToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InspectToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ UXFramework/ChromeUI/WindowsApplication1/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ToolStripManager.Renderer = New ChromeUI.ChromeUIRender
+    End Sub
+End Class
