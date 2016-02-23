403973b72740399d6fec4d628714b38c9489661b
amethyst-asuka <amethyst.asuka@gcmodeller.org>
2016-02-23 20:29:18 +0800

    user register on server

--- Microsoft.VisualBasic.NETProtocol/User/User.vb
+++ Microsoft.VisualBasic.NETProtocol/User/User.vb
@@ -1,8 +1,9 @@
 ﻿Imports Microsoft.VisualBasic.Net.NETProtocol.Protocols
 Imports Microsoft.VisualBasic.Net.Protocols.Reflection
+Imports Microsoft.VisualBasic.Parallel
 
 <Protocol(GetType(UserProtocols.Protocols))>
-Public Class User
+Public Class User : Implements IDisposable
 
     ReadOnly __updateThread As Persistent.Application.USER
 
@@ -21,5 +22,47 @@ Public Class User
     Private Shared Function __register(args As InitPOSTBack, endpoint As User) As Persistent.Application.USER
         Dim protocols As New ProtocolHandler(endpoint)
         Dim user As New Persistent.Application.USER(args.Portal, args.uid, AddressOf protocols.HandlePush)
+        Call RunTask(Sub() user.BeginConnect(AddressOf endpoint.__close))
+        Call Threading.Thread.Sleep(100)
+
+        Return user
     End Function
+
+    Private Sub __close()
+        ' DO NOTHING
+    End Sub
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' To detect redundant calls
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: dispose managed state (managed objects).
+                Call __updateThread.Free
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+            ' TODO: set large fields to null.
+        End If
+        disposedValue = True
+    End Sub
+
+    ' TODO: override Finalize() only if Dispose(disposing As Boolean) above has code to free unmanaged resources.
+    'Protected Overrides Sub Finalize()
+    '    ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+    '    Dispose(False)
+    '    MyBase.Finalize()
+    'End Sub
+
+    ' This code added by Visual Basic to correctly implement the disposable pattern.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        Dispose(True)
+        ' TODO: uncomment the following line if Finalize() is overridden above.
+        ' GC.SuppressFinalize(Me)
+    End Sub
+#End Region
+
 End Class
