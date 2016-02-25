dfbc7d92254a28139af36299aad9e3193bb29126
謝桂綱 <xie.guigang@gmail.com>
2016-02-25 18:27:28 +0800

    Marshal as , is that right?

---
+++ Microsoft.VisualBasic.Architecture.Framework/Extensions/Reflection/Marshal/IntPtr(Of T).vb
@@ -0,0 +1,61 @@
+﻿Imports System.Runtime.InteropServices.Marshal
+
+Namespace Marshal
+
+    Public Delegate Sub UnsafeCopy(Of T)(source As System.IntPtr, destination As T(), startIndex As Integer, length As Integer)
+
+    ''' <summary>
+    ''' 内存指针
+    ''' </summary>
+    ''' <typeparam name="T"></typeparam>
+    Public MustInherit Class IntPtr(Of T)
+
+        ''' <summary>
+        ''' 第一个位置
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property Scan0 As System.IntPtr
+
+        Protected __innerRaw As T()
+        Dim _current As Integer
+
+        Public ReadOnly Property Current As T
+            Get
+                Return Value(_current)
+            End Get
+        End Property
+
+        Default Public Property Value(p As Integer) As T
+            Get
+                If p < 0 OrElse p >= __innerRaw.Length Then
+                    Return Nothing
+                Else
+                    Return __innerRaw(p)
+                End If
+            End Get
+            Set(value As T)
+                __innerRaw(p) = value
+            End Set
+        End Property
+
+        Sub New(p As System.IntPtr, chunkSize As Integer, unsafe As UnsafeCopy(Of T))
+            Scan0 = p
+            __innerRaw = New T(chunkSize - 1) {}
+            Call unsafe(Scan0, __innerRaw, 0, __innerRaw.Length)
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return $"* {GetType(T).Name} + {_current} --> {Current}  // {Scan0.ToString}"
+        End Function
+
+        Public Shared Operator +(ptr As IntPtr(Of T), d As Integer) As T
+            ptr._current += d
+            Return ptr.Current
+        End Operator
+
+        Public Shared Operator -(ptr As IntPtr(Of T), d As Integer) As T
+            ptr._current -= d
+            Return ptr.Current
+        End Operator
+    End Class
+End Namespace
---
+++ Microsoft.VisualBasic.Architecture.Framework/Extensions/Reflection/Marshal/MarshalExtensions.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices.Marshal
+Imports Microsoft.VisualBasic.Marshal
+
+''' <summary>
+''' 
+''' </summary>
+Public Module MarshalExtensions
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <typeparam name="T">Integer, Char, Short, Long, Single, Byte, IntPtr, Double</typeparam>
+    ''' <param name="p"></param>
+    ''' <returns></returns>
+    ''' <remarks>
+    ''' Public Shared Sub Copy(source As IntPtr, destination() As Integer, startIndex As Integer, length As Integer)
+    ''' Public Shared Sub Copy(source As IntPtr, destination() As Char, startIndex As Integer, length As Integer)
+    ''' Public Shared Sub Copy(source As IntPtr, destination() As Short, startIndex As Integer, length As Integer)
+    ''' Public Shared Sub Copy(source As IntPtr, destination() As Long, startIndex As Integer, length As Integer)
+    ''' Public Shared Sub Copy(source As IntPtr, destination() As Single, startIndex As Integer, length As Integer)
+    ''' Public Shared Sub Copy(source As IntPtr, destination() As Byte, startIndex As Integer, length As Integer)
+    ''' Public Shared Sub Copy(source As IntPtr, destination() As IntPtr, startIndex As Integer, length As Integer)
+    ''' Public Shared Sub Copy(source As IntPtr, destination() As Double, startIndex As Integer, length As Integer)
+    ''' </remarks>
+    <Extension> Public Function MarshalAs(Of T)(p As System.IntPtr, chunkSize As Integer) As IntPtr(Of T)
+        Select Case GetType(T)
+            Case GetType(Integer) : Return DirectCast(CType(New [Integer](p, chunkSize), Object), IntPtr(Of T))
+            Case GetType(Char) : Return DirectCast(CType(New [Char](p, chunkSize), Object), IntPtr(Of T))
+            Case GetType(Short) : Return DirectCast(CType(New [Short](p, chunkSize), Object), IntPtr(Of T))
+            Case GetType(Long) : Return DirectCast(CType(New [Long](p, chunkSize), Object), IntPtr(Of T))
+            Case GetType(Single) : Return DirectCast(CType(New [Single](p, chunkSize), Object), IntPtr(Of T))
+            Case GetType(Byte) : Return DirectCast(CType(New [Byte](p, chunkSize), Object), IntPtr(Of T))
+            Case GetType(System.IntPtr) : Return DirectCast(CType(New IntPtr(p, chunkSize), Object), IntPtr(Of T))
+            Case GetType(Double) : Return DirectCast(CType(New [Double](p, chunkSize), Object), IntPtr(Of T))
+            Case Else
+                Throw New MemberAccessException(GetType(T).FullName & " is not a valid memory region!")
+        End Select
+    End Function
+End Module
