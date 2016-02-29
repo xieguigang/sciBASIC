0f06881e33ce8b4fb2e0fb7f5175012e0bcc8fe1
謝桂綱 <xie.guigang@gmail.com>
2016-02-29 13:33:24 +0800

    rename namespace

--- SharedProject.VB_FrameworkExtensions/Extensions/CodeDOM/VBC.vb
+++ SharedProject.VB_FrameworkExtensions/Extensions/CodeDOM/VBC.vb
@@ -2,49 +2,51 @@
 Imports System.Reflection
 Imports System.Text
 
-Public Module VBC
+Namespace CodeDOM_VBC
 
-    ''' <summary>
-    ''' 
-    ''' </summary>
-    ''' <param name="code">源代码</param>
-    ''' <returns></returns>
-    Public Function CompileCode(code As String, Optional ByRef errInfo As String = "") As Assembly
-        Dim codeProvider As New VBCodeProvider()
+    Public Module VBC
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="code">VisualBasic源代码</param>
+        ''' <returns></returns>
+        Public Function CompileCode(code As String, Optional ByRef errInfo As String = "") As Assembly
+            Dim codeProvider As New VBCodeProvider()
 #Disable Warning
-        Dim icc As ICodeCompiler = codeProvider.CreateCompiler
+            Dim icc As ICodeCompiler = codeProvider.CreateCompiler
 #Enable Warning
-        Dim Output As String = "Out.exe"
-        Dim parameters As New CompilerParameters()
-        Dim results As CompilerResults
+            Dim Output As String = "Out.exe"
+            Dim params As New CompilerParameters()
+            Dim results As CompilerResults
 
-        'Make sure we generate an EXE, not a DLL
-        parameters.GenerateExecutable = True
-        parameters.OutputAssembly = Output
-        results = icc.CompileAssemblyFromSource(parameters, code)
+            'Make sure we generate an EXE, not a DLL
+            params.GenerateExecutable = True
+            params.OutputAssembly = Output
+            results = icc.CompileAssemblyFromSource(params, code)
 
-        If results.Errors.Count > 0 Then            'There were compiler errors
-            Dim Err As StringBuilder =
+            If results.Errors.Count > 0 Then            'There were compiler errors
+                Dim Err As StringBuilder =
                 New StringBuilder("There were compiler errors:")
 
-            Call Err.AppendLine()
-            Call Err.AppendLine()
+                Call Err.AppendLine()
+                Call Err.AppendLine()
 
-            For Each CompErr As CompilerError In results.Errors
-                Dim errDetail As String = "Line number " & CompErr.Line &
+                For Each CompErr As CompilerError In results.Errors
+                    Dim errDetail As String = "Line number " & CompErr.Line &
                 ", Error Number: " & CompErr.ErrorNumber &
                 ", '" & CompErr.ErrorText & ";"
-                Call Err.AppendLine(errDetail)
-                Call Err.AppendLine()
-            Next
+                    Call Err.AppendLine(errDetail)
+                    Call Err.AppendLine()
+                Next
 
-            errInfo = Err.ToString
+                errInfo = Err.ToString
 
-            Return Nothing
-        Else
-            'Successful Compile
-            Return results.CompiledAssembly
-        End If
-    End Function
-
-End Module
+                Return Nothing
+            Else
+                'Successful Compile
+                Return results.CompiledAssembly
+            End If
+        End Function
+    End Module
+End Namespace
