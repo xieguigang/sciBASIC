8939a23ceee96526940e30ab2c9fd66557bef075
謝桂綱 <xie.guigang@gmail.com>
2016-02-29 12:38:10 +0800

    expands the stack
    not test yet, and probably  exists some bugs

--- Microsoft.VisualBasic.Architecture.Framework/Scripting/TokenIcer/LDM/Func.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Scripting/TokenIcer/LDM/Func.vb
@@ -2,7 +2,13 @@
 
 Namespace Scripting.TokenIcer
 
-    Public Class Func(Of Tokens)
+    Public MustInherit Class ExprToken(Of Tokens)
+
+        Public MustOverride Function ToArray(stackT As StackTokens(Of Tokens)) As Token(Of Tokens)()
+
+    End Class
+
+    Public Class Func(Of Tokens) : Inherits ExprToken(Of Tokens)
 
         Public Property Caller As List(Of InnerToken(Of Tokens))
         Public Property Args As Func(Of Tokens)()
@@ -24,9 +30,22 @@ Namespace Scripting.TokenIcer
         ''' 将表达式的栈空间展开
         ''' </summary>
         ''' <returns></returns>
-        Public Function ToArray() As Token(Of Tokens)()
-
+        Public Overrides Function ToArray(stackT As StackTokens(Of Tokens)) As Token(Of Tokens)()
+            Dim list As New List(Of Token(Of Tokens))
+            Call __expand(list, stackT)
+            Return list.ToArray
         End Function
+
+        Private Sub __expand(ByRef list As List(Of Token(Of Tokens)), stackT As StackTokens(Of Tokens))
+            For Each x In Caller
+                Call list.AddRange(x.ToArray(stackT))
+            Next
+            If Not Args.IsNullOrEmpty Then
+                For Each x In Args
+                    Call x.__expand(list, stackT)
+                Next
+            End If
+        End Sub
 
         Public Overrides Function ToString() As String
             If Args.IsNullOrEmpty Then
@@ -40,7 +59,8 @@ Namespace Scripting.TokenIcer
         End Function
     End Class
 
-    Public Class InnerToken(Of Tokens)
+    Public Class InnerToken(Of Tokens) : Inherits ExprToken(Of Tokens)
+
         Public Property obj As Token(Of Tokens)
         Public Property InnerStack As Func(Of Tokens)()
 
@@ -65,6 +85,19 @@ Namespace Scripting.TokenIcer
                 Dim inner As String() = InnerStack.ToArray(Function(x) x.ToString)
                 Dim s As String = String.Join(" ", inner)
                 Return $"({s})"
+            End If
+        End Function
+
+        Public Overrides Function ToArray(stackT As StackTokens(Of Tokens)) As Token(Of Tokens)()
+            If stackT.Equals(obj.TokenName, stackT.Pretend) Then
+                Dim list As New List(Of Token(Of Tokens))
+                For Each x In InnerStack
+                    Call list.AddRange(x.ToArray(stackT))
+                Next
+
+                Return list.ToArray
+            Else
+                Return {obj}
             End If
         End Function
     End Class
