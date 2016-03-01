9277f0418eb4269b2de9247f58bf3644b55365e3
謝桂綱 <xie.guigang@gmail.com>
2016-03-01 20:27:43 +0800

    list extensions

---
+++ Microsoft.VisualBasic.Architecture.Framework/ComponentModel/List(Of T).vb
@@ -0,0 +1,46 @@
+﻿Public Class List(Of T) : Inherits Generic.List(Of T)
+
+    Sub New(source As IEnumerable(Of T))
+        Call MyBase.New(source.ToArray)
+    End Sub
+
+    Sub New(ParamArray x As T())
+        Call MyBase.New(x)
+    End Sub
+
+    Sub New()
+    End Sub
+
+    Public Shared Operator +(list As List(Of T), x As T) As List(Of T)
+        Call list.Add(x)
+        Return list
+    End Operator
+
+    Public Shared Operator -(list As List(Of T), x As T) As List(Of T)
+        Call list.Remove(x)
+        Return list
+    End Operator
+
+    Public Shared Operator +(list As List(Of T), vals As IEnumerable(Of T)) As List(Of T)
+        Call list.AddRange(vals.ToArray)
+        Return list
+    End Operator
+
+    Public Shared Operator -(list As List(Of T), vals As IEnumerable(Of T)) As List(Of T)
+        If Not vals Is Nothing Then
+            For Each x As T In vals
+                Call list.Remove(x)
+            Next
+        End If
+        Return list
+    End Operator
+
+    Public Shared Operator -(list As List(Of T), index As Integer) As List(Of T)
+        Call list.RemoveAt(index)
+        Return list
+    End Operator
+
+    Public Shared Narrowing Operator CType(list As List(Of T)) As T()
+        Return list.ToArray
+    End Operator
+End Class
--- SharedProject.VB_FrameworkExtensions/Extensions/CodeDOM/VBC.vb
+++ SharedProject.VB_FrameworkExtensions/Extensions/CodeDOM/VBC.vb
@@ -6,29 +6,54 @@ Namespace CodeDOM_VBC
 
     Public Module VBC
 
+        Public Function CreateParameters(ref As IEnumerable(Of String), SDK As String, Optional dll As Boolean = True) As CompilerParameters
+            Dim args As CompilerParameters = If(dll, DllProfile, ExecutableProfile)
+            Dim libs As New List(Of String)
+
+            libs += From path As String
+                    In ref
+                    Where Array.IndexOf(DotNETFramework, IO.Path.GetFileNameWithoutExtension(path)) = -1
+                    Select path '
+            libs += {
+                SDK & "\System.dll",
+                SDK & "\System.Core.dll",
+                SDK & "\System.Data.dll",
+                SDK & "\System.Data.DataSetExtensions.dll",
+                SDK & "\System.Xml.dll",
+                SDK & "\System.Xml.Linq.dll"
+            }
+            Call args.ReferencedAssemblies.AddRange(libs)
+            Return args
+        End Function
+
         ''' <summary>
         ''' 
         ''' </summary>
         ''' <param name="code">VisualBasic源代码</param>
         ''' <returns></returns>
-        Public Function CompileCode(code As String, Optional ByRef errInfo As String = "") As Assembly
+        Public Function CompileCode(code As String, output As String, Optional ByRef errInfo As String = "") As Assembly
+            Dim params As New CompilerParameters()
+
+            'Make sure we generate an EXE, not a DLL
+            params.GenerateExecutable = True
+            params.OutputAssembly = output
+            Return VBC.CompileCode(code, params, errInfo)
+        End Function
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="code">VisualBasic源代码</param>
+        ''' <returns></returns>
+        Public Function CompileCode(code As String, args As CompilerParameters, Optional ByRef errInfo As String = "") As Assembly
             Dim codeProvider As New VBCodeProvider()
 #Disable Warning
             Dim icc As ICodeCompiler = codeProvider.CreateCompiler
 #Enable Warning
-            Dim Output As String = "Out.exe"
-            Dim params As New CompilerParameters()
-            Dim results As CompilerResults
-
-            'Make sure we generate an EXE, not a DLL
-            params.GenerateExecutable = True
-            params.OutputAssembly = Output
-            results = icc.CompileAssemblyFromSource(params, code)
+            Dim results As CompilerResults = icc.CompileAssemblyFromSource(args, code)
 
             If results.Errors.Count > 0 Then            'There were compiler errors
-                Dim Err As StringBuilder =
-                New StringBuilder("There were compiler errors:")
-
+                Dim Err As StringBuilder = New StringBuilder("There were compiler errors:")
                 Call Err.AppendLine()
                 Call Err.AppendLine()
 
