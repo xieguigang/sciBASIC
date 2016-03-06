9cb9823417b5bd88187fe65376897e236ffee351
謝桂綱 <xie.guigang@gmail.com>
2016-03-06 20:59:50 +0800

    add a integer pointer

---
+++ Microsoft.VisualBasic.Architecture.Framework/ComponentModel/DataStructures/Pointer.vb
@@ -0,0 +1,66 @@
+﻿Namespace ComponentModel.DataStructures
+
+    ''' <summary>
+    ''' <see cref="Int32"/>类型，一般用来进行数组操作的
+    ''' </summary>
+    Public Class Pointer
+
+        Dim __index As Integer
+
+        Sub New(n As Integer)
+            __index = n
+        End Sub
+
+        Sub New()
+            Call Me.New(Scan0)
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return __index
+        End Function
+
+        Public Shared Widening Operator CType(n As Integer) As Pointer
+            Return New Pointer(n)
+        End Operator
+
+        Public Shared Narrowing Operator CType(n As Pointer) As Integer
+            Return n.__index
+        End Operator
+
+        Public Overloads Shared Operator +(n As Pointer, x As Integer) As Pointer
+            n.__index += x
+            Return n
+        End Operator
+
+        Public Overloads Shared Operator +(x As Integer, n As Pointer) As Pointer
+            n.__index += x
+            Return n
+        End Operator
+
+        Public Overloads Shared Operator +(x As Pointer, n As Pointer) As Pointer
+            Return New Pointer(n.__index + x.__index)
+        End Operator
+
+        ''' <summary>
+        ''' 自增1，然后返回之前的数值
+        ''' </summary>
+        ''' <param name="x"></param>
+        ''' <returns></returns>
+        Public Overloads Shared Operator +(x As Pointer) As Integer
+            Dim p As Integer = x.__index
+            x.__index += 1
+            Return p
+        End Operator
+
+        ''' <summary>
+        ''' 自减1，然后返回之前的数值
+        ''' </summary>
+        ''' <param name="x"></param>
+        ''' <returns></returns>
+        Public Overloads Shared Operator -(x As Pointer) As Integer
+            Dim p As Integer = x.__index
+            x.__index -= 1
+            Return p
+        End Operator
+    End Class
+End Namespace
