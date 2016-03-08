f0d44421fdc37b8fb3838cf01b0565378a311443
謝桂綱 <xie.guigang@gmail.com>
2016-03-08 12:03:06 +0800

    add a network file module

---
+++ Microsoft.VisualBasic.Architecture.Framework/Extensions/IO/NetFile.vb
@@ -0,0 +1,55 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<PackageNamespace("IO.NetFile")>
+Public Module NetFile
+
+    <ExportAPI("MapNetFile")>
+    <Extension>
+    Public Function MapNetFile(url As String) As String
+        Dim path As String = url.GetMapPath
+
+        If path.FileExists Then
+            Return path
+        Else  ' 下载数据然后缓存
+            Call DownloadFile(url, path)
+            Return path
+        End If
+    End Function
+
+    <ExportAPI("NetFile.FileExists")>
+    <Extension>
+    Public Function NetFileExists(url As String) As Boolean
+        Return url.GetMapPath.FileExists
+    End Function
+
+    ''' <summary>
+    ''' 网络文件转换为本地文件路径
+    ''' </summary>
+    ''' <param name="url"></param>
+    ''' <returns></returns>
+    ''' 
+    <ExportAPI("Map.Path")>
+    <Extension>
+    Public Function GetMapPath(url As String) As String
+        If InStr(url, "http://", CompareMethod.Text) +
+            InStr(url, "https://", CompareMethod.Text) > 0 Then
+
+            url = Strings.Split(url, "//").Last
+            url = App.AppSystemTemp & "/" & url
+
+            Dim folders As String =
+                FileIO.FileSystem.GetParentPath(url)
+            Call FileIO.FileSystem.CreateDirectory(folders)
+
+            Return url
+        Else
+            If url.FileExists Then
+                Return url
+            Else
+                Throw New Exception(url & " is a unrecognized url path!")
+            End If
+        End If
+    End Function
+End Module
