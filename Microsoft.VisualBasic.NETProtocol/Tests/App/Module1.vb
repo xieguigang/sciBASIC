e43c5ebd67fe1c92a785fc4734e93478bf65d1fd
xieguigang <xie.guigang@gmail.com>
2016-03-11 13:17:58 +0800

    client connected successfully

--- Microsoft.VisualBasic.NETProtocol/Tests/App/Module1.vb
+++ Microsoft.VisualBasic.NETProtocol/Tests/App/Module1.vb
@@ -2,6 +2,14 @@
 
     Sub Main()
 
+        Pause()
+
+        Dim user As New Microsoft.VisualBasic.Net.NETProtocol.User(New Microsoft.VisualBasic.Net.IPEndPoint("127.0.0.1", 6354), 1234)
+
+
+
+        Pause()
+
     End Sub
 
 End Module
