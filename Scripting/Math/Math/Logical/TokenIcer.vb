e38c76481d8de5602a7cc90d68d07ce61afc951f
xieguigang <xie.guigang@gmail.com>
2016-03-19 21:39:24 +0800

    start to construct parser

--- Scripting/Math/Math/Logical/TokenIcer.vb
+++ Scripting/Math/Math/Logical/TokenIcer.vb
@@ -1,4 +1,7 @@
-﻿Namespace Logical
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Scripting.TokenIcer
+
+Namespace Logical
 
     Public Enum Tokens
         UNDEFINE
@@ -21,12 +24,12 @@
         Public ReadOnly Property Tokens As IReadOnlyDictionary(Of String, Tokens) =
             New Dictionary(Of String, Tokens) From {
  _
-            {"And", Logical.Tokens.Operator},
-            {"Or", Logical.Tokens.Operator},
-            {"Not", Logical.Tokens.Operator},
-            {"Xor", Logical.Tokens.Operator},
-            {"Nor", Logical.Tokens.Operator},
-            {"Nand", Logical.Tokens.Operator},
+            {"and", Logical.Tokens.Operator},
+            {"or", Logical.Tokens.Operator},
+            {"not", Logical.Tokens.Operator},
+            {"xor", Logical.Tokens.Operator},
+            {"nor", Logical.Tokens.Operator},
+            {"nand", Logical.Tokens.Operator},
             {"<<", Logical.Tokens.Operator},
             {"<", Logical.Tokens.Operator},
             {"<=", Logical.Tokens.Operator},
@@ -40,6 +43,58 @@
             {" ", Logical.Tokens.WhiteSpace}
         }
 
+        Const OPERATORS As String = "AndOrNotXorNorNand"
+        Const COMPARERS As String = "<<=>>~"
 
+        <Extension> Private Function __parseUNDEFINE(str As CharEnumerator, ByRef token As List(Of Char)) As Boolean
+
+        End Function
+
+        <Extension> Private Function __parseOperator(str As CharEnumerator, ByRef token As List(Of Char)) As Boolean
+
+        End Function
+
+        <Extension> Private Function __parseComparer(str As CharEnumerator, ByRef token As List(Of Char)) As Boolean
+
+        End Function
+
+
+        Public Function TryParse(s As String) As List(Of Token(Of Tokens))
+            Dim str As CharEnumerator = s.GetEnumerator
+            Dim tokens As New List(Of Token(Of Tokens))
+            Dim ch As Char
+            Dim token As New List(Of Char)
+            Dim type As Tokens = Logical.Tokens.UNDEFINE
+            Dim exitb As Boolean = False
+
+            If Not str.MoveNext() Then  ' Empty expression
+                Return New List(Of Token(Of Tokens))
+            End If
+
+            Do While True
+                ch = str.Current
+                token += ch
+
+                If OPERATORS.IndexOf(ch) > -1 Then
+                    Call __parseOperator(str, token)
+
+                ElseIf COMPARERS.IndexOf(ch) > -1 Then
+                    Call __parseComparer(str, token)
+
+                Else
+                    exitb = str.__parseUNDEFINE(token)
+                    type = Mathematical.Tokens.UNDEFINE
+                    tokens += New Token(Of Tokens)(type, New String(token))
+                End If
+
+                If Not exitb Then
+                    Exit Do
+                Else
+                    token.Clear()
+                End If
+            Loop
+
+            Return tokens
+        End Function
     End Module
 End Namespace
