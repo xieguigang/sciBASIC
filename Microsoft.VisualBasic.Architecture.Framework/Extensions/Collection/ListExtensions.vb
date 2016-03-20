e6a0e3b0877330a26a8c4a8fbb9ad120dac6038d
xieguigang <xie.guigang@gmail.com>
2016-03-20 20:22:46 +0800

    list iterator ctyper

--- Microsoft.VisualBasic.Architecture.Framework/Extensions/Collection/ListExtensions.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Extensions/Collection/ListExtensions.vb
@@ -2,6 +2,20 @@
 
 Public Module ListExtensions
 
+    <Extension> Public Function ToList(Of T, TOut)(source As IEnumerable(Of T),
+                                                   [CType] As Func(Of T, TOut),
+                                                   Optional parallel As Boolean = False) As List(Of TOut)
+        Dim result As List(Of TOut)
+
+        If parallel Then
+            result = (From x As T In source.AsParallel Select [CType](x)).ToList
+        Else
+            result = (From x As T In source Select [CType](x)).ToList
+        End If
+
+        Return result
+    End Function
+
     <Extension> Public Function ToList(Of T)(source As IEnumerable(Of T)) As List(Of T)
         Return New List(Of T)(source)
     End Function
