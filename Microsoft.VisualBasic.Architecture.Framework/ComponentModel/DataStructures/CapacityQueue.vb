99b484ef802458ee83f38bc57c89894eebf5c114
xieguigang <xie.guigang@gmail.com>
2016-03-21 17:04:19 +0800

    add a capacity queue object

---
+++ Microsoft.VisualBasic.Architecture.Framework/ComponentModel/DataStructures/CapacityQueue.vb
@@ -0,0 +1,58 @@
+﻿Imports Microsoft.VisualBasic.Serialization
+
+Namespace ComponentModel.Collection
+
+    Public Class CapacityQueue(Of T) : Inherits Language.ClassObject
+        Implements IEnumerable(Of T)
+
+        ReadOnly _queue As Queue(Of T)
+
+        Public ReadOnly Property Capacity As Integer
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="capacity">The initial number of elements that the System.Collections.Generic.Queue`1 can
+        ''' contain.</param>
+        Sub New(capacity As Integer)
+            _queue = New Queue(Of T)(capacity)
+        End Sub
+
+        Public Function Enqueue(x As T) As T
+            Dim o As T
+
+            Call _queue.Enqueue(x)
+
+            If _queue.Count = Capacity - 1 Then
+                o = _queue.Dequeue()
+            Else
+                o = _queue.Peek
+            End If
+
+            Return o
+        End Function
+
+        Public Sub Clear()
+            Call _queue.Clear()
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return Serialization.GetJson(Me.ToArray, GetType(T()))
+        End Function
+
+        Public Overloads Shared Operator +(q As CapacityQueue(Of T), x As T) As CapacityQueue(Of T)
+            Call q.Enqueue(x)
+            Return q
+        End Operator
+
+        Public Iterator Function GetEnumerator() As IEnumerator(Of T) Implements IEnumerable(Of T).GetEnumerator
+            For Each x As T In _queue
+                Yield x
+            Next
+        End Function
+
+        Private Iterator Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+            Yield GetEnumerator()
+        End Function
+    End Class
+End Namespace
--- Microsoft.VisualBasic.DataVisualization.DataMining.Framework/NeuralNetwork/TrainingUtils.vb
+++ Microsoft.VisualBasic.DataVisualization.DataMining.Framework/NeuralNetwork/TrainingUtils.vb
@@ -14,19 +14,35 @@
             Call Train()
         End Sub
 
+        Sub New(net As Network)
+            NeuronNetwork = net
+        End Sub
+
+        Public Sub Add(input As Double(), output As Double())
+            Call _dataSets.Add(New DataSet(input, output))
+        End Sub
+
         Public Sub Train()
             Call Helpers.Train(NeuronNetwork, _dataSets, TrainingType)
         End Sub
 
-        Public Sub Corrects(input As Double(), convertedResults As Double(), expectedResults As Double())
-            Dim offendingDataSet = _dataSets.FirstOrDefault(Function(x) x.Values.SequenceEqual(input) AndAlso x.Targets.SequenceEqual(convertedResults))
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="input">The inputs data</param>
+        ''' <param name="convertedResults">The error outputs</param>
+        ''' <param name="expectedResults">The corrects output</param>
+        Public Sub Corrects(input As Double(), convertedResults As Double(), expectedResults As Double(), Optional train As Boolean = True)
+            Dim offendingDataSet As DataSet = _dataSets.FirstOrDefault(Function(x) x.Values.SequenceEqual(input) AndAlso x.Targets.SequenceEqual(convertedResults))
             _dataSets.Remove(offendingDataSet)
 
             If Not _dataSets.Exists(Function(x) x.Values.SequenceEqual(input) AndAlso x.Targets.SequenceEqual(expectedResults)) Then
                 Call _dataSets.Add(New DataSet(input, expectedResults))
             End If
 
-            Call Train()
+            If train Then
+                Call Me.Train()
+            End If
         End Sub
     End Class
 End Namespace
