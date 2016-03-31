c0eb3a4d3638eacc73e60a54a0ef5c86fa923d7e
xieguigang <xie.guigang@gmail.com>
2016-03-31 22:38:50 +0800

    add a cluster tools

---
+++ Cli Tools/Cluster/CLI.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.DataMining.Framework.KMeans
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic
+
+Module CLI
+
+    <ExportAPI("/kmeans",
+               Info:="Performance a kmeans clustering operation.",
+               Usage:="/kmeans /MAT <entity_matrix.Csv> /n <num_of_cluster> [/map <Name> /out <cluster.csv>]")>
+    Public Function ClusterDataSet(args As CommandLine.CommandLine) As Integer
+        Dim inFile As String = args("/MAT")
+        Dim n As Integer = args.GetInt32("/n")
+        Dim out As String = args.GetValue("/out", inFile.TrimFileExt & ".Cluster.Csv")
+        Dim ds As IEnumerable(Of EntityLDM) = EntityLDM.Load(inFile, args.GetValue("/map", "Name"))
+        Dim maps As String() = ds.First.Properties.Keys.ToArray
+        Dim clusters As ClusterCollection(Of Entity) = n.ClusterDataSet(ds.ToArray(Function(x) x.ToModel))
+        Dim result As New List(Of EntityLDM)
+
+        n = 1
+
+        For Each cluster As Cluster(Of Entity) In clusters
+            Dim values As EntityLDM() = cluster.ToArray(Function(x) x.ToLDM(maps))
+
+            For Each x In values
+                x.Cluster = n
+            Next
+
+            result += values
+            n += 1
+        Next
+
+        Return result > out
+    End Function
+End Module
--- Microsoft.VisualBasic.DataMining.Framework/KMeans/Entity.vb
+++ Microsoft.VisualBasic.DataMining.Framework/KMeans/Entity.vb
@@ -7,6 +7,9 @@ Imports Microsoft.VisualBasic.Linq.Extensions
 
 Namespace KMeans
 
+    ''' <summary>
+    ''' 存储在Csv文件里面的数据模型
+    ''' </summary>
     Public Class EntityLDM : Inherits DynamicPropertyBase(Of Double)
         Implements sIdEnumerable
 
@@ -42,8 +45,16 @@ Namespace KMeans
                 .Properties = Properties.Values.ToArray
             }
         End Function
+
+        Public Shared Function Load(path As String, map As String) As EntityLDM()
+            Dim maps As New Dictionary(Of String, String) From {{map, NameOf(EntityLDM.Name)}}
+            Return path.LoadCsv(Of EntityLDM)(maps:=maps).ToArray
+        End Function
     End Class
 
+    ''' <summary>
+    ''' 计算所使用的对象实例实体模型
+    ''' </summary>
     Public Class Entity : Inherits EntityBase(Of Double)
         Implements sIdEnumerable
 
@@ -54,12 +65,12 @@ Namespace KMeans
         End Function
 
         ''' <summary>
-        ''' 
+        '''
         ''' </summary>
         ''' <param name="path">Csv文件之中除了第一列是名称标识符，其他的都必须是该实体对象的属性</param>
         ''' <returns></returns>
-        Public Shared Function Load(path As String) As Entity()
-            Dim data As List(Of EntityLDM) = path.LoadCsv(Of EntityLDM)
+        Public Shared Function Load(path As String, Optional map As String = "Name") As Entity()
+            Dim data As EntityLDM() = EntityLDM.Load(path, map)
             Dim source As Entity() = data.ToArray(
                 Function(x) New Entity With {
                     .uid = x.Name,
