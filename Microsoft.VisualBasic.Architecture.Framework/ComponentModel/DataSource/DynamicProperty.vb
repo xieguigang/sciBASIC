50710b23545ff1e017b98140e4c25dc117acf003
xieguigang <xie.guigang@gmail.com>
2016-04-05 03:14:40 +0800

    fix a bugs on the xml serialization

--- Microsoft.VisualBasic.Architecture.Framework/ComponentModel/DataSource/DynamicProperty.vb
+++ Microsoft.VisualBasic.Architecture.Framework/ComponentModel/DataSource/DynamicProperty.vb
@@ -1,4 +1,9 @@
-﻿Imports Microsoft.VisualBasic.Language
+﻿Imports System.Xml.Serialization
+Imports System.Web.Script.Serialization
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Serialization
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 
 Namespace ComponentModel.DataSourceModel
 
@@ -14,7 +19,12 @@ Namespace ComponentModel.DataSourceModel
     Public MustInherit Class DynamicPropertyBase(Of T)
         Implements IDynamicMeta(Of T)
 
-        Public Overridable Property Properties As Dictionary(Of String, T) Implements IDynamicMeta(Of T).Properties
+        ''' <summary>
+        '''
+        ''' </summary>
+        ''' <returns></returns>
+        ''' <remarks>Can not serialize the dictionary object in to xml document.</remarks>
+        <XmlIgnore> Public Overridable Property Properties As Dictionary(Of String, T) Implements IDynamicMeta(Of T).Properties
             Get
                 If _propHash Is Nothing Then
                     _propHash = New Dictionary(Of String, T)
@@ -51,5 +61,25 @@ Namespace ComponentModel.DataSourceModel
     ''' </summary>
     ''' <typeparam name="T"></typeparam>
     Public Class [Property](Of T) : Inherits DynamicPropertyBase(Of T)
+
+        <ScriptIgnore> Public Property source As Value(Of T)()
+            Get
+                Return Properties.ToArray(Function(x) New Value(Of T) With {.Name = x.Key, .x = x.Value})
+            End Get
+            Set(value As Value(Of T)())
+                Properties = value.ToDictionary(Function(x) x.Name, Function(x) x.x)
+            End Set
+        End Property
     End Class
+
+    Public Structure Value(Of T) : Implements sIdEnumerable
+
+        <XmlAttribute>
+        Public Property Name As String Implements sIdEnumerable.Identifier
+        <XmlElement> Public Property x As T
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+    End Structure
 End Namespace
