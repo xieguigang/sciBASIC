4cfedc69d7383d617745369d30c3c0a0d29a29d9
xieguigang <xie.guigang@gmail.com>
2016-04-12 00:08:18 +0800

    add html text parser

---
+++ Microsoft.VisualBasic.Architecture.Framework/Tools/HtmlParser/Table.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Text.RegularExpressions
+
+Namespace HtmlParser
+
+    Public Module TableParser
+
+        Public Function GetTablesHTML(html As String) As String()
+            Dim tbls As String() = Regex.Matches(html, "<table.+?</table>", RegexOptions.Singleline Or RegexOptions.IgnoreCase).ToArray
+            Return tbls
+        End Function
+
+        Public Function GetRowsHTML(table As String) As String()
+            Dim rows As String() = Regex.Matches(table, "<tr.+?</tr>", RegexOptions.Singleline Or RegexOptions.IgnoreCase).ToArray
+            Return rows
+        End Function
+
+        Public Function GetColumnsHTML(row As String) As String()
+            Dim cols As String() = Regex.Matches(row, "<td.+?</td>", RegexOptions.Singleline Or RegexOptions.IgnoreCase).ToArray
+            Return cols
+        End Function
+    End Module
+End Namespace
