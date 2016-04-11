68b3637d203445925acad114fc3edc157456e42c
xieguigang <xie.guigang@gmail.com>
2016-04-12 00:48:34 +0800

    trim html tag

--- Microsoft.VisualBasic.Architecture.Framework/Tools/HtmlParser/Table.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Tools/HtmlParser/Table.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.Linq
 
 Namespace HtmlParser
 
@@ -14,8 +15,14 @@ Namespace HtmlParser
             Return rows
         End Function
 
+        ''' <summary>
+        ''' The td tag is trimmed in this function.
+        ''' </summary>
+        ''' <param name="row"></param>
+        ''' <returns></returns>
         Public Function GetColumnsHTML(row As String) As String()
             Dim cols As String() = Regex.Matches(row, "<td.+?</td>", RegexOptions.Singleline Or RegexOptions.IgnoreCase).ToArray
+            cols = cols.ToArray(Function(s) s.GetValue)
             Return cols
         End Function
     End Module
