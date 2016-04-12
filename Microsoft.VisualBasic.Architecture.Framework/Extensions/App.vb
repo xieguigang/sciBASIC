8d413b72f9dc5f739afa94f59242cba0a26d5316
xieguigang <xie.guigang@gmail.com>
2016-04-12 15:42:39 +0800

    i joking

--- Microsoft.VisualBasic.Architecture.Framework/Extensions/App.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Extensions/App.vb
@@ -9,6 +9,28 @@ Imports Microsoft.VisualBasic.Parallel
 Imports Microsoft.VisualBasic.Parallel.Tasks
 Imports Microsoft.VisualBasic.Scripting.MetaData
 
+'                   _ooOoo_
+'                  o8888888o
+'                  88" . "88
+'                  (| -_- |)
+'                  O\  =  /O
+'               ____/`---'\____
+'             .'  \\|     |//  `.
+'            /  \\|||  :  |||//  \
+'           /  _||||| -:- |||||-  \
+'           |   | \\\  -  /// |   |
+'           | \_|  ''\---/''  |   |
+'           \  .-\__  `-`  ___/-. /
+'         ___`. .'  /--.--\  `. . __
+'      ."" '<  `.___\_<|>_/___.'  >'"".
+'     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+'     \  \ `-.   \_ __\ /__ _/   .-` /  /
+'======`-.____`-.___\_____/___.-`____.-'======
+'                   `=---='
+'^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+'           佛祖保佑       永无BUG
+'           心外无法       法外无心
+
 ''' <summary>
 ''' Provides information about, and means to manipulate, the current environment Application information collection.
 ''' (More easily runtime environment information provider on LINUX platform for visualbasic program.)
