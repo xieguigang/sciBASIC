3e38de1442b2e38e4bb628c1cc2945db5745499d
xieguigang <xie.guigang@gmail.com>
2016-04-22 14:19:29 +0800

    improvements on the range list

--- Microsoft.VisualBasic.Architecture.Framework/ComponentModel/Ranges/RangeList.vb
+++ Microsoft.VisualBasic.Architecture.Framework/ComponentModel/Ranges/RangeList.vb
@@ -17,13 +17,45 @@ Namespace ComponentModel.Ranges
             Return LQuery
         End Function
 
-        Public Function SelectValue(x As T) As V
+        Public Function SelectValue(x As T, Optional [throw] As Boolean = True, Optional ByRef success As Boolean = False) As V
             Dim n As RangeTagValue(Of T, V) = [Select](x)
             If n Is Nothing Then
-                Throw New DataException($"{x.GetJson} is not in any ranges!")
+                If [throw] Then
+                    Throw New DataException($"{x.GetJson} is not in any ranges!")
+                Else
+                    Return Nothing
+                End If
             Else
+                success = True
                 Return n.Value
             End If
         End Function
+
+        Public Function SelectValue(x As T, [default] As Func(Of T, V)) As V
+            Dim success As Boolean = False
+            Dim v As V = SelectValue(x, [throw]:=False, success:=success)
+
+            If success Then
+                Return v
+            Else
+                Return [default](x)
+            End If
+        End Function
+
+        Public ReadOnly Iterator Property Values As IEnumerable(Of V)
+            Get
+                For Each x As RangeTagValue(Of T, V) In Me
+                    Yield x.Value
+                Next
+            End Get
+        End Property
+
+        Public ReadOnly Iterator Property Keys As IEnumerable(Of Range(Of T))
+            Get
+                For Each x As RangeTagValue(Of T, V) In Me
+                    Yield x
+                Next
+            End Get
+        End Property
     End Class
 End Namespace
