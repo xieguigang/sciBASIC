102d34416856b4f8e629ed5244e585e02402c438
xieguigang <xie.guigang@gmail.com>
2016-05-03 07:51:04 +0800

    have no idea

---
+++ Datavisualization/Microsoft.VisualBasic.Imaging/Drawing3D/Surface.vb
@@ -0,0 +1,11 @@
+﻿
+Namespace Drawing3D
+
+    Public Class Surface
+
+        ''' <summary>
+        ''' 请注意，在这里面的点都是有顺序分别的
+        ''' </summary>
+        Protected _vertices() As Point3D
+    End Class
+End Namespace
