1c28a09682712000b64d0a7b64053777c86eae62
xieguigang <xie.guigang@gmail.com>
2016-05-04 03:27:31 +0800

    reorganized some namespace

--- Microsoft.VisualBasic.Architecture.Framework/Extensions/Extensions.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Extensions/Extensions.vb
@@ -700,31 +700,6 @@ Public Module Extensions
         End If
     End Function
 
-#If FRAMEWORD_CORE Then
-
-    '''<summary>
-    '''  Looks up a localized string similar to                     GNU GENERAL PUBLIC LICENSE
-    '''                       Version 3, 29 June 2007
-    '''
-    ''' Copyright (C) 2007 Free Software Foundation, Inc. &lt;http://fsf.org/&gt;
-    ''' Everyone is permitted to copy and distribute verbatim copies
-    ''' of this license document, but changing it is not allowed.
-    '''
-    '''                            Preamble
-    '''
-    '''  The GNU General Public License is a free, copyleft license for
-    '''software and other kinds of works.
-    '''
-    '''  The licenses for most software and other practical works are designed
-    '''to take away yo [rest of string was truncated]&quot;;.
-    '''</summary>
-    Public ReadOnly Property GPL3 As String
-        Get
-            Return My.Resources.gpl
-        End Get
-    End Property
-#End If
-
     ''' <summary>
     ''' Free this variable pointer in the memory.(销毁本对象类型在内存之中的指针)
     ''' </summary>
