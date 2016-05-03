3a7c327bfb77275136143102ceccbc2307af6c97
xieguigang <xie.guigang@gmail.com>
2016-05-04 03:41:10 +0800

    add a numbers group API

---
+++ Microsoft.VisualBasic.Architecture.Framework/Extensions/Math/NumberGroups.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Runtime.CompilerServices
+
+''' <summary>
+''' Simple number vector grouping
+''' </summary>
+Public Module NumberGroups
+
+    <Extension>
+    Public Function Groups(source As IEnumerable(Of Integer), offset As Integer) As List(Of Integer())
+        Dim list As New List(Of Integer())
+        Dim orders As Integer() = (From n As Integer
+                                   In source
+                                   Select n
+                                   Order By n Ascending).ToArray
+        Dim tag As Integer = orders(Scan0)
+        Dim tmp As New List(Of Integer) From {tag}
+
+        For Each x As Integer In orders.Skip(1)
+            If x - tag <= offset Then  ' 因为已经是经过排序了的，所以后面总是大于前面的
+                tmp += x
+            Else
+                tag = x
+                list += tmp.ToArray
+                tmp = New List(Of Integer) From {x}
+            End If
+        Next
+
+        If tmp.Count > 0 Then
+            list += tmp.ToArray
+        End If
+
+        Return list
+    End Function
+End Module
