497a6d3dfd3cf25c066eb319e680bb713b38df17
xieguigang <xie.guigang@gmail.com>
2016-05-06 19:24:22 +0800

    add a linux shell helper

---
+++ Microsoft.VisualBasic.Architecture.Framework/Language/UnixBash/LinuxRunHelper.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Reflection
+
+Namespace Language.UnixBash
+
+    Public Module LinuxRunHelper
+
+        Public Function ScriptMe() As String
+            Dim cmd As String = Assembly.GetExecutingAssembly.Location
+            Dim perl As String =
+$"#!/usr/bin/perl
+
+system(""mono {cmd.CliPath} $ARGV"");
+"
+            Return perl
+        End Function
+
+        Public Function PerlShell() As Integer
+            Return ScriptMe.SaveTo(Assembly.GetExecutingAssembly.Location.TrimFileExt)
+        End Function
+    End Module
+End Namespace
