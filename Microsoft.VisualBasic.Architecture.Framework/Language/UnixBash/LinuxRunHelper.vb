dcbc4b3da17230ab154a909593635735a81e926f
xieguigang <xie.guigang@gmail.com>
2016-05-07 13:17:55 +0800

    linux shell helper success

--- Microsoft.VisualBasic.Architecture.Framework/Language/UnixBash/LinuxRunHelper.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Language/UnixBash/LinuxRunHelper.vb
@@ -4,19 +4,30 @@ Namespace Language.UnixBash
 
     Public Module LinuxRunHelper
 
+        ''' <summary>
+        ''' perl ./<see cref="Assembly"/> @ARGV
+        ''' </summary>
+        ''' <returns></returns>
         Public Function ScriptMe() As String
             Dim cmd As String = Assembly.GetEntryAssembly.Location
             Dim perl As String =
 $"#!/usr/bin/perl
 
-print @ARGV;
-system(""mono {cmd.CliPath} @ARGV"");
+use strict;
+use warnings;
+use File::Basename;
+use File::Spec;
+
+my $cli = join "" "", @ARGV;
+
+print ""{App.AssemblyName} << $cli\n"";
+system(""mono {cmd.CliPath} $cli"");
 "
             Return perl
         End Function
 
         Public Function PerlShell() As Integer
-            Dim path As String = Assembly.GetEntryAssembly.Location.TrimFileExt
+            Dim path As String = Assembly.GetEntryAssembly.Location.TrimFileExt & ".pl"
             Return ScriptMe.SaveTo(path)
         End Function
     End Module
