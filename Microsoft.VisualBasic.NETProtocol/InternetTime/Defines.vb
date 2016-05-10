feb2ffaa3d1d7eb7808d8623926d0299c3a92f62
xieguigang <xie.guigang@gmail.com>
2016-05-11 01:14:19 +0800

    enums

---
+++ Microsoft.VisualBasic.NETProtocol/InternetTime/Defines.vb
@@ -0,0 +1,34 @@
+﻿Namespace InternetTime
+
+    ''' <summary>
+    ''' Leap indicator field values
+    ''' </summary>
+    Public Enum _LeapIndicator
+        NoWarning       '0 - No warning
+        LastMinute61    '1 - Last minute has 61 seconds
+        LastMinute59    '2 - Last minute has 59 seconds
+        Alarm           '3 - Alarm condition (clock not synchronized)
+    End Enum
+
+    ''' <summary>
+    ''' Mode field values
+    ''' </summary>
+    Public Enum _Mode
+        SymmetricActive     '1 - Symmetric active
+        SymmetricPassive    '2 - Symmetric pasive
+        Client              '3 - Client
+        Server              '4 - Server
+        Broadcast           '5 - Broadcast
+        Unknown             '0, 6, 7 - Reserved
+    End Enum
+
+    ''' <summary>
+    ''' Stratum field values
+    ''' </summary>
+    Public Enum _Stratum
+        Unspecified         '0 - unspecified or unavailable
+        PrimaryReference    '1 - primary reference (e.g. radio-clock)
+        SecondaryReference  '2-15 - secondary reference (via NTP or SNTP)
+        Reserved            '16-255 - reserved
+    End Enum
+End Namespace
--- Microsoft.VisualBasic.NETProtocol/InternetTime/InternetTime.vb
+++ Microsoft.VisualBasic.NETProtocol/InternetTime/InternetTime.vb
@@ -100,34 +100,7 @@ Imports System.Text
 'Of the approximate time from other sources. Since this only requires time accurate To a few decades, 
 'this Is Not a problem In general use.
 
-
-
 Namespace InternetTime
-    'Leap indicator field values
-    Public Enum _LeapIndicator
-        NoWarning       '0 - No warning
-        LastMinute61    '1 - Last minute has 61 seconds
-        LastMinute59    '2 - Last minute has 59 seconds
-        Alarm           '3 - Alarm condition (clock not synchronized)
-    End Enum
-
-    'Mode field values
-    Public Enum _Mode
-        SymmetricActive     '1 - Symmetric active
-        SymmetricPassive    '2 - Symmetric pasive
-        Client              '3 - Client
-        Server              '4 - Server
-        Broadcast           '5 - Broadcast
-        Unknown             '0, 6, 7 - Reserved
-    End Enum
-
-    'Stratum field values
-    Public Enum _Stratum
-        Unspecified         '0 - unspecified or unavailable
-        PrimaryReference    '1 - primary reference (e.g. radio-clock)
-        SecondaryReference  '2-15 - secondary reference (via NTP or SNTP)
-        Reserved            '16-255 - reserved
-    End Enum
 
     '/// <summary>
     '/// SNTPClient is a VB.NET# class designed to connect to time servers on the Internet and
