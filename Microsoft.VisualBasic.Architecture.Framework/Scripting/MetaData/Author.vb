760fb5b2e147127e3466876b22bee7af7debe29a
xieguigang <xie.guigang@gmail.com>
2016-05-17 13:08:18 +0800

    add author attribute for license

---
+++ Microsoft.VisualBasic.Architecture.Framework/Scripting/MetaData/Author.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Serialization
+
+Namespace Scripting.MetaData
+
+    <AttributeUsage(AttributeTargets.All, AllowMultiple:=True, Inherited:=True)>
+    Public Class Author : Inherits Attribute
+
+        Public ReadOnly Property value As NamedValue(Of String)
+
+        Sub New(name As String, email As String)
+            value = New NamedValue(Of String)(name, email)
+        End Sub
+
+        Public Sub EMail()
+            Call Diagnostics.Process.Start($"mailto://{value.x}")
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+    End Class
+End Namespace
