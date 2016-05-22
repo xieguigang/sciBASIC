ac7b57828a70987d9236b86e1c9aabcad4fe7d90
xieguigang <xie.guigang@gmail.com>
2016-05-23 06:36:30 +0800

    github API success

--- Microsoft.VisualBasic.Webservices.Bing/Test/Program.cs
+++ Microsoft.VisualBasic.Webservices.Bing/Test/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.VisualBasic.Extensions;
 using static Microsoft.VisualBasic.Webservices.Github.API.Search;
 using Microsoft.VisualBasic.Webservices.Github;
 using Microsoft.VisualBasic.Webservices.Github.API;
@@ -14,12 +15,13 @@ namespace Test
         static void Main(string[] args)
         {
 
-            var query = new UsersQuery { term ="xieguigang" };
+            var result = Microsoft.VisualBasic.Webservices.Bing.SearchEngineProvider.Search("GCModeller");
+            // Microsoft.VisualBasic.Webservices.Bing.SearchResult nextr = result.NextPage();
+
+            var query = new UsersQuery { term ="guigang" };
             var queryResult = Search.Users(query.Build(), UserSorts.joined);
 
-            var result = Microsoft.VisualBasic.Webservices.Bing.SearchEngineProvider.Search("GCModeller");
-            Microsoft.VisualBasic.Webservices.Bing.SearchResult nextr = result.NextPage();
-
+            Pause();
         }
     }
 }
