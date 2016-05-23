422ced932aff25c46efcd2fb9955210379304aa2
xieguigang <xie.guigang@gmail.com>
2016-05-23 19:18:45 +0800

    order list, should i departs the asc and desc orders??

--- Microsoft.VisualBasic.Architecture.Framework/ComponentModel/Ranges/RangeList.vb
+++ Microsoft.VisualBasic.Architecture.Framework/ComponentModel/Ranges/RangeList.vb
@@ -10,10 +10,13 @@ Namespace ComponentModel.Ranges
         End Sub
 
         Public Function [Select](x As T) As RangeTagValue(Of T, V)
-            Dim LQuery = (From r As RangeTagValue(Of T, V)
-                          In Me.AsQueryable
-                          Where r.IsInside(x)
-                          Select r).FirstOrDefault
+            Dim LQuery As RangeTagValue(Of T, V) =
+                LinqAPI.DefaultFirst(Of RangeTagValue(Of T, V)) <=
+                From r As RangeTagValue(Of T, V)
+                In Me.AsQueryable
+                Where r.IsInside(x)
+                Select r
+
             Return LQuery
         End Function
 
@@ -57,5 +60,33 @@ Namespace ComponentModel.Ranges
                 Next
             End Get
         End Property
+    End Class
+
+    Public Class OrderSelector(Of T As IComparable)
+
+        ReadOnly _source As T()
+
+        Sub New(source As IEnumerable(Of T), Optional asc As Boolean = True)
+            If asc Then
+                _source = source.OrderBy(Function(x) x).ToArray
+            Else
+                _source = source.OrderByDescending(Function(x) x).ToArray
+            End If
+        End Sub
+
+        ''' <summary>
+        ''' 直到当前元素大于指定值
+        ''' </summary>
+        ''' <param name="n"></param>
+        ''' <returns></returns>
+        Public Iterator Function SelectUntilGreaterThan(n As T) As IEnumerable(Of T)
+            For Each x In _source
+                If Language.LessThanOrEquals(x, n) Then
+                    Yield x
+                Else
+                    Exit For   ' 由于是经过排序了的，所以在这里不再小于的话，则后面的元素都不会再比他小了
+                End If
+            Next
+        End Function
     End Class
 End Namespace
