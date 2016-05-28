ad9afdacc640773f82f94b0d565535d01986f005
xieguigang <xie.guigang@gmail.com>
2016-05-29 02:37:34 +0800

    add organisation api

---
+++ GithubAPI/API/Organizations.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.Serialization
+Imports Microsoft.VisualBasic.Webservices.Github.Class
+
+Public Class Organizations
+
+    Public Function UserOrgs(username As String) As Organization()
+        Dim url As String = GithubAPI & $"/users/{username}/orgs"
+        Dim json As String = url.GetRequest(https:=True)
+        Dim orgs As Organization() = json.LoadObject(Of Organization())
+        Return orgs
+    End Function
+End Class
