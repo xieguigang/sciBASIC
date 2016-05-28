99a5733af4c6241cd7cad1314a4b535358ac95ec
xieguigang <xie.guigang@gmail.com>
2016-05-29 05:01:19 +0800

    try to translates some d3js function into visualbasic

--- Datavisualization/Microsoft.VisualBasic.Imaging/d3js/color/rgb.vb
+++ Datavisualization/Microsoft.VisualBasic.Imaging/d3js/color/rgb.vb
@@ -3,7 +3,7 @@
     Public Module rgb
 
         Public Function d3_rgb_hex(v As Byte) As String
-            Return If(v < "0x10", "0" + Math.Max(0, v).ToString(16), Math.Min(255, v).ToString(16))
+            Return If(v < "0x10", "0" + Math.Max(CByte(0), v).ToString(16), Math.Min(CByte(255), v).ToString(16))
         End Function
     End Module
 End Namespace
--- Datavisualization/Microsoft.VisualBasic.Imaging/d3js/interpolate/rgb.vb
+++ Datavisualization/Microsoft.VisualBasic.Imaging/d3js/interpolate/rgb.vb
@@ -5,16 +5,16 @@ Namespace d3.interpolate
 
     Public Module rgb
 
-        Public Function interpolateRgb(a As Drawing.Color, b As Drawing.Color) As Func(Of Integer, String)
-            a = d3.rgb(a)
-            b = d3.rgb(b)
+        Public Function interpolateRgb(a As Drawing.Color, b As Drawing.Color) As Func(Of Byte, String)
             Dim ar = a.R,
-      ag = a.G,
-      ab = a.B,
-      br = b.R - ar,
-      bg = b.G - ag,
-      bb = b.B - ab
-            Return Function(t As Integer) $"#{d3_rgb_hex(Math.Round(ar + br * t))}{d3_rgb_hex(Math.Round(ag + bg * t))}{d3_rgb_hex(Math.Round(ab + bb * t))}"
+                ag = a.G,
+                ab = a.B,
+                br = b.R - ar,
+                bg = b.G - ag,
+                bb = b.B - ab
+
+            Return Function(t As Byte) _
+                       $"#{d3_rgb_hex(Math.Round(ar + br * t))}{d3_rgb_hex(Math.Round(ag + bg * t))}{d3_rgb_hex(Math.Round(ab + bb * t))}"
         End Function
     End Module
 End Namespace
