e3d5298eb608d3e9cd2ebfca3762c5563e7ff082
xieguigang <xie.guigang@gmail.com>
2016-06-01 17:29:23 +0800

    schema interface

---
+++ Microsoft.VisualBasic.DataSchema.Extensions/Schema.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Serialization
+
+Public Class Schema(Of T As Attribute)
+
+    Public Property SchemaName As String
+    Public Property Fields As BindProperty(Of T)()
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
