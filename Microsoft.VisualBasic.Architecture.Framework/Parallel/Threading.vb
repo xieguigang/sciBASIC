c628208d1ee0b60426e761349a8825629ac51385
xieguigang <xie.guigang@gmail.com>
2016-06-03 17:05:47 +0800

    queue of the threads

--- Microsoft.VisualBasic.Architecture.Framework/Parallel/Threading.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Parallel/Threading.vb
@@ -1,40 +1,43 @@
-﻿Imports System.Runtime.CompilerServices
+﻿'Imports System.Runtime.CompilerServices
 
-Public Module PBSThreading
+'Namespace Parallel
 
-    ''' <summary>
-    ''' 
-    ''' </summary>
-    ''' <param name="Handles"></param>
-    ''' <param name="NumOfThreads">同时执行的句柄的数目</param>
-    ''' <returns></returns>
-    ''' <remarks></remarks>
-    <Extension> Public Function Invoke([Handles] As System.Action(), NumOfThreads As Integer) As Integer
-        Dim CurrentThreads As Integer
-        Dim InternalStartThread As System.Action(Of System.Action) = Sub(Handle As System.Action)
-                                                                         CurrentThreads += 1
-                                                                         Call Handle()
-                                                                         CurrentThreads -= 1
-                                                                     End Sub '启动计算线程
-        Dim InternalCreateThread As System.Action(Of System.Action) =
-            Sub(Handle As System.Action) Call (New System.Threading.Thread(start:=Sub() InternalStartThread([Handle]))).Start()
-        Dim St As Stopwatch = Stopwatch.StartNew
+'    Public Module PBSThreading
 
-        For Each Handle As System.Action In [Handles]
-_AWAIT:     Call System.Threading.Thread.Sleep(1)
+'        ''' <summary>
+'        ''' 
+'        ''' </summary>
+'        ''' <param name="Handles"></param>
+'        ''' <param name="NumOfThreads">同时执行的句柄的数目</param>
+'        ''' <returns></returns>
+'        ''' <remarks></remarks>
+'        <Extension> Public Function Invoke([Handles] As System.Action(), NumOfThreads As Integer) As Integer
+'            Dim CurrentThreads As Integer
+'            Dim InternalStartThread As System.Action(Of System.Action) = Sub(Handle As System.Action)
+'                                                                             CurrentThreads += 1
+'                                                                             Call Handle()
+'                                                                             CurrentThreads -= 1
+'                                                                         End Sub '启动计算线程
+'            Dim InternalCreateThread As System.Action(Of System.Action) =
+'            Sub(Handle As System.Action) Call (New System.Threading.Thread(start:=Sub() InternalStartThread([Handle]))).Start()
+'            Dim St As Stopwatch = Stopwatch.StartNew
 
-            If CurrentThreads > NumOfThreads Then
-                Call System.Threading.Thread.Sleep(10)
-                GoTo _AWAIT
-            End If
+'            For Each Handle As System.Action In [Handles]
+'_AWAIT:         Call System.Threading.Thread.Sleep(1)
 
-            Call InternalCreateThread(Handle)
-        Next
+'                If CurrentThreads > NumOfThreads Then
+'                    Call System.Threading.Thread.Sleep(10)
+'                    GoTo _AWAIT
+'                End If
 
-        Do While CurrentThreads > 0
-            Call System.Threading.Thread.Sleep(10)
-        Loop
+'                Call InternalCreateThread(Handle)
+'            Next
 
-        Return St.ElapsedTicks
-    End Function
-End Module
+'            Do While CurrentThreads > 0
+'                Call System.Threading.Thread.Sleep(10)
+'            Loop
+
+'            Return St.ElapsedTicks
+'        End Function
+'    End Module
+'End Namespace
