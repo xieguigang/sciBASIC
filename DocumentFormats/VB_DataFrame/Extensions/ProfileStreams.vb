74fce26a99db8b7dfbee108d101bca5ee6c79ffc
关倩倩 <amethyst.asuka@gcmodeller.org>
2016-06-05 02:44:31 +0800

    interface of profile engine

---
+++ DocumentFormats/VB_DataFrame/Extensions/ProfileStreams.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel
+Imports Microsoft.VisualBasic.ComponentModel.Settings
+
+Public Module ProfileStreams
+
+    <Extension>
+    Public Function WriteProfiles(Of T As IProfile)(x As T, path As String, Optional encoding As Encodings = Encodings.UTF8) As Boolean
+
+    End Function
+
+    <Extension>
+    Public Function GenerateProfiles(Of T As IProfile)(x As T) As KeyValuePair()
+
+    End Function
+End Module
