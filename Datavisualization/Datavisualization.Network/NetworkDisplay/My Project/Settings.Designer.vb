bccc5a436667deaee8791700c3a746fb4ef0850c
xieguigang <xie.guigang@gmail.com>
2016-06-05 19:21:47 +0800

    test success!!

--- Datavisualization/Datavisualization.Network/NetworkDisplay/My Project/Settings.Designer.vb
+++ Datavisualization/Datavisualization.Network/NetworkDisplay/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.NetworkDisplay.My.MySettings
+        Friend ReadOnly Property Settings() As Global.Microsoft.VisualBasic.DataVisualization.Network.Canvas.My.MySettings
             Get
-                Return Global.NetworkDisplay.My.MySettings.Default
+                Return Global.Microsoft.VisualBasic.DataVisualization.Network.Canvas.My.MySettings.Default
             End Get
         End Property
     End Module
