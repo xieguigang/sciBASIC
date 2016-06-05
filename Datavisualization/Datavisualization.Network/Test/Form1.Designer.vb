dcb032daadb3e4558db2eb7f1712b621ecd04d34
xieguigang <xie.guigang@gmail.com>
2016-06-05 23:26:48 +0800

    tweaks

--- Datavisualization/Datavisualization.Network/Test/Form1.Designer.vb
+++ Datavisualization/Datavisualization.Network/Test/Form1.Designer.vb
@@ -30,7 +30,7 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(993, 672)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Network Canvas"
         Me.ResumeLayout(False)
 
     End Sub
--- Datavisualization/Datavisualization.Network/Test/Form1.vb
+++ Datavisualization/Datavisualization.Network/Test/Form1.vb
@@ -3,30 +3,16 @@ Imports Microsoft.VisualBasic.DataVisualization.Network.Canvas
 Imports Microsoft.VisualBasic.DataVisualization.Network.FileStream
 
 Public Class Form1
-    Dim canvas As New Canvas
-    Sub New()
 
-        Call Module1.test()
-
-        ' This call is required by the designer.
-        InitializeComponent()
-
-        ' Add any initialization after the InitializeComponent() call.
-
-        Me.Controls.Add(canvas)
-        canvas.Dock = DockStyle.Fill
-    End Sub
+    Dim canvas As New Canvas With {
+        .Dock = DockStyle.Fill
+    }
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'canvas.Graph = CytoscapeExportAsGraph(
-        '    App.HOME & "\Resources\Edges.csv",
-        '    App.HOME & "\Resources\Nodes.csv")
-        'Try
+        Call Me.Controls.Add(canvas)
+
         canvas.Graph = CytoscapeExportAsGraph(
-         "F:\VisualBasic_AppFramework\Datavisualization\Datavisualization.Network\net_test\xcb-main-Edges.csv",
-         "F:\VisualBasic_AppFramework\Datavisualization\Datavisualization.Network\net_test\xcb-main-Nodes.csv")
-        '  Catch ex As Exception
-        '   Call App.LogException(ex)
-        '   End Try
+            App.HOME & "\Resources\xcb-main-Edges.csv",
+            App.HOME & "\Resources\xcb-main-Nodes.csv")
     End Sub
 End Class
