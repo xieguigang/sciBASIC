282ec4f22b06b002de4ac443d2fde0a1e865b4d6
xieguigang <xie.guigang@gmail.com>
2016-06-06 21:42:41 +0800

    update previews

--- Datavisualization/Datavisualization.Network/Test/Form1.Designer.vb
+++ Datavisualization/Datavisualization.Network/Test/Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(993, 672)
+        Me.ClientSize = New System.Drawing.Size(584, 381)
         Me.Name = "Form1"
         Me.Text = "Network Canvas"
         Me.ResumeLayout(False)
