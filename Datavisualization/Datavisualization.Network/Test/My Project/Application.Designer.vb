77ce39d115bf6b9059f9a1b95555b941aa677dd5
xieguigang <xie.guigang@gmail.com>
2016-06-10 03:47:18 +0800

    add 3d demo gif previews

--- Datavisualization/Datavisualization.Network/Test/My Project/Application.Designer.vb
+++ Datavisualization/Datavisualization.Network/Test/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Test.Form1
+            Me.MainForm = Global.Test.FormCanvas
         End Sub
     End Class
 End Namespace
