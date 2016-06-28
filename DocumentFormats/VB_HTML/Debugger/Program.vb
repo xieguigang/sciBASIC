b94339967ade47c18c5744bf8604e4a0a21020df
xieguigang <xie.guigang@gmail.com>
2016-06-29 07:43:17 +0800

    html markdown info

--- DocumentFormats/VB_HTML/Debugger/Program.vb
+++ DocumentFormats/VB_HTML/Debugger/Program.vb
@@ -1,4 +1,31 @@
-﻿
+﻿#Region "Microsoft.VisualBasic::4c59b09d84a397dcd3043ddb42979d24, ..\VB_HTML\Debugger\Program.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xieguigang (xie.guigang@live.com)
+    ' 
+    ' Copyright (c) 2016 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+
 Imports Microsoft.VisualBasic.MarkupLanguage
 Imports Microsoft.VisualBasic.MarkupLanguage.HTML
 Imports Microsoft.VisualBasic.MarkupLanguage.MarkDown
@@ -21,3 +48,4 @@ Module Program
         Call doc.Save("./trest.html")
     End Sub
 End Module
+
--- DocumentFormats/VB_HTML/VB_HTML/MarkDown/Markdown.vb
+++ DocumentFormats/VB_HTML/VB_HTML/MarkDown/Markdown.vb
@@ -1,4 +1,31 @@
-﻿Imports System
+﻿#Region "Microsoft.VisualBasic::02121fc39f26556d092468603d594730, ..\VB_HTML\VB_HTML\MarkDown\Markdown.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xieguigang (xie.guigang@live.com)
+    ' 
+    ' Copyright (c) 2016 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Imports System
 Imports System.Collections.Generic
 Imports System.Configuration
 Imports System.Text
