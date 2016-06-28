3e74510a4384e83ac82acee7a92afa44300197fe
xieguigang <xie.guigang@gmail.com>
2016-06-29 07:45:50 +0800

    rdf

--- DocumentFormats/RDF/RDF/Serializer/Serializer.vb
+++ DocumentFormats/RDF/RDF/Serializer/Serializer.vb
@@ -1,4 +1,31 @@
-﻿Imports System.Text
+﻿#Region "Microsoft.VisualBasic::682a446c3a9bdb4b1e43d0a25a3431e3, ..\RDF\RDF\Serializer\Serializer.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xieguigang (xie.guigang@live.com)
+    ' 
+    ' Copyright (c) 2016 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Imports System.Text
 Imports Microsoft.VisualBasic.DocumentFormat.RDF.Serialization
 
 Public Class Schema
@@ -190,3 +217,4 @@ Public Class Serializer(Of T As Class)
         Return DeSerialization(Stream)
     End Function
 End Class
+
