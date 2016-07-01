4e1d3f0c390b238b5cf1691c72807afea552b39a
xieguigang <xie.guigang@gmail.com>
2016-07-01 14:14:53 +0800

    wait

---
+++ DocumentFormats/VB_DataFrame/VB_DataFrame.Extensions/Extensions.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module SchemasAPI
+
+    <Extension>
+    Public Function SaveData(Of T As Class)(source As IEnumerable(Of T), DIR As String, Optional encoding As Encodings = Encodings.UTF8) As Boolean
+
+    End Function
+End Module
---
+++ DocumentFormats/VB_DataFrame/VB_DataFrame.Extensions/Schema.vb
@@ -0,0 +1,38 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Serialization.JSON
+Imports Microsoft.VisualBasic.Linq
+
+''' <summary>
+''' The schema project json file.
+''' </summary>
+Public Class Schema : Inherits ClassObject
+
+    ''' <summary>
+    ''' 默认的主文件的名称
+    ''' </summary>
+    Public Const DefaultName As String = NameOf(Schema) & ".json"
+
+    ''' <summary>
+    ''' {member.Name, fileName}
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Members As NamedValue(Of String)()
+        Get
+            Return Tables.ToArray(
+                Function(x) New NamedValue(Of String) With {
+                    .Name = x.Key,
+                    .x = x.Value
+                })
+        End Get
+        Set(value As NamedValue(Of String)())
+            _Tables = value.ToDictionary(Function(x) x.Name, Function(x) x.x)
+        End Set
+    End Property
+
+    Public ReadOnly Property Tables As IReadOnlyDictionary(Of String, String)
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
