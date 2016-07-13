b9ebaf7254b43da650e7604d162e3faffa27689f
关倩倩 <amethyst.asuka@gcmodeller.org>
2016-07-13 17:38:53 +0800

    improvem,ents on large csv document save

--- DocumentFormats/VB_DataFrame/VB_DataFrame/DocumentStream/StreamIO.vb
+++ DocumentFormats/VB_DataFrame/VB_DataFrame/DocumentStream/StreamIO.vb
@@ -1,27 +1,27 @@
 ﻿#Region "Microsoft.VisualBasic::dd68b5dafd9441d8dd9d01840d368595, ..\VisualBasic_AppFramework\DocumentFormats\VB_DataFrame\VB_DataFrame\DocumentStream\StreamIO.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -29,6 +29,7 @@ Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports System.Text
 Imports Microsoft.VisualBasic.DocumentFormat.Csv.StorageProvider.ComponentModels
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 
 Namespace DocumentStream
@@ -122,26 +123,38 @@ Namespace DocumentStream
             End Try
         End Function
 
+        ''' <summary>
+        ''' Save document data in rows blocks
+        ''' </summary>
+        ''' <param name="filepath"></param>
+        ''' <param name="rowBuffer"></param>
+        ''' <param name="encoding"></param>
+        ''' <returns></returns>
         Private Function __lazyInner(filepath As String, rowBuffer As RowObject(), encoding As Encoding) As Boolean
             Dim stopWatch = System.Diagnostics.Stopwatch.StartNew
-            Dim chunks As RowObject()() = rowBuffer.Split(10240)
+            Dim title As RowObject = rowBuffer.First
+            Dim chunks As IEnumerable(Of RowObject()) = rowBuffer.Skip(1).SplitIterator(10240)
             Dim handle As IO.FileStream =
                 IO.File.Open(filepath,
                              IO.FileMode.OpenOrCreate,
                              IO.FileAccess.ReadWrite)
 
             Using writer As New StreamWriter(handle, encoding)
-                Dim first As RowObject() = chunks(Scan0)
-
                 ' 第一行会因为并行化而出现在下几行的BUG，所以在这里分开对待
-                Call writer.WriteLine(first.First.AsLine)
-                Call writer.WriteLine(String.Join(vbCrLf, first.Skip(1).ToArray(Function(x) x.AsLine)))
+                Call writer.WriteLine(title.AsLine)
 
-                For Each block As RowObject() In chunks.Skip(1)
-                    Dim sBlock As String = (From row As RowObject
-                                            In block.AsParallel
-                                            Select row.AsLine).JoinBy(vbCrLf)
-                    Call writer.WriteLine(sBlock)
+                For Each block As RowObject() In chunks
+                    Dim sBlock As String() =
+                        LinqAPI.Exec(Of String) <=
+ _
+                        From row As RowObject
+                        In block.AsParallel
+                        Select row.AsLine
+
+                    For Each line As String In sBlock
+                        Call writer.WriteLine(line)
+                    Next
+
                     Call Console.Write(".")
                 Next
             End Using
