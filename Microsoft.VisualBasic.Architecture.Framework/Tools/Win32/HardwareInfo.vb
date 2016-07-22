a138521a17ef7b4b59ddfd84e35ad51f74ea4209
关倩倩 <amethyst.asuka@gcmodeller.org>
2016-07-22 21:28:24 +0800

    hard drive info success

--- Microsoft.VisualBasic.Architecture.Framework/Tools/Win32/HardwareInfo.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Tools/Win32/HardwareInfo.vb
@@ -1,4 +1,8 @@
-﻿Namespace Win32
+﻿Imports System.Management
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace Win32
 
     ''' <summary>
     ''' Get mother board serial numbers and CPU IDs in Visual Basic .NET
@@ -29,7 +33,7 @@
         ''' The following code gets a WMI object and selects Win32_Processor objects. It loops through them getting their processor IDs. 
         ''' </summary>
         ''' <returns></returns>
-        Public Function CpuId() As String
+        Public Function CPU_Id() As String
             Dim computer As String = "."
             Dim wmi As Object = GetObject("winmgmts:" & "{impersonationLevel=impersonate}!\\" & computer & "\root\cimv2")
             Dim processors As Object = wmi.ExecQuery("Select * from Win32_Processor")
@@ -42,5 +46,59 @@
             Dim uid As String = String.Join("-", cpu_ids.ToArray)
             Return uid
         End Function
+
+        ''' <summary>
+        ''' How to Retrieve the REAL Hard Drive Serial Number.
+        ''' 
+        ''' Shows you how to obtain the hardware serial number set by the manufacturer and 
+        ''' not the Volume Serial Number that changes after you format a hard drive.
+        ''' </summary>
+        ''' <returns></returns>
+        ''' <remarks>
+        ''' http://www.codeproject.com/Articles/6077/How-to-Retrieve-the-REAL-Hard-Drive-Serial-Number
+        ''' </remarks>
+        Public Function HarddriveInfo() As HardDrive()
+            Dim hds As New List(Of HardDrive)
+            Dim searcher As New ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive")
+            Dim i As Pointer = 0
+
+            For Each wmi_HD As ManagementObject In searcher.[Get]()
+                Dim model As String = wmi_HD("Model").ToString()
+                Dim type As String = wmi_HD("InterfaceType").ToString()
+
+                hds += New HardDrive With {
+                    .Model = model,
+                    .Type = type
+                }
+            Next
+
+            searcher = New ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMedia")
+
+            For Each wmi_HD As ManagementObject In searcher.[Get]()
+                ' get the hard drive from collection
+                ' using index
+                Dim hd As HardDrive = hds(++i)
+
+                ' get the hardware serial no.
+                If wmi_HD("SerialNumber") Is Nothing Then
+                    hd.SerialNo = "None"
+                Else
+                    hd.SerialNo = wmi_HD("SerialNumber").ToString()
+                End If
+            Next
+
+            Return hds
+        End Function
     End Module
+
+    Public Class HardDrive
+
+        Public Property Model As String
+        Public Property Type As String
+        Public Property SerialNo As String
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+    End Class
 End Namespace
