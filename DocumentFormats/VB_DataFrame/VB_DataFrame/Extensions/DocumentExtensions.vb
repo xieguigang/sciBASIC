c6ff1f0d001adf2c24b35b64500f1b8fdb519add
xieguigang <xie.guigang@gmail.com>
2016-07-31 20:07:50 +0800

    improvements on the VisualBasic table object extensions

---
+++ DocumentFormats/VB_DataFrame/VB_DataFrame/Extensions/DocumentExtensions.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.DocumentFormat.Csv.DocumentStream
+
+Public Module DocumentExtensions
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="cols"><see cref="File.Columns"/> filtering results.</param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function JoinColumns(cols As IEnumerable(Of String())) As DocumentStream.File
+        Dim array As String()() = cols.ToArray
+        Dim out As New DocumentStream.File
+
+        For i As Integer = 0 To array.First.Length - 1
+            Dim ind As Integer = i
+            out += New RowObject(array.Select(Function(x) x(ind)))
+        Next
+
+        Return out
+    End Function
+
+    <Extension>
+    Public Function Apply(ByRef row As RowObject, action As Func(Of String, String), Optional skip As Integer = 0) As RowObject
+        For i As Integer = skip To row._innerColumns.Count - 1
+            row._innerColumns(i) = action(row._innerColumns(i))
+        Next
+
+        Return row
+    End Function
+End Module
