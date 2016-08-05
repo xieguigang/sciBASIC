44eef3e4c50eda149d04c8593e14ceff68f866a3
xieguigang <genetics@smrucc.org>
2016-08-05 18:59:50 +0800

    extension for load dataset and entityobject collection from excel xlsx

--- DocumentFormats/VB_DataFrame/VB_DataFrame/Excel/Extensions.vb
+++ DocumentFormats/VB_DataFrame/VB_DataFrame/Excel/Extensions.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.DocumentFormat.Csv.DocumentStream
 
 Namespace Excel
 
@@ -24,8 +25,59 @@ Namespace Excel
         Public Function ReadXlsx(Of T As Class)(file As String,
                                                 Optional sheetName As String = "Sheet1",
                                                 Optional maps As Dictionary(Of String, String) = Nothing) As T()
-            Dim df As DocumentStream.DataFrame = file.ReadXlsx(sheetName)
+            Dim df As DataFrame = file.ReadXlsx(sheetName)
             Return df.AsDataSource(Of T)(False, maps)
         End Function
+
+        ''' <summary>
+        ''' <see cref="DataSet"/>
+        ''' </summary>
+        ''' <param name="file"></param>
+        ''' <param name="sheetName"></param>
+        ''' <param name="uidMaps"></param>
+        ''' <returns></returns>
+        <Extension>
+        Public Function LoadDataSet(file As String,
+                                    Optional sheetName As String = "Sheet1",
+                                    Optional uidMaps As String = Nothing) As DataSet()
+            Dim df As DataFrame = file.ReadXlsx(sheetName)
+            Call df.__updateMaps(uidMaps)
+            Return df.AsDataSource(Of DataSet)(False)
+        End Function
+
+        ''' <summary>
+        ''' <see cref="EntityObject"/>
+        ''' </summary>
+        ''' <param name="file"></param>
+        ''' <param name="sheetName"></param>
+        ''' <param name="uidMaps"></param>
+        ''' <returns></returns>
+        <Extension>
+        Public Function LoadEntitySet(file As String,
+                                      Optional sheetName As String = "Sheet1",
+                                      Optional uidMaps As String = Nothing) As EntityObject()
+            Dim df As DataFrame = file.ReadXlsx(sheetName)
+            Call df.__updateMaps(uidMaps)
+            Return df.AsDataSource(Of EntityObject)(False)
+        End Function
+
+        ''' <summary>
+        ''' 仅限于 <see cref="DataSet"/>和<see cref="EntityObject"/>
+        ''' </summary>
+        ''' <param name="df"></param>
+        ''' <param name="mapName"></param>
+        ''' 
+        <Extension>
+        Private Sub __updateMaps(df As DataFrame, mapName As String)
+            If String.IsNullOrEmpty(mapName) Then
+                mapName = df.HeadTitles(Scan0)
+            End If
+
+            Dim maps As New Dictionary(Of String, String) From {
+                {mapName, NameOf(DataSet.Identifier)}
+            }
+
+            Call df.ChangeMapping(maps)
+        End Sub
     End Module
 End Namespace
