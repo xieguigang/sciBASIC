73c10f0c25d1a89143b4332f12a5c6e4b7aebf25
xieguigang <xie.guigang@gmail.com>
2016-08-10 17:05:28 +0800

    improvements on the thread model

--- Microsoft.VisualBasic.Architecture.Framework/Parallel/Tasks/TaskQueue.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Parallel/Tasks/TaskQueue.vb
@@ -34,12 +34,25 @@ Namespace Parallel.Tasks
     End Interface
 
     ''' <summary>
-    ''' 
+    ''' 这个只有一条线程来执行
     ''' </summary>
     Public Class TaskQueue(Of T) : Implements IDisposable
 
         ReadOnly __tasks As New Queue(Of __task)
 
+        ''' <summary>
+        ''' 返回当前的任务池之中的任务数量
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property Tasks As Integer
+            Get
+                Return __tasks.Count
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' 会单独启动一条新的线程来用来执行任务队列
+        ''' </summary>
         Sub New()
             Call RunTask(AddressOf __taskQueueEXEC)
         End Sub
@@ -51,7 +64,8 @@ Namespace Parallel.Tasks
         ''' <returns>假若本对象已经开始Dispose了，则为完成的任务都会返回Nothing</returns>
         Public Function Join(handle As Func(Of T)) As T
             Dim task As New __task With {
-                .handle = handle
+                .handle = handle,
+                .receiveDone = New ManualResetEvent(False)
             }
             Call __tasks.Enqueue(task)
             Call task.receiveDone.WaitOne()
@@ -78,14 +92,22 @@ Namespace Parallel.Tasks
                 .callback = callback
             }
             Call __tasks.Enqueue(task)
-
-            If Not callback Is Nothing Then
-                Call RunTask(AddressOf task.TriggerCallback)
-            End If
         End Sub
 
+        ''' <summary>
+        ''' 当这个属性为False的时候说明没有任务在执行，此时为空闲状态
+        ''' </summary>
+        ''' <returns></returns>
         Public ReadOnly Property RunningTask As Boolean
+        Public ReadOnly Property uid As Integer
+            Get
+                Return Me.GetHashCode
+            End Get
+        End Property
 
+        ''' <summary>
+        ''' 有一条线程单独执行这个任务队列
+        ''' </summary>
         Private Sub __taskQueueEXEC()
             Do While Not disposedValue
                 If Not __tasks.Count = 0 Then
@@ -95,16 +117,16 @@ Namespace Parallel.Tasks
                     Call task.receiveDone.Set()
                     _RunningTask = False
                 Else
-                    Call Thread.Sleep(1)
+                    Call Thread.Sleep(1) ' 当前的线程处于空闲的状态
                 End If
             Loop
         End Sub
 
-        Private Class __task
+        Private Structure __task
 
             Public callback As Action(Of T)
             Public handle As Func(Of T)
-            Public receiveDone As New ManualResetEvent(False)
+            Public receiveDone As ManualResetEvent
 
             Public ReadOnly Property Value As T
 
@@ -115,14 +137,12 @@ Namespace Parallel.Tasks
                     Call App.LogException(ex)
                     Call ex.PrintException
                 End Try
-            End Sub
 
-            Sub TriggerCallback()
-                Call receiveDone.WaitOne()
-                Call receiveDone.Reset()
-                Call callback(Value)
+                If Not callback Is Nothing Then
+                    Call callback(Value)
+                End If
             End Sub
-        End Class
+        End Structure
 
 #Region "IDisposable Support"
         Private disposedValue As Boolean ' To detect redundant calls
---
+++ Microsoft.VisualBasic.Architecture.Framework/Parallel/Threads/ThreadPool.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Text
+Imports System.Threading
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Parallel.Linq
+Imports Microsoft.VisualBasic.Parallel.Tasks
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace Parallel.Threads
+
+    ''' <summary>
+    ''' 使用多条线程来执行任务队列
+    ''' </summary>
+    Public Class ThreadPool
+
+        ReadOnly __threads As TaskQueue(Of Long)()
+
+        Sub New(maxThread As Integer)
+            __threads = New TaskQueue(Of Long)(maxThread) {}
+
+            For i As Integer = 0 To __threads.Length - 1
+                __threads(i) = New TaskQueue(Of Long)
+            Next
+        End Sub
+
+        Sub New()
+            Me.New(LQuerySchedule.Recommended_NUM_THREADS)
+        End Sub
+
+        Public Sub RunTask(task As Action, Optional callback As Action(Of Long) = Nothing)
+            Dim h As Func(Of Long) = Function() Time(work:=task)
+            Call GetAvaliableThread.Enqueue(h, callback)
+        End Sub
+
+        ''' <summary>
+        ''' 这个函数总是会返回一个线程对象的
+        ''' 
+        ''' + 当有空闲的线程，会返回第一个空闲的线程
+        ''' + 当没有空闲的线程，则会返回任务队列最短的线程
+        ''' </summary>
+        ''' <returns></returns>
+        Private Function GetAvaliableThread() As TaskQueue(Of Long)
+            Dim [short] As TaskQueue(Of Long) = __threads.First
+
+            For Each t In __threads
+                If Not t.RunningTask Then
+                    Return t
+                Else
+                    If [short].Tasks > t.Tasks Then
+                        [short] = t
+                    End If
+                End If
+            Next
+
+            Return [short]
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return __threads.GetJson
+        End Function
+    End Class
+End Namespace
