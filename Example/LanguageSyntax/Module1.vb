e28879cdceccaa0313a1bba4bd35332f7fec2a45
xieguigang <genetics@smrucc.org>
2016-09-07 13:32:21 +0800

    add language syntax example

---
+++ Example/LanguageSyntax/Module1.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Language
+
+Module Module1
+
+    Sub Main()
+        Dim i As Int = 0
+
+        Call i.RangeDescription.__DEBUG_ECHO
+        Call (++i).RangeDescription.__DEBUG_ECHO
+
+        Pause()
+    End Sub
+
+    <Extension>
+    Public Function RangeDescription(x As Integer) As String
+        Return New Int(x).RangeDescription
+    End Function
+
+    <Extension>
+    Public Function RangeDescription(x As Int) As String
+        If 0 < x <= 100 Then
+            Return "0-100"
+        ElseIf 100 < x <= 200 Then
+            Return "100-200"
+        ElseIf 200 < x <= 300 Then
+            Return "200-300"
+        Else
+            Return "undefined"
+        End If
+    End Function
+End Module
