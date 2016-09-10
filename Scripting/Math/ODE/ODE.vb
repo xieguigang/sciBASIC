dacbf7d3518dc4b64e92dcfa4aea201e43bba447
xieguigang <xie.guigang@gmail.com>
2016-09-10 22:19:16 +0800

    fix ode initial value

--- Scripting/Math/ODE/ODE.vb
+++ Scripting/Math/ODE/ODE.vb
@@ -1,111 +1,15 @@
-﻿Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic.Serialization.JSON
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
 
 ''' <summary>
-''' 
+''' Ordinary differential equation(ODE).(常微分方程的模型)
 ''' </summary>
-''' <remarks>http://www.oschina.net/code/snippet_76_4433</remarks>
-Public Module ODESolver
-
-    ''' <summary>
-    ''' df函数指针，微分方程 ``df = f(x,y)``
-    ''' </summary>
-    ''' <param name="x"></param>
-    ''' <param name="y"></param>
-    ''' <returns></returns>
-    Public Delegate Function df(x As Double, y As Double) As Double
-
-    ''' <summary>
-    ''' 欧拉法解微分方程，分块数量为n, 解的区间为[a,b], 解向量为(x,y),方程初值为(x0,y0)，ODE的结果会从x和y这两个数组指针返回
-    ''' </summary>
-    ''' <param name="n"></param>
-    ''' <param name="a"></param>
-    ''' <param name="b"></param>
-    ''' <param name="y0"></param>
-    ''' 
-    <Extension>
-    Public Sub Eluer(ByRef df As ODE, n As Integer, a As Double, b As Double, y0 As Double)
-        Dim h As Double = (b - a) / n
-
-        df.x = New Double(n - 1) {}
-        df.y = New Double(n - 1) {}
-        df.x(Scan0) = a
-        df.y(Scan0) = y0
-
-        Dim x = df.x, y = df.y
-
-        For i As Integer = 1 To n - 1
-            x(i) = a + h * i
-            y(i) = x(i - 1) + h * df(x(i - 1), y(i - 1))
-        Next
-    End Sub
-
-    ''' <summary>
-    ''' 二阶龙格库塔法解解微分方程，分块数量为n, 解的区间为[a,b], 解向量为(x,y),方程初值为(x0, y0)
-    ''' 参考http://blog.sina.com.cn/s/blog_698c6a6f0100lp4x.html
-    ''' </summary>
-    ''' <param name="df"></param>
-    ''' <param name="n"></param>
-    ''' <param name="a"></param>
-    ''' <param name="b"></param>
-    ''' <param name="y0"></param>
-    <Extension>
-    Public Sub RK2(ByRef df As ODE, n As Integer, a As Double, b As Double, y0 As Double)
-        Dim h As Double = (b - a) / n
-
-        df.x = New Double(n - 1) {}
-        df.y = New Double(n - 1) {}
-        df.x(Scan0) = a
-        df.y(Scan0) = y0
-
-        Dim x = df.x, y = df.y
-        Dim k1 As Double, k2 As Double
-
-        For i As Integer = 1 To n - 1
-            x(i) = a + h * i
-            k1 = df(x(i - 1), y(i - 1))
-            k2 = df(x(i - 1) + h, y(i - 1) + h * k1)
-            y(i) = y(i - 1) + h / 2 * (k1 + k2)
-        Next
-    End Sub
-
-    ''' <summary>
-    ''' 四阶龙格库塔法解解微分方程，分块数量为n, 解的区间为[a,b], 解向量为(x,y),方程初值为(x0, y0)
-    ''' 参考http://blog.sina.com.cn/s/blog_698c6a6f0100lp4x.html 和维基百科
-    ''' </summary>
-    ''' <param name="df"></param>
-    ''' <param name="n"></param>
-    ''' <param name="a"></param>
-    ''' <param name="b"></param>
-    ''' <param name="y0"></param>
-    ''' 
-    <Extension>
-    Public Sub RK4(ByRef df As ODE, n As Integer, a As Double, b As Double, y0 As Double)
-        Dim h As Double = (b - a) / n
-
-        df.x = New Double(n - 1) {}
-        df.y = New Double(n - 1) {}
-        df.x(Scan0) = a
-        df.y(Scan0) = y0
-
-        Dim x = df.x, y = df.y
-        Dim k1, k2, k3, k4 As Double
-
-        For i As Integer = 1 To n - 1
-            x(i) = a + h * i
-            k1 = df(x(i - 1), y(i - 1))
-            k2 = df(x(i - 1) + 0.5 * h, y(i - 1) + 0.5 * h * k1)
-            k3 = df(x(i - 1) + 0.5 * h, y(i - 1) + 0.5 * h * k2)
-            k4 = df(x(i - 1) + h, y(i - 1) + h * k3)
-            y(i) = y(i - 1) + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
-        Next
-    End Sub
-End Module
-
 Public Class ODE
+
+#Region "Output results"
 
     Public Property x As Double()
     Public Property y As Double()
+#End Region
 
     ''' <summary>
     ''' Public Delegate Function df(x As Double, y As Double) As Double
@@ -113,6 +17,11 @@ Public Class ODE
     ''' </summary>
     ''' <returns></returns>
     Public Property df As ODESolver.df
+    ''' <summary>
+    ''' ``x=a``的时候的y初始值
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property y0 As Double
 
     Default Public ReadOnly Property GetValue(xi As Double, yi As Double) As Double
         Get
