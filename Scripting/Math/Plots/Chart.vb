df0a8711d1de7872b0d1e88e2373679b1592912a
xieguigang <xie.guigang@gmail.com>
2016-09-10 16:53:28 +0800

    charts plots

---
+++ Scripting/Math/Plots/Chart.vb
@@ -0,0 +1,72 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Serialization.JSON
+Imports Microsoft.VisualBasic.ComponentModel.DataStructures.SlideWindow
+
+Public Module Chart
+
+    ''' <summary>
+    ''' 绘图函数
+    ''' </summary>
+    ''' <param name="c"></param>
+    ''' <param name="size"></param>
+    ''' <param name="margin"></param>
+    ''' <param name="bg"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function Plot(c As IEnumerable(Of Serials), Optional size As Size = Nothing, Optional margin As Size = Nothing, Optional bg As String = "white") As Bitmap
+        If size.IsEmpty Then
+            size = New Size(4000, 3000)
+        End If
+        If margin.IsEmpty Then
+            margin = New Size(100, 100)
+        End If
+
+        Dim bmp As New Bitmap(size.Width, size.Height)
+        Dim bgColor As Color = bg.ToColor(onFailure:=Color.White)
+
+        Using g As Graphics = Graphics.FromImage(bmp)
+            Dim rect As New Rectangle(New Point, size)
+
+            Call g.FillRectangle(New SolidBrush(bgColor), rect)
+
+            For Each line As Serials In c
+                Dim pts = line.pts.SlideWindows(1)
+                Dim pen As New Pen(color:=line.color, width:=line.width) With {
+                    .DashStyle = line.lineType
+                }
+                Dim br As New SolidBrush(line.color)
+                Dim r As Single = line.PointSize
+
+                For Each pt In pts
+                    Dim a = pt.First
+                    Dim b = pt.Last
+                    Call g.DrawLine(pen, a, b)
+                    Call g.FillPie(br, a.X, a.Y, r, r, 0, 360)
+                    Call g.FillPie(br, b.X, b.Y, r, r, 0, 360)
+                Next
+            Next
+        End Using
+
+        Return bmp
+    End Function
+End Module
+
+Public Class Serials
+
+    Public pts As PointF()
+    Public lineType As DashStyle = DashStyle.Solid
+    Public title As String
+    ''' <summary>
+    ''' 点的半径大小
+    ''' </summary>
+    Public PointSize As Double = 1
+    Public color As Color = Color.Black
+    Public width As Single = 1
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
