39275fdecf61f548cd5dc0ed1cd02b65e8fa904a
xieguigang <genetics@smrucc.org>
2016-09-16 16:47:54 +0800

    begine bootstrap statics sampling method

---
+++ DataSciences/Math/Math/Bootstraping.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module Bootstraping
+
+    <Extension>
+    Public Iterator Function Samples(Of T)(source As IEnumerable(Of T), N As Integer, Optional B As Integer = 100) As IEnumerable(Of Microsoft.VisualBasic.ComponentModel.t(Of T()))
+
+    End Function
+End Module
