2b9dee84a7e72f0d77b271b24b6b69a16e742c34
xieguigang <xie.guigang@gmail.com>
2016-09-19 21:26:01 +0800

    fix namespace error

--- Data_science/Mathematical/Plots/Legend.vb
+++ Data_science/Mathematical/Plots/Legend.vb
@@ -3,7 +3,7 @@ Imports System.Drawing.Drawing2D
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
-Imports Microsoft.VisualBasic.Imaging.Drawing2D.VectorElements
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Vector.Shapes
 Imports Microsoft.VisualBasic.MarkupLanguage.CSS
 Imports Microsoft.VisualBasic.Serialization.JSON
 
