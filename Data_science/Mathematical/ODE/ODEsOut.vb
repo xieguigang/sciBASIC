08d5960d6f7358759532fe348d1f08088ea028fb
xieguigang <genetics@smrucc.org>
2016-09-20 15:26:45 +0800

    load odes output from dataframe

--- Data_science/Mathematical/ODE/ODEsOut.vb
+++ Data_science/Mathematical/ODE/ODEsOut.vb
@@ -1,6 +1,7 @@
 ﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.DocumentFormat.Csv
 Imports Microsoft.VisualBasic.DocumentFormat.Csv.DocumentStream
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Serialization.JSON
 
@@ -62,5 +63,41 @@ Public Class ODEsOut
         Next
 
         Return file
+    End Function
+
+    Public Shared Function LoadFromDataFrame(csv As String) As ODEsOut
+        Dim df As File = File.Load(csv)
+        Dim cols = df.Columns().ToArray
+        Dim X = cols(Scan0)
+        Dim y = cols.Skip(1).Take(cols.Length - 1 - 3).ToArray
+        Dim params = cols.Skip(cols.Length - 2).ToArray
+        Dim yData As NamedValue(Of Double())() =
+            LinqAPI.Exec(Of NamedValue(Of Double())) <= From s As String()
+                                                        In y
+                                                        Let name As String = s(Scan0)
+                                                        Let values As Double() = s.Skip(1).ToArray(AddressOf Val)
+                                                        Select New NamedValue(Of Double()) With {
+                                                            .Name = name,
+                                                            .x = values
+                                                        }
+        Return New ODEsOut With {
+            .x = X.Skip(1).ToArray(AddressOf Val),
+            .y = yData.ToDictionary,
+            .params = __getArgs(params(Scan0), params(1))  ' 由于没有信息可以了解哪些变量是y0初始值，所以在这里都把这些数据放在变量参数列表里面
+        }
+    End Function
+
+    Private Shared Function __getArgs(names As String(), values As String()) As Dictionary(Of String, Double)
+        Dim params As New Dictionary(Of String, Double)
+
+        For Each var As SeqValue(Of String) In names.SeqIterator
+            If String.IsNullOrEmpty(+var) Then
+                Exit For
+            Else
+                params(+var) = Val(values(var.i))
+            End If
+        Next
+
+        Return params
     End Function
 End Class
