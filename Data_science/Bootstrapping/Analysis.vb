9df221a8e2cc7fbfc3ccbb7ebf1769666e38e36b
xieguigang <genetics@smrucc.org>
2016-09-20 18:37:33 +0800

    hope the estimation works

--- Data_science/Bootstrapping/Analysis.vb
+++ Data_science/Bootstrapping/Analysis.vb
@@ -44,9 +44,28 @@ Public Module Analysis
         })
         Dim clusters = ClusterDataSet(clustersExpected, datasets)
         Dim out As New Dictionary(Of String, Dictionary(Of String, DoubleRange))
+        Dim bufs = codes.GroupBy(Function(x) x.Name).ToDictionary(Function(g) g.First.Name, Function(g) g.ToArray)
 
         For Each cluster As KMeansCluster(Of Entity) In clusters
+            Dim key As String = cluster.ClusterMean.JoinBy(",")
+            Dim value As New Dictionary(Of String, DoubleRange)
+            Dim data As New List(Of KeyValuePair(Of String, Double))
 
+            For Each x As Entity In cluster
+                data += bufs(x.uid).Select(Function(d) d.x.ToArray).MatrixAsIterator
+            Next
+
+            Dim pg = data.GroupBy(Function(p) p.Key)
+
+            For Each p In pg
+                Dim array = p.ToArray
+                Dim vals = array.ToArray(Function(x) x.Value)
+                value(p.Key) = New DoubleRange(vals.Min, vals.Max)
+            Next
+
+            out(key) = value
         Next
+
+        Return out
     End Function
 End Module
