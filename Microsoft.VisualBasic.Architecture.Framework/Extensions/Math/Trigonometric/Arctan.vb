2cefdb6b50d0c3e341b681428ebc776168c83166
xieguigang <xie.guigang@gmail.com>
2016-09-22 16:31:22 +0800

    AtanPrecise controls

--- Microsoft.VisualBasic.Architecture.Framework/Extensions/Math/Trigonometric/Arctan.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Extensions/Math/Trigonometric/Arctan.vb
@@ -2,18 +2,22 @@
 
     Partial Module Trigonometric
 
-        Const HalfPi = Math.PI / 2
+        Const HalfPI = Math.PI / 2
         Const n1 = 1.0R
         Const n05 = 0.5R
+
+        ''' <summary>
+        ''' 通过这个参数来控制计算精度，这个参数值越大，计算精度越高
+        ''' </summary>
+        Public AtanPrecise As Integer = 500
 
         ''' <summary>
         ''' Taylor Atan
         ''' </summary>
         ''' <param name="x"></param>
-        ''' <param name="precise">通过这个参数来控制计算精度，这个参数值越大，计算精度越高</param>
         ''' <returns></returns>
         ''' <remarks>Atan测试没有问题</remarks>>
-        Public Function Atn(x As Double, Optional precise As Integer = 500) As Double
+        Public Function Atn(x As Double) As Double
             If x = 1.0 Then
                 Return Math.PI / 4
             End If
@@ -21,15 +25,15 @@
                 Return -Atn(-x)
             End If
             If x > n1 Then
-                Return HalfPi - Atn(1 / x)
+                Return HalfPI - Atn(1 / x)
             End If
             If x > n05 Then
                 Return Atn(n1) + Atn((x - n1) / (1 + x))
             Else
-                Dim xPow2 = x * x
-                Dim n__1 = precise
-                Dim y = 1 / (2 * n__1 + 1)
-                Dim i As Integer = precise
+                Dim xPow2 As Double = x * x
+                Dim n__1 As Integer = AtanPrecise
+                Dim y As Double = 1 / (2 * n__1 + 1)
+                Dim i As Integer = AtanPrecise
 
                 While i > 0
                     y = (1 / (2 * n__1 - 1)) - (xPow2 * y)
