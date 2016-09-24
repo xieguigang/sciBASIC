2935f6d51d03cd075af216574c69b57675e4f134
xieguigang <xie.guigang@gmail.com>
2016-09-24 15:42:45 +0800

    color extensions

---
+++ Microsoft.VisualBasic.Architecture.Framework/Extensions/Image/Colors/HexColor.vb
@@ -0,0 +1,10 @@
+﻿Namespace Imaging
+
+    ''' <summary>
+    ''' Convert hex color string to RGB color
+    ''' </summary>
+    ''' <remarks>http://stackoverflow.com/questions/13356486/convert-hex-color-string-to-rgb-color</remarks>
+    Public Module HexColor
+
+    End Module
+End Namespace
