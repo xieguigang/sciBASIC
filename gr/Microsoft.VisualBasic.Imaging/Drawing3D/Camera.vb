c310068908aabd82356efb1ba79af55008040ecb
xieguigang <xie.guigang@gmail.com>
2016-09-29 22:47:55 +0800

    3D objects

---
+++ gr/Microsoft.VisualBasic.Imaging/Drawing3D/Camera.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace Drawing3D
+
+    Public Class Camera
+
+        Public ViewDistance!, angle!, fov!
+        Public screen As Size
+
+        Public Function Rotate(pt As Point3D) As Point3D
+            Return pt.RotateX(angle).RotateY(angle).RotateZ(angle)
+        End Function
+
+        Public Function Project(pt As Point3D)
+            Return pt.Project(screen.Width, screen.Height, fov, ViewDistance)
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+    End Class
+End Namespace
