30a1920f9749fd5f8ba9ed3b4ce50daed4a34c99
xieguigang <xie.guigang@gmail.com>
2016-10-04 01:24:21 +0800

    syntax references

--- Data/SearchEngine/Syntax.md
+++ Data/SearchEngine/Syntax.md
@@ -5,4 +5,11 @@
 + **NOT**: ``NOT term3``
 + Search **in Field**: ``<field>: expression``
 
-Please note that, the operator is case sensitive, all of them should be **UPCASE**
+Please note that:
+1. the operator is case sensitive, all of them should be **UPCASE**
+2. ``term`` can be regexp, but **MUST** term just a plant text
+   + If ``term`` start with character **#**, then ``term`` will translate as regexp
+   + If not, then will be treated as plant text, but wildcards will works
+     + allowed wildcards includes: 
+       + ``*`` for any characters
+       + ``%`` for any single characters 
--- Data/SearchEngine/SyntaxParser.vb
+++ Data/SearchEngine/SyntaxParser.vb
@@ -6,29 +6,6 @@ Imports Microsoft.VisualBasic.Scripting.TokenIcer
 
 Public Module SyntaxParser
 
-    ''' <summary>
-    ''' 大小写敏感，在使用之前要先用tolower或者toupper
-    ''' </summary>
-    ''' <param name="term$"></param>
-    ''' <param name="searchIn$"></param>
-    ''' <returns></returns>
-    Public Function ContainsAny(term$, searchIn$) As Boolean
-        Dim t1$() = term.Split(ASCII.Symbols)  ' term
-        Dim t2$() = term.Split(ASCII.Symbols)  ' 目标
-
-        For Each t$ In t1$
-            If t2.Located(t$) <> -1 Then
-                Return True
-            End If
-        Next
-
-        Return False
-    End Function
-
-    Public Function MustContains(term$, searchIn$) As Boolean
-        Return InStr(searchIn, term$, CompareMethod.Text) > 0
-    End Function
-
     Const AND$ = "AND"
     Const OR$ = "OR"
     Const NOT$ = "NOT"
