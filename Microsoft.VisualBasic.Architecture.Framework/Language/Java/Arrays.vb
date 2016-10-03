af1bb3aea22952ca2295a10e3f155cb925dc6e7e
xieguigang <xie.guigang@gmail.com>
2016-10-03 16:24:07 +0800

    arrays fill

---
+++ Microsoft.VisualBasic.Architecture.Framework/Language/Java/Arrays.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Language.Java
+
+    Public Module Arrays
+
+        <Extension>
+        Public Sub Fill(Of T)(ByRef a As T(), val As T)
+            For i% = 0 To a.Length - 1
+                a(i%) = val
+            Next
+        End Sub
+    End Module
+End Namespace
