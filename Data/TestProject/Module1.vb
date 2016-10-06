d08acf1b7ab25704c66fe9780a8facfcbe475346
xieguigang <xie.guigang@gmail.com>
2016-10-07 07:32:03 +0800

    simple fasta merge tool

--- Data/TestProject/Module1.vb
+++ Data/TestProject/Module1.vb
@@ -1,6 +1,17 @@
 ﻿Imports Microsoft.VisualBasic.Data.IO.SearchEngine
 
 Module Module1
+
+    Function Pair(Of T1, T2)(first As T1, second As T2) As (first As T1, second As T2)
+        Return (first, second)
+    End Function
+
+    Public ReadOnly Property Item(index As Integer) As ByRef Integer
+            Get
+
+        End Get
+    End Property
+
     Sub Main()
 
         Call "Aedes aegypti strain Liverpool supercont1.301 genomic scaffold, whole genome shotgun sequence".Match("(Aedes OR Aed) AND Aegypti").__DEBUG_ECHO
