8c987987990577cf428877ad16e7ac05a683143a
xieguigang <xie.guigang@gmail.com>
2016-10-11 15:55:41 +0800

    basic

--- CLI_tools/vbproj/Program.vb
+++ CLI_tools/vbproj/Program.vb
@@ -1,5 +1,6 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Language.UnixBash
 
 Module Program
 
@@ -7,8 +8,23 @@ Module Program
         Return GetType(Program).RunCLI(App.CommandLine)
     End Function
 
-    <ExportAPI("/config.output", Usage:="")>
+    <ExportAPI("/config.output", Usage:="/config.output /in <*.vbproj/DIR> /output <DIR> /c 'config=<Name>,platform=<type>'")>
     Public Function ConfigOutputPath(args As CommandLine) As Integer
+        Dim [in] As String = args("/in")
+        Dim output As String = args("/output")
+        Dim c As Dictionary(Of String, String) = args.GetDictionary("/c")
+        Dim files$()
 
+        If [in].FileExists Then
+            files = {[in]}
+        Else
+            files = (ls - l - r - wildcards("*.vbproj") <= [in]).ToArray
+        End If
+
+        For Each xml As String In files
+            Dim vbproj As Project = xml.LoadXml(Of Project)
+        Next
+
+        Return 0
     End Function
 End Module
