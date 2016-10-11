7daaf450c2420946525b5292b2d3020f1a2a99b7
xieguigang <xie.guigang@gmail.com>
2016-10-11 08:08:03 +0800

    improvements on the query statements and data object

--- Data/SearchEngine/QueryArgument.vb
+++ Data/SearchEngine/QueryArgument.vb
@@ -6,6 +6,12 @@ Public Class QueryArgument : Implements sIdEnumerable
     Public Property Name As String Implements sIdEnumerable.Identifier
     Public Property Expression As String
 
+    ''' <summary>
+    ''' The additional extension data.
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Data As Dictionary(Of String, String)
+
     Public Function Compile() As Expression
         Return Build(Expression)
     End Function
