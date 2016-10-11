a968786b1fc8f4290bb8090b7e7582ad83415c29
xieguigang <xie.guigang@gmail.com>
2016-10-11 15:44:04 +0800

    add test data

---
+++ CLI_tools/vbproj/Program.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+
+Module Program
+
+    Public Function Main(args As String()) As Integer
+        Return GetType(Program).RunCLI(App.CommandLine)
+    End Function
+
+    <ExportAPI("/config.output", Usage:="")>
+    Public Function ConfigOutputPath(args As CommandLine) As Integer
+
+    End Function
+End Module
