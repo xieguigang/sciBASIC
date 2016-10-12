06ed0b3ac78bed36410df797ad781ddf12219b03
xieguigang <xie.guigang@gmail.com>
2016-10-12 12:39:26 +0800

    split files

---
+++ CLI_tools/vbproj/Template/Project.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Public Class Project
+
+    <XmlAttribute> Public Property ToolsVersion As String
+    <XmlAttribute> Public Property DefaultTargets As String
+
+    <XmlElement("Import")>
+    Public Property [Imports] As Import()
+    <XmlElement("PropertyGroup")>
+    Public Property PropertyGroups As PropertyGroup()
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
+
+Public Class Import
+    <XmlAttribute> Public Property Project As String
+    <XmlAttribute> Public Property Condition As String
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
+
+Public Class ConditionValue
+    <XmlAttribute> Public Property Condition As String
+    <XmlText> Public Property value As String
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
--- CLI_tools/vbproj/Template/PropertyGroup.vb
+++ CLI_tools/vbproj/Template/PropertyGroup.vb
@@ -1,29 +1,6 @@
-﻿Imports System.Xml.Serialization
+﻿
+Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.Serialization.JSON
-
-Public Class Project
-
-    <XmlAttribute> Public Property ToolsVersion As String
-    <XmlAttribute> Public Property DefaultTargets As String
-
-    <XmlElement("Import")>
-    Public Property [Imports] As Import()
-    <XmlElement("PropertyGroup")>
-    Public Property PropertyGroups As PropertyGroup()
-
-    Public Overrides Function ToString() As String
-        Return Me.GetJson
-    End Function
-End Class
-
-Public Class Import
-    <XmlAttribute> Public Property Project As String
-    <XmlAttribute> Public Property Condition As String
-
-    Public Overrides Function ToString() As String
-        Return Me.GetJson
-    End Function
-End Class
 
 Public Class PropertyGroup
 
@@ -76,15 +53,6 @@ Public Class PropertyGroup
     Public Property OptionInfer As String
     Public Property SignAssembly As String
     Public Property DelaySign As String
-
-    Public Overrides Function ToString() As String
-        Return Me.GetJson
-    End Function
-End Class
-
-Public Class ConditionValue
-    <XmlAttribute> Public Property Condition As String
-    <XmlText> Public Property value As String
 
     Public Overrides Function ToString() As String
         Return Me.GetJson
