b2f30fd36f620c9dcc37b39f0f26450710bbb206
xieguigang <xie.guigang@gmail.com>
2016-10-21 17:38:55 +0800

    not sure how to convert the equations to matrix

---
+++ Data_science/Mathematical/Math/LP/Data/ObjectiveFunction.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace LP
+
+    Public Class ObjectiveFunction
+
+        Public Property Type As OptimizationType
+        Public Property xyz As Double()
+
+        Public Overrides Function ToString() As String
+            Return $"{Type.ToString}({xyz.GetJson})"
+        End Function
+    End Class
+End Namespace
