e1f8d80f7c0f28162c1144a51585fafee1c12fd7
xieguigang <xie.guigang@gmail.com>
2016-10-21 16:21:52 +0800

    java array operations

--- Microsoft.VisualBasic.Architecture.Framework/Language/Java/Arrays.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Language/Java/Arrays.vb
@@ -10,5 +10,11 @@ Namespace Language.Java
                 a(i%) = val
             Next
         End Sub
+
+        Public Function copyOfRange(Of T)(matrix As T(), start As Integer, length As Integer) As T()
+            Dim out As T() = New T(length - 1) {}
+            Call Array.Copy(matrix, start, out, Scan0, length)
+            Return out
+        End Function
     End Module
 End Namespace
