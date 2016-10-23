3c4d15e2a9105614f2e3147b3b7b041367071711
xieguigang <xie.guigang@gmail.com>
2016-10-23 18:07:24 +0800

    load bar data from csv

---
+++ Data_science/Mathematical/Plots/csv/BarData.vb
@@ -0,0 +1,52 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Data.csv.DocumentStream
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Linq
+
+Namespace csv
+
+    ''' <summary>
+    ''' 
+    ''' ###### csv format
+    ''' 
+    ''' ```
+    ''' "    ",serial1,serial2,...
+    ''' group1,
+    ''' group2,
+    ''' group3,
+    ''' ...
+    ''' ```
+    ''' </summary>
+    Public Module BarData
+
+        <Extension>
+        Public Function LoadBarData(csv$, Optional colors$() = Nothing) As BarDataGroup
+            Dim file As New File(csv)
+            Dim header As RowObject = file.First
+            Dim names$() = header.Skip(1).ToArray
+            Dim clData As Color() = If(
+                colors.IsNullOrEmpty,
+                ChartColors.Shuffles,
+                colors.ToArray(AddressOf ToColor))
+
+            Return New BarDataGroup With {
+                .Serials = names _
+                    .SeqIterator _
+                    .ToArray(Function(x) New NamedValue(Of Color) With {
+                        .Name = x.obj,
+                        .x = clData(x.i)
+                    }),
+                .Samples = file _
+                    .Skip(1) _
+                    .ToArray(Function(x) New BarDataSample With {
+                        .Tag = x.First,
+                        .data = x _
+                            .Skip(1) _
+                            .ToArray(AddressOf Val)
+                    })
+            }
+        End Function
+    End Module
+End Namespace
