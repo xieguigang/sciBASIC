a133a70d8aac30b80e18fc02d10af87184517a6e
xieguigang <xie.guigang@gmail.com>
2016-10-29 03:49:56 +0800

    ref odes

---
+++ Data_science/Mathematical/ODE/RefODEs.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.Mathematical.BasicR
+Imports Microsoft.VisualBasic.Mathematical.Calculus
+
+''' <summary>
+''' ``dy`` reference to the exists values.
+''' </summary>
+Public MustInherit Class RefODEs : Inherits ODEs
+
+    Public Property RefValues As ValueVector
+
+    Protected NotOverridable Overrides Sub func(dx As Double, ByRef dy As Vector)
+        RefValues += 1
+        Call func(dx, dy, RefValues)
+    End Sub
+
+    Protected MustOverride Overloads Sub func(dx#, ByRef dy As Vector, Y As ValueVector)
+
+End Class
---
+++ Data_science/Mathematical/ODE/ValueVector.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Public Class ValueVector : Inherits int
+
+    Public Property Y As Dictionary(Of NamedValue(Of Double()))
+
+    Default Public Overloads Property Value(name As var) As Double
+        Get
+            Return Y(name.Name).x(MyBase.value)
+        End Get
+        Set(value As Double)
+            Y(name.Name$).x(MyBase.value) = value
+        End Set
+    End Property
+
+    Public Overrides Function ToString() As String
+        Return $"[{value}] " & Y.Keys.ToArray.GetJson
+    End Function
+
+    ''' <summary>
+    ''' Move pointer value
+    ''' </summary>
+    ''' <param name="v"></param>
+    ''' <param name="n%"></param>
+    ''' <returns></returns>
+    Public Overloads Shared Operator +(v As ValueVector, n%) As ValueVector
+        v.value += n
+        Return v
+    End Operator
+End Class
