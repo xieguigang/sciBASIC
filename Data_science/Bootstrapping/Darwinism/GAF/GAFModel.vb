fb8fbe851affc0abe421eac32431d69ac5a51a40
xieguigang <xie.guigang@gmail.com>
2016-10-29 04:29:23 +0800

    not sure ref model for the de solver

--- Data_science/Bootstrapping/Darwinism/GAF/GAFModel.vb
+++ Data_science/Bootstrapping/Darwinism/GAF/GAFModel.vb
@@ -5,8 +5,6 @@ Namespace GAF
 
     Public MustInherit Class Model : Inherits MonteCarlo.Model
 
-        Protected MustOverride Overrides Sub func(dx As Double, ByRef dy As Vector)
-
 #Region "Not Required"
         Public NotOverridable Overrides Function eigenvector() As Dictionary(Of String, Eigenvector)
             Return Nothing
@@ -20,6 +18,5 @@ Namespace GAF
             Return Nothing
         End Function
 #End Region
-
     End Class
 End Namespace
---
+++ Data_science/Bootstrapping/Monte-Carlo/RefModel.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic.Mathematical.BasicR
+Imports Microsoft.VisualBasic.Mathematical.Calculus
+
+Namespace MonteCarlo
+
+    Public MustInherit Class RefModel : Inherits Model
+
+        Public Property RefValues As ValueVector
+
+        Dim RK%
+
+        ''' <summary>
+        ''' RK4每一次迭代会调用这个函数计算4次
+        ''' </summary>
+        ''' <param name="dx"></param>
+        ''' <param name="dy"></param>
+        Protected NotOverridable Overrides Sub func(dx As Double, ByRef dy As Vector)
+            RK += 1
+            func(dx, dy, RefValues)
+
+            If RK = 4 Then
+                RK = 0
+                RefValues += 1  ' 最开始是0，假若这句代码被放在func调用的前面首先自增1的话，会在末尾出现越界的问题
+            End If
+        End Sub
+
+        Protected MustOverride Overloads Sub func(dx#, ByRef dy As Vector, Y As ValueVector)
+    End Class
+End Namespace
