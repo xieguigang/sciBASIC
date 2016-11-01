6c05d87ac15387ee74c24f0dcb57cc398b49b44e
xieguigang <xie.guigang@gmail.com>
2016-11-01 15:45:27 +0800

    ODEsSolverTest

--- Data_science/Darwinism/GAF_estimates/README.md
+++ Data_science/Darwinism/GAF_estimates/README.md
@@ -1,5 +1,23 @@
 # Genetic Algorithm
 
+> Installing VisualBasic for data science via nuget:
+> ```
+> PM> Install-Package VB_AppFramework -Pre
+> ```
+> and then add reference to these dll modules:
+> + Microsoft.VisualBasic.Data.Csv.dll
+> + Microsoft.VisualBasic.DataMining.Framework.dll
+> + Microsoft.VisualBasic.Mathematical.dll
+> + Microsoft.VisualBasic.Mathematical.ODEsSolver.dll
+> + Microsoft.VisualBasic.Architecture.Framework_v3.0_22.0.76.201__8da45dcd8060cc9a.dll
+> + Microsoft.VisualBasic.Data.Bootstrapping.dll
+
+### RK4 ODEs solver in VisualBasic
+
+### GAF Core
+
+### GAF Parallel computing
+
 ## Testing
 
 ##### Problem & Goal
@@ -11,3 +29,9 @@
 ###### Preprocessing
 
 ##### GAF Estimates
+
+## Testing On Linux and Super Computer
+This demo has been tested successfully on CentOS 7 and China TianHe 1 Super Computer.
+
+![](./Kinetics_of_influenza_A_virus_infection_in_humans-TI.png)
+![](./Kinetics_of_influenza_A_virus_infection_in_humans-V.png)
--- Data_science/Darwinism/GAF_test2/Program.vb
+++ Data_science/Darwinism/GAF_test2/Program.vb
@@ -1,6 +1,35 @@
-﻿Module Program
+﻿Imports Microsoft.VisualBasic.Mathematical.Calculus
+Imports Microsoft.VisualBasic.Text
+Imports Microsoft.VisualBasic.Linq
+Imports System.Drawing
+Imports Microsoft.VisualBasic.Mathematical.Plots
+Imports Microsoft.VisualBasic.Imaging
+
+Module Program
 
     Sub Main()
+        Call ODEsSolverTest()
+    End Sub
 
+    Public Sub ODEsSolverTest()
+        Dim model As New Kinetics_of_influenza_A_virus_infection_in_humans
+        Dim result As ODEsOut = model.Solve(10000, 0, 10)
+
+        Call result.DataFrame("#TIME") _
+            .Save("./Kinetics_of_influenza_A_virus_infection_in_humans.csv", Encodings.ASCII)
+
+        Dim sT = result.y("T").x.SeqIterator.ToArray(Function(i) New PointF(result.x(i), +i))
+        Dim sI = result.y("I").x.SeqIterator.ToArray(Function(i) New PointF(result.x(i), +i))
+        Dim sV = result.y("V").x.SeqIterator.ToArray(Function(i) New PointF(result.x(i), +i))
+
+        Call {
+            Scatter.FromPoints(sT, "red", "Susceptible Cells"),
+            Scatter.FromPoints(sI, "lime", "Infected Cells")
+        }.Plot(fill:=False) _
+         .SaveAs("./Kinetics_of_influenza_A_virus_infection_in_humans-TI.png")
+        Call {
+            Scatter.FromPoints(sV, "skyblue", "Virus Load")
+        }.Plot(fill:=False) _
+         .SaveAs("./Kinetics_of_influenza_A_virus_infection_in_humans-V.png")
     End Sub
 End Module
