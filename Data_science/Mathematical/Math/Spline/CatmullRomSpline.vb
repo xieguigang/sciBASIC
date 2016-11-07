0171b61df5745dd0ca1786f7a9437a94c3b9149b
xieguigang <xie.guigang@gmail.com>
2016-11-07 15:38:04 +0800

    CatmullRomSpline code improvements

--- Data_science/Mathematical/Math/Spline/CatmullRomSpline.vb
+++ Data_science/Mathematical/Math/Spline/CatmullRomSpline.vb
@@ -1,37 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::fd5888630e7e92ecbcb5866fb4645ee5, ..\visualbasic_App\gr\Datavisualization.Network\Datavisualization.Network\Visualize\Lines.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports System.Drawing
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 
 Namespace Interpolation
 
+    ''' <summary>
+    ''' Calculates interpolated point between two points using Catmull-Rom Spline
+    ''' </summary>
+    ''' <remarks>https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline</remarks>
     Public Module CatmullRomSpline
 
         ''' <summary>
@@ -47,76 +52,104 @@ Namespace Interpolation
         ''' <returns>Calculated Spline Point</returns>
         ''' 
         <ExportAPI("CatmullRom.Spline", Info:="Calculates interpolated point between two points using Catmull-Rom Spline")>
-        Private Function PointOnCurve(p0 As Point, p1 As Point, p2 As Point, p3 As Point, t As Double) As Point
-            Dim ret As New Point()
-
-            Dim t2 As Single = t * t
-            Dim t3 As Single = t2 * t
-
-            ret.X = 0.5F * ((2.0F * p1.X) + (-p0.X + p2.X) * t + (2.0F * p0.X - 5.0F * p1.X + 4 * p2.X - p3.X) * t2 + (-p0.X + 3.0F * p1.X - 3.0F * p2.X + p3.X) * t3)
-            ret.Y = 0.5F * ((2.0F * p1.Y) + (-p0.Y + p2.Y) * t + (2.0F * p0.Y - 5.0F * p1.Y + 4 * p2.Y - p3.Y) * t2 + (-p0.Y + 3.0F * p1.Y - 3.0F * p2.Y + p3.Y) * t3)
+        Public Function PointOnCurve(p0 As PointF, p1 As PointF, p2 As PointF, p3 As PointF, t#) As PointF
+            Dim t2! = t * t
+            Dim t3! = t2 * t
+            Dim ret As New PointF With {
+                .X = 0.5F * (
+                    (2.0F * p1.X) + (-p0.X + p2.X) * t +
+                    (2.0F * p0.X - 5.0F * p1.X + 4 * p2.X - p3.X) * t2 +
+                    (-p0.X + 3.0F * p1.X - 3.0F * p2.X + p3.X) * t3),
+                .Y = 0.5F * (
+                    (2.0F * p1.Y) + (-p0.Y + p2.Y) * t +
+                    (2.0F * p0.Y - 5.0F * p1.Y + 4 * p2.Y - p3.Y) * t2 +
+                    (-p0.Y + 3.0F * p1.Y - 3.0F * p2.Y + p3.Y) * t3)
+            }
 
             Return ret
         End Function
 
         <ExportAPI("CatmullRom.Spline")>
-        Public Function CatmullRomSpline(Points As List(Of Point),
+        <Extension>
+        Public Function CatmullRomSpline(raw As List(Of Point),
                                          <Parameter("Interpolation.Steps")>
-                                         Optional InterpolationStep As Double = 0.1,
+                                         Optional interpolationStep# = 0.1,
                                          <Parameter("Is.Polygon")>
-                                         Optional IsPolygon As Boolean = False) As List(Of Point)
-            If Points.Count <= 2 Then
-                Return Points
-            End If
+                                         Optional isPolygon As Boolean = False) As List(Of Point)
 
-            If IsPolygon Then Return __catmullRomSplinePolygon(Points, InterpolationStep)
-
-            Dim result As New List(Of Point)
-            Dim yarray, xarray As New List(Of Double)
-            xarray.Add(Points(0).X - (Points(1).X - Points(0).X) / 2)
-            yarray.Add(Points(0).Y - (Points(1).Y - Points(0).Y) / 2)
-
-            For Each ps As Point In Points
-                xarray.Add(ps.X)
-                yarray.Add(ps.Y)
-            Next
-
-            xarray.Add((Points(Points.Count - 1).X - (Points(Points.Count - 2).X) / 2 + Points(Points.Count - 1).X))
-            yarray.Add((Points(Points.Count - 1).Y - (Points(Points.Count - 2).Y) / 2 + Points(Points.Count - 1).Y))
-
-            Dim r As New List(Of Point)
-            For i As Integer = 0 To yarray.Count - 1
-                r.Add(New Point(xarray(i), yarray(i)))
-            Next
-
-            For i As Integer = 3 To r.Count - 1
-                For k As Double = 0 To (1 - InterpolationStep) Step InterpolationStep
-                    result.Add(PointOnCurve(r(i - 3), r(i - 2), r(i - 1), r(i), k))
-                Next
-            Next
-            result.Add(Points(Points.Count - 1))
+            Dim data As New List(Of PointF)(
+                raw.Select(Function(pt) New PointF(pt.X, pt.Y)))
+            Dim spline = data.CatmullRomSpline(interpolationStep, isPolygon)
+            Dim result As New List(Of Point)(
+                spline.Select(Function(pt) New Point(pt.X, pt.Y)))
 
             Return result
         End Function
 
-        Private Function __catmullRomSplinePolygon(points As List(Of Point), InterpolationStep As Double) As List(Of Point)
-            Dim result As New List(Of Point)
+        <ExportAPI("CatmullRom.Spline")>
+        <Extension>
+        Public Function CatmullRomSpline(raw As List(Of PointF),
+                                         <Parameter("Interpolation.Steps")>
+                                         Optional interpolationStep# = 0.1,
+                                         <Parameter("Is.Polygon")>
+                                         Optional isPolygon As Boolean = False) As List(Of PointF)
+            If raw.Count <= 2 Then
+                Return raw
+            End If
+
+            If isPolygon Then
+                Return __catmullRomSplinePolygon(raw, interpolationStep)
+            End If
+
+            Dim yarray, xarray As New List(Of Double)
+            Dim result As New List(Of PointF)
+
+            Call xarray.Add(raw(0).X - (raw(1).X - raw(0).X) / 2)
+            Call yarray.Add(raw(0).Y - (raw(1).Y - raw(0).Y) / 2)
+
+            For Each ps As PointF In raw
+                Call xarray.Add(ps.X)
+                Call yarray.Add(ps.Y)
+            Next
+
+            Call xarray.Add((raw(raw.Count - 1).X - (raw(raw.Count - 2).X) / 2 + raw(raw.Count - 1).X))
+            Call yarray.Add((raw(raw.Count - 1).Y - (raw(raw.Count - 2).Y) / 2 + raw(raw.Count - 1).Y))
+
+            Dim r As New List(Of PointF)
+
+            For i As Integer = 0 To yarray.Count - 1
+                Call r.Add(New PointF(xarray(i), yarray(i)))
+            Next
+
+            For i As Integer = 3 To r.Count - 1
+                For k As Double = 0 To (1 - interpolationStep) Step interpolationStep
+                    Call result.Add(PointOnCurve(r(i - 3), r(i - 2), r(i - 1), r(i), k))
+                Next
+            Next
+
+            Call result.Add(raw(raw.Count - 1))
+
+            Return result
+        End Function
+
+        Private Function __catmullRomSplinePolygon(points As List(Of PointF), steps#) As List(Of PointF)
+            Dim result As New List(Of PointF)
 
             For i As Integer = 0 To points.Count - 1
                 If i = 0 Then
-                    For k As Double = 0 To (1 - InterpolationStep) Step InterpolationStep
+                    For k As Double = 0 To (1 - steps) Step steps
                         result.Add(PointOnCurve(points(points.Count - 1), points(i), points(i + 1), points(i + 2), k))
                     Next
                 ElseIf i = points.Count - 1 Then
-                    For k As Double = 0 To (1 - InterpolationStep) Step InterpolationStep
+                    For k As Double = 0 To (1 - steps) Step steps
                         result.Add(PointOnCurve(points(i - 1), points(i), points(0), points(1), k))
                     Next
                 ElseIf i = points.Count - 2 Then
-                    For k As Double = 0 To (1 - InterpolationStep) Step InterpolationStep
+                    For k As Double = 0 To (1 - steps) Step steps
                         result.Add(PointOnCurve(points(i - 1), points(i), points(i + 1), points(0), k))
                     Next
                 Else
-                    For k As Double = 0 To (1 - InterpolationStep) Step InterpolationStep
+                    For k As Double = 0 To (1 - steps) Step steps
                         result.Add(PointOnCurve(points(i - 1), points(i), points(i + 1), points(i + 2), k))
                     Next
                 End If
