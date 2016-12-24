29736a1e9cb793e7039c930cfb7dec4b8ed165b6
xieguigang <xie.guigang@gmail.com>
2016-12-24 18:31:03 +0800

    heatmap style improvements

--- Data_science/Mathematical/Plots/Heatmaps/HeatmapTable.vb
+++ Data_science/Mathematical/Plots/Heatmaps/HeatmapTable.vb
@@ -14,7 +14,7 @@ Public Module HeatmapTable
     ''' 只能够用来表示两两变量之间的相关度
     ''' </summary>
     ''' <param name="triangularStyle">
-    ''' 是否上部分显示圆，下部分显示数值？默认是全部都显示圆圈
+    ''' 是否上部分显示圆，下部分显示数值？默认是本三角样式
     ''' 圆的半径大小用来表示相关度的绝对值，颜色则是和普通的heatmap一样用来表示相关度的大小和方向
     ''' </param>
     ''' <returns></returns>
@@ -24,7 +24,7 @@ Public Module HeatmapTable
                          Optional size As Size = Nothing,
                          Optional margin As Size = Nothing,
                          Optional bg$ = "white",
-                         Optional triangularStyle As Boolean = False,
+                         Optional triangularStyle As Boolean = True,
                          Optional fontStyle$ = CSSFont.Win10Normal,
                          Optional legendTitle$ = "Heatmap Color Legend",
                          Optional legendFont As Font = Nothing,
@@ -32,13 +32,14 @@ Public Module HeatmapTable
                          Optional max# = 1,
                          Optional mainTitle$ = "heatmap",
                          Optional titleFont As Font = Nothing,
-                         Optional drawGrid As Boolean = True,
+                         Optional drawGrid As Boolean = False,
                          Optional drawValueLabel As Boolean = True,
                          Optional valuelabelFont As Font = Nothing) As Bitmap
 
         Return Heatmap.__plotInterval(
             Sub(g, region, array, left, font, dw, levels, top, colors)
 
+                ' 在绘制上三角的时候假设每一个对象的keys的顺序都是相同的
                 Dim keys$() = array(Scan0).Value.Keys.ToArray
                 Dim blockSize As New SizeF(dw, dw)  ' 每一个方格的大小
 
@@ -48,23 +49,33 @@ Public Module HeatmapTable
 
                 margin = region.Margin
 
-                For Each x As NamedValue(Of Dictionary(Of String, Double)) In array   ' 在这里绘制具体的矩阵
+                For Each x As SeqValue(Of NamedValue(Of Dictionary(Of String, Double))) In array.SeqIterator(offset:=1)  ' 在这里绘制具体的矩阵
+
+                    Dim i% = 1
 
                     For Each key$ In keys
-                        Dim c# = x.Value(key)
-                        Dim level% = levels(c#)  '  得到等级
-                        Dim color As Color = colors(   ' 得到当前的方格的颜色
-                            If(level% > colors.Length - 1,
-                            colors.Length - 1,
-                            level))
+                        Dim c# = (+x).Value(key)
                         Dim rect As New RectangleF(New PointF(left, top), blockSize)
-                        Dim b As New SolidBrush(color)
-                        Dim r As Single = Math.Abs(c) * dw / 2 ' 计算出半径的大小
-                        Dim d = dw / 2 - r
+                        Dim labelbrush As SolidBrush
 
-                        r *= 2
+                        If triangularStyle AndAlso i <= x.i Then  ' 只显示数值，而不绘制圆圈
+                            labelbrush = Brushes.Black
+                        Else
+                            Dim level% = levels(c#)  '  得到等级
+                            Dim color As Color = colors(   ' 得到当前的方格的颜色
+                                If(level% > colors.Length - 1,
+                                colors.Length - 1,
+                                level))
 
-                        Call g.FillPie(b, rect.Left + d, rect.Top + d, r, r, 0, 360)
+                            Dim b As New SolidBrush(color)
+                            Dim r As Single = Math.Abs(c) * dw / 2 ' 计算出半径的大小
+                            Dim d = dw / 2 - r
+
+                            r *= 2
+                            labelbrush = Brushes.White
+
+                            Call g.FillPie(b, rect.Left + d, rect.Top + d, r, r, 0, 360)
+                        End If
 
                         If drawGrid Then
                             Call g.DrawRectangles(Pens.WhiteSmoke, {rect})
@@ -73,20 +84,21 @@ Public Module HeatmapTable
                             key = c.FormatNumeric(2)
                             Dim ksz As SizeF = g.MeasureString(key, valuelabelFont)
                             Dim kpos As New PointF(rect.Left + (rect.Width - ksz.Width) / 2, rect.Top + (rect.Height - ksz.Height) / 2)
-                            Call g.DrawString(key, valuelabelFont, Brushes.White, kpos)
+                            Call g.DrawString(key, valuelabelFont, labelbrush, kpos)
                         End If
 
                         left.value += dw!
+                        i += 1
                     Next
 
                     left.value = margin.Width
                     top.value += dw!
 
-                    Dim sz As SizeF = g.MeasureString(x.Name, font)
+                    Dim sz As SizeF = g.MeasureString((+x).Name, font)
                     Dim y As Single = top.value - dw - (sz.Height - dw) / 2
                     Dim lx! = margin.Width - sz.Width - margin.Width * 0.1
 
-                    Call g.DrawString(x.Name, font, Brushes.Black, New PointF(lx, y))
+                    Call g.DrawString((+x).Name, font, Brushes.Black, New PointF(lx, y))
                 Next
 
             End Sub,
