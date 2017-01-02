ba4cc6aae4464da244a984993e6b7e2ef1fd6a7a
xieguigang <xie.guigang@gmail.com>
2017-01-02 23:57:12 +0800

    simple textrank calculation

--- Data/Trinity/TextRank.vb
+++ Data/Trinity/TextRank.vb
@@ -1,4 +1,10 @@
-﻿''' <summary>
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataStructures
+Imports Microsoft.VisualBasic.Data.visualize.Network
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports Microsoft.VisualBasic.Text
+
+''' <summary>
 ''' This module implements TextRank, an unsupervised keyword
 ''' significance scoring algorithm. TextRank builds a weighted
 ''' graph representation Of a document Using words As nodes
@@ -13,4 +19,30 @@
 ''' </summary>
 Public Module TextRank
 
+    ReadOnly sdeli As Char() = {"."c, "?"c, "!"c, ";"c}
+
+    <Extension>
+    Public Function Sentences(text$) As String()
+        Return text.Split(TextRank.sdeli)
+    End Function
+
+    <Extension>
+    Public Function Words(text$) As String()
+        Return text.Split(ASCII.Symbols)
+    End Function
+
+    <Extension>
+    Public Function TextGraph(sentences As IEnumerable(Of String)) As GraphMatrix
+        Dim net As New Network
+
+        For Each text As String In sentences
+
+            ' 假设每一句话之中的单词之间的顺序就是网络连接的方向
+            For Each t In text.Words.SlideWindows(2)
+                Call net.AddEdges(t.First, {t.Last})
+            Next
+        Next
+
+        Return New GraphMatrix(net)
+    End Function
 End Module
