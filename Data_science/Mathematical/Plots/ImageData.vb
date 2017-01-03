d2298992ff77b39666517e176f68b343d84b9f26
xieguigang <xie.guigang@gmail.com>
2017-01-03 14:35:08 +0800

    plot function

--- Data_science/Mathematical/Plots/ImageData.vb
+++ Data_science/Mathematical/Plots/ImageData.vb
@@ -1,6 +1,8 @@
 ﻿Imports System.Drawing
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Ranges
 Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing3D
 
 Public Module ImageDataExtensions
 
@@ -17,6 +19,7 @@ Public Module ImageDataExtensions
                End Function
     End Function
 
+    <Extension>
     Public Function SurfaceProvider(img As Image) As Func(Of Double, Double, (z#, color As Double))
         Dim bitmap As hBitmap = hBitmap.FromImage(img)
 
@@ -27,5 +30,29 @@ Public Module ImageDataExtensions
 
                    Return (Z:=b, Color:=h)
                End Function
+    End Function
+
+    <Extension>
+    Public Function Image2DMap(img As Image, Optional steps% = 5) As Bitmap
+        Dim color = img.PointZProvider
+        Dim xrange As DoubleRange = $"0 -> {img.Width}"
+        Dim yrange As DoubleRange = $"0 -> {img.Height}"
+
+        Return ScatterHeatmap.Plot(
+            color, xrange, yrange,
+            xsteps:=steps, ysteps:=steps)
+    End Function
+
+    <Extension>
+    Public Function Image3DMap(img As Image, camera As Camera, Optional steps% = 5) As Bitmap
+        Dim Z = img.SurfaceProvider
+        Dim xrange As DoubleRange = $"0 -> {img.Width}"
+        Dim yrange As DoubleRange = $"0 -> {img.Height}"
+
+        Return Plot3D.ScatterHeatmap.Plot(
+            Z, xrange, yrange,
+            camera,
+            xn:=img.Width / steps,
+            yn:=img.Height / steps)
     End Function
 End Module
