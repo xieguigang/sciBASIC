84bd00836ec0bd168bd4fadf8c5b942f9ea73974
xieguigang <xie.guigang@gmail.com>
2017-01-04 08:07:26 +0800

    GA fitness drivers

---
+++ Data_science/Bootstrapping/Darwinism/GAF/Drivers/RawCompare.vb
@@ -0,0 +1,10 @@
+﻿Namespace Darwinism.GAF
+
+    ''' <summary>
+    ''' 不像<see cref="GAFFitness"/>接受的是经过插值处理的原始数据，
+    ''' 这个``fitness``驱动程序接受的是未经过任何处理的原始数据
+    ''' </summary>
+    Public Class RawCompare
+
+    End Class
+End Namespace
