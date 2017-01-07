4b3956417b8d35ddf7fba249fda9d295c48449c0
xieguigang <xie.guigang@gmail.com>
2017-01-08 02:17:42 +0800

    not sure for the axis ticks auto scalling???

---
+++ Data_science/Mathematical/Plots/g/AxisScalling.vb
@@ -0,0 +1,17 @@
+﻿Public Module AxisScalling
+
+    Public Function GetAxisValues(max#, Optional min# = 0R) As Double()
+        Dim p10% = Fix(Math.Log10(max))
+
+    End Function
+
+    Public Function GetAxisByTick(max#, tick#, Optional min# = 0R) As Double()
+        Dim l As New List(Of Double)
+
+        For i As Double = min To max Step tick
+            Call l.Add(i)
+        Next
+
+        Return l.ToArray
+    End Function
+End Module
