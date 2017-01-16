87936689d663583dc490b1bf3d9e58095a7fbb77
xieguigang <xie.guigang@gmail.com>
2017-01-16 13:05:00 +0800

    using binary tree?

--- Data_science/Mathematical/Plots/Dendrogram/Layouts.vb
+++ Data_science/Mathematical/Plots/Dendrogram/Layouts.vb
@@ -1,4 +1,7 @@
-﻿Namespace Dendrogram
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataStructures.BinaryTree
+Imports Microsoft.VisualBasic.Imaging
+
+Namespace Dendrogram
 
     Public Module Layouts
 
@@ -6,7 +9,18 @@
         ''' 水平放置的
         ''' </summary>
         ''' <returns></returns>
-        Public Function HorizontalLayout()
+        ''' <remarks>
+        ''' ```
+        '''      +
+        '''      |
+        '''      |
+        ''' +----+------+
+        ''' |           |
+        ''' |           |
+        ''' +           +
+        ''' ```
+        ''' </remarks>
+        Public Function HorizontalLayout(Of T)(tree As BinaryTree(Of T), getDistance As Func(Of T, T, Double)) As BinaryTree(Of ILayoutedObject(Of T))
 
         End Function
 
@@ -14,7 +28,18 @@
         ''' 垂直放置的
         ''' </summary>
         ''' <returns></returns>
-        Public Function Vertical()
+        Public Function Vertical(Of T)(tree As BinaryTree(Of T), getDistance As Func(Of T, T, Double)) As BinaryTree(Of ILayoutedObject(Of T))
+
+        End Function
+
+        ''' <summary>
+        ''' 圆弧状的
+        ''' </summary>
+        ''' <returns></returns>
+        ''' <remarks>
+        ''' http://stackoverflow.com/questions/5089030/how-do-i-create-a-radial-cluster-like-the-following-code-example-in-python
+        ''' </remarks>
+        Public Function Radial(Of T)(tree As BinaryTree(Of T), getDistance As Func(Of T, T, Double)) As BinaryTree(Of ILayoutedObject(Of T))
 
         End Function
     End Module
