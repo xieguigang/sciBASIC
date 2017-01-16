80b7c6d104cb7b15cfe6cfb50452bb198a9429b9
xieguigang <xie.guigang@gmail.com>
2017-01-16 12:39:12 +0800

    layout object

---
+++ Data_science/Mathematical/Plots/Dendrogram/Layouts.vb
@@ -0,0 +1,21 @@
+﻿Namespace Dendrogram
+
+    Public Module Layouts
+
+        ''' <summary>
+        ''' 水平放置的
+        ''' </summary>
+        ''' <returns></returns>
+        Public Function HorizontalLayout()
+
+        End Function
+
+        ''' <summary>
+        ''' 垂直放置的
+        ''' </summary>
+        ''' <returns></returns>
+        Public Function Vertical()
+
+        End Function
+    End Module
+End Namespace
