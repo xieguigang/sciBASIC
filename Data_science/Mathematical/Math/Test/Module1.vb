6c209bb600702c562fb994b3e83ecae8496feb5c
xieguigang <xie.guigang@gmail.com>
2017-01-26 18:36:49 +0800

    add more test

--- Data_science/Mathematical/Math/Test/Module1.vb
+++ Data_science/Mathematical/Math/Test/Module1.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.Mathematical.Scripting
+﻿Imports System.Linq.Expressions
+Imports Microsoft.VisualBasic.Mathematical.Scripting
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 Module Module1
@@ -6,6 +7,7 @@ Module Module1
     Sub Main()
 
         Call ParameterCompute.Demo(123).GetJson.__DEBUG_ECHO
+        Call Test(1, 2)
 
         Pause()
 
@@ -16,5 +18,20 @@ Module Module1
         Call "(1+2)!/5".Evaluate
 
         Pause()
+    End Sub
+
+    Sub Test(a#, b#, Optional x$ = "(A + b)! * 100", Optional y# = 33, Optional z$ = "Y + 9")
+        Dim parameters As Dictionary(Of String, Double) =
+            New Expression(Of Func(Of Object))() {
+                Function() a,
+                Function() b,
+                Function() x,
+                Function() y,
+                Function() z
+        }.Evaluate
+
+        Dim json$ = parameters.GetJson(True)
+
+        Call json.__DEBUG_ECHO
     End Sub
 End Module
