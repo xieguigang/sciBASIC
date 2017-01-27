8644b215cfc8ce3fc193c4aa98c70140e879ca27
xieguigang <genetics@smrucc.org>
2017-01-27 17:27:15 +0800

    demo

---
+++ docs/guides/parameter_expression/Demo/Program.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Mathematical.Scripting
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Module Program
+
+    Sub Main()
+        Test(1, 2, 3, z:="a+b")
+        Pause()
+    End Sub
+
+    Function Test(a!, b&, c#, Optional x$ = "(A + b^2)! * 100", Optional y$ = "(cos(x/33)+1)^2 -3", Optional z$ = "log(-Y) + 9") As (before As Object(), after As Object())
+        Dim before As New Value(Of Object()), after As New Value(Of Object())
+
+        Call $"Parameter before the expression evaluation is: { (before = {a, b, c, x, y, z}).GetJson }".__DEBUG_ECHO
+        Call ParameterExpression.Apply(Function() {a, b, x, y, z})
+        Call $"Parameters after the expression evaluation is: { (after = {a, b, c, x, y, z}).GetJson }".__DEBUG_ECHO
+
+        Return (+before, +after)
+    End Function
+End Module
