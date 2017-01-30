ee26d7a698b5879d1b48f45fb6835c77e627c005
xieguigang <xie.guigang@gmail.com>
2017-01-31 00:09:15 +0800

    add example 3D models

--- gr/3DEngineTest/3DEngineTest/Landscape_model/FormLandscape.Designer.vb
+++ gr/3DEngineTest/3DEngineTest/Landscape_model/FormLandscape.Designer.vb
@@ -22,16 +22,51 @@ Partial Class FormLandscape
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LoadModelToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(742, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoadModelToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'LoadModelToolStripMenuItem
+        '
+        Me.LoadModelToolStripMenuItem.Name = "LoadModelToolStripMenuItem"
+        Me.LoadModelToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.LoadModelToolStripMenuItem.Text = "Load Model"
         '
         'FormLandscape
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(742, 493)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "FormLandscape"
         Me.Text = "VB.NET landscape model demo"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LoadModelToolStripMenuItem As ToolStripMenuItem
 End Class
--- gr/3DEngineTest/3DEngineTest/Landscape_model/FormLandscape.vb
+++ gr/3DEngineTest/3DEngineTest/Landscape_model/FormLandscape.vb
@@ -4,25 +4,42 @@ Imports Microsoft.VisualBasic.Linq
 
 Public Class FormLandscape
 
+    Dim canvas As GDIDevice
+
     Private Sub FormLandscape_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Dim project = Landscape.IO.Open("G:\GCModeller\src\runtime\sciBASIC#\gr\3DEngineTest\Hexagon.3mf")
-        Dim surfaces = project.GetSurfaces
-        Dim canvas As New GDIDevice With {
+        Dim surfaces = New Cube(10)
+
+        canvas = New GDIDevice With {
             .Painter = Sub(g, camera)
                            Call g.Clear(Color.LightBlue)
-                           Call camera.Draw(g, surfaces, True)
+                           Call surfaces.Draw(g, camera)
                        End Sub,
             .Dock = DockStyle.Fill,
             .AutoRotation = False,
             .Animation = Sub(_camera)
                              ' Update the variable after each frame.
-                             '_camera.angleX += 1
-                             '_camera.angleY += 1
-                             '_camera.angleZ += 1
+                             _camera.angleX += 1
+                             _camera.angleY += 1
+                             _camera.angleZ += 1
                          End Sub
         }
-
         Controls.Add(canvas)
         canvas.Run()
+    End Sub
+
+    Private Sub LoadModelToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoadModelToolStripMenuItem.Click
+        Using file As New OpenFileDialog With {
+            .Filter = "3D model(*.3mf)|*.3mf"
+        }
+            If file.ShowDialog = DialogResult.OK Then
+                Dim project = Landscape.IO.Open(file.FileName)
+                Dim surfaces = project.GetSurfaces
+
+                canvas.Painter = Sub(g, camera)
+                                     Call g.Clear(Color.White)
+                                     Call camera.Draw(g, surfaces, True)
+                                 End Sub
+            End If
+        End Using
     End Sub
 End Class
