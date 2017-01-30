2875fd670980d0a200be00d7402e829c3d6ad87b
xieguigang <xie.guigang@gmail.com>
2017-01-30 13:38:39 +0800

    3d models container

---
+++ gr/Landscape/Graphics.vb
@@ -0,0 +1,13 @@
+﻿''' <summary>
+''' The data model of the landscape 3D model.
+''' </summary>
+Public Class Graphics
+
+    Public Property Surfaces As Surface()
+    ''' <summary>
+    ''' The scene paint background, its value definition is the same as <see cref="Surface.paint"/>
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property bg As String
+
+End Class
--- gr/Landscape/Surface.vb
+++ gr/Landscape/Surface.vb
@@ -1,3 +1,51 @@
-﻿Public Class Surface
+﻿Imports System.Drawing
+Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
 
+''' <summary>
+''' 方便将3D模型保存与XML文件之中的数据模型对象
+''' </summary>
+Public Class Surface : Implements IEnumerable(Of Point3D)
+
+    ''' <summary>
+    ''' 请注意，在这里面的点都是有先后顺序分别的
+    ''' </summary>
+    <XmlElement>
+    Public Property vertices As Point3D()
+    ''' <summary>
+    ''' 这个是本``表面``对象所喷涂的纹理的定义，可以是颜色的名称或者表达式，也可以是图片的相对路径的引用
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property paint As String
+
+    ''' <summary>
+    ''' 将<see cref="paint"/>资源字符串转换为相对应的<see cref="Brush"/>画刷对象.
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property Brush As Brush
+        Get
+            Return paint.GetBrush
+        End Get
+    End Property
+
+    Public Function CreateObject() As Drawing3D.Surface
+        Return New Drawing3D.Surface With {
+            .vertices = vertices,
+            .brush = Brush
+        }
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return paint
+    End Function
+
+    Public Iterator Function GetEnumerator() As IEnumerator(Of Point3D) Implements IEnumerable(Of Point3D).GetEnumerator
+        For Each x As Point3D In vertices
+            Yield x
+        Next
+    End Function
+
+    Private Iterator Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+        Yield GetEnumerator()
+    End Function
 End Class
