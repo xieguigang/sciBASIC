c990720e65a2c9ee0b4252fe8f0d73a8cbe17f46
xieguigang <xie.guigang@gmail.com>
2017-02-01 16:20:51 +0800

    update demo

--- docs/guides/parameter_expression/Demo/Program.vb
+++ docs/guides/parameter_expression/Demo/Program.vb
@@ -37,6 +37,7 @@ Module Program
 
         Test(1, 2, 3, z:="a+b")
         Test(2, 3, 1, y:="(a+b)*c", z:="x+y")
+        StringTest(100.23)
         Pause()
 
         Dim math As New Expression
@@ -52,19 +53,27 @@ Module Program
         End With
     End Sub
 
+    Sub StringTest(z#,
+                   Optional pvalue# = 0.005,
+                   Optional title$ = "@This is title string interpolate test: \$z value is $z",
+                   Optional ylabel$ = "@Plots of the experiment data with \np-value cutoff: $pvalue, \nand z-value: $z (km/s).")
+
+        Call $"Parameter before the expression evaluation is: { ({z, pvalue, title, ylabel}).GetJson }".__DEBUG_ECHO
+        Call ParameterExpression.Apply(Function() {z, pvalue, title, ylabel})
+        Call $"Parameters after the expression evaluation is: { ({z, pvalue, title, ylabel}).GetJson(True) }".__DEBUG_ECHO
+
+    End Sub
+
     Function Test(a!, b&, c#,
                   Optional x$ = "(A + b^2)! * 100",
                   Optional y$ = "(cos(x/33)+1)^2 -3",
-                  Optional z$ = "log(-Y) + 9",
-                  Optional pvalue# = 0.005,
-                  Optional title$ = "@This is title string interpolate test: \$z value is $z",
-                  Optional ylabel$ = "@Plots of the experiment data with \np-value cutoff: $pvalue, \nand z-value: $z (km/s).") As (before As Object(), after As Object())
+                  Optional z$ = "log(-Y) + 9") As (before As Object(), after As Object())
 
         Dim before As New Value(Of Object()), after As New Value(Of Object())
 
-        Call $"Parameter before the expression evaluation is: { (before = {a, b, c, x, y, z, pvalue, title, ylabel}).GetJson }".__DEBUG_ECHO
-        Call ParameterExpression.Apply(Function() {a, b, c, x, y, z, title, pvalue, ylabel})
-        Call $"Parameters after the expression evaluation is: { (after = {a, b, c, x, y, z, title, pvalue, ylabel}).GetJson(True) }".__DEBUG_ECHO
+        Call $"Parameter before the expression evaluation is: { (before = {a, b, c, x, y, z}).GetJson }".__DEBUG_ECHO
+        Call ParameterExpression.Apply(Function() {a, b, c, x, y, z})
+        Call $"Parameters after the expression evaluation is: { (after = {a, b, c, x, y, z}).GetJson(True) }".__DEBUG_ECHO
 
         Return (+before, +after)
     End Function
