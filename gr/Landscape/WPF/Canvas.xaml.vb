83094e6c221b81e33cbce678c51465ec77b4707a
amethyst-asuka <amethyst.asuka@gcmodeller.org>
2017-02-03 16:29:48 +0800

    basic wpf 3d canvas

--- gr/Landscape/WPF/Canvas.xaml.vb
+++ gr/Landscape/WPF/Canvas.xaml.vb
@@ -14,10 +14,11 @@ Public Class Canvas
 
     Public Sub New()
         Call InitializeComponent()
+        Call BuildSolid(Vendor_3mf.IO.Open("G:\sciBASIC\gr\build_3DEngine\models\WOW_rthas the Lich King1.3mf"))
     End Sub
 
     Public Sub BuildSolid(project As Project)
-        For Each obj In project.model.resources.objects
+        For Each obj In project.model.resources.objects.Where(Function(o) Not o.mesh Is Nothing)
             ' Define 3D mesh object
             Dim mesh As New MeshGeometry3D()
 
@@ -133,47 +134,50 @@ Public Class Canvas
     End Sub
 
     Private Sub Grid_MouseMove(sender As Object, e As MouseEventArgs)
-        If __drag Then
-            Dim pos As Point = Mouse.GetPosition(viewport)
-            Dim actualPos As New Point(pos.X - viewport.ActualWidth / 2, viewport.ActualHeight / 2 - pos.Y)
-            Dim dx As Double = actualPos.X - __lastPos.X, dy As Double = actualPos.Y - __lastPos.Y
+        If Not __drag Then Return
 
-            Dim mouseAngle As Double = 0
-            If dx <> 0 AndAlso dy <> 0 Then
-                mouseAngle = Math.Asin(Math.Abs(dy) / Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2)))
-                If dx < 0 AndAlso dy > 0 Then
-                    mouseAngle += Math.PI / 2
-                ElseIf dx < 0 AndAlso dy < 0 Then
-                    mouseAngle += Math.PI
-                ElseIf dx > 0 AndAlso dy < 0 Then
-                    mouseAngle += Math.PI * 1.5
-                End If
-            ElseIf dx = 0 AndAlso dy <> 0 Then
-                mouseAngle = If(Math.Sign(dy) > 0, Math.PI / 2, Math.PI * 1.5)
-            ElseIf dx <> 0 AndAlso dy = 0 Then
-                mouseAngle = If(Math.Sign(dx) > 0, 0, Math.PI)
+        Dim pos As Point = Mouse.GetPosition(viewport)
+        Dim actualPos As New Point(pos.X - viewport.ActualWidth / 2, viewport.ActualHeight / 2 - pos.Y)
+        Dim dx As Double = actualPos.X - __lastPos.X, dy As Double = actualPos.Y - __lastPos.Y
+        Dim mouseAngle As Double = 0
+
+        If dx <> 0 AndAlso dy <> 0 Then
+            mouseAngle = Math.Asin(Math.Abs(dy) / Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2)))
+
+            If dx < 0 AndAlso dy > 0 Then
+                mouseAngle += Math.PI / 2
+            ElseIf dx < 0 AndAlso dy < 0 Then
+                mouseAngle += Math.PI
+            ElseIf dx > 0 AndAlso dy < 0 Then
+                mouseAngle += Math.PI * 1.5
             End If
-
-            Dim axisAngle As Double = mouseAngle + Math.PI / 2
-            Dim axis As New Vector3D(Math.Cos(axisAngle) * 4, Math.Sin(axisAngle) * 4, 0)
-            Dim rotation As Double = 0.01 * Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2))
-            Dim r As New QuaternionRotation3D(New Quaternion(axis, rotation * 180 / Math.PI))
-
-            For Each geometry As GeometryModel3D In __geometry
-                With TryCast(geometry.Transform, Transform3DGroup)
-                    .Children.Add(New RotateTransform3D(r))
-                End With
-            Next
-
-            __lastPos = actualPos
+        ElseIf dx = 0 AndAlso dy <> 0 Then
+            mouseAngle = If(Math.Sign(dy) > 0, Math.PI / 2, Math.PI * 1.5)
+        ElseIf dx <> 0 AndAlso dy = 0 Then
+            mouseAngle = If(Math.Sign(dx) > 0, 0, Math.PI)
         End If
+
+        Dim axisAngle As Double = mouseAngle + Math.PI / 2
+        Dim axis As New Vector3D(Math.Cos(axisAngle) * 4, Math.Sin(axisAngle) * 4, 0)
+        Dim rotation As Double = 0.01 * Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2))
+        Dim r As New QuaternionRotation3D(New Quaternion(axis, rotation * 180 / Math.PI))
+
+        For Each geometry As GeometryModel3D In __geometry
+            With TryCast(geometry.Transform, Transform3DGroup)
+                .Children.Add(New RotateTransform3D(r))
+            End With
+        Next
+
+        __lastPos = actualPos
     End Sub
 
     Private Sub Grid_MouseDown(sender As Object, e As MouseButtonEventArgs)
         If e.LeftButton <> MouseButtonState.Pressed Then
             Return
+        Else
+            __drag = True
         End If
-        __drag = True
+
         Dim pos As Point = Mouse.GetPosition(viewport)
         __lastPos = New Point(pos.X - viewport.ActualWidth / 2, viewport.ActualHeight / 2 - pos.Y)
     End Sub
