0aec68d9c986f1e7dce5fb2db9f953aab4645e21
xieguigang <xie.guigang@gmail.com>
2017-02-08 19:45:05 +0800

    improvem,ents on the dataframe system

--- Data/DataFrame/IO/Generic/EntityObject.vb
+++ Data/DataFrame/IO/Generic/EntityObject.vb
@@ -33,9 +33,10 @@ Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
 Namespace IO
 
     ''' <summary>
-    ''' The object entity, <see cref="DynamicPropertyBase(Of String)"/>, <see cref="String"/>
+    ''' The object entity, <see cref="DynamicPropertyBase(Of String)"/>, <see cref="String"/>.
+    ''' (有名称属性的表抽象对象)
     ''' </summary>
-    Public Class EntityObject : Inherits DynamicPropertyBase(Of String)
+    Public Class EntityObject : Inherits Table
         Implements INamedValue
 
         ''' <summary>
@@ -69,46 +70,6 @@ Namespace IO
                 {uidMap, NameOf(EntityObject.ID)}
             }
             Return path.LoadCsv(Of T)(explicit:=False, maps:=map)
-        End Function
-    End Class
-
-    ''' <summary>
-    ''' The numeric dataset, <see cref="DynamicPropertyBase(Of Double)"/>, <see cref="Double"/>.
-    ''' (数值类型的数据集合，每一个数据实体对象都有自己的编号以及数据属性)
-    ''' </summary>
-    Public Class DataSet : Inherits DynamicPropertyBase(Of Double)
-        Implements INamedValue
-
-        Public Property ID As String Implements INamedValue.Key
-
-        ''' <summary>
-        ''' Copy prop[erty value
-        ''' </summary>
-        ''' <returns></returns>
-        Public Overloads Function Copy() As DataSet
-            Return New DataSet With {
-                .ID = ID,
-                .Properties = New Dictionary(Of String, Double)(Properties)
-            }
-        End Function
-
-        ''' <summary>
-        ''' <paramref name="uidMap"/>一般情况下会自动进行判断，不需要具体的设置
-        ''' </summary>
-        ''' <param name="path"></param>
-        ''' <param name="uidMap">
-        ''' 默认是使用csv文件的第一行第一个单元格中的内容作为标识符，但是有时候可能标识符不是在第一列的，则这个时候就需要对这个参数进行赋值了
-        ''' </param>
-        ''' <returns></returns>
-        Public Shared Function LoadDataSet(path$, Optional uidMap$ = Nothing) As IEnumerable(Of DataSet)
-            If uidMap Is Nothing Then
-                Dim first As New RowObject(path.ReadFirstLine)
-                uidMap = first.First
-            End If
-            Dim map As New Dictionary(Of String, String) From {
-                {uidMap, NameOf(DataSet.ID)}
-            }
-            Return path.LoadCsv(Of DataSet)(explicit:=False, maps:=map)
         End Function
     End Class
 End Namespace
---
+++ Data/DataFrame/IO/Generic/Table.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+Namespace IO
+
+    ''' <summary>
+    ''' 没有名称属性的表抽象对象
+    ''' </summary>
+    Public Class Table : Inherits DynamicPropertyBase(Of String)
+    End Class
+End Namespace
