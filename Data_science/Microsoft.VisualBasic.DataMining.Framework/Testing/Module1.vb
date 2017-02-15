fcd521c50e8b5415c7ed9b7666ebcf19b0ab5649
xieguigang <xie.guigang@gmail.com>
2017-02-16 05:45:25 +0800

    build tree from network success

---
+++ Data_science/Microsoft.VisualBasic.DataMining.Framework/Testing/Module1.vb
@@ -0,0 +1,8 @@
+﻿Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports Microsoft.VisualBasic.DataMining.KMeans.BuildNodeTrees
+
+Module Module1
+    Sub Main()
+        Dim tree = Network.Load("G:\Xanthomonas_campestris_8004_uid15\genome\palindrome-motifs\palindrome_promoter=-250bp-cut=0.65,minw=6\binary-net").BuildTree
+    End Sub
+End Module
--- Data_science/Microsoft.VisualBasic.DataMining.Model.Network/BinaryTree/BuildNodeTrees.vb
+++ Data_science/Microsoft.VisualBasic.DataMining.Model.Network/BinaryTree/BuildNodeTrees.vb
@@ -7,8 +7,6 @@ Namespace KMeans
 
     Public Module BuildNodeTrees
 
-        Delegate Sub TreeTraveller(ByRef parent As EntityNode)
-
         ''' <summary>
         ''' 
         ''' </summary>
@@ -16,33 +14,34 @@ Namespace KMeans
         ''' <returns></returns>
         ''' <remarks>请注意：在这里面边是具有方向的，from到to</remarks>
         <Extension> Public Function BuildTree(net As Network) As EntityNode
-            Dim nodes As Dictionary(Of String, NetworkEdge()) =
+            Dim nodesTable As Dictionary(Of String, NetworkEdge()) =
                 net.Edges _
                 .GroupBy(Function(edge) edge.FromNode) _
                 .ToDictionary(Function(k) k.Key,
                               Function(c) c.ToArray)
             ' 从ROOT开始构建
-            Dim root As New EntityNode(Tree.ROOT)
-            Dim appendChilds As New TreeTraveller(
-                Sub(ByRef parent As EntityNode)
-
-                    Dim childs As IEnumerable(Of NetworkEdge) =
-                        nodes(Tree.ROOT) _
-                        .Select(Function(id) nodes(id.FromNode)) _
-                        .IteratesALL
-
-                    ' 遍历下一个连接的节点，并且进行递归构建出整个树
-                    For Each branch As NetworkEdge In childs
-                        With New EntityNode(branch.FromNode)
-                            Call appendChilds(.MySelf)
-                            Call parent.AddChild(.MySelf)
-                        End With
-                    Next
-                End Sub)
-
-            Call appendChilds(root)
-
-            Return root
+            With New EntityNode(Tree.ROOT)
+                Call .MySelf.__appendChilds(nodesTable)
+                Return .MySelf
+            End With
         End Function
+
+        <Extension> Private Sub __appendChilds(ByRef parent As EntityNode, nodesTable As Dictionary(Of String, NetworkEdge()))
+            If Not nodesTable.ContainsKey(parent.Name) Then
+                ' 由于键值都是key，所以对于leaf而言是找不到的
+                ' 已经没有子节点了，在这里直接退出递归
+                Return
+            End If
+
+            Dim childs As NetworkEdge() = nodesTable(parent.Name)
+
+            ' 遍历下一个连接的节点，并且进行递归构建出整个树
+            For Each branch As NetworkEdge In childs
+                With New EntityNode(branch.ToNode)
+                    Call .MySelf.__appendChilds(nodesTable)
+                    Call parent.AddChild(.MySelf)
+                End With
+            Next
+        End Sub
     End Module
 End Namespace
