5ca4baab47e352af7cfce992e3dccc6a314d366f
xieguigang <xie.guigang@gmail.com>
2017-02-19 15:16:11 +0800

    algorithm base

--- Microsoft.VisualBasic.Architecture.Framework/ComponentModel/Algorithm/base/Combination/Comb.vb
+++ Microsoft.VisualBasic.Architecture.Framework/ComponentModel/Algorithm/base/Combination/Comb.vb
@@ -28,6 +28,7 @@
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
 
 Namespace ComponentModel
 
@@ -37,9 +38,8 @@ Namespace ComponentModel
     ''' <remarks></remarks>
     Public Class Comb(Of T)
 
-        Dim lstSource As List(Of T)
+        Dim source As List(Of T)
         Dim p As Integer = 1
-        Dim _NewLine As Boolean = True
 
         ''' <summary>
         ''' 对象列表是否已经完全组合输出
@@ -49,7 +49,7 @@ Namespace ComponentModel
         ''' <remarks></remarks>
         Public ReadOnly Property EOL As Boolean
             Get
-                Return Not lstSource.Count >= 2
+                Return Not source.Count >= 2
             End Get
         End Property
 
@@ -59,50 +59,45 @@ Namespace ComponentModel
         ''' <value></value>
         ''' <returns></returns>
         ''' <remarks></remarks>
-        Public ReadOnly Property NewLine As Boolean
+        Public ReadOnly Property NewLine As Boolean = True
+
+        Public ReadOnly Property CombList As Tuple(Of T, T)()()
             Get
-                Return _NewLine
+                Dim out As Tuple(Of T, T)()() = __internalQuery.ToArray
+                Return out
             End Get
         End Property
 
-        Public ReadOnly Property CombList As KeyValuePair(Of T, T)()()
-            Get
-                Dim LQuery = (From Handle As Integer In lstSource.Sequence.Take(lstSource.Count - 1).AsParallel
-                              Let ArrayList = (From Hwnd As Integer In lstSource.Skip(Handle + 1).Sequence
-                                               Let b = Handle + 1
-                                               Select New KeyValuePair(Of T, T)(lstSource(Handle), lstSource(b + Hwnd))).ToArray
-                              Select ArrayList
-                              Order By ArrayList.Length Descending).ToArray
-                Return LQuery
+        Private Function __internalQuery() As IEnumerable(Of Tuple(Of T, T)())
+            Return From index As Integer
+                   In source.Sequence _
+                       .Take(source.Count - 1) _
+                       .AsParallel
+                   Let combTuples = {
+ _
+                       From offset As Integer
+                       In source.Skip(index + 1).Sequence
+                       Let b = index + 1
+                       Select New Tuple(Of T, T)(source(index), source(b + offset))
+                   }
+                   Select array = combTuples _
+                       .IteratesALL _
+                       .ToArray
+                   Order By array.Length Descending
+        End Function
 
-                'Dim n As Integer = ObjectCollection.Count - 1
-                'Dim List As List(Of KeyValuePair(Of T, T)()) = New List(Of KeyValuePair(Of T, T)())
-                'Dim TEMP As List(Of KeyValuePair(Of T, T)) = New List(Of KeyValuePair(Of T, T))
-
-                'For i As Integer = 0 To ObjectCollection.Count - 1
-                '    For idx As Integer = i + 1 To ObjectCollection.Count - 1
-                '        Call TEMP.Add(New KeyValuePair(Of T, T)(ObjectCollection(i), ObjectCollection(idx)))
-                '    Next
-                '    Call List.Add(TEMP.ToArray)
-                '    Call TEMP.Clear()
-                'Next
-
-                'Return List.ToArray
-            End Get
-        End Property
-
-        Public Function GetObjectPair() As KeyValuePair(Of T, T)
-            If lstSource.Count = 1 Then
+        Public Function GetObjectPair() As Tuple(Of T, T)
+            If source.Count = 1 Then
                 Return Nothing
             End If
 
-            If p < lstSource.Count Then
-                Dim [Object] As T = lstSource(p)
+            If p < source.Count Then
+                Dim o As T = source(p)
                 _NewLine = False
                 p += 1
-                Return New KeyValuePair(Of T, T)(lstSource(0), [Object])
+                Return New Tuple(Of T, T)(source(0), o)
             Else
-                lstSource.RemoveAt(0)
+                source.RemoveAt(0)
                 p = 1
                 Dim [ObjectPair] = GetObjectPair()
                 _NewLine = True
@@ -114,20 +109,26 @@ Namespace ComponentModel
         Friend Sub New()
         End Sub
 
-        Public Shared Function CreateObject(Collection As Generic.IEnumerable(Of T)) As Comb(Of T)
-            Return New Comb(Of T) With {.lstSource = Collection.ToList}
+        Public Shared Function CreateObject(source As IEnumerable(Of T)) As Comb(Of T)
+            Return New Comb(Of T) With {
+                .source = source.ToList
+            }
         End Function
 
         Public Overrides Function ToString() As String
-            Return String.Format("Comb(Of {0})::There is {1} object last.", GetType(T).FullName, lstSource.Count)
+            Return String.Format("Comb(Of {0})::There is {1} object last.", GetType(T).FullName, source.Count)
         End Function
 
-        Public Shared Widening Operator CType(ObjectCollection As T()) As Comb(Of T)
-            Return New Comb(Of T) With {.lstSource = ObjectCollection.ToList}
+        Public Shared Widening Operator CType(source As T()) As Comb(Of T)
+            Return New Comb(Of T) With {
+                .source = source.ToList
+            }
         End Operator
 
-        Public Shared Widening Operator CType(ObjectCollection As List(Of T)) As Comb(Of T)
-            Return New Comb(Of T) With {.lstSource = ObjectCollection}
+        Public Shared Widening Operator CType(source As List(Of T)) As Comb(Of T)
+            Return New Comb(Of T) With {
+                .source = source
+            }
         End Operator
 
         ''' <summary>
@@ -137,14 +138,14 @@ Namespace ComponentModel
         ''' <param name="source"></param>
         ''' <returns></returns>
         ''' <remarks></remarks>
-        Public Shared Iterator Function CreateCompleteObjectPairs(source As IEnumerable(Of T)) As IEnumerable(Of KeyValuePair(Of T, T)())
+        Public Shared Iterator Function CreateCompleteObjectPairs(source As IEnumerable(Of T)) As IEnumerable(Of Tuple(Of T, T)())
             Dim array As T() = source.ToArray
 
             For Each i As T In array
-                Dim tmp As New List(Of KeyValuePair(Of T, T))
+                Dim tmp As New List(Of Tuple(Of T, T))
 
                 For Each j As T In array
-                    tmp += New KeyValuePair(Of T, T)(i, j)
+                    tmp += New Tuple(Of T, T)(i, j)
                 Next
 
                 Yield tmp.ToArray
@@ -159,12 +160,12 @@ Namespace ComponentModel
     Public Module Comb
 
         <Extension>
-        Public Iterator Function CreateCombos(Of TA, TB)(sourceA As IEnumerable(Of TA), sourceB As IEnumerable(Of TB)) As IEnumerable(Of KeyValuePair(Of TA, TB))
+        Public Iterator Function CreateCombos(Of TA, TB)(sourceA As IEnumerable(Of TA), sourceB As IEnumerable(Of TB)) As IEnumerable(Of Tuple(Of TA, TB))
             Dim b As TB() = sourceB.ToArray
 
             For Each i As TA In sourceA
                 For Each j As TB In b
-                    Yield New KeyValuePair(Of TA, TB)(i, j)
+                    Yield New Tuple(Of TA, TB)(i, j)
                 Next
             Next
         End Function
