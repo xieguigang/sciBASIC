6232f5b5914aaa6a036bd5bcc059d5b7699b7d04
xieguigang <xie.guigang@gmail.com>
2017-02-24 20:57:00 +0800

    improvements on the network abstract API

---
+++ Data_science/Microsoft.VisualBasic.DataMining.Model.Network/KMeansNetwork.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports Microsoft.VisualBasic.Language
+
+Namespace KMeans
+
+    Public Module KMeansNetwork
+
+        <Extension>
+        Public Function ToNetwork(kmeans As IEnumerable(Of EntityLDM)) As Network
+            Dim data As EntityLDM() = kmeans.ToArray
+            Dim nodes As Dictionary(Of Node) = data _
+                .Select(Function(n) New Node With {
+                    .ID = n.Name,
+                    .NodeType = n.Cluster
+                }).ToDictionary
+            Dim edges As New List(Of NetworkEdge)
+
+            Return New Network With {
+                .Edges = edges,
+                .Nodes = nodes
+            }
+        End Function
+    End Module
+End Namespace
