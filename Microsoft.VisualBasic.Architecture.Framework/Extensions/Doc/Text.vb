53a9723a7c26a3a86f70be9705dd960819c24a34
谢桂纲 <xie.guigang@gcmodeller.org>
2017-03-22 15:33:47 +0800

    fix xml cref

--- Microsoft.VisualBasic.Architecture.Framework/Extensions/Doc/Text.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Extensions/Doc/Text.vb
@@ -43,7 +43,7 @@ Public Module TextDoc
     ''' 
     ''' </summary>
     ''' <param name="handle$">
-    ''' + 当这个参数为文件路径的时候会返回<see cref="IteratesALL(Of T)(IEnumerable(Of IEnumerable(Of T)))"/>函数的结果
+    ''' + 当这个参数为文件路径的时候会返回<see cref="Linq.IteratesALL(Of T)(IEnumerable(Of IEnumerable(Of T)))"/>函数的结果
     ''' + 当这个参数只是为文本字符串的时候，则会返回<see cref="lTokens"/>函数的结果
     ''' </param>
     ''' <returns></returns>
