a0a6ba6577a011e231e286497b19442e6ddcada1
谢桂纲 <xie.guigang@gcmodeller.org>
2017-03-29 13:40:06 +0800

    fix bugs for the large array xml loader

--- Microsoft.VisualBasic.Architecture.Framework/Text/Xml/Linq/Linq.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Text/Xml/Linq/Linq.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports System.Text
 Imports System.Xml
 
 Namespace Text.Xml.Linq
@@ -12,24 +13,51 @@ Namespace Text.Xml.Linq
             Return XmlDoc
         End Function
 
+        <Extension>
+        Public Function GetTypeName(type As Type, default$) As String
+            If [default].StringEmpty Then
+                Return type.Name
+            Else
+                Return [default]
+            End If
+        End Function
+
         ''' <summary>
         ''' 这个函数只建议在读取超大的XML文件的时候使用，并且这个XML文件仅仅是一个数组或者列表的序列化结果
         ''' </summary>
         ''' <typeparam name="T"></typeparam>
         ''' <param name="XML$">超大的XML文件的文件路径</param>
-        ''' <param name="typeName">列表之中的节点在XML之中的tag标记名称</param>
+        ''' <param name="typeName">
+        ''' 列表之中的节点在XML之中的tag标记名称，假若这个参数值为空的话，则会默认使用目标类型名称<see cref="Type.Name"/>
+        ''' </param>
         ''' <returns></returns>
         <Extension>
-        Public Iterator Function LoadXmlDataSet(Of T As Class)(XML$, typeName$) As IEnumerable(Of T)
+        Public Iterator Function LoadXmlDataSet(Of T As Class)(XML$, Optional typeName$ = Nothing) As IEnumerable(Of T)
+            Dim nodeName$ = GetType(T).GetTypeName([default]:=typeName)
             Dim XmlNodeList As XmlNodeList = XML _
                 .LoadXmlDocument _
-                .GetElementsByTagName(typeName)
+                .GetElementsByTagName(nodeName)
             Dim o As T
+            Dim sb As New StringBuilder
 
             For Each xmlNode As XmlNode In XmlNodeList
-                ' Reset the user
                 XML = xmlNode.InnerXml
-                o = XML.CreateObjectFromXmlFragment(Of T)
+
+                Call sb.Clear()
+                Call sb.AppendLine("<?xml version=""1.0"" encoding=""utf-16""?>")
+                Call sb.Append($"<{typeName} xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" ")
+
+                For Each attr As XmlAttribute In xmlNode.Attributes
+                    Call sb.Append($"{attr.Name}=""{attr.Value}""")
+                    Call sb.Append(" ")
+                Next
+
+                Call sb.AppendLine(">")
+                Call sb.AppendLine(XML)
+                Call sb.AppendLine($"</{typeName}>")
+
+                XML = sb.ToString
+                o = XML.LoadFromXml(Of T)
 
                 Yield o
             Next
