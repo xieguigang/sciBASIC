6a0a25c4562f49a14d1bae952c0269547cb29fcb
xieguigang <xie.guigang@gmail.com>
2017-03-30 19:52:58 +0800

    tuple range iterates???

--- Data_science/Mathematical/Plots/Testing/ChartingBase.Test/Module1.vb
+++ Data_science/Mathematical/Plots/Testing/ChartingBase.Test/Module1.vb
@@ -1,42 +1,49 @@
 ﻿#Region "Microsoft.VisualBasic::1d25732ebab32c6eb0b2c2c6a178c01e, ..\sciBASIC#\Data_science\Mathematical\Plots\Testing\ChartingBase.Test\Module1.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
-Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
-Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Axis
-Imports Microsoft.VisualBasic.Serialization.JSON
-Imports Microsoft.VisualBasic.Imaging
 Imports System.Drawing
 Imports Microsoft.VisualBasic.Data.ChartPlots.BarPlot.StyledBarplot
 Imports Microsoft.VisualBasic.Data.ChartPlots.Dendrogram
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Axis
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Mathematical
+Imports Microsoft.VisualBasic.Serialization.JSON
 
 Module Module1
 
     Sub Main()
+
+        For Each i% In (1, 1000).Iterates
+            println("%s = %X", i, i)
+        Next
+
+
+        Pause()
 
         Dim t As New Tree("root")
 
