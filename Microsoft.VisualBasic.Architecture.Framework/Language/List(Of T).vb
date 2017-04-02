62dbecd6674f568245edc154e1ab8acdcd455211
xieguigang <xie.guigang@gmail.com>
2017-04-02 21:35:14 +0800

    fixed for the ODEs GA demo

--- Microsoft.VisualBasic.Architecture.Framework/Language/List(Of T).vb
+++ Microsoft.VisualBasic.Architecture.Framework/Language/List(Of T).vb
@@ -1,31 +1,32 @@
 ﻿#Region "Microsoft.VisualBasic::a63d76923e137aa3fa4a0f6d33eb0412, ..\sciBASIC#\Microsoft.VisualBasic.Architecture.Framework\Language\List(Of T).vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
+Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.ComponentModel.Ranges
 Imports Microsoft.VisualBasic.FileIO
 Imports Microsoft.VisualBasic.Language.UnixBash.FileSystem
@@ -47,6 +48,20 @@ Namespace Language
         Dim __index As Pointer
 
 #Region "Improvements Index"
+
+        ''' <summary>
+        ''' 这个是为了ODEs计算模块所准备的一个数据接口
+        ''' </summary>
+        ''' <param name="address"></param>
+        ''' <returns></returns>
+        Default Public Overloads Property Item(address As IAddress(Of Integer)) As T
+            Get
+                Return Item(address.Address)
+            End Get
+            Set(value As T)
+                Item(address.Address) = value
+            End Set
+        End Property
 
         Default Public Overloads Property Item(index%) As T
             Get
