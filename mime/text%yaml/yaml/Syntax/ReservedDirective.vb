44dc163ad0d5c54e17ad980e94908efe5845f3ba
xieguigang <xie.guigang@gmail.com>
2017-04-15 00:57:36 +0800

    fix namespace

---
+++ mime/text%yaml/yaml/Syntax/ReservedDirective.vb
@@ -0,0 +1,40 @@
+﻿#Region "Microsoft.VisualBasic::d116ce402ceb51fc89af267d326fd0a1, ..\sciBASIC#\mime\MIME_Markups\YAML\Syntax\ReservedDirective.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xieguigang (xie.guigang@live.com)
+    '       xie (genetics@smrucc.org)
+    ' 
+    ' Copyright (c) 2016 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Imports System.Collections.Generic
+Imports System.Text
+
+Namespace Syntax
+    Public Class ReservedDirective
+        Inherits Directive
+        Public Name As String
+
+        Public Parameters As New List(Of String)()
+
+    End Class
+End Namespace
---
+++ mime/text%yaml/yaml/Syntax/YamlDocument.vb
@@ -0,0 +1,44 @@
+﻿#Region "Microsoft.VisualBasic::a11615f874cf8141f7ecef3bd079fab7, ..\sciBASIC#\mime\MIME_Markups\YAML\Syntax\YamlDocument.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xieguigang (xie.guigang@live.com)
+    '       xie (genetics@smrucc.org)
+    ' 
+    ' Copyright (c) 2016 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Imports System.Collections.Generic
+Imports System.Text
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+
+Namespace Syntax
+
+    Public Class YamlDocument
+
+        Public Root As DataItem
+
+        Public Directives As New List(Of Directive)()
+        Public AnchoredItems As New Dictionary(Of String, DataItem)()
+
+
+    End Class
+End Namespace
