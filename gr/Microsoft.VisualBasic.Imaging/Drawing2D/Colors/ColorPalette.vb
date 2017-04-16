89c46e85d5a5d7c51a3a468b9abb0fe51872d182
xieguigang <xie.guigang@gmail.com>
2017-04-16 20:14:27 +0800

    range index

--- gr/Microsoft.VisualBasic.Imaging/Drawing2D/Colors/ColorPalette.vb
+++ gr/Microsoft.VisualBasic.Imaging/Drawing2D/Colors/ColorPalette.vb
@@ -1,18 +1,22 @@
 ﻿Imports System.Drawing
 Imports System.Windows.Forms
 Imports Microsoft.VisualBasic.ComponentModel
-Imports ColorsPalette = Microsoft.VisualBasic.Language.List(Of Microsoft.VisualBasic.ComponentModel.Map(Of System.Drawing.Rectangle, System.Drawing.Color))
-
+Imports Microsoft.VisualBasic.ComponentModel.Ranges
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
+Imports ColorsPalette = Microsoft.VisualBasic.ComponentModel.Map(Of System.Drawing.Rectangle, System.Drawing.Color)
 
 Namespace Drawing2D.Colors
 
     Public Class ColorPalette
 
-        Dim level1Colors As New ColorsPalette
-        Dim level2Colors As New ColorsPalette
-
-        Dim n = 20
+        Dim level1Colors As ColorsPalette()
+        Dim level2Colors As ColorsPalette()
         Dim half!
+        Dim current% = 0
+        Dim index As SeqValue(Of IntRange)()
+
+        Public Event SelectColor(c As Color)
 
         Private Sub ColorPalette_Load(sender As Object, e As EventArgs) Handles Me.Load
             Call __colorsPaletteModels()
@@ -20,15 +24,23 @@ Namespace Drawing2D.Colors
         End Sub
 
         Private Sub ColorPalette_MouseClick(sender As Object, e As MouseEventArgs) Handles Me.MouseClick
-            If e.Location.Y > half Then
+            Dim ly = e.Location.Y
+            Dim i As Integer = index _
+                .Where(Function(x) (+x).IsInside(e.X)) _
+                .Select(Function(x) x.i) _
+                .DefaultFirst(-1)
+
+            If ly > half + 10 Then
                 ' 选二级颜色
-            Else
+                If i > -1 Then
+                    RaiseEvent SelectColor(level2Colors(i).Maps)
+                End If
+            ElseIf ly > 10 Then
                 ' 设置一级颜色
-
+                ' 进行界面刷新
+                Call Me.Invalidate()
+            Else
             End If
-
-            ' 进行界面刷新
-            Call Me.Invalidate()
         End Sub
 
         Private Sub ColorPalette_Resize(sender As Object, e As EventArgs) Handles Me.Resize
@@ -40,23 +52,68 @@ Namespace Drawing2D.Colors
         ''' 大小或者选择了新颜色之后需要重新生成模型
         ''' </summary>
         Private Sub __colorsPaletteModels()
+            Dim n = Designer.TSF.Length
             Dim dw! = Width / n
             Dim dh! = Height / 2 - 10
+            Dim y = 10
             Dim colors As Color() = Designer.TSF
+            Dim getColors =
+                Function() As ColorsPalette()
+                    Dim out As New List(Of ColorsPalette)
 
-            For i As Integer = 0 To n - 1
-                level1Colors += New Map(Of Rectangle, Color) With {
-                    .Key = New Rectangle With {
-                        .X = i * dw,
-                        .Y = 10,
-                        .Width = dw,
-                        .Height = dh
-                    },
-                    .Maps = colors(i)
-                }
-            Next
+                    For i As Integer = 0 To n - 1
+                        out += New Map(Of Rectangle, Color) With {
+                            .Key = New Rectangle With {
+                                .X = i * dw,
+                                .Y = y,
+                                .Width = dw,
+                                .Height = dh
+                            },
+                            .Maps = colors(i)
+                        }
+                    Next
 
+                    Return out
+                End Function
+
+            level1Colors = getColors()
             half = Height / 2
+
+            Dim c As Color()
+
+            If current = 0 Then
+                c = {
+                    colors.Last,
+                    colors(0),
+                    colors(1)
+                }
+            ElseIf current = colors.Length - 1 Then
+                c = {
+                    colors(colors.Length - 2),
+                    colors.Last,
+                    colors(0)
+                }
+            Else
+                c = {
+                    colors(current - 1),
+                    colors(current),
+                    colors(current + 1)
+                }
+            End If
+
+            colors = Designer.CubicSpline(c, n)
+            y = half + 10
+            level2Colors = getColors()
+            index = level2Colors _
+                .SeqIterator _
+                .Select(Function(i)
+                            Dim r = +i
+                            Return New SeqValue(Of IntRange) With {
+                                .i = i,
+                                .value = New IntRange(r.Key.Left, r.Key.Right)
+                            }
+                        End Function) _
+                .ToArray
         End Sub
 
         ''' <summary>
@@ -67,7 +124,7 @@ Namespace Drawing2D.Colors
         Private Sub ColorPalette_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
             Dim g As Graphics = e.Graphics
 
-            For Each block In level1Colors
+            For Each block As Map(Of Rectangle, Color) In level1Colors.JoinIterates(level2Colors)
                 Dim b As New SolidBrush(block.Maps)
                 Call g.FillRectangle(b, block.Key)
             Next
