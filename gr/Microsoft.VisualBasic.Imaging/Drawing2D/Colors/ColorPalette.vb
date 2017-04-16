f01d85dfdfcc1a4bc78a611d332726e6ef9dc47f
xieguigang <xie.guigang@gmail.com>
2017-04-16 17:16:06 +0800

    add trigger events

--- gr/Microsoft.VisualBasic.Imaging/Drawing2D/Colors/ColorPalette.vb
+++ gr/Microsoft.VisualBasic.Imaging/Drawing2D/Colors/ColorPalette.vb
@@ -32,6 +32,8 @@ Namespace Drawing2D.Colors
             Next
 
             half = Height / 2
+
+            Call Me.Invalidate()
         End Sub
 
         Private Sub ColorPalette_MouseClick(sender As Object, e As MouseEventArgs) Handles Me.MouseClick
@@ -41,10 +43,30 @@ Namespace Drawing2D.Colors
                 ' 设置一级颜色
 
             End If
+
+            ' 进行界面刷新
+            Call Me.Invalidate()
         End Sub
 
         Private Sub ColorPalette_Resize(sender As Object, e As EventArgs) Handles Me.Resize
             half = Height / 2
+            Call Me.Invalidate()
+        End Sub
+
+        ''' <summary>
+        ''' 大小或者选择了新颜色之后需要重新生成模型
+        ''' </summary>
+        Private Sub __colorsPaletteModels()
+
+        End Sub
+
+        ''' <summary>
+        ''' 大小改变了之后或者选择了新的颜色之后在这里进行界面重绘
+        ''' </summary>
+        ''' <param name="sender"></param>
+        ''' <param name="e"></param>
+        Private Sub ColorPalette_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+
         End Sub
     End Class
 End Namespace
