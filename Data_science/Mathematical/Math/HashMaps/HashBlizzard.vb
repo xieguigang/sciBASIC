ed053f72b9e1c9ebf0223db89071398562cafe06
谢桂纲 <xie.guigang@gcmodeller.org>
2017-04-29 13:37:56 +0800

    split hash map code modules

---
+++ Data_science/Mathematical/Math/HashMaps/HashBlizzard.vb
@@ -0,0 +1,81 @@
+﻿Namespace HashMaps
+
+    Public Class HashBlizzard
+
+        ReadOnly cryptTable(&H100 * 5 - 1) As UInt64
+
+        Sub New()
+            Call HashBlizzardInit()
+        End Sub
+
+        Private Sub HashBlizzardInit()
+            Dim seed As UInt64 = &H100001
+            Dim index1 As UInt64 = 0
+            Dim index2 As UInt64 = 0
+            Dim I As UInt64
+            Dim KKK As UInt64 = 0
+
+            For index1 = 0 To &H100 - 1
+                index2 = index1
+                For I = 0 To 4
+                    Dim temp1, temp2 As UInt64
+                    seed = (seed * 125 + 3) Mod &H2AAAAB
+                    temp1 = (seed And &HFFFF) << &H10
+                    seed = (seed * 125 + 3) Mod &H2AAAAB
+                    temp2 = (seed And &HFFFF)
+                    cryptTable(index2) = (temp1 Or temp2) '//|
+                    index2 += &H100
+                Next
+            Next
+        End Sub
+
+        Const __hashBlizzard_seed2& = &HEEEEEEEE
+
+        ''' <summary>
+        ''' 暴雪公司出名的哈希码.
+        ''' 测试了 二千万 GUID, 没有重复.但运算量比较大。
+        ''' </summary>
+        ''' <param name="key"></param>
+        ''' <param name="HasType">HasType =0 ,1 ,2 </param>
+        ''' <returns></returns>
+        Public Function HashBlizzard(Key As String, Optional HasType As Long = 0) As UInt64
+            Dim L As Int32 = Key.Length - 1
+            Dim KeyCharArr() As Char = Key.ToArray
+            Dim seed1 As UncheckedUInt64 = &H7FED7FED
+            Dim seed2 As New UncheckedUInt64(__hashBlizzard_seed2)
+            Dim LoopID As Int32 = 0
+
+            While (LoopID < L)
+                Dim ascCode As Int32 = Asc(KeyCharArr(LoopID))
+                seed1 = cryptTable((HasType << 8) + ascCode) Xor (seed1 + seed2)
+                seed2 = ascCode + seed1 + seed2 + (seed2 << 5) + 3
+                LoopID += 1
+            End While
+
+            Return seed1
+        End Function
+
+        ''' <summary>
+        ''' 暴雪公司著名的 HashMap .
+        ''' 测试了 二千万 GUID, 没有重复.但运算量比较大。
+        ''' </summary>
+        ''' <param name="KeyByte"></param>
+        ''' <param name="HasType">HasType =[0 ,1 ,2] </param>
+        ''' <returns></returns>
+        Public Function HashBlizzard(KeyByte() As Byte, Optional HasType As Long = 0) As UInt64
+            Dim L As Int32 = KeyByte.Length - 1
+            Dim seed1 As UncheckedUInt64 = &H7FED7FED
+            Dim seed2 As New UncheckedUInt64(__hashBlizzard_seed2)
+            Dim LoopID As Int32 = 0
+
+            While (LoopID < L)
+                Dim ascCode As Int32 = KeyByte(LoopID)
+                seed1 = cryptTable((HasType << 8) + ascCode) Xor (seed1 + seed2)
+                seed2 = ascCode + seed1 + seed2 + (seed2 << 5) + 3
+                LoopID += 1
+            End While
+
+            Return seed1
+        End Function
+    End Class
+End Namespace
---
+++ Data_science/Mathematical/Math/HashMaps/UncheckedUInt64.vb
@@ -0,0 +1,79 @@
+﻿
+Imports System.Runtime.InteropServices
+
+Namespace HashMaps
+
+    ''' <summary>
+    ''' <see cref="UInt64"/>
+    ''' </summary>
+    <StructLayout(LayoutKind.Explicit)> Public Structure UncheckedUInt64
+
+        <FieldOffset(0)> Private longValue As UInt64
+        <FieldOffset(0)> Private intValueLo As UInt32
+        <FieldOffset(4)> Private intValueHi As UInt32
+
+        Sub New(newLongValue As UInt64)
+            longValue = newLongValue
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return longValue
+        End Function
+
+        Public Overloads Shared Widening Operator CType(value As UInt64) As UncheckedUInt64
+            Return New UncheckedUInt64(value)
+        End Operator
+
+        Public Overloads Shared Widening Operator CType(value As UncheckedUInt64) As UInt64
+            Return value.longValue
+        End Operator
+
+        Public Overloads Shared Operator *(x As UncheckedUInt64, y As UInt64) As UncheckedUInt64
+            Return New UncheckedUInt64(x.longValue * y)
+        End Operator
+
+        Public Overloads Shared Operator /(x As UncheckedUInt64, y As UInt64) As UncheckedUInt64
+            Return New UncheckedUInt64(x.longValue / y)
+        End Operator
+
+        Public Overloads Shared Operator ^(x As UncheckedUInt64, y As Double) As UncheckedUInt64
+            Return New UncheckedUInt64(x.longValue ^ y)
+        End Operator
+
+        Public Overloads Shared Operator Xor(x As UncheckedUInt64, y As UInt64) As UncheckedUInt64
+            Return New UncheckedUInt64(x.longValue Xor y)
+        End Operator
+
+        Public Overloads Shared Operator +(x As UncheckedUInt64, y As UInt64) As UncheckedUInt64
+            Return New UncheckedUInt64(x.longValue + y)
+        End Operator
+
+        Public Overloads Shared Operator -(x As UncheckedUInt64, y As UInt64) As UncheckedUInt64
+            Return New UncheckedUInt64(x.longValue - y)
+        End Operator
+
+        Public Overloads Shared Operator <<(x As UncheckedUInt64, y As Int32) As UncheckedUInt64
+            Return New UncheckedUInt64(x.longValue << y)
+        End Operator
+
+        Public Overloads Shared Operator >>(x As UncheckedUInt64, y As Int32) As UncheckedUInt64
+            Return New UncheckedUInt64(x.longValue >> y)
+        End Operator
+
+        Public Overloads Shared Operator And(x As UncheckedUInt64, y As UInt64) As UncheckedUInt64
+            Return New UncheckedUInt64(x.longValue And y)
+        End Operator
+
+        Public Overloads Shared Operator =(x As UncheckedUInt64, y As UInt64) As UncheckedUInt64
+            Return New UncheckedUInt64(x.longValue)
+        End Operator
+
+        Public Overloads Shared Operator <>(x As UncheckedUInt64, y As UInt64) As UncheckedUInt64
+            Return New UncheckedUInt64(x.longValue <> y)
+        End Operator
+
+        Public Overloads Shared Operator Not(x As UncheckedUInt64) As UncheckedUInt64
+            Return New UncheckedUInt64(Not x.longValue)
+        End Operator
+    End Structure
+End Namespace
