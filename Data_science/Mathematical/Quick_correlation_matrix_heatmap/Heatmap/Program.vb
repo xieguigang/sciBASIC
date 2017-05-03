a47bdfb2f398d670548892943161febddf8518a2
xieguigang <xie.guigang@gmail.com>
2017-05-03 17:27:22 +0800

    improvements on the data frame API

--- Data_science/Mathematical/Quick_correlation_matrix_heatmap/Heatmap/Program.vb
+++ Data_science/Mathematical/Quick_correlation_matrix_heatmap/Heatmap/Program.vb
@@ -1,23 +1,19 @@
 ﻿Imports Microsoft.VisualBasic.Data.ChartPlots
+Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Mathematical.Correlations
 
 Module Program
 
     Sub Main()
-
+        Call heatmap2()
     End Sub
 
-
     Public Sub heatmap2()
-
-        Dim data = LoadData("G:\GCModeller\src\runtime\sciBASIC#\Data_science\Mathematical\images\heatmap\Sample.csv", True)
-        Dim spcc = data.CorrelationMatrix(AddressOf Spearman)
+        Dim data = DataSet.LoadDataSet("G:\GCModeller\src\runtime\sciBASIC#\Data_science\Mathematical\images\heatmap\Sample.heatmap.png")
+        Dim spcc = data.Vectors.CorrelationMatrix(AddressOf Spearman)
 
         Call HeatmapTable.Plot(spcc,) _
             .Save("G:\GCModeller\src\runtime\sciBASIC#\Data_science\Mathematical\images\heatmap\Sample.SPCC.png")
-        Call Microsoft.VisualBasic.Data.ChartPlots.Heatmap.Plot(spcc, mapLevels:=25) _
-            .Save("G:\GCModeller\src\runtime\sciBASIC#\Data_science\Mathematical\images\heatmap\Sample.heatmap.png")
-
     End Sub
 End Module
