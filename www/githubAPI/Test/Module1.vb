1d279db3b3933b4af1a28b5358b1adde4a40a7bd
谢桂纲 <xie.guigang@gcmodeller.org>
2017-05-04 12:03:18 +0800

    github test

--- www/githubAPI/Test/Module1.vb
+++ www/githubAPI/Test/Module1.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::4899f1daaca5d29acd6ff4224872ecdb, ..\sciBASIC#\www\githubAPI\Test\Module1.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -35,7 +35,7 @@ Module Module1
 
     Sub Main()
 
-        WebAPI.Proxy = "http://127.0.0.1:8087"
+        ' WebAPI.Proxy = "http://127.0.0.1:8087"
 
 
         Dim followers As User() = WebAPI.Users.Followers("xieguigang") '= "J:\GCModeller\src\runtime\sciBASIC#\www\data\github\followers.json".ReadAllText.LoadObject(Of User()) '"xieguigang".Followers
@@ -46,6 +46,9 @@ Module Module1
 
         Dim notFollings = following.WhoIsNotFollowMe(followers).ToArray
         Call notFollings.GetJson(True).SaveTo("./notfollowing.json")
+
+        Dim IamNotFollowings = following.WhoIamNotFollow(followers).ToArray
+        Call IamNotFollowings.GetJson(True).SaveTo("./I-am-not-Following.json")
 
         Pause()
     End Sub
