71608dbbe7e64832768faab5c8e16eb5999d6f9f
谢桂纲 <xie.guigang@gcmodeller.org>
2017-05-10 15:22:12 +0800

    try create force directed network generator

---
+++ gr/Datavisualization.Network/Datavisualization.Network/Layouts/forceNetwork.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+
+Namespace Layouts
+
+    Public Module forceNetwork
+
+        ''' <summary>
+        ''' Applies the spring embedder.
+        ''' </summary>
+        ''' <param name="Network"></param>
+        ''' <param name="iterations"></param>
+        <ExportAPI("Layout.ForceDirected")>
+        <Extension>
+        Public Sub doLayout(Network As NetworkGraph, iterations As Integer, size As Size)
+
+        End Sub
+    End Module
+End Namespace
