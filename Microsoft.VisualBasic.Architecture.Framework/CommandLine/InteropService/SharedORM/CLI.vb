e0a08f583d6f99ba8d044103fdd3530dca44c784
xieguigang <xie.guigang@gmail.com>
2017-05-11 21:54:02 +0800

    CLI helper

---
+++ Microsoft.VisualBasic.Architecture.Framework/CommandLine/InteropService/SharedORM/CLI.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.Language
+
+Namespace CommandLine.InteropService.SharedORM
+
+    <AttributeUsage(AttributeTargets.Module)>
+    Public Class CLIAttribute : Inherits Attribute
+
+        Public Shared Function ParseAssembly(dll$) As Type
+            Dim assembly As Assembly = Assembly.LoadFile(dll)
+            Dim type As Type = LinqAPI.DefaultFirst(Of Type) <=
+ _
+                From t As Type
+                In assembly.GetTypes
+                Where Not t.GetCustomAttribute(Of CLIAttribute) Is Nothing
+                Select t ' 
+
+            Return type
+        End Function
+    End Class
+End Namespace
