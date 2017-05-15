c8c29ef523b31c9ff68bb5fd0199c1dfe717ff2d
谢桂纲 <xie.guigang@gcmodeller.org>
2017-05-15 12:05:24 +0800

    degree as size for nodes

--- gr/Datavisualization.Network/Visualizer/Styling/NodeStyles.vb
+++ gr/Datavisualization.Network/Visualizer/Styling/NodeStyles.vb
@@ -39,6 +39,21 @@ Namespace Styling
 
     Public Module NodeStyles
 
+        <Extension> Public Function DegreeAsSize(nodes As IEnumerable(Of Node),
+                                                 getDegree As Func(Of Node, Double),
+                                                 sizeRange As DoubleRange) As Map(Of Node, Double)()
+            Return nodes.ValDegreeAsSize(getDegree, sizeRange)
+        End Function
+
+        <Extension> Public Function DegreeAsSize(nodes As IEnumerable(Of Node), sizeRange As DoubleRange, Optional degree$ = names.REFLECTION_ID_MAPPING_DEGREE) As Map(Of Node, Double)()
+            Dim valDegree = Function(node As Node)
+                                Return node.Data(degree).ParseDouble
+                            End Function
+            Return nodes.DegreeAsSize(
+                getDegree:=valDegree,
+                sizeRange:=sizeRange)
+        End Function
+
         ''' <summary>
         ''' 
         ''' </summary>
@@ -47,9 +62,9 @@ Namespace Styling
         ''' <param name="getDegree"></param>
         ''' <param name="sizeRange">将节点的大小映射到这个半径大小的区间之内</param>
         ''' <returns></returns>
-        <Extension> Public Function DegreeAsSize(Of T)(nodes As IEnumerable(Of T),
-                                                       getDegree As Func(Of T, Double),
-                                                       sizeRange As DoubleRange) As Map(Of T, Double)()
+        <Extension> Public Function ValDegreeAsSize(Of T)(nodes As IEnumerable(Of T),
+                                                          getDegree As Func(Of T, Double),
+                                                          sizeRange As DoubleRange) As Map(Of T, Double)()
             Dim array As T() = nodes.ToArray
             Dim degrees#() = array.Select(getDegree).ToArray
             Dim size#() = degrees.RangeTransform([to]:=sizeRange)
