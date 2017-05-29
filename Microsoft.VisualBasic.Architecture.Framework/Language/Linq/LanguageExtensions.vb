223cc5a93e93e9833cc16c207c2a3acf7aa33398
谢桂纲 <xie.guigang@gcmodeller.org>
2017-05-29 17:12:31 +0800

    add ranges syntax extensions

--- Microsoft.VisualBasic.Architecture.Framework/Language/Linq/LanguageExtensions.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Language/Linq/LanguageExtensions.vb
@@ -1,33 +1,34 @@
 ﻿#Region "Microsoft.VisualBasic::f46ce5d6cc87e4de06ae5273742cf995, ..\sciBASIC#\Microsoft.VisualBasic.Architecture.Framework\Language\Linq\LanguageExtensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.Ranges
 
 Namespace Language
 
@@ -46,6 +47,19 @@ Namespace Language
                 .Value = value,
                 .Description = descript
             }
+        End Sub
+
+        ''' <summary>
+        ''' From {"1,100", "100,1000", "200,500"}
+        ''' </summary>
+        ''' <param name="list"></param>
+        ''' <param name="range$"></param>
+        <Extension> Public Sub Add(list As List(Of IntRange), range$)
+            list += range
+        End Sub
+
+        <Extension> Public Sub Add(list As List(Of DoubleRange), range$)
+            list += range
         End Sub
     End Module
 End Namespace
