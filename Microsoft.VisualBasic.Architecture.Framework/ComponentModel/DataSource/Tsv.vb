a1fcd163559fec0cc9324c0b6adeda8a50f27075
xieguigang <xie.guigang@gmail.com>
2017-06-03 14:24:33 +0800

    improvements on the dataframe API

--- Microsoft.VisualBasic.Architecture.Framework/ComponentModel/DataSource/Tsv.vb
+++ Microsoft.VisualBasic.Architecture.Framework/ComponentModel/DataSource/Tsv.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::9e7d3681bc112ef9e381aa4b94de6e0c, ..\sciBASIC#\Microsoft.VisualBasic.Architecture.Framework\ComponentModel\DataSource\Tsv.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -95,7 +95,7 @@ Namespace ComponentModel.DataSourceModel
                 .ToArray
             Dim source = If(lower,
                 headers.Select(AddressOf Strings.LCase),
-                headers.As(Of IEnumerable(Of String)))
+                TryCast(headers, IEnumerable(Of String)))
             Return New IndexOf(Of String)(source)
         End Function
 
