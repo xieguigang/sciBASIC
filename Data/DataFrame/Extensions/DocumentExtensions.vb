6e4747543f1dfef3daba22349290afd255cd5aa3
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-09 16:55:08 +0800

    object renames

--- Data/DataFrame/Extensions/DocumentExtensions.vb
+++ Data/DataFrame/Extensions/DocumentExtensions.vb
@@ -61,8 +61,8 @@ Public Module DocumentExtensions
 
     <Extension>
     Public Function Apply(ByRef row As RowObject, action As Func(Of String, String), Optional skip As Integer = 0) As RowObject
-        For i As Integer = skip To row._innerColumns.Count - 1
-            row._innerColumns(i) = action(row._innerColumns(i))
+        For i As Integer = skip To row.buffer.Count - 1
+            row.buffer(i) = action(row.buffer(i))
         Next
 
         Return row
