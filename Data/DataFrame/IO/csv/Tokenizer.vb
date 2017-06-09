df4369ea05bb47ba29b3f2c99d7a07912c786f7f
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-09 15:45:15 +0800

    fix for continutes   ,"","","", in csv

--- Data/DataFrame/IO/csv/Tokenizer.vb
+++ Data/DataFrame/IO/csv/Tokenizer.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::fcaa2d92376c02eeb590423fbe9af323, ..\sciBASIC#\Data\DataFrame\IO\csv\Tokenizer.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -82,7 +82,11 @@ Namespace IO
             Dim preToken As Boolean = False
             Dim deliExit As Boolean = False
 
-            For Each c As Char In s.Replace("""""", "\""")
+            s = s.Replace(","""",", ",,")
+            s = s.Replace(","""",", ",,")
+            s = s.Replace("""""", "\""")
+
+            For Each c As Char In s
                 If c = ","c Then
                     If Not stack Then
                         Call tokens.Add(New String(temp.ToArray))
