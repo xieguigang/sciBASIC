5061f6ad9e736801588bdd9a5c22e43c6bb72466
xieguigang <xie.guigang@gmail.com>
2017-06-17 16:24:11 +0800

    improvements on the graphics helper

--- gr/Microsoft.VisualBasic.Imaging/Drawing2D/Math2D/ConvexHull/ConvexHull.vb
+++ gr/Microsoft.VisualBasic.Imaging/Drawing2D/Math2D/ConvexHull/ConvexHull.vb
@@ -7,6 +7,7 @@ Namespace Drawing2D.Math2D.ConvexHull
     ''' or in a Euclidean space (or, more generally, in an affine space over the reals) is the smallest 
     ''' convex set that contains X. For instance, when X is a bounded subset of the plane, the convex 
     ''' hull may be visualized as the shape enclosed by a rubber band stretched around X.
+    ''' (多边形的点的数量必须要至少3个点)
     ''' </summary>
     Public Module ConvexHull
 
@@ -18,10 +19,20 @@ Namespace Drawing2D.Math2D.ConvexHull
             Return ((q.X - p.X) * (r.Y - p.Y()) - (r.X - p.X) * (q.Y - p.Y)).CompareTo(0)
         End Function
 
+        ''' <summary>
+        ''' 点的数量必须要多于2个!
+        ''' </summary>
+        ''' <param name="points"></param>
+        ''' <returns></returns>
         Public Function JarvisMatch(points As IEnumerable(Of Point)) As Point()
             Return Math2D.ConvexHull.JarvisMatch.ConvexHull(points)
         End Function
 
+        ''' <summary>
+        ''' 点的数量必须要多于2个!
+        ''' </summary>
+        ''' <param name="points"></param>
+        ''' <returns></returns>
         Public Function GrahamScan(points As IEnumerable(Of Point)) As Point()
             Return Math2D.ConvexHull.GrahamScan.ConvexHull(points)
         End Function
