116dcd215918da411946b74619e97b6dbebae46b
xieguigang <xie.guigang@gmail.com>
2017-06-18 19:51:00 +0800

    network layout parameters configuration

--- gr/Datavisualization.Network/NetworkCanvas/Canvas.vb
+++ gr/Datavisualization.Network/NetworkCanvas/Canvas.vb
@@ -95,7 +95,7 @@ Public Class Canvas
         Me.ShowLabel = showLabel
     End Sub
 
-    Public ReadOnly Property FdgArgs As ForceDirectedArgs = Config.Load
+    Public ReadOnly Property FdgArgs As ForceDirectedArgs = Parameters.Load
 
     Public Sub SetRotate(x As Double)
         If Not space3D Then
