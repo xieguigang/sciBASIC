a7a6649b284ffd1a02f53d2d0f3bc3c7ef3b523f
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-27 12:17:38 +0800

    add graph model test

---
+++ gr/Datavisualization.Network/Datavisualization.Network/GraphTesting/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+
+Module Module1
+
+    Sub Main()
+
+        Dim graph = GraphAPI.CytoscapeExportAsGraph(
+        "D:\GCModeller\src\runtime\sciBASIC#\gr\Datavisualization.Network\net_test\xcb-main-Edges.csv",
+        "D:\GCModeller\src\runtime\sciBASIC#\gr\Datavisualization.Network\net_test\xcb-main-Nodes.csv")
+
+        Dim treeModel As New GraphTree(graph)
+
+    End Sub
+End Module
