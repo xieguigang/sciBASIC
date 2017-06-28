fce63531f002d47cabb1df80541f25d4295f23b2
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-28 13:19:54 +0800

    improvements on the dataframe API

--- Data/DataFrame/IO/Generic/EntityObject.vb
+++ Data/DataFrame/IO/Generic/EntityObject.vb
@@ -29,6 +29,7 @@
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.VisualBasic.Text
 
@@ -78,6 +79,33 @@ Namespace IO
 
         Public Shared Function LoadDataSet(path As String, Optional uidMap As String = Nothing, Optional tsv As Boolean = False) As IEnumerable(Of EntityObject)
             Return LoadDataSet(Of EntityObject)(path, uidMap, tsv)
+        End Function
+
+        Public Shared Function GetIDList(path$, Optional uidMap As String = Nothing, Optional tsv As Boolean = False, Optional ignoreMapErrors As Boolean = False) As String()
+            Dim table As File = If(tsv, File.LoadTsv(path), File.Load(path))
+            Dim getIDsDefault =
+                Function()
+                    Return table.Columns _
+                        .First _
+                        .Skip(1) _
+                        .ToArray
+                End Function
+
+            If uidMap.StringEmpty Then
+                ' 第一列的数据就是所需要的编号数据
+                Return getIDsDefault()
+            Else
+                With table.Headers.IndexOf(uidMap)
+                    If .ref = -1 AndAlso ignoreMapErrors Then
+                        Return getIDsDefault()
+                    Else
+                        ' 当不忽略错误的时候，不存在的uidMap其index位置会出现越界的错误直接在这里报错
+                        Return table.Columns(.ref) _
+                            .Skip(1) _
+                            .ToArray
+                    End If
+                End With
+            End If
         End Function
 
         Public Shared Function LoadDataSet(Of T As EntityObject)(path As String, Optional uidMap As String = Nothing, Optional tsv As Boolean = False) As IEnumerable(Of T)
