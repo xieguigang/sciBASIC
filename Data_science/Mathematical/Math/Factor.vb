76aa3c6a41d72f59378f333b0b5f0d9ea7d4187b
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-30 12:27:59 +0800

    fix function referecne errors

---
+++ Data_science/Mathematical/Math/Factor.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
+
+Public Class Factor(Of T As IComparable(Of T)) : Inherits int
+    Implements Value(Of T).IValueOf
+
+    Public Property FactorValue As T Implements Value(Of T).IValueOf.value
+
+    Public Overrides Function ToString() As String
+        Return FactorValue.ToString
+    End Function
+End Class
+
+Public Module FactorExtensions
+
+    ''' <summary>
+    ''' 这个函数和<see cref="SeqIterator"/>类似，但是这个函数之中添加了去重和排序
+    ''' </summary>
+    ''' <typeparam name="T"></typeparam>
+    ''' <param name="source"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function factors(Of T As IComparable(Of T))(source As IEnumerable(Of T)) As Factor(Of T)()
+        Dim array = source.ToArray
+        Dim unique As Index(Of T) = array _
+            .Distinct _
+            .OrderBy(Function(x) x) _
+            .Indexing
+        Dim out = array _
+            .Select(Function(x)
+                        Return New Factor(Of T) With {
+                            .value = unique.IndexOf(x),
+                            .FactorValue = x
+                        }
+                    End Function) _
+            .ToArray
+        Return out
+    End Function
+End Module
--- Microsoft.VisualBasic.Architecture.Framework/Scripting/InputHandler.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Scripting/InputHandler.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::7f6ca784c115cd5b02e32d8026e9f076, ..\sciBASIC#\Microsoft.VisualBasic.Architecture.Framework\Scripting\InputHandler.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -62,7 +62,7 @@ Namespace Scripting
             {GetType(Integer), AddressOf Casting.CastInteger},
             {GetType(Double), AddressOf Casting.ParseNumeric},
             {GetType(Long), AddressOf Casting.CastLong},
-            {GetType(Boolean), AddressOf getBoolean},
+            {GetType(Boolean), AddressOf ParseBoolean},
             {GetType(Char()), AddressOf Casting.CastCharArray},
             {GetType(Date), AddressOf Casting.CastDate},
             {GetType(StringBuilder), AddressOf Casting.CastStringBuilder},
