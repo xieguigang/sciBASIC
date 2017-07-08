72f91e4f5d7bbd6da384f606df9f97e94efbfa60
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-08 10:57:14 +0800

    ParallelogramLaw

---
+++ gr/physics/Force.vb
@@ -0,0 +1,44 @@
+﻿Imports Microsoft.VisualBasic.Mathematical.LinearAlgebra
+
+''' <summary>
+''' 力
+''' </summary>
+Public Class Force
+
+    ''' <summary>
+    ''' 力的大小
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Strength As Double
+    ''' <summary>
+    ''' 力的方向，与水平的夹角
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Angle As Double
+
+    Public Overrides Function ToString() As String
+        Return $"alpha={Angle}, with {Strength} newton."
+    End Function
+
+    Public Shared Operator ^(f As Force, n As Double) As Double
+        Return f.Strength ^ n
+    End Operator
+
+    Public Shared Operator *(x As Double, f As Force) As Double
+        Return x * f.Strength
+    End Operator
+
+    Public Shared Operator *(x As Integer, f As Force) As Double
+        Return x * f.Strength
+    End Operator
+
+    ''' <summary>
+    ''' 使用平行四边形法则进行力的合成
+    ''' </summary>
+    ''' <param name="f1"></param>
+    ''' <param name="f2"></param>
+    ''' <returns></returns>
+    Public Shared Operator +(f1 As Force, f2 As Force) As Force
+        Return Math.ParallelogramLaw(f1, f2)
+    End Operator
+End Class
---
+++ gr/physics/Math.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Math
+
+Public Module Math
+
+    Public Function ParallelogramLaw(f1 As Force, f2 As Force) As Force
+        Dim alpha = f1.Angle - f2.Angle
+        Dim F = f1 ^ 2 + f2 ^ 2 + 2 * f1 * f2 * Cos(alpha)
+        Dim sina = Sin(alpha) * f1 / F
+
+        alpha = Sinh(sina)
+
+        Return New Force With {
+            .Strength = F,
+            .Angle = alpha
+        }
+    End Function
+End Module
