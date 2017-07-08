5d87c1f355408ac39b36388169b6165a9e351f22
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-08 20:08:28 +0800

    test success

--- gr/physics/physics_Test/Program.vb
+++ gr/physics/physics_Test/Program.vb
@@ -5,12 +5,18 @@ Public Module Program
 
     Sub Main()
 
-        Dim f1 As New Force(100, 0)
-        Dim f2 As New Force(100, 1 / 2 * PI)
+        Call add(New Force(100, 0), New Force(100, 1 / 2 * PI))
 
-        Call $"{f1.ToString} + {f2.ToString} => {(f1 + f2).ToString}".__DEBUG_ECHO
+        Call add(New Force(100, 0), New Force(100, PI))
+        Call add(New Force(100, 0), New Force(100, PI * 2))
+
+        Call add(New Force(100, 0), New Force(100, PI + 1 / 2 * PI))
 
         Pause()
 
+    End Sub
+
+    Sub add(f1 As Force, f2 As Force)
+        Call $"||{f1.ToString}|| + ||{f2.ToString}||  =>  ||{(f1 + f2).ToString}||".__DEBUG_ECHO
     End Sub
 End Module
