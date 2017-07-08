01e40662a93f79b6e563b561d8b6a6df2b9d0647
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-08 13:58:15 +0800

    add draw label

--- gr/physics/Debugger.vb
+++ gr/physics/Debugger.vb
@@ -10,14 +10,21 @@ Public Module Debugger
         Return New PointF(v(X), v(Y))
     End Function
 
-    <Extension> Public Sub ShowForce(m As MassPoint, canvas As Graphics2D, F As Force)
+    <Extension> Public Sub ShowForce(m As MassPoint, ByRef canvas As Graphics2D, F As Force)
         Dim v = F.Decomposition2D
         Dim pen As New Pen(Color.Red) With {
             .EndCap = LineCap.Triangle,
             .Width = 3
         }
+        Dim font As New Font(FontFace.MicrosoftYaHei, 12, FontStyle.Bold)
+        Dim a = m.Point.Vector2D
+        Dim b = (m.Point + v).Vector2D
 
-        Call canvas.DrawCircle(m.Point.Vector2D, 10, Brushes.Black)
-        Call canvas.DrawLine(pen, m.Point.Vector2D, (m.Point + v).Vector2D)
+        With canvas
+            Call .DrawCircle(a, 10, Brushes.Black)
+            Call .DrawLine(pen, a, b)
+            Call .DrawString(m.ToString, font, Brushes.Black, a)
+            Call .DrawString(F.ToString, font, Brushes.Blue, b)
+        End With
     End Sub
 End Module
