c1b989e44f0f4f58c6f7626a131e0969b5a578db
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-09 05:43:18 +0800

    world object

--- gr/physics/MassPoint.vb
+++ gr/physics/MassPoint.vb
@@ -1,7 +1,14 @@
 ﻿Imports Microsoft.VisualBasic.Math.LinearAlgebra
 
+''' <summary>
+''' 质点，没有体积和大小，只有质量和电荷量的理想物理对象
+''' </summary>
 Public Class MassPoint
 
+    ''' <summary>
+    ''' 物体的质量，会影响重力，可能会影响摩擦力
+    ''' </summary>
+    ''' <returns></returns>
     Public Property Mass As Double
 
     ''' <summary>
@@ -17,13 +24,58 @@ Public Class MassPoint
     Public Property Charge As Double
 
     ''' <summary>
-    ''' 物体受力后产生位移
+    ''' 速度
     ''' </summary>
-    Public Sub ApplyForce(F As Force, Optional c# = 1)
-        If Point.Count = 2 Then
-            Point += F.Decomposition2D * c
-        Else
-            Point += F.Decomposition3D * c
-        End If
+    ''' <returns></returns>
+    Public Property Velocity As Vector
+    ''' <summary>
+    ''' 加速度
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Acceleration As Vector
+
+    ''' <summary>
+    ''' 对象所处的世界的空间类型
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property World As World.Type
+        Get
+            Return CType(Point.Count, World.Type)
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' 物体在dt时间段内以当前的速度<see cref="Velocity"/>之下产生一小部分的位移
+    ''' </summary>
+    Public Sub Displacement()
+        Point += Velocity
     End Sub
+
+    Public Overrides Function ToString() As String
+        If World = Physics.World.Type.Plain2D Then
+            Return Point.Vector2D.ToString
+        Else
+            Return Point.ToString
+        End If
+    End Function
+
+    ''' <summary>
+    ''' 物体受到外部力的作用下改变了加速度
+    ''' </summary>
+    ''' <param name="m"></param>
+    ''' <param name="F">这个是最终的合力</param>
+    ''' <returns></returns>
+    Public Shared Operator +(m As MassPoint, F As Force) As MassPoint
+        With m
+            Dim Fxyz As Vector = If(
+                .World = Physics.World.Type.Plain2D,
+                F.Decomposition2D,
+                F.Decomposition3D)
+            Dim a As Vector = Fxyz / .Mass  ' F=ma， 得到了各个空间分量上面的加速度
+
+            .Acceleration += a
+        End With
+
+        Return m
+    End Operator
 End Class
---
+++ gr/physics/World.vb
@@ -0,0 +1,22 @@
+﻿''' <summary>
+''' Physics world reactor
+''' </summary>
+Public Class World
+
+    Public Enum Type As Byte
+        Plain2D = 2
+        Spatial3D = 3
+    End Enum
+
+    Dim objects As List(Of MassPoint)
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="time%">迭代的次数</param>
+    Public Sub React(time As UInteger)
+        For i As UInteger = 0 To time
+
+        Next
+    End Sub
+End Class
