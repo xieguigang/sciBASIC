57f4d5b4aea08a7597d4f127b819bb4fec35272d
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-13 11:18:58 +0800

    node not found from the degree result ????

--- gr/Datavisualization.Network/Datavisualization.Network/Analysis/Statistics.vb
+++ gr/Datavisualization.Network/Datavisualization.Network/Analysis/Statistics.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::8062b57f99ce76b2e7acc34a4e993d19, ..\sciBASIC#\gr\Datavisualization.Network\Datavisualization.Network\Analysis\Statistics.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -31,6 +31,7 @@ Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph.Abstract
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Serialization.JSON
 Imports names = Microsoft.VisualBasic.Data.visualize.Network.FileStream.Generic.NameOf
 Imports NetGraph = Microsoft.VisualBasic.Data.visualize.Network.FileStream.NetworkTables
 
@@ -71,8 +72,15 @@ Namespace Analysis
 
             With net.Edges.ComputeDegreeData
                 For Each node As FileStream.Node In net.Nodes
-                    d = degrees(node.ID)
-                    node.Add(names.REFLECTION_ID_MAPPING_DEGREE, d)
+                    If Not degrees.ContainsKey(node.ID) Then
+                        Dim ex As New Exception("nodes: " & net.Nodes.Keys.GetJson)
+                        ex = New Exception("degrees: " & degrees.GetJson)
+                        ex = New Exception("Current node was not found! => " & node.GetJson)
+                        Throw ex
+                    Else
+                        d = degrees(node.ID)
+                        node.Add(names.REFLECTION_ID_MAPPING_DEGREE, d)
+                    End If
 
                     If .in.ContainsKey(node.ID) Then
                         d = .in(node.ID)
