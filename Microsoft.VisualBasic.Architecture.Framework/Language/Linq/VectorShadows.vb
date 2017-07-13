81632dedbd3b2a38fc3030ab6e7da37b3815b310
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-13 16:26:16 +0800

    basic vector model

--- Microsoft.VisualBasic.Architecture.Framework/Language/Linq/VectorShadows.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Language/Linq/VectorShadows.vb
@@ -1,21 +1,34 @@
-﻿Namespace Language
+﻿Imports System.Dynamic
+
+Namespace Language
 
     ''' <summary>
     ''' Vectorization programming language feature for VisualBasic
     ''' </summary>
     ''' <typeparam name="T"></typeparam>
-    Public Class VectorShadows(Of T) : Implements IEnumerable(Of T)
+    Public Class VectorShadows(Of T) : Inherits DynamicObject
+        Implements IEnumerable(Of T)
+
+        Dim vector As T()
+
+        Public ReadOnly Property Length As Integer
+            Get
+                Return vector.Length
+            End Get
+        End Property
 
         Sub New(source As IEnumerable(Of T))
-
+            vector = source.ToArray
         End Sub
 
-        Public Function GetEnumerator() As IEnumerator(Of T) Implements IEnumerable(Of T).GetEnumerator
-            Throw New NotImplementedException()
+        Public Iterator Function GetEnumerator() As IEnumerator(Of T) Implements IEnumerable(Of T).GetEnumerator
+            For Each x As T In vector
+                Yield x
+            Next
         End Function
 
-        Private Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
-            Throw New NotImplementedException()
+        Private Iterator Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+            Yield GetEnumerator()
         End Function
     End Class
 End Namespace
