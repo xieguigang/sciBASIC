7082c66c6b163dc0cadbfa825dd393e7b040d6c5
xieguigang <xie.guigang@gcmodeller.org>
2017-07-16 14:00:16 +0800

    add interface implementation

---
+++ Microsoft.VisualBasic.Architecture.Framework/Language/Linq/Vector.vb
@@ -0,0 +1,18 @@
+﻿Namespace Language
+
+    Public Class Vector(Of T) : Implements IEnumerable(Of T)
+
+        ReadOnly buffer As T()
+
+
+        Public Iterator Function GetEnumerator() As IEnumerator(Of T) Implements IEnumerable(Of T).GetEnumerator
+            For Each x In buffer
+                Yield x
+            Next
+        End Function
+
+        Private Iterator Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+            Yield GetEnumerator()
+        End Function
+    End Class
+End Namespace
