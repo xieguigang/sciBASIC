29a07db71bfb3547818038debbcea4c4b46ee197
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-22 15:57:27 +0800

    fix project reference

--- Data_science/Bootstrapping/Monte-Carlo/Bifurcation/Bifurcation.vb
+++ Data_science/Bootstrapping/Monte-Carlo/Bifurcation/Bifurcation.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::99f785604a0e259ea44fa12084094948, ..\sciBASIC#\Data_science\Bootstrapping\Monte-Carlo\Bifurcation\Bifurcation.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -136,10 +136,10 @@ Namespace MonteCarlo
             End If
 
             Dim result As ClusterCollection(Of Entity) =
-                ClusterDataSet(ncluster, inputs,
-                debug:=True,
-                [stop]:=[stop],
-                parallel:=True)
+                inputs.ClusterDataSet(ncluster,
+                                      debug:=True,
+                                      [stop]:=[stop],
+                                      parallel:=True)
 
             For Each cluster As SeqValue(Of KMeansCluster(Of Entity)) In result.SeqIterator
                 Dim datas As Dictionary(Of String, Double()) =
