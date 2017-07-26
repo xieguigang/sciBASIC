95904a6fa8dc8883a56386b09effc5cbc30efecb
谢桂纲 <xie.guigang@gcmodeller.org>
2017-07-26 16:09:21 +0800

    the highlight required merge style

--- mime/text%html/HTML/CSS/Stroke.vb
+++ mime/text%html/HTML/CSS/Stroke.vb
@@ -41,7 +41,7 @@ Namespace HTML.CSS
 
         Public Const AxisStroke$ = "stroke: black; stroke-width: 2px; stroke-dash: solid;"
         Public Const AxisGridStroke$ = "stroke: lightgray; stroke-width: 2px; stroke-dash: dash;"
-        Public Const HighlightStroke$ = "stroke: lightgray; stroke-width: 0.5px; stroke-dash: dash;"
+        Public Const HighlightStroke$ = "stroke: gray; stroke-width: 0.5px; stroke-dash: dash;"
 
         Public Property fill As String
         Public Property width As Single
