c0b183c27674534bb8f7b11b6570c2e678ca2ea4
asuka <amethyst.asuka@gcmodeller.org>
2017-07-29 14:51:48 +0800

    numerics vector converts

--- Data_science/Mathematica/Math/Math/Algebra/Vector/NumericsVector.vb
+++ Data_science/Mathematica/Math/Math/Algebra/Vector/NumericsVector.vb
@@ -1,31 +1,50 @@
-﻿Imports Microsoft.VisualBasic.Math.SyntaxAPI.Vectors
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Math.SyntaxAPI.Vectors
 Imports Microsoft.VisualBasic.Scripting
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace LinearAlgebra
 
     ''' <summary>
-    ''' <see cref="Integer"/>, <see cref="Long"/>, <see cref="ULong"/>
+    ''' Numerics vector converts for numeric types like:
+    ''' <see cref="Integer"/>, <see cref="Long"/>, <see cref="ULong"/>, <see cref="Byte"/>, <see cref="Single"/>
     ''' </summary>
-    ''' <typeparam name="T"></typeparam>
-    Public Class NumericsVector(Of T As {IComparable, Structure}) : Inherits GenericVector(Of Double)
+    Public Module NumericsVector
 
-        ReadOnly operators As NumericsOperator = NumericsOperator.GetOperators(GetType(T))
+        <Extension> Public Function AsVector(Of T As {Structure, IComparable, IComparable(Of T), IEquatable(Of T), IConvertible, IFormattable})(source As IEnumerable(Of T)) As Vector
+            Return New Vector(source.Select(Function(x) CDbl(CObj(x))))
+        End Function
 
-        Sub New()
-            Dim i As Integer
+        <Extension> Public Function AsInteger(vector As Vector) As Integer()
+            Return vector.Select(Function(x) CInt(x)).ToArray
+        End Function
 
-            Dim o = Me / 3
+        <Extension> Public Function AsLong(vector As Vector) As Long()
+            Return vector.Select(Function(x) CLng(x)).ToArray
+        End Function
 
-        End Sub
+        <Extension> Public Function AsSingle(vector As Vector) As Single()
+            Return vector.Select(Function(x) CSng(x)).ToArray
+        End Function
 
-        Public Overloads Shared Operator +(v As NumericsVector(Of T), o As Object) As Object
+        <Extension> Public Function AsUInteger(vector As Vector) As UInteger()
+            Return vector.Select(Function(x) CUInt(x)).ToArray
+        End Function
 
-        End Operator
+        <Extension> Public Function AsULong(vector As Vector) As ULong()
+            Return vector.Select(Function(x) CULng(x)).ToArray
+        End Function
 
+        <Extension> Public Function AsUShort(vector As Vector) As UShort()
+            Return vector.Select(Function(x) CUShort(x)).ToArray
+        End Function
 
-        Public Overloads Shared Operator /(v As NumericsVector(Of T), o As IComparable) As Object
+        <Extension> Public Function AsBytes(vector As Vector) As Byte()
+            Return vector.Select(Function(x) CByte(x)).ToArray
+        End Function
 
-        End Operator
-    End Class
+        <Extension> Public Function AsSByte(vector As Vector) As SByte()
+            Return vector.Select(Function(x) CSByte(x)).ToArray
+        End Function
+    End Module
 End Namespace
