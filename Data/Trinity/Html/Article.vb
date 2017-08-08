19860aea42b005baba2730d3bc36a3a86e71d8be
xieguigang <xie.guigang@gcmodeller.org>
2017-08-08 12:47:32 +0800

    duplicated commits

---
+++ Data/Trinity/Html/Article.vb
@@ -0,0 +1,30 @@
+﻿''' <summary>
+''' 文章正文数据模型
+''' </summary>
+Public Structure Article
+
+    ''' <summary>
+    ''' 文章标题
+    ''' </summary>
+    Public Property Title As String
+
+    ''' <summary>
+    ''' 正文文本
+    ''' </summary>
+    Public Property Content As String
+
+    ''' <summary>
+    ''' 带标签正文
+    ''' </summary>
+    Public Property ContentWithTags As String
+
+    ''' <summary>
+    ''' 文章发布时间
+    ''' </summary>
+    Public Property PublishDate As DateTime
+
+    Public Overrides Function ToString() As String
+        Return Title
+    End Function
+
+End Structure
--- Data/Trinity/TextRank.vb
+++ Data/Trinity/TextRank.vb
@@ -1,34 +1,34 @@
 ﻿#Region "Microsoft.VisualBasic::c38ba59d43decdae4483244cc0150aa6, ..\sciBASIC#\Data\Trinity\TextRank.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Algorithm.base
-Imports Microsoft.VisualBasic.Data.visualize.Network
+Imports Microsoft.VisualBasic.Data.visualize.Network.Analysis.PageRank
 Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
 Imports Microsoft.VisualBasic.Text
 
@@ -65,7 +65,7 @@ Public Module TextRank
 
     <Extension>
     Public Function TextGraph(sentences As IEnumerable(Of String)) As GraphMatrix
-        Dim net As New Network
+        Dim net As New NetworkTables
         Dim source As String() = sentences _
             .Select(AddressOf Trim) _
             .Where(Function(s) Not String.IsNullOrEmpty(s)) _
