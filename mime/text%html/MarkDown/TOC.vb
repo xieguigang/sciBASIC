2e1dbc2588752951c750b1ccde8947da7a29c259
asuka <amethyst.asuka@gcmodeller.org>
2017-08-13 01:52:36 +0800

    get headers

--- mime/text%html/MarkDown/TOC.vb
+++ mime/text%html/MarkDown/TOC.vb
@@ -1,4 +1,6 @@
 ﻿Imports System.Text
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
 
 Namespace MarkDown
 
@@ -14,6 +16,7 @@ Namespace MarkDown
         ''' <returns></returns>
         Public Function AddToc(md$, Optional numbering As Boolean = True, Optional autoSave As Boolean = True) As String
             Dim sb As New StringBuilder
+            Dim headers = GetHeaders(md)
 
             Call sb.AppendLine("<!-- vb.net-markdown-toc -->")
             Call sb.AppendLine("<!-- vb.net-markdown-toc-config
@@ -27,8 +30,37 @@ Namespace MarkDown
             Return sb.ToString
         End Function
 
-        Public Function GetHeaders(md$)
+        ''' <summary>
+        ''' 按header在markdown文档之中出现的顺序进行返回
+        ''' </summary>
+        ''' <param name="md$"></param>
+        ''' <returns></returns>
+        Public Function GetHeaders(md$) As String()
+            Dim headers As New List(Of String)
 
+            headers += MarkdownHTML._headerSetext.Matches(md).ToArray
+            headers += MarkdownHTML._headerAtx.Matches(md).ToArray
+
+            Dim orders As New List(Of SeqValue(Of String))
+            Dim pos%
+
+            For Each headerGroup As IGrouping(Of String, String) In headers.GroupBy(Function(s) s)
+                Do While True
+                    pos = InStr(pos, md, headerGroup.Key)
+
+                    If pos > 0 Then
+                        orders += New SeqValue(Of String) With {
+                            .i = pos,
+                            .value = headerGroup.Key
+                        }
+                    End If
+                Loop
+            Next
+
+            Return orders _
+                .OrderBy(Function(i) i.i) _
+                .Select(Function(s) s.value) _
+                .ToArray
         End Function
     End Module
 End Namespace
