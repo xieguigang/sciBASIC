4b9dea7c3697d6929c09894c48d50fe3942ec3bb
xieguigang <xie.guigang@gcmodeller.org>
2017-08-14 23:37:52 +0800

    named vector?

---
+++ Data_science/Mathematica/Math/Math/Algebra/Vector/NamedVector.vb
@@ -0,0 +1,10 @@
+﻿
+Namespace LinearAlgebra
+
+    ''' <summary>
+    ''' <see cref="Dictionary(Of String, Double)"/>
+    ''' </summary>
+    Public Class NamedVector
+
+    End Class
+End Namespace
