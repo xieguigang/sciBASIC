1ebfa9bcf0d14a650b3baf165481a9335ca20f8f
xieguigang <xie.guigang@gcmodeller.org>
2017-08-15 12:55:56 +0800

    namedvector factory

--- Data_science/Mathematica/Math/Math/Algebra/Vector/NamedVector.vb
+++ Data_science/Mathematica/Math/Math/Algebra/Vector/NamedVector.vb
@@ -1,23 +1,34 @@
 ﻿
+Imports Microsoft.VisualBasic.Serialization.JSON
+
 Namespace LinearAlgebra
 
     ''' <summary>
-    ''' <see cref="Dictionary(Of String, Double)"/>
+    ''' Factory for <see cref="Dictionary(Of String, Double)"/> to <see cref="Vector"/>
     ''' </summary>
-    Public Class NamedVector
+    Public Class NamedVectorFactory
 
-        Dim factors As Factor(Of String)()
-        Dim vector As Vector
+        ReadOnly factors As Factor(Of String)()
 
-        Sub New(vector As Dictionary(Of String, Double))
-            Me.factors = FactorExtensions.factors(vector.Keys)
-            Me.vector = factors _
-                .Select(Function(factor) vector(factor.FactorValue)) _
-                .AsVector
+        Sub New(factors As IEnumerable(Of String))
+            Me.factors = FactorExtensions.factors(factors)
         End Sub
 
+        Public Function AsVector(data As Dictionary(Of String, Double)) As Vector
+            Dim vector#() = New Double(factors.Length - 1) {}
+
+            For Each factor As Factor(Of String) In factors
+                vector(factor.Value) = data(factor)
+            Next
+
+            Return vector.AsVector
+        End Function
+
         Public Overrides Function ToString() As String
-            Return vector.ToString
+            Return factors _
+                .Select(Function(factor) factor.FactorValue) _
+                .ToArray _
+                .GetJson
         End Function
     End Class
 End Namespace
