b74e7273e023f4a44b045d9cee17896c1ce52a02
xieguigang <xie.guigang@gcmodeller.org>
2017-08-15 12:40:37 +0800

    create named vector

--- Data_science/Mathematica/Math/Math/Algebra/Vector/NamedVector.vb
+++ Data_science/Mathematica/Math/Math/Algebra/Vector/NamedVector.vb
@@ -6,5 +6,18 @@ Namespace LinearAlgebra
     ''' </summary>
     Public Class NamedVector
 
+        Dim factors As Factor(Of String)()
+        Dim vector As Vector
+
+        Sub New(vector As Dictionary(Of String, Double))
+            Me.factors = FactorExtensions.factors(vector.Keys)
+            Me.vector = factors _
+                .Select(Function(factor) vector(factor.FactorValue)) _
+                .AsVector
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return vector.ToString
+        End Function
     End Class
 End Namespace
