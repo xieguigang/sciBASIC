2d211bba8a03f1c3282527e5deb535a4334f2692
xieguigang <xie.guigang@gcmodeller.org>
2017-08-25 16:44:38 +0800

    add default value helper

---
+++ Microsoft.VisualBasic.Architecture.Framework/Language/API.vb
@@ -0,0 +1,29 @@
+﻿Namespace Language
+
+    Public Module LanguageAPI
+
+        ''' <summary>
+        ''' 模拟R语言之中的``%||%``操作符
+        ''' 
+        ''' ```R
+        ''' `%||%` &lt;- function(x, y) if (is.null(x)) y else x
+        ''' 
+        ''' NULL %||% 123
+        ''' # 123
+        ''' 
+        ''' 233 %||% 123
+        ''' # 233
+        ''' ```
+        ''' </summary>
+        ''' <typeparam name="T"></typeparam>
+        ''' <param name="x"></param>
+        ''' <param name="isNothing"></param>
+        ''' <returns></returns>
+        Public Function [Default](Of T)(x As T, Optional isNothing As Assert(Of Object) = Nothing) As DefaultValue(Of T)
+            Return New DefaultValue(Of T) With {
+                .Value = x,
+                .assert = isNothing Or DefaultValue(Of T).defaultAssert
+            }
+        End Function
+    End Module
+End Namespace
---
+++ Microsoft.VisualBasic.Architecture.Framework/Language/Value/Default.vb
@@ -0,0 +1,43 @@
+﻿Imports Microsoft.VisualBasic.Language.Perl
+
+Namespace Language
+
+    Public Delegate Function Assert(Of T)(obj As T) As Boolean
+
+    ''' <summary>
+    ''' The default value
+    ''' </summary>
+    ''' <typeparam name="T"></typeparam>
+    Public Structure DefaultValue(Of T)
+
+        ''' <summary>
+        ''' The default value
+        ''' </summary>
+        Dim Value As T
+        ''' <summary>
+        ''' asset that if target value is null?
+        ''' </summary>
+        Dim assert As Assert(Of Object)
+
+        Friend Shared ReadOnly defaultAssert As New DefaultValue(Of Assert(Of Object)) With {
+            .Value = AddressOf ExceptionHandler.Default,
+            .assert = Function(assert)
+                          Return assert Is Nothing
+                      End Function
+        }
+
+        Public Overrides Function ToString() As String
+            Return $"default({Value})"
+        End Function
+
+        Public Shared Operator Or(obj As T, [default] As DefaultValue(Of T)) As T
+            With [default]
+                If .assert(obj) Then
+                    Return .Value
+                Else
+                    Return obj
+                End If
+            End With
+        End Operator
+    End Structure
+End Namespace
