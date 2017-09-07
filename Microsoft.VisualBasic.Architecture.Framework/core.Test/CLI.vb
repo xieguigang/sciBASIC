77acc30b980bee599997e27e42ba6e2ce4ac581f
xieguigang <xie.guigang@gcmodeller.org>
2017-09-08 07:17:11 +0800

    add cli test

---
+++ Microsoft.VisualBasic.Architecture.Framework/core.Test/CLI.vb
@@ -0,0 +1,18 @@
+﻿Imports System.ComponentModel
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+
+Module CLI
+
+    Function Main() As Integer
+        Return GetType(CLI).RunCLI(App.CommandLine)
+    End Function
+
+    <ExportAPI("/CLI.docs.test")>
+    <Description("Test description text")>
+    <Usage("/CLI.docs.test [/echo ""hello world!"" /out <out.txt>]")>
+    <Argument("/echo", True, CLITypes.String, PipelineTypes.std_in, AcceptTypes:={GetType })>
+    Public Function CLIDocumentTest(args As CommandLine) As Integer
+
+    End Function
+End Module
