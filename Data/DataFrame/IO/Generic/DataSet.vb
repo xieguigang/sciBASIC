6d00d4a61afe9905f2e64f77ea6646d47075657d
xieguigang <xie.guigang@gcmodeller.org>
2017-09-13 15:10:08 +0800

    fix the vector runtime type casting error

--- Data/DataFrame/IO/Generic/DataSet.vb
+++ Data/DataFrame/IO/Generic/DataSet.vb
@@ -63,6 +63,12 @@ Namespace IO
             Return $"{ID} has ({Properties.Keys.Take(5).JoinBy(", ")}...) {MyBase.ToString}"
         End Function
 
+        ''' <summary>
+        ''' 直接使用<paramref name="labels"/>取出<see cref="Properties"/>之中的一个子集
+        ''' 对于不存在的属性，默认值为零
+        ''' </summary>
+        ''' <param name="labels"></param>
+        ''' <returns></returns>
         Public Function SubSet(labels As IEnumerable(Of String)) As DataSet
             Return New DataSet With {
                 .ID = ID,
