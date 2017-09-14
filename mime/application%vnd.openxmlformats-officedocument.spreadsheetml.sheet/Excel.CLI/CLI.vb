5262b555b79a09220830c77e75754b1c7a9d8f33
xieguigang <xie.guigang@gcmodeller.org>
2017-09-14 15:13:57 +0800

    add CLI API declare

--- mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel.CLI/CLI.vb
+++ mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel.CLI/CLI.vb
@@ -1,3 +1,31 @@
-﻿Module CLI
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.InteropService.SharedORM
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 
+<CLI> Module CLI
+
+    <ExportAPI("/Cbind")>
+    Public Function cbind(args As CommandLine) As Integer
+
+    End Function
+
+    <ExportAPI("/Rbind")>
+    Public Function rbind(args As CommandLine) As Integer
+
+    End Function
+
+    <ExportAPI("/push")>
+    Public Function pushTable(args As CommandLine) As Integer
+
+    End Function
+
+    <ExportAPI("/Create")>
+    Public Function newEmpty(args As CommandLine) As Integer
+
+    End Function
+
+    <ExportAPI("Extract")>
+    Public Function extract(args As CommandLine) As Integer
+
+    End Function
 End Module
