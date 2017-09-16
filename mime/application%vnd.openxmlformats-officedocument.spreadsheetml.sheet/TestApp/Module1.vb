85be7a50ac7de5e94ec5f846b63d43964107cf72
xieguigang <xie.guigang@gcmodeller.org>
2017-09-16 12:36:42 +0800

    update demo test

--- mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/TestApp/Module1.vb
+++
@@ -1,7 +0,0 @@
-﻿Module Module1
-
-    Sub Main()
-        Call MIME.Office.Excel.File.CreatePackage("G:\GCModeller\src\runtime\sciBASIC#\mime\application%vnd.openxmlformats-officedocument.spreadsheetml.sheet\test\zip", "G:\GCModeller\src\runtime\sciBASIC#\mime\application%vnd.openxmlformats-officedocument.spreadsheetml.sheet\test\zip.xlsx")
-    End Sub
-
-End Module
