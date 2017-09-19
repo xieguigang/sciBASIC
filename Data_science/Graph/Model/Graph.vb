84cd744ef7de53b6175f25e63b1d73d496f3bed3
xieguigang <xie.guigang@gcmodeller.org>
2017-09-19 21:20:28 +0800

    split source file

--- Data_science/Graph/Model/Graph.vb
+++ Data_science/Graph/Model/Graph.vb
@@ -27,23 +27,32 @@
 #End Region
 
 Imports System.Runtime.CompilerServices
-Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.ComponentModel
-Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
-Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
 
 ''' <summary>
 ''' A graph ``G = (V, E)`` consists of a set V of vertices and a set E edges, that is, unordered
 ''' pairs Of vertices. Unless explicitly stated otherwise, we assume that the graph Is simple,
 ''' that Is, it has no multiple edges And no self-loops.
 ''' </summary>
-Public Class Graph
+Public Class Graph : Implements IEnumerable(Of Edge)
 
 #Region "Let G=(V, E) be a simple graph"
     Dim edges As Dictionary(Of Edge)
     Dim vertices As Dictionary(Of Vertex)
     Dim buffer As HandledList(Of Vertex)
 #End Region
+
+    Public ReadOnly Property Size As (Vertex%, Edges%)
+        Get
+            Return (vertices.Count, edges.Count)
+        End Get
+    End Property
+
+    Public Function AddVertex(u As Vertex) As Graph
+        vertices += u
+        buffer.Add(u)
+        Return Me
+    End Function
 
     Public Function AddEdge(u As Vertex, v As Vertex) As Graph
         edges += New Edge With {
@@ -104,50 +113,14 @@ Public Class Graph
 
         Return Me
     End Function
-End Class
 
-''' <summary>
-''' 图之中的节点
-''' </summary>
-Public Class Vertex : Implements INamedValue
-    Implements IAddressOf
-
-    <XmlAttribute> Public Property Label As String Implements IKeyedEntity(Of String).Key
-    <XmlAttribute> Public Property ID As Integer Implements IAddress(Of Integer).Address
-
-    Public Overrides Function ToString() As String
-        Return $"({ID}) {Label}"
-    End Function
-End Class
-
-''' <summary>
-''' 节点之间的边
-''' </summary>
-Public Class Edge : Implements INamedValue
-
-    Public Property U As Vertex
-    Public Property V As Vertex
-
-    ''' <summary>
-    ''' ReadOnly unique-ID
-    ''' </summary>
-    ''' <returns></returns>
-    ''' 
-    Private Property Key As String Implements IKeyedEntity(Of String).Key
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Get
-            Return $"{U.ID}-{V.ID}"
-        End Get
-        Set(value As String)
-            ' DO Nothing
-        End Set
-    End Property
-
-    Public Overrides Function GetHashCode() As Integer
-        Return Key.GetHashCode
+    Public Iterator Function GetEnumerator() As IEnumerator(Of Edge) Implements IEnumerable(Of Edge).GetEnumerator
+        For Each edge As Edge In edges.Values
+            Yield edge
+        Next
     End Function
 
-    Public Overrides Function ToString() As String
-        Return $"({GetHashCode()}) {U} => {V}"
+    Private Iterator Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+        Yield GetEnumerator()
     End Function
 End Class
