adb0f930ed7d02fd69a058bf5d6e8e123cb69528
xieguigang <xie.guigang@gcmodeller.org>
2017-09-20 17:40:50 +0800

    tree to graph

---
+++ Data_science/Graph/Extensions.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
+
+Public Module Extensions
+
+    <Extension>
+    Public Function CreateGraph(Of T)(tree As Tree(Of T)) As Graph
+        Return New Graph().Add(tree)
+    End Function
+
+    <Extension>
+    Private Function Add(Of T)(g As Graph, tree As Tree(Of T)) As Graph
+        Call g.AddVertex(tree)
+
+        For Each child As Tree(Of T) In tree.Childs.SafeQuery
+            Call g.Add(child)
+            Call g.AddEdge(tree, child)
+        Next
+
+        Return g
+    End Function
+End Module
