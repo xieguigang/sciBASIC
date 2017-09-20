aeb6b895ef21ab07aee3c37e6055843cd0847120
xieguigang <xie.guigang@gcmodeller.org>
2017-09-20 15:08:04 +0800

    append binary tree

--- Data_science/Graph/Model/Tree.vb
+++ Data_science/Graph/Model/Tree.vb
@@ -1,7 +1,8 @@
-Public Class Tree: inherits Vertex
+Public Class Tree(Of T): inherits Vertex
 
-    Public Property Childs As Tree()
-	Public Property Parent As Tree
+    Public Property Childs As Tree(Of T)()
+	Public Property Parent As Tree(Of T)
+	Public Property Data As T
 	
 	Public ReadOnly Property QualifyName As String 
 Get
@@ -30,9 +31,9 @@ Return QualifyName
 End Function
 End Class
 
-Public Class BinaryTree : Inherits Tree
+Public Class BinaryTree(Of T As IComparable(Of T)) : Inherits Tree(Of T)
 
-Public Property Left As BinaryTree
+Public Property Left As BinaryTree(Of T)
 Get 
 Return Childs.ElementAtOrDefault(0)
 End Get
@@ -45,7 +46,7 @@ End If
 End Set
 End Property
 
-Public Property Right As BinaryTree
+Public Property Right As BinaryTree(of T)
 Get 
 Return Childs.ElementAtOrDefault(1)
 End Get
@@ -58,4 +59,61 @@ End If
 End Set
 End Property
 
+Const Duplicated$ = "Duplicated node was found!"
+
+        ''' <summary>
+        ''' Recursively locates an empty slot in the binary tree and inserts the node
+        ''' </summary>
+        ''' <param name="node"></param>
+        ''' <param name="tree"></param>
+        ''' <param name="[overrides]">
+        ''' 0不复写，函数自动处理
+        ''' &lt;0  LEFT
+        ''' >0 RIGHT
+        ''' </param>
+        Private Sub InternalAdd(node As BinaryTree(Of T), ByRef tree As BinaryTree(Of T), overrides%)
+            If tree Is Nothing Then
+                tree = node
+            Else
+                ' If we find a node with the same name then it's 
+                ' a duplicate and we can't continue
+                Dim comparison%
+
+                If [overrides] = 0 Then
+                    comparison = Node.Data.CompareTo(tree.Data)
+					
+                    If comparison = 0 Then
+                        Throw New Exception(Duplicated)
+                    End If
+                Else
+                    comparison = [overrides]
+                End If
+
+				' 在这里进行递归的比较查找，直到二叉树节点是一个空节点为止
+                If comparison < 0 Then
+                   Call InternalAdd(node, tree.Left, comparison)
+                Else
+                  Call InternalAdd(node, tree.Right, comparison)
+                End If
+            End If
+        End Sub
+
+		        ''' <summary>
+        ''' Add a symbol to the tree if it's a new one. Returns reference to the new
+        ''' node if a new node inserted, else returns null to indicate node already present.
+        ''' </summary>
+        ''' <param name="name">Name of node to add to tree</param>
+        ''' <param name="d">Value of node</param>
+        ''' <returns> Returns reference to the new node is the node was inserted.
+        ''' If a duplicate node (same name was located then returns null</returns>
+        Public Function Insert(name$, data As T) As BinaryTree(Of T)
+            Dim node As New BinaryTree(Of T) With { .Label = name, .Data = d, .ID = Count}
+            Try
+               Call InternalAdd(node, Me, 0)
+                Return node
+            Catch e As Exception
+                Dim ex = New Exception(node.ToString, e)
+                Return App.LogException(ex)
+            End Try
+        End Function
 End Class
