bcc049749d7e2458770260b89477b947df1c87a7
xieguigang <xie.guigang@gcmodeller.org>
2017-09-20 17:24:18 +0800

    split source file

--- Data_science/Graph/Analysis.vb
+++ Data_science/Graph/Analysis.vb
@@ -1,44 +1,46 @@
-﻿Public Module AnalysisExtensions
+﻿Imports System.Runtime.CompilerServices
 
-        ' Simple 'drawing' routines
-    <Extension>    Public Function Build(Of T)(tree As Tree(Of T)) As String
-            If tree Is Nothing Then
-                Return "()"
-            End If
+Public Module AnalysisExtensions
 
-          if tree.isleaf then 
- return tree.ID
-else 
-dim children = tree _
-.childs _
-.select(function(t) t.build) _
-.joinby(", ")
+    ' Simple 'drawing' routines
+    <Extension> Public Function Build(Of T)(tree As Tree(Of T)) As String
+        If tree Is Nothing Then
+            Return "()"
+        End If
 
-return $"{tree.ID}({children})" 
-		 end if 
-        End Function
-		
-		' summary this tree model its nodes as csv table
-		 <Extension> public iterator function Summary(Of T)(tree as tree(of T), Optional schema As Propertyinfo() = Nothing) As IEnumerable(Of NamedValue(Of Dictionary(of string ,string )))
-		 
-		 If schema.isnullorempty
-			schema = dataframework.schema(Of T)(index:= False, primitive:=True, access:=Readable)
-	End If
-		 
-		 yield tree.SummaryMe(schema)
-		 
-		 For Each c as Tree(Of T) In tree.Childs.SafeQuery
-		 yield c.Summary(schema)
-		 Next
-		end function 
-		
-		' 这个函数不会对childs进行递归
-		private function SummaryMe(Of T)(this As Tree(Of T),schema As Propertyinfo()) As namedValue(Of Dictionary(Of string ,String))
-		Dim name = this.label
-	dim values = schema.ToDictionary(function(key) key .name , function(read) safeCStr(read.getValue(this.data)))
-	values.Add("tree.ID", this.ID)
-	values.add("tree.Label", this.label)
-	
-	return new namedvalue(Of Dictionary(of string,string)) with { .name =  name , .value = values}
-		end function
+        If tree.isleaf Then
+            Return tree.ID
+        Else
+            Dim children = tree _
+            .childs _
+            .select(Function(t) t.build) _
+            .joinby(", ")
+
+            Return $"{tree.ID}({children})"
+        End If
+    End Function
+
+    ' summary this tree model its nodes as csv table
+    <Extension> Public Iterator Function Summary(Of T)(tree As tree(Of T), Optional schema As Propertyinfo() = Nothing) As IEnumerable(Of NamedValue(Of Dictionary(Of String, String)))
+
+        If schema.isnullorempty Then
+            schema = dataframework.schema(Of T)(index:=False, primitive:=True, access:=Readable)
+        End If
+
+        Yield tree.SummaryMe(schema)
+
+        For Each c As Tree(Of T) In tree.Childs.SafeQuery
+            Yield c.Summary(schema)
+        Next
+    End Function
+
+    ' 这个函数不会对childs进行递归
+    Private Function SummaryMe(Of T)(this As Tree(Of T), schema As Propertyinfo()) As namedValue(Of Dictionary(Of String, String))
+        Dim name = this.label
+        Dim values = schema.ToDictionary(Function(key) key.name, Function(read) safeCStr(read.getValue(this.data)))
+        values.Add("tree.ID", this.ID)
+        values.add("tree.Label", this.label)
+
+        Return New namedvalue(Of Dictionary(Of String, String)) With {.name = name, .value = values}
+    End Function
 End Module
--- Data_science/Graph/Model/Tree.vb
+++ Data_science/Graph/Model/Tree.vb
@@ -1,136 +1,48 @@
-Public Class Tree(Of T): inherits Vertex
+Public Class Tree(Of T) : Inherits Vertex
 
     Public Property Childs As Tree(Of T)()
-	Public Property Parent As Tree(Of T)
-	Public Property Data As T
-	
-	Public ReadOnly Property Count As Integer
-	Get 
-	If Childs.isNullorEmpty then 
-	return 1  ' 自己算一个节点，所以数量总是1的
-	else 
-	Dim n% = Childs.Length	
-	
-	for each node in childs
-		n += node .count ' 如果节点没有childs，则会返回1，因为他自身就是一个节点
-	next 
-	
-	return n
-	end if 
-	
-	End Get
-	End Property
-	
-	Public ReadOnly Property QualifyName As String 
-Get
-If Not Parent Is nothing then
-Return Parent.QualifyName & "." & Label
-Else 
-Return Label
-end if 
-End Get
-End Property	
+    Public Property Parent As Tree(Of T)
+    Public Property Data As T
 
-Public ReadOnly Property IsRoot As Boolean 
-Get 
-return Parent Is Nothing
-End Get
-End property
-
-Public ReadOnly Property isLeaf As Boolean 
-get
-Return Childs.IsNullOrEmpty
-end get 
-end property
-
-Public Overrides Function ToString As String 
-Return QualifyName
-End Function
-End Class
-
-Public Class BinaryTree(Of T As IComparable(Of T)) : Inherits Tree(Of T)
-
-Public Property Left As BinaryTree(Of T)
-Get 
-Return Childs.ElementAtOrDefault(0)
-End Get
-Set 
-If Childs.IsNullOrEmpty Then 
-Childs = {value, nothing}
-Else 
-Childs(0) = value
-End If
-End Set
-End Property
-
-Public Property Right As BinaryTree(of T)
-Get 
-Return Childs.ElementAtOrDefault(1)
-End Get
-Set 
-If Childs.IsNullOrEmpty OrElse Childs.Length < 2 Then 
-Childs = {nothing ,value}
-Else 
-Childs(1) = value
-End If
-End Set
-End Property
-
-Const Duplicated$ = "Duplicated node was found!"
-
-        ''' <summary>
-        ''' Recursively locates an empty slot in the binary tree and inserts the node
-        ''' </summary>
-        ''' <param name="node"></param>
-        ''' <param name="tree"></param>
-        ''' <param name="[overrides]">
-        ''' 0不复写，函数自动处理
-        ''' &lt;0  LEFT
-        ''' >0 RIGHT
-        ''' </param>
-        Private Sub InternalAdd(node As BinaryTree(Of T), ByRef tree As BinaryTree(Of T), overrides%)
-            If tree Is Nothing Then
-                tree = node
+    Public ReadOnly Property Count As Integer
+        Get
+            If Childs.IsNullOrEmpty Then
+                Return 1  ' 自己算一个节点，所以数量总是1的
             Else
-                ' If we find a node with the same name then it's 
-                ' a duplicate and we can't continue
-                Dim comparison%
+                Dim n% = Childs.Length
 
-                If [overrides] = 0 Then
-                    comparison = Node.Data.CompareTo(tree.Data)
-					
-                    If comparison = 0 Then
-                        Throw New Exception(Duplicated)
-                    End If
-                Else
-                    comparison = [overrides]
-                End If
+                For Each node In Childs
+                    n += node.Count ' 如果节点没有childs，则会返回1，因为他自身就是一个节点
+                Next
 
-				' 在这里进行递归的比较查找，直到二叉树节点是一个空节点为止
-                If comparison < 0 Then
-                   Call InternalAdd(node, tree.Left, comparison)
-                Else
-                  Call InternalAdd(node, tree.Right, comparison)
-                End If
+                Return n
             End If
-        End Sub
+        End Get
+    End Property
 
-		        ''' <summary>
-        ''' Add a symbol to the tree if it's a new one. Returns reference to the new
-        ''' node if a new node inserted, else returns null to indicate node already present.
-        ''' </summary>
-        ''' <param name="name">Name of node to add to tree</param>
-        ''' <param name="d">Value of node</param>
-        ''' <returns> Returns reference to the new node is the node was inserted.
-        ''' If a duplicate node (same name was located then returns null</returns>
-        Public Function Insert(name$, data As T) As BinaryTree(Of T)
-            Dim node As New BinaryTree(Of T) With { .Label = name, .Data = d, .ID = Count}
-            Try
-               Call InternalAdd(node, Me, 0)
-                Return node
-            Catch e As Exception
-                Dim ex = New Exception(node.ToString, e)
-                Return App.LogException(ex)
-            End Try
-        End Function
+    Public ReadOnly Property QualifyName As String
+        Get
+            If Not Parent Is Nothing Then
+                Return Parent.QualifyName & "." & Label
+            Else
+                Return Label
+            End If
+        End Get
+    End Property
+
+    Public ReadOnly Property IsRoot As Boolean
+        Get
+            Return Parent Is Nothing
+        End Get
+    End Property
+
+    Public ReadOnly Property IsLeaf As Boolean
+        Get
+            Return Childs.IsNullOrEmpty
+        End Get
+    End Property
+
+    Public Overrides Function ToString() As String
+        Return QualifyName
+    End Function
 End Class
