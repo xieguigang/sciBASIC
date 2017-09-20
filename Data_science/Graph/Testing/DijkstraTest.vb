4ee1d79048cd62f20222aecc83e0e7de4e93055e
xieguigang <xie.guigang@gcmodeller.org>
2017-09-20 21:29:59 +0800

    add weight in test

--- Data_science/Graph/Testing/DijkstraTest.vb
+++ Data_science/Graph/Testing/DijkstraTest.vb
@@ -9,17 +9,19 @@ Module DijkstraTest
             g.AddVertex(label:=i)
         Next
 
-        g.AddEdge(0, 1)
-        g.AddEdge(1, 2)
-        g.AddEdge(2, 3)
-        g.AddEdge(3, 4)
-        g.AddEdge(4, 5)
-        g.AddEdge(5, 2)
-        g.AddEdge(5, 3)
-        g.AddEdge(4, 8)
-        g.AddEdge(8, 9)
-        g.AddEdge(3, 7)
-        g.AddEdge(7, 9)
+        Dim weight As New Random
+
+        g.AddEdge(0, 1, weight.NextDouble)
+        g.AddEdge(1, 2, weight.NextDouble)
+        g.AddEdge(2, 3, weight.NextDouble)
+        g.AddEdge(3, 4, weight.NextDouble)
+        g.AddEdge(4, 5, weight.NextDouble)
+        g.AddEdge(5, 2, weight.NextDouble)
+        g.AddEdge(5, 3, weight.NextDouble)
+        g.AddEdge(4, 8, weight.NextDouble)
+        g.AddEdge(8, 9, weight.NextDouble)
+        g.AddEdge(3, 7, weight.NextDouble)
+        g.AddEdge(7, 9, weight.NextDouble)
 
         Dim Dijkstra As New Dijkstra.DijkstraRouteFind(g)
         Dim route = Dijkstra.CalculateMinCost(g.Vertex(0))
