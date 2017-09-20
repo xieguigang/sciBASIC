6595d6b21738c3c4b67c182177ccc8fbeccd8312
xieguigang <xie.guigang@gcmodeller.org>
2017-09-20 15:19:48 +0800

    tree to text

--- Data_science/Graph/Analysis.vb
+++ Data_science/Graph/Analysis.vb
@@ -1,4 +1,20 @@
 ﻿Public Module AnalysisExtensions
 
+        ' Simple 'drawing' routines
+    <Extension>    Public Function Build(tree As Tree(Of T)) As String
+            If tree Is Nothing Then
+                Return "()"
+            End If
 
+          if tree.isleaf then 
+ return tree.ID
+else 
+dim children = tree _
+.childs _
+.select(function(t) t.build) _
+.joinby(", ")
+
+return $"{tree.ID}({children})" 
+		 end if 
+        End Function
 End Module
