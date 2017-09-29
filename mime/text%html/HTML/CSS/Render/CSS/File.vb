80e54d1a83b2076f5588ee168dae4b6c3054ba5d
xieguigang <xie.guigang@gcmodeller.org>
2017-09-29 18:54:23 +0800

    required a new css file parser model?

---
+++ mime/text%html/HTML/CSS/Render/CSS/File.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+
+Namespace HTML.CSS.Render
+
+    Public Class CSSFile
+
+        Public Property Selectors As Selector()
+
+    End Class
+
+    Public Class Selector : Inherits [Property](Of String)
+        Implements INamedValue
+
+        Public Property Selector As String Implements IKeyedEntity(Of String).Key
+    End Class
+End Namespace
