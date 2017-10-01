18a183e332b28def81bfd3b62e78885363dab21e
xieguigang <xie.guigang@gcmodeller.org>
2017-10-01 23:56:42 +0800

    write cmd auto build script

---
+++ vs_solutions/installer/build.bat
@@ -0,0 +1,44 @@
+@echo off
+
+REM batch script for build sciBASIC# installer project
+REM output location is at ``./output`` directory
+REM redistribute the ``sciBASIC_installer.exe``
+
+REM installer packaged based on the MSBuild platform
+
+SET sciBASIC_sln="../Microsoft.VisualBasic.Architecture.Framework - ALL.sln"
+SET installer_sln="./vs_installer.sln"
+
+REM check the visual studio solution file exists or not?
+
+if exist %sciBASIC_sln% (
+    echo "%sciBASIC_sln% exist!"
+) else (
+    echo "Can not found sciBASIC# project!"	
+	pause
+	
+	exit -100
+)
+
+if exist %installer_sln% (
+    echo "%installer_sln% exist!"
+) else (
+    echo "Can not found sciBASIC# installer project!"	
+	pause
+	
+	exit -120
+)
+
+echo "[ALL] check success!"
+echo "Now run build for the sciBASIC# framework"
+
+REM cleanup the output workspace at first and then run build
+
+SET output=""
+
+RD /S /Q %output%
+MSBuild 
+
+
+
+
