b61b5f436165a07b75d8e1f73fa22fac951b9091
xieguigang <xie.guigang@gcmodeller.org>
2017-10-13 02:52:56 +0800

    delete source files

--- Microsoft.VisualBasic.Architecture.Framework/ApplicationServices/VBDev/Ngen/Registry.vb
+++ Microsoft.VisualBasic.Architecture.Framework/ApplicationServices/VBDev/Ngen/Registry.vb
@@ -1,36 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::4ba8235fd56964ebb23cfba2348edccf, ..\sciBASIC#\Microsoft.VisualBasic.Architecture.Framework\ApplicationServices\Tools\SoftwareToolkits\Ngen\Registry.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports Microsoft.Win32
 
 Namespace ApplicationServices
 
     Public Module RegistryUtils
+
+        ''' <summary>
+        ''' Register a .NET dll file as a COM component.(将某一个.NET语言所编写的DLL文件注册为COM组件)
+        ''' </summary>
+        ''' <param name="COM_Dll"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        <ExportAPI("RegAsm")>
+        Public Function RegisterCOMDll(<Parameter("COM.Dll", "The file path of the DLL file which will be registered as a COM component.")> COM_Dll As String) As Boolean
+            Dim Asm As Assembly = Assembly.LoadFile(COM_Dll)
+            Dim regAsm As New RegistrationServices
+            Dim bResult As Boolean = regAsm.RegisterAssembly(Asm, AssemblyRegistrationFlags.SetCodeBase)
+            Return bResult
+        End Function
 
         ''' <summary>
         ''' ### Registering Extensions of the .NET Framework
