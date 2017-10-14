1892e0191d574ee8d4e4ed9fb377044e54208e9d
xieguigang <xie.guigang@gcmodeller.org>
2017-10-14 23:25:03 +0800

    add zip helper

---
+++ vs_solutions/installer/zip/Program.vb
@@ -0,0 +1,36 @@
+﻿Imports System.IO.Compression
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+
+Module Program
+
+    Public Function Main() As Integer
+        Return GetType(Program).RunCLI(App.CommandLine)
+    End Function
+
+    <ExportAPI("/compress")>
+    <Usage("/compress [/directory] /out <out.zip> <directory/filelist>")>
+    Public Function Compress(args As CommandLine) As Integer
+        Dim isDirectory As Boolean = args.IsTrue("/directory")
+        Dim out$ = args <= "/out"
+
+        If isDirectory Then
+            Call GZip.DirectoryArchive(
+                args.Tokens(4),
+                saveZip:=out,
+                action:=ArchiveAction.Replace,
+                compression:=CompressionLevel.Fastest,
+                fileOverwrite:=Overwrite.Always)
+        Else
+            Call args.Tokens _
+                .Skip(3) _
+                .AddToArchive(
+                    out,
+                    action:=ArchiveAction.Replace,
+                    compression:=CompressionLevel.Fastest,
+                    fileOverwrite:=Overwrite.Always)
+        End If
+
+        Return 0
+    End Function
+End Module
