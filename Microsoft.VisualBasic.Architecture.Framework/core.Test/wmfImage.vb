c13b2f45bfeed2ac45b931116dd613f342b1f857
asuka <amethyst.asuka@gcmodeller.org>
2017-10-22 21:08:51 +0800

    split object

--- Microsoft.VisualBasic.Architecture.Framework/core.Test/wmfImage.vb
+++ Microsoft.VisualBasic.Architecture.Framework/core.Test/wmfImage.vb
@@ -7,7 +7,7 @@ Module wmfImage
 
         Using wmf As New Wmf(New Size(200, 200), "./test.wmf")
             Call wmf.Graphics.DrawString("Hello world!", New Font(FontFace.BookmanOldStyle, 16, FontStyle.Bold), Brushes.Red, New Point(20, 20))
-            Call wmf.Graphics.FillPie(Brushes.Green, New Rectangle(100, 100, 50, 50), 0, 96)
+            Call wmf.Graphics.FillPie(Brushes.Green, New Rectangle(100, 100, 50, 50), 0, 196)
         End Using
     End Sub
 End Module
