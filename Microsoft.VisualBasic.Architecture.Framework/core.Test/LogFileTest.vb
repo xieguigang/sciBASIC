2076b95a7c674eaf667b0b05624fc3cebbb4f482
asuka <amethyst.asuka@gcmodeller.org>
2017-10-28 22:13:13 +0800

    test on log file write?

---
+++ Microsoft.VisualBasic.Architecture.Framework/core.Test/LogFileTest.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
+
+Module LogFileTest
+
+    Sub Main()
+        WriteTes()
+        PrintTest()
+    End Sub
+
+    Const path$ = "./test.log"
+
+    Sub WriteTes()
+        Using log As New LogFile(path)
+
+        End Using
+    End Sub
+
+    Sub PrintTest()
+
+    End Sub
+End Module
