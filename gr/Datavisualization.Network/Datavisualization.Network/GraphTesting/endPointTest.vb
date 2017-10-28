c274739110666bb37f4e2c96e1aa72af32bcd534
xieguigang <xie.guigang@gcmodeller.org>
2017-10-28 22:34:43 +0800

    fix api referecne error

---
+++ gr/Datavisualization.Network/Datavisualization.Network/GraphTesting/endPointTest.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.Data.visualize.Network.FindPath
+
+Module endPointTest
+
+    Sub Main()
+        Dim g = ExampleNetwork()
+        Dim endPoints = g.EndPoints
+
+        Pause()
+    End Sub
+End Module
