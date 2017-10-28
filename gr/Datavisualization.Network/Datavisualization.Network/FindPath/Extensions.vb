a7da6513f107f57ce8f1004c8ac1e78c8f7b1d60
xieguigang <xie.guigang@gcmodeller.org>
2017-10-28 20:38:07 +0800

    fix reference errors

--- gr/Datavisualization.Network/Datavisualization.Network/FindPath/Extensions.vb
+++ gr/Datavisualization.Network/Datavisualization.Network/FindPath/Extensions.vb
@@ -1,33 +1,34 @@
 ﻿#Region "Microsoft.VisualBasic::2f1b03bfcf321fe1a04ffae2be32b805, ..\sciBASIC#\gr\Datavisualization.Network\Datavisualization.Network\FindPath\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 
 Namespace FindPath
 
@@ -39,7 +40,7 @@ Namespace FindPath
         ''' <param name="network"></param>
         ''' <returns></returns>
         <Extension>
-        Public Function EndPoints(network As NetworkTables) As (input As Node(), output As Node())
+        Public Function EndPoints(network As NetworkTables) As (input As FileStream.Node(), output As FileStream.Node())
 
         End Function
 
@@ -48,7 +49,8 @@ Namespace FindPath
         ''' </summary>
         ''' <param name="network"></param>
         ''' <returns></returns>
-        Public Iterator Function SubClusters(network As NetworkTables) As IEnumerable(Of NetworkTables)
+        <Extension>
+        Public Iterator Function IteratesSubNetworks(network As NetworkGraph) As IEnumerable(Of NetworkGraph)
 
         End Function
     End Module
