a06ec522dafe0442798db4c4956bc56a3a1c3bd2
xieguigang <xie.guigang@gcmodeller.org>
2017-10-30 13:18:28 +0800

    get collection intersaction

--- Microsoft.VisualBasic.Architecture.Framework/ComponentModel/DataStructures/Set/SetAPI.vb
+++ Microsoft.VisualBasic.Architecture.Framework/ComponentModel/DataStructures/Set/SetAPI.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::3876b6eed33a0a99378bfedad808b1b7, ..\sciBASIC#\Microsoft.VisualBasic.Architecture.Framework\ComponentModel\DataStructures\Set\SetAPI.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -41,24 +41,33 @@ Public Module SetAPI
     ''' <param name="s2">Any set.</param>
     ''' <returns>A new <see cref="[Set]">Set</see> object that contains the members
     ''' that were common to both of the input sets.</returns>
-    ''' 
     <Extension>
-    Public Function Intersection(Of T)(s1 As IEnumerable(Of T), s2 As IEnumerable(Of T), __uid As GetUid(Of T)) As T()
-        Dim tag As String = NameOf(s1) '  由于需要交换标签，所以在这里不能使用并行化
-        Dim uids = (From x As T In s1.AsParallel Select uid = __uid(x), st = tag, x).AsList
-        tag = NameOf(s2)
-        uids += (From x As T In s2.AsParallel Select uid = __uid(x), st = tag, x).ToArray
-        Dim Groups = (From x In uids Select x Group x By x.uid Into Group)  ' 按照uid字符串进行分组
-        Dim GetIntersects = (From Group In Groups.AsParallel
-                             Let source = Group.Group.ToArray  ' 对每一个分组数据，根据标签的数量来了解是否为交集的一部分，当为交集元素的时候，标签的数量是两个，即同时存在于两个集合之众
-                             Let tags As String() = source.ToArray(Function(x) x.st).Distinct.ToArray
-                             Where tags.Length > 1
-                             Select source).ToArray
-        tag = NameOf(s1)  ' 总是挑选出s1的数据
-        Dim result As T() = (From x In GetIntersects.AsParallel
-                             Select (From o In x
-                                     Where String.Equals(tag, o.st)
-                                     Select o).ToArray(Function(o) o.x)).ToVector
+    Public Function Intersection(Of T)(s1 As IEnumerable(Of T), s2 As IEnumerable(Of T), getUID As GetUid(Of T)) As T()
+        Dim tags = (From x As T In s1 Let uid As String = getUID(x) Select uid, tag = NameOf(s1), x).AsList +
+            From x As T
+            In s2
+            Let uid As String = getUID(x)
+            Select uid,
+                tag = NameOf(s2),
+                x
+        Dim intersectGroups = From g
+                              In tags.GroupBy(Function(x) x.uid) ' 按照uid字符串进行分组
+                              Let source = g.ToArray
+                              Let taglist As String() = source _
+                                   .Select(Function(x) x.tag) _
+                                   .Distinct _
+                                   .ToArray
+                              Where taglist.Length > 1 ' 对每一个分组数据，根据标签的数量来了解是否为交集的一部分，当为交集元素的时候，标签的数量是两个，即同时存在于两个集合之众
+                              Select source
+        ' 总是挑选出s1的数据
+        Dim result = LinqAPI.Exec(Of T) _
+ _
+            () <= From x
+                  In intersectGroups
+                  Let s1Group = x.Where(Function(o) o.tag = NameOf(s1)).First
+                  Let value As T = s1Group.x
+                  Select value
+
         Return result
     End Function
 
