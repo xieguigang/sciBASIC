565f0108c162e87193dccdbdf16da417507c0561
xieguigang <xie.guigang@gcmodeller.org>
2017-11-05 19:26:46 +0800

    the bug exists

---
+++ Microsoft.VisualBasic.Architecture.Framework/core.Test/reflectionTest.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+
+Public Module reflectionTest
+
+    Sub Main()
+        Dim dll = "G:\GCModeller\src\runtime\sciBASIC#\Microsoft.VisualBasic.Architecture.Framework\core.Test\bin\Debug\Microsoft.VisualBasic.Architecture.Framework_v3.0_22.0.76.201__8da45dcd8060cc9a.dll"
+
+        Dim entry = RunDllEntryPoint.GetDllMethod(Assembly.LoadFile(dll), "test")
+    End Sub
+End Module
