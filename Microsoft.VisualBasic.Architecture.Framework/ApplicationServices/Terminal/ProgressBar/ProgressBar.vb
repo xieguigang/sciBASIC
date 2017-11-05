5b72b29cc6372d4e59e57403e52d5e80bdaccc21
xieguigang <xie.guigang@gcmodeller.org>
2017-11-05 19:06:54 +0800

    found a reflection bug, not sure why?

--- Microsoft.VisualBasic.Architecture.Framework/ApplicationServices/Terminal/ProgressBar/ProgressBar.vb
+++ Microsoft.VisualBasic.Architecture.Framework/ApplicationServices/Terminal/ProgressBar/ProgressBar.vb
@@ -32,12 +32,38 @@ Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace Terminal
 
-    Public Structure Theme
+    ''' <summary>
+    ''' The <see cref="ConsoleColor"/> theme for the <see cref="ProgressBar"/>
+    ''' </summary>
+    Public Structure ColorTheme
 
-        Dim BackgroundColor As ConsoleColor
-        Dim ProgressBarColor As ConsoleColor
-        Dim ProgressMsgColor As ConsoleColor
-        Dim MessageDetailColor As ConsoleColor
+        ' [ERROR 2017/11/5 下午 0704:37] <Print>:System.Exception: Print 
+        '  ---> System.Reflection.TargetInvocationException: 调用的目标发生了异常。 
+        '  ---> System.Exception: [
+        '           "未能从程序集“Microsoft.VisualBasic.Architecture.Framework_v3.0_22.0.76.201__8da45dcd8060cc9a, Version=3.0.32.34335, Culture=neutral, PublicKeyToken=null”中加载类型“Microsoft.VisualBasic.Terminal.ColorTheme”。",
+        '           "未能从程序集“Microsoft.VisualBasic.Architecture.Framework_v3.0_22.0.76.201__8da45dcd8060cc9a, Version=3.0.32.34335, Culture=neutral, PublicKeyToken=null”中加载类型“Microsoft.VisualBasic.Terminal.ColorTheme”。"
+        '       ] 
+        '  ---> System.Reflection.ReflectionTypeLoadException: 无法加载一个或多个请求的类型。有关更多信息，请检索 LoaderExceptions 属性。
+
+        ' 在 System.Reflection.RuntimeModule.GetTypes(RuntimeModule Module)
+        ' 在 System.Reflection.Assembly.GetTypes()
+        ' 在 Microsoft.VisualBasic.CommandLine.Reflection.RunDllEntryPoint.GetTypes(Assembly assm)
+        ' --- 内部异常堆栈跟踪的结尾 ---
+        ' 在 Microsoft.VisualBasic.CommandLine.Reflection.RunDllEntryPoint.GetTypes(Assembly assm)
+        ' 在 Microsoft.VisualBasic.CommandLine.Reflection.RunDllEntryPoint.GetDllMethod(Assembly assembly, String entryPoint)
+        ' 在 SMRUCC.WebCloud.httpd.CLI.RunDll(CommandLine args)
+        ' --- 内部异常堆栈跟踪的结尾 ---
+        ' 在 System.RuntimeMethodHandle.InvokeMethod(Object target, Object[] arguments, Signature sig, Boolean constructor)
+        ' 在 System.Reflection.RuntimeMethodInfo.UnsafeInvokeInternal(Object obj, Object[] parameters, Object[] arguments)
+        ' 在 System.Reflection.RuntimeMethodInfo.Invoke(Object obj, BindingFlags invokeAttr, Binder binder, Object[] parameters, CultureInfo culture)
+        ' 在 System.Reflection.MethodBase.Invoke(Object obj, Object[] parameters)
+        ' 在 Microsoft.VisualBasic.CommandLine.Reflection.EntryPoints.APIEntryPoint.__directInvoke(Object[] callParameters, Object target, Boolean Throw)
+        ' --- 内部异常堆栈跟踪的结尾 ---
+
+        Public Property BackgroundColor As ConsoleColor
+        Public Property ProgressBarColor As ConsoleColor
+        Public Property ProgressMsgColor As ConsoleColor
+        Public Property MessageDetailColor As ConsoleColor
 
         Public ReadOnly Property IsEmpty As Boolean
             Get
@@ -48,15 +74,15 @@ Namespace Terminal
             End Get
         End Property
 
-        Public Shared ReadOnly Property DefaultTheme As New DefaultValue(Of Theme) With {
+        Public Shared ReadOnly Property DefaultTheme As New DefaultValue(Of ColorTheme) With {
             .LazyValue = Function() [Default](),
             .assert = Function(t)
-                          Return DirectCast(t, Theme).IsEmpty
+                          Return DirectCast(t, ColorTheme).IsEmpty
                       End Function
         }
 
-        Public Shared Function [Default]() As Theme
-            Return New Theme With {
+        Public Shared Function [Default]() As ColorTheme
+            Return New ColorTheme With {
                 .BackgroundColor = ConsoleColor.Cyan,
                 .MessageDetailColor = ConsoleColor.White,
                 .ProgressBarColor = ConsoleColor.Yellow,
@@ -79,7 +105,7 @@ Namespace Terminal
 
         Dim current%
         Dim y%
-        Dim theme As Theme
+        Dim theme As ColorTheme
 
         ''' <summary>
         ''' 
@@ -87,14 +113,14 @@ Namespace Terminal
         ''' <param name="title"></param>
         ''' <param name="Y">The row position number of the progress bar.</param>
         ''' <param name="CLS">Clear the console screen?</param>
-        Sub New(title$, Y%, Optional CLS As Boolean = False, Optional theme As Theme = Nothing)
+        Sub New(title$, Y%, Optional CLS As Boolean = False, Optional theme As ColorTheme = Nothing)
             If CLS AndAlso App.IsConsoleApp Then
                 Call Console.Clear()
             End If
 
             Call Console.WriteLine(title)
 
-            Me.theme = theme Or Theme.DefaultTheme
+            Me.theme = theme Or ColorTheme.DefaultTheme
             Me.y = Y
 
             AddHandler TerminalEvents.Resize, AddressOf __resize
