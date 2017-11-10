1ab26d54db998a1d89520591423a7fc2fc8583e7
xieguigang <xie.guigang@gcmodeller.org>
2017-11-10 21:46:27 +0800

    fix object reference

--- Data/Trinity/Test/Module1.vb
+++ Data/Trinity/Test/Module1.vb
@@ -1,34 +1,33 @@
 ﻿#Region "Microsoft.VisualBasic::c111fb5d5effc09cd916b70827e89f81, ..\sciBASIC#\Data\Trinity\Test\Module1.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
+Imports Microsoft.VisualBasic.Data.Graph.Analysis.PageRank
 Imports Microsoft.VisualBasic.Data.NLP
-Imports Microsoft.VisualBasic.Data.visualize.Network
-Imports Microsoft.VisualBasic.Data.visualize.Network.Analysis.PageRank
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 Module Module1
@@ -54,14 +53,14 @@ available here"
 
         Call result.GetJson(True).EchoLine
 
-        Dim net = g.GetNetwork
+        'Dim net = g.GetNetwork
 
-        For Each node In net.Nodes
-            node.Properties.Add("PageRank", result(node.ID))
-        Next
+        'For Each node In net.Nodes
+        '    node.Properties.Add("PageRank", result(node.ID))
+        'Next
 
-        Call net.Save("G:\GCModeller\src\runtime\sciBASIC#\Data\TextRank\")
+        'Call net.Save("G:\GCModeller\src\runtime\sciBASIC#\Data\TextRank\")
 
-        Pause()
+        'Pause()
     End Sub
 End Module
