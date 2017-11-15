28c13f98e63e6537da364b486e8046c457f1e769
xieguigang <xie.guigang@gcmodeller.org>
2017-11-15 17:53:44 +0800

    add link

---
+++ Data_science/Graph/API/PageRank/WeightedPageRankGraph.vb
@@ -0,0 +1,12 @@
+﻿
+Namespace Analysis.PageRank
+
+    ''' <summary>
+    ''' PageRank on undirected and weighted graph
+    ''' 
+    ''' > https://github.com/jia1546/PageRank
+    ''' </summary>
+    Public Class WeightedPageRankGraph
+
+    End Class
+End Namespace
