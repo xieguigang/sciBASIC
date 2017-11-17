baa2f4ecebe79d3403655f603566ebddede4b7a0
xieguigang <xie.guigang@gcmodeller.org>
2017-11-17 22:46:16 +0800

    fix for sample model inherits

--- Microsoft.VisualBasic.Architecture.Framework/Extensions/Math/StatisticsMathExtensions/DataSample.vb
+++ Microsoft.VisualBasic.Architecture.Framework/Extensions/Math/StatisticsMathExtensions/DataSample.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::79f5f596a931c56c0e7bb4ddc5b88e36, ..\sciBASIC#\Microsoft.VisualBasic.Architecture.Framework\ComponentModel\Algorithm\DataSample.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -39,15 +39,19 @@ Namespace Math.Statistics
     Public Class DataSample(Of T As {IComparable, Structure})
         Implements IEnumerable(Of T)
 
-        Public ReadOnly Property Min As T
+        Protected Friend ranges As IRanges(Of T)
+        Protected Friend buffer As List(Of T)
+        Protected Friend means As Double
+
+        Public Overridable ReadOnly Property Min As T
             Get
-                Return Ranges.Min
+                Return ranges.Min
             End Get
         End Property
 
-        Public ReadOnly Property Max As T
+        Public Overridable ReadOnly Property Max As T
             Get
-                Return Ranges.Max
+                Return ranges.Max
             End Get
         End Property
 
@@ -56,15 +60,16 @@ Namespace Math.Statistics
         ''' </summary>
         ''' <returns></returns>
         Public ReadOnly Property Mean As Double
-
-        Protected Friend ranges As IRanges(Of T)
-        Protected Friend buffer As List(Of T)
+            Get
+                Return means
+            End Get
+        End Property
 
         ''' <summary>
         ''' The sample size
         ''' </summary>
         ''' <returns></returns>
-        Public ReadOnly Property Length As Integer
+        Public ReadOnly Property SampleSize As Integer
             <MethodImpl(MethodImplOptions.AggressiveInlining)>
             Get
                 Return buffer.Count
@@ -76,11 +81,6 @@ Namespace Math.Statistics
 
         Sub New(sample As IEnumerable(Of T))
             buffer = New List(Of T)(sample)
-        End Sub
-
-        Friend Sub New(sample As IEnumerable(Of T), mean#)
-            Me.New(sample)
-            Me.Mean = mean
         End Sub
 
         Public Iterator Function Split(partSize As Integer) As IEnumerable(Of T())
@@ -110,7 +110,8 @@ Namespace Math.Statistics
         <Extension>
         Public Function IntegerSample(data As IEnumerable(Of Integer)) As DataSample(Of Integer)
             Dim buf As Integer() = data.ToArray
-            Return New DataSample(Of Integer)(buf, buf.Average) With {
+            Return New DataSample(Of Integer)(buf) With {
+                .means = buf.Average,
                 .ranges = New IntRange(buf.Min, buf.Max)
             }
         End Function
@@ -118,7 +119,8 @@ Namespace Math.Statistics
         <Extension>
         Public Function DoubleSample(data As IEnumerable(Of Double)) As DataSample(Of Double)
             Dim buf As Double() = data.ToArray
-            Return New DataSample(Of Double)(buf, buf.Average) With {
+            Return New DataSample(Of Double)(buf) With {
+                .means = buf.Average,
                 .ranges = New DoubleRange(buf.Min, buf.Max)
             }
         End Function
