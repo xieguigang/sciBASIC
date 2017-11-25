1cd8db1c7fc259dd24d2f9b54bb42f726da8b259
xieguigang <xie.guigang@gcmodeller.org>
2017-11-25 11:02:43 +0800

    add markdown 2 html extensions

---
+++ mime/text%html/Extensions.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.MIME.Markup.MarkDown
+
+Public Module Extensions
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    <Extension>
+    Public Function Markdown2HTML(markdown$, Optional opt As MarkdownOptions = Nothing) As String
+        Return New MarkdownHTML(opt Or MarkdownOptions.DefaultOption).Transform(text:=markdown)
+    End Function
+End Module
