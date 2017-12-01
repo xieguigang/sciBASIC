5edccaba61c20aee61121d88cb7b7e0e33527593
xieguigang <xie.guigang@gcmodeller.org>
2017-12-01 16:42:21 +0800

    rename namespace

---
+++ www/Microsoft.VisualBasic.Webservices.Bing/Academic/ProfileResult.vb
@@ -0,0 +1,14 @@
+﻿Namespace Academic
+
+    ''' <summary>
+    ''' Example as:
+    ''' 
+    ''' https://cn.bing.com/academic/profile?id=24ca0003c2b5935f1335003ca712b889&encoded=0&v=paper_preview&mkt=zh-cn
+    ''' </summary>
+    Public Module ProfileResult
+
+        Public Function GetProfile(url As String) As ArticleProfile
+
+        End Function
+    End Module
+End Namespace
