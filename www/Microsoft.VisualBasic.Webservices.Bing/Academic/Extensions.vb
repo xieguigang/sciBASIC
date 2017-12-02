b73cf6d40a3abec2700c33b0d95db4bd5f537d01
xieguigang <xie.guigang@gcmodeller.org>
2017-12-02 13:16:37 +0800

    config for file save path

--- www/Microsoft.VisualBasic.Webservices.Bing/Academic/Extensions.vb
+++ www/Microsoft.VisualBasic.Webservices.Bing/Academic/Extensions.vb
@@ -6,13 +6,21 @@ Namespace Academic
 
     Public Module Extensions
 
-        Public Sub Build_KB(term$, out$, Optional pages% = 10, Optional sleepInterval% = 5000)
+        Public Sub Build_KB(term$, out$, Optional pages% = 10, Optional flat As Boolean = True, Optional sleepInterval% = 5000)
             For Each entry As (refer$, list As NamedValue(Of String)()) In AcademicSearch.Query(term, pages)
                 For Each url As NamedValue(Of String) In entry.list
                     Try
                         With url.GetDetails(refer:=entry.refer)
+                            Dim path$
+
+                            If flat Then
+                                path = $"{out}/{ .GetProfileID}.xml"
+                            Else
+                                path = $"{out}/{Mid(.GetProfileID, 3)}/{ .GetProfileID}.xml"
+                            End If
+
                             Call .GetXml _
-                                 .SaveTo($"{out}/{ .GetProfileID}.xml", TextEncodings.UTF8WithoutBOM)
+                                 .SaveTo(path, TextEncodings.UTF8WithoutBOM)
                         End With
                     Catch ex As Exception
                         ex = New Exception(url.Value, ex)
