e4768d0dcf22ffa13eddbba19f08a43cc979b778
xieguigang <xie.guigang@gcmodeller.org>
2017-12-08 23:53:15 +0800

    add named tuple

---
+++ Microsoft.VisualBasic.Architecture.Framework/ComponentModel/ValuePair/NamedTuple.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+
+Namespace ComponentModel
+
+    Public Class NamedTuple(Of T) : Implements INamedValue
+
+        Public Property Name As String Implements IKeyedEntity(Of String).Key
+        Public Property Item1 As T
+        Public Property Item2 As T
+
+        Sub New()
+        End Sub
+
+        Sub New(item1 As T, item2 As T)
+            Me.Item1 = item1
+            Me.Item2 = item2
+        End Sub
+
+        Sub New(name$, item1 As T, item2 As T)
+            Me.New(item1, item2)
+            Me.Name = name
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return Name
+        End Function
+    End Class
+End Namespace
