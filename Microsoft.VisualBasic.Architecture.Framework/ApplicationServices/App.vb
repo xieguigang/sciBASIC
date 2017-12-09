2dd8bb42e070f95e3714cc9b5151f30f550e8d34
xieguigang <xie.guigang@gcmodeller.org>
2017-12-10 04:33:24 +0800

    Dim pt As PlatformID = Platform
            ' 枚举值在.NET和Mono之间可能会不一样??
            If pt.ToString = NameOf(PlatformID.Unix) Then
                Return False
            ElseIf pt.ToString = NameOf(PlatformID.MacOSX) Then
                Return False
            End If
    
            Return pt = PlatformID.Win32NT OrElse
                pt = PlatformID.Win32S OrElse
                pt = PlatformID.Win32Windows OrElse
                pt = PlatformID.WinCE OrElse
                pt = PlatformID.Xbox

--- Microsoft.VisualBasic.Architecture.Framework/ApplicationServices/App.vb
+++ Microsoft.VisualBasic.Architecture.Framework/ApplicationServices/App.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::725cf3eb95b0e1cb4833f76547c0fd87, ..\sciBASIC#\Microsoft.VisualBasic.Architecture.Framework\ApplicationServices\App.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -761,6 +761,9 @@ Public Module App
     ''' </summary>
     ''' <returns></returns>
     Private Function __isMicrosoftPlatform() As Boolean
+#If UNIX Then
+        Return False
+#Else
         Dim pt As PlatformID = Platform
 
         ' 枚举值在.NET和Mono之间可能会不一样??
@@ -775,6 +778,7 @@ Public Module App
             pt = PlatformID.Win32Windows OrElse
             pt = PlatformID.WinCE OrElse
             pt = PlatformID.Xbox
+#End If
     End Function
 
     ''' <summary>
