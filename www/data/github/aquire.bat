5843d23e5913163cf329cb4e936ebc2e195aa31d
xieguigang <xie.guigang@gcmodeller.org>
2017-12-10 18:34:41 +0800

    config projects

--- www/data/github/aquire.bat
+++ www/data/github/aquire.bat
@@ -1,1 +1,2 @@
 "../../githubAPI/github-vcard/bin/x64/Release/github-vcard.exe" /relationships /user xieguigang /out ./github-relationships
+"../../githubAPI/github-vcard/bin/x64/Release/github-vcard.exe" /write /user xieguigang
