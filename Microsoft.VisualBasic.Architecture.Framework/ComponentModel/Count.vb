5e9d8cf2e275ec1ae2ad6976b45622442e046acc
xieguigang <xie.guigang@gcmodeller.org>
2017-12-13 20:29:43 +0800

    add KO coverage counts

--- Microsoft.VisualBasic.Architecture.Framework/ComponentModel/Count.vb
+++ Microsoft.VisualBasic.Architecture.Framework/ComponentModel/Count.vb
@@ -6,7 +6,15 @@ Namespace ComponentModel
     ''' <summary>
     ''' The object counter
     ''' </summary>
-    Public Class Counter : Inherits Value(Of Integer)
+    Public Class Counter : Inherits int
+
+        Sub New()
+            MyBase.New(0)
+        End Sub
+
+        Sub New(hits As Integer)
+            Call MyBase.New(x:=hits)
+        End Sub
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Sub Hit()
---
+++ Microsoft.VisualBasic.Architecture.Framework/ComponentModel/DataSource/Abstract.vb
@@ -0,0 +1,26 @@
+﻿Namespace ComponentModel.DataSourceModel
+
+    Public Enum PropertyAccess As Byte
+        NotSure = 0
+        Readable = 2
+        Writeable = 4
+        ReadWrite = Readable And Writeable
+    End Enum
+
+    ''' <summary>
+    ''' 在数据框数据映射操作之中是否忽略掉这个属性或者方法？
+    ''' </summary>
+    <AttributeUsage(AttributeTargets.Property Or AttributeTargets.Method, AllowMultiple:=False, Inherited:=True)>
+    Public Class DataIgnoredAttribute : Inherits Attribute
+    End Class
+
+    ''' <summary>
+    ''' Class field reader
+    ''' </summary>
+    ''' <typeparam name="T"></typeparam>
+    ''' <typeparam name="Tout"></typeparam>
+    ''' <param name="[in]"></param>
+    ''' <returns></returns>
+    Public Delegate Function Projector(Of T, Tout)([in] As T) As Tout
+
+End Namespace
