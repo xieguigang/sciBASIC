d0dd8b776a7481ce329d9227ca14f44ba3081f85
xieguigang <xie.guigang@gcmodeller.org>
2017-12-18 10:08:13 +0800

    perl

---
+++ Microsoft.VisualBasic.Architecture.Framework/CommandLine/InteropService/SharedORM/Perl.vb
@@ -0,0 +1,19 @@
+﻿Namespace CommandLine.InteropService.SharedORM
+
+    ''' <summary>
+    ''' 为了构建GCModeller的脚本自动化而构建的源代码生成器，生成``perl module(*.pm)``
+    ''' </summary>
+    Public Class Perl : Inherits CodeGenerator
+
+        Dim namespace$
+
+        Public Sub New(CLI As Type, namespace$)
+            MyBase.New(CLI)
+            Me.namespace = [namespace]
+        End Sub
+
+        Public Overrides Function GetSourceCode() As String
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
