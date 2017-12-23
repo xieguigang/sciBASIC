60eaa47d3d2e58840507eeb36e2ac5d5e7e0bef6
xieguigang <xie.guigang@gcmodeller.org>
2017-12-24 03:21:23 +0800

    throw exception details

--- Microsoft.VisualBasic.Core/CommandLine/InteropService/SharedORM/API.vb
+++ Microsoft.VisualBasic.Core/CommandLine/InteropService/SharedORM/API.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::63a7aa425e88f888a9ef228616030b27, ..\sciBASIC#\Microsoft.VisualBasic.Architecture.Framework\CommandLine\InteropService\SharedORM\API.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -32,7 +32,7 @@ Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Emit.Marshal
 Imports Microsoft.VisualBasic.Language
-Imports Microsoft.VisualBasic.Scripting.Runtime
+Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.VisualBasic.Text
 
 Namespace CommandLine.InteropService.SharedORM
@@ -63,7 +63,22 @@ Namespace CommandLine.InteropService.SharedORM
 
             ' 逻辑符号只存在于optional之中
             Dim booleans$() = Nothing
-            Dim params As NamedValue(Of String)() = arguments.BuildArguments(optionals, booleans)
+            Dim params As NamedValue(Of String)()
+
+            Try
+                params = arguments.BuildArguments(optionals, booleans)
+            Catch ex As Exception
+                Dim msg$ = $"Invalid commandline usage({usage})!" & vbCrLf & vbCrLf
+                Dim details = New Dictionary(Of String, String()) From {
+                    {NameOf(arguments), arguments},
+                    {NameOf(optionals), optionals},
+                    {NameOf(booleans), booleans}
+                }.GetJson
+
+                msg = msg & details
+
+                Throw New ArgumentException(msg, ex)
+            End Try
 
             Dim model As New CommandLine With {
                 .Name = name,
