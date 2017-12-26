17812a529ab3b36319731009dad47e2e1e8e9587
xieguigang <xie.guigang@gcmodeller.org>
2017-12-26 09:49:01 +0800

    fix code merge conflicts

--- www/Microsoft.VisualBasic.Webservices.Bing/test/DEBUG_MAIN.vb
+++ www/Microsoft.VisualBasic.Webservices.Bing/test/DEBUG_MAIN.vb
@@ -1,36 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::248d799ca69c963837018fbbfc39a535, ..\sciBASIC#\www\Microsoft.VisualBasic.Webservices.Bing\test\DEBUG_MAIN.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports Microsoft.VisualBasic.Webservices
+Imports Microsoft.VisualBasic.Webservices.Bing
 
 Module DEBUG_MAIN
 
     Sub Main()
+
+        Dim trans = Translation.GetTranslation("aminoglycoside antibiotic")
+
+
+        Pause()
+
         Dim n = Bing.SearchEngineProvider.Search("D-fructuronate: C6H9O7 kegg")
     End Sub
 End Module
