e23021e2156ff487dcf22365d854c56d29f5cdee
xieguigang <xie.guigang@gcmodeller.org>
2017-12-26 20:48:48 +0800

    create sub new

--- Microsoft.VisualBasic.Core/CommandLine/InteropService/SharedORM/Perl.vb
+++ Microsoft.VisualBasic.Core/CommandLine/InteropService/SharedORM/Perl.vb
@@ -1,30 +1,32 @@
 ﻿#Region "Microsoft.VisualBasic::fd2ea82722674bdbf322d04c9a104a8a, ..\sciBASIC#\Microsoft.VisualBasic.Core\CommandLine\InteropService\SharedORM\Perl.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
+
+Imports System.Text
 
 Namespace CommandLine.InteropService.SharedORM
 
@@ -94,7 +96,23 @@ Namespace CommandLine.InteropService.SharedORM
         End Sub
 
         Public Overrides Function GetSourceCode() As String
-            Throw New NotImplementedException()
+            Dim perl As New StringBuilder
+
+            Call perl.AppendLine("#!/usr/bin/perl")
+            Call perl.AppendLine()
+
+            Call perl.AppendLine($"package {exe};")
+
+            Call perl.AppendLine("sub new {")
+            Call perl.AppendLine("my $class = shift;")
+            Call perl.AppendLine("my $self = {")
+            Call perl.AppendLine("    path = shift")
+            Call perl.AppendLine("};")
+            Call perl.AppendLine("bless $self, $class;")
+            Call perl.AppendLine("return $self;")
+            Call perl.AppendLine("}")
+
+            Return perl.ToString
         End Function
     End Class
 End Namespace
