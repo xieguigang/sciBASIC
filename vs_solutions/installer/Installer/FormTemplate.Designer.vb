633ab97193de41c68e8f24dd22c4eef4a24d7d01
xieguigang <xie.guigang@gcmodeller.org>
2018-01-05 14:20:37 +0800

    add forest plot description

--- vs_solutions/installer/Installer/FormTemplate.Designer.vb
+++ vs_solutions/installer/Installer/FormTemplate.Designer.vb
@@ -25,13 +25,11 @@ Partial Class FormTemplate
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormTemplate))
         Me.ButtonNext = New System.Windows.Forms.Button()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.LabelTitle = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'ButtonNext
@@ -44,7 +42,7 @@ Partial Class FormTemplate
         Me.ButtonNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.ButtonNext.Font = New System.Drawing.Font("Segoe UI", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ButtonNext.ForeColor = System.Drawing.Color.White
-        Me.ButtonNext.Location = New System.Drawing.Point(504, 328)
+        Me.ButtonNext.Location = New System.Drawing.Point(646, 476)
         Me.ButtonNext.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
         Me.ButtonNext.MaximumSize = New System.Drawing.Size(120, 32)
         Me.ButtonNext.MinimumSize = New System.Drawing.Size(120, 32)
@@ -58,7 +56,7 @@ Partial Class FormTemplate
         '
         Me.PictureBox1.BackgroundImage = Global.Installer.My.Resources.Resources.Microsoft_Visual_Studio_logo_256x256
         Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-        Me.PictureBox1.Location = New System.Drawing.Point(504, 12)
+        Me.PictureBox1.Location = New System.Drawing.Point(646, 12)
         Me.PictureBox1.MaximumSize = New System.Drawing.Size(120, 120)
         Me.PictureBox1.MinimumSize = New System.Drawing.Size(120, 120)
         Me.PictureBox1.Name = "PictureBox1"
@@ -66,56 +64,44 @@ Partial Class FormTemplate
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
         '
-        'Panel1
-        '
-        Me.Panel1.BackColor = System.Drawing.Color.WhiteSmoke
-        Me.Panel1.Controls.Add(Me.Label3)
-        Me.Panel1.Controls.Add(Me.Label2)
-        Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
-        Me.Panel1.Location = New System.Drawing.Point(0, 0)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(149, 374)
-        Me.Panel1.TabIndex = 2
-        '
         'Label3
         '
         Me.Label3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.ForeColor = System.Drawing.Color.Gray
-        Me.Label3.Location = New System.Drawing.Point(12, 140)
+        Me.Label3.Location = New System.Drawing.Point(12, 185)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(155, 28)
         Me.Label3.TabIndex = 2
-        Me.Label3.Text = "3. Finish"
+        Me.Label3.Text = "Finish"
         Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'Label2
         '
         Me.Label2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.Color.Gray
-        Me.Label2.Location = New System.Drawing.Point(12, 105)
+        Me.Label2.Location = New System.Drawing.Point(12, 150)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(155, 28)
         Me.Label2.TabIndex = 1
-        Me.Label2.Text = "2. Installing"
+        Me.Label2.Text = "Installing"
         Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'Label1
         '
         Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.Gray
-        Me.Label1.Location = New System.Drawing.Point(12, 70)
+        Me.Label1.Location = New System.Drawing.Point(12, 115)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(155, 28)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "1. Welcome"
+        Me.Label1.Text = "Welcome"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'LabelTitle
         '
         Me.LabelTitle.AutoSize = True
         Me.LabelTitle.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelTitle.Location = New System.Drawing.Point(168, 32)
+        Me.LabelTitle.Location = New System.Drawing.Point(12, 31)
         Me.LabelTitle.Name = "LabelTitle"
         Me.LabelTitle.Size = New System.Drawing.Size(84, 32)
         Me.LabelTitle.TabIndex = 3
@@ -126,9 +112,11 @@ Partial Class FormTemplate
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 19.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(636, 374)
+        Me.ClientSize = New System.Drawing.Size(778, 522)
         Me.Controls.Add(Me.LabelTitle)
-        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.ButtonNext)
         Me.Font = New System.Drawing.Font("Segoe UI", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
@@ -136,13 +124,10 @@ Partial Class FormTemplate
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
         Me.MaximizeBox = False
-        Me.MaximumSize = New System.Drawing.Size(652, 413)
-        Me.MinimumSize = New System.Drawing.Size(652, 413)
         Me.Name = "FormTemplate"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
         Me.Text = "Form1"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -150,7 +135,6 @@ Partial Class FormTemplate
 
     Protected Friend WithEvents ButtonNext As Button
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Panel1 As Panel
     Friend WithEvents LabelTitle As Label
     Friend WithEvents Label1 As Label
     Friend WithEvents Label3 As Label
