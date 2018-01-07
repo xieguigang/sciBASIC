afe979a9a6cf6e22d927cd695018e44fe52e75ea
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-07 17:03:40 +0800

    config for excel cli test

---
+++ mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel.CLI/test.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic.MIME.Office
+
+Module test
+
+    Sub Main()
+        Call test()
+    End Sub
+
+    Sub test()
+        Call New Excel.XML.docProps.app With {
+            .HeadingPairs = New Excel.XML.docProps.Vectors With {
+                .vector = New Excel.XML.docProps.vector With {
+                    .variants = {New Excel.XML.docProps.variant With {.i4 = 4444, .lpstr = "1234"}, New Excel.XML.docProps.variant With {.i4 = 4444, .lpstr = "ffff"}},
+                    .baseType = "fffffffffff",
+                    .size = "4453"
+            }
+            },
+            .TitlesOfParts = New Excel.XML.docProps.Vectors With {.vector = New Excel.XML.docProps.vector With {.baseType = "test", .variants = {New Excel.XML.docProps.variant With {.i4 = "dddd", .lpstr = "1"}}}}
+        }.GetXml.SaveTo("D:\rrrr.xml")
+
+        Pause()
+    End Sub
+End Module
