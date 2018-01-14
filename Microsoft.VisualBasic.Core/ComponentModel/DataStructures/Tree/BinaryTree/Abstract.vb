c0fb56c864d89899662d665d580153e0a4bc3ada
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-14 15:17:36 +0800

    add node abstract model

--- Microsoft.VisualBasic.Core/ComponentModel/DataStructures/Tree/BinaryTree/Abstract.vb
+++ Microsoft.VisualBasic.Core/ComponentModel/DataStructures/Tree/BinaryTree/Abstract.vb
@@ -1,4 +1,7 @@
-﻿Namespace ComponentModel.DataStructures.BinaryTree
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+Imports Microsoft.VisualBasic.Language
+
+Namespace ComponentModel.DataStructures.BinaryTree
 
     ''' <summary>
     ''' The generic object comparer model:
@@ -13,4 +16,15 @@
     ''' <returns></returns>
     Public Delegate Function CompareOf(Of T)(a As T, b As T) As Integer
 
+    Public MustInherit Class TreeMap(Of K, V)
+        Implements IKeyedEntity(Of K)
+        Implements Value(Of V).IValueOf
+        Implements IComparable(Of K)
+
+        Public Property Key As K Implements IKeyedEntity(Of K).Key
+        Public Property value As V Implements Value(Of V).IValueOf.Value
+
+        Public MustOverride Function CompareTo(other As K) As Integer Implements IComparable(Of K).CompareTo
+
+    End Class
 End Namespace
