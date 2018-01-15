dd079d2517d728118c336899ffeb05d508024a3b
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-15 21:08:12 +0800

    write index

--- Data/BinaryData/BinaryData/Repository/BinarySearchIndex.vb
+++ Data/BinaryData/BinaryData/Repository/BinarySearchIndex.vb
@@ -1,30 +1,32 @@
 ﻿#Region "Microsoft.VisualBasic::01e4e81fafca0d309f11d40e3680191d, ..\sciBASIC#\Data\BinaryData\BinaryData\Repository\BinarySearchIndex.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
+
+Imports System.IO
 
 ''' <summary>
 ''' Using the binary tree as the search index.
@@ -49,5 +51,15 @@ Public Class Index
     ''' </summary>
     ''' <returns></returns>
     Public Property Offset As Long
-End Class
+    Public Property left As Long
+    Public Property right As Long
 
+    Public Function Write(out As BinaryDataWriter) As Long
+        Call out.Write(Key, BinaryStringFormat.DwordLengthPrefix)
+        Call out.Write(Offset)
+        Call out.Write(left)
+        Call out.Write(right)
+
+        Return out.Position
+    End Function
+End Class
