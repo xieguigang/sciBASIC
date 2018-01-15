892781074c5e765a2cfc6742ab7967c46715f0ca
xieguigang <xie.guigang@gcmodeller.org>
2018-01-15 17:28:59 +0800

    add plot test

---
+++ Data_science/Mathematica/Plot/test/SampleVisualizeTest.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.Data.ChartPlots.Statistics
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
+
+Module SampleVisualizeTest
+
+    Sub Main()
+        Dim norm As Vector = rand(100)
+
+        Call norm.NormalDistributionPlot.AsGDIImage.SaveAs("./nd.png")
+    End Sub
+End Module
