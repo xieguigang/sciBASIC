ceb56a8cab511fcd7b442673ab1821ccf8bbb279
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-18 21:34:45 +0800

    RegressionPlot

---
+++ Data_science/Mathematica/Plot/Plots-statistics/RegressionPlot.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.Bootstrapping
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Microsoft.VisualBasic.Scripting.Runtime
+
+Public Module RegressionPlot
+
+    <Extension>
+    Public Function Plot(fit As FittedResult,
+                         Optional size$ = "2000,1800",
+                         Optional bg$ = "white",
+                         Optional margin$ = g.DefaultPadding) As GraphicsData
+        Dim plotInternal =
+            Sub(ByRef g As IGraphics, region As GraphicsRegion)
+
+            End Sub
+
+        Return g.GraphicsPlots(
+            size.SizeParser,
+            margin,
+            bg,
+            plotInternal)
+    End Function
+End Module
