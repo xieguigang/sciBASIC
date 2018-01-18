03f1bdfef1e5a435589ea3ba2e65c53fed7d857d
xieguigang <xie.guigang@gcmodeller.org>
2018-01-18 11:40:02 +0800

    split source file

---
+++ mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel/Model/Directory/_rels.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.MIME.Office.Excel.XML._rels
+
+Public Class _rels : Inherits Directory
+
+    Sub New(ROOT$)
+        Call MyBase.New(ROOT)
+    End Sub
+
+    Public Property rels As rels
+
+    Protected Overrides Sub _loadContents()
+        rels = (Folder & "/.rels").LoadXml(Of rels)
+    End Sub
+
+    Protected Overrides Function _name() As String
+        Return NameOf(Excel._rels)
+    End Function
+End Class
---
+++ mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel/Model/Directory/docProps.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic.MIME.Office.Excel.XML.docProps
+
+Public Class docProps : Inherits Directory
+
+    Public Property core As core
+    Public Property app As XML.docProps.app
+    Public Property custom As custom
+
+    Sub New(ROOT$)
+        Call MyBase.New(ROOT)
+    End Sub
+
+    ''' <summary>
+    ''' 有些文件可能是会不存在的，所以在这里就不抛出错误了，直接返回Nothing
+    ''' </summary>
+    Protected Overrides Sub _loadContents()
+        core = (Folder & "/core.xml").LoadXml(Of core)(ThrowEx:=False)
+        custom = (Folder & "/custom.xml").LoadXml(Of custom)(ThrowEx:=False)
+        app = (Folder & "/app.xml").LoadXml(Of XML.docProps.app)(ThrowEx:=False)
+    End Sub
+
+    Protected Overrides Function _name() As String
+        Return NameOf(docProps)
+    End Function
+End Class
