4fdc85bf2d76d928cb4f05c8012ef6ef774c5a0b
xieguigang <xie.guigang@gcmodeller.org>
2018-01-18 10:15:29 +0800

    improvements on the debug view

--- Microsoft.VisualBasic.Core/Language/Linq/Vectorization/Vector.vb
+++ Microsoft.VisualBasic.Core/Language/Linq/Vectorization/Vector.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::fabde4994c111ad37ce2952426f687c4, ..\sciBASIC#\Microsoft.VisualBasic.Core\Language\Linq\Vectorization\Vector.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -236,7 +236,7 @@ Namespace Language.Vectorization
             End Set
         End Property
 
-        Public Iterator Function Subset(booleans As IEnumerable(Of Boolean)) As IEnumerable
+        Public Iterator Function Subset(booleans As IEnumerable(Of Boolean)) As IEnumerable(Of T)
             For Each index In booleans.SeqIterator
                 If index.value = True Then
                     Yield buffer(index.i)
@@ -291,6 +291,10 @@ Namespace Language.Vectorization
             buffer = data.ToArray
         End Sub
 #End Region
+
+        Public Overrides Function ToString() As String
+            Return $"{buffer.Length} @ {GetType(T).FullName}"
+        End Function
 
         Public Iterator Function GetEnumerator() As IEnumerator(Of T) Implements IEnumerable(Of T).GetEnumerator
             For Each x In buffer
