1d5eb038ea94fbf383cd1da4e64dacb1bcc9d805
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-19 22:15:29 +0800

    add 3d model

--- vs_solutions/logo/Program.vb
+++ vs_solutions/logo/Program.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Drawing
 Imports Microsoft.VisualBasic.Imaging
-Imports Microsoft.VisualBasic.Imaging.BitmapImage
+Imports Microsoft.VisualBasic.Imaging.Drawing3D
+Imports Microsoft.VisualBasic.Imaging.Drawing3D.Models.Isometric.Shapes
 
 ''' <summary>
 ''' sciBASIC framework logo generator. A demo for the sciBASIC graphics artist system.
@@ -11,6 +12,11 @@ Module Program
 
     Sub Main()
         Using g As Graphics2D = New Size(500, 300).CreateGDIDevice(filled:=Color.White)
+
+            Dim isometricView As New IsometricEngine
+
+            isometricView.Add(New Prism(New Point3D(0, 0, 0), 1, 1, 1), Color.FromArgb(33, 150, 243))
+            isometricView.Draw(g)
 
             Call g.DrawString("sci", New Font(FontFace.SegoeUI, 24), Brushes.CadetBlue, New PointF(200, 200))
             Call g.DrawString("BASIC#", New Font(FontFace.SegoeUI, 36), Brushes.DeepSkyBlue, New PointF(300, 100))
