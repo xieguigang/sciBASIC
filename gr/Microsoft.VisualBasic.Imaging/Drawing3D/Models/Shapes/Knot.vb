e24b4ab1cd69519f9246a7256a07c2476be02ca0
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-20 17:58:27 +0800

    add logo drawer

--- gr/Microsoft.VisualBasic.Imaging/Drawing3D/Models/Shapes/Knot.vb
+++ gr/Microsoft.VisualBasic.Imaging/Drawing3D/Models/Shapes/Knot.vb
@@ -30,13 +30,18 @@ Namespace Drawing3D.Models.Isometric.Shapes
 
     Public Class Knot : Inherits Shape3D
 
-        Public Sub New(origin As Point3D)
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="origin">The shape model location</param>
+        ''' <param name="scale#">The scale size</param>
+        Public Sub New(origin As Point3D, Optional scale# = 1 / 5)
             Push((New Prism(Math3D.ORIGIN, 5, 1, 1)).paths)
             Push((New Prism(New Point3D(4, 1, 0), 1, 4, 1)).paths)
             Push((New Prism(New Point3D(4, 4, -2), 1, 1, 3)).paths)
             Push(New Path3D(New Point3D() {New Point3D(0, 0, 2), New Point3D(0, 0, 1), New Point3D(1, 0, 1), New Point3D(1, 0, 2)}))
             Push(New Path3D(New Point3D() {New Point3D(0, 0, 2), New Point3D(0, 1, 2), New Point3D(0, 1, 1), New Point3D(0, 0, 1)}))
-            ScalePath3Ds(Math3D.ORIGIN, 1.0 / 5.0)
+            ScalePath3Ds(Math3D.ORIGIN, scale)
             TranslatePath3Ds(-0.1, 0.15, 0.4)
             TranslatePath3Ds(origin.X, origin.Y, origin.Z)
         End Sub
--- vs_solutions/logo/Program.vb
+++ vs_solutions/logo/Program.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Drawing
 Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.BitmapImage
 Imports Microsoft.VisualBasic.Imaging.Drawing3D
 Imports Microsoft.VisualBasic.Imaging.Drawing3D.Models.Isometric.Shapes
 
@@ -10,24 +11,44 @@ Module Program
 
     Const save$ = "./logo.png"
 
-    Sub Main()
-        Using g As Graphics2D = New Size(500, 300).CreateGDIDevice(filled:=Color.White)
+    ReadOnly BLUE As Color = Color.FromArgb(50, 60, 160)
+    ReadOnly GREEN As Color = Color.FromArgb(50, 160, 60)
+    ReadOnly RED As Color = Color.FromArgb(160, 60, 50)
+    ReadOnly TEAL As Color = Color.FromArgb(0, 180, 180)
+    ReadOnly YELLOW As Color = Color.FromArgb(180, 180, 0)
+    ReadOnly LIGHT_GREEN As Color = Color.FromArgb(40, 180, 40)
+    ReadOnly PURPLE As Color = Color.FromArgb(180, 0, 180)
 
+    Sub Main()
+        Dim logo As Bitmap, fontName$ = FontFace.Verdana
+        Dim color1 As New SolidBrush(Color.FromArgb(0, 65, 102))
+        Dim color2 As New SolidBrush(Color.FromArgb(0, 172, 221))
+
+        Using g As Graphics2D = New Size(900, 800).CreateGDIDevice(filled:=Color.Transparent)
             Dim isometricView As New IsometricEngine
 
-            isometricView.Add(New Prism(New Point3D(0, 0, 0), 1, 1, 1), Color.FromArgb(33, 150, 243))
+            isometricView.Add(New Knot(New Point3D(1, 1, 1), scale:=1), GREEN)
             isometricView.Draw(g)
 
-            Call g.DrawString("sci", New Font(FontFace.SegoeUI, 24), Brushes.CadetBlue, New PointF(200, 200))
-            Call g.DrawString("BASIC#", New Font(FontFace.SegoeUI, 36), Brushes.DeepSkyBlue, New PointF(300, 100))
-            Call g.DrawString("http://sciBASIC.NET", New Font(FontFace.SegoeUI, 24), Brushes.Blue, New PointF(250, 200))
-
-            Call g.ImageResource.SaveAs(path:=save, format:=ImageFormats.Png)
+            logo = g.ImageResource
         End Using
 
-        'Call save _
-        '    .LoadImage _
-        '    .ColorReplace(Color.White, Color.Transparent) _
-        '    .SaveAs(save)
+        logo = logo.CorpBlank(blankColor:=Color.Transparent)
+
+        Using g As Graphics2D = New Size(1800, 500).CreateGDIDevice(filled:=Color.Transparent)
+
+            Call g.DrawImageUnscaled(logo, New Point(50, 50))
+
+            Call g.DrawString("sci", New Font(fontName, 120), color1, New PointF(400, 45))
+            Call g.DrawString("BASIC#", New Font(fontName, 200), color2, New PointF(575, 10))
+            Call g.DrawString("http://sciBASIC.NET", New Font(FontFace.SegoeUI, 48), color1, New PointF(630, 300))
+
+            logo = g.ImageResource
+        End Using
+
+        Call logo _
+            .CorpBlank(blankColor:=Color.Transparent, margin:=30) _
+            .SaveAs(save)
+
     End Sub
 End Module
