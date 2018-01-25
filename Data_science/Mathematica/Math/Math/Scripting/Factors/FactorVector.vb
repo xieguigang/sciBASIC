7685e2c40f51ff603175a18883cc31dc0a875cee
xieguigang <xie.guigang@gcmodeller.org>
2018-01-25 15:42:19 +0800

    split source file

--- Data_science/Mathematica/Math/Math/Scripting/Factors/FactorVector.vb
+++ Data_science/Mathematica/Math/Math/Scripting/Factors/FactorVector.vb
@@ -27,8 +27,6 @@
 #End Region
 
 Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic.ComponentModel.Collection
-Imports Microsoft.VisualBasic.Math.LinearAlgebra
 Imports Microsoft.VisualBasic.Math.SyntaxAPI.Vectors
 Imports Microsoft.VisualBasic.Serialization.JSON
 
@@ -92,55 +90,5 @@ Namespace Scripting
         Public Overrides Function ToString() As String
             Return index.Keys.ToArray.GetJson
         End Function
-    End Class
-
-    Public Class NamedVector : Inherits FactorVector(Of Double)
-
-        Public Sub Add(factor$, value#)
-            If buffer Is Nothing Then
-                buffer = {}
-                index = New Dictionary(Of String, Integer)
-            End If
-
-            Call index.Add(factor, buffer.Length)
-            Call buffer.Add(value)
-        End Sub
-
-        Public Overloads Function GetJson(format As String) As String
-            Dim table = AsTable() _
-                .ToDictionary(Function(key, v) key,
-                              Function(key, v)
-                                  Return v.ToString(format)
-                              End Function)
-            Return table.GetJson
-        End Function
-
-        Public Shared Widening Operator CType(table As Dictionary(Of String, Double)) As NamedVector
-            Dim index As Index(Of String) = table.Keys.Indexing
-            Dim vector As Vector = index _
-                .Objects _
-                .Select(Function(key) table(key)) _
-                .AsVector
-
-            Return New NamedVector With {
-                .index = index,
-                .buffer = vector
-            }
-        End Operator
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Shared Narrowing Operator CType(vector As NamedVector) As Dictionary(Of String, Double)
-            Return vector.AsTable
-        End Operator
-
-        Public Shared Operator *(A As NamedVector, B As NamedVector) As NamedVector
-            Dim names$() = A.Keys.AsSet Or B.Keys
-            Dim result = A.Vector(names).AsVector * B.Vector(names).AsVector
-
-            Return New NamedVector With {
-                .buffer = result,
-                .index = names.Indexing
-            }
-        End Operator
     End Class
 End Namespace
--- Data_science/Mathematica/Math/Math/Scripting/Factors/NamedVector.vb
+++ Data_science/Mathematica/Math/Math/Scripting/Factors/NamedVector.vb
@@ -1,79 +1,57 @@
-﻿#Region "Microsoft.VisualBasic::792a39df9b7cc0cdd8043f1972d44e88, ..\sciBASIC#\Data_science\Mathematica\Math\Math\Scripting\Factors\NamedVector.vb"
-
-' Author:
-' 
-'       asuka (amethyst.asuka@gcmodeller.org)
-'       xieguigang (xie.guigang@live.com)
-'       xie (genetics@smrucc.org)
-' 
-' Copyright (c) 2018 GPL3 Licensed
-' 
-' 
-' GNU GENERAL PUBLIC LICENSE (GPL3)
-' 
-' This program is free software: you can redistribute it and/or modify
-' it under the terms of the GNU General Public License as published by
-' the Free Software Foundation, either version 3 of the License, or
-' (at your option) any later version.
-' 
-' This program is distributed in the hope that it will be useful,
-' but WITHOUT ANY WARRANTY; without even the implied warranty of
-' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-' GNU General Public License for more details.
-' 
-' You should have received a copy of the GNU General Public License
-' along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-#End Region
-
-Imports System.Runtime.CompilerServices
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace Scripting
 
-    ''' <summary>
-    ''' Factory for <see cref="Dictionary(Of String, Double)"/> to <see cref="Vector"/>
-    ''' </summary>
-    Public Class NamedVectorFactory
+    Public Class NamedVector : Inherits FactorVector(Of Double)
 
-        Public ReadOnly Property Keys As String()
+        Public Sub Add(factor$, value#)
+            If buffer Is Nothing Then
+                buffer = {}
+                index = New Dictionary(Of String, Integer)
+            End If
 
-        ReadOnly factors As Factor(Of String)()
-
-        Sub New(factors As IEnumerable(Of String))
-            Me.Keys = factors.ToArray
-            Me.factors = FactorExtensions.factors(Keys)
+            Call index.Add(factor, buffer.Length)
+            Call buffer.Add(value)
         End Sub
 
+        Public Overloads Function GetJson(format As String) As String
+            Dim table = AsTable() _
+                .ToDictionary(Function(key, v) key,
+                              Function(key, v)
+                                  Return v.ToString(format)
+                              End Function)
+            Return table.GetJson
+        End Function
+
+        Public Shared Widening Operator CType(table As Dictionary(Of String, Double)) As NamedVector
+            Dim index As Index(Of String) = table.Keys.Indexing
+            Dim vector As Vector = index _
+                .Objects _
+                .Select(Function(key) table(key)) _
+                .AsVector
+
+            Return New NamedVector With {
+                .index = index,
+                .buffer = vector
+            }
+        End Operator
+
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Function EmptyVector() As Vector
-            Return New Vector(factors.Length - 1)
-        End Function
+        Public Shared Narrowing Operator CType(vector As NamedVector) As Dictionary(Of String, Double)
+            Return vector.AsTable
+        End Operator
 
-        Public Function AsVector(data As Dictionary(Of String, Double)) As Vector
-            Dim vector#() = New Double(factors.Length - 1) {}
+        Public Shared Operator *(A As NamedVector, B As NamedVector) As NamedVector
+            Dim names$() = A.Keys.AsSet Or B.Keys
+            Dim result = A.Vector(names).AsVector * B.Vector(names).AsVector
 
-            For Each factor As Factor(Of String) In factors
-                vector(factor.Value) = data(factor)
-            Next
-
-            Return vector.AsVector
-        End Function
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Function Translate(vector As Vector) As Dictionary(Of String, Double)
-            Return factors.ToDictionary(
-                Function(factor) factor.FactorValue,
-                Function(i) vector(CInt(i.Value)))
-        End Function
-
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overrides Function ToString() As String
-            Return factors _
-                .Select(Function(factor) factor.FactorValue) _
-                .ToArray _
-                .GetJson
-        End Function
+            Return New NamedVector With {
+                .buffer = result,
+                .index = names.Indexing
+            }
+        End Operator
     End Class
 End Namespace
