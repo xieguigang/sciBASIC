ad9723df457e6cfd2c8cbbccff59ef48b06fbb2e
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-01-27 20:33:47 +0800

    add namespaceDoc

---
+++ Microsoft.VisualBasic.Core/Language/NamespaceDoc.vb
@@ -0,0 +1,36 @@
+﻿#Region "Microsoft.VisualBasic::020de3ba4fa5c55ac0ba48a7ccb7da97, ..\sciBASIC#\Microsoft.VisualBasic.Core\Language\Class.vb"
+
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Namespace Language
+
+    ''' <summary>
+    ''' The VisualBasic.NET language addons
+    ''' </summary>
+    Friend Class NamespaceDoc
+    End Class
+End Namespace
