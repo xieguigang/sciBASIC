b3b4bd70ebaf15734f0e253526852a19590cbbf0
xieguigang <xie.guigang@gcmodeller.org>
2018-01-28 19:16:50 +0800

    fix namespace reference

--- mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel/Model/Xmlns.vb
+++ mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel/Model/Xmlns.vb
@@ -1,55 +1,57 @@
 ﻿#Region "Microsoft.VisualBasic::3bfa0c35a87496311d07d27d838c457d, ..\sciBASIC#\mime\application%vnd.openxmlformats-officedocument.spreadsheetml.sheet\Excel\Model\Xmlns.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
-Public Module Xmlns
+Namespace Model
 
-    Public Const cp$ = "http://schemas.openxmlformats.org/package/2006/metadata/core-properties"
-    Public Const dc$ = "http://purl.org/dc/elements/1.1/"
-    Public Const dcterms$ = "http://purl.org/dc/terms/"
-    Public Const dcmitype$ = "http://purl.org/dc/dcmitype/"
-    Public Const xsi$ = "http://www.w3.org/2001/XMLSchema-instance"
-    Public Const vt$ = "http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes"
+    Public Module Xmlns
 
-    Public Const r$ = "http://schemas.openxmlformats.org/officeDocument/2006/relationships"
-    Public Const mc$ = "http://schemas.openxmlformats.org/markup-compatibility/2006"
-    Public Const x15$ = "http://schemas.microsoft.com/office/spreadsheetml/2010/11/main"
-    Public Const xr2$ = "http://schemas.microsoft.com/office/spreadsheetml/2015/revision2"
-    Public Const x15ac$ = "http://schemas.microsoft.com/office/spreadsheetml/2010/11/ac"
+        Public Const cp$ = "http://schemas.openxmlformats.org/package/2006/metadata/core-properties"
+        Public Const dc$ = "http://purl.org/dc/elements/1.1/"
+        Public Const dcterms$ = "http://purl.org/dc/terms/"
+        Public Const dcmitype$ = "http://purl.org/dc/dcmitype/"
+        Public Const xsi$ = "http://www.w3.org/2001/XMLSchema-instance"
+        Public Const vt$ = "http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes"
 
-    Public Const x14$ = "http://schemas.microsoft.com/office/spreadsheetml/2009/9/main"
-    Public Const x14ac$ = "http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac"
-    Public Const xr$ = "http://schemas.microsoft.com/office/spreadsheetml/2014/revision"
-    Public Const xr3$ = "http://schemas.microsoft.com/office/spreadsheetml/2016/revision3"
+        Public Const r$ = "http://schemas.openxmlformats.org/officeDocument/2006/relationships"
+        Public Const mc$ = "http://schemas.openxmlformats.org/markup-compatibility/2006"
+        Public Const x15$ = "http://schemas.microsoft.com/office/spreadsheetml/2010/11/main"
+        Public Const xr2$ = "http://schemas.microsoft.com/office/spreadsheetml/2015/revision2"
+        Public Const x15ac$ = "http://schemas.microsoft.com/office/spreadsheetml/2010/11/ac"
 
-    Public Const x16r2$ = "http://schemas.microsoft.com/office/spreadsheetml/2015/02/main"
+        Public Const x14$ = "http://schemas.microsoft.com/office/spreadsheetml/2009/9/main"
+        Public Const x14ac$ = "http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac"
+        Public Const xr$ = "http://schemas.microsoft.com/office/spreadsheetml/2014/revision"
+        Public Const xr3$ = "http://schemas.microsoft.com/office/spreadsheetml/2016/revision3"
 
-    Public Const a$ = "http://schemas.openxmlformats.org/drawingml/2006/main"
+        Public Const x16r2$ = "http://schemas.microsoft.com/office/spreadsheetml/2015/02/main"
 
-    Public Const worksheet$ = "application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"
+        Public Const a$ = "http://schemas.openxmlformats.org/drawingml/2006/main"
 
-End Module
+        Public Const worksheet$ = "application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"
+    End Module
+End Namespace
