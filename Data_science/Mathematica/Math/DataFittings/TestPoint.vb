ad600db4fde4f2f5d9170abd4b141b3e4d5c6406
xieguigang <xie.guigang@gcmodeller.org>
2018-02-05 10:18:28 +0800

    fix for the xml namespace

--- Data_science/Mathematica/Math/DataFittings/TestPoint.vb
+++ Data_science/Mathematica/Math/DataFittings/TestPoint.vb
@@ -1,40 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::b698d41d6af38eb61e5c978aa142fb1f, ..\sciBASIC#\Data_science\Mathematica\Math\DataFittings\TestPoint.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports System.Drawing
 Imports System.Runtime.CompilerServices
+Imports System.Xml.Serialization
 
+<XmlType("point", [Namespace]:="http://scibasic.net/math/Bootstrapping")>
 Public Structure TestPoint
 
-    Public Property X As Double
-    Public Property Y As Double
-    Public Property Yfit As Double
+    <XmlAttribute("x")> Public Property X As Double
+    <XmlAttribute("y")> Public Property Y As Double
+    <XmlAttribute("fx")> Public Property Yfit As Double
 
+    <XmlIgnore>
     Public ReadOnly Property Err As Double
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Get
