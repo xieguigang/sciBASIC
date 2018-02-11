dce8a152c84e20d809fb8c683ac8d8af1a70fc03
xieguigang <xie.guigang@gcmodeller.org>
2018-02-11 17:23:09 +0800

    improvements on the xml data models

--- mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel/IO/xl/worksheets/sheet.xml.vb
+++ mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel/IO/xl/worksheets/sheet.xml.vb
@@ -1,28 +1,28 @@
 ﻿#Region "Microsoft.VisualBasic::ec2108df552351c67b8dd950ce22a8a8, ..\sciBASIC#\mime\application%vnd.openxmlformats-officedocument.spreadsheetml.sheet\Excel\IO\xl\worksheets\sheet.xml.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
@@ -108,14 +108,25 @@ Namespace XML.xl.worksheets
     End Class
 
     Public Class sheetView
+        <XmlAttribute> Public Property zoomScale As String
+        <XmlAttribute> Public Property zoomScaleNormal As String
         <XmlAttribute> Public Property tabSelected As String
         <XmlAttribute> Public Property workbookViewId As String
         Public Property selection As selection
+        Public Property pane As pane
+    End Class
+
+    Public Class pane
+        <XmlAttribute> Public Property ySplit As String
+        <XmlAttribute> Public Property topLeftCell As String
+        <XmlAttribute> Public Property activePane As String
+        <XmlAttribute> Public Property state As String
     End Class
 
     Public Class selection
         <XmlAttribute> Public Property activeCell As String
         <XmlAttribute> Public Property sqref As String
+        <XmlAttribute> Public Property pane As String
     End Class
 
     Public Structure dimension
@@ -140,6 +151,7 @@ Namespace XML.xl.worksheets
     Public Structure row
         <XmlAttribute> Public Property r As String
         <XmlAttribute> Public Property spans As String
+        <XmlAttribute> Public Property s As String
 
         <XmlAttribute(NameOf(dyDescent), [Namespace]:=OpenXML.x14ac)>
         Public Property dyDescent As String
