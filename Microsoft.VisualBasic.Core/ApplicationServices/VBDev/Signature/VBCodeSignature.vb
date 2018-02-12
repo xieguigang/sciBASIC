1d858123d478b76c9c81328efa2bea29e7323405
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-02-12 18:22:42 +0800

    vb identifer pattern

---
+++ Microsoft.VisualBasic.Core/ApplicationServices/VBDev/Signature/VBCodeSignature.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+Namespace ApplicationServices.Development
+
+    Module VBCodeSignature
+
+        Const TypePatterns$ = "^\s+(Class)|(Module)|(Structure)|(Enum)\s+" & Text.
+
+        <Extension>
+        Public Iterator Function PopulateModules(vb As String) As IEnumerable(Of NamedCollection(Of String))
+
+
+
+        End Function
+    End Module
+End Namespace
