aeb55a0d8d4fccc31771e414ca4607c46d016be0
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-02-12 18:34:36 +0800

    add code comments

--- Microsoft.VisualBasic.Core/ApplicationServices/VBDev/Signature/VBCodeSignature.vb
+++ Microsoft.VisualBasic.Core/ApplicationServices/VBDev/Signature/VBCodeSignature.vb
@@ -7,13 +7,18 @@ Namespace ApplicationServices.Development
     Module VBCodeSignature
 
         Const AccessPattern$ = "((Public)|(Private)|(Friend)|(Protected)|(\s))*"
-        Const TypePatterns$ = "^\s+" & AccessPattern & "\s*((Class)|(Module)|(Structure)|(Enum))\s+" & VBLanguage.IdentiferPattern
+        Const TypePatterns$ = "^\s+" & AccessPattern & "\s*((Class)|(Module)|(Structure)|(Enum)|(Delegate))\s+" & VBLanguage.IdentiferPattern
         Const PropertyPatterns$ = "^\s+" & AccessPattern & "\s*((ReadOnly)|(WriteOnly)|(Default)|(\s))*\s*Property\s+" & VBLanguage.IdentiferPattern
         Const MethodPatterns$ = "^\s+" & AccessPattern & "\s*((Sub)|(Function)|(Iterator)|(\s))*\s+" & VBLanguage.IdentiferPattern
 
         <Extension>
         Public Iterator Function PopulateModules(vb As String) As IEnumerable(Of NamedCollection(Of String))
+            Dim types$() = vb.Matches(TypePatterns, RegexICMul).ToArray
+            Dim properties$() = vb.Matches(PropertyPatterns, RegexICMul).ToArray
+            Dim methods$() = vb.Matches(MethodPatterns, RegexICMul).ToArray
 
+            ' 使用InStr进行位置的确定
+            ' 并且使用前导的空格的数量来确定从属关系
 
 
         End Function
