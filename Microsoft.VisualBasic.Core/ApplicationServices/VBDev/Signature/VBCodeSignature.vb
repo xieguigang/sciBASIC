25a649a6f09ab6f86667b24c6b4eb620cc05763b
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-02-12 18:30:39 +0800

    add vb code element patterns

--- Microsoft.VisualBasic.Core/ApplicationServices/VBDev/Signature/VBCodeSignature.vb
+++ Microsoft.VisualBasic.Core/ApplicationServices/VBDev/Signature/VBCodeSignature.vb
@@ -1,11 +1,15 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Scripting.SymbolBuilder
 
 Namespace ApplicationServices.Development
 
     Module VBCodeSignature
 
-        Const TypePatterns$ = "^\s+(Class)|(Module)|(Structure)|(Enum)\s+" & Text.
+        Const AccessPattern$ = "((Public)|(Private)|(Friend)|(Protected)|(\s))*"
+        Const TypePatterns$ = "^\s+" & AccessPattern & "\s*((Class)|(Module)|(Structure)|(Enum))\s+" & VBLanguage.IdentiferPattern
+        Const PropertyPatterns$ = "^\s+" & AccessPattern & "\s*((ReadOnly)|(WriteOnly)|(Default)|(\s))*\s*Property\s+" & VBLanguage.IdentiferPattern
+        Const MethodPatterns$ = "^\s+" & AccessPattern & "\s*((Sub)|(Function)|(Iterator)|(\s))*\s+" & VBLanguage.IdentiferPattern
 
         <Extension>
         Public Iterator Function PopulateModules(vb As String) As IEnumerable(Of NamedCollection(Of String))
