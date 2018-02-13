cf255a3763e9f2faebccafed0df0fb1039c1507f
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-02-13 15:48:27 +0800

    improvements on the script builder

--- Microsoft.VisualBasic.Core/Scripting/ScriptBuilder.vb
+++ Microsoft.VisualBasic.Core/Scripting/ScriptBuilder.vb
@@ -1,31 +1,32 @@
 ﻿#Region "Microsoft.VisualBasic::4160b0cb3e10b50a1fd4239a40633bba, ..\sciBASIC#\Microsoft.VisualBasic.Core\Scripting\ScriptBuilder.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports System.Text
 Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.Text
@@ -39,6 +40,13 @@ Namespace Scripting.SymbolBuilder
 
         Public ReadOnly Property Script As StringBuilder
 
+        Default Public WriteOnly Property Assign(name As String) As String
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
+            Set
+                Call Script.Replace($"{{${name}}}", Value)
+            End Set
+        End Property
+
         Sub New(sb As StringBuilder)
             Script = sb
         End Sub
@@ -49,6 +57,10 @@ Namespace Scripting.SymbolBuilder
 
         Sub New()
             Call Me.New(capacity:=1024)
+        End Sub
+
+        Sub New(script$)
+            Call Me.New(New StringBuilder(script))
         End Sub
 
         ''' <summary>
