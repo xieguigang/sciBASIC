77bac375967c86fb1062e1f5f3111c760e7be9fb
谢桂纲 <xie.guigang@gcmodeller.org>
2018-02-26 21:45:34 +0800

    add extensions

--- Microsoft.VisualBasic.Core/Extensions/Collection/ListExtensions.vb
+++ Microsoft.VisualBasic.Core/Extensions/Collection/ListExtensions.vb
@@ -1,44 +1,44 @@
 ﻿#Region "Microsoft.VisualBasic::023d911e219432dfd5e138e98c5fb44f, Microsoft.VisualBasic.Core\Extensions\Collection\ListExtensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module ListExtensions
-    ' 
-    '     Function: __reversedTake, AsHashList, AsHashSet, HasKey, Indexing
-    '               rand, ReorderByKeys, TopMostFrequent
-    ' 
-    '     Sub: ForEach, Swap
-    ' 
-    ' /********************************************************************************/
+' Module ListExtensions
+' 
+'     Function: __reversedTake, AsHashList, AsHashSet, HasKey, Indexing
+'               rand, ReorderByKeys, TopMostFrequent
+' 
+'     Sub: ForEach, Swap
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -46,6 +46,7 @@ Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.ComponentModel.DataStructures
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 
@@ -247,6 +248,12 @@ Public Module ListExtensions
         End If
 
         Return result
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    <Extension>
+    Public Function AsLoop(Of T)(src As IEnumerable(Of T)) As LoopArray(Of T)
+        Return New LoopArray(Of T)(src)
     End Function
 
     ''' <summary>
