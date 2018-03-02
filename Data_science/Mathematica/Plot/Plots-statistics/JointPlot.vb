9481e38477cbf855db5eb6ad4870b4d0554a5efa
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-03-02 14:57:04 +0800

    joint plot?

---
+++ Data_science/Mathematica/Plot/Plots-statistics/JointPlot.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.Imaging.Driver
+
+Public Module JointPlot
+
+    Public Function JointScatter() As GraphicsData
+
+    End Function
+
+    Public Function JointContour() As GraphicsData
+
+    End Function
+End Module
