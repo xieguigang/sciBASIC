dec4703d241cdc3466600f3369f4fa7522806b2a
xieguigang <xie.guigang@gcmodeller.org>
2018-03-13 13:28:33 +0800

    word class for nlp

---
+++ Microsoft.VisualBasic.Core/Extensions/Trinity/Word.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Xml.Serialization
+
+Namespace Data.Trinity.NLP
+
+    Public Class Word
+
+        <XmlAttribute> Public Property [Class] As WordClass
+        <XmlAttribute> Public Property Text As String
+
+        Public Overrides Function ToString() As String
+            Return $"{[Class].Description} {Text}"
+        End Function
+    End Class
+End Namespace
