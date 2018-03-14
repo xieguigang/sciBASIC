a8d58189116db5f0ce5d30d41deef5fac06ac250
xieguigang <xie.guigang@gcmodeller.org>
2018-03-14 09:58:37 +0800

    add unit factor

---
+++ Microsoft.VisualBasic.Core/ComponentModel/Ranges/Unit.vb
@@ -0,0 +1,42 @@
+﻿Namespace ComponentModel.Ranges
+
+    Public NotInheritable Class Unit
+
+        Private Sub New()
+        End Sub
+
+        ''' <summary>
+        ''' 分（d） ``10^-1``
+        ''' </summary>
+        Public Const d = 10 ^ -1
+        ''' <summary>
+        ''' 厘（c） ``10^-2``
+        ''' </summary>
+        Public Const c = 10 ^ -2
+        ''' <summary>
+        ''' 毫（m） ``10^-3``
+        ''' </summary>
+        Public Const m = 10 ^ -3
+        ''' <summary>
+        ''' 微（μ） ``10^-6``
+        ''' </summary>
+        Public Const u = 10 ^ -6
+        ''' <summary>
+        ''' 纳（n） ``10^-9``
+        ''' </summary>
+        Public Const n = 10 ^ -9
+        ''' <summary>
+        ''' 皮（p） ``10^-12``
+        ''' </summary>
+        Public Const p = 10 ^ -12
+        ''' <summary>
+        ''' 飞（f） ``10^-15``
+        ''' </summary>
+        Public Const f = 10 ^ -15
+        ''' <summary>
+        ''' 阿（a） ``10^-18``
+        ''' </summary>
+        Public Const a = 10 ^ -18
+
+    End Class
+End Namespace
