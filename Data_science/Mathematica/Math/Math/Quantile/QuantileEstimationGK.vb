55a9e7f203c96597c1dc3c633cfe41c190889ebb
xieguigang <xie.guigang@gcmodeller.org>
2018-03-15 10:50:19 +0800

    improvements on the quantile API code style

--- Data_science/Mathematica/Math/Math/Quantile/QuantileEstimationGK.vb
+++ Data_science/Mathematica/Math/Math/Quantile/QuantileEstimationGK.vb
@@ -1,49 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::03e76218133e6625d786c5afd72b0cc0, Data_science\Mathematica\Math\Math\Quantile\QuantileEstimationGK.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class QuantileEstimationGK
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: Query, ToString
-    ' 
-    '         Sub: compress, (+2 Overloads) Insert
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class QuantileEstimationGK
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: Query, ToString
+' 
+'         Sub: compress, (+2 Overloads) Insert
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports System.Text
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports sys = System.Math
@@ -115,6 +116,7 @@ Namespace Quantile
                 .GetJson
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Sub Insert(v&)
             Call Insert(CDbl(v))
         End Sub
