6d3827fc8165474c4ade5d4ff8ae50427e5b3a6c
xieguigang <xie.guigang@gcmodeller.org>
2018-03-16 15:37:41 +0800

    draw hull polygon

--- gr/Microsoft.VisualBasic.Imaging/Drawing2D/Math2D/ConvexHull/ConvexHull.vb
+++ gr/Microsoft.VisualBasic.Imaging/Drawing2D/Math2D/ConvexHull/ConvexHull.vb
@@ -1,46 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::56c3fa26e3e0e3e8745bb3f1160015f8, gr\Microsoft.VisualBasic.Imaging\Drawing2D\Math2D\ConvexHull\ConvexHull.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module ConvexHull
-    ' 
-    '         Function: GrahamScan, JarvisMatch, turn
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module ConvexHull
+' 
+'         Function: GrahamScan, JarvisMatch, turn
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Drawing
+Imports System.Runtime.CompilerServices
 
 Namespace Drawing2D.Math2D.ConvexHull
 
@@ -57,6 +58,7 @@ Namespace Drawing2D.Math2D.ConvexHull
         Public Const TURN_RIGHT% = -1
         Public Const TURN_NONE = 0
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Friend Function turn(p As Point, q As Point, r As Point) As Integer
             Return ((q.X - p.X) * (r.Y - p.Y()) - (r.X - p.X) * (q.Y - p.Y)).CompareTo(0)
         End Function
---
+++ gr/Microsoft.VisualBasic.Imaging/Drawing2D/Math2D/HullPolygonDraw.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+
+Namespace Drawing2D.Math2D
+
+    Public Module HullPolygonDraw
+
+        <Extension>
+        Public Sub DrawHullPolygon(g As IGraphics, polygon As IEnumerable(Of PointF), color As Color, Optional strokeWidth! = 3, Optional alpha% = 200)
+
+        End Sub
+    End Module
+End Namespace
