9174b3877375ed25ae73684a9c5464fd659969a8
xieguigang <xie.guigang@gcmodeller.org>
2018-03-16 15:41:27 +0800

    fix for drawer method

--- gr/Microsoft.VisualBasic.Imaging/Drawing2D/Math2D/HullPolygonDraw.vb
+++ gr/Microsoft.VisualBasic.Imaging/Drawing2D/Math2D/HullPolygonDraw.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
 Imports System.Runtime.CompilerServices
 
 Namespace Drawing2D.Math2D
@@ -6,8 +7,19 @@ Namespace Drawing2D.Math2D
     Public Module HullPolygonDraw
 
         <Extension>
-        Public Sub DrawHullPolygon(g As IGraphics, polygon As IEnumerable(Of PointF), color As Color, Optional strokeWidth! = 3, Optional alpha% = 200)
+        Public Sub DrawHullPolygon(g As IGraphics, polygon As IEnumerable(Of PointF), color As Color, Optional strokeWidth! = 3, Optional alpha% = 180)
+            Dim path As New GraphicsPath
 
+            Call path.AddPolygon(polygon.ToArray)
+            Call path.CloseFigure()
+
+            Dim alphaBrush As New SolidBrush(color.Alpha(alpha))
+            Dim stroke As New Pen(color, strokeWidth) With {
+                .DashStyle = DashStyle.Dash
+            }
+
+            Call g.FillPath(alphaBrush, path)
+            Call g.DrawPath(stroke, path)
         End Sub
     End Module
 End Namespace
