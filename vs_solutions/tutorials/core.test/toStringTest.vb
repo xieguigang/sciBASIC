4f722af605b4e113d91f6d9cebd69d4d7aa90038
xieguigang <xie.guigang@gcmodeller.org>
2018-03-18 15:18:05 +0800

    add benchmark test

---
+++ vs_solutions/tutorials/core.test/toStringTest.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Threading
+
+Module toStringTest
+
+    Sub Main()
+
+        Dim strings$()
+
+        Call BENCHMARK(Sub() strings = populatestrings(no:=True).Select(AddressOf Scripting.ToString).ToArray)
+        Call BENCHMARK(Sub() strings = populatestrings(no:=False).Select(AddressOf Scripting.ToString).ToArray)
+
+        Pause()
+    End Sub
+
+    Public Iterator Function populatestrings(no As Boolean) As IEnumerable(Of NoCStr)
+        For i As Integer = 0 To 200
+            If no Then
+                Yield New NoCStr
+            Else
+                Yield New hasCStr
+            End If
+
+            Thread.Sleep(10)
+        Next
+    End Function
+
+    Public Class NoCStr
+
+        Dim s As String = RandomASCIIString(5, skipSymbols:=True)
+
+        Public Overrides Function ToString() As String
+            Return s
+        End Function
+    End Class
+
+    Public Class hasCStr : Inherits NoCStr
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Widening Operator CType(h As hasCStr) As String
+            Return h.ToString
+        End Operator
+    End Class
+End Module
