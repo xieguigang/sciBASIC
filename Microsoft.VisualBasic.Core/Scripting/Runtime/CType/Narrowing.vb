f6294e09ed7280cd22ce9823ac1153cfd8d19700
xieguigang <xie.guigang@gcmodeller.org>
2018-03-18 15:37:38 +0800

    GetNarrowingOperator

---
+++ Microsoft.VisualBasic.Core/Scripting/Runtime/CType/Narrowing.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
+
+Namespace Scripting.Runtime
+
+    Public Module NarrowingReflection
+
+        Const NarrowingOperator As BindingFlags = BindingFlags.Public Or BindingFlags.Static
+
+        Public Delegate Function INarrowingOperator(Of T)(obj As Object) As T
+
+        <Extension>
+        Public Function GetNarrowingOperator(Of T)(type As Type) As INarrowingOperator(Of T)
+            ' 函数找不到会返回Nothing
+            Dim methods = type.GetMethod("op_Explict", NarrowingOperator)
+
+            If methods Is Nothing Then
+                Return Nothing
+            Else
+                Dim op_Explict As INarrowingOperator(Of T) = methods.CreateDelegate(GetType(INarrowingOperator(Of T)))
+                Return op_Explict
+            End If
+        End Function
+    End Module
+End Namespace
--- vs_solutions/tutorials/core.test/toStringTest.vb
+++ vs_solutions/tutorials/core.test/toStringTest.vb
@@ -1,14 +1,44 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports System.Threading
+Imports Microsoft.VisualBasic.Scripting.Runtime
 
 Module toStringTest
 
     Sub Main()
 
+        Call reflectionTest()
+
+        Dim list1 = populatestrings(True)
+        Dim list2 = populatestrings(False)
         Dim strings$()
 
-        Call BENCHMARK(Sub() strings = populatestrings(no:=True).Select(AddressOf Scripting.ToString).ToArray)
-        Call BENCHMARK(Sub() strings = populatestrings(no:=False).Select(AddressOf Scripting.ToString).ToArray)
+        Call BENCHMARK(Sub() strings = list1.Select(AddressOf Scripting.ToString).ToArray)
+        Call BENCHMARK(Sub() strings = list2.Select(AddressOf Scripting.ToString).ToArray)
+        Call BENCHMARK(Sub() strings = list1.Select(Function(x) x.ToString).ToArray)
+
+
+        '   Call BENCHMARK(Sub() CStr(list1(0)))
+        Call BENCHMARK(Sub() strings = {CStr(DirectCast(list2(0), hasCStr))})
+
+        '   Call BENCHMARK(Sub() CType(list1(0), String))
+        Call BENCHMARK(Sub() strings = {CType(DirectCast(list2(0), hasCStr), String)})
+
+        Pause()
+    End Sub
+
+    Private Sub reflectionTest()
+
+        Dim type = GetType(NoCStr)
+        Dim type2 = GetType(hasCStr)
+
+
+        Dim op = type.GetNarrowingOperator(Of String)
+
+        Console.WriteLine(op(New NoCStr))
+
+        op = type2.GetNarrowingOperator(Of String)
+
+        Console.WriteLine(op(New hasCStr))
 
         Pause()
     End Sub
@@ -21,7 +51,7 @@ Module toStringTest
                 Yield New hasCStr
             End If
 
-            Thread.Sleep(10)
+            ' Thread.Sleep(10)
         Next
     End Function
 
@@ -37,7 +67,7 @@ Module toStringTest
     Public Class hasCStr : Inherits NoCStr
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Shared Widening Operator CType(h As hasCStr) As String
+        Public Shared Narrowing Operator CType(h As hasCStr) As String
             Return h.ToString
         End Operator
     End Class
