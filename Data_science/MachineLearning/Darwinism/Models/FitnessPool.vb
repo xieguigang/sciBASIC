707808ef4d224feb87bd6665f49b5741da1eba6c
xieguigang <xie.guigang@gcmodeller.org>
2018-03-18 14:56:30 +0800

    get key for cache

--- Data_science/MachineLearning/Darwinism/Models/FitnessPool.vb
+++ Data_science/MachineLearning/Darwinism/Models/FitnessPool.vb
@@ -1,55 +1,70 @@
 ﻿#Region "Microsoft.VisualBasic::96dbd90f8e16809fa949a179af621bc5, Data_science\MachineLearning\Darwinism\Models\FitnessPool.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class FitnessPool
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Function: Fitness
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class FitnessPool
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Function: Fitness
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports Microsoft.VisualBasic.Language.Default
+
 Namespace Darwinism.Models
 
+    ''' <summary>
+    ''' Compute fitness and cache the result data in this pool.
+    ''' </summary>
+    ''' <typeparam name="Individual"></typeparam>
     Public Class FitnessPool(Of Individual)
 
         Protected Friend ReadOnly cache As New Dictionary(Of String, Double)
         Protected caclFitness As Func(Of Individual, Double)
+        Protected indivToString As Func(Of Individual, String)
 
-        Sub New(cacl As Func(Of Individual, Double))
+        Shared ReadOnly objToString As New DefaultValue(Of Func(Of Individual, String))(AddressOf Scripting.ToString)
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="cacl">Expression for descript how to calculate the fitness.</param>
+        ''' <param name="toString">Obj to dictionary key</param>
+        Sub New(cacl As Func(Of Individual, Double), Optional toString As Func(Of Individual, String) = Nothing)
             caclFitness = cacl
+            indivToString = toString Or objToString
         End Sub
 
         Sub New()
@@ -61,7 +76,7 @@ Namespace Darwinism.Models
         ''' <param name="[in]"></param>
         ''' <returns></returns>
         Public Function Fitness([in] As Individual) As Double
-            Dim key$ = [in].ToString
+            Dim key$ = indivToString([in])
             Dim fit As Double
 
             SyncLock cache
