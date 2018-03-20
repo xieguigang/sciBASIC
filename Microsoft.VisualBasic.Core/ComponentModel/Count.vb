c8675719282bd6e619d69f5ab53791a07c1f5055
xieguigang <xie.guigang@gcmodeller.org>
2018-03-20 14:59:13 +0800

    add xml code comments

--- Microsoft.VisualBasic.Core/ComponentModel/Count.vb
+++ Microsoft.VisualBasic.Core/ComponentModel/Count.vb
@@ -56,7 +56,7 @@ Namespace ComponentModel
     Public Class Counter : Inherits int
 
         ''' <summary>
-        ''' Create a new integer counter.(新建一个计数器)
+        ''' Create a new integer counter start from ZERO.(新建一个计数器)
         ''' </summary>
         Sub New()
             MyBase.New(0)
