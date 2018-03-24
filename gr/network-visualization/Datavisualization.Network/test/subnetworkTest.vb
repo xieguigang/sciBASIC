bdc9a599536356c64b1fdefa0c3fa82056b98664
xieguigang <xie.guigang@gcmodeller.org>
2018-03-24 22:12:42 +0800

    fix reference errors

--- gr/network-visualization/Datavisualization.Network/test/subnetworkTest.vb
+++ gr/network-visualization/Datavisualization.Network/test/subnetworkTest.vb
@@ -1,54 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::6059518711c141d108dcf146445bbc06, gr\network-visualization\Datavisualization.Network\GraphTesting\subnetworkTest.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module subnetworkTest
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Module subnetworkTest
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports Microsoft.VisualBasic.Data.visualize.Network.FindPath
 
 
 Module subnetworkTest
 
     Sub Main()
-        Dim g = ExampleNetwork()
+        'Dim g = ExampleNetwork()
 
-        Dim subNetworks = g.IteratesSubNetworks.ToArray
+        'Dim subNetworks = g.IteratesSubNetworks.ToArray
 
-        Pause()
+        'Pause()
     End Sub
 End Module
