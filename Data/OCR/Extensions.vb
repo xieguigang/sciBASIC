8835ceb7fca8b6e4524079f244e01e016d18933c
xieguigang <xie.guigang@gcmodeller.org>
2018-03-26 16:51:22 +0800

    try to fix for the default assert of the gdi+ objects

--- Data/OCR/Extensions.vb
+++ Data/OCR/Extensions.vb
@@ -90,22 +90,17 @@ Public Module Extensions
         Dim equals As ISimilarity(Of Double) =
             Function(a, b)
                 If a = b Then
-                    Return 0
-                Else
                     Return 1
+                Else
+                    Return 0
                 End If
             End Function
-        Dim local As New GSW(Of Double)(query, subject, equals, AddressOf asChar)
+        Dim local As New GSW(Of Double)(subject, query, equals, AddressOf asChar)
         Dim objects = local.GetMatches(local.MaxScore * cutoff)
         Dim viewSize = view.Size
 
         For Each region As Match In objects
-            If (region.ToA - region.FromA) / query.Length >= 0.9 Then
-                Dim left = region.FromB
-                Dim length = region.ToB - left
-
-                Yield left.TranslateRegion(size, viewSize)
-            End If
+            Yield region.FromA.TranslateRegion(size, viewSize)
         Next
     End Function
 
