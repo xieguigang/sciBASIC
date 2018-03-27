b8af6a2e2c2c880f56747eab60fe924582ff582a
xieguigang <xie.guigang@gcmodeller.org>
2018-03-27 10:25:38 +0800

    build the library tree

--- Data/OCR/Library.vb
+++ Data/OCR/Library.vb
@@ -16,15 +16,37 @@ Public Class Library
 
     ReadOnly tree As BinaryTree(Of OpticalCharacter, Char)
     ReadOnly cutoff#
+    ReadOnly compares As Comparison(Of OpticalCharacter)
 
     Sub New(font As Font, Optional characters$ = enUS, Optional cutoff# = 0.98)
         Me.font = font
         Me.cutoff = cutoff
         Me.tree = BuildLibraryTree(font, characters, cutoff)
+        Me.compares = CompareTo(cutoff)
     End Sub
 
-    Public Function Match(OpticalCharacter As OpticalCharacter) As (score#, recognized As Char)
+    Public Shared Function CompareTo(cutoff As Double) As Comparison(Of OpticalCharacter)
+        Return Function(a As OpticalCharacter, b As OpticalCharacter) As Integer
+                   Dim score# = a.Compare(b)
 
+                   If score >= cutoff Then
+                       Return 0
+                   ElseIf score >= cutoff * 0.8 Then
+                       Return 1
+                   Else
+                       Return -1
+                   End If
+               End Function
+    End Function
+
+    Public Function Match(OpticalCharacter As OpticalCharacter) As (score#, recognized As Char)
+        Dim find = tree.Find(OpticalCharacter, compares)
+
+        If find Is Nothing Then
+            Return (-1, Nothing)
+        Else
+            Return (find.Key.Compare(OpticalCharacter), find.Value)
+        End If
     End Function
 
     Public Overrides Function ToString() As String
@@ -32,19 +54,7 @@ Public Class Library
     End Function
 
     Public Shared Function BuildLibraryTree(font As Font, characters$, Optional color$ = "black", Optional cutoff# = 0.98) As BinaryTree(Of OpticalCharacter, Char)
-        Dim compares As Comparison(Of OpticalCharacter) =
-            Function(a As OpticalCharacter, b As OpticalCharacter) As Integer
-                Dim score# = a.Compare(b)
-
-                If score >= cutoff Then
-                    Return 0
-                ElseIf score >= cutoff * 0.8 Then
-                    Return 1
-                Else
-                    Return -1
-                End If
-            End Function
-        Dim tree As New AVLTree(Of OpticalCharacter, Char)(compares, Function(c) c.char)
+        Dim tree As New AVLTree(Of OpticalCharacter, Char)(Library.CompareTo(cutoff), Function(c) c.char)
         Dim size As Size = font.Height.SquareSize
         Dim fontColor As New SolidBrush(color.TranslateColor)
         Dim ZERO As New Point(0, 0)
