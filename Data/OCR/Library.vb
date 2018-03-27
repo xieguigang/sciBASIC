d1646162de706f1a30a9b5e0e55c455f4e1bacf9
xieguigang <xie.guigang@gcmodeller.org>
2018-03-27 10:08:41 +0800

    add character model

---
+++ Data/OCR/Library.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.ComponentModel.Algorithm.BinaryTree
+
+''' <summary>
+''' The character library tree
+''' </summary>
+Public Class Library
+
+    ReadOnly font As Font
+
+    Const Numeric$ = "0123456789.+-"
+    Const Symbols$ = "/*-+()=^\<>,:;""'{}[]_&%$#@!~`?"
+    Const enUS$ = Numeric & Symbols & "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    ReadOnly tree As BinaryTree(Of OpticalCharacter, Char)
+    ReadOnly cutoff#
+
+    Sub New(font As Font, Optional characters$ = enUS, Optional cutoff# = 0.98)
+        Me.font = font
+        Me.cutoff = cutoff
+        Me.tree = BuildLibraryTree(font, characters, cutoff)
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return $"[{font.ToString}] {tree.PopulateNodes.Values.CharString}"
+    End Function
+
+    Public Shared Function BuildLibraryTree(font As Font, characters$, cutoff#) As BinaryTree(Of OpticalCharacter, Char)
+
+    End Function
+End Class
--- Data/OCR/OpticalCharacter.vb
+++ Data/OCR/OpticalCharacter.vb
@@ -6,7 +6,7 @@ Imports Microsoft.VisualBasic.Imaging.BitmapImage
 Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
 
-Public Class Character
+Public Class OpticalCharacter
 
     ''' <summary>
     ''' 使用-1分割，按照行进行排列
@@ -41,15 +41,14 @@ Public Class Character
     ''' </summary>
     ''' <param name="test">模板样本数据</param>
     ''' <returns></returns>
-    Public Function Compare(test As Character) As Double
+    Public Function Compare(test As OpticalCharacter) As Double
         Return SSM(PixelsVector, test.PixelsVector)
     End Function
 
     Public Overrides Function ToString() As String
-        Dim lines = PixelsVector.Split(Function(x) x = -1.0R)
         Dim sb As New StringBuilder
 
-        For Each line As Double() In lines
+        For Each line As Double() In PixelsVector.Split(Function(x) x = -1.0R)
             For Each c As Double In line
                 If c = 0R Then
                     Call sb.Append(" ")
