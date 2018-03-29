a2afa5c28107232484395b055702ad39bb02f156
xieguigang <xie.guigang@gcmodeller.org>
2018-03-29 10:48:03 +0800

    get unix time stamp

--- Microsoft.VisualBasic.Core/Extensions/ValueTypes/DateTimeHelper.vb
+++ Microsoft.VisualBasic.Core/Extensions/ValueTypes/DateTimeHelper.vb
@@ -1,45 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::ac4c164f25c967e7d69690aae9b8ae02, Microsoft.VisualBasic.Core\Extensions\ValueTypes\DateTimeHelper.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module DateTimeHelper
-    ' 
-    '         Properties: MonthList
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: DateSeq, FillDateZero, GetMonthInteger, YYMMDD
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module DateTimeHelper
+' 
+'         Properties: MonthList
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: DateSeq, FillDateZero, GetMonthInteger, YYMMDD
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -146,6 +146,17 @@ Namespace ValueTypes
             Dim mm$ = If(x.Month < 10, "0" & x.Month, x.Month)
             Dim dd$ = If(x.Day < 10, "0" & x.Day, x.Day)
             Return $"{x.Year}-{mm}-{dd}"
+        End Function
+
+        ''' <summary>
+        ''' Convert <see cref="DateTime"/> to unix time stamp
+        ''' </summary>
+        ''' <param name="time"></param>
+        ''' <returns></returns>
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        <Extension>
+        Public Function UnixTimeStamp(time As DateTime) As Long
+            Return (time - New DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds
         End Function
     End Module
 End Namespace
