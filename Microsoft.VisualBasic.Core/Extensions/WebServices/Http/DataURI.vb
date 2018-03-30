d0c22c0a8d1589b0bf9de658e58d2bf38b89807a
xieguigang <xie.guigang@gcmodeller.org>
2018-03-30 10:57:10 +0800

    reverse triangle

--- Microsoft.VisualBasic.Core/Extensions/WebServices/Http/DataURI.vb
+++ Microsoft.VisualBasic.Core/Extensions/WebServices/Http/DataURI.vb
@@ -1,45 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::18d4047bed67c21ceb35469e9c65792c, Microsoft.VisualBasic.Core\Extensions\WebServices\Http\DataURI.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class DataURI
-    ' 
-    '         Properties: base64, chartSet, mime
-    ' 
-    '         Constructor: (+3 Overloads) Sub New
-    '         Function: FromFile, ToStream, ToString, URIParser
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class DataURI
+' 
+'         Properties: base64, chartSet, mime
+' 
+'         Constructor: (+3 Overloads) Sub New
+'         Function: FromFile, ToStream, ToString, URIParser
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -48,6 +48,7 @@ Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ApplicationServices
 Imports Microsoft.VisualBasic.Net.Protocols
+Imports Microsoft.VisualBasic.Text
 
 Namespace Net.Http
 
@@ -101,6 +102,11 @@ Namespace Net.Http
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Shared Function FromFile(file As String) As DataURI
             Return New DataURI(file)
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Function SVGImage(svg As String) As DataURI
+            Return New DataURI(base64:=TextEncodings.UTF8WithoutBOM.GetBytes(svg).ToBase64String, mine:="image/svg+xml")
         End Function
 
         Public Shared Function URIParser(uri As String) As DataURI
