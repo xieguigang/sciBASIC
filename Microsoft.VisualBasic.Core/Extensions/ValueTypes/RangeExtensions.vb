596f1768c35eafa97e8643a14d8e26960bc5443b
xieguigang <xie.guigang@gcmodeller.org>
2018-04-08 18:11:39 +0800

    fix for zero divide

--- Microsoft.VisualBasic.Core/Extensions/ValueTypes/RangeExtensions.vb
+++ Microsoft.VisualBasic.Core/Extensions/ValueTypes/RangeExtensions.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::d4e4cf9439b7d04614d1030c4fd0f6d6, Microsoft.VisualBasic.Core\Extensions\ValueTypes\RangeExtensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module RangeExtensions
-    ' 
-    '         Function: Percentage
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module RangeExtensions
+' 
+'         Function: Percentage
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -49,7 +49,7 @@ Namespace ValueTypes
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         <Extension> Public Function Percentage(range As DoubleRange, value#) As Double
-            Return (value - range.Min) / range.Length
+            Return If(value = range.Min, 0, (value - range.Min) / range.Length)
         End Function
     End Module
 End Namespace
