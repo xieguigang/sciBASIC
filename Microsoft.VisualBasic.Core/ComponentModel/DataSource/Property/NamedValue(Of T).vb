5254009b2f2bd6dc327fb3b69f53e1bae7a1c73b
xieguigang <xie.guigang@gcmodeller.org>
2018-04-09 11:07:27 +0800

    tweaks on the named value debug display

--- Microsoft.VisualBasic.Core/ComponentModel/DataSource/Property/NamedValue(Of T).vb
+++ Microsoft.VisualBasic.Core/ComponentModel/DataSource/Property/NamedValue(Of T).vb
@@ -1,46 +1,46 @@
 ﻿#Region "Microsoft.VisualBasic::517f6ef4fe299053a12679463f263739, Microsoft.VisualBasic.Core\ComponentModel\DataSource\Property\NamedValue(Of T).vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Structure NamedValue
-    ' 
-    '         Properties: Description, IsEmpty, Name, Value, ValueType
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: FixValue, ToString
-    '         Operators: (+2 Overloads) +, <>, =
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Structure NamedValue
+' 
+'         Properties: Description, IsEmpty, Name, Value, ValueType
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: FixValue, ToString
+'         Operators: (+2 Overloads) +, <>, =
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -123,10 +123,13 @@ Namespace ComponentModel.DataSourceModel
         ''' <returns></returns>
         Public Overrides Function ToString() As String
             Try
+                ' 2018-4-9 在这里将显示修改为类型的ToString方法，这样子就可以由
+                ' 用户来通过重写ToString方法来自定义显示，而非强制使用GetJson方法
+                ' 将全部的对象都显示出来，对于属性很多的对象GetJson方法显示的效果不是太好
                 If Description.StringEmpty Then
-                    Return $"{Name} --> {Value.GetJson}"
+                    Return $"{Name} --> {Value.ToString}"
                 Else
-                    Return $"{Name} --> {Value.GetJson} ({Description})"
+                    Return $"{Name} --> {Value.ToString} ({Description})"
                 End If
             Catch ex As Exception
                 Return Name
