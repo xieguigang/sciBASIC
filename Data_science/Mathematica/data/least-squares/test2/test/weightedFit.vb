4932c77f357df6830519871f1cf4159ae75f7a1f
xieguigang <xie.guigang@gcmodeller.org>
2018-04-10 13:57:15 +0800

    1 / x^2 as weight

--- Data_science/Mathematica/data/least-squares/test2/test/weightedFit.vb
+++ Data_science/Mathematica/data/least-squares/test2/test/weightedFit.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.Data.Bootstrapping
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
 
 Module weightedFitTest
 
@@ -6,6 +7,8 @@ Module weightedFitTest
         Dim X#() = {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7}
         Dim Y#() = {4.09818, 4.39655, 4.61435, 4.95867, 5.26182, 5.55079, 5.84748, 6.11208}
         Dim W#() = {1, 1, 1, 1, 1, 1, 1, 1}
+
+        W = 1 / (X.AsVector ^ 2)
 
         Dim fit1 As WeightedFit = WeightedLinearRegression.Regress(X, Y, W, 1)
         Dim fit2 As WeightedFit = WeightedLinearRegression.Regress(X, Y, W, 2)
