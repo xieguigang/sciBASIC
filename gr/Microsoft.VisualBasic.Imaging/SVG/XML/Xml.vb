91a172dc75e52543702368828b3a18be5c76ac0c
xieguigang <xie.guigang@gcmodeller.org>
2018-04-11 09:28:53 +0800

    add marker property

--- gr/Microsoft.VisualBasic.Imaging/SVG/XML/Xml.vb
+++ gr/Microsoft.VisualBasic.Imaging/SVG/XML/Xml.vb
@@ -224,6 +224,12 @@ Namespace SVG.XML
     Public Class polyline : Inherits node
 
         <XmlAttribute> Public Property points As String()
+        <XmlAttribute("marker-end")>
+        Public Property markerEnd As String
+
+        Public Overrides Function ToString() As String
+            Return points.JoinBy(" ")
+        End Function
 
         Public Shared Operator +(line As polyline, offset As PointF) As polyline
             ' Throw New NotImplementedException
