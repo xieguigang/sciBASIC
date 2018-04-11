6a76b753e57035d2a775d1f522bc2c24d0db8053
xieguigang <xie.guigang@gcmodeller.org>
2018-04-11 10:43:04 +0800

    get html document node tag name

---
+++ mime/text%html/HTML/jQuery/Extensions.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.MIME.Markup.HTML.XmlMeta
+
+Namespace HTML.jQuery
+
+    Public Module Extensions
+
+        ''' <summary>
+        ''' 如果定义了<see cref="XmlTypeAttribute"/>，则优先使用这个属性之中的<see cref="XmlTypeAttribute.TypeName"/>
+        ''' 作为文档元素标签的名字，没有找到的话，则直接使用``Class Name``来作为文档元素标签的名字
+        ''' </summary>
+        ''' <typeparam name="T"></typeparam>
+        ''' <param name="element"></param>
+        ''' <returns></returns>
+        <Extension>
+        Public Function TagName(Of T As Node)(element As T) As String
+            Dim type As Type = GetType(T)
+            Dim xmlType As XmlTypeAttribute = type.GetCustomAttribute(Of XmlTypeAttribute)
+
+            If xmlType Is Nothing Then
+                Return type.Name
+            Else
+                Return xmlType.TypeName
+            End If
+        End Function
+    End Module
+End Namespace
