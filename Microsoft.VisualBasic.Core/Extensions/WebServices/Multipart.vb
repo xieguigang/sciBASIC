5324f34ec76067ddb63435889d3fadbf6c27eb78
xieguigang <xie.guigang@gcmodeller.org>
2018-04-15 15:34:57 +0800

    dump multipart form data

--- Microsoft.VisualBasic.Core/Extensions/WebServices/Multipart.vb
+++ Microsoft.VisualBasic.Core/Extensions/WebServices/Multipart.vb
@@ -1,49 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::2bd339b862a52de751d989ed472bd92b, Microsoft.VisualBasic.Core\Extensions\WebServices\Multipart.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class MultipartForm
-    ' 
-    '         Function: POST
-    ' 
-    '         Sub: (+2 Overloads) Add
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class MultipartForm
+' 
+'         Function: POST
+' 
+'         Sub: (+2 Overloads) Add
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
 Imports System.Net
+Imports System.Runtime.CompilerServices
 Imports System.Text
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
@@ -108,6 +109,11 @@ Namespace Net.Http
                 Call .WriteLine(, Encoding.ASCII)
                 Call .WriteLine("--" & boundary & "--", Encoding.ASCII)
             End With
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Sub Dump(path As String)
+            Call buffer.ToArray.FlushStream(path)
         End Sub
 
         ''' <summary>
