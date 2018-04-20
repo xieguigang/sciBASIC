7a6d236c11ffecf726bf058ec086c17073bfa09e
xieguigang <xie.guigang@gcmodeller.org>
2018-04-20 23:01:07 +0800

    xml abstract data model

---
+++ Microsoft.VisualBasic.Core/ComponentModel/File/XmlDataModel.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Xml
+Imports System.Xml.Serialization
+
+Namespace ComponentModel
+
+    Public MustInherit Class XmlDataModel
+
+        ''' <summary>
+        ''' ReadOnly
+        ''' </summary>
+        ''' <returns></returns>
+        ''' 
+        <XmlAnyElement>
+        Public Property TypeComment As XmlComment
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
+            Get
+                Return GetTypeReferenceComment()
+            End Get
+            Set(value As XmlComment)
+                ' Do Nothing
+            End Set
+        End Property
+
+        Private Function GetTypeReferenceComment() As XmlComment
+            Dim modelType As Type = MyClass.GetType
+            Dim fullName$ = modelType.FullName
+            Dim assembly$ = modelType.Assembly.CodeBase.FileName
+            Dim trace$ = "DataModel: " & fullName & vbCrLf & "assembly: " & assembly
+
+            Return New XmlDocument().CreateComment(trace)
+        End Function
+    End Class
+End Namespace
