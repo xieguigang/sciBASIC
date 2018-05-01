e9821bd1617d611cf117a9ba9a587e7369becf8a
xieguigang <xie.guigang@gcmodeller.org>
2018-05-01 18:37:17 +0800

    https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect/565282#

--- Microsoft.VisualBasic.Core/Extensions/Image/Math/Models/Line.vb
+++ Microsoft.VisualBasic.Core/Extensions/Image/Math/Models/Line.vb
@@ -52,61 +52,66 @@ Namespace Imaging.Math2D
 
         Public Shared ReadOnly Empty As New Line
 
-        Dim P1, P2 As PointF
+        ''' <summary>
+        ''' (<see cref="X1"/>, <see cref="Y1"/>)
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property P1 As PointF
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
+            Get
+                Return New PointF(X1, Y1)
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' (<see cref="X2"/>, <see cref="Y2"/>)
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property P2 As PointF
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
+            Get
+                Return New PointF(X2, Y2)
+            End Get
+        End Property
+
+        Public ReadOnly Property Length As Double
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
+            Get
+                Return P1.Distance(P2)
+            End Get
+        End Property
 
         Public Sub New(p1 As PointF, p2 As PointF)
-            Me.P1 = p1
-            Me.P2 = p2
+            Me.X1 = p1.X
+            Me.Y1 = p1.Y
+            Me.X2 = p2.X
+            Me.Y2 = p2.Y
         End Sub
 
 #Region "Points"
-        Public Property X1() As Single
-            <MethodImpl(MethodImplOptions.AggressiveInlining)>
-            Get
-                Return P1.X
-            End Get
-            <MethodImpl(MethodImplOptions.AggressiveInlining)>
-            Set
-                P1.X = Value
-            End Set
-        End Property
-
-        Public Property X2() As Single
-            <MethodImpl(MethodImplOptions.AggressiveInlining)>
-            Get
-                Return P2.X
-            End Get
-            <MethodImpl(MethodImplOptions.AggressiveInlining)>
-            Set
-                P2.X = Value
-            End Set
-        End Property
-
-        Public Property Y1() As Single
-            <MethodImpl(MethodImplOptions.AggressiveInlining)>
-            Get
-                Return P1.Y
-            End Get
-            <MethodImpl(MethodImplOptions.AggressiveInlining)>
-            Set
-                P1.Y = Value
-            End Set
-        End Property
-
-        Public Property Y2() As Single
-            <MethodImpl(MethodImplOptions.AggressiveInlining)>
-            Get
-                Return P2.Y
-            End Get
-            <MethodImpl(MethodImplOptions.AggressiveInlining)>
-            Set
-                P2.Y = Value
-            End Set
-        End Property
+        Public Property X1 As Single
+        Public Property X2 As Single
+        Public Property Y1 As Single
+        Public Property Y2 As Single
 #End Region
 
         Public Overrides Function ToString() As String
             Return $"[{{{X1}, {Y1}}}, {{{X2}, {Y2}}}]"
         End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Widening Operator CType(pointTuple As (X#, Y#)()) As Line
+            Return New Line With {
+                .X1 = pointTuple(0).X,
+                .Y1 = pointTuple(0).Y,
+                .X2 = pointTuple(1).X,
+                .Y2 = pointTuple(1).Y
+            }
+        End Operator
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Widening Operator CType(twoPoints As PointF()) As Line
+            Return New Line(twoPoints(0), twoPoints(1))
+        End Operator
     End Structure
 End Namespace
