e35d41730f80b7f30ea688af1e727902b423ccd4
xieguigang <xie.guigang@gcmodeller.org>
2018-05-21 09:26:06 +0800

    string builder helpers

---
+++ Microsoft.VisualBasic.Core/Extensions/StringHelpers/StringBuilders.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Text
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Linq
+
+Public Module StringBuilders
+
+    <Extension>
+    Public Function Replace(sb As StringBuilder, ParamArray replacements As NamedValue(Of String)()) As StringBuilder
+        For Each tuple As NamedValue(Of String) In replacements.SafeQuery
+            Call sb.Replace(tuple.Name, tuple.Value)
+        Next
+
+        Return sb
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    <Extension>
+    Public Function Replace(sb As StringBuilder, find$, value As Func(Of String)) As StringBuilder
+        Return sb.Replace(find, value())
+    End Function
+End Module
