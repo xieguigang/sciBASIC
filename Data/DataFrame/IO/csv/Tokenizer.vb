18593cef20ce9da8d8168026c630857f2f1e6fe0
xieguigang <xie.guigang@gcmodeller.org>
2018-05-25 12:59:07 +0800

    csv row parser bugs was fixed

--- Data/DataFrame/IO/csv/Tokenizer.vb
+++ Data/DataFrame/IO/csv/Tokenizer.vb
@@ -99,15 +99,18 @@ Namespace IO
             Dim tokens As New List(Of String)
             Dim temp As New List(Of Char)
             ' 解析器是否是处于由双引号所产生的栈之中？
-            Dim openEscaping As Boolean = False
+            Dim openStack As Boolean = False
             Dim buffer As New Pointer(Of Char)(s)
+            Dim doubleQuot$ = quot & quot
 
             Do While Not buffer.EndRead
                 Dim c As Char = +buffer
 
-                If openEscaping Then
+                If openStack Then
 
                     If c = quot Then
+
+                        ' \" 会被转义为单个字符 "
                         If temp.StartEscaping Then
                             Call temp.RemoveLast
                             Call temp.Add(c)
@@ -115,14 +118,28 @@ Namespace IO
                             ' 查看下一个字符是否为分隔符
                             ' 因为前面的 Dim c As Char = +buffer 已经位移了，所以在这里直接取当前的字符
                             Dim peek = buffer.Current
+                            ' 也有可能是 "" 转义 为单个 "
+                            Dim lastQuot = (temp > 0 AndAlso temp.Last <> quot)
 
-                            If peek = delimiter OrElse buffer.EndRead Then
+                            If temp = 0 AndAlso peek = delimiter Then
+
+                                ' openStack意味着前面已经出现一个 " 了
+                                ' 这里又出现了一个 " 并且下一个字符为分隔符
+                                ' 则说明是 "", 当前的cell内容是一个空字符串
+                                tokens += ""
+                                temp *= 0
+                                buffer += 1
+                                openStack = False
+
+                            ElseIf (peek = delimiter OrElse buffer.EndRead) AndAlso lastQuot Then
+
                                 ' 下一个字符为分隔符，则结束这个token
-                                tokens += New String(temp)
+                                tokens += New String(temp).Replace(doubleQuot, quot)
                                 temp *= 0
                                 ' 跳过下一个分隔符，因为已经在这里判断过了
                                 buffer += 1
-                                openEscaping = False
+                                openStack = False
+
                             Else
                                 ' 不是，则继续添加
                                 temp += c
@@ -135,10 +152,10 @@ Namespace IO
                 Else
                     If temp.Count = 0 AndAlso c = quot Then
                         ' token的第一个字符串为双引号，则开始转义
-                        openEscaping = True
+                        openStack = True
                     Else
                         If c = delimiter Then
-                            tokens += New String(temp)
+                            tokens += New String(temp).Replace(doubleQuot, quot)
                             temp *= 0
                         Else
                             temp += c
@@ -148,7 +165,7 @@ Namespace IO
             Loop
 
             If temp.Count > 0 Then
-                tokens += New String(temp)
+                tokens += New String(temp).Replace(doubleQuot, quot)
             End If
 
             Return tokens
