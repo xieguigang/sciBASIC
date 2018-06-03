bfc8dbcf1af2499b07a63f649c3b07dbb8e9edc2
xieguigang <xie.guigang@gcmodeller.org>
2018-06-03 20:41:13 +0800

    kmeans of dataset matrix

--- Data_science/Mathematica/Plot/Chart/CLI.vb
+++ Data_science/Mathematica/Plot/Chart/CLI.vb
@@ -1,50 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::f0c22137a2283a14c452ecc8f47f666f, Data_science\Mathematica\Plot\Chart\CLI.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module CLI
-    ' 
-    '     Function: Scatter
-    ' 
-    ' /********************************************************************************/
+' Module CLI
+' 
+'     Function: Scatter
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.ComponentModel
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Data.csv.IO
-Imports Microsoft.VisualBasic.Language
-Imports Microsoft.VisualBasic.Text
+Imports Microsoft.VisualBasic.DataMining
+Imports Microsoft.VisualBasic.DataMining.KMeans
 
 Module CLI
 
@@ -61,5 +62,21 @@ Module CLI
         Dim out$ = args("/out") Or $"{[in].TrimSuffix}_[{fx.NormalizePathString},{fy.NormalizePathString}].png"
         Dim csv = DataSet.LoadDataSet([in]).ToArray
 
+    End Function
+
+    <ExportAPI("/kmeans")>
+    <Usage("/kmeans /in <matrix.csv> [/n <expected_cluster_numbers, default=3> /out <clusters.csv>]")>
+    <Group("Data tools")>
+    Public Function KMeansCluster(args As CommandLine) As Integer
+        Dim in$ = args("/in")
+        Dim n% = args("/n") Or 5
+        Dim out$ = args("/out") Or $"{[in].TrimSuffix}.kmeans.csv"
+        Dim data As DataSet() = DataSet.LoadDataSet([in]).ToArray
+        Dim clusters As IEnumerable(Of EntityClusterModel) =
+            data _
+            .ToKMeansModels _
+            .Kmeans(expected:=n)
+
+        Return clusters.SaveTo(out).CLICode
     End Function
 End Module
