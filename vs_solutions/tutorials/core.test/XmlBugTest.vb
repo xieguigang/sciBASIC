62f80a0d1ff64bae45c014bbc4808123f18c880e
xieguigang <xie.guigang@gcmodeller.org>
2018-06-05 16:34:32 +0800

    reproduce the bug

--- vs_solutions/tutorials/core.test/XmlBugTest.vb
+++ vs_solutions/tutorials/core.test/XmlBugTest.vb
@@ -1,70 +1,213 @@
-﻿Imports System.Xml.Serialization
+﻿Imports System.Xml
+Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.ComponentModel
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Serialization.JSON
+Imports Microsoft.VisualBasic.Text.Xml.Linq
+Imports TestProject.XmlCommentBug
 
 Module XmlBugTest
 
     Sub Main()
 
-        Dim xml = New XmlCommentBug With {
-            .sss1 = "sfsfsfsf",
-            .sss2 = False,
-            .sss3 = "sssss", .sss4 = 4342423,
-            .sss5 = New textNode With {.largeText = "aaaa
-            sd
-fs
-fs
-fsd
-f
-sdfsdddsfdf
-sd
-fsd
-f
-sd
-fsd
-f
-sd
-fsd
-ffffffffffffffffff  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
-aaa"},
-            .sss6 = {"eeeeee", "asdasda", "gggggggggggg"},
-            .list = {New propertyType With {.s = "sffsdfsd", .t = Now}, New propertyType With {.s = "eeeeee", .t = "8888888"}},
-            .list2 = .list
-        }.GetXml
-
-        ' removes xml comment
-        xml = xml.StringReplace("<![-]{2}.+?[-]{2}>", "").Replace("utf-16", "utf-8").Replace(<xmlns>xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"</xmlns>.Value, "")
-
-        ' reload
-        Dim reloadObject As XmlCommentBug = xml.LoadFromXml(Of XmlCommentBug)
-
+        Dim works = GetCompleteEntity(57652)
+        Dim notWorking = GetCompleteEntity(51311)
 
         Pause()
     End Sub
+
+    Public Function GetCompleteEntity(chebiId$) As ChEBIEntity()
+        Dim url$ = $"http://www.ebi.ac.uk/webservices/chebi/2.0/test/getCompleteEntity?chebiId={chebiId}"
+        Dim xml$ = url.GET
+        Dim out As ChEBIEntity() = REST.ParsingRESTData(xml)
+        Return out
+    End Function
 End Module
 
-Public Class XmlCommentBug : Inherits XmlDataModel
+Namespace XmlCommentBug
 
-    Public Property sss1 As String
-    Public Property sss2 As Boolean
-    Public Property sss3 As String
-    Public Property sss4 As Long
-    Public Property sss5 As textNode
-    <XmlElement>
-    Public Property sss6 As String()
+    <XmlRoot("getCompleteEntityResponse", [Namespace]:="http://www.ebi.ac.uk/webservices/chebi")>
+    Public Structure REST
 
-    <XmlElement("list222")>
-    Public Property list As propertyType()
-    <XmlElement("list333")>
-    Public Property list2 As propertyType()
-End Class
+        <XmlElement("return")> Public Property [return] As ChEBIEntity
 
-Public Class textNode
-    Public Property largeText As String
-End Class
+        Public Shared Function ParsingRESTData(result$) As ChEBIEntity()
+            Dim xml As XmlDocument = result.LoadXmlDocument
+            Dim nodes = xml.GetElementsByTagName("S:Body")
+            Dim out As New List(Of ChEBIEntity)
 
-Public Class propertyType
-    Public Property s As String
-    Public Property t As String
-End Class
+            For Each node As XmlNode In nodes
+                For Each rep As XmlNode In node.ChildNodes
+                    For Each child As XmlNode In rep.ChildNodes
+                        If child.Name <> "return" Then
+                            Continue For
+                        End If
+
+                        result = child.InnerXml
+                        result = $"<{NameOf(ChEBIEntity)}>" & result & $"</{NameOf(ChEBIEntity)}>"
+                        result = result.Replace(" xmlns=""https://www.ebi.ac.uk/webservices/chebi""", "")
+
+                        Try
+                            out += result.CreateObjectFromXmlFragment(Of ChEBIEntity)
+                        Catch ex As Exception
+                            Throw New Exception(node.InnerText, ex)
+                        End Try
+                    Next
+                Next
+            Next
+
+            Return out
+        End Function
+    End Structure
+
+    Public Class ChEBIEntity : Inherits XmlDataModel
+        ' Implements INamedValue
+        '  Implements IMolecule
+
+        ''' <summary>
+        ''' Chebi的主ID
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property chebiId As String 'Implements INamedValue.Key, IMolecule.ID
+
+        Public Property chebiAsciiName As String 'Implements IMolecule.Name
+        Public Property definition As String
+        Public Property status As String
+        Public Property smiles As String
+        Public Property inchi As String
+        Public Property inchiKey As String
+        Public Property charge As Integer
+        Public Property mass As Double 'Implements IMolecule.Mass
+        Public Property entityStar As Integer
+        <XmlElement>
+        Public Property Synonyms As Synonyms()
+        <XmlElement>
+        Public Property IupacNames As Synonyms()
+        <XmlElement>
+        Public Property Citations As Synonyms()
+        Public Property Formulae As Formulae
+        ''' <summary>
+        ''' 次级编号，和主编号都代表同一样物质
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlElement>
+        Public Property SecondaryChEBIIds As String()
+        <XmlElement>
+        Public Property RegistryNumbers As RegistryNumbers()
+        <XmlElement>
+        Public Property ChemicalStructures As ChemicalStructures()
+        <XmlElement>
+        Public Property DatabaseLinks As DatabaseLinks()
+        <XmlElement>
+        Public Property OntologyParents As OntologyParents()
+        <XmlElement>
+        Public Property OntologyChildren As OntologyParents()
+        <XmlElement>
+        Public Property CompoundOrigins As CompoundOrigin()
+
+        Private Property Formula As String ' Implements IMolecule.Formula
+            Get
+                Return Formulae.data
+            End Get
+            Set(value As String)
+                Formulae.data = value
+            End Set
+        End Property
+    End Class
+
+    Public Class CompoundOrigin
+        Public Property speciesText As String
+        Public Property speciesAccession As String
+        Public Property componentText As String
+        Public Property componentAccession As String
+        Public Property SourceType As String
+        Public Property SourceAccession As String
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+    End Class
+
+    Public Class OntologyParents
+        Public Property chebiName As String
+        Public Property chebiId As String
+        Public Property type As String
+        Public Property status As String
+        Public Property cyclicRelationship As Boolean
+
+        Public Overrides Function ToString() As String
+            Return chebiName
+        End Function
+    End Class
+
+    Public Class DatabaseLinks
+
+        Public Property data As String
+        Public Property type As String
+
+        Public Const HMDB_accession$ = "HMDB accession"
+        Public Const KEGG_COMPOUND_accession$ = "KEGG COMPOUND accession"
+        Public Const KEGG_DRUG_accession$ = "KEGG DRUG accession"
+
+        Public Overrides Function ToString() As String
+            Return $"[{type}] {data}"
+        End Function
+    End Class
+
+    Public Class ChemicalStructures
+        Public Property [structure] As String
+        Public Property type As String
+        Public Property dimension As String
+        Public Property defaultStructure As String
+    End Class
+
+    Public Class Synonyms
+
+        Public Property data As String
+        Public Property source As String
+        Public Property type As String
+
+        Public Overrides Function ToString() As String
+            Return $"[{source}] {data}"
+        End Function
+    End Class
+
+    Public Class RegistryNumbers
+
+        Public Property data As String
+        Public Property source As String
+        Public Property type As String
+
+        Public Const Type_Reaxys$ = "Reaxys Registry Number"
+        Public Const Type_Beilstein$ = "Beilstein Registry Number"
+        Public Const Type_CAS$ = "CAS Registry Number"
+
+        Public Overrides Function ToString() As String
+            Return data
+        End Function
+
+    End Class
+
+    ''' <summary>
+    ''' 分子式
+    ''' </summary>
+    Public Class Formulae
+
+        ''' <summary>
+        ''' 分子式字符串
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property data As String
+        ''' <summary>
+        ''' 分子式的数据来源
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property source As String
+
+        Public Overrides Function ToString() As String
+            Return data
+        End Function
+    End Class
+End Namespace
