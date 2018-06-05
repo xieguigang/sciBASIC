c662be9e0efc537f3ae640b57c16ae899d0744fb
xieguigang <xie.guigang@gcmodeller.org>
2018-06-05 17:39:22 +0800

    split source file

---
+++ Data_science/Mathematica/Math/ConsoleApp1/ConsoleApp1/DATA.vb
@@ -0,0 +1,46 @@
+﻿Public Class htest
+
+    Public Property statistic As Dictionary(Of String, Double)
+    Public Property parameters As Dictionary(Of String, Double)
+    Public Property pvalue As Double
+    Public Property n As Integer
+    Public Property b As Double
+
+End Class
+
+''' <summary>
+''' mHG definition:
+''' 
+''' ```
+'''   mHG(lambdas) = min over 1 &lt;= n &lt;= N Of HGT (b_n(lambdas); N, B, n)
+''' ```
+'''   
+''' Where ``HGT`` Is the hypergeometric tail:
+''' 
+''' ```
+'''   HGT(b; N, B, n) = Probability(X >= b)
+''' ```
+'''   
+''' And:
+''' 
+''' ```
+'''   b_n = sum over 1 &lt;= i &lt;= n Of lambdas[i]
+''' ```
+''' </summary>
+Public Class mHGstatisticInfo
+
+    ''' <summary>
+    ''' the statistic itself
+    ''' </summary>
+    Public Property mHG As Double
+    ''' <summary>
+    ''' the index For which it was obtained
+    ''' </summary>
+    Public Property n As Double
+    ''' <summary>
+    ''' (Short For b_n) - sum over ``1 &lt;= i &lt;= n`` Of lambdas[i]
+    ''' </summary>
+    Public Property b As Double
+
+End Class
+
