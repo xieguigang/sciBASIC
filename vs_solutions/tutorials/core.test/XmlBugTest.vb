46fbd7ebb0fba0f8176421fd7e752ffcccdc44c7
xieguigang <xie.guigang@gcmodeller.org>
2018-06-05 17:02:10 +0800

    bug repreoduce

--- vs_solutions/tutorials/core.test/XmlBugTest.vb
+++ vs_solutions/tutorials/core.test/XmlBugTest.vb
@@ -10,33 +10,52 @@ Module XmlBugTest
 
     Sub Main()
 
+        Call test2()
 
-
-        Dim works = GetCompleteEntity(57652)
-        Dim notWorking = GetCompleteEntity(51311)
+        Dim Working1 = GetCompleteEntity(51311, False)
+        Dim notWorking = GetCompleteEntity(51311, True)
 
         Pause()
     End Sub
 
-    Public Function GetCompleteEntity(chebiId$) As ChEBIEntity()
+    Public Function getCompleteEntity(chebiId$, bugtest As Boolean) As Object
         Dim url$ = $"http://www.ebi.ac.uk/webservices/chebi/2.0/test/getCompleteEntity?chebiId={chebiId}"
         Dim xml$ = url.GET
-        Dim out As ChEBIEntity() = REST.ParsingRESTData(xml)
-        Return out
+
+        Return REST.ParsingRESTData(xml, bugtest)
     End Function
+
+    Sub test2()
+        Dim Xml$ = New Working With {.p1 = "sfdsfsdfs", .p2 = Now, .p3 = 454353453, .p4 = {"qqqqqqqqq", "eeeeeeee", "2222", "asdasd"}}.GetXml
+
+        ' reload
+        Dim obj As Working = Xml.LoadFromXml(Of Working)
+
+        Call obj.GetJson.__DEBUG_ECHO
+    End Sub
 End Module
 
 Namespace XmlCommentBug
+
+    Public Class Working : Inherits XmlDataModel
+
+        Public Property p1 As String
+        Public Property p2 As Date
+        Public Property p3 As Long
+
+        <XmlElement>
+        Public Property p4 As String()
+
+    End Class
 
     <XmlRoot("getCompleteEntityResponse", [Namespace]:="http://www.ebi.ac.uk/webservices/chebi")>
     Public Structure REST
 
         <XmlElement("return")> Public Property [return] As ChEBIEntity
 
-        Public Shared Function ParsingRESTData(result$) As ChEBIEntity()
+        Public Shared Function ParsingRESTData(result$, bugtest As Boolean) As Object
             Dim xml As XmlDocument = result.LoadXmlDocument
             Dim nodes = xml.GetElementsByTagName("S:Body")
-            Dim out As New List(Of ChEBIEntity)
 
             For Each node As XmlNode In nodes
                 For Each rep As XmlNode In node.ChildNodes
@@ -49,20 +68,82 @@ Namespace XmlCommentBug
                         result = $"<{NameOf(ChEBIEntity)}>" & result & $"</{NameOf(ChEBIEntity)}>"
                         result = result.Replace(" xmlns=""https://www.ebi.ac.uk/webservices/chebi""", "")
 
-                        Try
-                            out += result.CreateObjectFromXmlFragment(Of ChEBIEntity)
-                        Catch ex As Exception
-                            Throw New Exception(node.InnerText, ex)
-                        End Try
+                        If bugtest Then
+                            result = result.Replace("<ChEBIEntity>", "<ChEBIEntity_XmlCommentBug>").Replace("</ChEBIEntity>", "</ChEBIEntity_XmlCommentBug>")
+                            Return result.CreateObjectFromXmlFragment(Of ChEBIEntity_XmlCommentBug)
+                        Else
+                            Return result.CreateObjectFromXmlFragment(Of ChEBIEntity)
+                        End If
                     Next
                 Next
             Next
 
-            Return out
+            Throw New NotImplementedException
         End Function
     End Structure
 
-    Public Class ChEBIEntity : Inherits XmlDataModel
+    ''' <summary>
+    ''' not working
+    ''' </summary>
+    Public Class ChEBIEntity_XmlCommentBug : Inherits XmlDataModel
+        ' Implements INamedValue
+        '  Implements IMolecule
+
+        ''' <summary>
+        ''' Chebi的主ID
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property chebiId As String 'Implements INamedValue.Key, IMolecule.ID
+
+        Public Property chebiAsciiName As String 'Implements IMolecule.Name
+        Public Property definition As String
+        Public Property status As String
+        Public Property smiles As String
+        Public Property inchi As String
+        Public Property inchiKey As String
+        Public Property charge As Integer
+        Public Property mass As Double 'Implements IMolecule.Mass
+        Public Property entityStar As Integer
+        <XmlElement>
+        Public Property Synonyms As Synonyms()
+        <XmlElement>
+        Public Property IupacNames As Synonyms()
+        <XmlElement>
+        Public Property Citations As Synonyms()
+        Public Property Formulae As Formulae
+        ''' <summary>
+        ''' 次级编号，和主编号都代表同一样物质
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlElement>
+        Public Property SecondaryChEBIIds As String()
+        <XmlElement>
+        Public Property RegistryNumbers As RegistryNumbers()
+        <XmlElement>
+        Public Property ChemicalStructures As ChemicalStructures()
+        <XmlElement>
+        Public Property DatabaseLinks As DatabaseLinks()
+        <XmlElement>
+        Public Property OntologyParents As OntologyParents()
+        <XmlElement>
+        Public Property OntologyChildren As OntologyParents()
+        <XmlElement>
+        Public Property CompoundOrigins As CompoundOrigin()
+
+        Private Property Formula As String ' Implements IMolecule.Formula
+            Get
+                Return Formulae.data
+            End Get
+            Set(value As String)
+                Formulae.data = value
+            End Set
+        End Property
+    End Class
+
+    ''' <summary>
+    ''' working
+    ''' </summary>
+    Public Class ChEBIEntity ': Inherits XmlDataModel
         ' Implements INamedValue
         '  Implements IMolecule
 
