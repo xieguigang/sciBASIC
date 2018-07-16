755ade10acc602a6481fb641d9459eb915077610
xieguigang <xie.guigang@gcmodeller.org>
2018-07-16 16:55:57 +0800

    fix object reference errors

--- Data/DataFrame/Extensions/DataImports.vb
+++ Data/DataFrame/Extensions/DataImports.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::4f7afc044f26800ae0234fd60c64c4f2, Data\DataFrame\Extensions\DataImports.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module DataImports
-    ' 
-    '     Function: (+2 Overloads) [Imports], FixLengthImports, (+3 Overloads) ImportsData, ImportsTsv, (+2 Overloads) RowParsing
-    '               SampleForType
-    ' 
-    ' /********************************************************************************/
+' Module DataImports
+' 
+'     Function: (+2 Overloads) [Imports], FixLengthImports, (+3 Overloads) ImportsData, ImportsTsv, (+2 Overloads) RowParsing
+'               SampleForType
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -44,6 +44,7 @@ Imports System.Runtime.CompilerServices
 Imports System.Text
 Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Scripting.MetaData
@@ -150,7 +151,7 @@ Public Module DataImports
     ''' <param name="maps"></param>
     ''' <returns></returns>
     <Extension>
-    Public Function ImportsTsv(Of T As Class)(lines As IEnumerable(Of String), Optional maps As Dictionary(Of String, String) = Nothing) As T()
+    Public Function ImportsTsv(Of T As Class)(lines As IEnumerable(Of String), Optional maps As NameMapping = Nothing) As T()
         Return ImportsData(lines, ASCII.TAB) _
             .AsDataSource(Of T)(maps:=maps)
     End Function
--- Microsoft.VisualBasic.Core/ComponentModel/System.Collections.Generic/MapsHelper.vb
+++ Microsoft.VisualBasic.Core/ComponentModel/System.Collections.Generic/MapsHelper.vb
@@ -1,61 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::66f0b60ec1f735a22f5f3204e303ba2f, Microsoft.VisualBasic.Core\ComponentModel\System.Collections.Generic\MapsHelper.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Structure MapsHelper
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: GetValue, ToString
-    ' 
-    '     Structure Map
-    ' 
-    '         Properties: Key, Maps
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: ToString
-    '         Interface IMap
-    ' 
-    '             Properties: Key, Maps
-    ' 
-    ' 
-    ' 
-    '     Structure IDMap
-    ' 
-    '         Properties: Key, Maps
-    ' 
-    '         Function: ParseFromTsv, ParseTsvFile, ToString, TSV
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Structure MapsHelper
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: GetValue, ToString
+' 
+'     Structure Map
+' 
+'         Properties: Key, Maps
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: ToString
+'         Interface IMap
+' 
+'             Properties: Key, Maps
+' 
+' 
+' 
+'     Structure IDMap
+' 
+'         Properties: Key, Maps
+' 
+'         Function: ParseFromTsv, ParseTsvFile, ToString, TSV
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -71,15 +71,21 @@ Namespace ComponentModel
     ''' 其实这个对象就是字典查询的一个简化操作而已
     ''' </summary>
     ''' <typeparam name="T"></typeparam>
-    Public Class MapsHelper(Of T)
+    Public Class MapsHelper(Of T) : Implements IEnumerable(Of Map(Of String, T))
 
         Protected ReadOnly __default As T
-        Protected ReadOnly __maps As IReadOnlyDictionary(Of String, T)
+        Protected ReadOnly __maps As IDictionary(Of String, T)
 
         Default Public ReadOnly Property Value(key$) As T
             <MethodImpl(MethodImplOptions.AggressiveInlining)>
             Get
                 Return GetValue(key)
+            End Get
+        End Property
+
+        Public ReadOnly Property [Default] As T
+            Get
+                Return __default
             End Get
         End Property
 
@@ -98,7 +104,17 @@ Namespace ComponentModel
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Overrides Function ToString() As String
-            Return __maps.DictionaryData.GetJson
+            Return __maps.ToDictionary.GetJson
+        End Function
+
+        Public Iterator Function GetEnumerator() As IEnumerator(Of Map(Of String, T)) Implements IEnumerable(Of Map(Of String, T)).GetEnumerator
+            For Each tuple As KeyValuePair(Of String, T) In __maps
+                Yield New Map(Of String, T)(tuple.Key, tuple.Value)
+            Next
+        End Function
+
+        Private Iterator Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+            Yield GetEnumerator()
         End Function
     End Class
 
@@ -110,6 +126,10 @@ Namespace ComponentModel
                 dictionary Or New Dictionary(Of String, String)().AsDefault,
                 [default]
             )
+        End Sub
+
+        Public Sub Add(key$, map$)
+            Call __maps.Add(key, map)
         End Sub
 
         Public Shared Widening Operator CType(table$(,)) As NameMapping
