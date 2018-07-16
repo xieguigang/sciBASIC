16fc1e0179d99374311bee2b5781329df03a4367
xieguigang <xie.guigang@gcmodeller.org>
2018-07-16 21:32:08 +0800

    my namespace?

---
+++ Microsoft.VisualBasic.Core/My/Log4VB.vb
@@ -0,0 +1,9 @@
+﻿Namespace My
+
+    ''' <summary>
+    ''' VB.NET <see cref="Console"/> log framework.
+    ''' </summary>
+    Public Module Log4VB
+
+    End Module
+End Namespace
---
+++ vs_solutions/tutorials/core.test/unitTest.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Ranges
+
+Module unitTest
+
+    Sub Main()
+        Dim KB As New UnitValue(Of ByteSize)(128 * 1024 * 1024, ByteSize.KB)
+
+        Call KB.__DEBUG_ECHO
+        Call KB.Scale(ByteSize.GB).__DEBUG_ECHO
+        Call KB.Scale(ByteSize.B).__DEBUG_ECHO
+        Call KB.Scale(ByteSize.MB).__DEBUG_ECHO
+        Call KB.Scale(ByteSize.TB).__DEBUG_ECHO
+        Call KB.Scale(ByteSize.KB).__DEBUG_ECHO
+        Call KB.Scale(ByteSize.TB).Scale(ByteSize.MB).__DEBUG_ECHO
+
+        Pause()
+    End Sub
+End Module
