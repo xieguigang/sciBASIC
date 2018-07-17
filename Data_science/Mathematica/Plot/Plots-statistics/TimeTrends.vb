d3e4683f4328665139b74290d4822d9ab85bb21e
xieguigang <xie.guigang@gcmodeller.org>
2018-07-17 14:40:24 +0800

    add test data

---
+++ Data_science/Mathematica/Plot/Plots-statistics/TimeTrends.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports Microsoft.VisualBasic.Imaging.Driver
+
+Public Module TimeTrends
+
+    Public Structure TimePoint
+
+        Dim [date] As Date
+        Dim average As Double
+        ''' <summary>
+        ''' [min, max]
+        ''' </summary>
+        Dim range As DoubleRange
+
+        Public Overrides Function ToString() As String
+            Return $"<{[date].ToString}> {average} IN [{range.Min}, {range.Max}]"
+        End Function
+    End Structure
+
+    Public Function Plot() As GraphicsData
+
+    End Function
+End Module
