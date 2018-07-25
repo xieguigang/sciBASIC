31bf763c35e5c9fba452ca6f4ca2d50bbe39c5f0
xieguigang <xie.guigang@gcmodeller.org>
2018-07-25 16:27:13 +0800

    add namespace for plots

---
+++ Data_science/Mathematica/Plot/Plots-statistics/PCA/ScreePlot.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Imaging.Driver
+Imports PCA_analysis = Microsoft.VisualBasic.Math.LinearAlgebra.PCA
+
+Namespace PCA
+
+    ''' <summary>
+    ''' 碎石图
+    ''' </summary>
+    Public Module ScreePlot
+
+        <Extension>
+        Public Function Plot(pca As PCA_analysis) As GraphicsData
+
+        End Function
+    End Module
+End Namespace
