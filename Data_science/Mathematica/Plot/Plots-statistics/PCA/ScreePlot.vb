0447fc28dd301164101e88c56c3e27f8fbfd4259
xieguigang <xie.guigang@gcmodeller.org>
2018-07-25 16:44:12 +0800

    add xml code comments

--- Data_science/Mathematica/Plot/Plots-statistics/PCA/ScreePlot.vb
+++ Data_science/Mathematica/Plot/Plots-statistics/PCA/ScreePlot.vb
@@ -1,5 +1,12 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Axis
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
 Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.MIME.Markup.HTML.CSS
+Imports Microsoft.VisualBasic.Scripting.Runtime
 Imports PCA_analysis = Microsoft.VisualBasic.Math.LinearAlgebra.PCA
 
 Namespace PCA
@@ -10,8 +17,23 @@ Namespace PCA
     Public Module ScreePlot
 
         <Extension>
-        Public Function Plot(pca As PCA_analysis) As GraphicsData
+        Public Function Plot(pca As PCA_analysis,
+                             Optional size$ = "3300,2700",
+                             Optional margin$ = g.DefaultUltraLargePadding,
+                             Optional bg$ = "white",
+                             Optional lineStroke$ = Stroke.HighlightStroke,
+                             Optional pointSize! = 10,
+                             Optional title$ = "PCA ScreePlot") As GraphicsData
 
+            Dim cv As Vector = pca.CumulativeVariance
+            Dim X = (cv.Dim + 1).Sequence.AsVector.CreateAxisTicks
+            Dim Y = cv.CreateAxisTicks
+            Dim plotInternal =
+                Sub(ByRef g As IGraphics, region As GraphicsRegion)
+
+                End Sub
+
+            Return g.GraphicsPlots(size.SizeParser, margin, bg, plotInternal)
         End Function
     End Module
 End Namespace
