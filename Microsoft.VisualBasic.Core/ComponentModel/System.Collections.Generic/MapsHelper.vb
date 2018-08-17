cf76ed7b3fd83cfad39e1aec744230e05e46c229
xieguigang <xie.guigang@gcmodeller.org>
2018-08-17 10:05:19 +0800

    code style improvements

--- Microsoft.VisualBasic.Core/ComponentModel/System.Collections.Generic/MapsHelper.vb
+++ Microsoft.VisualBasic.Core/ComponentModel/System.Collections.Generic/MapsHelper.vb
@@ -1,74 +1,75 @@
 ﻿#Region "Microsoft.VisualBasic::3ad747902d7a001d492b7b8daa8b6b89, Microsoft.VisualBasic.Core\ComponentModel\System.Collections.Generic\MapsHelper.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class MapsHelper
-    ' 
-    '         Properties: [Default]
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: GetEnumerator, GetValue, IEnumerable_GetEnumerator, ToString
-    ' 
-    '     Class NameMapping
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Sub: Add
-    ' 
-    '     Structure Map
-    ' 
-    '         Properties: Key, Maps
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: ToString
-    '         Interface IMap
-    ' 
-    '             Properties: Key, Maps
-    ' 
-    ' 
-    ' 
-    '     Structure IDMap
-    ' 
-    '         Properties: Key, Maps
-    ' 
-    '         Function: ParseFromTsv, ParseTsvFile, ToString, TSV
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class MapsHelper
+' 
+'         Properties: [Default]
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: GetEnumerator, GetValue, IEnumerable_GetEnumerator, ToString
+' 
+'     Class NameMapping
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Sub: Add
+' 
+'     Structure Map
+' 
+'         Properties: Key, Maps
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: ToString
+'         Interface IMap
+' 
+'             Properties: Key, Maps
+' 
+' 
+' 
+'     Structure IDMap
+' 
+'         Properties: Key, Maps
+' 
+'         Function: ParseFromTsv, ParseTsvFile, ToString, TSV
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Language.Default
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.VisualBasic.Text
 
@@ -127,12 +128,11 @@ Namespace ComponentModel
 
     Public Class NameMapping : Inherits MapsHelper(Of String)
 
+        Shared ReadOnly emptyMaps As DefaultValue(Of Dictionary(Of String, String)) = New Dictionary(Of String, String)()
+
         Sub New(Optional dictionary As Dictionary(Of String, String) = Nothing,
                 Optional default$ = Nothing)
-            Call MyBase.New(
-                dictionary Or New Dictionary(Of String, String)().AsDefault,
-                [default]
-            )
+            Call MyBase.New(dictionary Or emptyMaps, [default])
         End Sub
 
         Public Sub Add(key$, map$)
