87f8bf962bca1a806c38a70bad9bef218516f5ad
xieguigang <xie.guigang@gcmodeller.org>
2018-09-30 16:45:06 +0800

    code style improvements

---
+++ Data_science/Mathematica/WaveletTransform/Constructors/WaveletConstructor.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Collections
+Imports System.Linq
+
+Public MustInherit Class WaveletConstructor : Implements IEnumerable(Of Wavelet)
+
+    ReadOnly wavelets As Wavelet()
+
+    Sub New(wavelets As IEnumerable(Of Wavelet))
+        Me.wavelets = wavelets.ToArray
+    End Sub
+
+    Public Function GetEnumerator() As IEnumerator(Of Wavelet) Implements IEnumerable(Of Wavelet).GetEnumerator
+        Throw New NotImplementedException()
+    End Function
+
+    Private Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+        Throw New NotImplementedException()
+    End Function
+End Class
