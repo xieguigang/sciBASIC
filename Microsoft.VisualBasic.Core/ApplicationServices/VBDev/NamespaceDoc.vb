b316d72c848aa46f91939b79504397bb4bf80111
xieguigang <xie.guigang@gcmodeller.org>
2018-10-06 02:14:10 +0800

    add namespace docs

---
+++ Microsoft.VisualBasic.Core/ApplicationServices/VBDev/NamespaceDoc.vb
@@ -0,0 +1,8 @@
+﻿Namespace ApplicationServices.Development
+
+    ''' <summary>
+    ''' The VisualBasic source code file related extension api module.
+    ''' </summary>
+    Friend Class NamespaceDoc
+    End Class
+End Namespace
