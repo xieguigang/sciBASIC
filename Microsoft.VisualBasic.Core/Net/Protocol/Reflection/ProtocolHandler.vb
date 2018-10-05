2bc9ca269b8a8a42baf5a3b7ab04c9a7a1ed0778
xieguigang <xie.guigang@gcmodeller.org>
2018-10-06 02:52:14 +0800

    code style improvements of the tcp protocol handler

--- Microsoft.VisualBasic.Core/Net/Protocol/Reflection/ProtocolHandler.vb
+++ Microsoft.VisualBasic.Core/Net/Protocol/Reflection/ProtocolHandler.vb
@@ -1,56 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::a13dbeea771851b44abef67ab08614a7, Microsoft.VisualBasic.Core\Net\Protocol\Reflection\ProtocolHandler.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class ProtocolHandler
-    ' 
-    '         Properties: DeclaringType, ProtocolEntry
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: GetMethod, HandlePush, HandleRequest, (+2 Overloads) SafelyCreateObject, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class ProtocolHandler
+' 
+'         Properties: DeclaringType, ProtocolEntry
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: GetMethod, HandlePush, HandleRequest, (+2 Overloads) SafelyCreateObject, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Net.Abstract
 Imports Microsoft.VisualBasic.Net.Http
 Imports Microsoft.VisualBasic.Net.Tcp
+Imports TcpEndPoint = System.Net.IPEndPoint
 
 Namespace Net.Protocols.Reflection
 
     ''' <summary>
     ''' 这个模块只处理<see cref="DataRequestHandler"/>类型的接口
+    ''' 
+    ''' ```vbnet
+    ''' <see cref="System.Delegate"/> Function(request As <see cref="RequestStream"/>, RemoteAddress As <see cref="TcpEndPoint"/>) As <see cref="RequestStream"/>
+    ''' ```
     ''' </summary>
     Public Class ProtocolHandler : Inherits IProtocolHandler
 
@@ -66,6 +73,11 @@ Namespace Net.Protocols.Reflection
             Return $"*{ProtocolEntry}   ---> {DeclaringType.FullName}  //{Protocols.Count} Protocols."
         End Function
 
+        Const AllInstanceMethod As BindingFlags =
+            BindingFlags.Public Or
+            BindingFlags.NonPublic Or
+            BindingFlags.Instance
+
         ''' <summary>
         ''' 请注意，假若没有在目标的类型定义之中查找出入口点的定义，则这个构造函数会报错，
         ''' 假若需要安全的创建对象，可以使用<see cref="ProtocolHandler.SafelyCreateObject(Of T)(T)"/>函数
@@ -79,17 +91,18 @@ Namespace Net.Protocols.Reflection
             Me.ProtocolEntry = entry?.EntryPoint
 
             ' 解析出所有符合 WrapperClassTools.Net.DataRequestHandler 接口类型的函数方法
-            Dim Methods = type.GetMethods(System.Reflection.BindingFlags.Public Or
-                                          System.Reflection.BindingFlags.NonPublic Or
-                                          System.Reflection.BindingFlags.Instance)
-            Dim LQuery = (From entryPoint In Methods.AsParallel
+            Dim Methods = type.GetMethods(bindingAttr:=AllInstanceMethod)
+            Dim LQuery = (From entryPoint As MethodInfo
+                          In Methods
                           Let Protocol As Protocol = Protocol.GetEntryPoint(entryPoint)
                           Let method As DataRequestHandler = GetMethod(obj, entryPoint)
                           Where Not (Protocol Is Nothing) AndAlso
                               Not method Is Nothing
                           Select Protocol, entryPoint, method)
 
-            Me.Protocols = LQuery.ToDictionary(Function(element) element.Protocol.EntryPoint,
+            Me.Protocols = LQuery.ToDictionary(Function(element)
+                                                   Return element.Protocol.EntryPoint
+                                               End Function,
                                                Function(element) element.method)
         End Sub
 
@@ -115,6 +128,7 @@ Namespace Net.Protocols.Reflection
             End Try
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Function HandlePush(uid As Long, request As RequestStream) As RequestStream
             Return HandleRequest(request, Nothing)
         End Function
@@ -125,7 +139,7 @@ Namespace Net.Protocols.Reflection
         ''' <param name="request">The request stream object which contains the commands from the client</param>
         ''' <param name="remoteDevcie">The IPAddress of the target incoming client data request.</param>
         ''' <returns></returns>
-        Public Overrides Function HandleRequest(request As RequestStream, remoteDevcie As System.Net.IPEndPoint) As RequestStream
+        Public Overrides Function HandleRequest(request As RequestStream, remoteDevcie As TcpEndPoint) As RequestStream
             If request.ProtocolCategory <> Me.ProtocolEntry Then
 #If DEBUG Then
                 Call $"Protocol_entry:={request.ProtocolCategory} was not found!".__DEBUG_ECHO
@@ -137,7 +151,8 @@ Namespace Net.Protocols.Reflection
 #If DEBUG Then
                 Call $"Protocol:={request.Protocol} was not found!".__DEBUG_ECHO
 #End If
-                Return NetResponse.RFC_NOT_IMPLEMENTED  ' 没有找到相对应的协议处理逻辑，则没有实现相对应的数据协议处理方法
+                ' 没有找到相对应的协议处理逻辑，则没有实现相对应的数据协议处理方法
+                Return NetResponse.RFC_NOT_IMPLEMENTED
             End If
 
             Dim EntryPoint As DataRequestHandler = Me.Protocols(request.Protocol)
@@ -145,51 +160,56 @@ Namespace Net.Protocols.Reflection
             Return value
         End Function
 
-        Private Shared Function GetMethod(obj As Object, entryPoint As System.Reflection.MethodInfo) As Net.Abstract.DataRequestHandler
-            Dim parameters As System.Reflection.ParameterInfo() = entryPoint.GetParameters
+        Private Shared Function GetMethod(obj As Object, entryPoint As MethodInfo) As DataRequestHandler
+            Dim parameters As ParameterInfo() = entryPoint.GetParameters
 
             If Not entryPoint.ReturnType.Equals(GetType(RequestStream)) Then
                 Return Nothing
-            End If
-
-            If parameters.Length > 3 Then
+            ElseIf parameters.Length > 3 Then
                 Return Nothing
             End If
 
             If parameters.Length = 0 Then
-                Return AddressOf New __protocolInvoker(obj, entryPoint).InvokeProtocol0
-            End If
-
-            If parameters.Length = 1 Then
-                If Not parameters.First.ParameterType.Equals(GetType(Long)) Then
-                    Return Nothing
-                End If
-
-                Return AddressOf New __protocolInvoker(obj, entryPoint).InvokeProtocol1
-            End If
-
-            If parameters.Length = 2 Then
-                If (Not parameters.First.ParameterType.Equals(GetType(Long)) OrElse
-                Not parameters.Last.ParameterType.Equals(GetType(RequestStream))) Then
-                    Return Nothing
-                End If
-
-                Return AddressOf New __protocolInvoker(obj, entryPoint).InvokeProtocol2
-            End If
-
-            If parameters.Length = 3 Then
-                If (Not parameters.First.ParameterType.Equals(GetType(Long)) OrElse
-                Not parameters(1).ParameterType.Equals(GetType(RequestStream)) OrElse
-                Not parameters.Last.ParameterType.Equals(GetType(System.Net.IPEndPoint))) Then
-                    Return Nothing
-                End If
-
-                Return AddressOf New __protocolInvoker(obj, entryPoint).InvokeProtocol3
+                Return AddressOf New ProtocolInvoker(obj, entryPoint).InvokeProtocol0
+            ElseIf parameters.Length = 1 Then
+                Return method1(obj, entryPoint, parameters)
+            ElseIf parameters.Length = 2 Then
+                Return method2(obj, entryPoint, parameters)
+            ElseIf parameters.Length = 3 Then
+                Return method3(obj, entryPoint, parameters)
             End If
 
             Return Nothing
         End Function
 
+        Private Shared Function method3(obj As Object, entryPoint As MethodInfo, parameters As ParameterInfo()) As DataRequestHandler
+            If (Not parameters.First.ParameterType.Equals(GetType(Long)) OrElse
+                Not parameters(1).ParameterType.Equals(GetType(RequestStream)) OrElse
+                Not parameters.Last.ParameterType.Equals(GetType(TcpEndPoint))) Then
+                Return Nothing
+            Else
+                Return AddressOf New ProtocolInvoker(obj, entryPoint).InvokeProtocol3
+            End If
+        End Function
+
+        Private Shared Function method2(obj As Object, entryPoint As MethodInfo, parameters As ParameterInfo()) As DataRequestHandler
+            If (Not parameters.First.ParameterType.Equals(GetType(Long)) OrElse
+                Not parameters.Last.ParameterType.Equals(GetType(RequestStream))) Then
+                Return Nothing
+            Else
+                Return AddressOf New ProtocolInvoker(obj, entryPoint).InvokeProtocol2
+            End If
+        End Function
+
+        Private Shared Function method1(obj As Object, entryPoint As MethodInfo, parameters As ParameterInfo()) As DataRequestHandler
+            If Not parameters.First.ParameterType.Equals(GetType(Long)) Then
+                Return Nothing
+            Else
+                Return AddressOf New ProtocolInvoker(obj, entryPoint).InvokeProtocol1
+            End If
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Shared Narrowing Operator CType(handler As ProtocolHandler) As DataRequestHandler
             Return AddressOf handler.HandleRequest
         End Operator
--- Microsoft.VisualBasic.Core/Net/Protocol/Reflection/ProtocolInvoker.vb
+++ Microsoft.VisualBasic.Core/Net/Protocol/Reflection/ProtocolInvoker.vb
@@ -1,43 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::e44aee8874f54cb5fde833f9594f3e66, Microsoft.VisualBasic.Core\Net\Protocol\Reflection\ProtocolInvoker.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class __protocolInvoker
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: InvokeProtocol0, InvokeProtocol1, InvokeProtocol2, InvokeProtocol3, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class __protocolInvoker
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: InvokeProtocol0, InvokeProtocol1, InvokeProtocol2, InvokeProtocol3, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -47,29 +47,30 @@ Imports Microsoft.VisualBasic.Win32
 
 Namespace Net.Protocols.Reflection
 
-    Friend Class __protocolInvoker
+    Friend Class ProtocolInvoker
 
-        ReadOnly obj As Object, Method As MethodInfo
+        ReadOnly obj As Object
+        ReadOnly method As MethodInfo
 
-        Sub New(obj As Object, Method As MethodInfo)
+        Sub New(obj As Object, method As MethodInfo)
             Me.obj = obj
-            Me.Method = Method
+            Me.method = method
         End Sub
 
         Public Function InvokeProtocol0(request As RequestStream, remoteDevice As System.Net.IPEndPoint) As RequestStream
-            Dim value = Method.Invoke(obj, Nothing)
+            Dim value = method.Invoke(obj, Nothing)
             Dim data = DirectCast(value, RequestStream)
             Return data
         End Function
 
         Public Function InvokeProtocol1(request As RequestStream, remoteDevice As System.Net.IPEndPoint) As RequestStream
-            Dim value = Method.Invoke(obj, {})
+            Dim value = method.Invoke(obj, {})
             Dim data = DirectCast(value, RequestStream)
             Return data
         End Function
 
         Public Function InvokeProtocol2(request As RequestStream, remoteDevice As System.Net.IPEndPoint) As RequestStream
-            Dim value = Method.Invoke(obj, {request})
+            Dim value = method.Invoke(obj, {request})
             Dim data = DirectCast(value, RequestStream)
             Return data
         End Function
@@ -82,11 +83,11 @@ Namespace Net.Protocols.Reflection
         ''' <returns></returns>
         Public Function InvokeProtocol3(request As RequestStream, remoteDevice As System.Net.IPEndPoint) As RequestStream
             Try
-                Dim value = Method.Invoke(obj, {request, remoteDevice})
+                Dim value = method.Invoke(obj, {request, remoteDevice})
                 Dim data = DirectCast(value, RequestStream)
                 Return data
             Catch ex As Exception
-                ex = New Exception(Method.FullName, ex)
+                ex = New Exception(method.FullName, ex)
 
                 If WindowsServices.Initialized Then
                     Call ServicesLogs.LogException(ex)
@@ -98,7 +99,7 @@ Namespace Net.Protocols.Reflection
         End Function
 
         Public Overrides Function ToString() As String
-            Return $"{obj.ToString} -> {Method.Name}"
+            Return $"{obj.ToString} -> {method.Name}"
         End Function
     End Class
 End Namespace
