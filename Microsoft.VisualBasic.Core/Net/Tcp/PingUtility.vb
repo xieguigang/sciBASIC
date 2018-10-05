08aa5cf0b82d122559c6dd6f6c1b4bd8e16aa20e
xieguigang <xie.guigang@gcmodeller.org>
2018-10-05 23:37:27 +0800

    code style improvememnts

--- Microsoft.VisualBasic.Core/Net/Tcp/PingUtility.vb
+++ Microsoft.VisualBasic.Core/Net/Tcp/PingUtility.vb
@@ -1,60 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::7425feb3762a22b9565e8163e3eca1d8, Microsoft.VisualBasic.Core\ApplicationServices\Tools\Network\Tcp\PingUtility.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module PingUtility
-    ' 
-    ' 
-    '         Enum ConnectionState
-    ' 
-    ' 
-    ' 
-    ' 
-    '  
-    ' 
-    '     Function: InternetGetConnectedState, IsOffline, Ping
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module PingUtility
+' 
+' 
+'         Enum ConnectionState
+' 
+' 
+' 
+' 
+'  
+' 
+'     Function: InternetGetConnectedState, IsOffline, Ping
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Collections.Generic
-Imports System.Text
+Imports System.IO
 Imports System.Net
 Imports System.Net.NetworkInformation
+Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
-Imports System.Threading
-Imports System.IO
+Imports System.Text
+Imports Microsoft.VisualBasic.Language
 
 Namespace Net
 
@@ -78,7 +78,8 @@ Namespace Net
             INTERNET_CONNECTION_CONFIGURED = &H40
         End Enum
 
-        <DllImport("wininet", CharSet:=CharSet.Auto)> Public Function InternetGetConnectedState(ByRef lpdwFlags As ConnectionState, dwReserved As Integer) As Boolean
+        <DllImport("wininet", CharSet:=CharSet.Auto)>
+        Public Function InternetGetConnectedState(ByRef lpdwFlags As ConnectionState, dwReserved As Integer) As Boolean
         End Function
 
         Public Function IsOffline() As Boolean
@@ -94,70 +95,40 @@ Namespace Net
         ''' <summary>
         ''' 返回与目标远程机器之间的平均通信时间长度
         ''' </summary>
-        ''' <param name="IP"></param>
+        ''' <param name="ip"></param>
         ''' <param name="out">Default is console.</param>
+        ''' <param name="nTimes">By default is ping 4 times</param>
         ''' <returns></returns>
         ''' <remarks></remarks>
-        Public Function Ping(IP As IPAddress, Optional timeOut As UInteger = 3000, Optional out As StreamWriter = Nothing) As Double
-            'set options ttl=128 and no fragmentation
+        Public Function Ping(ip As IPAddress,
+                             Optional timeOut As UInteger = 3000,
+                             Optional out As TextWriter = Nothing,
+                             Optional nTimes% = 4,
+                             Optional displayEcho As Boolean = True) As Double
+
+            ' set options ttl=128 and no fragmentation
             Dim options As New PingOptions(128, True)
-
-            'create a Ping object
-            Dim pingTools As New System.Net.NetworkInformation.Ping()
-
-            '32 empty bytes buffer
+            ' create a Ping object
+            Dim pingTools As New Ping()
+            ' 32 empty bytes buffer
             Dim data As Byte() = New Byte(31) {}
-
             Dim received As Integer = 0
             Dim responseTimes As New List(Of Long)()
 
-            If out Is Nothing Then
-                out = New StreamWriter(Console.OpenStandardOutput)
-            End If
+            out = out Or App.StdOut
 
-            'ping 4 times
-            For i As Integer = 0 To 3
-                Dim reply As PingReply
+            For i As Integer = 0 To nTimes - 1
+                With pingTools.PingOnce(ip, timeOut, data, options)
+                    responseTimes += .responseTime
+                    received += .success
 
-                Try
-                    reply = pingTools.Send(IP, timeOut, data, options)
-                Catch ex As Exception
-                    received += 1
-                    responseTimes.Add(10 * 1000)
-                    Continue For
-                End Try
-
-                If reply Is Nothing Then
-                    Call "Ping failed for an unknown reason".__DEBUG_ECHO
-                    Continue For
-                End If
-
-                Dim msg$
-
-                Select Case reply.Status
-
-                    Case IPStatus.Success
-                        If reply.Options Is Nothing Then
-                            msg = $"Reply from {reply.Address}: bytes={reply.Buffer.Length} time={reply.RoundtripTime}ms"
-                        Else
-                            msg = $"Reply from {reply.Address}: bytes={reply.Buffer.Length} time={reply.RoundtripTime}ms TTL={reply.Options.Ttl}"
-                        End If
-
-                        received += 1
-                        responseTimes.Add(reply.RoundtripTime)
-
-                    Case IPStatus.TimedOut
-                        msg = "Request timed out."
-
-                    Case Else
-                        msg = $"Ping failed {reply.Status.ToString}"
-
-                End Select
-
-                Call out.WriteLine(msg$)
+                    If displayEcho Then
+                        Call out.WriteLine(.Msg)
+                    End If
+                End With
             Next
 
-            'statistics calculations
+            ' statistics calculations
             Dim averageTime As Long = -1
             Dim minimumTime As Long = 0
             Dim maximumTime As Long = 0
@@ -174,18 +145,72 @@ Namespace Net
                         minimumTime = responseTimes(i)
                     End If
                 End If
+
                 averageTime += responseTimes(i)
             Next
 
+            If displayEcho Then
+                Call out.Print(minimumTime, maximumTime, received, averageTime, ip)
+            End If
+
+            If received <= 0 Then
+                ' Ping不通
+                Return Double.MaxValue
+            Else
+                Return averageTime \ received
+            End If
+        End Function
+
+        <Extension>
+        Private Function PingOnce(pingTool As Ping, ip As IPAddress, timeOut As UInt32,
+                                  data As Byte(),
+                                  options As PingOptions) As (success%, responseTime&, Msg$)
+            Dim reply As PingReply = Nothing
+            Dim received%
+
+            Try
+                reply = pingTool.Send(ip, timeOut, data, options)
+            Catch ex As Exception
+                Return (0, 10 * 1000, "Error!")
+            End Try
+
+            If reply Is Nothing Then
+                Return (0, 10 * 1000, "Error!")
+            End If
+
+            Dim responseTime&
+            Dim msg$
+
+            Select Case reply.Status
+                Case IPStatus.Success
+                    If reply.Options Is Nothing Then
+                        msg = $"Reply from {reply.Address}: bytes={reply.Buffer.Length} time={reply.RoundtripTime}ms"
+                    Else
+                        msg = $"Reply from {reply.Address}: bytes={reply.Buffer.Length} time={reply.RoundtripTime}ms TTL={reply.Options.Ttl}"
+                    End If
+
+                    received = 1
+                    responseTime = reply.RoundtripTime
+                Case IPStatus.TimedOut
+                    msg = "Request timed out."
+                Case Else
+                    msg = $"Ping failed {reply.Status.ToString}"
+            End Select
+
+            Return (received, responseTime, msg)
+        End Function
+
+        <Extension>
+        Private Sub Print(out As TextWriter, minimumTime&, maximumTime&, received%, averageTime&, ip As IPAddress)
             Dim statistics As New StringBuilder()
-            statistics.AppendFormat("Ping statistics for {0}:", IP.ToString())
+            statistics.AppendFormat("Ping statistics for {0}:", ip.ToString())
             statistics.AppendLine()
             statistics.AppendFormat("   Packets: Sent = 4, Received = {0}, Lost = {1} <{2}% loss>,", received, 4 - received, Convert.ToInt32(((4 - received) * 100) \ 4))
             statistics.AppendLine()
             statistics.Append("Approximate round trip times in milli-seconds:")
             statistics.AppendLine()
 
-            'show only if loss is not 100%
+            ' show only if loss is not 100%
             If averageTime <> -1 Then
                 statistics.AppendFormat("    Minimum = {0}ms, Maximum = {1}ms, Average = {2}ms", minimumTime, maximumTime, CLng(averageTime \ received))
             End If
@@ -193,12 +218,6 @@ Namespace Net
             out.WriteLine()
             out.WriteLine(statistics.ToString())
             out.WriteLine()
-
-            If received <= 0 Then  'Ping不通
-                Return Double.MaxValue
-            End If
-
-            Return averageTime \ received
-        End Function
+        End Sub
     End Module
 End Namespace
