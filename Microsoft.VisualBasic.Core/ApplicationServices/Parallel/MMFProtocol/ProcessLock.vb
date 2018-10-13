0c2dcb7931c9bccaa70a8957ac718c02b1db90ac
xieguigang <xie.guigang@gcmodeller.org>
2018-10-13 21:36:26 +0800

    fix namespace imports

--- Microsoft.VisualBasic.Core/ApplicationServices/Parallel/MMFProtocol/ProcessLock.vb
+++ Microsoft.VisualBasic.Core/ApplicationServices/Parallel/MMFProtocol/ProcessLock.vb
@@ -1,49 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::2313a1f38dd64df2ff7c3f7d68db61cd, Microsoft.VisualBasic.Core\ApplicationServices\Parallel\MMFProtocol\ProcessLock.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class ProcessLock
-    ' 
-    '         Properties: Locked
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Sub: (+2 Overloads) Dispose, ProcessLockDataArrival
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class ProcessLock
+' 
+'         Properties: Locked
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Sub: (+2 Overloads) Dispose, ProcessLockDataArrival
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Text.Encoding
+Imports System.Threading
 
 Namespace Parallel.MMFProtocol
 
@@ -51,7 +52,7 @@ Namespace Parallel.MMFProtocol
     ''' 进程排斥锁
     ''' </summary>
     ''' <remarks></remarks>
-    Public Class ProcessLock : Implements System.IDisposable
+    Public Class ProcessLock : Implements IDisposable
 
         Private Const PROCESS_CHECKOUT_SIGNAL As String = "processlock-checkout-signal::[Guid('4CBC086D-179C-494E-81C6-A040667B49F0')]"
         Private Const PROCESS_LOCKED___SIGNAL As String = "processlock-checkout-locked::[Guid('4CBC086D-179C-494E-81C6-A040667B49F0')]"
@@ -101,7 +102,7 @@ Namespace Parallel.MMFProtocol
             Get
                 'socket会首先尝试发送一个信号，然后根据有无响应来判断是否还有其他的服务器进程的运行
                 Call ProcessLock.SendMessage(Unicode.GetBytes(PROCESS_CHECKOUT_SIGNAL))
-                Call System.Threading.Thread.Sleep(100)       '等待响应的超时时间为100ms
+                Call Thread.Sleep(100)       '等待响应的超时时间为100ms
 
                 Return f_ProcessLock
             End Get
