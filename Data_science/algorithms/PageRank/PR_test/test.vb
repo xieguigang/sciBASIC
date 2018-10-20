ec9d144ba48d51fc1de6d8c70bbed078bd21f542
xieguigang <xie.guigang@gcmodeller.org>
2018-10-20 15:29:02 +0800

    update source file header

--- Data_science/algorithms/PageRank/PR_test/test.vb
+++ Data_science/algorithms/PageRank/PR_test/test.vb
@@ -1,41 +1,41 @@
-﻿#Region "Microsoft.VisualBasic::0014aa89565d07e55d9bd84661e82a21, Data_science\algorithms\PageRank\PR_test\test.vb"
+﻿#Region "Microsoft.VisualBasic::efab0a3b492516d6b494387cb3a1e2f1, Data_science\algorithms\PageRank\PR_test\test.vb"
 
-' Author:
-' 
-'       asuka (amethyst.asuka@gcmodeller.org)
-'       xie (genetics@smrucc.org)
-'       xieguigang (xie.guigang@live.com)
-' 
-' Copyright (c) 2018 GPL3 Licensed
-' 
-' 
-' GNU GENERAL PUBLIC LICENSE (GPL3)
-' 
-' 
-' This program is free software: you can redistribute it and/or modify
-' it under the terms of the GNU General Public License as published by
-' the Free Software Foundation, either version 3 of the License, or
-' (at your option) any later version.
-' 
-' This program is distributed in the hope that it will be useful,
-' but WITHOUT ANY WARRANTY; without even the implied warranty of
-' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-' GNU General Public License for more details.
-' 
-' You should have received a copy of the GNU General Public License
-' along with this program. If not, see <http://www.gnu.org/licenses/>.
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xie (genetics@smrucc.org)
+    '       xieguigang (xie.guigang@live.com)
+    ' 
+    ' Copyright (c) 2018 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-' /********************************************************************************/
+    ' /********************************************************************************/
 
-' Summaries:
+    ' Summaries:
 
-' Module test
-' 
-'     Sub: Main, Test_weightedPR, TestPageRank
-' 
-' /********************************************************************************/
+    ' Module test
+    ' 
+    '     Sub: Main, Test_weightedPR, TestPageRank
+    ' 
+    ' /********************************************************************************/
 
 #End Region
 
