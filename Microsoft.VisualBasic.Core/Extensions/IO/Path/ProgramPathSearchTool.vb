1d6be490f4b66b7c1f2db0283a7ea864c0a628f9
xieguigang <xie.guigang@gcmodeller.org>
2018-10-23 09:38:12 +0800

    code styles improvements for file search

--- Microsoft.VisualBasic.Core/Extensions/IO/Path/ProgramPathSearchTool.vb
+++ Microsoft.VisualBasic.Core/Extensions/IO/Path/ProgramPathSearchTool.vb
@@ -62,86 +62,92 @@ Namespace FileIO.Path
         ''' <remarks></remarks>
         '''
         <ExportAPI("Search.Scripts", Info:="Search for the path of a script file with a specific extension name.")>
-        Public Iterator Function SearchScriptFile(dir$, keyword$, Optional withExtension$ = "") As IEnumerable(Of String)
+        Public Function SearchScriptFile(dir$, keyword$, Optional withExtension$ = Nothing) As IEnumerable(Of String)
             Dim scriptFileNameRule$ = $"*{keyword}*{withExtension}"
-            Dim files = FileSystem.GetFiles(dir, TopDirectory, scriptFileNameRule)
+            Dim extNameAssert As Assert(Of String)
+
+            If withExtension.StringEmpty Then
+                extNameAssert = Function(path) path.ExtensionSuffix.StringEmpty
+            Else
+                extNameAssert = Function(path) True
+            End If
+
+            Return searchImpl(dir, {scriptFileNameRule}).Where(Function(file) extNameAssert(file))
+        End Function
+
+        Private Shared Iterator Function searchImpl(dir$, rules$()) As IEnumerable(Of String)
+            Dim files = FileSystem.GetFiles(dir, TopDirectory, rules)
             Dim binDIR As String = $"{dir}/bin/"
             Dim programDIR As String = $"{dir}/Program"
             Dim scriptsDIR As String = $"{dir}/scripts"
-            Dim testExtensionName As Assert(Of String)
-
-            If withExtension.StringEmpty Then
-                testExtensionName = Function(path) path.ExtensionSuffix.StringEmpty
-            Else
-                testExtensionName = Function(path) True
-            End If
 
             For Each folder As String In {binDIR, programDIR, scriptsDIR}
                 If FileSystem.DirectoryExists(folder) Then
-                    For Each file As String In ls - l - scriptFileNameRule <= folder
-                        If testExtensionName(file) Then
-                            Yield file
-                        End If
+                    For Each file As String In ls - l - rules <= folder
+                        Yield file
                     Next
                 End If
             Next
 
             For Each file As String In files
-                If testExtensionName(file) Then
-                    Yield file
-                End If
+                Yield file
             Next
         End Function
 
         ''' <summary>
-        '''
+        ''' Search for the directories which its name was matched the keyword pattern.
         ''' </summary>
-        ''' <param name="SpecificDrive">所制定进行搜索的驱动器，假若希望搜索整个硬盘，请留空字符串</param>
+        ''' <param name="SpecificDrive">所指定进行搜索的驱动器，假若希望搜索整个硬盘，请留空字符串</param>
         ''' <returns></returns>
         ''' <remarks></remarks>
         '''
         <ExportAPI("DIR.Search.Program_Directory",
                    Info:="Search for the directories which its name was matched the keyword pattern.")>
-        Public Function SearchDirectory(Keyword As String, SpecificDrive As String) As String()
-            Dim Drives As ReadOnlyCollection(Of DriveInfo) =
-                If(String.IsNullOrEmpty(SpecificDrive),
-                   FileIO.FileSystem.Drives,
-                   New ReadOnlyCollection(Of IO.DriveInfo)(
-                       {FileIO.FileSystem.GetDriveInfo(SpecificDrive)}))
-            Dim DIRs As New List(Of String)
+        Iterator Public Function SearchDirectory(keyword$, Optional specificDrive$ = Nothing) As IEnumerable(Of String)
+            Dim drives As ReadOnlyCollection(Of DriveInfo)
 
-            For Each Drive As DriveInfo In Drives
-                DIRs += SearchDrive(Drive, Keyword)
+            If String.IsNullOrEmpty(specificDrive) Then
+                drives = FileSystem.Drives
+            Else
+                drives = New ReadOnlyCollection(Of DriveInfo)({FileSystem.GetDriveInfo(specificDrive)})
+            End If
+
+            For Each drive As DriveInfo In drives
+                For Each dir As String In SearchDrive(drive, keyword)
+                    Yield dir
+                Next
             Next
-
-            Return DIRs.ToArray
         End Function
 
-        Private Function SearchDrive(Drive As DriveInfo, keyword As String) As String()
-            If Not Drive.IsReady Then
+        Private Function SearchDrive(drive As DriveInfo, keyword As String) As String()
+            If Not drive.IsReady Then
                 Return New String() {}
             End If
 
-            Dim driveName$ = Drive.RootDirectory.FullName
-            Dim DriveRoot = FileSystem.GetDirectories(driveName, SearchOption.SearchTopLevelOnly, keyword)
+            Dim driveName$ = drive.RootDirectory.FullName
+            Dim driveRoot = FileSystem.GetDirectories(driveName, SearchOption.SearchTopLevelOnly, keyword)
             Dim files As New List(Of String)
-            Dim ProgramFiles As String = String.Format("{0}/Program Files", Drive.RootDirectory.FullName)
-            If FileIO.FileSystem.DirectoryExists(ProgramFiles) Then
+            Dim ProgramFiles As String = String.Format("{0}/Program Files", drive.RootDirectory.FullName)
+
+            If FileSystem.DirectoryExists(ProgramFiles) Then
                 Call files.AddRange(BranchRule(ProgramFiles, keyword))
             End If
 
-            Dim ProgramFilesX86 = String.Format("{0}/Program Files(x86)", Drive.RootDirectory.FullName)
-            If FileIO.FileSystem.DirectoryExists(ProgramFilesX86) Then
+            Dim ProgramFilesX86 = String.Format("{0}/Program Files(x86)", drive.RootDirectory.FullName)
+
+            If FileSystem.DirectoryExists(ProgramFilesX86) Then
                 Call files.AddRange(BranchRule(ProgramFilesX86, keyword))
             End If
-            Call files.AddRange(DriveRoot)
-            Call files.AddRange(DriveRoot.Select(Function(rootDir) BranchRule(rootDir, keyword)).Unlist)
+
+            Call files.AddRange(driveRoot)
+            Call files.AddRange(driveRoot.Select(Function(rootDir) BranchRule(rootDir, keyword)).Unlist)
 
             Return files.ToArray
         End Function
 
         ''' <summary>
-        ''' Invoke the search session for the program file using a specific keyword string value.(使用某个关键词来搜索目标应用程序)
+        ''' Invoke the search session for the program file using a specific keyword string value.
+        ''' (使用某个关键词来搜索目标应用程序)
         ''' </summary>
         ''' <param name="DIR"></param>
         ''' <param name="Keyword"></param>
@@ -149,31 +155,11 @@ Namespace FileIO.Path
         ''' <remarks></remarks>
         <ExportAPI("File.Search.Program",
                    Info:="Invoke the search session for the program file using a specific keyword string value.")>
-        Public Function SearchProgram(DIR As String, Keyword As String) As String()
-            Dim ExeNameRule As String = String.Format("*{0}*.exe", Keyword)
-            Dim DllNameRule As String = String.Format("*{0}*.dll", Keyword)
+        Public Function SearchProgram(Dir$, keyword$) As String()
+            Dim exeNameRule As String = $"*{keyword}*.exe"
+            Dim dllNameRule As String = $"*{keyword}*.dll"
 
-            Dim Files = FileIO.FileSystem.GetFiles(DIR, FileIO.SearchOption.SearchTopLevelOnly, ExeNameRule, DllNameRule)
-            Dim binDIR As String = String.Format("{0}/bin/", DIR)
-            Dim ProgramDIR As String = String.Format("{0}/Program", DIR)
-            Dim buffer As New List(Of String)
-
-            If FileIO.FileSystem.DirectoryExists(binDIR) Then
-                buffer += FileIO.FileSystem.GetFiles(
-                    binDIR,
-                    FileIO.SearchOption.SearchTopLevelOnly,
-                    ExeNameRule, DllNameRule)
-            End If
-            If FileIO.FileSystem.DirectoryExists(ProgramDIR) Then
-                buffer += FileIO.FileSystem.GetFiles(
-                    ProgramDIR,
-                    FileIO.SearchOption.SearchTopLevelOnly,
-                    ExeNameRule, DllNameRule)
-            End If
-
-            buffer += Files
-
-            Return buffer.ToArray
+            Return searchImpl(Dir, {exeNameRule, dllNameRule})
         End Function
     End Class
 End Namespace
