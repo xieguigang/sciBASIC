7bf49e5b1a97ef4d0e458856073f495fa7f0e87c
xieguigang <xie.guigang@gcmodeller.org>
2018-11-08 10:38:18 +0800

    file components

---
+++ mime/application%netcdf/Components/Header.vb
@@ -0,0 +1,77 @@
+﻿Imports Microsoft.VisualBasic.Data.IO
+
+''' <summary>
+''' Object with the fields:
+''' 
+'''  + `recordDimension` Number with the length of record dimension
+'''  + `dimensions`: List of dimensions
+'''  + `globalAttributes`: List of global attributes
+'''  + `variables`: List of variables
+'''  
+''' </summary>
+Public Class Header
+
+#Region "Grammar constants"
+    Const ZERO = 0
+    Const NC_DIMENSION = 10
+    Const NC_VARIABLE = 11
+    Const NC_ATTRIBUTE = 12
+#End Region
+
+    ''' <summary>
+    ''' Number with the length of record dimension
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property recordDimension As Integer
+    ''' <summary>
+    ''' List of dimensions
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property dimensions
+    ''' <summary>
+    ''' List of global attributes
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property globalAttributes
+    ''' <summary>
+    ''' List of variables
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property variables
+    Public Property version As Byte
+
+    ''' <summary>
+    ''' Read the header of the file
+    ''' </summary>
+    ''' <param name="buffer">Buffer for the file data</param>
+    ''' <param name="version">Version of the file</param>
+    Sub New(buffer As BinaryDataReader, version As Byte)
+        ' Length of record dimension
+        ' sum of the varSize's of all the record variables.
+        Me.recordDimension = buffer.ReadUInt32
+        Me.version = version
+
+        ' List of dimensions
+    End Sub
+
+    Const NC_UNLIMITED = 0
+
+    ''' <summary>
+    ''' List of dimensions
+    ''' </summary>
+    ''' <param name="buffer">Buffer for the file data</param>
+    ''' <returns>
+    ''' Ojbect containing the following properties:
+    ''' 
+    '''  + `dimensions` that Is an array of dimension object
+    '''  + `name` String with the name of the dimension
+    '''  + `size`: Number with the size of the dimension dimensions: dimensions
+    '''  + `recordId`: the id Of the dimension that has unlimited size Or undefined,
+    '''  + `recordName`: name of the dimension that has unlimited size
+    '''  
+    ''' </returns>
+    Public Function dimensionsList(buffer As BinaryDataReader)
+
+    End Function
+
+End Class
---
+++ mime/application%netcdf/netCDFReader.vb
@@ -0,0 +1,62 @@
+﻿#Region "Microsoft.VisualBasic::744d821b68c4d06cbdde77dd4d9e2937, mime\application%netcdf\netCDF.vb"
+
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+
+' /********************************************************************************/
+
+' Summaries:
+
+' Class netCDF
+' 
+' 
+' 
+' /********************************************************************************/
+
+#End Region
+
+Imports Microsoft.VisualBasic.Data.IO
+Imports Microsoft.VisualBasic.Language
+
+Public Class netCDFReader
+
+    Dim buffer As BinaryDataReader
+    Dim header
+
+    Const Magic$ = "CDF"
+
+    Sub New(buffer As BinaryDataReader)
+        Dim version As Value(Of Byte) = Scan0
+
+        buffer.ByteOrder = ByteOrder.BigEndian
+        ' Test if file in support format
+        Utils.notNetcdf(buffer.ReadString(3) <> Magic, $"should start with {Magic}")
+        Utils.notNetcdf((version = buffer.ReadByte) > 2, "unknown version")
+
+    End Sub
+
+End Class
