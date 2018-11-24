d48bd498a6b4ec6e5a7ee263ac172002d988ca68
xieguigang <xie.guigang@gcmodeller.org>
2018-11-24 15:39:14 +0800

    add xml code comments for interface methods

--- Microsoft.VisualBasic.Core/Extensions/Doc/LargeTextFile.vb
+++ Microsoft.VisualBasic.Core/Extensions/Doc/LargeTextFile.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::6084412ffb4bd5143038ed15f4aa6dff, Microsoft.VisualBasic.Core\Extensions\Doc\LargeTextFile.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module LargeTextFile
-    ' 
-    '     Function: __textPartitioning, IteratesStream, IteratesTableData, Merge, Peeks
-    '               Tails, TextPartition
-    ' 
-    ' /********************************************************************************/
+' Module LargeTextFile
+' 
+'     Function: __textPartitioning, IteratesStream, IteratesTableData, Merge, Peeks
+'               Tails, TextPartition
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -86,26 +86,6 @@ Public Module LargeTextFile
         Loop
     End Function
 
-    <ExportAPI("Partitioning")>
-    Public Function TextPartition(data As IEnumerable(Of String)) As String()()
-        Dim maxSize As Double = New StringBuilder(1024 * 1024).MaxCapacity
-        Return __textPartitioning(data.ToArray, maxSize)
-    End Function
-
-    Private Function __textPartitioning(dat As String(), maxSize As Double) As String()()
-        Dim currentSize As Double = (From s As String In dat.AsParallel Select CDbl(Len(s))).Sum
-        If currentSize > maxSize Then
-            Dim SplitTokens = dat.Split(CInt(dat.Length / 2))
-            If SplitTokens.Length > 1 Then
-                Return (From n In SplitTokens Select __textPartitioning(n, maxSize)).ToVector
-            Else
-                Return SplitTokens
-            End If
-        Else
-            Return New String()() {dat}
-        End If
-    End Function
-
     ''' <summary>
     ''' 当一个文件非常大以致无法使用任何现有的文本编辑器查看的时候，可以使用本方法查看其中的一部分数据 
     ''' </summary>
@@ -113,12 +93,14 @@ Public Module LargeTextFile
     ''' <remarks></remarks>
     ''' 
     <ExportAPI("Peeks")>
-    Public Function Peeks(path As String, length As Integer) As String
-        Dim ChunkBuffer As Char() = New Char(length - 1) {}
-        Using Reader = FileIO.FileSystem.OpenTextFileReader(path)
-            Call Reader.ReadBlock(ChunkBuffer, 0, ChunkBuffer.Length)
+    Public Function Peeks(path As String, Optional length% = 5 * 1024) As String
+        Dim buffer As Char() = New Char(length - 1) {}
+
+        Using reader As StreamReader = FileIO.FileSystem.OpenTextFileReader(path)
+            Call reader.ReadBlock(buffer, 0, buffer.Length)
         End Using
-        Return New String(value:=ChunkBuffer)
+
+        Return New String(value:=buffer)
     End Function
 
     ''' <summary>
@@ -128,14 +110,18 @@ Public Module LargeTextFile
     ''' <param name="length">Peeks of the number of characters.(字符的数目)</param>
     ''' <param name="encoding">Default value is <see cref="DefaultEncoding"/></param>
     ''' <returns></returns>
-    ''' <remarks></remarks>
+    ''' <remarks>
+    ''' 请注意，如果字符编码是不定长的，则返回的字符串可能会出现乱码的问题
+    ''' </remarks>
     <ExportAPI("Tails")>
     <Extension>
     Public Function Tails(path$, <Parameter("characters", "The number of the characters, not the bytes value.")> length%, Optional encoding As Encoding = Nothing) As String
+        Dim textEncoder As Encoding = encoding Or DefaultEncoding
+
         If Not path.FileExists Then
             Return Nothing
         Else
-            length *= 8
+            length *= (textEncoder.GetBytes("a").Length + 1)
         End If
 
         Using reader As New FileStream(path, FileMode.Open)
@@ -143,13 +129,51 @@ Public Module LargeTextFile
                 length = reader.Length
             End If
 
-            Dim chunkBuffer As Byte() = New Byte(length - 1) {}
+            Dim buffer As Byte() = New Byte(length - 1) {}
 
             Call reader.Seek(reader.Length - length, SeekOrigin.Begin)
-            Call reader.Read(chunkBuffer, 0, chunkBuffer.Length)
+            Call reader.Read(buffer, 0, buffer.Length)
 
-            Dim value$ = (encoding Or DefaultEncoding).GetString(chunkBuffer)
+            Dim value$ = textEncoder.GetString(buffer)
             Return value
+        End Using
+    End Function
+
+    ''' <summary>
+    ''' Get last line of the target text file.
+    ''' </summary>
+    ''' <param name="path$"></param>
+    ''' <param name="encoding"></param>
+    ''' <param name="newLine$"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function GetLastLine(path$, Optional encoding As Encoding = Nothing, Optional newLine$ = vbLf) As String
+        Using sr As New StreamReader(path, encoding Or UTF8)
+            Dim lastline As String
+            Dim i As Integer = 2
+
+            Call sr.DiscardBufferedData()
+
+            Do
+                If i <= sr.BaseStream.Length Then
+                    sr.BaseStream.Seek(sr.BaseStream.Length - i, SeekOrigin.Begin)
+                    lastline = sr.ReadToEnd
+
+                    If lastline.StartsWith(newLine) Then
+                        Exit Do
+                    End If
+
+                    i += 1
+                Else
+                    ' 目标文本文件只有一行数据
+                    sr.BaseStream.Seek(Scan0, SeekOrigin.Begin)
+                    Return sr.ReadToEnd
+                End If
+            Loop
+
+            ' 因为空格可能是所需要的字符串的数据
+            ' 所以在这里只取出前后的newline字符串
+            Return lastline.Trim(ASCII.CR, ASCII.LF)
         End Using
     End Function
 
