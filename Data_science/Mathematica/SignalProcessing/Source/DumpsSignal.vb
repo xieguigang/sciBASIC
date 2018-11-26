29673d27c9df06a4f9fb352c739ff2eeff4d8d00
xieguigang <xie.guigang@gcmodeller.org>
2018-11-26 20:14:32 +0800

    fix namesapce

--- Data_science/Mathematica/SignalProcessing/Source/DumpsSignal.vb
+++ Data_science/Mathematica/SignalProcessing/Source/DumpsSignal.vb
@@ -42,39 +42,42 @@
 
 #End Region
 
-''' <summary>
-''' 
-''' </summary>
-''' <remarks>
-''' Inspired by Lee Byron's test data generator.
-''' </remarks>
-Public Module TestProvider
+Namespace Source
 
     ''' <summary>
-    ''' Generate signal data for test
+    ''' 
     ''' </summary>
-    ''' <param name="length">信号的长度，点的数量</param>
-    ''' <param name="m">信号的叠加次数</param>
-    ''' <returns></returns>
-    Public Function bumps(length%, Optional m% = 25) As Double()
-        Dim a#() = New Double(length - 1) {}
-        Dim seed As New Random
+    ''' <remarks>
+    ''' Inspired by Lee Byron's test data generator.
+    ''' </remarks>
+    Public Module DumpsSignal
 
-        For i As Integer = 0 To m - 1
-            Call bump(a, length, seed)
-        Next
+        ''' <summary>
+        ''' Generate signal data for test
+        ''' </summary>
+        ''' <param name="length">信号的长度，点的数量</param>
+        ''' <param name="m">信号的叠加次数</param>
+        ''' <returns></returns>
+        Public Function bumps(length%, Optional m% = 25) As Double()
+            Dim a#() = New Double(length - 1) {}
+            Dim seed As New Random
 
-        Return a
-    End Function
+            For i As Integer = 0 To m - 1
+                Call bump(a, length, seed)
+            Next
 
-    Private Sub bump(ByRef a#(), length%, seed As Random)
-        Dim x = 1 / (0.1 + seed.NextDouble),
-        y = 2 * seed.NextDouble - 0.5,
-        Z = 10 / (0.1 + seed.NextDouble)
+            Return a
+        End Function
 
-        For i As Integer = 0 To length - 1
-            Dim w = (i / length - y) * Z
-            a(i) += x * Math.Exp(-w * w)
-        Next
-    End Sub
-End Module
+        Private Sub bump(ByRef a#(), length%, seed As Random)
+            Dim x = 1 / (0.1 + seed.NextDouble),
+                y = 2 * seed.NextDouble - 0.5,
+                Z = 10 / (0.1 + seed.NextDouble)
+
+            For i As Integer = 0 To length - 1
+                Dim w = (i / length - y) * Z
+                a(i) += x * Math.Exp(-w * w)
+            Next
+        End Sub
+    End Module
+End Namespace
