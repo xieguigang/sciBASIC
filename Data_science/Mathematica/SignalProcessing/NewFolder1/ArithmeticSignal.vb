e86c859e958bfc55863611e83639394e28c4b906
xieguigang <xie.guigang@gcmodeller.org>
2018-11-26 19:56:35 +0800

    merge source files

--- Data_science/Mathematica/SignalProcessing/NewFolder1/ArithmeticSignal.vb
+++ Data_science/Mathematica/SignalProcessing/NewFolder1/ArithmeticSignal.vb
@@ -87,4 +87,58 @@ Namespace de.rub.dks.signal.generator.sound.arithmetic
 
 	End Class
 
+
+    Public Class SumSignal
+        Inherits ArithmeticSignal
+
+        Public Sub New(ByVal aa As de.rub.dks.signal.generator.sound.Signal, ByVal bb As de.rub.dks.signal.generator.sound.Signal)
+            MyBase.New(aa, bb)
+        End Sub
+
+        Public Overrides Function calculate(ByVal freq1 As Double, ByVal phase1 As Double, ByVal freq2 As Double, ByVal phase2 As Double) As Double
+            Return a.calculate(freq1, phase1) + b.calculate(freq2, phase2)
+        End Function
+
+
+    End Class
+
+    Public Class MultiplySignal
+        Inherits ArithmeticSignal
+
+        Public Sub New(ByVal aa As de.rub.dks.signal.generator.sound.Signal, ByVal bb As de.rub.dks.signal.generator.sound.Signal)
+            MyBase.New(aa, bb)
+        End Sub
+
+        Public Overrides Function calculate(ByVal freq1 As Double, ByVal phase1 As Double, ByVal freq2 As Double, ByVal phase2 As Double) As Double
+            Return a.calculate(freq1, phase1) * b.calculate(freq2, phase2)
+        End Function
+
+    End Class
+
+    Public Class DivisionSignal
+        Inherits ArithmeticSignal
+
+        Public Sub New(ByVal aa As de.rub.dks.signal.generator.sound.Signal, ByVal bb As de.rub.dks.signal.generator.sound.Signal)
+            MyBase.New(aa, bb)
+        End Sub
+
+        Public Overrides Function calculate(ByVal freq1 As Double, ByVal phase1 As Double, ByVal freq2 As Double, ByVal phase2 As Double) As Double
+            Return a.calculate(freq1, phase1) / b.calculate(freq2, phase2)
+        End Function
+
+    End Class
+
+    Public Class DifferenceSignal
+        Inherits ArithmeticSignal
+
+        Public Sub New(ByVal aa As de.rub.dks.signal.generator.sound.Signal, ByVal bb As de.rub.dks.signal.generator.sound.Signal)
+            MyBase.New(aa, bb)
+        End Sub
+
+
+        Public Overrides Function calculate(ByVal freq1 As Double, ByVal phase1 As Double, ByVal freq2 As Double, ByVal phase2 As Double) As Double
+            Return a.calculate(freq1, phase1) - b.calculate(freq2, phase2)
+        End Function
+
+    End Class
 End Namespace
