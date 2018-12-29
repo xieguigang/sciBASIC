50c3460313fae1596e55ae5ae910d4075878a474
xieguigang <xie.guigang@gcmodeller.org>
2018-12-29 22:21:27 +0800

    RB tree node insert

--- gr/network-visualization/Datavisualization.Network/Layouts/Cola/DirectedEdge.vb
+++ gr/network-visualization/Datavisualization.Network/Layouts/Cola/DirectedEdge.vb
@@ -2,12 +2,17 @@
 
 Namespace Layouts.Cola
 
+    ''' <summary>
+    ''' An object with three point properties, the intersection with the
+    ''' source rectangle (sourceIntersection), the intersection with then
+    ''' target rectangle (targetIntersection), And the point an arrow
+    ''' head of the specified size would need to start (arrowStart).
+    ''' </summary>
     Public Structure DirectedEdge
 
         Public sourceIntersection As Point2D
         Public targetIntersection As Point2D
         Public arrowStart As Point2D
-
 
     End Structure
 End Namespace
--- gr/network-visualization/Datavisualization.Network/Layouts/Cola/Extensions.vb
+++ gr/network-visualization/Datavisualization.Network/Layouts/Cola/Extensions.vb
@@ -6,6 +6,13 @@ Namespace Layouts.Cola
 
     Public Module Extensions
 
+        ''' <summary>
+        ''' Returns the endpoints of a line that connects the centre of two rectangles.
+        ''' </summary>
+        ''' <param name="source">The source Rectangle.</param>
+        ''' <param name="target">The target Rectangle.</param>
+        ''' <param name="ah">The size of the arrow head, a distance to shorten the line by.</param>
+        ''' <returns></returns>
         Public Function makeEdgeBetween(source As Rectangle2D, target As Rectangle2D, ah As number) As DirectedEdge
             Dim si = source.rayIntersection(target.CenterX, target.CenterY) Or New Point2D(source.CenterX, source.CenterY).AsDefault
             Dim ti = target.rayIntersection(source.CenterX, source.CenterY) Or New Point2D(target.CenterX, target.CenterY).AsDefault
@@ -22,5 +29,35 @@ Namespace Layouts.Cola
                 }
             }
         End Function
+
+        ''' <summary>
+        ''' Returns the intersection of a line from the given point to the centre
+        ''' of the target rectangle where it intersects the rectanngle.
+        ''' </summary>
+        ''' <param name="s">The source point.</param>
+        ''' <param name="target">The target Rectangle.</param>
+        ''' <param name="ah">The size of the arrow head, a distance to shorten the
+        ''' line by.</param>
+        ''' <returns>The point an arrow head of the specified size would need to start.</returns>
+        Public Function makeEdgeTo(s As Point2D, target As Rectangle2D, ah As number) As Point2D
+            Dim ti = target.rayIntersection(s.X, s.Y)
+
+            If (ti Is Nothing) Then
+                ti = New Point2D With {
+                    .X = target.CenterX,
+                    .Y = target.CenterY
+                }
+            End If
+
+            Dim dx = ti.X - s.X
+            Dim dy = ti.Y - s.Y
+            Dim l = Math.Sqrt(dx * dx + dy * dy)
+
+            Return New Point2D With {
+                .X = ti.X - ah * dx / l,
+                .Y = ti.Y - ah * dy / l
+            }
+        End Function
+
     End Module
 End Namespace
