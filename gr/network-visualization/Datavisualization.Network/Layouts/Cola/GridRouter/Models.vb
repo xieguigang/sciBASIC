8ecc7e29c8d97458a5dc678aded1dd3cfaf4faa9
xieguigang <xie.guigang@gcmodeller.org>
2018-12-31 22:23:47 +0800

    fix for model

--- gr/network-visualization/Datavisualization.Network/Layouts/Cola/GridRouter/Models.vb
+++ gr/network-visualization/Datavisualization.Network/Layouts/Cola/GridRouter/Models.vb
@@ -7,16 +7,39 @@ Namespace Layouts.Cola.GridRouter
         Function getChildren(v As Node) As number()
         Function getBounds(v As Node) As Rectangle2D
     End Interface
-    Public Class NodeWrapper {
-        leaf: Boolean;
-        parent: NodeWrapper;
-        ports: Vert[];
-        constructor(public id: number, public rect: Rectangle, public children: number[]) {
-            this.leaf = TypeOf children === 'undefined' || children.length === 0;
-        }
-    End Class 
 
-    export Class Vert {
-        constructor(public id: number, public x:number, public y: number, public node: NodeWrapper = null, public line = null) {}
-    }
+    Public Class NodeWrapper
+        Public leaf As Boolean
+        Public parent As NodeWrapper
+        Public ports As Vert()
+        Public id As number
+        Public rect As Rectangle2D
+        Public children As number()
+
+        Public Sub New(id As number, rect As Rectangle2D, children As number())
+            Me.id = id
+            Me.rect = rect
+            Me.children = children
+
+            leaf = children.IsNullOrEmpty
+        End Sub
+    End Class
+
+    Public Class Vert
+
+        Public id As number
+        Public x As number
+        Public y As number
+        Public node As NodeWrapper
+        Public line
+
+        Sub New(id As number, x As number, y As number, Optional node As NodeWrapper = Nothing, Optional line As Object = Nothing)
+            Me.id = id
+            Me.x = x
+            Me.y = y
+            Me.node = node
+            Me.line = line
+        End Sub
+    End Class
+
 End Namespace
