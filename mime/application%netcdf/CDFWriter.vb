35a3789a5a8ff5eb25536cd6510d63c38fc4b44e
xieguigang <xie.guigang@gcmodeller.org>
2019-01-18 13:42:11 +0800

    improvements of the debug viewer

--- mime/application%netcdf/CDFWriter.vb
+++ mime/application%netcdf/CDFWriter.vb
@@ -1,6 +1,5 @@
 ﻿Imports System.IO
 Imports System.Runtime.CompilerServices
-Imports System.Text
 Imports Microsoft.VisualBasic.Data.IO
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
--- mime/application%netcdf/ToString.vb
+++ mime/application%netcdf/ToString.vb
@@ -1,46 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::ca0c93cca11f4ddfe93a497fa0957972, mime\application%netcdf\ToString.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module ToStringHelper
-    ' 
-    '     Function: toString
-    ' 
-    ' /********************************************************************************/
+' Module ToStringHelper
+' 
+'     Function: toString
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports System.Text
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.MIME.application.netCDF.Components
 
 ''' <summary>
@@ -51,10 +52,12 @@ Module ToStringHelper
     <Extension>
     Public Function toString(file As netCDFReader) As String
         Dim result As New StringBuilder
+        Dim [dim] As Dimension
 
         result.AppendLine("DIMENSIONS")
-        For Each dimension In file.dimensions
-            result.AppendLine($"  {dimension.name.PadEnd(30)} = size: {dimension.size}")
+        For Each dimension As SeqValue(Of Dimension) In file.dimensions.SeqIterator
+            [dim] = dimension
+            result.AppendLine($"  [{dimension.i.ToString.PadLeft(2)}] {[dim].name.PadEnd(25)} = size: {[dim].size}")
         Next
 
         result.AppendLine()
