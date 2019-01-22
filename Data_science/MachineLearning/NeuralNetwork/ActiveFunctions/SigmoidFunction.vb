c75f57461308a22d121b86e051b0e70de3e00a01
xieguigang <xie.guigang@gcmodeller.org>
2019-01-22 15:44:45 +0800

    add Relu function

--- Data_science/MachineLearning/NeuralNetwork/ActiveFunctions/SigmoidFunction.vb
+++ Data_science/MachineLearning/NeuralNetwork/ActiveFunctions/SigmoidFunction.vb
@@ -1,4 +1,4 @@
-﻿#Region "Microsoft.VisualBasic::1ee2645f7cc80252ba873ad250db36bc, Data_science\MachineLearning\NeuralNetwork\ActiveFunctions\SigmoidFunction.vb"
+﻿#Region "Microsoft.VisualBasic::8aa8bf036afa8b2a1a870a552c01f739, Data_science\MachineLearning\NeuralNetwork\ActiveFunctions\Sigmoid.vb"
 
     ' Author:
     ' 
@@ -31,11 +31,10 @@
 
     ' Summaries:
 
-    '     Class SigmoidFunction
+    '     Class Sigmoid
     ' 
-    '         Properties: Alpha, Store
+    '         Properties: Store
     ' 
-    '         Constructor: (+2 Overloads) Sub New
     '         Function: [Function], Derivative, Derivative2
     ' 
     ' 
@@ -43,138 +42,38 @@
 
 #End Region
 
-' AForge Neural Net Library
-' AForge.NET framework
-' http://www.aforgenet.com/framework/
-'
-' Copyright © AForge.NET, 2007-2012
-' contacts@aforgenet.com
-'
-
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.MachineLearning.NeuralNetwork.StoreProcedure
-Imports Microsoft.VisualBasic.Text.Xml.Models
 
 Namespace NeuralNetwork.Activations
 
     ''' <summary>
-    ''' Sigmoid activation function.
+    ''' https://github.com/trentsartain/Neural-Network/blob/master/NeuralNetwork/NeuralNetwork/Network/Sigmoid.cs
     ''' </summary>
-    '''
-    ''' <remarks><para>The class represents sigmoid activation function with
-    ''' the next expression:
-    ''' <code lang="none">
-    '''                1
-    ''' f(x) = ------------------
-    '''        1 + exp(-alpha * x)
-    '''
-    '''           alpha * exp(-alpha * x )
-    ''' f'(x) = ---------------------------- = alpha * f(x) * (1 - f(x))
-    '''           (1 + exp(-alpha * x))^2
-    ''' </code>
-    ''' </para>
-    '''
-    ''' <para>Output range of the function: <b>[0, 1]</b>.</para>
-    ''' 
-    ''' <para>Functions graph:</para>
-    ''' <img src="img/neuro/sigmoid.bmp" width="242" height="172" />
-    ''' </remarks>
-    ''' 
-    <Serializable>
-    Public Class SigmoidFunction : Implements IActivationFunction
-
-        ''' <summary>
-        ''' Sigmoid's alpha value.
-        ''' </summary>
-        ''' 
-        ''' <remarks><para>The value determines steepness of the function. Increasing value of
-        ''' this property changes sigmoid to look more like a threshold function. Decreasing
-        ''' value of this property makes sigmoid to be very smooth (slowly growing from its
-        ''' minimum value to its maximum value).</para>
-        '''
-        ''' <para>Default value is set to <b>2</b>.</para>
-        ''' </remarks>
-        ''' 
-        Public Property Alpha() As Double = 2.0R
+    Public NotInheritable Class SigmoidFunction : Implements IActivationFunction
 
         Public ReadOnly Property Store As ActiveFunction Implements IActivationFunction.Store
             Get
                 Return New ActiveFunction With {
-                    .Arguments = {
-                        New NamedValue With {.name = "alpha", .text = Alpha}
-                    },
-                    .Name = NameOf(SigmoidFunction)
+                    .Arguments = {},
+                    .name = NameOf(SigmoidFunction)
                 }
             End Get
         End Property
 
-        ''' <summary>
-        ''' Initializes a new instance of the <see cref="SigmoidFunction"/> class.
-        ''' </summary>
-        Public Sub New()
-        End Sub
-
-        ''' <summary>
-        ''' Initializes a new instance of the <see cref="SigmoidFunction"/> class.
-        ''' </summary>
-        ''' 
-        ''' <param name="alpha">Sigmoid's alpha value.</param>
-        ''' 
-        Public Sub New(alpha As Double)
-            Me._Alpha = alpha
-        End Sub
-
-
-        ''' <summary>
-        ''' Calculates function value.
-        ''' </summary>
-        '''
-        ''' <param name="x">Function input value.</param>
-        ''' 
-        ''' <returns>Function output value, <i>f(x)</i>.</returns>
-        '''
-        ''' <remarks>The method calculates function value at point <paramref name="x"/>.</remarks>
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Function [Function](x As Double) As Double Implements IActivationFunction.[Function]
-            Return (1 / (1 + Math.Exp(-_Alpha * x)))
-        End Function
-
-        ''' <summary>
-        ''' Calculates function derivative.
-        ''' </summary>
-        ''' 
-        ''' <param name="x">Function input value.</param>
-        ''' 
-        ''' <returns>Function derivative, <i>f'(x)</i>.</returns>
-        ''' 
-        ''' <remarks>The method calculates function derivative at point <paramref name="x"/>.</remarks>
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Function Derivative(x As Double) As Double Implements IActivationFunction.Derivative
-            Dim y As Double = [Function](x)
-
-            Return (_Alpha * y * (1 - y))
+            Return x * (1 - x)
         End Function
 
-        ''' <summary>
-        ''' Calculates function derivative.
-        ''' </summary>
-        ''' 
-        ''' <param name="y">Function output value - the value, which was obtained
-        ''' with the help of "Function" method.</param>
-        ''' 
-        ''' <returns>Function derivative, <i>f'(x)</i>.</returns>
-        ''' 
-        ''' <remarks><para>The method calculates the same derivative value as the
-        ''' <see cref="Derivative"/> method, but it takes not the input <b>x</b> value
-        ''' itself, but the function value, which was calculated previously with
-        ''' the help of "Function" method.</para>
-        ''' 
-        ''' <para><note>Some applications require as function value, as derivative value,
-        ''' so they can save the amount of calculations using this method to calculate derivative.</note></para>
-        ''' </remarks>
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function [Function](x As Double) As Double Implements IActivationFunction.Function
+            Return If(x < -45.0, 0.0, If(x > 45.0, 1.0, 1.0 / (1.0 + Math.Exp(-x))))
+        End Function
+
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Function Derivative2(y As Double) As Double Implements IActivationFunction.Derivative2
-            Return (_Alpha * y * (1 - y))
+            Return Derivative(y)
         End Function
     End Class
 End Namespace
