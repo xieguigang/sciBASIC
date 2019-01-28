bf155b26225e11084d7c17d70790031f470c282b
xieguigang <xie.guigang@gcmodeller.org>
2019-01-28 17:11:22 +0800

    fix object reference error

--- Data/DataFrame/IO/Generic/DataSet.vb
+++ Data/DataFrame/IO/Generic/DataSet.vb
@@ -132,7 +132,7 @@ Namespace IO
                                                             Optional encoding As Encoding = Nothing) As IEnumerable(Of T)
 
             Dim mapFrom$ = uidMap Or New DefaultValue(Of String) With {
-                .LazyValue = New Func(Of String)(Function() __getID(path)).AsLazy
+                .lazy = New Func(Of String)(Function() __getID(path)).AsLazy
             }
             Return path.LoadCsv(Of T)(
                 explicit:=False,
