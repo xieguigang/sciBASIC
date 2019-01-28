bb81589441a7676f1157db86024b07a89dc4a9ee
xieguigang <xie.guigang@gcmodeller.org>
2019-01-28 20:29:27 +0800

    fix data type error

--- mime/application%netcdf/HDF5/chunked/Chunk.vb
+++ mime/application%netcdf/HDF5/chunked/Chunk.vb
@@ -5,19 +5,13 @@ Namespace org.renjin.hdf5.chunked
 	''' </summary>
 	Public MustInherit Class Chunk
 
-		Private ReadOnly chunkOffset() As Long
-
-		Public Sub New(chunkOffset() As Long)
+        Public Sub New(chunkOffset() As Long)
 			Me.chunkOffset = chunkOffset
 		End Sub
 
 		Public Overridable Property ChunkOffset As Long()
-			Get
-				Return chunkOffset
-			End Get
-		End Property
 
-		Public MustOverride Function getDoubleAt(i As Integer) As Double
+        Public MustOverride Function getDoubleAt(i As Integer) As Double
 	End Class
 
 End Namespace
