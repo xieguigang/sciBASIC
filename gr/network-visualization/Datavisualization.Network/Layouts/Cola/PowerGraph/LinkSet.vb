7f6af01b5f2799de4748061af83d39ca494219b1
xieguigang <xie.guigang@gcmodeller.org>
2019-01-30 09:33:17 +0800

    linkset

--- gr/network-visualization/Datavisualization.Network/Layouts/Cola/PowerGraph/LinkSet.vb
+++ gr/network-visualization/Datavisualization.Network/Layouts/Cola/PowerGraph/LinkSet.vb
@@ -1,58 +1,65 @@
 ﻿Namespace Layouts.Cola
 
+    Public Class LinkSets
 
-    Class LinkSets
-        Private sets As any = New Object() {}
-        Private n As Double = 0
-        Public Function count() As Double
-            Return Me.n
-        End Function
-        Private Function contains(id As Double) As Boolean
+        Dim sets As New Dictionary(Of String, ModuleSet)
+
+        Public ReadOnly Property count() As Integer
+
+        Private Function contains(id As Integer) As Boolean
             Dim result = False
-            Me.forAllModules(Function(m)
+
+            Me.forAllModules(Sub(m)
                                  If Not result AndAlso m.id = id Then
                                      result = True
                                  End If
-
-                             End Function)
+                             End Sub)
             Return result
         End Function
+
         Private Sub add(linktype As Double, m As [Module])
             Dim s As ModuleSet = If(Me.sets.Have(linktype), Me.sets(linktype), InlineAssignHelper(Me.sets(linktype), New ModuleSet()))
             s.add(m)
-            Me.n += 1
+            Me._count += 1
         End Sub
+
         Private Sub remove(linktype As Double, m As [Module])
             Dim ms = DirectCast(Me.sets(linktype), ModuleSet)
             ms.remove(m)
             If ms.count() = 0 Then
                 Delete(Me.sets, linktype)
             End If
-            Me.n -= 1
+            Me._count -= 1
         End Sub
-        Private Sub forAll(f As Action(Of ModuleSet, Number))
-            For Each linktype As var In Me.sets.keys
-                f(DirectCast(Me.sets(linktype), ModuleSet), linktype)
+
+        Public Sub forAll(f As Action(Of ModuleSet, Integer))
+            For Each linktype As String In Me.sets.Keys
+                f(Me.sets(linktype), linktype)
             Next
         End Sub
+
         Private Sub forAllModules(f As Action(Of [Module]))
-            Me.forAll(Function(ms, lt) ms.forAll(f))
+            Me.forAll(Sub(ms, lt) Call ms.forAll(f))
         End Sub
+
         Private Function intersection(other As LinkSets) As LinkSets
             Dim result As New LinkSets()
-            Me.forAll(Function(ms, lt)
-                          If other.sets.Have(lt) Then
+
+            Me.forAll(Sub(ms, lt)
+                          If other.sets.ContainsKey(lt.ToString) Then
                               Dim i = ms.intersection(other.sets(lt))
                               Dim n = i.count()
+
                               If n > 0 Then
                                   result.sets(lt) = i
-                                  result.n += n
+                                  result._count += n
                               End If
                           End If
+                      End Sub)
 
-                      End Function)
             Return result
         End Function
+
         Private Shared Function InlineAssignHelper(Of T)(ByRef target As T, value As T) As T
             target = value
             Return value
--- gr/network-visualization/Datavisualization.Network/Layouts/Cola/PowerGraph/Module.vb
+++ gr/network-visualization/Datavisualization.Network/Layouts/Cola/PowerGraph/Module.vb
@@ -1,17 +1,42 @@
-﻿Namespace Layouts.Cola
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Layouts.Cola
 
 
     Public Class [Module]
 
         Public gid As Double
-        Public id As Double
+        Public id As Integer
 
-        Public outgoing As New List(Of LinkSets)
-        Public incoming As New List(Of LinkSets)
-        Public children As New List(Of ModuleSet)
+        Public outgoing As New LinkSets
+        Public incoming As New LinkSets
+        Public children As New ModuleSet
         Public definition As any
 
-        Public Sub New(id As Double, Optional outgoing As LinkSets = Nothing, Optional incoming As LinkSets = Nothing, Optional children As ModuleSet = Nothing, Optional definition As any = Nothing)
+        Public ReadOnly Property isLeaf() As Boolean
+            Get
+                Return Me.children.Count() = 0
+            End Get
+        End Property
+
+        Public ReadOnly Property isIsland() As Boolean
+            Get
+                Return Me.outgoing.Count() = 0 AndAlso Me.incoming.Count() = 0
+            End Get
+        End Property
+
+        Public ReadOnly Property isPredefined() As Boolean
+            Get
+                Return Me.definition IsNot Nothing
+            End Get
+        End Property
+
+        Public Sub New(id%,
+                       Optional outgoing As LinkSets = Nothing,
+                       Optional incoming As LinkSets = Nothing,
+                       Optional children As ModuleSet = Nothing,
+                       Optional definition As any = Nothing)
+
             Me.id = id
             Me.outgoing = outgoing
             Me.incoming = incoming
@@ -26,57 +51,53 @@
                                              End Sub)
                                End Sub)
         End Sub
-
-        Private Function isLeaf() As Boolean
-            Return Me.children.Count() = 0
-        End Function
-
-        Private Function isIsland() As Boolean
-            Return Me.outgoing.Count() = 0 AndAlso Me.incoming.Count() = 0
-        End Function
-
-        Private Function isPredefined() As Boolean
-            Return Me.definition IsNot Nothing
-        End Function
     End Class
 
+    Public Class ModuleSet
 
-    Class ModuleSet
-        Private table As any = New Object() {}
-        Private Function count() As Double
-            Return [Object].keys(Me.table).length
-        End Function
-        Private Function intersection(other As ModuleSet) As ModuleSet
+        Dim table As New Dictionary(Of String, [Module])
+
+        Public ReadOnly Property count() As Integer
+            Get
+                Return table.Count
+            End Get
+        End Property
+
+        Public Function intersection(other As ModuleSet) As ModuleSet
             Dim result = New ModuleSet()
             result.table = intersection(Me.table, other.table)
             Return result
         End Function
-        Private Function intersectionCount(other As ModuleSet) As Double
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function intersectionCount(other As ModuleSet) As Integer
             Return Me.intersection(other).count()
         End Function
-        Private Function contains(id As Double) As Boolean
-            Return Me.table.Have(id)
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function contains(id As Double) As Boolean
+            Return Me.table.ContainsKey(id)
         End Function
-        Private Sub add(m As [Module])
-            Me.table(m.id) = m
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Sub add(m As [Module])
+            Me.table(m.id.ToString) = m
         End Sub
-        Private Sub remove(m As [Module])
-            Delete(Me.table, m.id)
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Sub remove(m As [Module])
+            Call Me.table.Remove(m.id.ToString)
         End Sub
-        Private Sub forAll(f As Action(Of [Module]))
-            For Each mid As var In Me.table.keys
+
+        Public Sub forAll(f As Action(Of [Module]))
+            For Each mid As String In Me.table.Keys
                 f(Me.table(mid))
             Next
         End Sub
-        Private Function modules() As [Module]()
-            Dim vs = New Object() {}
-            Me.forAll(Function(m)
-                          If Not m.isPredefined() Then
-                              vs.push(m)
-                          End If
 
-                      End Function)
-            Return vs
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function modules() As [Module]()
+            Return table.Values.Where(Function(m) m.isPredefined).ToArray
         End Function
     End Class
 End Namespace
