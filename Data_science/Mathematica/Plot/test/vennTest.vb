fbdf61d11ab27727a5374b9558760111cd48cb52
xieguigang <xie.guigang@gcmodeller.org>
2019-03-15 20:44:21 +0800

    try to draw venn3

--- Data_science/Mathematica/Plot/test/vennTest.vb
+++ Data_science/Mathematica/Plot/test/vennTest.vb
@@ -48,7 +48,9 @@ Module vennTest
     Sub Main()
         Dim a As New VennSet With {.color = Color.AliceBlue, .intersections = New Dictionary(Of String, Integer) From {{"b", 100}}, .Name = "a", .Size = 500}
         Dim b As New VennSet With {.color = Color.DarkGreen, .intersections = New Dictionary(Of String, Integer) From {{"a", 100}}, .Name = "b", .Size = 110}
+        Dim c As New VennSet With {.color = Color.DeepSkyBlue, .intersections = New Dictionary(Of String, Integer) From {{"a", 300}, {"b", 30}}, .Name = "c", .Size = 500}
 
         Call VennPlot.Venn2(a, b).AsGDIImage.SaveAs("./venn2.png")
+        Call VennPlot.Venn3(a, b, c).AsGDIImage.SaveAs("./venn3.png")
     End Sub
 End Module
