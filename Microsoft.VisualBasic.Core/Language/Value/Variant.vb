a3265165ff8e5801bac72f02c2d9529f389c7102
xieguigang <xie.guigang@gcmodeller.org>
2019-03-22 10:50:25 +0800

    renames union type to Variant

--- Microsoft.VisualBasic.Core/Language/Value/Variant.vb
+++ Microsoft.VisualBasic.Core/Language/Value/Variant.vb
@@ -2,7 +2,7 @@
 
 Namespace Language
 
-    Public Class UnionType(Of A, B) : Inherits Value(Of Object)
+    Public Class [Variant](Of A, B) : Inherits Value(Of Object)
 
         Public Overrides Function GetUnderlyingType() As Type
             If Value Is Nothing Then
@@ -27,52 +27,52 @@ Namespace Language
         End Property
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Widening Operator CType(obj As UnionType(Of A, B)) As Type
+        Public Overloads Shared Widening Operator CType(obj As [Variant](Of A, B)) As Type
             Return obj.GetUnderlyingType
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Narrowing Operator CType(obj As UnionType(Of A, B)) As A
+        Public Overloads Shared Narrowing Operator CType(obj As [Variant](Of A, B)) As A
             Return obj.VA
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Narrowing Operator CType(obj As UnionType(Of A, B)) As B
+        Public Overloads Shared Narrowing Operator CType(obj As [Variant](Of A, B)) As B
             Return obj.VB
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Widening Operator CType(a As A) As UnionType(Of A, B)
-            Return New UnionType(Of A, B) With {.Value = a}
+        Public Overloads Shared Widening Operator CType(a As A) As [Variant](Of A, B)
+            Return New [Variant](Of A, B) With {.Value = a}
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Widening Operator CType(b As B) As UnionType(Of A, B)
-            Return New UnionType(Of A, B) With {.Value = b}
+        Public Overloads Shared Widening Operator CType(b As B) As [Variant](Of A, B)
+            Return New [Variant](Of A, B) With {.Value = b}
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Operator =(obj As UnionType(Of A, B), a As A) As Boolean
+        Public Overloads Shared Operator =(obj As [Variant](Of A, B), a As A) As Boolean
             Return obj.VA.Equals(a)
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Operator <>(obj As UnionType(Of A, B), a As A) As Boolean
+        Public Overloads Shared Operator <>(obj As [Variant](Of A, B), a As A) As Boolean
             Return Not obj = a
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Operator =(obj As UnionType(Of A, B), b As B) As Boolean
+        Public Overloads Shared Operator =(obj As [Variant](Of A, B), b As B) As Boolean
             Return obj.VB.Equals(b)
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Operator <>(obj As UnionType(Of A, B), b As B) As Boolean
+        Public Overloads Shared Operator <>(obj As [Variant](Of A, B), b As B) As Boolean
             Return Not obj = b
         End Operator
     End Class
 
-    Public Class UnionType(Of A, B, C) : Inherits UnionType(Of A, B)
+    Public Class [Variant](Of A, B, C) : Inherits [Variant](Of A, B)
 
         Public ReadOnly Property VC As C
             <MethodImpl(MethodImplOptions.AggressiveInlining)>
@@ -82,33 +82,33 @@ Namespace Language
         End Property
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Narrowing Operator CType(obj As UnionType(Of A, B, C)) As C
+        Public Overloads Shared Narrowing Operator CType(obj As [Variant](Of A, B, C)) As C
             Return obj.VC
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Narrowing Operator CType(obj As UnionType(Of A, B, C)) As A
+        Public Overloads Shared Narrowing Operator CType(obj As [Variant](Of A, B, C)) As A
             Return obj.VA
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Narrowing Operator CType(obj As UnionType(Of A, B, C)) As B
+        Public Overloads Shared Narrowing Operator CType(obj As [Variant](Of A, B, C)) As B
             Return obj.VB
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Widening Operator CType(c As C) As UnionType(Of A, B, C)
-            Return New UnionType(Of A, B, C) With {.Value = c}
+        Public Overloads Shared Widening Operator CType(c As C) As [Variant](Of A, B, C)
+            Return New [Variant](Of A, B, C) With {.Value = c}
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Widening Operator CType(a As A) As UnionType(Of A, B, C)
-            Return New UnionType(Of A, B, C) With {.Value = a}
+        Public Overloads Shared Widening Operator CType(a As A) As [Variant](Of A, B, C)
+            Return New [Variant](Of A, B, C) With {.Value = a}
         End Operator
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Overloads Shared Widening Operator CType(b As B) As UnionType(Of A, B, C)
-            Return New UnionType(Of A, B, C) With {.Value = b}
+        Public Overloads Shared Widening Operator CType(b As B) As [Variant](Of A, B, C)
+            Return New [Variant](Of A, B, C) With {.Value = b}
         End Operator
     End Class
 End Namespace
