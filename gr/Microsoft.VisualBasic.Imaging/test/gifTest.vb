800b51f00f1a943e672a0c3a0c2248653a3cb7fe
xieguigang <xie.guigang@gcmodeller.org>
2019-03-24 00:04:50 +0800

    test success

---
+++ gr/Microsoft.VisualBasic.Imaging/test/gifTest.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Drawing
+Imports System.IO
+Imports Microsoft.VisualBasic.Imaging
+
+Module gifTest
+
+    Sub Main()
+        Using file As FileStream = "./test.gif".Open, gif As New GifEncoder(file)
+            Dim frame1 As Graphics2D = New Size(200, 200).CreateGDIDevice
+            Dim frame2 As Graphics2D = New Size(200, 200).CreateGDIDevice
+
+            Call frame1.DrawString("Hello", New Font(FontFace.BookmanOldStyle, 20, FontStyle.Bold), Brushes.Red, 10, 10)
+            Call frame2.DrawString("World", New Font(FontFace.BookmanOldStyle, 20, FontStyle.Bold), Brushes.Red, 10, 10)
+
+            Call gif.AddFrame(frame1,,, New TimeSpan(0, 0, 3))
+            Call gif.AddFrame(frame2,,, New TimeSpan(0, 0, 3))
+        End Using
+    End Sub
+End Module
