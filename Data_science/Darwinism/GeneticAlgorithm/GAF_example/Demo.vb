f200ed9da2335c816d8a822ff846c257b965912a
xieguigang <xie.guigang@gcmodeller.org>
2019-04-04 09:49:08 +0800

    fix project reference errors

--- Data_science/Darwinism/GeneticAlgorithm/GAF_example/Demo.vb
+++ Data_science/Darwinism/GeneticAlgorithm/GAF_example/Demo.vb
@@ -1,55 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::6b63a9a19482bb6d0bc5fd9426ccdd06, Data_science\Darwinism\GeneticAlgorithm\GAF_example\Demo.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class Demo
-    ' 
-    '     Sub: Main
-    '     Class MyVector
-    ' 
-    '         Properties: Vector
-    ' 
-    '         Function: clone, Crossover, Mutate, ToString
-    ' 
-    '     Class MyVectorFitness
-    ' 
-    '         Function: Calculate
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Class Demo
+' 
+'     Sub: Main
+'     Class MyVector
+' 
+'         Properties: Vector
+' 
+'         Function: clone, Crossover, Mutate, ToString
+' 
+'     Class MyVectorFitness
+' 
+'         Function: Calculate
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports GAF_example
 Imports Microsoft.VisualBasic.Extensions
 Imports Microsoft.VisualBasic.MachineLearning.Darwinism.GAF
 Imports Microsoft.VisualBasic.MachineLearning.Darwinism.GAF.Helper
@@ -126,6 +127,12 @@ Public Class Demo
         Implements Fitness(Of MyVector)
 
         ReadOnly target As Integer() = {10, 20, 30, 40, 50}
+
+        Public ReadOnly Property Cacheable As Boolean Implements Fitness(Of MyVector).Cacheable
+            Get
+                Return False
+            End Get
+        End Property
 
         Public Function Calculate(chromosome As MyVector) As Double Implements Fitness(Of MyVector).Calculate
             Return FitnessHelper.Calculate(chromosome.Vector, target)
