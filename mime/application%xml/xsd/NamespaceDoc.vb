45b5c2729cf726b9a4d163f6e2816942ca9f97d8
xieguigang <xie.guigang@gcmodeller.org>
2019-04-18 10:58:54 +0800

    add dev notes

---
+++ mime/application%xml/xsd/NamespaceDoc.vb
@@ -0,0 +1,10 @@
+﻿Namespace xsd
+
+    ''' <summary>
+    ''' The xml element data model in xsd definition file.
+    ''' </summary>
+    Public NotInheritable Class NamespaceDoc
+        Private Sub New()
+        End Sub
+    End Class
+End Namespace
