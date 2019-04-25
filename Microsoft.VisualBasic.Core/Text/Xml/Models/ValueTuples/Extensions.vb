7d32cee8a29a0483a029fcf40c130207dd8bc8cb
xieguigang <xie.guigang@gcmodeller.org>
2019-04-25 10:44:09 +0800

    improvements of the xml data model helper

---
+++ Microsoft.VisualBasic.Core/Text/Xml/Models/ValueTuples/Extensions.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Text.Xml.Models
+
+    Public Module Extensions
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        <Extension>
+        Public Function ToDictionary(Of T)(properties As IEnumerable(Of [Property]), parser As Func(Of String, T)) As Dictionary(Of String, T)
+            Return properties.ToDictionary(Function(p) p.name, Function(p) parser(p.value))
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        <Extension>
+        Public Function ToProperties(Of T)(table As Dictionary(Of String, T), toString As Func(Of T, String)) As IEnumerable(Of [Property])
+            Return table.Select(Function(p) New [Property] With {.name = p.Key, .value = toString(p.Value)})
+        End Function
+    End Module
+End Namespace
