4becd3f1b68bd44d5e70c020f4a25315f8bc2e4e
xieguigang <xie.guigang@gcmodeller.org>
2019-04-26 10:06:28 +0800

    code style improvements

--- Data/BinaryData/BinaryData/SQLite3/Sqlite3Database.vb
+++ Data/BinaryData/BinaryData/SQLite3/Sqlite3Database.vb
@@ -1,53 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::673b6ce11259922ccd6c6eae821fa6cc, Data\BinaryData\BinaryData\SQLite3\Sqlite3Database.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class Sqlite3Database
-    ' 
-    '         Properties: GetTables, Header
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: GetTable
-    ' 
-    '         Sub: Dispose, Initialize, InitializeMasterTable
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class Sqlite3Database
+' 
+'         Properties: GetTables, Header
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: GetTable
+' 
+'         Sub: Dispose, Initialize, InitializeMasterTable
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Collections.Generic
 Imports System.IO
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.IO.ManagedSqlite.Core.Internal
 Imports Microsoft.VisualBasic.Data.IO.ManagedSqlite.Core.Objects
 Imports Microsoft.VisualBasic.Data.IO.ManagedSqlite.Core.Objects.Headers
@@ -63,11 +64,11 @@ Namespace ManagedSqlite.Core
     ''' </summary>
     Public Class Sqlite3Database : Implements IDisposable
 
-        Private ReadOnly _settings As Sqlite3Settings
-        Private ReadOnly _reader As ReaderBase
+        ReadOnly _settings As Sqlite3Settings
+        ReadOnly _reader As ReaderBase
 
-        Private _sizeInPages As UInteger
-        Private _masterTable As Sqlite3MasterTable
+        Dim _sizeInPages As UInteger
+        Dim _masterTable As Sqlite3MasterTable
 
         Public Property Header() As DatabaseHeader
 
@@ -78,12 +79,17 @@ Namespace ManagedSqlite.Core
         End Property
 
         Public Sub New(file As Stream, Optional settings As Sqlite3Settings = Nothing)
-            _settings = If(settings, New Sqlite3Settings())
+            _settings = settings Or Sqlite3Settings.GetDefaultSettings
             _reader = New ReaderBase(file)
 
-            Initialize()
-            InitializeMasterTable()
+            Call Initialize()
+            Call InitializeMasterTable()
         End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Function OpenFile(dbFile As String, Optional settings As Sqlite3Settings = Nothing) As Sqlite3Database
+            Return New Sqlite3Database(dbFile.Open(FileMode.Open, doClear:=False), settings)
+        End Function
 
         Private Sub Initialize()
             Header = DatabaseHeader.Parse(_reader)
@@ -95,14 +101,12 @@ Namespace ManagedSqlite.Core
 
             ' TODO: Warn on mismatch
             _sizeInPages = Math.Max(expectedPages, Header.DatabaseSizeInPages)
-
             _reader.ApplySqliteDatabaseHeader(Header)
         End Sub
 
         Private Sub InitializeMasterTable()
             ' Parse table on Page 1, the sqlite_master table
             Dim rootBtree As BTreePage = BTreePage.Parse(_reader, 1)
-
             ' Fake the schema for the sqlite_master table
             Dim schemaRow As New Sqlite3SchemaRow() With {
                  .Type = "table",
@@ -127,6 +131,7 @@ Namespace ManagedSqlite.Core
                 ' Found it
                 Dim root As BTreePage = BTreePage.Parse(_reader, table.RootPage)
                 Dim tbl As New Sqlite3Table(_reader, root, table)
+
                 Return tbl
             Next
 
