a17db51d1445bf7c2adb81623673ff802f55b0d6
xieguigang <xie.guigang@gcmodeller.org>
2019-04-30 09:18:20 +0800

    add file helper

--- CLI_tools/Cluster/CLI.vb
+++ CLI_tools/Cluster/CLI.vb
@@ -1,49 +1,46 @@
 ﻿#Region "Microsoft.VisualBasic::79a11d9c497e61551a37bf755679ac89, CLI_tools\Cluster\CLI.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module CLI
-    ' 
-    '     Function: bTree, bTreeCluster, bTreePartitioning, ClusterDataSet, ClusterNumbers
-    ' 
-    ' /********************************************************************************/
+' Module CLI
+' 
+'     Function: bTree, bTreeCluster, bTreePartitioning, ClusterDataSet, ClusterNumbers
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports Microsoft.VisualBasic
 Imports Microsoft.VisualBasic.CommandLine.Reflection
-Imports Microsoft.VisualBasic.DataMining.Framework.KMeans
-Imports Microsoft.VisualBasic.DataVisualization.Network.FileStream
-Imports Microsoft.VisualBasic.DocumentFormat.Csv
+Imports Microsoft.VisualBasic.DataMining.KMeans
 Imports Microsoft.VisualBasic.Linq
 
 Module CLI
@@ -62,18 +59,18 @@ Module CLI
         Dim nums As Double()
 
         If [in].FileExists Then
-            nums = [in].ReadAllLines.ToArray(Function(s) Val(s))
+            nums = [in].ReadAllLines.Select(Function(s) Val(s)).ToArray
             If String.IsNullOrEmpty(out) Then
-                out = [in].TrimFileExt & ".cluster.csv"
+                out = [in].TrimSuffix & ".cluster.csv"
             End If
         Else
-            nums = [in].Split(","c).ToArray(Function(s) Val(s.Trim))
+            nums = [in].Split(","c).Select(Function(s) Val(s.Trim)).ToArray
             If String.IsNullOrEmpty(out) Then
                 out = App.CurrentDirectory & "/ClusterNumber.Csv"
             End If
         End If
 
-        Return nums.ValueGroups(args.GetInt32("/n")) > out
+        Return nums.ValueGroups(args.GetInt32("/n")) >> out.file
     End Function
 
     <ExportAPI("/kmeans",
@@ -91,54 +88,54 @@ Module CLI
         Return Kmeans(ds, n) > out
     End Function
 
-    <ExportAPI("/bTree.Cluster",
-               Info:="Performance a binary tree clustering operations on the input data set.",
-               Usage:="/bTree.Cluster /MAT <entity_matrix.csv> [/map <Name> /parallel /out <out.cluster.csv>]")>
-    Public Function bTreeCluster(args As CommandLine.CommandLine) As Integer
-        Dim inFile As String = args - "/MAT"
-        Dim parallel As Boolean = args.GetBoolean("/parallel")
-        Dim out As String = args.GetValue("/out", $"{inFile.TrimFileExt}.{NameOf(bTreeCluster)}.csv")
-        Dim map As String = args.GetValue("/map", "Name")
-        Dim maps As Dictionary(Of String, String) =
-            New Dictionary(Of String, String) From {{map, NameOf(EntityLDM.Name)}}
-        Dim dataSet As IEnumerable(Of EntityLDM) =
-            inFile.LoadCsv(Of EntityLDM)(maps:=maps)
-        dataSet = dataSet.TreeCluster
-        Return dataSet.SaveTo(out)
-    End Function
+    '<ExportAPI("/bTree.Cluster",
+    '           Info:="Performance a binary tree clustering operations on the input data set.",
+    '           Usage:="/bTree.Cluster /MAT <entity_matrix.csv> [/map <Name> /parallel /out <out.cluster.csv>]")>
+    'Public Function bTreeCluster(args As CommandLine.CommandLine) As Integer
+    '    Dim inFile As String = args - "/MAT"
+    '    Dim parallel As Boolean = args.GetBoolean("/parallel")
+    '    Dim out As String = args.GetValue("/out", $"{inFile.TrimFileExt}.{NameOf(bTreeCluster)}.csv")
+    '    Dim map As String = args.GetValue("/map", "Name")
+    '    Dim maps As Dictionary(Of String, String) =
+    '        New Dictionary(Of String, String) From {{map, NameOf(EntityLDM.Name)}}
+    '    Dim dataSet As IEnumerable(Of EntityLDM) =
+    '        inFile.LoadCsv(Of EntityLDM)(maps:=maps)
+    '    dataSet = dataSet.TreeCluster
+    '    Return dataSet.SaveTo(out)
+    'End Function
 
-    ''' <summary>
-    ''' Converts the bTree clustering result into the cytoscape network model file.
-    ''' </summary>
-    ''' <param name="args"></param>
-    ''' <returns></returns>
-    <ExportAPI("/bTree",
-               Info:="Converts the bTree clustering result into the cytoscape network model file.",
-               Usage:="/bTree /cluster <tree.cluster.csv> [/out <outDIR>]")>
-    Public Function bTree(args As CommandLine.CommandLine) As Integer
-        Dim inFile As String = args - "/cluster"
-        Dim out As String = ("/out" <= args) ^ $"{inFile.TrimFileExt}-bTree/"
-        Dim clusters As IEnumerable(Of EntityLDM) = inFile.LoadCsv(Of EntityLDM)
-        Dim tree As Network = clusters.bTreeNET
-        Return tree > out
-    End Function
+    '''' <summary>
+    '''' Converts the bTree clustering result into the cytoscape network model file.
+    '''' </summary>
+    '''' <param name="args"></param>
+    '''' <returns></returns>
+    '<ExportAPI("/bTree",
+    '           Info:="Converts the bTree clustering result into the cytoscape network model file.",
+    '           Usage:="/bTree /cluster <tree.cluster.csv> [/out <outDIR>]")>
+    'Public Function bTree(args As CommandLine.CommandLine) As Integer
+    '    Dim inFile As String = args - "/cluster"
+    '    Dim out As String = ("/out" <= args) ^ $"{inFile.TrimFileExt}-bTree/"
+    '    Dim clusters As IEnumerable(Of EntityLDM) = inFile.LoadCsv(Of EntityLDM)
+    '    Dim tree As Network = clusters.bTreeNET
+    '    Return tree > out
+    'End Function
 
-    ''' <summary>
-    ''' Partitioning the binary tree cluster by using the clustering path.
-    ''' </summary>
-    ''' <param name="args"></param>
-    ''' <returns></returns>
-    <ExportAPI("/bTree.Partitioning",
-               Info:="Partitioning the binary tree cluster by using the clustering path.",
-               Usage:="/bTree.Partitioning /cluster <clusters.csv> [/depth <-1> /out <partions.csv>]")>
-    <ParameterInfo("/depth", True,
-                   Description:="The maximum depth of the clustering path using for the partitioning operations. 
-Default or value zero or negative is using the shortest path in the cluster entity.")>
-    Public Function bTreePartitioning(args As CommandLine.CommandLine) As Integer
-        Dim inFile As String = args - "/cluster"
-        Dim depth As Integer = args.GetValue("/depth", -1)
-        Dim out As String = args.GetValue("/out", inFile.TrimFileExt & $".{depth}.csv")
-        Dim result As List(Of Partition) = inFile.LoadCsv(Of EntityLDM).Partitioning(depth)
-        Return result > out
-    End Function
+    '    ''' <summary>
+    '    ''' Partitioning the binary tree cluster by using the clustering path.
+    '    ''' </summary>
+    '    ''' <param name="args"></param>
+    '    ''' <returns></returns>
+    '    <ExportAPI("/bTree.Partitioning",
+    '               Info:="Partitioning the binary tree cluster by using the clustering path.",
+    '               Usage:="/bTree.Partitioning /cluster <clusters.csv> [/depth <-1> /out <partions.csv>]")>
+    '    <Argument("/depth", True,
+    '                   Description:="The maximum depth of the clustering path using for the partitioning operations. 
+    'Default or value zero or negative is using the shortest path in the cluster entity.")>
+    '    Public Function bTreePartitioning(args As CommandLine.CommandLine) As Integer
+    '        Dim inFile As String = args - "/cluster"
+    '        Dim depth As Integer = args.GetValue("/depth", -1)
+    '        Dim out As String = args.GetValue("/out", inFile.TrimFileExt & $".{depth}.csv")
+    '        Dim result As List(Of Partition) = inFile.LoadCsv(Of EntityLDM).Partitioning(depth)
+    '        Return result > out
+    '    End Function
 End Module
---
+++ Microsoft.VisualBasic.Core/My/File.vb
@@ -0,0 +1,121 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Threading
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Language.UnixBash.FileSystem
+Imports Microsoft.VisualBasic.Text
+
+Namespace My
+
+    ''' <summary>
+    ''' A internal file handler
+    ''' </summary>
+    Module File
+
+        ReadOnly opendHandles As Dictionary(Of Integer, FileHandle)
+
+        Sub New()
+            opendHandles = New Dictionary(Of Integer, FileHandle)
+        End Sub
+
+        Friend Function __getHandle(path As Integer) As FileHandle
+            If Not opendHandles.ContainsKey(path) Then
+                Throw New ObjectNotFoundException($"Path {path} pointer to a null file handle!")
+            Else
+                Return opendHandles(path)
+            End If
+        End Function
+
+        ''' <summary>
+        ''' 不存在的文件句柄会在这个函数之中被忽略掉
+        ''' </summary>
+        ''' <param name="file%"></param>
+        Public Sub Close(file%)
+            SyncLock opendHandles
+                With opendHandles
+                    If .ContainsKey(file) Then
+                        Call .Remove(file)
+                    Else
+                        ' Do Nothing.
+                    End If
+                End With
+            End SyncLock
+        End Sub
+
+        Dim __handle As Value(Of Integer) = New Value(Of Integer)(Integer.MinValue)
+
+        ''' <summary>
+        ''' Open a file system handle
+        ''' </summary>
+        ''' <param name="file"></param>
+        ''' <param name="encoding"></param>
+        ''' <returns></returns>
+        <Extension> Public Function OpenHandle(file$, Optional encoding As Encodings = Encodings.UTF8) As Integer
+            If String.IsNullOrEmpty(file) Then
+                Throw New NullReferenceException("File handle null pointer!")
+            End If
+
+            SyncLock opendHandles
+                SyncLock __handle
+                    __handle.Value += 1
+
+                    Dim handle As New FileHandle With {
+                        .encoding = encoding.CodePage,
+                        .FileName = file,
+                        .handle = __handle.Value
+                    }
+
+                    Call opendHandles.Add(__handle.Value, handle)
+                    Call FileIO.FileSystem.CreateDirectory(file.ParentPath)
+
+                    Return __handle.Value
+                End SyncLock
+            End SyncLock
+        End Function
+
+        Public Function OpenTemp() As Integer
+            Return OpenHandle(App.GetAppSysTempFile(App.Process.Id))
+        End Function
+
+        ''' <summary>
+        ''' Is this file opened
+        ''' </summary>
+        ''' <param name="filename"></param>
+        ''' <returns></returns>
+        <Extension> Public Function FileOpened(filename As String) As Boolean
+            If Not filename.FileExists Then
+                Return False
+            Else
+                Try
+                    Using file As New IO.FileStream(filename, IO.FileMode.OpenOrCreate)
+                    End Using
+
+                    Return False
+                Catch ex As Exception
+                    Return True
+                Finally
+                End Try
+            End If
+        End Function
+
+        ''' <summary>
+        ''' 等待文件句柄的关闭
+        ''' </summary>
+        ''' <param name="file$"></param>
+        ''' <param name="timeout">等待的时间长度，默认为100s，单位为毫秒</param>
+        ''' <returns></returns>
+        Public Function Wait(file$, Optional timeout& = 1000 * 100) As Boolean
+            Dim sw As Stopwatch = Stopwatch.StartNew
+
+            Do While file.FileOpened
+                Call Thread.Sleep(1)
+
+                If sw.ElapsedMilliseconds >= timeout Then
+                    Return False
+                End If
+            Loop
+
+            Return True
+        End Function
+    End Module
+End Namespace
+
