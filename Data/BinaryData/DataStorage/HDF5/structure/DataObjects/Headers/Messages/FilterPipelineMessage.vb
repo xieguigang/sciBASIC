e42eff3066961a2741474eac7ed7713cb1e3d1b9
xieguigang <xie.guigang@gcmodeller.org>
2019-05-21 14:29:29 +0800

    FilterPipelineMessage

---
+++ Data/BinaryData/DataStorage/HDF5/structure/DataObjects/Headers/Messages/FilterPipelineMessage.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.VisualBasic.Data.IO.HDF5.IO
+
+Namespace HDF5.[Structure]
+
+    Public Class FilterPipelineMessage : Inherits Message
+
+        Public ReadOnly Property version As Integer
+        Public ReadOnly Property numberOfFilters As Integer
+
+
+        Public Sub New([in] As BinaryReader, sb As Superblock, address As Long)
+            MyBase.New(address)
+
+            Me.version = [in].readByte()
+            Me.numberOfFilters = [in].readByte
+
+            Throw New NotImplementedException
+        End Sub
+    End Class
+End Namespace
