15b0220b0739f644c41bf39fa47f58b6deffc980
xieguigang <xie.guigang@gcmodeller.org>
2019-05-21 09:47:56 +0800

    A internal pointer in a hdf5 binary data file.

---
+++ Data/BinaryData/DataStorage/HDF5/HDF5Ptr.vb
@@ -0,0 +1,24 @@
+﻿Namespace HDF5
+
+    ''' <summary>
+    ''' A internal pointer in a hdf5 binary data file.
+    ''' </summary>
+    Public MustInherit Class HDF5Ptr
+
+        Protected m_address&
+
+        Public ReadOnly Property address As Long
+            Get
+                Return m_address
+            End Get
+        End Property
+
+        Sub New(address&)
+            Me.m_address = address
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return $"&{address} {Me.GetType.Name}"
+        End Function
+    End Class
+End Namespace
