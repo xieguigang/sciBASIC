1701035386004d2f272abcfb6d80ca35f33b2fe7
xieguigang <xie.guigang@gcmodeller.org>
2019-05-22 10:53:42 +0800

    fix for invalid type

--- Data/BinaryData/DataStorage/HDF5/FileDump.vb
+++ Data/BinaryData/DataStorage/HDF5/FileDump.vb
@@ -1,50 +1,52 @@
 ﻿#Region "Microsoft.VisualBasic::c9e97eaf70b6e4ebe19dd5238d04f05e, Data\BinaryData\DataStorage\HDF5\FileDump.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module FileDump
-    ' 
-    '         Sub: CreateFileDump
-    ' 
-    '     Interface IFileDump
-    ' 
-    '         Sub: printValues
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module FileDump
+' 
+'         Sub: CreateFileDump
+' 
+'     Interface IFileDump
+' 
+'         Sub: printValues
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.IO
 Imports System.Runtime.CompilerServices
+Imports System.Text
 
 Namespace HDF5
 
@@ -58,6 +60,13 @@ Namespace HDF5
 
     Public Interface IFileDump
 
-        Sub printValues(console As System.IO.StringWriter)
+        ''' <summary>
+        ''' 可以通过下面的两种方法构建出所需要的<paramref name="console"/>参数
+        ''' 
+        ''' + <see cref="StringBuilder"/> => new <see cref="TextWriter"/>
+        ''' + <see cref="StreamWriter"/>
+        ''' </summary>
+        ''' <param name="console"></param>
+        Sub printValues(console As TextWriter)
     End Interface
 End Namespace
