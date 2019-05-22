c1078113c8df82b1a5e406c7dfbf8c5059e1dd8d
xieguigang <xie.guigang@gcmodeller.org>
2019-05-22 11:37:32 +0800

    hdf5 file

---
+++ Data/BinaryData/DataStorage/HDF5/HDF5File.vb
@@ -0,0 +1,84 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.IO.HDF5.IO
+Imports Microsoft.VisualBasic.Data.IO.HDF5.Structure
+Imports BinaryReader = Microsoft.VisualBasic.Data.IO.HDF5.IO.BinaryReader
+
+Namespace HDF5
+
+    Public Class HDF5File : Implements IDisposable
+
+        ReadOnly reader As BinaryReader
+        ReadOnly fileName As String
+
+        Public ReadOnly Property Superblock As Superblock
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
+            Get
+                Return New Superblock(reader, Scan0)
+            End Get
+        End Property
+
+        Public ReadOnly Property rootObjects As Dictionary(Of String, DataObjectFacade)
+
+        Default Public ReadOnly Property GetObject(symbolName As String) As DataObjectFacade
+            Get
+                Return rootObjects _
+                    .Keys _
+                    .First(Function(name) name.TextEquals(symbolName)) _
+                    .GetValueOrDefault(rootObjects)
+            End Get
+        End Property
+
+        Sub New(fileName As String)
+            Me.reader = New BinaryFileReader(fileName)
+            Me.fileName = fileName.FileName
+        End Sub
+
+        Private Sub parseHeader()
+            Dim sb As Superblock = Me.Superblock
+            Dim rootSymbolTableEntry As SymbolTableEntry = sb.rootGroupSymbolTableEntry
+            Dim objectFacade As New DataObjectFacade(Me.reader, sb, "root", rootSymbolTableEntry.objectHeaderAddress)
+            Dim rootGroup As New Group(Me.reader, sb, objectFacade)
+            Dim objects As List(Of DataObjectFacade) = rootGroup.objects
+
+            _rootObjects = objects.ToDictionary(Function(o) o.symbolName)
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return fileName
+        End Function
+
+#Region "IDisposable Support"
+        Private disposedValue As Boolean ' 要检测冗余调用
+
+        ' IDisposable
+        Protected Overridable Sub Dispose(disposing As Boolean)
+            If Not disposedValue Then
+                If disposing Then
+                    ' TODO: 释放托管状态(托管对象)。
+                    Call reader.Dispose()
+                End If
+
+                ' TODO: 释放未托管资源(未托管对象)并在以下内容中替代 Finalize()。
+                ' TODO: 将大型字段设置为 null。
+            End If
+            disposedValue = True
+        End Sub
+
+        ' TODO: 仅当以上 Dispose(disposing As Boolean)拥有用于释放未托管资源的代码时才替代 Finalize()。
+        'Protected Overrides Sub Finalize()
+        '    ' 请勿更改此代码。将清理代码放入以上 Dispose(disposing As Boolean)中。
+        '    Dispose(False)
+        '    MyBase.Finalize()
+        'End Sub
+
+        ' Visual Basic 添加此代码以正确实现可释放模式。
+        Public Sub Dispose() Implements IDisposable.Dispose
+            ' 请勿更改此代码。将清理代码放入以上 Dispose(disposing As Boolean)中。
+            Dispose(True)
+            ' TODO: 如果在以上内容中替代了 Finalize()，则取消注释以下行。
+            ' GC.SuppressFinalize(Me)
+        End Sub
+#End Region
+
+    End Class
+End Namespace
