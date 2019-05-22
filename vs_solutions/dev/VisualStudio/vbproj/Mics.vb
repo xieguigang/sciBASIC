1f88d46178ab3aa3c2d64be978b616d857533897
xieguigang <xie.guigang@gcmodeller.org>
2019-05-22 19:09:24 +0800

    split source files

---
+++ vs_solutions/dev/VisualStudio/vbproj/Mics.vb
@@ -0,0 +1,31 @@
+﻿
+Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Public Class Target
+    <XmlAttribute>
+    Public Property Name As String
+End Class
+
+Public Class Import
+
+    <XmlAttribute> Public Property Project As String
+    <XmlAttribute> Public Property Condition As String
+    <XmlAttribute> Public Property Label As String
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
+
+Public Class ConditionValue
+
+    <XmlAttribute>
+    Public Property Condition As String
+    <XmlText>
+    Public Property value As String
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
--- vs_solutions/dev/VisualStudio/vbproj/Project.vb
+++ vs_solutions/dev/VisualStudio/vbproj/Project.vb
@@ -124,30 +124,3 @@ Public Class Project : Implements ISaveHandle, IFileReference
         Return Save(path, encoding.CodePage)
     End Function
 End Class
-
-Public Class Target
-    <XmlAttribute> Public Property Name As String
-End Class
-
-Public Class Import
-
-    <XmlAttribute> Public Property Project As String
-    <XmlAttribute> Public Property Condition As String
-    <XmlAttribute> Public Property Label As String
-
-    Public Overrides Function ToString() As String
-        Return Me.GetJson
-    End Function
-End Class
-
-Public Class ConditionValue
-
-    <XmlAttribute>
-    Public Property Condition As String
-    <XmlText>
-    Public Property value As String
-
-    Public Overrides Function ToString() As String
-        Return Me.GetJson
-    End Function
-End Class
