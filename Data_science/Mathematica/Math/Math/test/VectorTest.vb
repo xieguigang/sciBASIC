b528ff2e3072a986f0556c0de4b1cf05fa025de0
xieguigang <xie.guigang@gcmodeller.org>
2019-05-30 15:57:54 +0800

    numpy.Mean test success

--- Data_science/Mathematica/Math/Math/test/VectorTest.vb
+++ Data_science/Mathematica/Math/Math/test/VectorTest.vb
@@ -40,6 +40,7 @@
 #End Region
 
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports numpy = Microsoft.VisualBasic.Math
 
 Module VectorTest
 
@@ -47,6 +48,19 @@ Module VectorTest
         Dim x As Double() = {423, 4, 2, 4, 24, 2, 3, 423, 4, 2, 3, 4, 23, 4, 2, 4, 2, 3, 4, 2, 4, 2}
         Dim y As Vector = Vector.Call(Of Double)(New Func(Of Double, Double, Double)(AddressOf Math.Log), x, 2).ToArray
         Dim z As Vector = Vector.Call(Function(a, b) a / b, x, 1000000)
+
+        Call numpyTest()
+
+        Pause()
+    End Sub
+
+    Sub numpyTest()
+        Dim a = {New Vector({1.0#, 2.0#}), New Vector({3.0#, 4.0#})}
+
+        Console.WriteLine(numpy.Mean(a).ToString)
+        Console.WriteLine(numpy.Mean(a, axis:=0).ToString)
+        Console.WriteLine(numpy.Mean(a, axis:=1).ToString)
+
 
         Pause()
     End Sub
