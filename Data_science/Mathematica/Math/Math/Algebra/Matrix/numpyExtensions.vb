cea3b3844b165848376d100f68b505df131df9bd
xieguigang <xie.guigang@gcmodeller.org>
2019-05-30 14:47:15 +0800

    Update numpyExtensions.vb
    make apply function more generic

--- Data_science/Mathematica/Math/Math/Algebra/Matrix/numpyExtensions.vb
+++ Data_science/Mathematica/Math/Math/Algebra/Matrix/numpyExtensions.vb
@@ -17,11 +17,15 @@ Imports Microsoft.VisualBasic.Math.LinearAlgebra
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     <Extension>
     Public Function Mean(matrix As IEnumerable(Of Vector), Optional axis% = -1) As Vector
-        Return matrix.Apply(Function(x) x.Average, axis:=axis)
+        Return matrix.Apply(Function(x) x.Average, axis:=axis, aggregate:=AddressOf AsVector)
     End Function
 
     <Extension>
-    Public Function Apply(matrix As IEnumerable(Of Vector), math As Func(Of IEnumerable(Of Double), Double), axis%) As Vector
+    Public Function Apply(Of T, Tout)(matrix As IEnumerable(Of Vector),
+                                      math As Func(Of IEnumerable(Of Double), T),
+                                      axis%,
+                                      aggregate As Func(Of IEnumerable(Of T), Tout)) As Tout
+
         ' >>> a = np.array([[1, 2], [3, 4]])
         ' >>> np.mean(a)
         ' 2.5
@@ -30,9 +34,9 @@ Imports Microsoft.VisualBasic.Math.LinearAlgebra
         ' >>> np.mean(a, axis=1)
         ' Array([ 1.5, 3.5])
         If axis < 0 Then
-            Return New Double() {math(matrix.IteratesALL)}
+            Return aggregate({math(matrix.IteratesALL)})
         ElseIf axis = 0 Then
-            Return Iterator Function() As IEnumerable(Of Double)
+            Return Iterator Function() As IEnumerable(Of T)
                        Dim data As Vector() = matrix.ToArray
                        Dim columns As Integer = data(Scan0).Length
 #Disable Warning
@@ -40,11 +44,11 @@ Imports Microsoft.VisualBasic.Math.LinearAlgebra
                            Yield math(data.Select(Function(row) row(i)))
                        Next
 #Enable Warning
-                   End Function().AsVector
+                   End Function().doCall(aggregate)
         ElseIf axis = 1 Then
             Return matrix _
                 .Select(Function(r) math(r)) _
-                .AsVector
+                .doCall(aggregate)
         Else
             Throw New NotImplementedException
         End If
@@ -52,7 +56,19 @@ Imports Microsoft.VisualBasic.Math.LinearAlgebra
 
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     <Extension>
+    Private Function doCall(Of T, Tout)(input As T, aggregate As Func(Of T, Tout)) As Tout
+        Return aggregate(input)
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    <Extension>
     Public Function Std(matrix As IEnumerable(Of Vector), Optional axis% = -1) As Vector
-        Return matrix.Apply(Function(x) x.StdError, axis:=axis)
+        Return matrix.Apply(Function(x) x.StdError, axis:=axis, aggregate:=AddressOf AsVector)
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    <Extension>
+    Public Function Sort(matrix As IEnumerable(Of Vector), Optional axis% = -1) As IEnumerable(Of Vector)
+        Return matrix.Apply(Function(x) x.Sort, axis:=axis, aggregate:=Function(collect) collect.Select(AddressOf AsVector))
     End Function
 End Module
