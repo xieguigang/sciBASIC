2f89f1c4996b59d5ccdeeb7760cf78f301de14eb
xieguigang <xie.guigang@gcmodeller.org>
2019-06-03 13:49:51 +0800

    snapshot sotrage

---
+++ Data_science/MachineLearning/MachineLearning/NeuralNetwork/StoreProcedure/Snapshot.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace NeuralNetwork.StoreProcedure
+
+    ''' <summary>
+    ''' 对于大型的神经网络而言，反复的构建XML数据模型将会额外的消耗掉大量的时间，导致训练的时间过长
+    ''' 在这里通过这个持久性的快照对象来减少这种反复创建XML数据快照的问题
+    ''' </summary>
+    Public Class Snapshot
+
+        ReadOnly snapshot As NeuralNetwork
+        ReadOnly source As Network
+
+        Sub New(model As Network)
+            source = model
+            snapshot = CreateSnapshot.TakeSnapshot(model, 0)
+        End Sub
+
+        Public Function UpdateSnapshot() As Snapshot
+
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function WriteIntegralXML(path As String) As Boolean
+            Return snapshot.GetXml.SaveTo(path, throwEx:=False)
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function WriteScatteredParts(directory As String) As Boolean
+            Return snapshot.ScatteredStore(store:=directory)
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return source.ToString
+        End Function
+    End Class
+End Namespace
