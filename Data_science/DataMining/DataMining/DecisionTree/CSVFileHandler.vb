349d4a3381e5034bd6db6e0bfaf415f2ff2c36c8
xieguigang <xie.guigang@gcmodeller.org>
2019-06-13 18:38:57 +0800

    add Bootstraping samples for  RandomForests

--- Data_science/DataMining/DataMining/DecisionTree/CSVFileHandler.vb
+++ Data_science/DataMining/DataMining/DecisionTree/CSVFileHandler.vb
@@ -1,12 +1,20 @@
-Imports System.IO
-Imports System.Text
+﻿Namespace DecisionTree
 
-Namespace DecisionTree
-
+    ''' <summary>
+    ''' 分类用的对象实例
+    ''' </summary>
     Public Class Entity
 
         Public Property variables As String()
+        ''' <summary>
+        ''' 分类结果
+        ''' </summary>
+        ''' <returns></returns>
         Public Property decisions As String
+
+        Public Overrides Function ToString() As String
+            Return decisions
+        End Function
 
     End Class
 End Namespace
