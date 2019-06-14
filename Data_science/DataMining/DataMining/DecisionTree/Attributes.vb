a545e6f66034f9daa745e425f1c76dfeeb205990
xieguigang <xie.guigang@gcmodeller.org>
2019-06-14 15:54:04 +0800

    improvements of the data models

--- Data_science/DataMining/DataMining/DecisionTree/Attributes.vb
+++ Data_science/DataMining/DataMining/DecisionTree/Attributes.vb
@@ -1,3 +1,4 @@
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace DecisionTree
@@ -8,11 +9,11 @@ Namespace DecisionTree
     Public Class Attributes
 
         Public ReadOnly Property name As String
-        Public ReadOnly Property differentAttributeNames As List(Of String)
+        Public ReadOnly Property differentAttributeNames As String()
 
         Public Property InformationGain As Double
 
-        Public Sub New(name As String, differentAttributenames As List(Of String))
+        Public Sub New(name As String, differentAttributenames$())
             Me.name = name
             Me.differentAttributeNames = differentAttributenames
         End Sub
@@ -21,19 +22,12 @@ Namespace DecisionTree
             Return $"Dim {name} As {differentAttributeNames.GetJson} = {InformationGain}"
         End Function
 
-        Public Shared Function GetDifferentAttributeNamesOfColumn(data As DataTable, columnIndex As Integer) As List(Of String)
-            Dim differentAttributes As New List(Of String)()
-
-            For i As Integer = 0 To data.Rows.Count - 1
-                Dim index = i
-                Dim found = differentAttributes.Any(Function(t) t.ToUpper().Equals(data.Rows(index)(columnIndex).ToString().ToUpper()))
-
-                If Not found Then
-                    differentAttributes.Add(data.Rows(i)(columnIndex).ToString())
-                End If
-            Next
-
-            Return differentAttributes
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Function GetDifferentAttributeNamesOfColumn(data As DataTable, columnIndex As Integer) As String()
+            Return data.rows _
+                .Select(Function(d) d(columnIndex)) _
+                .Distinct _
+                .ToArray
         End Function
     End Class
 End Namespace
--- Data_science/DataMining/DataMining/DecisionTree/CSVFileHandler.vb
+++ Data_science/DataMining/DataMining/DecisionTree/CSVFileHandler.vb
@@ -35,12 +35,27 @@ Namespace DecisionTree
         ''' <returns></returns>
         Public Property rows As Entity()
 
+        ''' <summary>
+        ''' Get the property fields count
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property columns As Integer
+            Get
+                Return headers.Length
+            End Get
+        End Property
+
         Default Public ReadOnly Property GetRow(index As Integer) As Entity
             <MethodImpl(MethodImplOptions.AggressiveInlining)>
             Get
                 Return rows(index)
             End Get
         End Property
+
+        Public Sub RemoveColumn(index As Integer)
+            headers = headers.Delete(index)
+            rows.DoEach(Sub(r) Call r.entityVector.Delete(index))
+        End Sub
 
         Public Overrides Function ToString() As String
             Return headers.GetJson
