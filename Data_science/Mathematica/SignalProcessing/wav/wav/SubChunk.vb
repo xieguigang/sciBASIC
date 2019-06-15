af9d5bdeac7566f7b000d40b4aa0f0ee34646630
xieguigang <xie.guigang@gcmodeller.org>
2019-06-15 16:12:52 +0800

    split source file

--- Data_science/Mathematica/SignalProcessing/wav/wav/SubChunk.vb
+++ Data_science/Mathematica/SignalProcessing/wav/wav/SubChunk.vb
@@ -5,7 +5,7 @@ Module Module1
     Sub Main()
 
         Dim wav = "E:\VB_GamePads\runtime\sciBASIC#\Data_science\Mathematica\SignalProcessing\wav\SND_FISH_TROPICAL_03.wav".OpenBinaryReader
-        Dim header As wave = wave.ParseHeader(wav)
+        Dim header As File = File.ParseHeader(wav)
 
 
 
@@ -14,36 +14,9 @@ Module Module1
 
 End Module
 
-Public Class wave
 
-    Public Property magic As String
-    Public Property fileSize As Integer
-    Public Property format As String
 
-    Public Property fmt As FMTSubChunk
-    Public Property data As DataSubChunk
-
-    Public Shared Function ParseHeader(wav As BinaryDataReader) As wave
-        Return New wave With {
-            .magic = wav.ReadString(4),
-            .fileSize = wav.ReadInt32,
-            .format = wav.ReadString(4),
-            .fmt = New FMTSubChunk With {
-             .ChunkID = wav.ReadString(4),
-            .ChunkSize = wav.ReadInt32,
-            .audioFormat = wav.ReadInt16,
-            .Channels = wav.ReadInt16,
-            .SampleRate = wav.ReadInt32,
-            .ByteRate = wav.ReadInt32,
-            .BlockAlign = wav.ReadInt16,
-            .BitsPerSample = wav.ReadInt16
-        },
-        .data = DataSubChunk.ParseData(wav)
-        }
-    End Function
-End Class
-
-Public Class SubChunk
+Public MustInherit Class SubChunk
 
     Public Property ChunkID As String
     Public Property ChunkSize As Integer
