6e53f4b4bc2a40ea10e904dc26ab100922ca020f
xieguigang <xie.guigang@gcmodeller.org>
2019-06-20 11:07:45 +0800

    Update Extensions.vb

--- gr/network-visualization/Datavisualization.Network/Graph/Extensions.vb
+++ gr/network-visualization/Datavisualization.Network/Graph/Extensions.vb
@@ -69,7 +69,6 @@ Namespace Graph
 
         <Extension>
         Public Sub ApplyAnalysis(ByRef net As NetworkGraph)
-
             For Each node In net.vertex
                 node.data.Neighbours = net.GetNeighbours(node.Label).ToArray
             Next
