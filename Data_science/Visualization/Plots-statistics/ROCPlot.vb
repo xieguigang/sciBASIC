754ad0c444457ce6a84c716125ff6f5544159fad
xieguigang <xie.guigang@gcmodeller.org>
2019-06-25 15:56:22 +0800

    ROC plot from cli

--- Data_science/Visualization/Plots-statistics/ROCPlot.vb
+++ Data_science/Visualization/Plots-statistics/ROCPlot.vb
@@ -1,41 +1,41 @@
 ﻿#Region "Microsoft.VisualBasic::f78171b16b18b4e55437b84dd033d40d, Data_science\Visualization\Plots-statistics\ROCPlot.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module ROCPlot
-    ' 
-    '     Function: CreateSerial, Plot
-    ' 
-    ' /********************************************************************************/
+' Module ROCPlot
+' 
+'     Function: CreateSerial, Plot
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -43,6 +43,7 @@ Imports System.Drawing
 Imports System.Drawing.Drawing2D
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Legend
+Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.DataMining
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
@@ -70,8 +71,26 @@ Public Module ROCPlot
             .lineType = DashStyle.Solid,
             .PointSize = 5,
             .Shape = LegendStyles.Triangle,
-            .pts = points.OrderBy(Function(p) p.pt.X).ToArray
+            .pts = points _
+                .OrderBy(Function(p) p.pt.X) _
+                .ToArray
         }
+    End Function
+
+    <Extension>
+    Public Function CreateSerial(test As IEnumerable(Of DataSet)) As SerialData
+        Dim data As DataSet() = test.ToArray
+        Dim specificity = data(Scan0).Properties.Keys.First(Function(key) key.TextEquals(NameOf(Validation.Specificity)))
+        Dim sensibility = data(Scan0).Properties.Keys.First(Function(key) key.TextEquals(NameOf(Validation.Sensibility)))
+
+        Return data _
+            .Select(Function(d)
+                        Return New Validation With {
+                            .Specificity = d(specificity),
+                            .Sensibility = d(sensibility)
+                        }
+                    End Function) _
+            .CreateSerial
     End Function
 
     Public Function Plot(roc As SerialData,
