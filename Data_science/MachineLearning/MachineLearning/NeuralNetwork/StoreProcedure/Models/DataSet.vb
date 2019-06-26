255e02efd35bca3707ce9f15b0490e23239b6d03
xieguigang <xie.guigang@gcmodeller.org>
2019-06-26 10:24:15 +0800

    add output names

--- Data_science/MachineLearning/MachineLearning/NeuralNetwork/StoreProcedure/Models/DataSet.vb
+++ Data_science/MachineLearning/MachineLearning/NeuralNetwork/StoreProcedure/Models/DataSet.vb
@@ -54,7 +54,6 @@ Imports System.Runtime.CompilerServices
 Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.Language
-Imports Microsoft.VisualBasic.Text.Xml.Models
 
 Namespace NeuralNetwork.StoreProcedure
 
@@ -76,52 +75,7 @@ Namespace NeuralNetwork.StoreProcedure
         <XmlElement("normalization")>
         Public Property NormalizeMatrix As NormalizeMatrix
 
-        Public Class SampleList : Inherits ListOf(Of Sample)
-
-            ''' <summary>
-            ''' 样本列表
-            ''' </summary>
-            ''' <returns></returns>
-            <XmlElement("sample")> Public Property items As Sample()
-
-            Default Public ReadOnly Property Item(index As Integer) As Sample
-                <MethodImpl(MethodImplOptions.AggressiveInlining)>
-                Get
-                    Return items(index)
-                End Get
-            End Property
-
-            <MethodImpl(MethodImplOptions.AggressiveInlining)>
-            Protected Overrides Function getSize() As Integer
-                Return items?.Length
-            End Function
-
-            Public Iterator Function [Select](Of T)(project As Func(Of Sample, Integer, T)) As IEnumerable(Of T)
-                Dim i As VBInteger = Scan0
-
-                For Each item As Sample In items
-                    Yield project(item, ++i)
-                Next
-            End Function
-
-            <MethodImpl(MethodImplOptions.AggressiveInlining)>
-            Public Shared Widening Operator CType(samples As Sample()) As SampleList
-                Return New SampleList With {
-                    .items = samples
-                }
-            End Operator
-
-            <MethodImpl(MethodImplOptions.AggressiveInlining)>
-            Public Shared Widening Operator CType(samples As List(Of Sample)) As SampleList
-                Return New SampleList With {
-                    .items = samples.ToArray
-                }
-            End Operator
-
-            Protected Overrides Function getCollection() As IEnumerable(Of Sample)
-                Return items
-            End Function
-        End Class
+        Public Property output As String()
 
         ''' <summary>
         ''' 样本的矩阵大小：``[属性长度, 样本数量]``
