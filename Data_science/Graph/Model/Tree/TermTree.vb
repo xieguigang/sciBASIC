d6efc3a464facfec04f13cbea3e6a8a1611f5454
xieguigang <xie.guigang@gcmodeller.org>
2019-07-09 08:56:46 +0800

    visit a string key tree

---
+++ Data_science/Graph/Model/Tree/TermTree.vb
@@ -0,0 +1,22 @@
+﻿''' <summary>
+''' A tree with string term as key
+''' </summary>
+Public Class TermTree(Of T) : Inherits Tree(Of T, String)
+
+    Default Public Overloads Property Child(path As String) As T
+        Get
+            Return Visit(path.Split("/"c)).Data
+        End Get
+        Set(value As T)
+            Visit(path.Split("/"c)).Data = value
+        End Set
+    End Property
+
+    Public Function Visit(path As String()) As TermTree(Of T)
+        If path.Length = 1 Then
+            Return MyBase.Child(path(Scan0))
+        Else
+            Return Visit(path.Skip(1).ToArray)
+        End If
+    End Function
+End Class
