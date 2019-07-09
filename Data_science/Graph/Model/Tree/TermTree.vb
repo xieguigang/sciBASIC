d36d18440ca1e0d0d8b8f51e1456c245a0840344
xieguigang <xie.guigang@gcmodeller.org>
2019-07-09 09:14:17 +0800

    add term tree node

--- Data_science/Graph/Model/Tree/TermTree.vb
+++ Data_science/Graph/Model/Tree/TermTree.vb
@@ -1,4 +1,5 @@
-﻿''' <summary>
+﻿Imports System.Runtime.CompilerServices
+''' <summary>
 ''' A tree with string term as key
 ''' </summary>
 Public Class TermTree(Of T) : Inherits Tree(Of T, String)
@@ -17,6 +18,44 @@ Public Class TermTree(Of T) : Inherits Tree(Of T, String)
             Return MyBase.Child(path(Scan0))
         Else
             Return Visit(path.Skip(1).ToArray)
+        End If
+    End Function
+
+    Private Function newChild(name As String, value As T) As TermTree(Of T)
+        Return New TermTree(Of T) With {
+            .Data = value,
+            .ID = Me.ID + Childs.Count,
+            .Label = name,
+            .Parent = Me,
+            .Childs = New Dictionary(Of String, Tree(Of T, String))
+        }
+    End Function
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="path">Path tokens should seperated with delimiter ``/``.</param>
+    ''' <param name="value"></param>
+    ''' <returns></returns>
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function Add(path As String, value As T) As TermTree(Of T)
+        Return Add(path.Split("/"c), value)
+    End Function
+
+    Public Function Add(path As String(), value As T) As TermTree(Of T)
+        Dim next$ = path(Scan0)
+
+        If path.Length = 1 Then
+            Childs.Add([next], newChild([next], value))
+
+            ' return this new tree leaf
+            Return Childs([next])
+        Else
+            If Not Childs.ContainsKey([next]) Then
+                Childs.Add([next], newChild([next], Nothing))
+            End If
+
+            Return DirectCast(Childs([next]), TermTree(Of T)).Add(path.Skip(1).ToArray, value)
         End If
     End Function
 End Class
