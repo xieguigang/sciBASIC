b8f8fa8e415b249d161892b2a148e5716ee745a1
xieguigang <xie.guigang@gcmodeller.org>
2019-07-14 01:15:53 +0800

    run avi render test

---
+++ gr/network-visualization/test/testRenderAVI.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Drawing
+Imports System.Windows.Forms
+Imports Microsoft.VisualBasic.Data.visualize.Network.Canvas
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
+
+Module testRenderAVI
+
+    Sub Main()
+        Dim graph = CytoscapeExportAsGraph(
+            App.HOME & "\Resources\xcb-main-Edges.csv",
+            App.HOME & "\Resources\xcb-main-Nodes.csv")
+
+        Dim video = AVI.DoRenderVideo(graph, {1440, 900})
+
+        Call video.WriteBuffer("X:\network.avi")
+    End Sub
+End Module
