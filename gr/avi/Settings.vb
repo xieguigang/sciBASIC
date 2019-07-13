58b88a42bfeeac4f11152e57a2c8fd8970dd4086
xieguigang <xie.guigang@gcmodeller.org>
2019-07-13 22:59:55 +0800

    split into multiple source files

---
+++ gr/avi/Settings.vb
@@ -0,0 +1,6 @@
+﻿Public Class Settings
+
+    Public Property width As Integer
+    Public Property height As Integer
+
+End Class
--- gr/test/Module1.vb
+++ gr/test/Module1.vb
@@ -7,7 +7,7 @@ Module Module1
     Sub Main()
         Dim frame As Bitmap = "E:\ba3a4a086e061d95dfc331c47bf40ad163d9ca04.jpg".LoadImage
         Dim avi As New Encoder(New Settings With {.width = frame.Width, .height = frame.Height})
-        Dim stream As New AviStream(24, frame.Width, frame.Height)
+        Dim stream As New AVIStream(24, frame.Width, frame.Height)
 
         For i As Integer = 0 To 1024
             Call stream.addFrame(frame)
