fcecd3900820186eafd10fec921c9d3eb8473b7b
xieguigang <xie.guigang@gcmodeller.org>
2019-07-14 01:09:52 +0800

    render avi video

---
+++ gr/network-visualization/NetworkCanvas/AVI.vb
@@ -0,0 +1,74 @@
+﻿Imports System.Drawing.Drawing2D
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
+Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts.Interfaces
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.AVIMedia
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Scripting.Runtime
+
+Public Module AVI
+
+    <Extension>
+    Public Function DoRenderVideo(network As NetworkGraph,
+                                  frameSize As [Variant](Of Size, Integer(), String),
+                                  physics As ForceDirectedArgs,
+                                  Optional ShowLabels As Boolean = True,
+                                  Optional render3D As Boolean = False,
+                                  Optional fps As Integer = 24,
+                                  Optional drawFrames As Integer = 8192) As Encoder
+        Dim canvasSize As Size
+        Dim renderer As Renderer
+
+        If frameSize Like GetType(String) Then
+            canvasSize = frameSize.TryCast(Of String).SizeParser
+        ElseIf frameSize Like GetType(Size) Then
+            canvasSize = frameSize
+        Else
+            canvasSize = New Size With {
+                .Width = frameSize.TryCast(Of Integer()).GetValue(0),
+                .Height = frameSize.TryCast(Of Integer()).GetValue(1)
+            }
+        End If
+
+        Dim g As Graphics
+        Dim region As New Rectangle(New Point, canvasSize)
+        Dim engine As IForceDirected
+
+        If render3D Then
+            engine = New ForceDirected3D(network, physics.Stiffness, physics.Repulsion, physics.Damping)
+            renderer = New Renderer3D(Function() g, Function() region, engine)
+        Else
+            engine = New ForceDirected2D(network, physics.Stiffness, physics.Repulsion, physics.Damping)
+            renderer = New Renderer(Function() g, Function() region, engine)
+        End If
+
+        renderer.ShowLabels = ShowLabels
+
+        Dim avi As New AVIStream(fps, canvasSize.Width, canvasSize.Height)
+        Dim r As Double
+
+        For i As Integer = 0 To drawFrames
+            Using canvas As Graphics2D = canvasSize.CreateGDIDevice
+                g = canvas.Graphics
+                g.CompositingQuality = CompositingQuality.HighQuality
+                g.SmoothingMode = SmoothingMode.HighQuality
+
+                If render3D Then
+                    r += 0.4
+                    DirectCast(renderer, Renderer3D).rotate = r
+                End If
+
+                Call engine.Calculate(0.05F)
+                Call renderer.Draw(0.05F, physicsUpdate:=False)
+
+                Call avi.addFrame(canvas.ImageResource)
+            End Using
+        Next
+
+        Dim video As New Encoder(New Settings With {.width = canvasSize.Width, .height = canvasSize.Height})
+        Call video.streams.Add(avi)
+        Return video
+    End Function
+End Module
--- gr/network-visualization/NetworkCanvas/Canvas.vb
+++ gr/network-visualization/NetworkCanvas/Canvas.vb
@@ -1,50 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::1a3f6fb328b6ff3e360418f6dba7a8b5, gr\network-visualization\NetworkCanvas\Canvas.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class Canvas
-    ' 
-    '     Properties: AutoRotate, DynamicsRadius, FdgArgs, Graph, ShowLabel
-    '                 ViewDistance
-    ' 
-    '     Sub: [Stop], __invokePaint, __invokeSet, __physicsUpdates, Canvas_Disposed
-    '          Canvas_Load, Canvas_Paint, Run, SetFDGParams, SetRotate
-    '          WriteLayout
-    ' 
-    ' /********************************************************************************/
+' Class Canvas
+' 
+'     Properties: AutoRotate, DynamicsRadius, FdgArgs, Graph, ShowLabel
+'                 ViewDistance
+' 
+'     Sub: [Stop], __invokePaint, __invokeSet, __physicsUpdates, Canvas_Disposed
+'          Canvas_Load, Canvas_Paint, Run, SetFDGParams, SetRotate
+'          WriteLayout
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.ComponentModel
+Imports System.Drawing.Drawing2D
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
 Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts.Interfaces
@@ -63,18 +64,13 @@ Public Class Canvas
     Public Property Graph(Optional space3D As Boolean = False) As NetworkGraph
         Get
             If net Is Nothing Then
-                Call __invokeSet(New NetworkGraph, Me.space3D)
+                Call setupGraph(New NetworkGraph, Me.space3D)
             End If
 
             Return net
         End Get
         Set(value As NetworkGraph)
-            Me.space3D = space3D
-
-            __invokeSet(value, Me.space3D)
-#If DEBUG Then
-            ' Call value.GetJson(True).__DEBUG_ECHO
-#End If
+            Call setupGraph(value, space3D)
         End Set
     End Property
 
@@ -83,10 +79,11 @@ Public Class Canvas
     ''' </summary>
     Dim space3D As Boolean
 
-    Private Sub __invokeSet(g As NetworkGraph, space As Boolean)
+    Private Sub setupGraph(net As NetworkGraph, space As Boolean)
         Dim showLabel As Boolean = Me.ShowLabel
 
-        net = g
+        Me.net = net
+        Me.space3D = space3D
 
         If Not inputs Is Nothing Then
             Call inputs.Dispose()
@@ -95,14 +92,14 @@ Public Class Canvas
         End If
 
         If space Then
-            fdgPhysics = New ForceDirected3D(net, FdgArgs.Stiffness, FdgArgs.Repulsion, FdgArgs.Damping)
+            fdgPhysics = New ForceDirected3D(Me.net, FdgArgs.Stiffness, FdgArgs.Repulsion, FdgArgs.Damping)
             fdgRenderer = New Renderer3D(
                 Function() paper,
                 Function() New Rectangle(New Point, Size),
                 fdgPhysics, DynamicsRadius)
             inputs = New Input3D(Me)
         Else
-            fdgPhysics = New ForceDirected2D(net, FdgArgs.Stiffness, FdgArgs.Repulsion, FdgArgs.Damping)
+            fdgPhysics = New ForceDirected2D(Me.net, FdgArgs.Stiffness, FdgArgs.Repulsion, FdgArgs.Damping)
             fdgRenderer = New Renderer(
                 Function() paper,
                 Function() New Rectangle(New Point, Size),
@@ -144,8 +141,8 @@ Public Class Canvas
     ''' <summary>
     ''' The graphics updates thread.
     ''' </summary>
-    Protected Friend timer As New UpdateThread(30, AddressOf __invokePaint)
-    Protected Friend physicsEngine As New UpdateThread(30, AddressOf __physicsUpdates)
+    Protected Friend timer As New UpdateThread(30, AddressOf doPaint)
+    Protected Friend physicsEngine As New UpdateThread(30, AddressOf doPhysicsUpdates)
     ''' <summary>
     ''' The graphics rendering provider
     ''' </summary>
@@ -186,7 +183,7 @@ Public Class Canvas
         End Set
     End Property
 
-    Private Sub __invokePaint()
+    Private Sub doPaint()
         On Error Resume Next
 
         Call Me.Invoke(Sub() Call Invalidate())
@@ -198,7 +195,7 @@ Public Class Canvas
         End If
     End Sub
 
-    Private Sub __physicsUpdates()
+    Private Sub doPhysicsUpdates()
         SyncLock fdgRenderer
             If Not fdgRenderer Is Nothing Then
                 Call fdgRenderer.PhysicsEngine.Calculate(0.05F)
@@ -208,8 +205,8 @@ Public Class Canvas
 
     Private Sub Canvas_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
         paper = e.Graphics
-        paper.CompositingQuality = Drawing2D.CompositingQuality.HighQuality
-        paper.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        paper.CompositingQuality = CompositingQuality.HighQuality
+        paper.SmoothingMode = SmoothingMode.HighQuality
 
         Call fdgRenderer.Draw(0.05F, physicsUpdate:=False)
     End Sub
@@ -217,7 +214,9 @@ Public Class Canvas
     Dim inputs As InputDevice
 
     Private Sub Canvas_Load(sender As Object, e As EventArgs) Handles Me.Load
-        If Graph Is Nothing Then  ' 假若在load之间已经加载graph数据则在这里会清除掉先前的数据，所以需要判断一下
+        If Graph Is Nothing Then
+            ' 假若在load之前已经加载graph数据则在这里会清除掉先前的数据
+            ' 所以需要判断一下
             Graph = New NetworkGraph
         End If
 
