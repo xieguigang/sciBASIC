260a3d8dee50b13600f31acbb7e1ba2cb251260c
xieguigang <xie.guigang@gcmodeller.org>
2019-07-13 23:10:21 +0800

    framestream

---
+++ gr/avi/FrameStream.vb
@@ -0,0 +1,20 @@
+﻿Public Class FrameStream
+
+    Public ReadOnly Property length As Integer
+
+    ReadOnly temp$
+
+    Sub New(hashCode As Integer, buf As Byte())
+        length = buf.Length
+        temp = App.GetAppSysTempFile(".frame", App.PID, $"/{hashCode}/")
+        buf.FlushStream(temp)
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return $"{length} bytes"
+    End Function
+
+    Public Shared Narrowing Operator CType(stream As FrameStream) As Byte()
+        Return stream.temp.ReadBinary
+    End Operator
+End Class
