5c1cd9ca292b85cfec2b27fe9e14e81ecb750942
xieguigang <xie.guigang@gcmodeller.org>
2019-07-14 09:11:38 +0800

    use Reciprocal gamma function ?

--- Data_science/Darwinism/NonlinearGrid/TopologyInference/Models/Correlation.vb
+++ Data_science/Darwinism/NonlinearGrid/TopologyInference/Models/Correlation.vb
@@ -1,48 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::ac9d30dc9167c97f8dc53d80086bdca5, Data_science\Darwinism\NonlinearGrid\TopologyInference\Models\Correlation.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class Correlation
-    ' 
-    '     Properties: B, BC
-    ' 
-    '     Function: Clone, Evaluate, ToString
-    ' 
-    ' /********************************************************************************/
+' Class Correlation
+' 
+'     Properties: B, BC
+' 
+'     Function: Clone, Evaluate, ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic.MachineLearning.NeuralNetwork.Activations
+Imports Microsoft.VisualBasic.Math.Distributions
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
 Imports Microsoft.VisualBasic.Serialization
 
@@ -58,7 +58,7 @@ Public Class Correlation : Implements ICloneable(Of Correlation)
     Public Property B As Vector
     Public Property BC As Double
 
-    ReadOnly sigmoid As New BipolarSigmoid
+    ' ReadOnly sigmoid As New BipolarSigmoid
 
     ''' <summary>
     ''' sigmoid(C + B*X)
@@ -71,7 +71,11 @@ Public Class Correlation : Implements ICloneable(Of Correlation)
         ' 因为实际的X值可能会比较大，这样子累加之后的相关系数会非常高
         ' 故而会导致相应的系统变量的结果值很容易的就出现无穷大的结果值
         ' 添加了BipolarSigmoid函数的约束之后可以避免出现这种情况
-        Return sigmoid.Function(BC + (B * X).Sum)
+        ' Return BC + sigmoid.Function((B * X).Sum)
+
+        ' Reciprocal gamma function
+        Dim c As Vector = BC + 1 / MathGamma.gamma(B * X)
+        Return c.Sum
     End Function
 
     Public Overrides Function ToString() As String
--- Data_science/Mathematica/Math/Math/Distributions/Gamma.vb
+++ Data_science/Mathematica/Math/Math/Distributions/Gamma.vb
@@ -1,46 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::eb8bb1c84e0a4977f09c2bb21a9d28c0, Data_science\Mathematica\Math\Math\Distributions\Gamma.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module MathGamma
-    ' 
-    '         Function: gamma, lngamma
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module MathGamma
+' 
+'         Function: gamma, lngamma
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
 Imports sys = System.Math
 
 Namespace Distributions
@@ -122,10 +123,15 @@ Namespace Distributions
         ''' 0.8935153492876909
         ''' ```
         ''' </remarks>
-        ''' 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         <Extension>
         Public Function Γ(x As Double) As Double
             Return x.gamma
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function gamma(a As Vector) As Vector
+            Return New Vector(From x As Double In a Select gamma(x))
         End Function
 
         ''' <summary>
