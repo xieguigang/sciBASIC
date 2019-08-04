1b82215f9d0e7fa72426952228752f542beb5a36
xieguigang <xie.guigang@gcmodeller.org>
2019-08-04 22:18:14 +0800

    javascript -> typescript -> visualbasic?

---
+++ Data_science/Graph/Model/Tree/KdTree/KdTree.vb
@@ -0,0 +1,31 @@
+﻿Namespace KdTree
+
+    Public Class KdTree
+
+        Dim dimensions As Integer()
+        Dim points As Object()
+        Dim metric As Object
+
+        Sub New(points As Object(), metric As Object, dimensions As Integer())
+            Me.points = points
+            Me.metric = metric
+            Me.dimensions = dimensions
+        End Sub
+
+        Private Function buildTree(points As Object(), depth As Integer, parent As Node) As Node
+            Dim [dim] = depth Mod dimensions.Length
+            Dim median As Double
+            Dim node As Node
+
+            If points.Length = 0 Then
+                Return Nothing
+            ElseIf points.Length = 1 Then
+                Return New Node(points(Scan0), [dim], parent)
+            End If
+
+            'points.Sort(Function(a, b)
+            '                Return a(dimensions([dim])) - b(dimensions([dim]))
+            '            End Function)
+        End Function
+    End Class
+End Namespace
---
+++ Data_science/Graph/Model/Tree/KdTree/Node.vb
@@ -0,0 +1,18 @@
+﻿Namespace KdTree
+
+    Public Class Node : Inherits Vertex
+
+        Public Property obj As Object
+        Public Property left As Node
+        Public Property right As Node
+        Public Property parent As Node
+        Public Property dimension As Integer
+
+        Sub New(obj As Object, dimension%, parent As Node)
+            Me.obj = obj
+            Me.dimension = dimension
+            Me.parent = parent
+        End Sub
+
+    End Class
+End Namespace
