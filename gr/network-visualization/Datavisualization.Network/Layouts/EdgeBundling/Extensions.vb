981eaa75f0c214b753b11689530498a2d7bdf58a
xieguigang <xie.guigang@gcmodeller.org>
2019-08-09 15:39:27 +0800

    add algorithm description of edge bundling

---
+++ gr/network-visualization/Datavisualization.Network/Layouts/EdgeBundling/Extensions.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Drawing
+Imports stdNum = System.Math
+
+Namespace Layouts.EdgeBundling
+
+    <HideModuleName> Module Extensions
+
+        Public ReadOnly PHI As Double = (1 + stdNum.Sqrt(5)) / 2
+
+        Public Function lerp(a As PointF, b As PointF, delta As Double) As PointF
+            Return New PointF With {
+                .X = a.X * (1 - delta) + b.X * delta,
+                .Y = a.Y * (1 - delta) + b.Y * delta
+            }
+        End Function
+    End Module
+End Namespace
