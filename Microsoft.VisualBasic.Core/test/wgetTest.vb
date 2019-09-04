395a6025a7e88d09d6882b6ce4ef72944bdcb4ab
xieguigang <xie.guigang@gcmodeller.org>
2019-09-04 15:32:05 +0800

    improvements of the http file downloader code

---
+++ Microsoft.VisualBasic.Core/test/wgetTest.vb
@@ -0,0 +1,9 @@
+﻿Imports Microsoft.VisualBasic.Net.Http
+
+Module wgetTest
+    Sub Main()
+        Dim wget As New wget("http://mona.fiehnlab.ucdavis.edu/rest/downloads/retrieve/9c822c48-67f4-4600-8b81-ef7491008245", "D:\Database\KEGG\organism\KEGG_Organism\ath\test.png")
+
+        Call wget.Run()
+    End Sub
+End Module
