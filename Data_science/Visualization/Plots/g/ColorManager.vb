0352803efe5d372acef69ee1658a19cfe73a2b51
xieguigang <xie.guigang@gcmodeller.org>
2019-10-30 15:26:53 +0800

    fix xml entity escape in the generated cli pipeline source code

---
+++ Data_science/Visualization/Plots/g/ColorManager.vb
@@ -0,0 +1,75 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
+
+Namespace Graphic
+
+    Public MustInherit Class ColorProfile
+
+        Protected colors As Color()
+
+        Sub New(colorSchema As String)
+            colors = Designer.GetColors(colorSchema)
+        End Sub
+
+        Public MustOverride Function GetColor(item As NamedValue(Of Double)) As Color
+
+    End Class
+
+    Public Class CategoryColorProfile : Inherits ColorProfile
+
+        ReadOnly category As Dictionary(Of String, String)
+        ReadOnly categoryIndex As Index(Of String)
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="category">[term => category label]</param>
+        ''' <param name="colorSchema$"></param>
+        Sub New(category As Dictionary(Of String, String), colorSchema$)
+            Call MyBase.New(colorSchema)
+
+            Me.category = category
+            Me.categoryIndex = category.Values.Indexing
+
+            If colors.Length < categoryIndex.Count Then
+                colors = Designer.CubicSpline(colors, n:=categoryIndex.Count)
+            End If
+        End Sub
+
+        Public Overrides Function GetColor(item As NamedValue(Of Double)) As Color
+            Dim category As String = Me.category(item.Name)
+            Dim i As Integer = categoryIndex(category)
+
+            Return colors(i)
+        End Function
+    End Class
+
+    ''' <summary>
+    ''' scale(color_schema_term_name)
+    ''' </summary>
+    Public Class ValueScaleColorProfile : Inherits ColorProfile
+
+        Dim valueRange As DoubleRange
+        Dim indexRange As DoubleRange
+
+        Public Sub New(data As IEnumerable(Of NamedValue(Of Double)), colorSchema$, level%)
+            MyBase.New(colorSchema)
+
+            With data.ToArray
+                valueRange = .Select(Function(item) item.Value).ToArray
+                colors = Designer.CubicSpline(colors, n:=level)
+                indexRange = {0, .Length - 1}
+            End With
+        End Sub
+
+        Public Overrides Function GetColor(item As NamedValue(Of Double)) As Color
+            Dim index As Integer = valueRange.ScaleMapping(item.Value, indexRange)
+            Dim color As Color = colors(index)
+
+            Return color
+        End Function
+    End Class
+End Namespace
--- Microsoft.VisualBasic.Core/CommandLine/InteropService/SharedORM/CodeGenerator.vb
+++ Microsoft.VisualBasic.Core/CommandLine/InteropService/SharedORM/CodeGenerator.vb
@@ -1,47 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::9f7ca69490a4b9091c58e6698b7c17bf, Microsoft.VisualBasic.Core\CommandLine\InteropService\SharedORM\CodeGenerator.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class CodeGenerator
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Function: EnumeratesAPI, GetManualPage
-    ' 
-    '     Class APITuple
-    ' 
-    '         Properties: API, CLI
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class CodeGenerator
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Function: EnumeratesAPI, GetManualPage
+' 
+'     Class APITuple
+' 
+'         Properties: API, CLI
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -51,6 +51,8 @@ Imports Microsoft.VisualBasic.CommandLine.ManView
 Imports Microsoft.VisualBasic.CommandLine.Reflection.EntryPoints
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Text.Xml
 
 Namespace CommandLine.InteropService.SharedORM
 
@@ -108,9 +110,9 @@ Namespace CommandLine.InteropService.SharedORM
 
                 Try
                     help =
-$"```
-{apiUsage.Replace("<", "&lt;")}
-```" & vbCrLf & api.Info
+$"```bash
+{apiUsage.DoCall(AddressOf XmlEntity.EscapingXmlEntity)}
+```" & vbCrLf & api.Info.DoCall(AddressOf XmlEntity.EscapingXmlEntity)
 
                     CLI = New NamedValue(Of CommandLine) With {
                         .Name = api.EntryPoint.Name,
