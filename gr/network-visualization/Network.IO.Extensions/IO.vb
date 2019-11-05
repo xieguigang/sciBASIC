f81a91a57d9bc2b8758108988158b27b98f3d304
xieguigang <xie.guigang@gcmodeller.org>
2019-11-05 15:43:09 +0800

    IO load and save

--- gr/network-visualization/Network.IO.Extensions/IO.vb
+++ gr/network-visualization/Network.IO.Extensions/IO.vb
@@ -1,3 +1,37 @@
-﻿Public Module IO
+﻿Imports System.Text
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream.Generic
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Data.csv
+Imports System.Runtime.CompilerServices
 
+Public Module IO
+
+    ''' <summary>
+    '''
+    ''' </summary>
+    ''' <param name="outDIR">The data directory for the data export, if the value of this directory is null then the data
+    ''' will be exported at the current work directory.
+    ''' (进行数据导出的文件夹，假若为空则会保存数据至当前的工作文件夹之中)</param>
+    ''' <param name="encoding">The file encoding of the exported node and edge csv file.</param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    ''' 
+    <Extension>
+    Public Function Save(Of T_Node As Node, T_Edge As NetworkEdge)(network As Network(Of T_Node, T_Edge), outDIR$, encoding As Encoding) As Boolean
+        With outDIR Or App.CurrentDirectory.AsDefault
+            Call network.nodes.SaveTo($"{ .ByRef}/nodes.csv", False, encoding)
+            Call network.edges.SaveTo($"{ .ByRef}/network-edges.csv", False, encoding)
+        End With
+
+        Return True
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function Load(Of T_Node As Node, T_Edge As NetworkEdge)(directory As String) As Network(Of T_Node, T_Edge)
+        Return New Network(Of T_Node, T_Edge) With {
+            .edges = $"{directory}/network-edges.csv".LoadCsv(Of T_Edge),
+            .nodes = $"{directory}/nodes.csv".LoadCsv(Of T_Node)
+        }
+    End Function
 End Module
