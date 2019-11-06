fa803dd1c666cdca60627258b0f05767379ee916
xieguigang <xie.guigang@gcmodeller.org>
2019-11-06 10:13:42 +0800

    fix api reference errors

--- gr/network-visualization/Datavisualization.Network/Graph/Extensions.vb
+++ gr/network-visualization/Datavisualization.Network/Graph/Extensions.vb
@@ -96,45 +96,5 @@ Namespace Graph
                 End If
             Next
         End Function
-
-        ''' <summary>
-        ''' 移除的重复的边
-        ''' </summary>
-        ''' <remarks></remarks>
-        ''' <param name="directed">是否忽略方向？</param>
-        ''' <param name="ignoreTypes">是否忽略边的类型？</param>
-        <Extension> Public Function RemoveDuplicated(Of T As NetworkEdge)(
-                                                    edges As IEnumerable(Of T),
-                                                    Optional directed As Boolean = True,
-                                                    Optional ignoreTypes As Boolean = False) As T()
-            Dim uid = Function(edge As T) As String
-                          If directed Then
-                              Return edge.GetDirectedGuid(ignoreTypes)
-                          Else
-                              Return edge.GetNullDirectedGuid(ignoreTypes)
-                          End If
-                      End Function
-            Dim LQuery = edges _
-                .GroupBy(uid) _
-                .Select(Function(g) g.First) _
-                .ToArray
-
-            Return LQuery
-        End Function
-
-        ''' <summary>
-        ''' 移除自身与自身的边
-        ''' </summary>
-        ''' <remarks></remarks>
-        <Extension> Public Function RemoveSelfLoop(Of T As NetworkEdge)(edges As IEnumerable(Of T)) As T()
-            Dim LQuery = LinqAPI.Exec(Of T) <=
- _
-                From x As T
-                In edges
-                Where Not x.SelfLoop
-                Select x
-
-            Return LQuery
-        End Function
     End Module
 End Namespace
---
+++ gr/network-visualization/Datavisualization.Network/Graph/Model/NameOf.vb
@@ -0,0 +1,67 @@
+﻿#Region "Microsoft.VisualBasic::e83918c71948532ae816052639a6ff51, gr\network-visualization\Datavisualization.Network\IO\Generic\NameOf.vb"
+
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+
+' /********************************************************************************/
+
+' Summaries:
+
+'     Module NamesOf
+' 
+' 
+' 
+' 
+' /********************************************************************************/
+
+#End Region
+
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+
+Namespace FileStream.Generic
+
+    ''' <summary>
+    ''' The preset names value for edge type <see cref="EdgeData.Properties"/> and node type <see cref="NodeData.Properties"/>
+    ''' </summary>
+    Public Module NamesOf
+
+        Public Const REFLECTION_ID_MAPPING_FROM_NODE As String = "fromNode"
+        Public Const REFLECTION_ID_MAPPING_TO_NODE As String = "toNode"
+        Public Const REFLECTION_ID_MAPPING_CONFIDENCE As String = "confidence"
+        Public Const REFLECTION_ID_MAPPING_INTERACTION_TYPE As String = "interaction_type"
+
+        ''' <summary>
+        ''' <see cref="Node.label"/>
+        ''' </summary>
+        Public Const REFLECTION_ID_MAPPING_IDENTIFIER As String = "ID"
+        Public Const REFLECTION_ID_MAPPING_NODETYPE As String = "nodeType"
+        Public Const REFLECTION_ID_MAPPING_DEGREE$ = "degree"
+        Public Const REFLECTION_ID_MAPPING_DEGREE_IN$ = REFLECTION_ID_MAPPING_DEGREE & ".in"
+        Public Const REFLECTION_ID_MAPPING_DEGREE_OUT$ = REFLECTION_ID_MAPPING_DEGREE & ".out"
+
+    End Module
+End Namespace
