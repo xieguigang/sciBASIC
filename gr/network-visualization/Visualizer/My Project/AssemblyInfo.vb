7b2a28d56e5b3276130110450579179b52cb4a2e
xieguigang <xie.guigang@gcmodeller.org>
2019-11-10 16:28:48 +0800

    Update AssemblyInfo.vb
    edit assembly information

--- gr/network-visualization/Visualizer/My Project/AssemblyInfo.vb
+++ gr/network-visualization/Visualizer/My Project/AssemblyInfo.vb
@@ -8,12 +8,12 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("VisualizationExtensions")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("VisualizationExtensions")> 
-<Assembly: AssemblyCopyright("Copyright ©  2016")> 
-<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyTitle("Network graph data render engine")>
+<Assembly: AssemblyDescription("Network graph data render engine")>
+<Assembly: AssemblyCompany("GPL3")>
+<Assembly: AssemblyProduct("sciBASIC.NET")>
+<Assembly: AssemblyCopyright("Copyright © I@xieguigang.me 2019")>
+<Assembly: AssemblyTrademark("sciBASIC.NET")>
 
 <Assembly: ComVisible(False)>
 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("4.1230.*")> 
+<Assembly: AssemblyFileVersion("3.110.*")> 
