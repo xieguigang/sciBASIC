364943dc0acdc2e94bbd86ab983f2035999fd44a
xieguigang <xie.guigang@gcmodeller.org>
2019-11-14 10:42:11 +0800

    wmf test success

---
+++ Microsoft.VisualBasic.Core/test/wmfTest.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging
+
+Module wmfTest
+
+    Sub Main()
+        Using wmf As New Wmf(New Size(1200, 500), "./test.wmf", backgroundColor:="white")
+            Call wmf.DrawString("Hello world", New Font("Microsoft YaHei", 64, FontStyle.Bold), Brushes.Red, New PointF(100, 100))
+        End Using
+
+        Using png As Graphics2D = New Size(1200, 500).CreateGDIDevice(Color.White)
+            Call png.DrawString("Hello world", New Font("Microsoft YaHei", 64, FontStyle.Bold), Brushes.Red, New PointF(100, 100))
+            Call png.ImageResource.SaveAs("./test.png")
+        End Using
+    End Sub
+End Module
