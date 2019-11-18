da038532d6d55f6ad291e16dae59c3dc5c720681
xieguigang <xie.guigang@gcmodeller.org>
2019-11-18 13:31:15 +0800

    config for test

---
+++ Data/DataFrame.Extensions/Outlining/OutliningDataLoader.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.csv.IO
+
+Namespace Outlining
+
+    Public Module OutliningDataLoader
+
+        <Extension>
+        Public Function LoadOutlining(Of T As Class)(filepath As String) As IEnumerable(Of T)
+            Dim file As File = File.Load(filepath)
+            ' 按照列空格进行文件的等级切割
+        End Function
+    End Module
+End Namespace
