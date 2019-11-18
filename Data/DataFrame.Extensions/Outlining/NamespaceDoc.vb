16749414b1baf2f55644a8c12cfcbb2c67bb4039
xieguigang <xie.guigang@gcmodeller.org>
2019-11-18 13:07:31 +0800

    add dev note

--- Data/DataFrame.Extensions/Outlining/NamespaceDoc.vb
+++ Data/DataFrame.Extensions/Outlining/NamespaceDoc.vb
@@ -1,3 +1,8 @@
-﻿Public Class Class1
+﻿Namespace Outlining
 
-End Class
+    ''' <summary>
+    ''' 采用分级显示的功能进行复杂对象的序列化存储操作
+    ''' </summary>
+    Module NamespaceDoc
+    End Module
+End Namespace
