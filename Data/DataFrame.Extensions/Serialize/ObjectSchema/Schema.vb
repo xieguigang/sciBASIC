77907d7bffdb363f0d89a89966b4f17477996c7f
xieguigang <xie.guigang@gcmodeller.org>
2019-11-18 13:53:25 +0800

    fix api reference errors

--- Data/DataFrame.Extensions/Serialize/ObjectSchema/Schema.vb
+++ Data/DataFrame.Extensions/Serialize/ObjectSchema/Schema.vb
@@ -1,45 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::79f2a16f800cb4ffdf222a98b2b1aac5, Data\DataFrame.Extensions\Schema.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class Schema
-    ' 
-    '     Properties: Members, Tables, Type
-    ' 
-    '     Function: (+2 Overloads) GetSchema, Primitive, ToString
-    ' 
-    '     Sub: __memberStack
-    ' 
-    ' /********************************************************************************/
+' Class Schema
+' 
+'     Properties: Members, Tables, Type
+' 
+'     Function: (+2 Overloads) GetSchema, Primitive, ToString
+' 
+'     Sub: __memberStack
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -125,7 +125,7 @@ Namespace Serialize.ObjectSchema
                 pType = prop.PropertyType
 
                 ' 假若是基本类型或者字符串，则会直接添加
-                If Primitive(pType) Then
+                If DataFramework.IsPrimitive(pType) Then
                     members += New NamedValue(Of String) With {
                         .Name = $"{parent}::{prop.Name}",
                         .Value = path
@@ -137,7 +137,7 @@ Namespace Serialize.ObjectSchema
                     If elType Is Nothing OrElse elType.Equals(pType) Then
                         ' 不是集合类型
                         Call __memberStack(members, pType, $"{parent}::{prop.Name}", parent.Replace("::", "/") & $"/{prop.Name}.Csv")
-                    ElseIf Primitive(elType) Then
+                    ElseIf DataFramework.IsPrimitive(elType) Then
                         ' 基本类型
                         members += New NamedValue(Of String) With {
                             .Name = $"{parent}::{prop.Name}",
