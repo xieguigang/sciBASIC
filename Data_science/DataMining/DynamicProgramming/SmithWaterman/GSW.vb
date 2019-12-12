11eada5b25aabc8108c2a84a2750ee26021cbe29
xieguigang <xie.guigang@gcmodeller.org>
2019-12-12 09:31:08 +0800

    fix namespace reference errors

--- Data_science/DataMining/DynamicProgramming/SmithWaterman/GSW.vb
+++ Data_science/DataMining/DynamicProgramming/SmithWaterman/GSW.vb
@@ -1,55 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::a2a049a0b52db5293a737b38a98754a6, Data_science\DataMining\DynamicProgramming\SmithWaterman\GSW.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class GSW
-    ' 
-    '         Properties: AlignmentScore, Matches, MaxScore, query, subject
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: __similarity, GetDPMAT, GetMatches, GetTraceback, traceback
-    ' 
-    '         Sub: buildMatrix, getTrackback
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class GSW
+' 
+'         Properties: AlignmentScore, Matches, MaxScore, query, subject
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: __similarity, GetDPMAT, GetMatches, GetTraceback, traceback
+' 
+'         Sub: buildMatrix, getTrackback
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Algorithm.DynamicProgramming
+Imports Microsoft.VisualBasic.ComponentModel.Algorithm.DynamicProgramming.Levenshtein.LevenshteinDistance
 Imports Microsoft.VisualBasic.Language
-Imports Microsoft.VisualBasic.Text.Levenshtein.LevenshteinDistance
 Imports Microsoft.VisualBasic.Text.Xml.Models
 
 Namespace SmithWaterman
