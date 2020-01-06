789a49f440cb25e8650c3d3f75d07f19163536d9
xieguigang <xie.guigang@gcmodeller.org>
2020-01-06 21:03:45 +0800

    value reader

--- Microsoft.VisualBasic.Core/Extensions/Reflection/Reflection.vb
+++ Microsoft.VisualBasic.Core/Extensions/Reflection/Reflection.vb
@@ -671,48 +671,6 @@ NULL:       If Not strict Then
         End Try
     End Function
 
-    ''' <summary>
-    ''' 只对属性有效，出错会返回空值
-    ''' </summary>
-    ''' <param name="obj"></param>
-    ''' <param name="Name"></param>
-    ''' <returns></returns>
-    '''
-    <ExportAPI("GetValue")>
-    <Extension> Public Function GetValue(Type As Type, obj As Object, Name As String) As Object
-        Try
-            Return getValueInternal(Type, obj, Name)
-        Catch ex As Exception
-            Return App.LogException(ex, $"{GetType(Extensions).FullName}::{NameOf(GetValue)}")
-        End Try
-    End Function
-
-    Private Function getValueInternal(type As Type, obj As Object, Name As String) As Object
-        Dim [property] = type.GetProperty(Name, BindingFlags.Public Or BindingFlags.Instance)
-
-        If [property] Is Nothing Then
-            Return Nothing
-        Else
-            Dim value = [property].GetValue(obj, Nothing)
-            Return value
-        End If
-    End Function
-
-    ''' <summary>
-    ''' 只对属性有效，出错会返回空值
-    ''' </summary>
-    ''' <param name="obj"></param>
-    ''' <param name="Name"></param>
-    ''' <returns></returns>
-    <Extension> Public Function GetValue(Of T)(Type As Type, obj As Object, Name As String) As T
-        Dim value = Type.GetValue(obj, Name)
-        If value Is Nothing Then
-            Return Nothing
-        End If
-        Dim cast As T = DirectCast(value, T)
-        Return cast
-    End Function
-
 #If NET_40 = 0 Then
 
     ''' <summary>
---
+++ Microsoft.VisualBasic.Core/Extensions/Reflection/ValueReader.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+Public Module ValueReader
+
+    ''' <summary>
+    ''' 只对属性有效，出错会返回空值
+    ''' </summary>
+    ''' <param name="obj"></param>
+    ''' <param name="Name"></param>
+    ''' <returns></returns>
+    '''
+    <ExportAPI("GetValue")>
+    <Extension> Public Function GetValue(Type As Type, obj As Object, Name As String) As Object
+        Try
+            Return getValueInternal(Type, obj, Name)
+        Catch ex As Exception
+            Return App.LogException(ex, $"{GetType(Extensions).FullName}::{NameOf(GetValue)}")
+        End Try
+    End Function
+
+    Private Function getValueInternal(type As Type, obj As Object, Name As String) As Object
+        Dim [property] = type.GetProperty(Name, PublicProperty)
+
+        If [property] Is Nothing Then
+            Return Nothing
+        Else
+            Dim value = [property].GetValue(obj, Nothing)
+            Return value
+        End If
+    End Function
+
+    ''' <summary>
+    ''' 只对属性有效，出错会返回空值
+    ''' </summary>
+    ''' <param name="obj"></param>
+    ''' <param name="Name"></param>
+    ''' <returns></returns>
+    <Extension> Public Function GetValue(Of T)(Type As Type, obj As Object, Name As String) As T
+        Dim value = Type.GetValue(obj, Name)
+        If value Is Nothing Then
+            Return Nothing
+        End If
+        Dim cast As T = DirectCast(value, T)
+        Return cast
+    End Function
+End Module
