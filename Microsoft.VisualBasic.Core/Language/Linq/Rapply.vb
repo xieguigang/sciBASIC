47017ebed7f01f8987d0a9a1a18e0ac0dba8454d
xieguigang <xie.guigang@gcmodeller.org>
2020-01-19 09:07:53 +0800

    implemenets lapply function

--- Microsoft.VisualBasic.Core/Language/Linq/Rapply.vb
+++ Microsoft.VisualBasic.Core/Language/Linq/Rapply.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Runtime.CompilerServices
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 
 Namespace Language
@@ -10,7 +11,25 @@ Namespace Language
 
         <Extension>
         Public Function lapply(Of Tin As INamedValue, TOut)(sequence As IEnumerable(Of Tin), apply As [Delegate], ParamArray args As Object()) As Dictionary(Of String, TOut)
+            Dim result As New Dictionary(Of String, TOut)
+            Dim key$
+            Dim value As Object
+            Dim populateArguments = Iterator Function(item As Tin) As IEnumerable(Of Object)
+                                        Yield item
 
+                                        For Each val As Object In args
+                                            Yield val
+                                        Next
+                                    End Function
+            Dim method As MethodInfo = apply.Method
+
+            For Each item As Tin In sequence
+                key = item.Key
+                value = method.Invoke(apply.Target, populateArguments(item).ToArray)
+                result.Add(key, value)
+            Next
+
+            Return result
         End Function
     End Module
 End Namespace
