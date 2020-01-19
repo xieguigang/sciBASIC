59f4bc094de55b6ede420d65463f133e5b809e6b
xieguigang <xie.guigang@gcmodeller.org>
2020-01-19 08:59:30 +0800

    implements a lapply function

---
+++ Microsoft.VisualBasic.Core/Language/Linq/Rapply.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+
+Namespace Language
+
+    ''' <summary>
+    ''' Helper for implements ``lapply`` and ``sapply`` liked operations from R language
+    ''' </summary>
+    Public Module Rapply
+
+        <Extension>
+        Public Function lapply(Of Tin As INamedValue, TOut)(sequence As IEnumerable(Of Tin), apply As [Delegate], ParamArray args As Object()) As Dictionary(Of String, TOut)
+
+        End Function
+    End Module
+End Namespace
---
+++ Microsoft.VisualBasic.Core/test/lapplyTest.vb
@@ -0,0 +1,30 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Language
+
+Module lapplyTest
+
+    Dim list As NamedValue(Of Double())()
+
+    Sub New()
+        list = {
+            New NamedValue(Of Double()) With {.Name = App.NextTempName, .Value = {1, 1, 11}},
+            New NamedValue(Of Double()) With {.Name = App.NextTempName, .Value = {2, 11, 711}},
+            New NamedValue(Of Double()) With {.Name = App.NextTempName, .Value = {3, 11, 611}},
+            New NamedValue(Of Double()) With {.Name = App.NextTempName, .Value = {4, 111, 151}},
+            New NamedValue(Of Double()) With {.Name = App.NextTempName, .Value = {5, 1111, 11}},
+            New NamedValue(Of Double()) With {.Name = App.NextTempName, .Value = {6, 11, 114}},
+            New NamedValue(Of Double()) With {.Name = App.NextTempName, .Value = {7, 11, 11}},
+            New NamedValue(Of Double()) With {.Name = App.NextTempName, .Value = {8, 12, 811}},
+            New NamedValue(Of Double()) With {.Name = App.NextTempName, .Value = {9, 1444, 311}}
+        }
+    End Sub
+
+    Sub Main()
+        Dim result = list.lapply(Of Double)(Function(a As NamedValue(Of Double()), x As Double, y As Double)
+                                                Return a.Value(Scan0) + x + y * a.Value.Average
+                                            End Function, 5, 6)
+
+        Pause()
+    End Sub
+
+End Module
