575ddc801ccdea6e4486f1f9e8af5ace4c226b6d
xieguigang <xie.guigang@gcmodeller.org>
2020-02-05 15:47:54 +0800

    improvements of the version text

--- gr/Microsoft.VisualBasic.Imaging/SVG/XML/SVG.vb
+++ gr/Microsoft.VisualBasic.Imaging/SVG/XML/SVG.vb
@@ -1,49 +1,49 @@
 ﻿#Region "Microsoft.VisualBasic::0f246f086dbffcf343515b1900325466, gr\Microsoft.VisualBasic.Imaging\SVG\XML\SVG.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class SVGXml
-    ' 
-    '         Properties: circles, defs, desc, enable_background, height
-    '                     images, Layers, lines, overflow, path
-    '                     polygon, polyline, rect, space, styleCSS
-    '                     texts, title, transform, version, viewBox
-    '                     width, WriterComment
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Function: AddLayer, GetSVGXml, (+2 Overloads) SaveAsXml, Size, TryLoad
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class SVGXml
+' 
+'         Properties: circles, defs, desc, enable_background, height
+'                     images, Layers, lines, overflow, path
+'                     polygon, polyline, rect, space, styleCSS
+'                     texts, title, transform, version, viewBox
+'                     width, WriterComment
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Function: AddLayer, GetSVGXml, (+2 Overloads) SaveAsXml, Size, TryLoad
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -54,6 +54,7 @@ Imports System.Xml
 Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.Imaging.SVG.CSS
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.MIME.Markup.HTML
 Imports Microsoft.VisualBasic.Text
 Imports htmlNode = Microsoft.VisualBasic.MIME.Markup.HTML.XmlMeta.Node
@@ -138,7 +139,7 @@ Namespace SVG.XML
         Public Property WriterComment As XmlComment
             <MethodImpl(MethodImplOptions.AggressiveInlining)>
             Get
-                Return XmlComment.CreateComment
+                Return (Me.GetType.DoCall(AddressOf XmlDataModel.GetTypeReferenceComment) & vbCrLf & vbCrLf & XmlComment).CreateComment
             End Get
             Set
             End Set
