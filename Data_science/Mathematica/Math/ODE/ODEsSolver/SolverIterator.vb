f1a47dba5fcd0802c47eed52b56a690ad40dc22f
xieguigang <xie.guigang@gcmodeller.org>
2020-02-19 14:18:27 +0800

    split module files

---
+++ Data_science/Mathematica/Math/ODE/ODEsSolver/SolverIterator.vb
@@ -0,0 +1,35 @@
+﻿
+Public Class SolverIterator
+
+    Dim rk4 As RungeKutta4
+    Dim solverEnumerator As IEnumerator(Of Integer)
+
+    Public ReadOnly Property RK4Solver As RungeKutta4
+        Get
+            Return rk4
+        End Get
+    End Property
+
+    Sub New(rk4 As RungeKutta4)
+        Me.rk4 = rk4
+    End Sub
+
+    Public Function Config(y0 As Double(), n As Integer, a As Double, b As Double) As SolverIterator
+        solverEnumerator = rk4 _
+            .solverIteration(y0, n, a, b) _
+            .GetEnumerator
+
+        Return Me
+    End Function
+
+    ''' <summary>
+    ''' 这个方法接口主要是应用于模拟器计算
+    ''' </summary>
+    Public Sub Tick()
+        solverEnumerator.MoveNext()
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return rk4.ToString
+    End Function
+End Class
