53b531f2bb5dfe78fcd3a88fb2eea957673aa145
xieguigang <xie.guigang@gcmodeller.org>
2020-03-28 18:40:42 +0800

    config for test

--- mime/application%rdf+xml/test/Test1.vb
+++ mime/application%rdf+xml/test/Test1.vb
@@ -1,67 +1,67 @@
-﻿#Region "Microsoft.VisualBasic::157cc6edb990bac83505cbe15bc6ec52, mime\application%rdf+xml\TestProject\Test1.vb"
+﻿'#Region "Microsoft.VisualBasic::157cc6edb990bac83505cbe15bc6ec52, mime\application%rdf+xml\TestProject\Test1.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+'    ' Author:
+'    ' 
+'    '       asuka (amethyst.asuka@gcmodeller.org)
+'    '       xie (genetics@smrucc.org)
+'    '       xieguigang (xie.guigang@live.com)
+'    ' 
+'    ' Copyright (c) 2018 GPL3 Licensed
+'    ' 
+'    ' 
+'    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+'    ' 
+'    ' 
+'    ' This program is free software: you can redistribute it and/or modify
+'    ' it under the terms of the GNU General Public License as published by
+'    ' the Free Software Foundation, either version 3 of the License, or
+'    ' (at your option) any later version.
+'    ' 
+'    ' This program is distributed in the hope that it will be useful,
+'    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    ' GNU General Public License for more details.
+'    ' 
+'    ' You should have received a copy of the GNU General Public License
+'    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+'    ' /********************************************************************************/
 
-    ' Summaries:
+'    ' Summaries:
 
-    ' Class RDFD
-    ' 
-    '     Properties: CDList
-    '     Class CD
-    ' 
-    '         Properties: Artist, Company, Country, IgnoredProperty, Price
-    '                     Year
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'    ' Class RDFD
+'    ' 
+'    '     Properties: CDList
+'    '     Class CD
+'    ' 
+'    '         Properties: Artist, Company, Country, IgnoredProperty, Price
+'    '                     Year
+'    ' 
+'    ' 
+'    ' 
+'    ' /********************************************************************************/
 
-#End Region
+'#End Region
 
-Imports Microsoft.VisualBasic.DocumentFormat.RDF
-Imports Microsoft.VisualBasic.DocumentFormat.RDF.Serialization
+'Imports Microsoft.VisualBasic.DocumentFormat.RDF
+'Imports Microsoft.VisualBasic.DocumentFormat.RDF.Serialization
 
-<RDFNamespaceImports("cd", "http://www.recshop.fake/cd#")>
-Public Class RDFD
+'<RDFNamespaceImports("cd", "http://www.recshop.fake/cd#")>
+'Public Class RDFD
 
-    <RDFElement("cd")> Public Property CDList As CD()
+'    <RDFElement("cd")> Public Property CDList As CD()
 
-    <RDFDescription(About:="http://www.recshop.fake/cd/Empire Burlesque")>
-    <RDFType("cd")>
-    Public Class CD : Inherits RDFEntity
-        <RDFElement("artist")> Public Property Artist As String
-        <RDFElement("country")> Public Property Country As String
-        <RDFElement("company")> Public Property Company As String
-        <RDFElement("price")> Public Property Price As String
-        <RDFElement("year")> Public Property Year As String
+'    <RDFDescription(About:="http://www.recshop.fake/cd/Empire Burlesque")>
+'    <RDFType("cd")>
+'    Public Class CD : Inherits RDFEntity
+'        <RDFElement("artist")> Public Property Artist As String
+'        <RDFElement("country")> Public Property Country As String
+'        <RDFElement("company")> Public Property Company As String
+'        <RDFElement("price")> Public Property Price As String
+'        <RDFElement("year")> Public Property Year As String
 
-        <RDFIgnore> Public Property IgnoredProperty As KeyValuePair(Of Integer, String)
-    End Class
-End Class
+'        <RDFIgnore> Public Property IgnoredProperty As KeyValuePair(Of Integer, String)
+'    End Class
+'End Class
---
+++ mime/application%rdf+xml/test/mathMLloaderTest.vb
@@ -0,0 +1,6 @@
+﻿Module mathMLloaderTest
+
+    Sub Main()
+
+    End Sub
+End Module
