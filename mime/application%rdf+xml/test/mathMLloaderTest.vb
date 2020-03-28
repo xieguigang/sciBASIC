3340d96f80895bfd8d546b6ba9eaa409ec1fd60b
xieguigang <xie.guigang@gcmodeller.org>
2020-03-28 19:31:48 +0800

    config test

--- mime/application%rdf+xml/test/mathMLloaderTest.vb
+++ mime/application%rdf+xml/test/mathMLloaderTest.vb
@@ -1,6 +1,10 @@
-﻿Module mathMLloaderTest
+﻿Imports Microsoft.VisualBasic.MIME.application.rdf_xml
+
+Module mathMLloaderTest
 
     Sub Main()
+        Dim simple = "E:\GCModeller\src\runtime\sciBASIC#\mime\etc\kinetics1.xml".LoadXml(Of Math)
 
+        Pause()
     End Sub
 End Module
