9a755ed56942759ce8b70a7aa8277201a278edd2
xieguigang <xie.guigang@gcmodeller.org>
2020-03-30 20:49:13 +0800

    fix bugs of take snapshots

--- Data_science/MachineLearning/CellularAutomaton/Extensions.vb
+++ Data_science/MachineLearning/CellularAutomaton/Extensions.vb
@@ -6,10 +6,14 @@ Public Module Extensions
 
     <Extension>
     Public Sub TakeSnapshots(Of T As Individual)(simulator As Simulator(Of T), getKey As Func(Of T, String), counts As Dictionary(Of String, List(Of Integer)))
-        Dim groups = simulator.Snapshot.GroupBy(getKey).ToArray
+        Dim groups = simulator.Snapshot.GroupBy(getKey).ToDictionary(Function(n) n.Key, Function(n) n.Count)
 
-        For Each group As IGrouping(Of String, T) In groups
-            counts(group.Key).Add(group.Count)
+        For Each type In counts.Keys
+            If groups.ContainsKey(type) Then
+                counts(type).Add(groups(type))
+            Else
+                counts(type).Add(0)
+            End If
         Next
     End Sub
 End Module
