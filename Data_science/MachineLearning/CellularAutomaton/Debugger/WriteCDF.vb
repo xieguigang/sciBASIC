1caf1ccc21984a4b8ce3f85333e599d008e26aa8
xieguigang <xie.guigang@gcmodeller.org>
2020-03-30 21:42:28 +0800

    write debugger data into cdf file

--- Data_science/MachineLearning/CellularAutomaton/Debugger/WriteCDF.vb
+++ Data_science/MachineLearning/CellularAutomaton/Debugger/WriteCDF.vb
@@ -1,6 +1,36 @@
-﻿Module WriteCDF
+﻿Imports Microsoft.VisualBasic.Data.IO.netCDF
+Imports Microsoft.VisualBasic.Data.IO.netCDF.Components
+Imports Microsoft.VisualBasic.Language
 
-    Public Function Flush(file As String, cache As List(Of Integer)()(), type As Type)
+Module WriteCDF
 
-    End Function
+    Public Sub Flush(path As String, cache As List(Of Integer)()(), type As Type)
+        Using file As New CDFWriter(path)
+            Dim w = cache(Scan0).Length
+            Dim h = cache.Length
+            Dim data As CDFData
+            Dim id As i32 = Scan0
+            Dim dims As Dimension()
+            Dim attrs As attribute()
+
+            file.GlobalAttributes(
+                New attribute With {.name = "schema", .type = CDFDataTypes.CHAR, .value = type.FullName},
+                New attribute With {.name = "size\width", .type = CDFDataTypes.INT, .value = w},
+                New attribute With {.name = "size\height", .type = CDFDataTypes.INT, .value = h}
+            )
+            file.Dimensions(Dimension.Integer)
+
+            For j As Integer = 0 To w - 1
+                For i As Integer = 0 To h - 1
+                    data = cache(i)(j).ToArray
+                    dims = {Dimension.Integer}
+                    attrs = {
+                        New attribute With {.name = "i", .type = CDFDataTypes.INT, .value = i},
+                        New attribute With {.name = "j", .type = CDFDataTypes.INT, .value = j}
+                    }
+                    file.AddVariable((++id).ToHexString, data, dims, attrs)
+                Next
+            Next
+        End Using
+    End Sub
 End Module
