7918abbc5f6ab60be824bd5be5934fbb01f5f786
xieguigang <xie.guigang@gcmodeller.org>
2020-04-02 21:52:41 +0800

    add expression model

---
+++ mime/application%rdf+xml/MathML/BinaryExpression.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic.Language
+
+Namespace MathML
+
+    Public Class BinaryExpression
+
+        Public Property [operator] As String
+
+        Public Property applyleft As [Variant](Of BinaryExpression, String)
+        Public Property applyright As [Variant](Of BinaryExpression, String)
+
+        Public Overrides Function ToString() As String
+            Dim left As String
+            Dim right As String
+
+            If applyleft Like GetType(String) Then
+                left = applyleft.TryCast(Of String)
+            Else
+                left = $"( {applyleft.TryCast(Of BinaryExpression).ToString} )"
+            End If
+
+            If applyright Like GetType(String) Then
+                right = applyright.TryCast(Of String)
+            Else
+                right = $"( {applyright.TryCast(Of BinaryExpression).ToString} )"
+            End If
+
+            Return $"{left} {[operator]} {right}"
+        End Function
+
+    End Class
+End Namespace
--- mime/application%rdf+xml/MathML/contentBuilder.vb
+++ mime/application%rdf+xml/MathML/contentBuilder.vb
@@ -1,8 +1,19 @@
-﻿Namespace MathML
+﻿Imports System.Xml
+
+Namespace MathML
 
     Module contentBuilder
 
         Public Function ToString(lambda As Apply) As String
+
+        End Function
+
+        ''' <summary>
+        ''' 因为反序列化存在一个元素顺序的bug，所以在这里不可以通过反序列化来进行表达式的解析
+        ''' </summary>
+        ''' <param name="mathML"></param>
+        ''' <returns></returns>
+        Public Function ParseXml(mathML As XmlTextReader) As BinaryExpression
 
         End Function
     End Module
