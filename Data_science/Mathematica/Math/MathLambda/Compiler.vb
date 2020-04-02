41a8f7ce81372349f3b2968ed49866e80c63a3ca
xieguigang <xie.guigang@gcmodeller.org>
2020-04-02 21:42:44 +0800

    math lambda compiler

---
+++ Data_science/Mathematica/Math/MathLambda/Compiler.vb
@@ -0,0 +1,10 @@
+﻿
+''' <summary>
+''' mathML -> lambda -> linq expression -> compile VB lambda
+''' </summary>
+Public Module Compiler
+
+    Public Function CreateLambda()
+
+    End Function
+End Module
