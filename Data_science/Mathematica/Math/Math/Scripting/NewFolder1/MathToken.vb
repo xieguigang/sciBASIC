ce84f91f7973139495a9b4d2eb61e9690bd22a0f
xieguigang <xie.guigang@gcmodeller.org>
2020-04-03 23:43:40 +0800

    token model

---
+++ Data_science/Mathematica/Math/Math/Scripting/NewFolder1/MathToken.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic.Scripting.TokenIcer
+
+Public Class MathToken : Inherits CodeToken(Of MathTokens)
+
+    Sub New(name As MathTokens, text As String)
+        Call MyBase.New(name, text)
+    End Sub
+End Class
+
+Public Enum MathTokens
+    Invalid
+    Literal
+    [Operator]
+    Open
+    Close
+    Symbol
+End Enum
