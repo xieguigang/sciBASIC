fd68df71915d238de3cb10f24e0dcd326ce59f95
xieguigang <xie.guigang@gcmodeller.org>
2020-04-06 16:44:48 +0800

    unsure for handle the missing operator

--- mime/application%xml/MathML/contentBuilder.vb
+++ mime/application%xml/MathML/contentBuilder.vb
@@ -1,47 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::7220c76bb3218d0ca113b0f9a9c0fc32, mime\application%xml\MathML\contentBuilder.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module ContentBuilder
-    ' 
-    '         Function: getTextSymbol, parseInternal, ParseXml, SimplyOperator, ToString
-    '                   TrimWhitespace
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module ContentBuilder
+' 
+'         Function: getTextSymbol, parseInternal, ParseXml, SimplyOperator, ToString
+'                   TrimWhitespace
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Text
 
@@ -117,11 +118,20 @@ Namespace MathML
             End If
         End Function
 
+        ReadOnly symbols As Index(Of String) = {"apply", "ci", "cn"}
+
         <Extension>
         Private Function parseInternal(apply As XmlElement) As BinaryExpression
-            Dim [operator] = apply.elements(Scan0)
+            Dim [operator] As XmlElement
             Dim left, right As [Variant](Of BinaryExpression, SymbolExpression)
             Dim applys = apply.getElementsByTagName("apply").ToArray
+
+            If apply.elements(Scan0).name Like symbols Then
+                [operator] = New XmlElement With {.name = "times"}
+                apply.elements = {[operator]}.Join(apply.elements).ToArray
+            Else
+                [operator] = apply.elements(Scan0)
+            End If
 
             If applys.Length = 1 Then
                 If apply.elements(1).name = "apply" Then
