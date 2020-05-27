ea4e0fc775169356652b0d8db2bb4f2dc1ad4d43
xieguigang <xie.guigang@gcmodeller.org>
2020-05-27 20:44:03 +0800

    unix man page data model

--- Microsoft.VisualBasic.Core/ApplicationServices/Terminal/Utility/UnixMan/ManualPages.vb
+++ Microsoft.VisualBasic.Core/ApplicationServices/Terminal/Utility/UnixMan/ManualPages.vb
@@ -54,7 +54,7 @@
 
 Namespace ApplicationServices.Terminal.Utility
 
-    Public Class ManualPages : Implements System.IDisposable
+    Public Class ManualPages : Implements IDisposable
 
         Protected _manualData As List(Of String)
         Protected p As Integer
@@ -156,98 +156,5 @@ Namespace ApplicationServices.Terminal.Utility
             GC.SuppressFinalize(Me)
         End Sub
 #End Region
-    End Class
-
-    ''' <summary>
-    ''' 有显示标题的
-    ''' </summary>
-    Public Class IndexedManual : Inherits ManualPages
-
-        Dim Title As String
-
-        ''' <summary>
-        ''' 与<see cref="ManualPages"></see>所不同的是，本对象之中的这个字符串数组表示的是一页帮助，而不是一行帮助信息
-        ''' </summary>
-        ''' <param name="Pages"></param>
-        ''' <remarks></remarks>
-        Sub New(Pages As IEnumerable(Of String), title$)
-            Call MyBase.New(Pages)
-            Me.Title = title & vbCrLf & vbCrLf & Navigations
-        End Sub
-
-        Const Navigations As String = "Press [HOME] goto first page and [END] goto last page. [PageUp] for previous page."
-
-        ''' <summary>
-        ''' 使用[Enter][Down_arrow][pagedown]翻下一页[Up_arrow][Pageup]翻上一页，[q]或者[esc]结束，[home]第一页[end]最后一页
-        ''' </summary>
-        ''' <param name="initLines">无用的参数</param>
-        ''' <param name="printLines">无用的参数</param>
-        ''' <remarks></remarks>
-        Public Overrides Sub ShowManual(Optional initLines As Integer = 50, Optional printLines As Integer = 10)
-            Dim currentPage As String = _manualData(p)
-            Call PrintPrompted(p, initLines, printLines)
-
-            Do While p < Me._manualData.Count - 1
-
-                Dim c As ConsoleKeyInfo = Console.ReadKey
-
-                If c.Key = ConsoleKey.Enter OrElse
-                    c.Key = ConsoleKey.DownArrow OrElse
-                    c.Key = ConsoleKey.PageDown Then
-
-                    p += 1
-                    Call PrintPrompted(p, initLines, printLines)
-
-                ElseIf c.Key = ConsoleKey.Escape OrElse
-                    c.Key = ConsoleKey.Q Then
-                    Call Console.WriteLine()
-                    Return
-
-                ElseIf c.Key = ConsoleKey.UpArrow OrElse
-                    c.Key = ConsoleKey.PageUp Then
-                    If p > 0 Then p -= 1
-                    Call PrintPrompted(p, initLines, printLines)
-
-                ElseIf c.Key = ConsoleKey.Home Then
-                    p = 0
-                    Call PrintPrompted(p, initLines, printLines)
-
-                ElseIf c.Key = ConsoleKey.End Then
-                    p = _manualData.Count - 1
-                    Call PrintPrompted(p, initLines, printLines)
-                End If
-            Loop
-        End Sub
-
-        ''' <summary>
-        ''' 
-        ''' </summary>
-        ''' <param name="p">Current page index</param>
-        Private Sub PrintPrompted(p As Integer, initLines As Integer, printLines As Integer)
-            Dim currentPage As String = _manualData(p)
-
-            Call Console.Clear()
-            Call Console.WriteLine(Title)
-            Call Console.WriteLine()
-            Call Console.WriteLine(__sp(p, _manualData.Count))
-            Call Console.WriteLine(vbCrLf)
-
-            Dim buf As String() = Strings.Split(currentPage, vbCrLf)
-            If buf.Length >= 20 Then
-                Using Man As ManualPages = New ManualPages(buf)
-                    Call Man.ShowManual(initLines, printLines)
-                End Using
-            Else
-                Call Console.WriteLine(currentPage)
-            End If
-
-            If p < _manualData.Count - 1 Then
-                Call Console.WriteLine(vbCrLf & vbCrLf & MANUAL_PAGE_PROMPTED)
-            End If
-        End Sub
-
-        Private Shared Function __sp(p As Integer, n As Integer) As String
-            Return $"---------------------------------------------------------------------------------------------------{p + 1}/{n}--"
-        End Function
     End Class
 End Namespace
---
+++ Microsoft.VisualBasic.Core/ApplicationServices/Terminal/Utility/UnixMan/UnixManPage.vb
@@ -0,0 +1,69 @@
+﻿Namespace ApplicationServices.Terminal.Utility
+
+    ''' <summary>
+    ''' man手册页（manual pages，“手册”），是类UNIX系统最重要的手册工具。多数类UNIX都预装了它，这也包括Arch。使用man手册页的命令是：man。
+    ''' </summary>
+    Public Class UnixManPage
+
+        Public Property PROLOG As String
+
+        ''' <summary>
+        ''' 手册叙述对象名称，及简要描述。
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property NAME As String
+        ''' <summary>
+        ''' 命令参数格式，或者函数调用格式等。
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property SYNOPSIS As String
+        ''' <summary>
+        ''' 对叙述对象更加详细的描述。
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property DESCRIPTION As String
+        ''' <summary>
+        ''' 由浅入深的使用示例。
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property EXAMPLES As String
+        ''' <summary>
+        ''' 命令行或者函数调用参数的意义。
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property OPTIONS As String
+        ''' <summary>
+        ''' 不同返回（退出）代码的含义。
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property EXIT_STATUS As String
+        ''' <summary>
+        ''' 与叙述对象相关的文件。
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property FILES As String
+        ''' <summary>
+        ''' 已知的bug。
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property BUGS As String
+        ''' <summary>
+        ''' 相关内容列表。
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property SEE_ALSO As String
+        Public Property AUTHOR As String
+        Public Property HISTORY As String
+        Public Property COPYRIGHT As String
+        Public Property LICENSE As String
+        Public Property WARRANTY As String
+
+        ''' <summary>
+        ''' 生成man page的文本内容
+        ''' </summary>
+        ''' <returns></returns>
+        Public Overrides Function ToString() As String
+            Return ""
+        End Function
+    End Class
+End Namespace
