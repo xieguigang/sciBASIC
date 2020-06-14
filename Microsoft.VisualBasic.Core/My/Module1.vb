0472e958480c557895e3dd952236252f2aab4f2a
xieguigang <xie.guigang@gcmodeller.org>
2020-06-14 22:13:11 +0800

    file format flags

---
+++ Microsoft.VisualBasic.Core/My/Module1.vb
@@ -0,0 +1,12 @@
+﻿Namespace My
+
+    Public Enum FileFormats
+        xml
+        json
+        csv
+        ''' <summary>
+        ''' tsv
+        ''' </summary>
+        table
+    End Enum
+End Namespace
