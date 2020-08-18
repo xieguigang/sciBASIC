efdaf449393f3d302408dc378c83e0ebb7d1405f
xieguigang <xie.guigang@gcmodeller.org>
2020-08-18 22:49:47 +0800

    im[ropvements of the data model

--- Data_science/Mathematica/SignalProcessing/wav/wav/SubChunk/Data.vb
+++ Data_science/Mathematica/SignalProcessing/wav/wav/SubChunk/Data.vb
@@ -1,55 +1,57 @@
 ﻿#Region "Microsoft.VisualBasic::31c7f97b66510e2e447684754acc60fc, Data_science\Mathematica\SignalProcessing\wav\wav\SubChunk\Data.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class DataSubChunk
-    ' 
-    '     Properties: data
-    ' 
-    '     Function: loadData, ParseData
-    ' 
-    ' Structure Sample
-    ' 
-    '     Function: Parse16Bit, Parse8Bit, ToString
-    ' 
-    ' /********************************************************************************/
+' Class DataSubChunk
+' 
+'     Properties: data
+' 
+'     Function: loadData, ParseData
+' 
+' Structure Sample
+' 
+'     Function: Parse16Bit, Parse8Bit, ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
 Imports Microsoft.VisualBasic.Data.IO
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 Public Class DataSubChunk : Inherits SubChunk
+    Implements Enumeration(Of Sample)
 
     Public Property data As Sample()
 
@@ -87,5 +89,15 @@ Public Class DataSubChunk : Inherits SubChunk
             Case Else
                 Throw New NotImplementedException(format.GetJson)
         End Select
+    End Function
+
+    Public Iterator Function GenericEnumerator() As IEnumerator(Of Sample) Implements Enumeration(Of Sample).GenericEnumerator
+        For Each sample In data
+            Yield sample
+        Next
+    End Function
+
+    Public Iterator Function GetEnumerator() As IEnumerator Implements Enumeration(Of Sample).GetEnumerator
+        Yield GetEnumerator()
     End Function
 End Class
