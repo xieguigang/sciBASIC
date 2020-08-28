b10cd19d815c19ce6d8aa717c18d35253aa18b4e
xieguigang <xie.guigang@gcmodeller.org>
2020-08-29 01:48:17 +0800

    removes ascii text file parser for SVM models

--- Data_science/MachineLearning/MachineLearning/SVM/StorageProcedure/TextWriter/ModelText.vb
+++ Data_science/MachineLearning/MachineLearning/SVM/StorageProcedure/TextWriter/ModelText.vb
@@ -1,48 +1,49 @@
 ﻿#Region "Microsoft.VisualBasic::5559f4fb8d58a18ba65e926124908765, Data_science\MachineLearning\MachineLearning\SVM\StorageProcedure\TextWriter\ModelText.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module ModelText
-    ' 
-    '         Function: (+2 Overloads) Read
-    ' 
-    '         Sub: (+2 Overloads) Write
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module ModelText
+' 
+'         Function: (+2 Overloads) Read
+' 
+'         Sub: (+2 Overloads) Write
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
+Imports System.Text
 Imports Microsoft.VisualBasic.Text
 
 Namespace SVM
@@ -50,179 +51,11 @@ Namespace SVM
     Module ModelText
 
         ''' <summary>
-        ''' Reads a Model from the provided file.
-        ''' </summary>
-        ''' ''' <param name="filename">The name of the file containing the Model</param>
-        ''' <returns>the Model</returns>
-        Public Function Read(filename As String) As Model
-            Dim input = File.OpenRead(filename)
-
-            Try
-                Return Read(input)
-            Finally
-                input.Close()
-            End Try
-        End Function
-
-        ''' <summary>
-        ''' Reads a Model from the provided stream.
-        ''' </summary>
-        ''' ''' <param name="stream">The stream from which to read the Model.</param>
-        ''' <returns>the Model</returns>
-        Public Function Read(stream As Stream) As Model
-            Dim input As StreamReader = New StreamReader(stream)
-
-            ' read parameters
-
-            Dim model As Model = New Model()
-            Dim param As Parameter = New Parameter()
-            model.Parameter = param
-            model.Rho = Nothing
-            model.PairwiseProbabilityA = Nothing
-            model.PairwiseProbabilityB = Nothing
-            model.ClassLabels = Nothing
-            model.NumberOfSVPerClass = Nothing
-            Dim headerFinished = False
-
-            While Not headerFinished
-                Dim line As String = input.ReadLine()
-                Dim cmd, arg As String
-                Dim splitIndex = line.IndexOf(" "c)
-
-                If splitIndex >= 0 Then
-                    cmd = line.Substring(0, splitIndex)
-                    arg = line.Substring(splitIndex + 1)
-                Else
-                    cmd = line
-                    arg = ""
-                End If
-
-                arg = arg.ToLower()
-                Dim i, n As Integer
-
-                Select Case cmd
-                    Case "svm_type"
-                        param.SvmType = CType([Enum].Parse(GetType(SvmType), arg.ToUpper()), SvmType)
-                    Case "kernel_type"
-                        If Equals(arg, "polynomial") Then arg = "poly"
-                        param.KernelType = CType([Enum].Parse(GetType(KernelType), arg.ToUpper()), KernelType)
-                    Case "degree"
-                        param.Degree = Integer.Parse(arg)
-                    Case "gamma"
-                        param.Gamma = Double.Parse(arg)
-                    Case "coef0"
-                        param.Coefficient0 = Double.Parse(arg)
-                    Case "nr_class"
-                        model.NumberOfClasses = Integer.Parse(arg)
-                    Case "total_sv"
-                        model.SupportVectorCount = Integer.Parse(arg)
-                    Case "rho"
-                        n = CInt(model.NumberOfClasses * (model.NumberOfClasses - 1) / 2)
-                        model.Rho = New Double(n - 1) {}
-                        Dim rhoParts As String() = arg.Split()
-
-                        For i = 0 To n - 1
-                            model.Rho(i) = Double.Parse(rhoParts(i))
-                        Next
-
-                    Case "label"
-                        n = model.NumberOfClasses
-                        model.ClassLabels = New Integer(n - 1) {}
-                        Dim labelParts As String() = arg.Split()
-
-                        For i = 0 To n - 1
-                            model.ClassLabels(i) = Integer.Parse(labelParts(i))
-                        Next
-
-                    Case "probA"
-                        n = CInt(model.NumberOfClasses * (model.NumberOfClasses - 1) / 2)
-                        model.PairwiseProbabilityA = New Double(n - 1) {}
-                        Dim probAParts As String() = arg.Split()
-
-                        For i = 0 To n - 1
-                            model.PairwiseProbabilityA(i) = Double.Parse(probAParts(i))
-                        Next
-
-                    Case "probB"
-                        n = CInt(model.NumberOfClasses * (model.NumberOfClasses - 1) / 2)
-                        model.PairwiseProbabilityB = New Double(n - 1) {}
-                        Dim probBParts As String() = arg.Split()
-
-                        For i = 0 To n - 1
-                            model.PairwiseProbabilityB(i) = Double.Parse(probBParts(i))
-                        Next
-
-                    Case "nr_sv"
-                        n = model.NumberOfClasses
-                        model.NumberOfSVPerClass = New Integer(n - 1) {}
-                        Dim nrsvParts As String() = arg.Split()
-
-                        For i = 0 To n - 1
-                            model.NumberOfSVPerClass(i) = Integer.Parse(nrsvParts(i))
-                        Next
-
-                    Case "SV"
-                        headerFinished = True
-                    Case Else
-                        Throw New Exception("Unknown text in model file")
-                End Select
-            End While
-
-            ' read sv_coef and SV
-
-            Dim m = model.NumberOfClasses - 1
-            Dim l = model.SupportVectorCount
-            model.SupportVectorCoefficients = New Double(m - 1)() {}
-
-            For i = 0 To m - 1
-                model.SupportVectorCoefficients(i) = New Double(l - 1) {}
-            Next
-
-            model.SupportVectors = New Node(l - 1)() {}
-
-            For i = 0 To l - 1
-                Dim parts As String() = input.ReadLine().Trim().Split()
-
-                For k = 0 To m - 1
-                    model.SupportVectorCoefficients(k)(i) = Double.Parse(parts(k))
-                Next
-
-                Dim n = parts.Length - m
-                model.SupportVectors(i) = New Node(n - 1) {}
-
-                For j = 0 To n - 1
-                    Dim nodeParts = parts(m + j).Split(":"c)
-                    model.SupportVectors(i)(j) = New Node()
-                    model.SupportVectors(i)(j).Index = Integer.Parse(nodeParts(0))
-                    model.SupportVectors(i)(j).Value = Double.Parse(nodeParts(1))
-                Next
-            Next
-
-            Return model
-        End Function
-
-        ''' <summary>
-        ''' Writes a model to the provided filename.  This will overwrite any previous data in the file.
-        ''' </summary>
-        ''' ''' <param name="filename">The desired file</param>
-        ''' ''' <param name="model">The Model to write</param>
-        Public Sub Write(filename As String, model As Model)
-            Dim stream = File.Open(filename, FileMode.Create)
-
-            Try
-                Write(stream, model)
-            Finally
-                stream.Close()
-            End Try
-        End Sub
-
-        ''' <summary>
         ''' Writes a model to the provided stream.
         ''' </summary>
-        ''' ''' <param name="stream">The output stream</param>
+        ''' ''' <param name="output">The output stream</param>
         ''' ''' <param name="model">The model to write</param>
-        Public Sub Write(stream As Stream, model As Model)
-            Dim output As StreamWriter = New StreamWriter(stream)
+        Public Sub Write(output As TextWriter, model As Model)
             Dim param = model.Parameter
 
             output.Write("svm_type {0}" & ASCII.LF, param.SvmType)
@@ -318,6 +151,16 @@ Namespace SVM
 
             output.Flush()
         End Sub
+
+        Public Function ToString(model As Model) As String
+            Dim sb As New StringBuilder
+
+            Using text As New StringWriter(sb)
+                Call Write(text, model)
+            End Using
+
+            Return sb.ToString
+        End Function
     End Module
 End Namespace
 
--- Data_science/MachineLearning/MachineLearning/SVM/StorageProcedure/TextWriter/TransformText.vb
+++ Data_science/MachineLearning/MachineLearning/SVM/StorageProcedure/TextWriter/TransformText.vb
@@ -1,48 +1,49 @@
 ﻿#Region "Microsoft.VisualBasic::939f454935f73ed79a038a06271ce13e, Data_science\MachineLearning\MachineLearning\SVM\StorageProcedure\TextWriter\TransformText.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module TransformText
-    ' 
-    '         Function: (+2 Overloads) Read
-    ' 
-    '         Sub: (+2 Overloads) Write
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module TransformText
+' 
+'         Function: (+2 Overloads) Read
+' 
+'         Sub: (+2 Overloads) Write
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
+Imports System.Text
 
 Namespace SVM
 
@@ -51,10 +52,9 @@ Namespace SVM
         ''' <summary>
         ''' Writes this Range transform to a stream.
         ''' </summary>
-        ''' <param name="stream">The stream to write to</param>
+        ''' <param name="output">The stream to write to</param>
         ''' <param name="r">The range to write</param>
-        Public Sub Write(stream As Stream, r As RangeTransform)
-            Dim output As StreamWriter = New StreamWriter(stream)
+        Public Sub Write(output As TextWriter, r As RangeTransform)
             output.WriteLine(r._length)
             output.Write(r._inputStart(0))
 
@@ -74,64 +74,14 @@ Namespace SVM
             output.Flush()
         End Sub
 
-        ''' <summary>
-        ''' Writes this Range transform to a file.    This will overwrite any previous data in the file.
-        ''' </summary>
-        ''' <param name="outputFile">The file to write to</param>
-        ''' <param name="r">The Range to write</param>
-        Public Sub Write(outputFile As String, r As RangeTransform)
-            Dim s = File.Open(outputFile, FileMode.Create)
+        Public Function ToString(transform As RangeTransform) As String
+            Dim sb As New StringBuilder
 
-            Try
-                Write(s, r)
-            Finally
-                s.Close()
-            End Try
-        End Sub
+            Using text As New StringWriter(sb)
+                Call Write(text, transform)
+            End Using
 
-        ''' <summary>
-        ''' Reads a Range transform from a file.
-        ''' </summary>
-        ''' <param name="inputFile">The file to read from</param>
-        ''' <returns>The Range transform</returns>
-        Public Function Read(inputFile As String) As RangeTransform
-            Dim s = File.OpenRead(inputFile)
-
-            Try
-                Return Read(s)
-            Finally
-                s.Close()
-            End Try
-        End Function
-
-        ''' <summary>
-        ''' Reads a Range transform from a stream.
-        ''' </summary>
-        ''' <param name="stream">The stream to read from</param>
-        ''' <returns>The Range transform</returns>
-        Public Function Read(stream As Stream) As RangeTransform
-            Dim input As StreamReader = New StreamReader(stream)
-            Dim length As Integer = Integer.Parse(input.ReadLine())
-            Dim inputStart = New Double(length - 1) {}
-            Dim inputScale = New Double(length - 1) {}
-            Dim parts As String() = input.ReadLine().Split()
-
-            For i = 0 To length - 1
-                inputStart(i) = Double.Parse(parts(i))
-            Next
-
-            parts = input.ReadLine().Split()
-
-            For i = 0 To length - 1
-                inputScale(i) = Double.Parse(parts(i))
-            Next
-
-            parts = input.ReadLine().Split()
-
-            Dim outputStart = Double.Parse(parts(0))
-            Dim outputScale = Double.Parse(parts(1))
-
-            Return New RangeTransform(inputStart, inputScale, outputStart, outputScale, length)
+            Return sb.ToString
         End Function
     End Module
 End Namespace
