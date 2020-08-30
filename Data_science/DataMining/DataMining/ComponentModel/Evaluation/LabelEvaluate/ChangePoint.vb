cb3a1b6ed7357fdac2586c6e6b390c60c6eb2953
xieguigang <xie.guigang@gcmodeller.org>
2020-08-30 22:59:24 +0800

    ROC curve

---
+++ Data_science/DataMining/DataMining/ComponentModel/Evaluation/LabelEvaluate/ChangePoint.vb
@@ -0,0 +1,18 @@
+﻿Namespace ComponentModel.Evaluation
+
+    Friend Class ChangePoint
+
+        Public Sub New(tp As Integer, fp As Integer, tn As Integer, fn As Integer)
+            Me.TP = tp
+            Me.FP = fp
+            Me.TN = tn
+            Me.FN = fn
+        End Sub
+
+        Public TP, FP, TN, FN As Integer
+
+        Public Overrides Function ToString() As String
+            Return String.Format("{0}:{1}:{2}:{3}", TP, FP, TN, FN)
+        End Function
+    End Class
+End Namespace
---
+++ Data_science/DataMining/DataMining/ComponentModel/Evaluation/LabelEvaluate/RankPair.vb
@@ -0,0 +1,50 @@
+﻿Namespace ComponentModel.Evaluation
+
+    ''' <summary>
+    ''' Class encoding a member of a ranked set of labels.
+    ''' </summary>
+    Public Class RankPair : Implements IComparable(Of RankPair)
+
+        ''' <summary>
+        ''' The score for this pair.
+        ''' </summary>
+        Public ReadOnly Property Score As Double
+
+        ''' <summary>
+        ''' The Label for this pair.
+        ''' </summary>
+        Public ReadOnly Property Label As Double
+
+        ''' <summary>
+        ''' Constructor.
+        ''' </summary>
+        ''' <param name="score">Score for this pair</param>
+        ''' <param name="label">Label associated with the given score</param>
+        Public Sub New(score As Double, label As Double)
+            _Score = score
+            _Label = label
+        End Sub
+
+#Region "IComparable<RankPair> Members"
+
+        ''' <summary>
+        ''' Compares this pair to another.  It will end up in a sorted list in decending score order.
+        ''' </summary>
+        ''' <param name="other">The pair to compare to</param>
+        ''' <returns>Whether this should come before or after the argument</returns>
+        Public Function CompareTo(other As RankPair) As Integer Implements IComparable(Of RankPair).CompareTo
+            Return other.Score.CompareTo(Score)
+        End Function
+
+#End Region
+
+        ''' <summary>
+        ''' Returns a string representation of this pair.
+        ''' </summary>
+        ''' <returns>A string in the for Score:Label</returns>
+        Public Overrides Function ToString() As String
+            Return String.Format("{0}:{1}", Score, Label)
+        End Function
+    End Class
+
+End Namespace
