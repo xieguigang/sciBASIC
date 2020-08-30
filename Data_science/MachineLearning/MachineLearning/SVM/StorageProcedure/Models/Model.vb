4763af846e1136daa558b99f39c58fed3eacde6f
xieguigang <xie.guigang@gcmodeller.org>
2020-08-30 21:18:33 +0800

    svm model store procedure

--- Data_science/MachineLearning/MachineLearning/SVM/StorageProcedure/Models/Model.vb
+++ Data_science/MachineLearning/MachineLearning/SVM/StorageProcedure/Models/Model.vb
@@ -1,4 +1,6 @@
-﻿Namespace SVM.StorageProcedure
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace SVM.StorageProcedure
 
     Public Class RangeTransformModel
 
@@ -8,8 +10,23 @@
         Public Property outputScale As Double
         Public Property length As Integer
 
+        Sub New()
+        End Sub
+
+        Sub New(range As RangeTransform)
+            inputStart = range._inputStart
+            inputScale = range._inputScale
+            outputStart = range._outputStart
+            outputScale = range._outputScale
+            length = range._length
+        End Sub
+
         Public Function GetTransform() As IRangeTransform
             Return New RangeTransform(inputStart, inputScale, outputStart, outputScale, length)
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
         End Function
 
     End Class
@@ -19,8 +36,20 @@
         Public Property means As Double()
         Public Property stddevs As Double()
 
+        Sub New()
+        End Sub
+
+        Sub New(gaussian As GaussianTransform)
+            means = gaussian._means
+            stddevs = gaussian._stddevs
+        End Sub
+
         Public Function GetTransform() As IRangeTransform
             Return New GaussianTransform(means, stddevs)
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return means.GetJson
         End Function
 
     End Class
--- Data_science/MachineLearning/MachineLearning/SVM/StorageProcedure/Models/SvmModelJSON.vb
+++ Data_science/MachineLearning/MachineLearning/SVM/StorageProcedure/Models/SvmModelJSON.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.DataMining.ComponentModel.Encoder
+Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace SVM.StorageProcedure
 
@@ -9,12 +10,40 @@ Namespace SVM.StorageProcedure
         Public Property gaussianTransform As GaussianTransformModel
         Public Property factors As ColorClass()
 
+        Public Shared Function CreateJSONModel(svm As SVMModel) As SvmModelJSON
+            Return New SvmModelJSON With {
+                .model = svm.model,
+                .factors = svm.factors.Colors,
+                .gaussianTransform = If(TypeOf svm.transform Is GaussianTransform, New GaussianTransformModel(svm.transform), Nothing),
+                .rangeTransform = If(TypeOf svm.transform Is RangeTransform, New RangeTransformModel(svm.transform), Nothing)
+            }
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+
     End Class
 
     Public Class SVMMultipleSetJSON
 
         Public Property dimensionNames As String()
         Public Property topics As Dictionary(Of String, SvmModelJSON)
+
+        Public Shared Function CreateJSONModel(svm As SVMMultipleSet) As SVMMultipleSetJSON
+            Return New SVMMultipleSetJSON With {
+                .dimensionNames = svm.dimensionNames,
+                .topics = svm.topics _
+                    .ToDictionary(Function(a) a.Key,
+                                  Function(a)
+                                      Return SvmModelJSON.CreateJSONModel(a.Value)
+                                  End Function)
+            }
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
 
     End Class
 End Namespace
