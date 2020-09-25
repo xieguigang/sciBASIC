8c58411e1985a5d730346800b461c2ba2152922e
xieguigang <xie.guigang@gcmodeller.org>
2020-09-26 02:59:54 +0800

    Imports Microsoft.VisualBasic.ComponentModel.Collection

--- Data_science/Visualization/Plots-statistics/Heatmap/Internal.vb
+++ Data_science/Visualization/Plots-statistics/Heatmap/Internal.vb
@@ -45,6 +45,7 @@
 Imports System.Drawing
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ApplicationServices
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Axis
 Imports Microsoft.VisualBasic.Data.csv.IO
