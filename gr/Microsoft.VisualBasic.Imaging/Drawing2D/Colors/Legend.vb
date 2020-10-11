093fc8dbb87b3791f8915eb33fcfdbf330f956f5
xieguigang <xie.guigang@gcmodeller.org>
2020-10-11 17:25:31 +0800

    tweaks of the color bends legend plot

--- gr/Microsoft.VisualBasic.Imaging/Drawing2D/Colors/Legend.vb
+++ gr/Microsoft.VisualBasic.Imaging/Drawing2D/Colors/Legend.vb
@@ -1,44 +1,44 @@
 ﻿#Region "Microsoft.VisualBasic::4c5fd78a555d5f7528ac1fbef332a01c, gr\Microsoft.VisualBasic.Imaging\Drawing2D\Colors\Legend.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module Legends
-    ' 
-    '         Function: ColorLegendHorizontal, (+2 Overloads) ColorMapLegend
-    ' 
-    '         Sub: ColorLegendHorizontal, ColorMapLegend
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module Legends
+' 
+'         Function: ColorLegendHorizontal, (+2 Overloads) ColorMapLegend
+' 
+'         Sub: ColorLegendHorizontal, ColorMapLegend
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -95,7 +95,7 @@ Namespace Drawing2D.Colors
 
         Public Const DefaultPadding$ = "padding:50px 50px 100px 50px;"
 
-        ReadOnly defaultLegendSize As [Default](Of  Size) = New Size(800, 1024)
+        ReadOnly defaultLegendSize As [Default](Of Size) = New Size(800, 1024)
 
         ''' <summary>
         ''' 竖直的颜色图例，输出的图例的大小默认为：``{800, 1024}``
@@ -169,20 +169,24 @@ Namespace Drawing2D.Colors
                                   tickAxisStroke As Pen,
                                   Optional unmapColor$ = Nothing,
                                   Optional ruleOffset! = 10,
-                                  Optional roundDigit% = 2)
+                                  Optional roundDigit% = 2,
+                                  Optional legendOffsetLeft! = -99999)
 
             Dim titleSize As SizeF = g.MeasureString(title, titleFont)
-            Dim legendOffsetLeft!, legendOffsetTop!
+            Dim legendOffsetTop!
             Dim legendWidth! = layout.Width / 3 ' 颜色谱的宽度为layout的 1/3
             Dim legendHeight!
             Dim d!
+            Dim offsetAuto As Boolean = legendOffsetLeft < 0
 
             ' 首先计算出layout
             legendOffsetTop = titleSize.Height * 2 + 5
 
-            ' 下面的三个元素在宽度上面各自占1/3
-            ' 空白 | legend | 标尺
-            legendOffsetLeft = legendWidth
+            If offsetAuto Then
+                ' 下面的三个元素在宽度上面各自占1/3
+                ' 空白 | legend | 标尺
+                legendOffsetLeft = legendWidth
+            End If
 
             If unmapColor.StringEmpty Then
                 ' 没有unmap的颜色，则颜色谱的高度占据剩下的所有高度
