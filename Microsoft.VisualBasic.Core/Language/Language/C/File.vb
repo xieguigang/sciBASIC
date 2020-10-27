a6428296dd061167754a7a346efbe9b6a0257640
xieguigang <xie.guigang@gcmodeller.org>
2020-10-27 19:26:58 +0800

    postscript graphics

--- Microsoft.VisualBasic.Core/Language/Language/C/File.vb
+++ Microsoft.VisualBasic.Core/Language/Language/C/File.vb
@@ -1,46 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::b2460ab5907996636f333af293e0c332, Microsoft.VisualBasic.Core\Language\Language\C\File.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module File
-    ' 
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module File
+' 
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
+Imports System.Runtime.CompilerServices
 
 Namespace Language.C
 
@@ -58,5 +59,10 @@ Namespace Language.C
         ''' Specifies the end of a stream.(文件结束)
         ''' </summary>
         Public Const SEEK_END As SeekOrigin = SeekOrigin.End
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Sub fprintf(fp As StreamWriter, format As String, ParamArray args As Object())
+            Call fp.Write(sprintf(format, args))
+        End Sub
     End Module
 End Namespace
--- gr/Microsoft.VisualBasic.Imaging/PostScript/GraphicsPS.vb
+++ gr/Microsoft.VisualBasic.Imaging/PostScript/GraphicsPS.vb
@@ -1,61 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::68ea56b9d22c2830315c3c8d5e513acb, gr\Microsoft.VisualBasic.Imaging\PostScript\GraphicsPS.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class GraphicsPS
-    ' 
-    '         Properties: Clip, ClipBounds, CompositingMode, CompositingQuality, DpiX
-    '                     DpiY, InterpolationMode, IsClipEmpty, IsVisibleClipEmpty, PageScale
-    '                     PageUnit, PixelOffsetMode, RenderingOrigin, Size, SmoothingMode
-    '                     TextContrast, TextRenderingHint, Transform, VisibleClipBounds
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: (+3 Overloads) BeginContainer, GetContextInfo, GetNearestColor, (+8 Overloads) IsVisible, MeasureCharacterRanges
-    '                   (+7 Overloads) MeasureString
-    ' 
-    '         Sub: AddMetafileComment, Clear, (+4 Overloads) CopyFromScreen, Dispose, (+4 Overloads) DrawArc
-    '              (+3 Overloads) DrawBezier, (+2 Overloads) DrawBeziers, DrawCircle, (+4 Overloads) DrawClosedCurve, (+7 Overloads) DrawCurve
-    '              (+4 Overloads) DrawEllipse, (+2 Overloads) DrawIcon, DrawIconUnstretched, (+30 Overloads) DrawImage, (+4 Overloads) DrawImageUnscaled
-    '              DrawImageUnscaledAndClipped, (+4 Overloads) DrawLine, (+2 Overloads) DrawLines, DrawPath, (+4 Overloads) DrawPie
-    '              (+2 Overloads) DrawPolygon, (+4 Overloads) DrawRectangle, (+2 Overloads) DrawRectangles, (+6 Overloads) DrawString, EndContainer
-    '              (+36 Overloads) EnumerateMetafile, (+2 Overloads) ExcludeClip, (+6 Overloads) FillClosedCurve, (+4 Overloads) FillEllipse, FillPath
-    '              (+3 Overloads) FillPie, (+4 Overloads) FillPolygon, (+4 Overloads) FillRectangle, (+2 Overloads) FillRectangles, FillRegion
-    '              (+2 Overloads) Flush, (+3 Overloads) IntersectClip, (+2 Overloads) MultiplyTransform, ReleaseHdc, ReleaseHdcInternal
-    '              ResetClip, ResetTransform, Restore, (+2 Overloads) RotateTransform, (+2 Overloads) ScaleTransform
-    '              (+9 Overloads) SetClip, (+2 Overloads) TransformPoints, (+2 Overloads) TranslateClip, (+2 Overloads) TranslateTransform
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class GraphicsPS
+' 
+'         Properties: Clip, ClipBounds, CompositingMode, CompositingQuality, DpiX
+'                     DpiY, InterpolationMode, IsClipEmpty, IsVisibleClipEmpty, PageScale
+'                     PageUnit, PixelOffsetMode, RenderingOrigin, Size, SmoothingMode
+'                     TextContrast, TextRenderingHint, Transform, VisibleClipBounds
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: (+3 Overloads) BeginContainer, GetContextInfo, GetNearestColor, (+8 Overloads) IsVisible, MeasureCharacterRanges
+'                   (+7 Overloads) MeasureString
+' 
+'         Sub: AddMetafileComment, Clear, (+4 Overloads) CopyFromScreen, Dispose, (+4 Overloads) DrawArc
+'              (+3 Overloads) DrawBezier, (+2 Overloads) DrawBeziers, DrawCircle, (+4 Overloads) DrawClosedCurve, (+7 Overloads) DrawCurve
+'              (+4 Overloads) DrawEllipse, (+2 Overloads) DrawIcon, DrawIconUnstretched, (+30 Overloads) DrawImage, (+4 Overloads) DrawImageUnscaled
+'              DrawImageUnscaledAndClipped, (+4 Overloads) DrawLine, (+2 Overloads) DrawLines, DrawPath, (+4 Overloads) DrawPie
+'              (+2 Overloads) DrawPolygon, (+4 Overloads) DrawRectangle, (+2 Overloads) DrawRectangles, (+6 Overloads) DrawString, EndContainer
+'              (+36 Overloads) EnumerateMetafile, (+2 Overloads) ExcludeClip, (+6 Overloads) FillClosedCurve, (+4 Overloads) FillEllipse, FillPath
+'              (+3 Overloads) FillPie, (+4 Overloads) FillPolygon, (+4 Overloads) FillRectangle, (+2 Overloads) FillRectangles, FillRegion
+'              (+2 Overloads) Flush, (+3 Overloads) IntersectClip, (+2 Overloads) MultiplyTransform, ReleaseHdc, ReleaseHdcInternal
+'              ResetClip, ResetTransform, Restore, (+2 Overloads) RotateTransform, (+2 Overloads) ScaleTransform
+'              (+9 Overloads) SetClip, (+2 Overloads) TransformPoints, (+2 Overloads) TranslateClip, (+2 Overloads) TranslateTransform
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -63,6 +63,8 @@ Imports System.Drawing
 Imports System.Drawing.Drawing2D
 Imports System.Drawing.Imaging
 Imports System.Drawing.Text
+Imports System.IO
+Imports Microsoft.VisualBasic.Language.C
 
 Namespace PostScript
 
@@ -88,9 +90,55 @@ Namespace PostScript
         Public Overrides Property Transform As Matrix
         Public Overrides ReadOnly Property VisibleClipBounds As RectangleF
 
+        Dim ps_fontsize% = 15
+        Dim buffer As New MemoryStream
+        Dim fp As StreamWriter
+        Dim originx, originy As Single
+
         Sub New(size As Size)
             Me.Size = size
+            Me.fp = New StreamWriter(buffer)
+
+            fprintf(fp, "%%!PS-Adobe-3.0 EPSF-3.0\n")
+            fprintf(fp, "%%%%DocumentData: Clean7Bit\n")
+            fprintf(fp, "%%%\%Origin: %10.2f %10.2f\n", originx, originy)
+            fprintf(fp, "%%%%BoundingBox: %10.2f %10.2f %10.2f %10.2f\n", originx, originy, size.Width, size.Height)
+            fprintf(fp, "%%%%LanguageLevel: 2\n")
+            fprintf(fp, "%%%%Pages: 1\n")
+            fprintf(fp, "%%%%Page: 1 1                           \n")
+            fprintf(fp, "%% Convert to PDF with something like this:\n")
+            fprintf(fp, "%% gs -o OutputFileName.pdf -sDEVICE=pdfwrite -dEPSCrop InputFileName.ps\n")
+            fprintf(fp, "%% PostScript generated using the PStools library\n")
+            fprintf(fp, "%% from the Binghamton Optimality Research Group\n")
+            fprintf(fp, "%% Get the library at https://github.com/profmadden/pstools\n")
+            fprintf(fp, "%% This library is free to use, however you see fit.  It would be\n")
+            fprintf(fp, "%% nice if you let us know that you're using it, though!\n")
+            fprintf(fp, "%% Drop us an email at pmadden@binghamton.edu, or pop by our\n")
+            fprintf(fp, "%% web page, https://optimal.cs.binghamton.edu\n")
+            fprintf(fp, "%% Standard use-at-your-own-risk stuff applies....\n")
+            fprintf(fp, "/Courier findfont 15 scalefont setfont\n")
         End Sub
+
+        Public Function linewidth(width As Single) As GraphicsPS
+            fprintf(fp, "%f setlinewidth\n", width)
+            Return Me
+        End Function
+
+        Public Function color(r!, g!, b!) As GraphicsPS
+            fprintf(fp, "%3.2f %3.2f %3.2f setrgbcolor\n", r, g, b)
+            Return Me
+        End Function
+
+        Public Function font(name As String, fontsize!) As GraphicsPS
+            fprintf(fp, "/%s findfont %f scalefont setfont\n", name, fontsize)
+            ps_fontsize = fontsize
+            Return Me
+        End Function
+
+        Public Function note(noteText As String) As GraphicsPS
+            fprintf(fp, "%% %s\n", noteText)
+            Return Me
+        End Function
 
         Public Overrides Sub AddMetafileComment(data() As Byte)
             Throw New NotImplementedException()
@@ -381,7 +429,7 @@ Namespace PostScript
         End Sub
 
         Public Overrides Sub DrawLine(pen As Pen, x1 As Single, y1 As Single, x2 As Single, y2 As Single)
-            Throw New NotImplementedException()
+            fprintf(fp, "newpath %f %f moveto %f %f lineto stroke\n", x1, y1, x2, y2)
         End Sub
 
         Public Overrides Sub DrawLines(pen As Pen, points() As PointF)
@@ -413,7 +461,13 @@ Namespace PostScript
         End Sub
 
         Public Overrides Sub DrawCircle(center As PointF, fill As Color, stroke As Pen, radius As Single)
-            Throw New NotImplementedException()
+            fprintf(fp, "%f %f %f 0 360 arc closepath\n", center.X, center.Y, radius)
+
+            If Not fill.IsTransparent Then
+                fprintf(fp, "gsave fill grestore stroke\n")
+            Else
+                fprintf(fp, "stroke\n")
+            End If
         End Sub
 
         Public Overrides Sub DrawPolygon(pen As Pen, points() As PointF)
@@ -433,7 +487,24 @@ Namespace PostScript
         End Sub
 
         Public Overrides Sub DrawRectangle(pen As Pen, x As Single, y As Single, width As Single, height As Single)
-            Throw New NotImplementedException()
+            Dim x1 = x, y1 = y
+            Dim x2 = x1 + width
+            Dim y2 = y1 + height
+
+            fprintf(fp, "newpath %f %f moveto ", x1, y1)
+            fprintf(fp, "%f %f lineto ", x2, y1)
+            fprintf(fp, "%f %f lineto ", x2, y2)
+            fprintf(fp, "%f %f lineto ", x1, y2)
+            fprintf(fp, "%f %f lineto ", x1, y1)
+            '           If (Fill())Then
+            '{
+            '	If (stroke) Then
+            '                   fprintf(context -> fp, "closepath gsave fill grestore stroke\n");
+            '	Else
+            '                   fprintf(context -> fp, "closepath fill\n");
+            '}
+
+            fprintf(fp, "closepath stroke\n")
         End Sub
 
         Public Overrides Sub DrawRectangle(pen As Pen, x As Integer, y As Integer, width As Integer, height As Integer)
@@ -465,7 +536,7 @@ Namespace PostScript
         End Sub
 
         Public Overrides Sub DrawString(s As String, font As Font, brush As Brush, x As Single, y As Single)
-            Throw New NotImplementedException()
+            fprintf(fp, "%f %f moveto (%s) show\n", x, y, s)
         End Sub
 
         Public Overrides Sub DrawString(s As String, font As Font, brush As Brush, x As Single, y As Single, format As StringFormat)
@@ -729,7 +800,8 @@ Namespace PostScript
         End Sub
 
         Public Overrides Sub Flush()
-            Throw New NotImplementedException()
+            fprintf(fp, "%%%%EOF\n")
+            fp.Flush()
         End Sub
 
         Public Overrides Sub Flush(intention As FlushIntention)
