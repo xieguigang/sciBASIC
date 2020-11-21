e1804833a2bd3d2ff2dec7cbf2e112a8dbb47d4b
xieguigang <xie.guigang@gcmodeller.org>
2020-11-22 00:44:08 +0800

    fix of layout bugs

--- Data_science/DataMining/hierarchical-clustering/hierarchical-clustering/v2/DendrogramPanelv2.vb
+++ Data_science/DataMining/hierarchical-clustering/hierarchical-clustering/v2/DendrogramPanelv2.vb
@@ -4,6 +4,7 @@ Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
 Imports Microsoft.VisualBasic.DataMining.HierarchicalClustering.DendrogramVisualize
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.MIME.Markup.HTML.CSS
 
 Public Class DendrogramPanelv2 : Inherits Plot
@@ -23,16 +24,16 @@ Public Class DendrogramPanelv2 : Inherits Plot
         Dim unitWidth As Double = plotRegion.Height / hist.Leafs
         Dim scaleX As d3js.scale.LinearScale = d3js.scale.linear().domain({0, hist.DistanceValue}).range(integers:={plotRegion.Left, plotRegion.Right})
 
-        Call plotInternal(hist, unitWidth, g, plotRegion, 0, scaleX, Nothing)
+        Call DendrogramPlot(hist, unitWidth, g, plotRegion, 0, scaleX, Nothing)
     End Sub
 
-    Private Overloads Sub plotInternal(partition As Cluster,
-                                       unitWidth As Double,
-                                       ByRef g As IGraphics,
-                                       plotRegion As Rectangle,
-                                       i As Integer,
-                                       scaleX As d3js.scale.LinearScale,
-                                       parentPt As PointF)
+    Private Overloads Sub DendrogramPlot(partition As Cluster,
+                                         unitWidth As Double,
+                                         g As IGraphics,
+                                         plotRegion As Rectangle,
+                                         i As Integer,
+                                         scaleX As d3js.scale.LinearScale,
+                                         parentPt As PointF)
 
         Dim orders As Cluster() = partition.Children.OrderBy(Function(a) a.Leafs).ToArray
         Dim x = plotRegion.Right - scaleX(partition.DistanceValue)
@@ -42,7 +43,7 @@ Public Class DendrogramPanelv2 : Inherits Plot
             y = i * unitWidth + unitWidth / 2
         Else
             ' 连接节点在中间？
-            y = i * unitWidth + unitWidth / 2 + (partition.Leafs * unitWidth) / 2
+            y = i * unitWidth + (partition.Leafs * unitWidth) / 2
         End If
 
         Call g.DrawLine(Pens.Blue, parentPt, New PointF(parentPt.X, y))
@@ -54,12 +55,12 @@ Public Class DendrogramPanelv2 : Inherits Plot
         If partition.isLeaf Then
             Return
         Else
-            parentPt = New PointF(x, y)
-
             Dim n As Integer = 0
 
+            parentPt = New PointF(x, y)
+
             For Each part As Cluster In orders
-                Call plotInternal(part, unitWidth, g, plotRegion, i + n, scaleX, parentPt)
+                Call DendrogramPlot(part, unitWidth, g, plotRegion, i + n, scaleX, parentPt)
                 n += part.Leafs
             Next
         End If
