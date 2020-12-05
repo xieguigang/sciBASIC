08cea8fb8ebc8cc44713b4464fa2d366693e2cff
xieguigang <xie.guigang@gcmodeller.org>
2020-12-05 15:39:03 +0800

    improvements of the type helper

--- Data/BinaryData/msgpack/Serialization/Reflection/TypeInfo.vb
+++ Data/BinaryData/msgpack/Serialization/Reflection/TypeInfo.vb
@@ -1,16 +1,29 @@
-﻿Namespace Serialization.Reflection
+﻿Imports System.Collections
+Imports Microsoft.VisualBasic.Emit.Delegates
+
+Namespace Serialization.Reflection
 
     Public Class TypeInfo
 
-        Public Property IsGenericList As Boolean
-        Public Property IsGenericDictionary As Boolean
-        Public Property IsSerializableGenericCollection As Boolean
+        Public ReadOnly Property IsGenericList As Boolean
+        Public ReadOnly Property IsGenericDictionary As Boolean
+        Public ReadOnly Property Schema As Type
+
+        Public ReadOnly Property IsSerializableGenericCollection As Boolean
+            Get
+                Return IsGenericList OrElse IsGenericDictionary
+            End Get
+        End Property
 
         Public Sub New(type As Type)
-            IsGenericList = type.GetInterface("System.Collections.Generic.IList`1") IsNot Nothing
-            IsGenericDictionary = type.GetInterface("System.Collections.Generic.IDictionary`2") IsNot Nothing
-            IsSerializableGenericCollection = IsGenericList OrElse IsGenericDictionary
+            IsGenericList = type.ImplementInterface(GetType(IList))
+            IsGenericDictionary = type.ImplementInterface(GetType(IDictionary))
+            Schema = type
         End Sub
+
+        Public Overrides Function ToString() As String
+            Return Schema.FullName
+        End Function
 
     End Class
 End Namespace
