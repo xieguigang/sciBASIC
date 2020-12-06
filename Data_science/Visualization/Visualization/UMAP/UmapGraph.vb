f7a57d6386b4e68e60f31287e03360a6caee6722
xieguigang <xie.guigang@gcmodeller.org>
2020-12-06 12:29:02 +0800

    add projection location into graph data

--- Data_science/Visualization/Visualization/UMAP/UmapGraph.vb
+++ Data_science/Visualization/Visualization/UMAP/UmapGraph.vb
@@ -1,6 +1,9 @@
-﻿Imports System.Runtime.CompilerServices
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
 Imports Microsoft.VisualBasic.DataMining.UMAP
+Imports Microsoft.VisualBasic.Language
 
 Public Module UmapGraph
 
@@ -8,9 +11,26 @@ Public Module UmapGraph
     Public Function CreateGraph(umap As Umap, labels As String()) As NetworkGraph
         Dim matrix = umap.GetGraph.ToArray
         Dim g As New NetworkGraph
+        Dim points As PointF() = Nothing
+        Dim data As NodeData = Nothing
+        Dim index As i32 = Scan0
+
+        If umap.dimension = 2 Then
+            points = umap.GetPoint2D
+        End If
 
         For Each label As String In labels
-            Call g.CreateNode(label)
+            If Not points Is Nothing Then
+                data = New NodeData With {
+                    .label = label,
+                    .origID = label,
+                    .initialPostion = New FDGVector2(points(++index))
+                }
+            Else
+                data = Nothing
+            End If
+
+            g.CreateNode(label, data)
         Next
 
         For i As Integer = 0 To matrix.Length - 1
--- Data_science/Visualization/Visualization/UMAP/UmapRender.vb
+++ Data_science/Visualization/Visualization/UMAP/UmapRender.vb
@@ -6,21 +6,27 @@ Imports Microsoft.VisualBasic.Imaging.Driver
 Public Module UmapRender
 
     <Extension>
+    Public Function GetPoint2D(umap As Umap) As PointF()
+        If umap.dimension <> 2 Then
+            Throw New InvalidProgramException($"the given umap projection result(dimension={umap.dimension}) is not a 2D data!")
+        Else
+            Return umap.GetEmbedding() _
+                .Select(Function(vec)
+                            Return New PointF With {.X = vec(0), .Y = vec(1)}
+                        End Function) _
+                .ToArray()
+        End If
+    End Function
+
+    <Extension>
     Public Function DrawUmap2D(umap As Umap, Optional labels As IEnumerable(Of String) = Nothing) As GraphicsData
         Dim labelList As String() = Nothing
 
-        If umap.dimension <> 2 Then
-            Throw New InvalidProgramException($"the given umap projection result(dimension={umap.dimension}) is not a 2D data!")
-        End If
         If Not labels Is Nothing Then
             labelList = labels.ToArray
         End If
 
-        Dim embeddings = umap.GetEmbedding() _
-            .Select(Function(vec)
-                        Return New PointF With {.X = vec(0), .Y = vec(1)}
-                    End Function) _
-            .ToArray()
+        Dim embeddings As PointF() = umap.GetPoint2D
 
 
     End Function
