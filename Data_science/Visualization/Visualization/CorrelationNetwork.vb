265ec3ee61936fc2d2b7d3f895c799985f0dacf4
xieguigang <xie.guigang@gcmodeller.org>
2020-12-06 20:10:16 +0800

    build network graph from correlation matrix data

--- Data_science/Visualization/Visualization/CorrelationNetwork.vb
+++ Data_science/Visualization/Visualization/CorrelationNetwork.vb
@@ -1,41 +1,41 @@
 ﻿#Region "Microsoft.VisualBasic::e07fc09acb483d849b0bd5155e2f15fc, Data_science\Visualization\Visualization\CorrelationNetwork.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module CorrelationNetwork
-    ' 
-    '     Function: (+2 Overloads) BuildNetwork, HowStrong
-    ' 
-    ' /********************************************************************************/
+' Module CorrelationNetwork
+' 
+'     Function: (+2 Overloads) BuildNetwork, HowStrong
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -43,6 +43,8 @@ Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream.Generic
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 Imports Microsoft.VisualBasic.Math.Correlations
 Imports Microsoft.VisualBasic.Math.DataFrame
 Imports stdNum = System.Math
@@ -53,7 +55,7 @@ Imports stdNum = System.Math
 Public Module CorrelationNetwork
 
     <Extension>
-    Public Function BuildNetwork(data As IEnumerable(Of DataSet), cutoff#) As (net As NetworkTables, matrix As DistanceMatrix)
+    Public Function BuildNetwork(data As IEnumerable(Of DataSet), cutoff#) As (net As NetworkGraph, matrix As DistanceMatrix)
         Return data.MatrixBuilder(AddressOf Correlations.GetPearson, False).BuildNetwork(cutoff)
     End Function
 
@@ -64,39 +66,39 @@ Public Module CorrelationNetwork
     ''' <param name="cutoff"></param>
     ''' <returns></returns>
     <Extension>
-    Public Function BuildNetwork(matrix As DistanceMatrix, cutoff#) As (net As NetworkTables, matrix As DistanceMatrix)
-        Dim nodes As New Dictionary(Of Node)
-        Dim edges As New Dictionary(Of String, NetworkEdge)
+    Public Function BuildNetwork(matrix As DistanceMatrix, cutoff#) As (net As NetworkGraph, matrix As DistanceMatrix)
+        Dim g As New NetworkGraph
         Dim cor As Double
+        Dim nodeData As NodeData
+        Dim linkdata As EdgeData
 
         For Each id As String In matrix.keys
-            nodes += New Node With {
-                .ID = id
-            }
+            nodeData = New NodeData With {.origID = id, .label = id}
+            g.CreateNode(id, nodeData)
         Next
+
+        Dim uid As String
 
         For Each id As String In matrix.keys
             For Each partner As String In matrix.keys
                 cor = matrix(id, partner)
 
                 If stdNum.Abs(cor) >= cutoff Then
-                    Dim uid$ = {partner, id}.OrderBy(Function(s) s).JoinBy(" - ")
-
-                    If Not edges.ContainsKey(uid) Then
-                        edges(uid) = New NetworkEdge With {
-                            .fromNode = id,
-                            .toNode = partner,
-                            .value = cor,
-                            .interaction = HowStrong(cor)
+                    uid$ = {partner, id}.OrderBy(Function(s) s).JoinBy(" - ")
+                    linkdata = New EdgeData With {
+                        .label = uid,
+                        .length = cor,
+                        .Properties = New Dictionary(Of String, String) From {
+                            {NamesOf.REFLECTION_ID_MAPPING_INTERACTION_TYPE, HowStrong(cor)}
                         }
-                    End If
+                    }
+
+                    g.CreateEdge(id, partner, cor, linkdata)
                 End If
             Next
         Next
 
-        Dim net As New NetworkTables(nodes.Values, edges.Values)
-
-        Return (net, matrix)
+        Return (g, matrix)
     End Function
 
     Private Function HowStrong(c#) As String
