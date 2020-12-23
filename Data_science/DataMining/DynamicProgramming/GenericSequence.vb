588f71782eb59a50911a2800c7879d233da0c1c1
xieguigang <xie.guigang@gcmodeller.org>
2020-12-23 18:24:34 +0800

    generic sequence symbol

---
+++ Data_science/DataMining/DynamicProgramming/GenericSequence.vb
@@ -0,0 +1,45 @@
+﻿Public Class GenericSequence(Of T)
+
+    Dim m_seq As T()
+    Dim m_equals As Func(Of T, T, Boolean)
+
+    Default Public ReadOnly Property Item(i As Integer) As T
+        Get
+            Return m_seq(i)
+        End Get
+    End Property
+
+    Public ReadOnly Property length As Integer
+        Get
+            Return m_seq.Length
+        End Get
+    End Property
+
+    Public Function GetAtOrDefault(i As Integer) As T
+        If i >= m_seq.Length Then
+            Return Nothing
+        Else
+            Return m_seq(i)
+        End If
+    End Function
+
+    Public Shared Operator =(a As GenericSequence(Of T), b As GenericSequence(Of T)) As Boolean
+        If a Is b Then
+            Return True
+        ElseIf a.length <> b.length Then
+            Return False
+        Else
+            For i As Integer = 0 To a.length - 1
+                If Not a.m_equals(a.m_seq(i), b.m_seq(i)) Then
+                    Return False
+                End If
+            Next
+
+            Return True
+        End If
+    End Operator
+
+    Public Shared Operator <>(a As GenericSequence(Of T), b As GenericSequence(Of T)) As Boolean
+        Return Not a = b
+    End Operator
+End Class
---
+++ Data_science/DataMining/DynamicProgramming/GenericSymbol.vb
@@ -0,0 +1,6 @@
+﻿Public Class GenericSymbol(Of T)
+
+    Friend ReadOnly m_equals As Func(Of T, T, Boolean)
+    Friend ReadOnly m_similarity As Func(Of T, T, Double)
+
+End Class
