9bc8a00177494e47a75167492862a825e00f8391
xieguigang <xie.guigang@gcmodeller.org>
2020-12-27 21:11:38 +0800

    split souec file

--- Data_science/DataMining/DynamicProgramming/NeedlemanWunsch/Workspace.vb
+++ Data_science/DataMining/DynamicProgramming/NeedlemanWunsch/Workspace.vb
@@ -54,9 +54,7 @@
 #End Region
 
 Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic.ComponentModel.Algorithm.DynamicProgramming
 Imports Microsoft.VisualBasic.Linq
-Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace NeedlemanWunsch
 
@@ -148,47 +146,5 @@ Namespace NeedlemanWunsch
             __toChar = toChar
             scoreMatrix = score
         End Sub
-    End Class
-
-    Public Class ScoreMatrix(Of T)
-
-        ''' <summary>
-        ''' get gap open penalty </summary>
-        ''' <returns> gap open penalty </returns>
-        Public Property GapPenalty As Integer = 1
-
-        ''' <summary>
-        ''' get match score </summary>
-        ''' <returns> match score </returns>
-        Public Property MatchScore As Integer = 1
-
-        ''' <summary>
-        ''' get mismatch score </summary>
-        ''' <returns> mismatch score </returns>
-        Public Property MismatchScore As Integer = -1
-
-        Friend ReadOnly __equals As IEquals(Of T)
-
-        Sub New(match As IEquals(Of T))
-            __equals = match
-        End Sub
-
-        ''' <summary>
-        ''' if char a is equal to char b
-        ''' return the match score
-        ''' else return mismatch score
-        ''' </summary>
-        Public Overridable Function getMatchScore(a As T, b As T) As Integer
-            If __equals(a, b) Then
-                Return MatchScore
-            Else
-                Return MismatchScore
-            End If
-        End Function
-
-        Public Overrides Function ToString() As String
-            Return Me.GetJson
-        End Function
-
     End Class
 End Namespace
