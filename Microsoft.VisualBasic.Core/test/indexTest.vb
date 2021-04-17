e57aedfca45ac9cda36eabddd9d9aa75b55c00f1
xieguigang <xie.guigang@gcmodeller.org>
2021-04-17 20:01:31 +0800

    test string find

--- Microsoft.VisualBasic.Core/test/indexTest.vb
+++ Microsoft.VisualBasic.Core/test/indexTest.vb
@@ -1,49 +1,52 @@
 ﻿#Region "Microsoft.VisualBasic::be910c7052ce588d26e7084892359f54, Microsoft.VisualBasic.Core\test\indexTest.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module indexTest
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Module indexTest
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic.Data.Trinity
 
 Module indexTest
 
     Sub Main()
+        Call stringTest()
+
         Dim index As New WordSimilarityIndex(Of String)
 
         For Each item As String In 5000.SeqRandom.Select(Function(l) RandomASCIIString(20, True))
@@ -53,6 +56,39 @@ Module indexTest
         Next
 
         Dim result = index.FindMatches("Aaaaaaaaaaaaaaaaaaaa").ToArray
+
+        Pause()
+    End Sub
+
+    Sub stringTest()
+        Dim key As String = "abc"
+        Dim keyRegexp As New Regex("[a][b][c]", RegexOptions.Singleline Or RegexOptions.Compiled)
+        Dim target As String = "sdfm,sdfjklsdfsabcklfs"
+        Dim loops As Integer = 100000000
+
+        Call BENCHMARK(Sub()
+                           For i As Integer = 0 To loops
+                               If target.Contains(key) Then
+
+                               End If
+                           Next
+                       End Sub)
+
+        Call BENCHMARK(Sub()
+                           For i As Integer = 0 To loops
+                               If target.IndexOf(key) > -1 Then
+
+                               End If
+                           Next
+                       End Sub)
+
+        Call BENCHMARK(Sub()
+                           For i As Integer = 0 To loops
+                               If keyRegexp.Match(key).Success Then
+
+                               End If
+                           Next
+                       End Sub)
 
         Pause()
     End Sub
