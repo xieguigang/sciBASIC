8cbdc544908c7cd2db533cfefb9cff87950a7fd0
xieguigang <xie.guigang@gcmodeller.org>
2021-05-01 18:19:45 +0800

    improvements of the html parser api

--- Microsoft.VisualBasic.Core/src/Text/Parser/HtmlParser/Table.vb
+++ Microsoft.VisualBasic.Core/src/Text/Parser/HtmlParser/Table.vb
@@ -1,46 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::3b8d61b2e49b24f41dbcb2d1e65263b8, Microsoft.VisualBasic.Core\src\Text\Parser\HtmlParser\Table.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module TableParser
-    ' 
-    '         Function: GetColumnsHTML, GetRowsHTML, GetTablesHTML, ParseHtmlMeta
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module TableParser
+' 
+'         Function: GetColumnsHTML, GetRowsHTML, GetTablesHTML, ParseHtmlMeta
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports System.Text
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Linq
 Imports r = System.Text.RegularExpressions.Regex
@@ -60,12 +61,18 @@ Namespace Text.Parser.HtmlParser
         ''' 
         <Extension>
         Public Function GetTablesHTML(html As String, Optional greedy As Boolean = False) As String()
+            ' 20210501 bugs fixed of table tag in html comments
+            ' will make this parser error
+            Dim stripComments = New StringBuilder(html) _
+                .RemovesHtmlComments _
+                .ToString
             Dim regxp As String = If(greedy, "<table.+</table>", "<table.+?</table>")
-            Dim tbls As String() = r.Matches(html, regxp, RegexICSng).ToArray
+            Dim tbls As String() = r.Matches(stripComments, regxp, RegexICSng).ToArray
             Return tbls
         End Function
 
         Const RowPatterns$ = "<tr.+?</tr>"
+        Const RowGreedyPatterns$ = "<tr.+</tr>"
         Const ColumnPatterns$ = "(<td.+?</td>)|(<th.+?</th>)"
 
         ''' <summary>
@@ -75,11 +82,13 @@ Namespace Text.Parser.HtmlParser
         ''' <returns></returns>
         ''' 
         <Extension>
-        Public Function GetRowsHTML(table As String) As String()
+        Public Function GetRowsHTML(table As String, Optional greedy As Boolean = False) As String()
             If table Is Nothing Then
                 Return {}
             Else
-                Dim rows As String() = r.Matches(table, RowPatterns, RegexICSng).ToArray
+                Dim regxp As String = If(greedy, RowGreedyPatterns, RowPatterns)
+                Dim rows As String() = r.Matches(table, regxp, RegexICSng).ToArray
+
                 Return rows
             End If
         End Function
@@ -91,11 +100,14 @@ Namespace Text.Parser.HtmlParser
         ''' <returns></returns>
         ''' 
         <Extension>
-        Public Function GetColumnsHTML(row As String) As String()
-            Dim cols As String() = r.Matches(row, ColumnPatterns, RegexICSng).ToArray
+        Public Function GetColumnsHTML(row As String, Optional greedy As Boolean = False) As String()
+            Dim columnPattern = If(greedy, ColumnPatterns.Replace("?", ""), ColumnPatterns)
+            Dim cols As String() = r.Matches(row, columnPattern, RegexICSng).ToArray
+
             cols = cols _
                 .Select(Function(s) s.GetValue) _
                 .ToArray
+
             Return cols
         End Function
 
