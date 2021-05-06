8f1272f9bed585e05a515c87ae8bc23440c18b20
xieguigang <xie.guigang@gcmodeller.org>
2021-05-06 22:49:51 +0800

    improvements of the stack helper

--- Microsoft.VisualBasic.Core/src/ComponentModel/Counter.vb
+++ Microsoft.VisualBasic.Core/src/ComponentModel/Counter.vb
@@ -1,50 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::9d92e0c5aa27014389535aef2669c1eb, Microsoft.VisualBasic.Core\src\ComponentModel\Count.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class Counter
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    ' 
-    '         Function: Hit
-    ' 
-    '         Sub: Add
-    ' 
-    '     Module CounterExtensions
-    ' 
-    '         Function: AsInteger, AsNumeric
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class Counter
+' 
+'         Constructor: (+2 Overloads) Sub New
+' 
+'         Function: Hit
+' 
+'         Sub: Add
+' 
+'     Module CounterExtensions
+' 
+'         Function: AsInteger, AsNumeric
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -111,6 +111,24 @@ Namespace ComponentModel
         <Extension>
         Public Function AsInteger(Of K)(counter As Dictionary(Of K, Counter)) As Dictionary(Of K, Integer)
             Return counter.ToDictionary(Function(c) c.Key, Function(c) CInt(c.Value))
+        End Function
+
+        ''' <summary>
+        ''' populate n elements from current stack
+        ''' </summary>
+        ''' <typeparam name="T"></typeparam>
+        ''' <param name="stack"></param>
+        ''' <param name="n"></param>
+        ''' <returns></returns>
+        <Extension>
+        Public Function Pop(Of T)(stack As Stack(Of T), n As Counter) As T()
+            Dim out As T() = New T(n.Value - 1) {}
+
+            For i As Integer = 0 To out.Length - 1
+                out(i) = stack.Pop
+            Next
+
+            Return out
         End Function
     End Module
 End Namespace
