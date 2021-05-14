e3cdaeefebfea3c466c20678d3cc9657db83a09a
关倩倩 <amethyst.asuka@gcmodeller.org>
2021-05-14 23:51:06 +0800

    create html tree stack

--- mime/text%html/Parser/HtmlParser.vb
+++ mime/text%html/Parser/HtmlParser.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.Emit.Marshal
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.MIME.Markup.HTML
 
 Public Class HtmlParser
@@ -15,14 +16,26 @@ Public Class HtmlParser
 
     Private Shared Sub WalkElement(i As Pointer(Of Token), parent As HtmlElement)
 LOOP_NEXT:
-        Dim a As Token = ++i
+        Dim a As Value(Of Token) = ++i
 
-        Select Case a.name
+        Select Case CType(a, Token).name
             Case HtmlTokens.openTag
                 ' add new element tag
+                ' <tag
                 Dim newTag As New HtmlElement With {.Name = (++i).text}
 
+                Do While (a = ++i).name <> HtmlTokens.closeTag
+                    ' name=value
+                    If i.Current.name = HtmlTokens.equalsSymbol Then
+                        i.MoveNext()
+                        newTag.Add(CType(a, Token).text, (++i).text)
+                    Else
+                        newTag.Add(CType(a, Token).text, "")
+                    End If
+                Loop
+
                 parent.Add(newTag)
+
                 WalkElement(i, newTag)
             Case HtmlTokens.splash
                 If i.Current.name = HtmlTokens.closeTag Then
@@ -33,14 +46,8 @@ LOOP_NEXT:
                     parent.Add(New InnerPlantText("/"))
                 End If
             Case HtmlTokens.text
-                If i.Current.name = HtmlTokens.equalsSymbol Then
-                    ' xxx = xxx
-                    i.MoveNext()
-                    parent.Add(a.text, (++i).text)
-                    GoTo LOOP_NEXT
-                Else
-                    parent.Add(New InnerPlantText(a.text))
-                End If
+                parent.Add(New InnerPlantText(CType(a, Token).text))
+                GoTo LOOP_NEXT
         End Select
     End Sub
 End Class
