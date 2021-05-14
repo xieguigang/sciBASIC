0cbb06bfc8830bd52e421b9d4be1da1cb2baee55
关倩倩 <amethyst.asuka@gcmodeller.org>
2021-05-14 23:09:47 +0800

    config for test

--- mime/text%html/Parser/TokenIcer.vb
+++ mime/text%html/Parser/TokenIcer.vb
@@ -1,11 +1,43 @@
-﻿Public Class TokenIcer
+﻿Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Text.Parser
+
+Public Class TokenIcer
+
+    Dim buf As New CharBuffer
+    Dim text As CharPtr
+    Dim escape As New Escaping
+
+    Sub New(document As CharPtr)
+        Me.text = document
+    End Sub
 
     Public Iterator Function GetTokens() As IEnumerable(Of Token)
+        Dim token As New Value(Of Token)
 
+        Do While text
+            If Not token = WalkChar(++text) Is Nothing Then
+                Yield CType(token, Token)
+            End If
+        Loop
+
+        If buf > 0 Then
+            Yield MeasureToken(New String(buf.PopAllChars))
+        End If
     End Function
 
     Private Function WalkChar(c As Char) As Token
 
     End Function
+
+    Private Function MeasureToken(text As String) As Token
+
+    End Function
+
+End Class
+
+Friend Class Escaping
+
+    Public [string] As Boolean
+    Public quote As Char
 
 End Class
--- mime/text%html/test/HTMLParserTest.vb
+++ mime/text%html/test/HTMLParserTest.vb
@@ -1,44 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::5f4a02196e5d904c443739b01603be6d, mime\text%html\test\HTMLParserTest.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module HTMLParserTest
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Module HTMLParserTest
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports Microsoft.VisualBasic.MIME.Markup
 Imports Microsoft.VisualBasic.MIME.Markup.HTML
 
 Module HTMLParserTest
@@ -50,8 +51,11 @@ Module HTMLParserTest
          </div>).ToString
 
     Sub Main()
+        Dim tokens = New TokenIcer(testHTML).GetTokens.ToArray
 
-        Dim content = TextAPI.TryParse(testHTML).ToArray
+
+
+        ' Dim content = TextAPI.TryParse(testHTML).ToArray
 
         Pause()
     End Sub
