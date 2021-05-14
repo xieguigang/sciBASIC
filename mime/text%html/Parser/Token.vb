02fa093aa202689b76ad55a7d3199eba2451033c
关倩倩 <amethyst.asuka@gcmodeller.org>
2021-05-14 22:49:13 +0800

    parse html tokens?

---
+++ mime/text%html/Parser/Token.vb
@@ -0,0 +1,8 @@
+﻿Imports Microsoft.VisualBasic.Scripting.TokenIcer
+
+Public Class Token : Inherits CodeToken(Of HtmlTokens)
+
+    Sub New(type As HtmlTokens, text As String)
+        Call MyBase.New(type, text)
+    End Sub
+End Class
---
+++ mime/text%html/Parser/TokenIcer.vb
@@ -0,0 +1,11 @@
+﻿Public Class TokenIcer
+
+    Public Iterator Function GetTokens() As IEnumerable(Of Token)
+
+    End Function
+
+    Private Function WalkChar(c As Char) As Token
+
+    End Function
+
+End Class
