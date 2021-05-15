9f68b312238af465780e24ab6af3cba7f737996a
xieguigang <xie.guigang@gcmodeller.org>
2021-05-15 17:37:51 +0800

    text parser

--- Data/GraphQuery/Query/Parser.vb
+++ Data/GraphQuery/Query/Parser.vb
@@ -17,31 +17,3 @@ Public Class Parser
     End Function
 
 End Class
-
-Public MustInherit Class ParserFunction
-
-    Public MustOverride Function GetToken(document As InnerPlantText) As InnerPlantText
-
-End Class
-
-Public Class InternalInvoke : Inherits ParserFunction
-
-    Public Property name As String
-
-    Public Overrides Function GetToken(document As InnerPlantText) As InnerPlantText
-        Throw New NotImplementedException()
-    End Function
-End Class
-
-Public Class CustomFunction : Inherits ParserFunction
-
-    Dim parse As Func(Of InnerPlantText, InnerPlantText)
-
-    Sub New(parse As Func(Of InnerPlantText, InnerPlantText))
-        Me.parse = parse
-    End Sub
-
-    Public Overrides Function GetToken(document As InnerPlantText) As InnerPlantText
-        Throw New NotImplementedException()
-    End Function
-End Class
---
+++ Data/GraphQuery/TextParser/ParserFunction.vb
@@ -0,0 +1,33 @@
+﻿
+Imports Microsoft.VisualBasic.MIME.Markup.HTML
+
+Namespace TextParser
+
+    Public MustInherit Class ParserFunction
+
+        Public MustOverride Function GetToken(document As InnerPlantText) As InnerPlantText
+
+    End Class
+
+    Public Class InternalInvoke : Inherits ParserFunction
+
+        Public Property name As String
+
+        Public Overrides Function GetToken(document As InnerPlantText) As InnerPlantText
+            Throw New NotImplementedException()
+        End Function
+    End Class
+
+    Public Class CustomFunction : Inherits ParserFunction
+
+        Dim parse As Func(Of InnerPlantText, InnerPlantText)
+
+        Sub New(parse As Func(Of InnerPlantText, InnerPlantText))
+            Me.parse = parse
+        End Sub
+
+        Public Overrides Function GetToken(document As InnerPlantText) As InnerPlantText
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
