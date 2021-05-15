d35c25fb7451d8e42c2a760e67fe33d83435227f
xieguigang <xie.guigang@gcmodeller.org>
2021-05-15 19:48:31 +0800

    implements css query

--- Data/GraphQuery/Engine.vb
+++ Data/GraphQuery/Engine.vb
@@ -7,6 +7,30 @@ Imports Microsoft.VisualBasic.MIME.Markup.HTML
 Public Class Engine
 
     Public Function Execute(document As HtmlElement, query As Query) As JsonElement
+        If Not query.members.IsNullOrEmpty Then
+            ' object
+            Return QueryObject(document, query)
+        Else
+            ' value
+            Return QueryValue(document, query)
+        End If
+    End Function
 
+    Private Function QueryValue(document As HtmlElement, query As Query) As JsonValue
+        Dim value As InnerPlantText = query.parser.Parse(document, query.isArray, Me)
+        Dim valStr As String = value.GetPlantText
+        Dim json As New JsonValue(valStr)
+
+        Return json
+    End Function
+
+    Private Function QueryObject(document As HtmlElement, query As Query) As JsonObject
+        Dim obj As New JsonObject
+
+        For Each member As Query In query.members
+            obj.Add(member.name, Execute(document, member))
+        Next
+
+        Return obj
     End Function
 End Class
--- Data/GraphQuery/Query/Parser.vb
+++ Data/GraphQuery/Query/Parser.vb
@@ -6,8 +6,82 @@ Public Class Parser
     Public Property parameters As String()
     Public Property pipeNext As Parser
 
-    Public Function Parse(document As InnerPlantText, env As Engine) As InnerPlantText
-        Throw New NotImplementedException
+    Public Function Parse(document As InnerPlantText, isArray As Boolean, env As Engine) As InnerPlantText
+        Dim value As InnerPlantText
+
+        Select Case func
+            Case "css"
+                value = CssQuery(document, isArray)
+            Case "attr"
+                value = QueryAttribute(document, isArray)
+            Case "xpath"
+                value = XPathQuery(document, isArray)
+            Case Else
+                Throw New NotImplementedException(func)
+        End Select
+
+        If Not pipeNext Is Nothing Then
+            value = pipeNext.Parse(value, isArray, env)
+        End If
+
+        Return value
+    End Function
+
+    Private Function XPathQuery(document As HtmlElement, isArray As Boolean) As InnerPlantText
+
+    End Function
+
+    Private Function CssQuery(document As HtmlElement, isArray As Boolean) As InnerPlantText
+        Dim query As String = parameters(Scan0)
+
+        If query.First = "#"c Then
+            ' get element by id
+            Return DirectCast(document, HtmlElement).getElementById(query.Substring(1))
+        ElseIf query.First = "."c Then
+            Dim n As String = parameters.ElementAtOrDefault(1)
+            Dim list As HtmlElement() = DirectCast(document, HtmlElement).getElementsByClassName(query.Substring(1))
+
+            If isArray Then
+                ' get elements by class name
+                Return New HtmlElement With {
+                    .TagName = query,
+                    .HtmlElements = list
+                }
+            Else
+                Return list(CInt(Val(n)))
+            End If
+        Else
+            Dim n As String = parameters.ElementAtOrDefault(1)
+            Dim list As HtmlElement() = DirectCast(document, HtmlElement).getElementsByTagName(query)
+
+            If isArray Then
+                ' get elements by tag name
+                Return New HtmlElement With {
+                    .TagName = query,
+                    .HtmlElements = list
+                }
+            Else
+                Return list(CInt(Val(n)))
+            End If
+        End If
+    End Function
+
+    Private Function QueryAttribute(document As HtmlElement, isArray As Boolean) As InnerPlantText
+        If isArray Then
+            Return New HtmlElement With {
+                .HtmlElements = DirectCast(document, HtmlElement)(parameters(Scan0)).Values _
+                    .Select(Function(a)
+                                Return New InnerPlantText With {
+                                    .InnerText = a
+                                }
+                            End Function) _
+                    .ToArray
+            }
+        Else
+            Return New InnerPlantText With {
+                .InnerText = DirectCast(document, HtmlElement)(parameters(Scan0)).Value
+            }
+        End If
     End Function
 
     Public Overrides Function ToString() As String
