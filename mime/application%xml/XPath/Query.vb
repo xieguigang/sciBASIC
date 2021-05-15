d59b103c52cf0519fedbbc63ca149cc7ace45ffe
xieguigang <xie.guigang@gcmodeller.org>
2021-05-15 20:06:32 +0800

    xpath models

---
+++ mime/application%xml/XPath/Query.vb
@@ -0,0 +1,3 @@
+﻿Public Class Query
+
+End Class
---
+++ mime/application%xml/XPath/XPath.vb
@@ -0,0 +1,44 @@
+﻿
+''' <summary>
+''' 1. ``nodename`` 选取此节点的所有子节点。
+''' 2. ``/``        从根节点选取。
+''' 3. ``//``       从匹配选择的当前节点选择文档中的节点，而不考虑它们的位置。
+''' 4. ``.``        选取当前节点。
+''' 5. ``..``       选取当前节点的父节点。
+''' 6. ``@``        选取属性。
+''' </summary>
+Public MustInherit Class XPath
+
+    Public Property expression As String
+
+    Public Shared Function Parse(expression As String) As XPath
+
+    End Function
+
+End Class
+
+Public Class SelectByNodeName : Inherits XPath
+
+End Class
+
+Public Class RootPathSelector : Inherits XPath
+
+End Class
+
+Public Class CurrentNodes : Inherits XPath
+
+End Class
+
+Public Class CurrentNode : Inherits XPath
+
+End Class
+
+Public Class ParentNode : Inherits XPath
+
+
+End Class
+
+Public Class SelectAttributes : Inherits XPath
+
+
+End Class
