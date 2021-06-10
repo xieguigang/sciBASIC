aae627b8bbb7e6bc79a518e7fc697d6d077b9792
xieguigang <xie.guigang@gcmodeller.org>
2021-06-10 23:50:25 +0800

    Update Sqlite3Database.vb

--- Data/BinaryData/DataStorage/SQLite3/Sqlite3Database.vb
+++ Data/BinaryData/DataStorage/SQLite3/Sqlite3Database.vb
@@ -1,52 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::7e34192d086e6c575a04bc3c602d8318, Data\BinaryData\DataStorage\SQLite3\Sqlite3Database.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class Sqlite3Database
-    ' 
-    '         Properties: GetTables, Header
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: GetTable, OpenFile
-    ' 
-    '         Sub: Dispose, Initialize, InitializeMasterTable
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class Sqlite3Database
+' 
+'         Properties: GetTables, Header
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: GetTable, OpenFile
+' 
+'         Sub: Dispose, Initialize, InitializeMasterTable
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Collections.Generic
 Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.IO.ManagedSqlite.Core.Internal
