e9f291d55ae93b3a4299a81455bfd167db509c0d
xieguigang <xie.guigang@gcmodeller.org>
2021-06-20 14:03:18 +0800

    Update imageSmoothTest.vb

--- gr/Microsoft.VisualBasic.Imaging/test/imageSmoothTest.vb
+++ gr/Microsoft.VisualBasic.Imaging/test/imageSmoothTest.vb
@@ -1,5 +1,6 @@
 ﻿Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Filters
+Imports Microsoft.VisualBasic.Linq
 
 Module imageSmoothTest
 
@@ -10,5 +11,20 @@ Module imageSmoothTest
         Call bitmap2.SaveAs("E:\Resources\a.html.png")
 
         Call GaussBlur.GaussBlur(bitmap1).SaveAs("E:\Resources\a.htm222l.png")
+
+
+        Dim matrix As New Filters.Matrix(bitmap1)
+
+        Call matrix.GetBitmap(Matrix2DFilters.Median).SaveAs("E:\Resources\a.htm222l_median.png")
+        Call matrix.GetBitmap(Matrix2DFilters.Mean).SaveAs("E:\Resources\a.htm222l_mean.png")
+        Call matrix.GetBitmap(Matrix2DFilters.Min).SaveAs("E:\Resources\a.htm222l_min.png")
+        Call matrix.GetBitmap(Matrix2DFilters.Max).SaveAs("E:\Resources\a.htm222l_max.png")
+
+
+        Call matrix.GetBitmap(Matrix2DFilters.Median).DoCall(AddressOf GaussBlur.GaussBlur).SaveAs("E:\Resources\a.htm222l_median+Gauss.png")
+        Call New Matrix(GaussBlur.GaussBlur(bitmap1)).GetBitmap(Matrix2DFilters.Median).SaveAs("E:\Resources\a.htm222l_Gauss+median.png")
+        Call New Matrix(GaussBlur.GaussBlur(bitmap1)).GetBitmap(Matrix2DFilters.Mean).SaveAs("E:\Resources\a.htm222l_Gauss+mean.png")
+        Call New Matrix(GaussBlur.GaussBlur(bitmap1)).GetBitmap(Matrix2DFilters.Min).SaveAs("E:\Resources\a.htm222l_Gauss+min.png")
+        Call New Matrix(GaussBlur.GaussBlur(bitmap1)).GetBitmap(Matrix2DFilters.Max).SaveAs("E:\Resources\a.htm222l_Gauss+max.png")
     End Sub
 End Module
