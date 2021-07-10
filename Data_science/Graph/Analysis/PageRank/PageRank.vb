c82c3f1452dfe060c338c421b72b2e1673611713
xieguigang <xie.guigang@gcmodeller.org>
2021-07-10 21:04:30 +0800

    make parallel

--- Data_science/Graph/Analysis/PageRank/PageRank.vb
+++ Data_science/Graph/Analysis/PageRank/PageRank.vb
@@ -1,46 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::4371a6d5bcf83146a00c22b0c7bfa73b, Data_science\Graph\Analysis\PageRank\PageRank.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class PageRank
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: ComputePageRank, PageRankGenerator, TransposeLinkMatrix
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class PageRank
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: ComputePageRank, PageRankGenerator, TransposeLinkMatrix
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
 
 Namespace Analysis.PageRank
@@ -189,16 +190,20 @@ Namespace Analysis.PageRank
         Private Function PageRankLoop1(ByRef iNew As Vector,
                                        ByRef iOld As Vector,
                                        ByRef checkSteps%,
-                                       ByRef alpha#,
+                                       alpha#,
                                        ByRef M%,
                                        ByRef N%,
                                        ByRef leafNodes As List(Of Integer),
-                                       ByRef at As List(Of Integer)(),
-                                       ByRef numLinks As Vector) As Vector
+                                       at As List(Of Integer)(),
+                                       numLinks As Vector) As Vector
+
+            Dim tempVec As Vector
 
             For i As Integer = 0 To checkSteps - 1
                 ' swap arrays
-                Call iOld.Swap(iNew)
+                tempVec = iNew
+                iNew = iOld
+                iOld = tempVec
 
                 ' an element in the 1 x I vector. 
                 ' all elements are identical.
@@ -213,16 +218,25 @@ Namespace Analysis.PageRank
                 End If
 
                 ' the elements of the H x I multiplication
+                Dim eval = N.Sequence _
+                    .AsParallel _
+                    .Select(Function(j)
+                                Dim page As List(Of Integer) = at(j)
+                                Dim h As Double = 0
+
+                                If page.Count > 0 Then
+                                    ' .DotProduct
+                                    h = alpha * tempVec.Take(page).DotProduct(1.0 / numLinks.Take(page))
+                                End If
+
+                                Return (h + oneAv + oneIv, j)
+                            End Function) _
+                    .OrderBy(Function(d) d.j) _
+                    .Select(Function(d) d.Item1) _
+                    .ToArray
+
                 For j As Integer = 0 To N - 1
-                    Dim page As List(Of Integer) = at(j)
-                    Dim h As Double = 0
-
-                    If page.Count > 0 Then
-                        ' .DotProduct
-                        h = alpha * iOld.Take(page).DotProduct(1.0 / numLinks.Take(page))
-                    End If
-
-                    iNew(j) = h + oneAv + oneIv
+                    iNew(j) = eval(j)
                 Next
             Next
 
