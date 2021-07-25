e2a0a48cbb83a8c91a70b202a94a529312cfa68b
xieguigang <xie.guigang@gcmodeller.org>
2021-07-25 18:29:22 +0800

    Update BitmapBuffer.vb

--- Microsoft.VisualBasic.Core/src/Extensions/Image/Bitmap/BitmapBuffer.vb
+++ Microsoft.VisualBasic.Core/src/Extensions/Image/Bitmap/BitmapBuffer.vb
@@ -1,51 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::5d3a8020e3457d39268c231f63cb01ac, Microsoft.VisualBasic.Core\src\Extensions\Image\Bitmap\BitmapBuffer.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class BitmapBuffer
-    ' 
-    '         Properties: Height, Size, Stride, Width
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: FromBitmap, FromImage, GetEnumerator, GetImage, GetIndex
-    '                   GetPixel, IEnumerable_GetEnumerator, OutOfRange
-    ' 
-    '         Sub: Dispose, SetPixel
-    ' 
-    '         Operators: +
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class BitmapBuffer
+' 
+'         Properties: Height, Size, Stride, Width
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: FromBitmap, FromImage, GetEnumerator, GetImage, GetIndex
+'                   GetPixel, IEnumerable_GetEnumerator, OutOfRange
+' 
+'         Sub: Dispose, SetPixel
+' 
+'         Operators: +
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -128,6 +128,12 @@ Namespace Imaging.BitmapImage
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Function GetIndex(x As Integer, y As Integer) As Integer
             y = y * (Width * channels)
+            x = x * channels
+            Return x + y
+        End Function
+
+        Public Shared Function GetIndex(x As Integer, y As Integer, width As Integer, channels As Integer) As Integer
+            y = y * (width * channels)
             x = x * channels
             Return x + y
         End Function
