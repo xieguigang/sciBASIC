9850795b349762fca75f8f004fae12057418c47f
xieguigang <xie.guigang@gcmodeller.org>
2021-08-07 16:27:08 +0800

    Update Edge.vb

--- Data_science/Graph/Analysis/Louvain/Edge.vb
+++ Data_science/Graph/Analysis/Louvain/Edge.vb
@@ -1,10 +1,17 @@
 ﻿
 Namespace Analysis.Louvain
 
-    Public Class Edge
-        Friend v As Integer 'v表示连接点的编号,w表示此边的权值
+    Friend Class Edge
+
+        ''' <summary>
+        ''' v表示连接点的编号,w表示此边的权值
+        ''' </summary>
+        Friend v As Integer
         Friend weight As Double
-        Friend [next] As Integer 'next负责连接和此点相关的边
+        ''' <summary>
+        ''' next负责连接和此点相关的边
+        ''' </summary>
+        Friend [next] As Integer
 
         Friend Sub New()
         End Sub
