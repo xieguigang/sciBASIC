eb2f5981ed7345cf5496ea7a412aa66406b595be
xieguigang <xie.guigang@gcmodeller.org>
2021-08-07 17:05:25 +0800

    \Communities of network graph

---
+++ gr/network-visualization/Datavisualization.Network/Analysis/Communities.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+
+Namespace Analysis
+
+    Public Class Communities
+
+        Public Property g As NetworkGraph
+        Public Property community As String()
+
+        Public Function modularity() As Double
+
+        End Function
+
+        Public Function membership() As String()
+
+        End Function
+    End Class
+End Namespace
