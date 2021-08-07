43da88425e2ca018c3dd28ca1ad021abc9943eef
xieguigang <xie.guigang@gcmodeller.org>
2021-08-07 20:22:52 +0800

    config test

--- gr/network-visualization/Datavisualization.Network/test/LouvainTest.vb
+++ gr/network-visualization/Datavisualization.Network/test/LouvainTest.vb
@@ -8,10 +8,11 @@ Module LouvainTest
 
     Sub Main()
 
-        Dim links = "E:\GCModeller\src\runtime\sciBASIC#\Data_science\algorithms\Louvain\facebook_combined.txt".ReadAllLines.Skip(1).Select(Function(str) Strings.Trim(str).StringSplit("\s+")).ToArray
+        Dim links As String()() = "E:\GCModeller\src\runtime\sciBASIC#\Data_science\algorithms\Louvain\facebook_combined.txt".ReadAllLines.Skip(1).Select(Function(str) Strings.Trim(str).StringSplit("\s+")).ToArray
         Dim g As New NetworkGraph
 
-        For Each line In links
+        ' build network via links
+        For Each line As String() In links
             If g.GetElementByID(line(0)) Is Nothing Then
                 g.CreateNode(line(0))
             End If
@@ -22,14 +23,21 @@ Module LouvainTest
             g.CreateEdge(g.GetElementByID(line(0)), g.GetElementByID(line(1)), 1)
         Next
 
-        Dim clusters = Communities.Analysis(g)
+        ' the original network with communities labeled
+        Dim clusters As NetworkGraph = Communities.Analysis(g)
 
         Call Console.WriteLine(Communities.Modularity(clusters))
         Call Console.WriteLine(Communities.Community(g).GetJson(indent:=True))
 
-        Call clusters.Tabular.Save("E:\GCModeller\src\runtime\sciBASIC#\Data_science\algorithms\Louvain\facebook_combined_graph")
+        Call clusters _
+            .Tabular _
+            .Save("E:\GCModeller\src\runtime\sciBASIC#\Data_science\algorithms\Louvain\facebook_combined_graph")
 
         Pause()
+    End Sub
+
+    Sub RunAnalysis(filepath As String)
+
     End Sub
 
 End Module
