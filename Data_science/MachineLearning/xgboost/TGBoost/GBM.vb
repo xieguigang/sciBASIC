cdbab09cada5e78f7d90a3b84e058bfbf70f01b4
xieguigang <xie.guigang@gcmodeller.org>
2021-09-04 23:55:54 +0800

    code style improvements

--- Data_science/MachineLearning/xgboost/TGBoost/GBM.vb
+++ Data_science/MachineLearning/xgboost/TGBoost/GBM.vb
@@ -59,7 +59,7 @@ Namespace train
         Public Overridable ReadOnly Property first_round_pred As Double
         Public Overridable ReadOnly Property eta As Double
         Public Overridable ReadOnly Property loss As Loss
-        Public Overridable ReadOnly Property trees As List(Of Tree)
+        Public Overridable ReadOnly Property trees As New List(Of Tree)
 
         Public Sub New()
         End Sub
@@ -146,68 +146,109 @@ Namespace train
             'Start learning
             GBM.logger.info("TGBoost start training")
 
-            For i = 0 To num_boost_round - 1
-                Dim tree As New Tree(min_sample_split, min_child_weight, max_depth, colsample, rowsample, lambda, gamma, num_thread, attribute_list.cat_features_cols)
-                tree.fit(attribute_list, class_list, row_sampler, col_sampler)
-                'when finish building this tree, update the class_list.pred, grad, hess
-                class_list.update_pred(_eta)
-                class_list.update_grad_hess(_loss, Me.scale_pos_weight)
+            For i As Integer = 0 To num_boost_round - 1
+                If Not doIteration(
+                    i,
+                    attribute_list,
+                    class_list,
+                    row_sampler,
+                    col_sampler,
+                    calculate_metric,
+                    do_validation,
+                    valset,
+                    val_pred,
+                    maximize,
+                    best_val_metric,
+                    best_round,
+                    become_worse_round,
+                    early_stopping_rounds
+                ) Then
 
-
-                'save this tree
-                _trees.Add(tree)
-                GBM.logger.log(MSG_TYPES.INF, String.Format("current tree has {0:D} nodes,including {1:D} nan tree nodes", tree.nodes_cnt, tree.nan_nodes_cnt))
-
-                'print training information
-                If eval_metric.Equals("") Then
-                    GBM.logger.log(MSG_TYPES.FINEST, String.Format("TGBoost round {0:D}", i))
-                Else
-                    Dim train_metric = calculate_metric(_loss.transform(class_list.pred), class_list.label)
-
-                    If Not do_validation Then
-                        GBM.logger.log(MSG_TYPES.INF, String.Format("TGBoost round {0:D},train-{1}:{2:F6}", i, eval_metric, train_metric))
-                    Else
-                        Dim cur_tree_pred As Double() = tree.predict(valset.origin_feature)
-
-                        For n = 0 To val_pred.Length - 1
-                            val_pred(n) += _eta * cur_tree_pred(n)
-                        Next
-
-                        Dim val_metric = calculate_metric(_loss.transform(val_pred), valset.label)
-                        GBM.logger.log(MSG_TYPES.INF, String.Format("TGBoost round {0:D},train-{1}:{2:F6},val-{3}:{4:F6}", i, eval_metric, train_metric, eval_metric, val_metric))
-                        'check whether to early stop
-                        If maximize Then
-                            If val_metric > best_val_metric Then
-                                best_val_metric = val_metric
-                                best_round = i
-                                become_worse_round = 0
-                            Else
-                                become_worse_round += 1
-                            End If
-
-                            If become_worse_round > early_stopping_rounds Then
-                                GBM.logger.log(MSG_TYPES.INF, String.Format("TGBoost training stop,best round is {0:D},best val-{1} is {2:F6}", i, eval_metric, best_val_metric))
-                                Exit For
-                            End If
-                        Else
-
-                            If val_metric < best_val_metric Then
-                                best_val_metric = val_metric
-                                best_round = i
-                                become_worse_round = 0
-                            Else
-                                become_worse_round += 1
-                            End If
-
-                            If become_worse_round > early_stopping_rounds Then
-                                GBM.logger.log(MSG_TYPES.INF, String.Format("TGBoost training stop,best round is {0:D},best val-{1} is {2:F6}", i, eval_metric, best_val_metric))
-                                Exit For
-                            End If
-                        End If
-                    End If
+                    Exit For
                 End If
             Next
         End Sub
+
+        Private Function doIteration(i As Integer,
+                                     ByRef attribute_list As AttributeList,
+                                     ByRef class_list As ClassList,
+                                     ByRef row_sampler As RowSampler,
+                                     ByRef col_sampler As ColumnSampler,
+                                     ByRef calculate_metric As IMetric,
+                                     ByRef do_validation As Boolean,
+                                     ByRef valset As ValidationData,
+                                     ByRef val_pred As Double(),
+                                     ByRef maximize As Boolean,
+                                     ByRef best_val_metric As Double,
+                                     ByRef best_round As Integer,
+                                     ByRef become_worse_round As Integer,
+                                     ByRef early_stopping_rounds As Integer) As Boolean
+
+            Dim tree As New Tree(min_sample_split, min_child_weight, max_depth, colsample, rowsample, lambda, gamma, num_thread, attribute_list.cat_features_cols)
+
+            tree.fit(attribute_list, class_list, row_sampler, col_sampler)
+            'when finish building this tree, update the class_list.pred, grad, hess
+            class_list.update_pred(_eta)
+            class_list.update_grad_hess(_loss, Me.scale_pos_weight)
+
+            'save this tree
+            trees.Add(tree)
+            GBM.logger.log(MSG_TYPES.INF, String.Format("current tree has {0:D} nodes,including {1:D} nan tree nodes", tree.nodes_cnt, tree.nan_nodes_cnt))
+
+            'print training information
+            If eval_metric = Metrics.none Then
+                GBM.logger.log(MSG_TYPES.FINEST, String.Format("TGBoost round {0:D}", i))
+                Return True
+            End If
+
+            Dim train_metric = calculate_metric(_loss.transform(class_list.pred), class_list.label)
+
+            If Not do_validation Then
+                GBM.logger.log(MSG_TYPES.INF, String.Format("TGBoost round {0:D},train-{1}:{2:F6}", i, eval_metric, train_metric))
+                Return True
+            End If
+
+            Dim cur_tree_pred As Double() = tree.predict(valset.origin_feature)
+
+            For n = 0 To val_pred.Length - 1
+                val_pred(n) += _eta * cur_tree_pred(n)
+            Next
+
+            Dim val_metric = calculate_metric(_loss.transform(val_pred), valset.label)
+
+            GBM.logger.log(MSG_TYPES.INF, String.Format("TGBoost round {0:D},train-{1}:{2:F6},val-{3}:{4:F6}", i, eval_metric, train_metric, eval_metric, val_metric))
+
+            'check whether to early stop
+            If maximize Then
+                If val_metric > best_val_metric Then
+                    best_val_metric = val_metric
+                    best_round = i
+                    become_worse_round = 0
+                Else
+                    become_worse_round += 1
+                End If
+
+                If become_worse_round > early_stopping_rounds Then
+                    GBM.logger.log(MSG_TYPES.INF, String.Format("TGBoost training stop,best round is {0:D},best val-{1} is {2:F6}", i, eval_metric, best_val_metric))
+                    Return False
+                End If
+            Else
+                If val_metric < best_val_metric Then
+                    best_val_metric = val_metric
+                    best_round = i
+                    become_worse_round = 0
+                Else
+                    become_worse_round += 1
+                End If
+
+                If become_worse_round > early_stopping_rounds Then
+                    GBM.logger.log(MSG_TYPES.INF, String.Format("TGBoost training stop,best round is {0:D},best val-{1} is {2:F6}", i, eval_metric, best_val_metric))
+                    Return False
+                End If
+            End If
+
+            Return True
+        End Function
 
         Public Overridable Function predict(features As Single()()) As Double()
             Dim pred = New Double(features.Length - 1) {}
