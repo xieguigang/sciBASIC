04c88096fee4d26aec9d809cc6183c1d4517d3b6
xieguigang <xie.guigang@gcmodeller.org>
2021-09-04 21:45:01 +0800

    how to convert result data set?

---
+++ Data_science/MachineLearning/XGBoostDataSet/Conversion.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.TagData
+Imports Microsoft.VisualBasic.MachineLearning.XGBoost.train
+
+Public Module Conversion
+
+    <Extension>
+    Public Function ToTrainingSet(matrix As DoubleTagged(Of Single())(), fieldNames As String()) As TrainData
+
+    End Function
+
+    <Extension>
+    Public Function ToValidateSet(matrix As DoubleTagged(Of Single())(), fieldNames As String()) As ValidationData
+
+    End Function
+
+    <Extension>
+    Public Function ToTestDataSet(matrix As Single()()) As TestData
+
+    End Function
+End Module
