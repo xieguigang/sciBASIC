a8de773494830fc2106b4b5cdf8c2263e9588350
关倩倩 <amethyst.asuka@gcmodeller.org>
2021-09-05 14:22:06 +0800

    test lpp model

--- Data_science/Mathematica/data/LP/LP_tool/LPPExamples.vb
+++ Data_science/Mathematica/data/LP/LP_tool/LPPExamples.vb
@@ -1,45 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::047a439a96aca3eceaec2eac71f03df2, Data_science\Mathematica\data\LP\LP_tool\LPPExamples.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class LPPExamples
-    ' 
-    '     Function: maximizeExample, minimizeExample, smallMinimizeExample, strictEquality, transshipment
-    ' 
-    ' /********************************************************************************/
+' Class LPPExamples
+' 
+'     Function: maximizeExample, minimizeExample, smallMinimizeExample, strictEquality, transshipment
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports Microsoft.VisualBasic.Math.Algebra.LinearProgramming
+Imports Microsoft.VisualBasic.Math.LinearAlgebra.LinearProgramming
 
 Public Class LPPExamples
 
---
+++ Data_science/Mathematica/data/LP/LP_tool/Module2.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.VisualBasic.Math.LinearAlgebra.LinearProgramming
+
+Module Module2
+
+    Sub Main()
+
+        Dim model = "E:\repo\xDoc\Yilia\runtime\sciBASIC#\Data_science\Mathematica\data\LP\map00220_lpp.XML".LoadXml(Of LPPModel)
+
+        Pause()
+
+    End Sub
+
+End Module
