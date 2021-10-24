ffea6b90bfa931b38afe073463527c2a582cfe25
xieguigang <xie.guigang@gcmodeller.org>
2021-10-24 19:15:14 +0800

    Update Base64Codec.vb

--- Microsoft.VisualBasic.Core/src/Net/HTTP/Stream/Base64Codec.vb
+++ Microsoft.VisualBasic.Core/src/Net/HTTP/Stream/Base64Codec.vb
@@ -1,43 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::f7d1301b41b5c674e1d858db0d6cb10f, Microsoft.VisualBasic.Core\src\Net\HTTP\Stream\Base64Codec.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module Base64Codec
-    ' 
-    '         Function: __getImageFromBase64, __toBase64String, Base64RawBytes, Base64String, DecodeBase64
-    '                   GetImage, (+3 Overloads) ToBase64String, (+2 Overloads) ToStream
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module Base64Codec
+' 
+'         Function: __getImageFromBase64, __toBase64String, Base64RawBytes, Base64String, DecodeBase64
+'                   GetImage, (+3 Overloads) ToBase64String, (+2 Overloads) ToStream
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -90,6 +90,12 @@ Namespace Net.Http
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         <Extension>
         Public Function ToBase64String(byts As IEnumerable(Of Byte)) As String
+            Return Convert.ToBase64String(byts.ToArray)
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        <Extension>
+        Public Function ToBase64String(byts As MemoryStream) As String
             Return Convert.ToBase64String(byts.ToArray)
         End Function
 #End Region
