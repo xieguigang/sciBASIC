e1322bf6bc0210fd1a5e608064c2d321dd8a3e43
xieguigang <xie.guigang@gcmodeller.org>
2021-11-02 19:25:03 +0800

    Cumulative Distribution Function via solve ODE

--- Data_science/Mathematica/Math/ODE/Extensions.vb
+++ Data_science/Mathematica/Math/ODE/Extensions.vb
@@ -1,53 +1,85 @@
 ﻿#Region "Microsoft.VisualBasic::e59387981fdef0ea010741b4f63afd65, Data_science\Mathematica\Math\ODE\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Extensions
-    ' 
-    '     Function: Let, Solve
-    ' 
-    ' /********************************************************************************/
+' Module Extensions
+' 
+'     Function: Let, Solve
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Linq.Expressions
 Imports System.Reflection
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 Imports Microsoft.VisualBasic.Math.Calculus.Dynamics
 Imports Microsoft.VisualBasic.Math.Calculus.Dynamics.Data
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
 Imports Microsoft.VisualBasic.Serialization.JSON
 
-<HideModuleName> Public Module Extensions
+<HideModuleName>
+Public Module Extensions
+
+    ''' <summary>
+    ''' Cumulative Distribution Function via solve ODE
+    ''' </summary>
+    ''' <param name="p"></param>
+    ''' <param name="range"></param>
+    ''' <param name="resolution"></param>
+    ''' <param name="p0"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function CDF(p As Func(Of Double, Double),
+                        range As DoubleRange,
+                        Optional resolution As Integer = 50000,
+                        Optional p0 As Double = 0,
+                        Optional ByRef x As Double() = Nothing,
+                        Optional ByRef y As Double() = Nothing) As Double
+
+        Dim ode As New ODE With {
+            .ID = p.ToString,
+            .y0 = p0,
+            .df = Function(xi, yi) p(xi)
+        }
+        Dim solve = ode.RK4(resolution, range.Min, range.Max)
+        Dim sum As Double = solve.Y.vector.Last
+
+        x = solve.X.ToArray
+        y = solve.Y.vector
+
+        Return sum - p0
+    End Function
 
     ''' <summary>
     ''' Solve the target ODEs dynamics system by using the RK4 solver.
