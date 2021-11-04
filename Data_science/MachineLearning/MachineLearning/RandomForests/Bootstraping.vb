0ebb7bda57cbbdbc3be9024f04130a0f8c4c6ebc
xieguigang <xie.guigang@gcmodeller.org>
2021-11-04 22:10:35 +0800

    gini for information

--- Data_science/MachineLearning/MachineLearning/RandomForests/Bootstraping.vb
+++ Data_science/MachineLearning/MachineLearning/RandomForests/Bootstraping.vb
@@ -89,20 +89,5 @@ Namespace RandomForests
                 }
             Next
         End Function
-
-        ''' <summary>
-        ''' 基尼系数的选择的标准就是每个子节点达到最高的纯度，即落在子节点中的所有观察都属于同一个分类，
-        ''' 此时基尼系数最小，纯度最高，不确定度最小。
-        ''' 
-        ''' 基尼指数越大，说明不确定性就越大；基尼系数越小，不确定性越小，数据分割越彻底，越干净。
-        ''' </summary>
-        ''' <param name="p"></param>
-        ''' <returns></returns>
-        ''' 
-        <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        <Extension>
-        Public Function Gini(p As IEnumerable(Of Double)) As Double
-            Return 1 - (Aggregate pk As Double In p Into Sum(pk ^ 2))
-        End Function
     End Module
 End Namespace
