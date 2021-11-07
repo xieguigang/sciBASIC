55b2b8a50cd38f6770cb61322bb3915d4015a8f3
xieguigang <xie.guigang@gcmodeller.org>
2021-11-07 12:49:55 +0800

    Update Paragraph.vb

--- Data/Trinity/Model/Paragraph.vb
+++ Data/Trinity/Model/Paragraph.vb
@@ -1,4 +1,6 @@
-﻿Namespace Model
+﻿Imports Microsoft.VisualBasic.Linq
+
+Namespace Model
 
     Public Class Paragraph
 
@@ -9,6 +11,12 @@
         End Function
 
         Public Shared Iterator Function Segmentation(text As String) As IEnumerable(Of Paragraph)
+            For Each block As String() In text.LineTokens.Split(Function(str) str.Trim = "")
+                Yield block.JoinBy(" ").DoCall(AddressOf GetParagraph)
+            Next
+        End Function
+
+        Private Shared Function GetParagraph(text As String) As Paragraph
 
         End Function
 
