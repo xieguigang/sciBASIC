dcd5edf0503475c86ae9c43812209bbec2e5b2bc
xieguigang <xie.guigang@gcmodeller.org>
2021-11-07 13:12:05 +0800

    Update Paragraph.vb

--- Data/Trinity/Model/Paragraph.vb
+++ Data/Trinity/Model/Paragraph.vb
@@ -12,11 +12,20 @@ Namespace Model
 
         Public Shared Iterator Function Segmentation(text As String) As IEnumerable(Of Paragraph)
             For Each block As String() In text.LineTokens.Split(Function(str) str.Trim = "")
-                Yield block.JoinBy(" ").DoCall(AddressOf GetParagraph)
+                If block.All(Function(str) str.Trim.StringEmpty) Then
+                    Continue For
+                End If
+
+                Yield block _
+                    .Select(AddressOf Strings.Trim) _
+                    .JoinBy(" ") _
+                    .Trim _
+                    .DoCall(AddressOf GetParagraph)
             Next
         End Function
 
         Private Shared Function GetParagraph(text As String) As Paragraph
+            Dim sentence As String() = text.Split("."c)
 
         End Function
 
