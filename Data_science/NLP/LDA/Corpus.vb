c6a6973459adb3b091b3057741c66b3c03386c58
xieguigang <xie.guigang@gcmodeller.org>
2021-11-21 00:25:12 +0800

    document data I/O

--- Data_science/NLP/LDA/Corpus.vb
+++ Data_science/NLP/LDA/Corpus.vb
@@ -1,7 +1,6 @@
 ﻿Imports System.IO
 Imports System.Text
 Imports Microsoft.VisualBasic.Language
-Imports Microsoft.VisualBasic.Language.Values
 
 Namespace LDA
 
@@ -15,6 +14,24 @@ Namespace LDA
 
         ReadOnly documentList As IList(Of Integer())
         ReadOnly vocabularyField As Vocabulary
+
+        Public Overridable ReadOnly Property VocabularySize As Integer
+            Get
+                Return vocabularyField.size()
+            End Get
+        End Property
+
+        Public Overridable ReadOnly Property Vocabulary As Vocabulary
+            Get
+                Return vocabularyField
+            End Get
+        End Property
+
+        Public Overridable ReadOnly Property Document As Integer()()
+            Get
+                Return toArray()
+            End Get
+        End Property
 
         Public Sub New()
             documentList = New List(Of Integer())()
@@ -38,12 +55,6 @@ Namespace LDA
             Return documentList.ToArray()
         End Function
 
-        Public Overridable ReadOnly Property VocabularySize As Integer
-            Get
-                Return vocabularyField.size()
-            End Get
-        End Property
-
         Public Overrides Function ToString() As String
             Dim sb As New StringBuilder()
 
@@ -63,81 +74,11 @@ Namespace LDA
         ''' <returns> a corpus </returns>
         ''' <exception cref="IOException"> </exception>
         Public Shared Function load(folderPath As String) As Corpus
-            Dim corpus As Corpus = New Corpus()
-
-            For Each filepath In folderPath.ListFiles()
-                Dim file = filepath.Open(doClear:=False, [readOnly]:=True)
-                Dim br As StreamReader = New StreamReader(file, Encoding.UTF8)
-                Dim line As New Value(Of String)
-                Dim wordList As New List(Of String)()
-
-                While Not line = br.ReadLine() Is Nothing
-                    Dim words = line.Split(" ")
-
-                    For Each word In words
-
-                        If word.Trim().Length < 2 Then
-                            Continue For
-                        End If
-
-                        wordList.Add(word)
-                    Next
-                End While
-
-                br.Close()
-                corpus.addDocument(wordList)
-            Next
-
-            If corpus.VocabularySize = 0 Then
-                Return Nothing
-            End If
-
-            Return corpus
+            Return DocumentLoader.load(folderPath)
         End Function
 
-        Public Overridable ReadOnly Property Vocabulary As Vocabulary
-            Get
-                Return vocabularyField
-            End Get
-        End Property
-
-        Public Overridable ReadOnly Property Document As Integer()()
-            Get
-                Return toArray()
-            End Get
-        End Property
-
         Public Shared Function loadDocument(path As String, vocabulary As Vocabulary) As Integer()
-            Dim br As StreamReader = New StreamReader(path)
-            Dim line As New Value(Of String)
-            Dim wordList As New List(Of Integer)()
-
-            While Not line = br.ReadLine() Is Nothing
-                Dim words = line.Split(" ")
-
-                For Each word In words
-
-                    If word.Trim().Length < 2 Then
-                        Continue For
-                    End If
-
-                    Dim id = vocabulary.getId(word)
-
-                    If id IsNot Nothing Then
-                        wordList.Add(id)
-                    End If
-                Next
-            End While
-
-            br.Close()
-            Dim result = New Integer(wordList.Count - 1) {}
-            Dim i = 0
-
-            For Each [integer] As Integer In wordList
-                result(System.Math.Min(Threading.Interlocked.Increment(i), i - 1)) = [integer]
-            Next
-
-            Return result
+            Return DocumentLoader.loadDocument(path, vocabulary)
         End Function
     End Class
 End Namespace
---
+++ Data_science/NLP/LDA/DocumentLoader.vb
@@ -0,0 +1,81 @@
+﻿Imports System.IO
+Imports System.Text
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Language.Values
+
+Namespace LDA
+
+    Module DocumentLoader
+
+        ''' <summary>
+        ''' Load documents from disk
+        ''' </summary>
+        ''' <param name="folderPath"> is a folder, which contains text documents. </param>
+        ''' <returns> a corpus </returns>
+        ''' <exception cref="IOException"> </exception>
+        Public Function load(folderPath As String) As Corpus
+            Dim corpus As Corpus = New Corpus()
+
+            For Each filepath As String In folderPath.ListFiles()
+                Dim file = filepath.Open(doClear:=False, [readOnly]:=True)
+                Dim br As New StreamReader(file, Encoding.UTF8)
+                Dim line As New Value(Of String)
+                Dim wordList As New List(Of String)()
+
+                While Not line = br.ReadLine() Is Nothing
+                    Dim words = line.Split(" ")
+
+                    For Each word As String In words
+                        If word.Trim().Length < 2 Then
+                            Continue For
+                        End If
+
+                        wordList.Add(word)
+                    Next
+                End While
+
+                br.Close()
+                corpus.addDocument(wordList)
+            Next
+
+            If corpus.VocabularySize = 0 Then
+                Return Nothing
+            End If
+
+            Return corpus
+        End Function
+
+        Public Function loadDocument(path As String, vocabulary As Vocabulary) As Integer()
+            Dim br As New StreamReader(path)
+            Dim line As New Value(Of String)
+            Dim wordList As New List(Of Integer)()
+
+            While Not line = br.ReadLine() Is Nothing
+                Dim words = line.Split(" ")
+
+                For Each word As String In words
+                    If word.Trim().Length < 2 Then
+                        Continue For
+                    End If
+
+                    Dim id = vocabulary.getId(word)
+
+                    If id IsNot Nothing Then
+                        wordList.Add(id)
+                    End If
+                Next
+            End While
+
+            Call br.Close()
+
+            Dim result = New Integer(wordList.Count - 1) {}
+            Dim i As i32 = 0
+
+            For Each [integer] As Integer In wordList
+                result(++i) = [integer]
+            Next
+
+            Return result
+        End Function
+    End Module
+End Namespace
