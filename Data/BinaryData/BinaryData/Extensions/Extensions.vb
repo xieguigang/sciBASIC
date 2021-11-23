2496cca30926e764f27939d4336585c07ba0ff6c
xieguigang <xie.guigang@gcmodeller.org>
2021-11-23 21:09:08 +0800

    Update Extensions.vb

--- Data/BinaryData/BinaryData/Extensions/Extensions.vb
+++ Data/BinaryData/BinaryData/Extensions/Extensions.vb
@@ -132,21 +132,12 @@ End Interface
     ''' </summary>
     ''' <param name="path"></param>
     ''' <param name="encoding"></param>
-    ''' <param name="buffered%">
-    ''' 默认的缓存临界大小是10MB，当超过这个大小的时候则不会进行缓存，假若没有操作这个临界值，则程序会一次性读取所有数据到内存之中以提高IO性能
-    ''' </param>
     ''' <returns></returns>
+    ''' 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     <Extension>
-    Public Function OpenBinaryReader(path$, Optional encoding As Encodings = Encodings.ASCII, Optional buffered& = 1024 * 1024 * 10) As BinaryDataReader
-        If FileIO.FileSystem.GetFileInfo(path).Length <= buffered Then
-            ' 文件数据将会被缓存
-            Dim byts As Byte() = FileIO.FileSystem.ReadAllBytes(path)
-            Dim ms As New MemoryStream(byts)
-
-            Return New BinaryDataReader(ms, encoding)
-        Else
-            Return New BinaryDataReader(File.OpenRead(path), encoding)
-        End If
+    Public Function OpenBinaryReader(path$, Optional encoding As Encodings = Encodings.ASCII) As BinaryDataReader
+        Return New BinaryDataReader(path.Open(FileMode.Open, doClear:=False, [readOnly]:=True), encoding)
     End Function
 
     ''' <summary>
