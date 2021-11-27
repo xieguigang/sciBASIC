37c92ceb6a5d38904375415e90e757f36d2d7bc6
xieguigang <xie.guigang@gcmodeller.org>
2021-11-27 16:46:28 +0800

    how to write model file?

---
+++ Data_science/MachineLearning/MachineLearning/CNN/SaveModel.vb
@@ -0,0 +1,13 @@
+﻿Imports System.IO
+Imports System.Runtime.CompilerServices
+
+Namespace Convolutional
+
+    Public Module SaveModel
+
+        <Extension>
+        Public Function Save(model As CeNiN, file As Stream) As Boolean
+
+        End Function
+    End Module
+End Namespace
