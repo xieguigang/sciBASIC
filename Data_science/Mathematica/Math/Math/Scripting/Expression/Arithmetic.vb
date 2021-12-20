5410a63580438eb5182bd6e936f80d5f813a4337
xieguigang <xie.guigang@gcmodeller.org>
2021-12-20 22:10:28 +0800

    improvements of the math expression engine

--- Data_science/Mathematica/Math/Math/Scripting/Expression/Arithmetic.vb
+++ Data_science/Mathematica/Math/Math/Scripting/Expression/Arithmetic.vb
@@ -1,44 +1,44 @@
 ﻿#Region "Microsoft.VisualBasic::bf2346c3b38a43659be692ec265f04df, Data_science\Mathematica\Math\Math\Scripting\Expression\Arithmetic.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module Arithmetic
-    ' 
-    '         Properties: Arithmetic
-    ' 
-    '         Function: Evaluate, Factorial, RND
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module Arithmetic
+' 
+'         Properties: Arithmetic
+' 
+'         Function: Evaluate, Factorial, RND
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -62,10 +62,10 @@ Namespace Scripting.MathExpression
  _
             Function(a, b) a + b,
             Function(a, b) a - b,
-            Function(a, b) a * b,
-            Function(a, b) a / b,
-            Function(a, b) a \ b,
-            Function(a, b) a Mod b,
+            Function(a, b) If(a = 0.0 OrElse b = 0.0, 0.0, a * b),
+            Function(a, b) If(a = 0.0, 0.0, a / b),
+            Function(a, b) If(a = 0.0, 0.0, a \ b),
+            Function(a, b) If(a = 0.0, 0.0, a Mod b),
             AddressOf stdNum.Pow,
             AddressOf Factorial
         }
