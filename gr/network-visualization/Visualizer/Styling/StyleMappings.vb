bdba3bd96a2441872e49a2f33b9e74b3f7a90117
xieguigang <xie.guigang@gcmodeller.org>
2022-01-02 22:53:46 +0800

    Update StyleMappings.vb

--- gr/network-visualization/Visualizer/Styling/StyleMappings.vb
+++ gr/network-visualization/Visualizer/Styling/StyleMappings.vb
@@ -80,7 +80,9 @@ Namespace Styling
         End Function
 
         <Extension>
-        Public Function NumericMapping(Of T As IDynamicsTable)(source As IEnumerable(Of T), key$, range As DoubleRange) As Map(Of T, Double)()
+        Public Function NumericMapping(Of T As IDynamicsTable)(source As IEnumerable(Of T),
+                                                               key$,
+                                                               range As DoubleRange) As Map(Of T, Double)()
             Dim properties = GetProperty(Of T)()
             Dim array As T() = source.ToArray
             Dim flag As T = array(Scan0)
@@ -166,7 +168,11 @@ Namespace Styling
         ''' <param name="level%"></param>
         ''' <returns></returns>
         <Extension>
-        Public Function ColorMapping(Of T As IDynamicsTable)(source As IEnumerable(Of T), key$, colorSchema$, Optional level% = 100) As Map(Of T, Color)()
+        Public Function ColorMapping(Of T As IDynamicsTable)(source As IEnumerable(Of T),
+                                                             key$,
+                                                             colorSchema$,
+                                                             Optional level% = 100) As Map(Of T, Color)()
+
             Dim levels As Map(Of T, Double)() = source.NumericMapping(key, {0, 1})
             Dim out As New List(Of Map(Of T, Color))
             Dim colors As Color() = Designer.GetColors(colorSchema, level)
