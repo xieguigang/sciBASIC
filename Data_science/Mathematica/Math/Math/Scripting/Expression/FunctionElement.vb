b20f5de65b05648684335e17e2cf85543662b0be
xieguigang <xie.guigang@gcmodeller.org>
2022-02-03 01:42:58 +0800

    improvements of the xml format layout

--- Data_science/Mathematica/Math/Math/Scripting/Expression/FunctionElement.vb
+++ Data_science/Mathematica/Math/Math/Scripting/Expression/FunctionElement.vb
@@ -1,46 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::b4c672f7a224b41eafd26e0aff1db998, Data_science\Mathematica\Math\Math\Scripting\Expression\FunctionElement.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class FunctionElement
-    ' 
-    '         Properties: lambda, name, parameters
-    ' 
-    '         Function: ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class FunctionElement
+' 
+'         Properties: lambda, name, parameters
+' 
+'         Function: ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
+
+Imports System.Xml.Serialization
 
 Namespace Scripting.MathExpression
 
@@ -49,8 +51,11 @@ Namespace Scripting.MathExpression
     ''' </summary>
     Public Class FunctionElement
 
+        <XmlAttribute>
         Public Property name As String
+        <XmlAttribute>
         Public Property parameters As String()
+        <XmlText>
         Public Property lambda As String
 
         Public Overrides Function ToString() As String
