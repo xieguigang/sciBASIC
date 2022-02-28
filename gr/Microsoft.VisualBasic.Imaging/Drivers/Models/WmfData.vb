7275112085bfd52ee0d665dd37265f41fd3ec188
xieguigang <xie.guigang@gcmodeller.org>
2022-02-28 23:22:47 +0800

    Update WmfData.vb

--- gr/Microsoft.VisualBasic.Imaging/Drivers/Models/WmfData.vb
+++ gr/Microsoft.VisualBasic.Imaging/Drivers/Models/WmfData.vb
@@ -1,57 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::fa83d214381edf125e10e2958e3df58b, gr\Microsoft.VisualBasic.Imaging\Drivers\Models\WmfData.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class WmfData
-    ' 
-    '         Properties: Driver
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: GetDataURI, (+2 Overloads) Save, wmfTmp
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class WmfData
+' 
+'         Properties: Driver
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: GetDataURI, (+2 Overloads) Save, wmfTmp
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Drawing
+Imports System.Drawing.Imaging
 Imports System.IO
 Imports Microsoft.VisualBasic.ApplicationServices
+Imports Microsoft.VisualBasic.Imaging.BitmapImage
 Imports Microsoft.VisualBasic.MIME.Html.CSS
 Imports Microsoft.VisualBasic.Net.Http
 
 Namespace Driver
 
     Public Class WmfData : Inherits GraphicsData
+        Implements SaveGdiBitmap
 
         Public Overrides ReadOnly Property Driver As Drivers
             Get
@@ -100,6 +103,10 @@ Namespace Driver
 
         Friend Shared Function wmfTmp() As String
             Return TempFileSystem.GetAppSysTempFile(".wmf", App.PID, RandomASCIIString(10, skipSymbols:=True))
+        End Function
+
+        Public Overloads Function Save(stream As Stream, format As ImageFormat) As Boolean Implements SaveGdiBitmap.Save
+            Return Save(stream)
         End Function
     End Class
 End Namespace
