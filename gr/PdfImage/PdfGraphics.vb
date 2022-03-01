c8c13e35c7f6ea279a373f39edc86f2fa079adbd
xieguigang <xie.guigang@gcmodeller.org>
2022-03-01 21:46:47 +0800

    draw pdf graphics elements

--- gr/PdfImage/PdfGraphics.vb
+++ gr/PdfImage/PdfGraphics.vb
@@ -406,15 +406,15 @@ Public Class PdfGraphics : Inherits MockGDIPlusGraphics
     End Sub
 
     Public Overrides Sub DrawLine(pen As Pen, pt1 As PointF, pt2 As PointF)
-        Throw New NotImplementedException()
+        Call DrawLine(pen, pt1.X, pt1.Y, pt2.X, pt2.Y)
     End Sub
 
     Public Overrides Sub DrawLine(pen As Pen, pt1 As Point, pt2 As Point)
-        Throw New NotImplementedException()
+        Call DrawLine(pen, pt1.X, pt1.Y, pt2.X, pt2.Y)
     End Sub
 
     Public Overrides Sub DrawLine(pen As Pen, x1 As Integer, y1 As Integer, x2 As Integer, y2 As Integer)
-        Throw New NotImplementedException()
+        Call DrawLine(pen, CSng(x1), CSng(y1), CSng(x2), CSng(y2))
     End Sub
 
     Public Overrides Sub DrawLine(pen As Pen, x1 As Single, y1 As Single, x2 As Single, y2 As Single)
@@ -691,11 +691,15 @@ Public Class PdfGraphics : Inherits MockGDIPlusGraphics
     End Sub
 
     Public Overrides Sub FillEllipse(brush As Brush, rect As Rectangle)
-        Throw New NotImplementedException()
+        Call FillEllipse(brush, New RectangleF(rect.X, rect.Y, rect.Width, rect.Height))
     End Sub
 
     Public Overrides Sub FillEllipse(brush As Brush, rect As RectangleF)
-        Throw New NotImplementedException()
+        Dim x As Double = rect.X + rect.Width / 2
+        Dim y As Double = rect.Y - rect.Height / 2
+
+        g.SetColorNonStroking(DirectCast(brush, SolidBrush).Color)
+        g.DrawOval(x, y, rect.Width, rect.Height, PaintOp.Fill)
     End Sub
 
     Public Overrides Sub FillEllipse(brush As Brush, x As Single, y As Single, width As Single, height As Single)
@@ -711,7 +715,11 @@ Public Class PdfGraphics : Inherits MockGDIPlusGraphics
     End Sub
 
     Public Overrides Sub FillPie(brush As Brush, rect As Rectangle, startAngle As Single, sweepAngle As Single)
-        Throw New NotImplementedException()
+        Dim x As Double = rect.X + rect.Width / 2
+        Dim y As Double = rect.Y - rect.Height / 2
+
+        g.SetColorNonStroking(DirectCast(brush, SolidBrush).Color)
+        g.DrawOval(x, y, rect.Width, rect.Height, PaintOp.Fill)
     End Sub
 
     Public Overrides Sub FillPie(brush As Brush, x As Integer, y As Integer, width As Integer, height As Integer, startAngle As Integer, sweepAngle As Integer)
