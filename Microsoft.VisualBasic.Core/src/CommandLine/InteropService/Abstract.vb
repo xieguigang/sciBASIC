bbb8c4b6ca5bab08f7833072ae0a756668f90150
xieguigang <xie.guigang@gcmodeller.org>
2022-03-09 22:29:27 +0800

    create slave task

--- Microsoft.VisualBasic.Core/src/CommandLine/InteropService/Abstract.vb
+++ Microsoft.VisualBasic.Core/src/CommandLine/InteropService/Abstract.vb
@@ -1,59 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::e7ab0853d5c13c177cac4cf7c81a9ace, Microsoft.VisualBasic.Core\src\CommandLine\InteropService\Abstract.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class InteropService
-    ' 
-    '         Properties: IORedirect, IsAvailable, Path
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Function: GetLastCLRException, GetLastError, RunDotNetApp, RunProgram, ToString
-    ' 
-    '     Interface AppDriver
-    ' 
-    '         Properties: App
-    ' 
-    '     Class CLIBuilder
-    ' 
-    '         Function: ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class InteropService
+' 
+'         Properties: IORedirect, IsAvailable, Path
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Function: GetLastCLRException, GetLastError, RunDotNetApp, RunProgram, ToString
+' 
+'     Interface AppDriver
+' 
+'         Properties: App
+' 
+'     Class CLIBuilder
+' 
+'         Function: ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
+Imports Microsoft.VisualBasic.CommandLine.InteropService.Pipeline
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Text
 Imports Microsoft.VisualBasic.Text.Parser
@@ -161,6 +162,10 @@ Namespace CommandLine.InteropService
             Dim tracess = logs(-1).Where(Function(s) Not s.StringEmpty).Select(Function(s) Mid(s, 6).Trim).ToArray
 
             Return ExceptionData.CreateInstance(message, tracess, typeINF)
+        End Function
+
+        Public Function CreateSlave(args As String) As RunSlavePipeline
+            Return New RunSlavePipeline(_executableAssembly, args)
         End Function
 
         ''' <summary>
