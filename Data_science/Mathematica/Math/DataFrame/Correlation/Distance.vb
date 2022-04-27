1a5bcac92a200de908bec83c0d95e29f064a1e2a
xieguigang <xie.guigang@gcmodeller.org>
2022-04-27 15:58:10 +0800

    Update Distance.vb

--- Data_science/Mathematica/Math/DataFrame/Correlation/Distance.vb
+++ Data_science/Mathematica/Math/DataFrame/Correlation/Distance.vb
@@ -1,51 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::082178c76b74da8d7fd6c81c49ac2e28, sciBASIC#\Data_science\Mathematica\Math\DataFrame\Correlation\Distance.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 48
-    '    Code Lines: 29
-    ' Comment Lines: 13
-    '   Blank Lines: 6
-    '     File Size: 1.96 KB
+' Summaries:
 
 
-    ' Module Distance
-    ' 
-    '     Function: Correlation, Euclidean, Similarity
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 48
+'    Code Lines: 29
+' Comment Lines: 13
+'   Blank Lines: 6
+'     File Size: 1.96 KB
+
+
+' Module Distance
+' 
+'     Function: Correlation, Euclidean, Similarity
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -54,6 +54,7 @@ Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Math.Correlations
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.Math.Statistics.Hypothesis
 
 Public Module Distance
 
@@ -79,7 +80,7 @@ Public Module Distance
                       Return (Correlations.Spearman(x, y), 0)
                   End Function
         Else
-            cor = AddressOf Builder.corTuple
+            cor = AddressOf Mantel.Pearson
         End If
 
         Return data.MatrixBuilder(cor, type:=DataType.Correlation)
