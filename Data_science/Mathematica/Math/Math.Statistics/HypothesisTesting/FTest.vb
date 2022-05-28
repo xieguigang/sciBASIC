71a405a727f0d0e42871f80a13a0164476770112
xieguigang <xie.guigang@gcmodeller.org>
2022-05-28 14:22:01 +0800

    f-test

--- Data_science/Mathematica/Math/Math.Statistics/HypothesisTesting/FTest.vb
+++ Data_science/Mathematica/Math/Math.Statistics/HypothesisTesting/FTest.vb
@@ -29,18 +29,31 @@ Public Class FTest
         End Get
     End Property
 
+    Public ReadOnly Property F As Double
+        Get
+            Dim q As Double = XVariance ^ 2
+            Dim p As Double = YVariance ^ 2
+
+            Return q / p
+        End Get
+    End Property
+
+    Public ReadOnly Property PValue As Double
+        Get
+            Dim f As Double = Me.F
+            Dim cumulative As Double = Distribution.FDistribution(f, XdegreeOfFreedom, YdegreeOfFreedom)
+
+            Return (1 - cumulative) * 2
+        End Get
+    End Property
+
     Sub New(x As Double(), y As Double())
         Me.x = x
         Me.y = y
     End Sub
 
-    Public Function PValue() As Double
-        Dim q As Double = XVariance ^ 2
-        Dim p As Double = YVariance ^ 2
-        Dim f As Double = q / p
-        Dim cumulative As Double = Distribution.FDistribution(f, XdegreeOfFreedom, YdegreeOfFreedom)
-
-        Return (1 - cumulative) * 2
+    Public Overrides Function ToString() As String
+        Return $"F-statistics: {F.ToString("G4")}, p-value: {PValue.ToString("G3")}"
     End Function
 
 End Class
