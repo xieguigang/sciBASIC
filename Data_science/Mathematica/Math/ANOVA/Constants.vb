a4ed2718fdf8027cad262250340ef6d81974e410
xieguigang <xie.guigang@gcmodeller.org>
2022-05-28 15:47:30 +0800

    ad anova code

---
+++ Data_science/Mathematica/Math/ANOVA/Constants.vb
@@ -0,0 +1,7 @@
+﻿Namespace stats
+    Public Class Constants
+        Public Shared P_FIVE_PERCENT As String = "p<.05"
+        Public Shared P_ONE_PERCENT As String = "p<.01"
+    End Class
+
+End Namespace
---
+++ Data_science/Mathematica/Math/ANOVA/Group.vb
@@ -0,0 +1,13 @@
+﻿Namespace stats
+    Public Class Group
+
+        Public ary As Double()
+        Public mean As Double
+        Public raisedSum As Double
+
+        Public Sub New(ByVal ary As Double())
+            Me.ary = ary
+        End Sub
+    End Class
+
+End Namespace
