cc972f005147e5732d6299ec9e301f4f5f9d0676
guigang xie <gg.xie@bionovogene.com>
2022-06-27 12:46:52 +0800

    add object models

---
+++ Data/BinaryData/HDSPack/StreamGroup.vb
@@ -0,0 +1,3 @@
+﻿Public Class StreamGroup
+
+End Class
--- Data/BinaryData/HDSPack/StreamPack.vb
+++ Data/BinaryData/HDSPack/StreamPack.vb
@@ -1,7 +1,15 @@
 
+Imports System.IO
+
 ''' <summary>
 ''' Hierarchical Data Stream Pack, A hdf5 liked file format
 ''' </summary>
 Public Class StreamPack
+
+    ReadOnly superBlock As StreamGroup
+
+    Public Function OpenBlock(fileName As String) As Stream
+
+    End Function
 
 End Class
