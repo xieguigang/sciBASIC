fc60d89ea8e62d5d3e327f35ba97e0cd2fcdfd0e
guigang xie <gg.xie@bionovogene.com>
2022-06-27 17:28:33 +0800

    write tree data

--- Data/BinaryData/HDSPack/BinaryStream/TreeWriter.vb
+++ Data/BinaryData/HDSPack/BinaryStream/TreeWriter.vb
@@ -1,11 +1,41 @@
 ﻿Imports System.IO
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.IO
+Imports Microsoft.VisualBasic.Text
 
 Friend Module TreeWriter
 
     <Extension>
     Public Function GetBuffer(root As StreamGroup) As Byte()
-        Using ms As New MemoryStream
+        Using ms As New MemoryStream, bin As New BinaryDataWriter(ms, encoding:=Encodings.UTF8WithoutBOM)
+            Dim buf As Byte()
+
+            Call bin.Write(root.referencePath.ToString, BinaryStringFormat.ZeroTerminated)
+
+            For Each file As StreamObject In root.files
+                If TypeOf file Is StreamGroup Then
+                    buf = DirectCast(file, StreamGroup).GetBuffer
+                Else
+                    buf = DirectCast(file, StreamBlock).GetBuffer
+                End If
+
+                bin.Write(buf.Length)
+                bin.Write(buf)
+                bin.Flush()
+            Next
+
+            Return ms.ToArray
+        End Using
+    End Function
+
+    <Extension>
+    Private Function GetBuffer(file As StreamBlock) As Byte()
+        Using ms As New MemoryStream, bin As New BinaryDataWriter(ms, encoding:=Encodings.UTF8WithoutBOM)
+            Call bin.Write(file.referencePath.ToString, BinaryStringFormat.ZeroTerminated)
+            Call bin.Write(file.offset)
+            Call bin.Write(file.size)
+            Call bin.Flush()
+
             Return ms.ToArray
         End Using
     End Function
