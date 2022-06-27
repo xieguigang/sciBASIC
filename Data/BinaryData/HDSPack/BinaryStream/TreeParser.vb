9736d70a159f2cae87a18a1e8e7e6643b1fc30f4
guigang xie <gg.xie@bionovogene.com>
2022-06-27 16:03:36 +0800

    move to filesystem folder

---
+++ Data/BinaryData/HDSPack/BinaryStream/TreeParser.vb
@@ -0,0 +1,8 @@
+﻿Imports System.IO
+
+Friend Module TreeParser
+
+    Public Function Parse(buffer As Stream) As StreamGroup
+
+    End Function
+End Module
---
+++ Data/BinaryData/HDSPack/BinaryStream/TreeWriter.vb
@@ -0,0 +1,11 @@
+﻿Imports System.IO
+
+Friend Module TreeWriter
+
+    Public Function GetBuffer() As MemoryStream
+        Dim ms As New MemoryStream
+
+
+    End Function
+
+End Module
