b5abcf9ff970a3514756a811de71150041911404
guigang xie <gg.xie@bionovogene.com>
2022-06-27 16:12:16 +0800

    save file test

--- Data/BinaryData/HDSPack/test/Program.vb
+++ Data/BinaryData/HDSPack/test/Program.vb
@@ -1,7 +1,16 @@
 Imports System
+Imports Microsoft.VisualBasic.DataStorage.HDSPack
 
 Module Program
+
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Using hds As New StreamPack("./test.hds")
+            Dim image = "D:\GCModeller\src\runtime\sciBASIC#\etc\ch07_18.png".ReadBinary
+            Dim block = hds.OpenBlock("/path/to/the/image/file/ch07-18.png")
+
+            Call block.Write(image, Scan0, image.Length)
+            Call block.Flush()
+            Call block.Dispose()
+        End Using
     End Sub
 End Module
