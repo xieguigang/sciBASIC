4ee68fb6276c54af3837914732f4bcc64645436c
guigang xie <gg.xie@bionovogene.com>
2022-06-28 10:59:08 +0800

    folder first and then files

--- Data/BinaryData/HDSPack/Debugger.vb
+++ Data/BinaryData/HDSPack/Debugger.vb
@@ -33,18 +33,21 @@ Public Module Debugger
     ''' <param name="text"></param>
     <Extension>
     Public Sub Tree(dir As StreamGroup, text As TextWriter, Optional indent As Integer = 0)
-        text.WriteLine(dir.ToString)
-        TreeInternal(dir, text, indent)
+        Call text.WriteLine(dir.ToString)
+        Call treeInternal(dir, text, indent)
     End Sub
 
-    Private Sub TreeInternal(dir As StreamGroup, text As TextWriter, indent As Integer)
-        For Each file As StreamObject In dir.files
+    Private Sub treeInternal(dir As StreamGroup, text As TextWriter, indent As Integer)
+        For Each file As StreamObject In From f As StreamObject
+                                         In dir.files
+                                         Order By If(TypeOf f Is StreamGroup, 0, 1)
+
             If TypeOf file Is StreamBlock Then
                 Call text.WriteLine($"{New String(" "c, indent * 4)}|-- " & file.ToString)
             Else
                 Call text.WriteLine()
                 Call text.WriteLine($"{New String(" "c, indent * 4)}|-- " & file.ToString)
-                Call TreeInternal(dir:=file, text, indent + 1)
+                Call treeInternal(dir:=file, text, indent + 1)
             End If
         Next
     End Sub
