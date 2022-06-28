7022413a7536e764d37197e35b90119bd9dad1e0
guigang xie <gg.xie@bionovogene.com>
2022-06-28 11:23:57 +0800

    split source code module

---
+++ Data/BinaryData/HDSPack/Metadata/AttributeMetadata.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Reflection
+
+Public Class AttributeMetadata
+
+    Public Property name As String
+    Public Property type As String
+    Public Property data As Byte()
+
+    Public ReadOnly Property GetUnderlyingType As Type
+        Get
+            If type.StringEmpty Then
+                Return GetType(Void)
+            Else
+                Return TypeInfo.GetType(type)
+            End If
+        End Get
+    End Property
+
+    Public Overrides Function ToString() As String
+        Return $"Dim {name} As {type} = binary({StringFormats.Lanudry(data.Length)})"
+    End Function
+
+End Class
--- Data/BinaryData/HDSPack/Metadata/LazyAttribute.vb
+++ Data/BinaryData/HDSPack/Metadata/LazyAttribute.vb
@@ -1,5 +1,4 @@
-﻿Imports System.Reflection
-Imports System.Text
+﻿Imports System.Text
 Imports Microsoft.VisualBasic.Data.IO.MessagePack
 Imports Microsoft.VisualBasic.ValueTypes
 
@@ -60,24 +59,3 @@ Public Class LazyAttribute
     End Function
 End Class
 
-Public Class AttributeMetadata
-
-    Public Property name As String
-    Public Property type As String
-    Public Property data As Byte()
-
-    Public ReadOnly Property GetUnderlyingType As Type
-        Get
-            If type.StringEmpty Then
-                Return GetType(Void)
-            Else
-                Return TypeInfo.GetType(type)
-            End If
-        End Get
-    End Property
-
-    Public Overrides Function ToString() As String
-        Return $"Dim {name} As {type} = binary({StringFormats.Lanudry(data.Length)})"
-    End Function
-
-End Class
