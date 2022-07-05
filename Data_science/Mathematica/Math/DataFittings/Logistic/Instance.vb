c27215dcae6db137856957781fc6d283d1332179
guigang xie <gg.xie@bionovogene.com>
2022-07-05 10:01:33 +0800

    Update Instance.vb

--- Data_science/Mathematica/Math/DataFittings/Logistic/Instance.vb
+++ Data_science/Mathematica/Math/DataFittings/Logistic/Instance.vb
@@ -1,8 +1,19 @@
 ﻿Imports Microsoft.VisualBasic.Serialization.JSON
 
+''' <summary>
+''' classify training model
+''' </summary>
 Public Class Instance
 
+    ''' <summary>
+    ''' the real label data
+    ''' </summary>
+    ''' <returns></returns>
     Public Property label As Integer
+    ''' <summary>
+    ''' the object properties vector
+    ''' </summary>
+    ''' <returns></returns>
     Public Property x As Double()
 
     Public Sub New(label As Integer, x As Integer())
