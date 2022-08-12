a9b349a684683ad380c794a816aae6532b3a3ac4
xieguigang <xie.guigang@gcmodeller.org>
2022-08-13 00:40:28 +0800

    Update sync_gitlink.cmd

--- .github/sync_gitlink.cmd
+++ .github/sync_gitlink.cmd
@@ -2,12 +2,7 @@
 
 REM git remote add gitlink https://gitlink.org.cn/xieguigang/sciBASIC.git
 
-git pull local HEAD
-git pull gitee HEAD
 git pull gitlink HEAD
-
-git push local HEAD
-git push gitee HEAD
 git push gitlink HEAD
 
 echo synchronization of this code repository job done!
