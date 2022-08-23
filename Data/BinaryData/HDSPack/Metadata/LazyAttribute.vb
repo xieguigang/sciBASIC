5c386bee819c8dfab70e4abb1d107f165111829e
guigang xie <xie.guigang@gcmodeller.org>
2022-08-23 16:19:29 +0800

    check file is exists or not

--- Data/BinaryData/HDSPack/Metadata/LazyAttribute.vb
+++ Data/BinaryData/HDSPack/Metadata/LazyAttribute.vb
@@ -1,56 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::705dbeb819706291d60c1f71bd85a160, sciBASIC#\Data\BinaryData\HDSPack\Metadata\LazyAttribute.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 92
-    '    Code Lines: 77
-    ' Comment Lines: 0
-    '   Blank Lines: 15
-    '     File Size: 3.86 KB
+' Summaries:
 
 
-    ' Class LazyAttribute
-    ' 
-    '     Properties: attributes
-    ' 
-    '     Function: GetBuffer, GetEnumerator, (+2 Overloads) GetValue, IEnumerable_GetEnumerator, ToArray
-    ' 
-    '     Sub: Add
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 92
+'    Code Lines: 77
+' Comment Lines: 0
+'   Blank Lines: 15
+'     File Size: 3.86 KB
+
+
+' Class LazyAttribute
+' 
+'     Properties: attributes
+' 
+'     Function: GetBuffer, GetEnumerator, (+2 Overloads) GetValue, IEnumerable_GetEnumerator, ToArray
+' 
+'     Sub: Add
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -85,7 +85,7 @@ Public Class LazyAttribute : Implements IEnumerable(Of String)
             }
         End If
 
-        Call attributes.Add(name, attr)
+        attributes(name) = attr
     End Sub
 
     Public Function GetValue(name As String) As Object
