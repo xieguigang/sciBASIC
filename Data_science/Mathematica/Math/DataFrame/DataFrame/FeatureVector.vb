c578fbe960f6679967a6fd461721997b5e42be81
xieguigang <xie.guigang@gcmodeller.org>
2022-09-11 11:05:02 +0800

    add data frame model

---
+++ Data_science/Mathematica/Math/DataFrame/DataFrame/FeatureVector.vb
@@ -0,0 +1,55 @@
+﻿
+Public Class FeatureVector
+
+    Public ReadOnly Property vector As Array
+    Public ReadOnly Property type As Type
+    Public ReadOnly Property isScalar As Boolean
+        Get
+            Return vector Is Nothing OrElse vector.Length = 0 OrElse vector.Length = 1
+        End Get
+    End Property
+
+    Sub New(ints As IEnumerable(Of Integer))
+        vector = ints.ToArray
+        type = GetType(Integer)
+    End Sub
+
+    Sub New(factors As IEnumerable(Of String))
+        vector = factors.ToArray
+        type = GetType(String)
+    End Sub
+
+    Sub New(floats As IEnumerable(Of Single))
+        vector = floats.ToArray
+        type = GetType(Single)
+    End Sub
+
+    Sub New(doubles As IEnumerable(Of Double))
+        vector = doubles.ToArray
+        type = GetType(Double)
+    End Sub
+
+    Sub New(int16 As IEnumerable(Of Short))
+        vector = int16.ToArray
+        type = GetType(Short)
+    End Sub
+
+    Sub New(int64 As IEnumerable(Of Long))
+        vector = int64.ToArray
+        type = GetType(Long)
+    End Sub
+
+    Sub New(logicals As IEnumerable(Of Boolean))
+        vector = logicals.ToArray
+        type = GetType(Boolean)
+    End Sub
+
+    Public Function [TryCast](Of T)() As T()
+        Throw New NotImplementedException
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return $"[{type.Name}] {vector.Length} elements"
+    End Function
+
+End Class
