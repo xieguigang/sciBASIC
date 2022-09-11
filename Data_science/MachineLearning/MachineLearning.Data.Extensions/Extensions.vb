8f7047fb700530059634f4be6720c6b6fd13a8f9
xieguigang <xie.guigang@gcmodeller.org>
2022-09-11 13:03:23 +0800

    fix of the symbol reference error

--- Data_science/MachineLearning/MachineLearning.Data.Extensions/Extensions.vb
+++ Data_science/MachineLearning/MachineLearning.Data.Extensions/Extensions.vb
@@ -55,10 +55,7 @@
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection
-Imports Microsoft.VisualBasic.Data.csv
-Imports Microsoft.VisualBasic.MachineLearning.NeuralNetwork.StoreProcedure
-Imports Microsoft.VisualBasic.MachineLearning.QLearning.DataModel
-Imports Microsoft.VisualBasic.Text
+Imports Microsoft.VisualBasic.MachineLearning.ComponentModel.StoreProcedure
 Imports row = Microsoft.VisualBasic.Data.csv.IO.DataSet
 
 Public Module Extensions
@@ -78,24 +75,3 @@ Public Module Extensions
             .ToArray
     End Function
 End Module
-
-Public Class QTableDump
-
-    ReadOnly __buffer As New Dictionary(Of IndexCurve)
-
-    Public Sub Dump(table As IQTable, iteration As Integer)
-        For Each o In table.Table.Values
-            For i As Integer = 0 To table.ActionRange - 1
-                Dim uid As String = $"[{i}] {o.EnvirState}"
-                If Not __buffer.ContainsKey(uid) Then
-                    Call __buffer.Add(uid, New IndexCurve(uid))
-                End If
-                Call __buffer(uid).Properties.Add(iteration, o.Qvalues(i))
-            Next
-        Next
-    End Sub
-
-    Public Sub Save(path As String)
-        Call __buffer.Values.SaveTo(path, Encodings.ASCII)
-    End Sub
-End Class
---
+++ Data_science/MachineLearning/MachineLearning.Data.Extensions/QTableDump.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.MachineLearning.QLearning.DataModel
+Imports Microsoft.VisualBasic.Text
+
+Public Class QTableDump
+
+    ReadOnly __buffer As New Dictionary(Of IndexCurve)
+
+    Public Sub Dump(table As IQTable, iteration As Integer)
+        For Each o In table.Table.Values
+            For i As Integer = 0 To table.ActionRange - 1
+                Dim uid As String = $"[{i}] {o.EnvirState}"
+                If Not __buffer.ContainsKey(uid) Then
+                    Call __buffer.Add(uid, New IndexCurve(uid))
+                End If
+                Call __buffer(uid).Properties.Add(iteration, o.Qvalues(i))
+            Next
+        Next
+    End Sub
+
+    Public Sub Save(path As String)
+        Call __buffer.Values.SaveTo(path, Encodings.ASCII)
+    End Sub
+End Class
