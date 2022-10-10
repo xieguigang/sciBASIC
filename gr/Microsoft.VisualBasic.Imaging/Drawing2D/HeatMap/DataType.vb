eabc4c56b21b042fb02f15a2a4e4caf92f565516
xieguigang <xie.guigang@gcmodeller.org>
2022-10-10 20:26:37 +0800

    add new source of heatmap raster engine

---
+++ gr/Microsoft.VisualBasic.Imaging/Drawing2D/HeatMap/DataType.vb
@@ -0,0 +1,8 @@
+﻿
+Namespace HeatMap
+    Public Class DataType
+        Public Property X As Integer
+        Public Property Y As Integer
+        Public Property Weight As Double
+    End Class
+End Namespace
