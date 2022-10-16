0bc4e66fccd5bf2c1699035da32d9bcc943abfdc
xieguigang <xie.guigang@gcmodeller.org>
2022-10-17 04:21:50 +0800

    scan dir

--- www/Microsoft.VisualBasic.NETProtocol/Test/emailParser.vb
+++ www/Microsoft.VisualBasic.NETProtocol/Test/emailParser.vb
@@ -5,22 +5,29 @@ Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
 Public Module emailParser
 
     Sub Main()
-        Dim raw = "C:\Users\Administrator\Downloads\aaaa.eml"
-        Dim email = EmlReader.ParseEMail(raw)
-        Dim tables = email.BodyContent.GetTablesHTML
-        Dim test = getInnerTable(tables(59)).GetColumnsHTML.Select(Function(c) c.StripHTMLTags).ToArray
-        Dim data = tables.Select(Function(t)
-                                     Return getInnerTable(t).GetColumnsHTML.Select(Function(r) r.StripHTMLTags()).ToArray
-                                 End Function).Where(Function(r) r.Length > 3) _
-            .ToArray
+        Dim rawDir = "C:\Users\Administrator\Downloads"
 
-        Using book As New StreamWriter(raw.ChangeSuffix("txt").Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False))
-            For Each line In data.Where(Function(n) n.Length = 8)
-                Call book.WriteLine(line.JoinBy(vbTab))
-            Next
-        End Using
+        Call parseDataFiles(rawDir.ListFiles("*.eml"))
 
         Pause()
+    End Sub
+
+    Sub parseDataFiles(files As IEnumerable(Of String))
+        For Each raw As String In files
+            Dim email = EmlReader.ParseEMail(raw)
+            Dim tables = email.BodyContent.GetTablesHTML
+            Dim test = getInnerTable(tables(59)).GetColumnsHTML.Select(Function(c) c.StripHTMLTags).ToArray
+            Dim data = tables.Select(Function(t)
+                                         Return getInnerTable(t).GetColumnsHTML.Select(Function(r) r.StripHTMLTags()).ToArray
+                                     End Function).Where(Function(r) r.Length > 3) _
+                .ToArray
+
+            Using book As New StreamWriter(raw.ChangeSuffix("txt").Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False))
+                For Each line In data.Where(Function(n) n.Length = 8)
+                    Call book.WriteLine(line.JoinBy(vbTab))
+                Next
+            End Using
+        Next
     End Sub
 
     Function getInnerTable(text As String) As String
