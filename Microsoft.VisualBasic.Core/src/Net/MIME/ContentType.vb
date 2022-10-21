240f238784b794385be25a5768f4da5ada5adeb3
xieguigang <xie.guigang@gcmodeller.org>
2022-10-22 01:26:28 +0800

    config for webview2

--- Microsoft.VisualBasic.Core/src/Net/MIME/ContentType.vb
+++ Microsoft.VisualBasic.Core/src/Net/MIME/ContentType.vb
@@ -1,67 +1,73 @@
 ﻿#Region "Microsoft.VisualBasic::ba3d66170f3cba105d18b9a45d3ae4e3, sciBASIC#\Microsoft.VisualBasic.Core\src\Net\MIME\ContentType.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 74
-    '    Code Lines: 38
-    ' Comment Lines: 25
-    '   Blank Lines: 11
-    '     File Size: 2.23 KB
+' Summaries:
 
 
-    '     Structure ContentType
-    ' 
-    '         Properties: Details, FileExt, IsEmpty, MIMEType, Name
-    ' 
-    '         Function: __createObject, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 74
+'    Code Lines: 38
+' Comment Lines: 25
+'   Blank Lines: 11
+'     File Size: 2.23 KB
+
+
+'     Structure ContentType
+' 
+'         Properties: Details, FileExt, IsEmpty, MIMEType, Name
+' 
+'         Function: __createObject, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
 Imports Microsoft.VisualBasic.Language.Default
 Imports Microsoft.VisualBasic.Text
 
 Namespace Net.Protocols.ContentTypes
 
+#Disable Warning BC40000 ' Type or member is obsolete
     ''' <summary>
     ''' MIME types / Internet Media Types
     ''' </summary>
-    Public Structure ContentType : Implements IsEmpty
+    ''' 
+    <ClassInterface(ClassInterfaceType.AutoDual)>
+    <ComVisible(True)>
+    Public Class ContentType : Implements IsEmpty
+#Enable Warning BC40000 ' Type or member is obsolete
 
         ''' <summary>
         ''' Type name or brief info
@@ -123,6 +129,6 @@ Namespace Net.Protocols.ContentTypes
                 Return mime
             End If
         End Function
-    End Structure
+    End Class
 
 End Namespace
