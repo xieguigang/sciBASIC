b2a53e16f2673a67b06ce8d0af1ff7394b94ca7a
xieguigang <xie.guigang@gcmodeller.org>
2022-11-07 21:22:19 +0800

    needs a unify model for multiple kind of filesystem?

---
+++ Microsoft.VisualBasic.Core/src/ApplicationServices/FileSystem/IFileSystemEnvironment.vb
@@ -0,0 +1,12 @@
+﻿
+Namespace ApplicationServices
+
+    Public Interface IFileSystemEnvironment
+
+        ''' <summary>
+        ''' the file system environment is readonly?
+        ''' </summary>
+        ReadOnly Property [readonly] As Boolean
+
+    End Interface
+End Namespace
