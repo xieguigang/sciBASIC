4d812dbcdd9111004dba7f4043a74529b9a724de
xieguigang <xie.guigang@gcmodeller.org>
2022-11-10 22:01:28 +0800

    Update IFileSystemEnvironment.vb

--- Microsoft.VisualBasic.Core/src/ApplicationServices/FileSystem/IFileSystemEnvironment.vb
+++ Microsoft.VisualBasic.Core/src/ApplicationServices/FileSystem/IFileSystemEnvironment.vb
@@ -1,12 +1,50 @@
 ﻿
+Imports System.IO
+
 Namespace ApplicationServices
 
+    ''' <summary>
+    ''' an abstract interface for union filesystem:
+    ''' 
+    ''' 1. local filesystem
+    ''' 2. zip archive
+    ''' 3. HDS streampack filesystem
+    ''' 
+    ''' </summary>
     Public Interface IFileSystemEnvironment
 
         ''' <summary>
         ''' the file system environment is readonly?
         ''' </summary>
         ReadOnly Property [readonly] As Boolean
+
+        ''' <summary>
+        ''' open a specific file for read and write
+        ''' </summary>
+        ''' <param name="path"></param>
+        ''' <param name="mode"></param>
+        ''' <param name="access"></param>
+        ''' <returns></returns>
+        Function OpenFile(path As String,
+                          Optional mode As FileMode = FileMode.OpenOrCreate,
+                          Optional access As FileAccess = FileAccess.Read) As Stream
+        ''' <summary>
+        ''' delete target file which is specific by path
+        ''' </summary>
+        ''' <param name="path"></param>
+        ''' <returns></returns>
+        Function DeleteFile(path As String) As Boolean
+        ''' <summary>
+        ''' check the specific file is exists on current filesystem or not?
+        ''' </summary>
+        ''' <param name="path"></param>
+        ''' <returns></returns>
+        Function FileExists(path As String) As Boolean
+
+        ''' <summary>
+        ''' close current filesystem session
+        ''' </summary>
+        Sub Close()
 
     End Interface
 End Namespace
