8f36d615496ceaf286836850e888a56eb5eb9d17
guigang xie <xie.guigang@gcmodeller.org>
2023-02-06 11:23:45 +0800

    fix of the namespace reference error

--- Data_science/MachineLearning/MachineLearning/RandomForests/StringTokenizer.vb
+++ Data_science/MachineLearning/MachineLearning/RandomForests/StringTokenizer.vb
@@ -1,19 +1,22 @@
-﻿Friend Class StringTokenizer
-    Private line As String
-    Private v As String
+﻿Namespace RandomForests
 
-    Public Sub New(line As String, v As String)
-        Me.line = line
-        Me.v = v
-    End Sub
+    Friend Class StringTokenizer
+        Private line As String
+        Private v As String
 
-    Friend ReadOnly Property countTokens As Integer
-        Get
+        Public Sub New(line As String, v As String)
+            Me.line = line
+            Me.v = v
+        End Sub
+
+        Friend ReadOnly Property countTokens As Integer
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
+
+        Friend Function nextToken() As String
             Throw New NotImplementedException()
-        End Get
-    End Property
-
-    Friend Function nextToken() As String
-        Throw New NotImplementedException()
-    End Function
-End Class
+        End Function
+    End Class
+End Namespace
