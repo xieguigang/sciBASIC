cd548b635d0fab67aa88c7bc6fca6b33186d1f06
guigang xie <xie.guigang@gcmodeller.org>
2023-02-06 11:27:30 +0800

    move helper module to base runtime project

---
+++ Microsoft.VisualBasic.Core/src/Language/Language/Java/StringTokenizer.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Language.Java
+
+    Public Class StringTokenizer
+
+        ReadOnly rawText As String
+        ReadOnly sep As String
+        ReadOnly tokens As String()
+
+        Dim i As i32 = 0
+
+        Public ReadOnly Property countTokens As Integer
+            Get
+                Return tokens.Length
+            End Get
+        End Property
+
+        Public Sub New(text As String, sep As String)
+            Me.rawText = text
+            Me.sep = sep
+            Me.tokens = text.StringSplit(sep)
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function nextToken() As String
+            Return tokens.ElementAtOrDefault(++i)
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return $"{countTokens} tokens from '{rawText}'"
+        End Function
+    End Class
+End Namespace
