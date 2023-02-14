dfc6977590e0aa43a9c56df3320e43977a23852d
xieguigang <xie.guigang@gcmodeller.org>
2023-02-14 21:25:15 +0800

    Update StringHelpers.vb

--- Microsoft.VisualBasic.Core/src/Extensions/StringHelpers/StringHelpers.vb
+++ Microsoft.VisualBasic.Core/src/Extensions/StringHelpers/StringHelpers.vb
@@ -70,7 +70,6 @@ Imports System.Runtime.CompilerServices
 Imports System.Text
 Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic.CommandLine.Reflection
-Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language
