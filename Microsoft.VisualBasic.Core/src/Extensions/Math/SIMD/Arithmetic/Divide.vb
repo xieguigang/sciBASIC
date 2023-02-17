15bcab9ed6d7a8eafbf4c1cb18b17c0ef3b7db25
guigang xie <xie.guigang@gcmodeller.org>
2023-02-17 14:23:46 +0800

    SIMD divide

--- Microsoft.VisualBasic.Core/src/Extensions/Math/SIMD/Arithmetic/Divide.vb
+++ Microsoft.VisualBasic.Core/src/Extensions/Math/SIMD/Arithmetic/Divide.vb
@@ -81,6 +81,16 @@ Namespace Math.SIMD
             Return result
         End Function
 
+        Public Shared Function int32_op_divide_int32_scalar(v1 As Integer(), v2 As Double) As Double()
+            Dim result As Double() = New Double(v1.Length - 1) {}
+
+            For i As Integer = 0 To v1.Length - 1
+                result(i) = v1(i) / v2
+            Next
+
+            Return result
+        End Function
+
         Public Shared Function f64_op_divide_f64(v1 As Double(), v2 As Double()) As Double()
             Dim result As Double() = New Double(v1.Length - 1) {}
 
