0b7ccb6edd5d7f7b329b844a162594d7fcad2273
guigang xie <xie.guigang@gcmodeller.org>
2023-02-23 14:21:14 +0800

    fix of the missing schema mapping attribute

--- Data/BinaryData/netCDF/netCDFReader.vb
+++ Data/BinaryData/netCDF/netCDFReader.vb
@@ -1,66 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::aa0992daef190db66208c96421c69b1a, sciBASIC#\Data\BinaryData\netCDF\netCDFReader.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 316
-    '    Code Lines: 166
-    ' Comment Lines: 114
-    '   Blank Lines: 36
-    '     File Size: 11.89 KB
+' Summaries:
 
 
-    ' Class netCDFReader
-    ' 
-    '     Properties: dimensions, globalAttributes, recordDimension, variables, version
-    ' 
-    '     Constructor: (+3 Overloads) Sub New
-    ' 
-    '     Function: attributeExists, dataVariableExists, (+2 Overloads) getDataVariable, getDataVariableAsString, getDataVariableEntry
-    '               Open, ToString
-    ' 
-    '     Sub: (+2 Overloads) Dispose, getDataVariable, Print
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 316
+'    Code Lines: 166
+' Comment Lines: 114
+'   Blank Lines: 36
+'     File Size: 11.89 KB
+
+
+' Class netCDFReader
+' 
+'     Properties: dimensions, globalAttributes, recordDimension, variables, version
+' 
+'     Constructor: (+3 Overloads) Sub New
+' 
+'     Function: attributeExists, dataVariableExists, (+2 Overloads) getDataVariable, getDataVariableAsString, getDataVariableEntry
+'               Open, ToString
+' 
+'     Sub: (+2 Overloads) Dispose, getDataVariable, Print
+' 
+' /********************************************************************************/
 
 #End Region
 
-#If netcore5 = 1 Then
-Imports System.Data
-#End If
-
-Imports System.Data
 Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports System.Text
@@ -70,6 +65,10 @@ Imports Microsoft.VisualBasic.DataStorage.netCDF.Data
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.VisualBasic.Text
+
+#If NETCOREAPP Then
+Imports System.Data
+#End If
 
 ''' <summary>
 ''' The dotCDF file of a CDF contains magic numbers and numerous internal records are used to organize information
