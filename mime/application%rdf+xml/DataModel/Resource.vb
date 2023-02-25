19049f298707407a4d6d822d76bcb63c4b01b213
xieguigang <xie.guigang@gcmodeller.org>
2023-02-25 17:15:26 +0800

    make improvements of the RDF data models

--- mime/application%rdf+xml/DataModel/Resource.vb
+++ mime/application%rdf+xml/DataModel/Resource.vb
@@ -1,63 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::3343a3813507482c7ff0576988818369, sciBASIC#\mime\application%rdf+xml\DataModel\Resource.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 26
-    '    Code Lines: 17
-    ' Comment Lines: 3
-    '   Blank Lines: 6
-    '     File Size: 662 B
+' Summaries:
 
 
-    ' Class Resource
-    ' 
-    '     Properties: resource
-    ' 
-    ' Class DataValue
-    ' 
-    '     Properties: type, value
-    ' 
-    '     Function: ToString
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 26
+'    Code Lines: 17
+' Comment Lines: 3
+'   Blank Lines: 6
+'     File Size: 662 B
+
+
+' Class Resource
+' 
+'     Properties: resource
+' 
+' Class DataValue
+' 
+'     Properties: type, value
+' 
+'     Function: ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Runtime.Serialization
 Imports System.Xml.Serialization
-Imports Microsoft.VisualBasic.Serialization.JSON
 
 ''' <summary>
 ''' a data resource reference
@@ -66,18 +64,5 @@ Public Class Resource
 
     <XmlAttribute("resource", [Namespace]:=RDFEntity.XmlnsNamespace)>
     Public Property resource As String
-End Class
-
-Public Class DataValue
-
-    <XmlAttribute("datatype", [Namespace]:=RDFEntity.XmlnsNamespace)>
-    <DataMember(Name:="type")>
-    Public Property type As String
-    <XmlText>
-    Public Property value As String
-
-    Public Overrides Function ToString() As String
-        Return Me.GetJson
-    End Function
 
 End Class
--- mime/application%rdf+xml/RDFEntity.vb
+++ mime/application%rdf+xml/RDFEntity.vb
@@ -1,67 +1,68 @@
 ﻿#Region "Microsoft.VisualBasic::289bd077cc0adcd7e3371ebc6f810714, sciBASIC#\mime\application%rdf+xml\RDFEntity.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 92
-    '    Code Lines: 35
-    ' Comment Lines: 42
-    '   Blank Lines: 15
-    '     File Size: 3.03 KB
+' Summaries:
 
 
-    ' Class RDFEntity
-    ' 
-    '     Properties: about, comment, Properties, range, RDFId
-    ' 
-    '     Function: ToString
-    ' 
-    ' Class RDFProperty
-    ' 
-    ' 
-    ' 
-    ' Class EntityProperty
-    ' 
-    '     Properties: dataType, resource, value
-    ' 
-    '     Constructor: (+3 Overloads) Sub New
-    '     Function: ToString
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 92
+'    Code Lines: 35
+' Comment Lines: 42
+'   Blank Lines: 15
+'     File Size: 3.03 KB
+
+
+' Class RDFEntity
+' 
+'     Properties: about, comment, Properties, range, RDFId
+' 
+'     Function: ToString
+' 
+' Class RDFProperty
+' 
+' 
+' 
+' Class EntityProperty
+' 
+'     Properties: dataType, resource, value
+' 
+'     Constructor: (+3 Overloads) Sub New
+'     Function: ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 
@@ -113,9 +114,9 @@ Public Class RDFProperty : Inherits EntityProperty
 End Class
 
 ''' <summary>
-''' 
+''' DataValue
 ''' </summary>
-Public MustInherit Class EntityProperty
+Public Class EntityProperty
 
     ''' <summary>
     ''' rdf:datatype
@@ -147,15 +148,22 @@ Public MustInherit Class EntityProperty
     Sub New()
     End Sub
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Protected Sub New(dt As String)
         dataType = dt
     End Sub
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Protected Sub New(type As Type)
         Call Me.New(type.SchemaDataType)
     End Sub
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function ParseValue() As Object
+        Return Scripting.CTypeDynamic(value, Me.SchemaDataType)
+    End Function
+
     Public Overrides Function ToString() As String
-        Return $"({Me.SchemaDataType.ToString}) {value}; resource: {resource}"
+        Return $"[resource: {resource}] ({Me.SchemaDataType.FullName}) '{value}'"
     End Function
 End Class
