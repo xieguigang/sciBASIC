f6dece89558b0759f91f7049c83f898501d1152d
xieguigang <xie.guigang@gcmodeller.org>
2023-02-25 14:20:06 +0800

    how to read xml to clr object?

--- mime/application%xml/XmlGeneric/GraphWriter.vb
+++ mime/application%xml/XmlGeneric/GraphWriter.vb
@@ -1,3 +1,23 @@
-﻿Public Class GraphWriter
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
+
+Public Class GraphWriter
+
+    ReadOnly graph As SoapGraph
+
+    Sub New(type As Type)
+        graph = SoapGraph.GetSchema(type)
+    End Sub
+
+    Public Function Load(xml As XmlElement) As Object
+
+    End Function
+
+    Public Shared Function LoadXml(Of T)(xml As String) As T
+        Dim doc As XmlElement = XmlElement.ParseXmlText(xml.SolveStream)
+        Dim writer As New GraphWriter(GetType(T))
+        Dim obj As Object = writer.Load(doc)
+
+        Return obj
+    End Function
 
 End Class
