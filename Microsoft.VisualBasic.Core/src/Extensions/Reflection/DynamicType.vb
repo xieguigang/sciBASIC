ebb36aa02df8881e2709d9cfee5e44ec0c70b408
xieguigang <xie.guigang@gcmodeller.org>
2023-03-05 00:37:09 +0800

    Update DynamicType.vb

--- Microsoft.VisualBasic.Core/src/Extensions/Reflection/DynamicType.vb
+++ Microsoft.VisualBasic.Core/src/Extensions/Reflection/DynamicType.vb
@@ -1,6 +1,8 @@
-﻿Imports System.Reflection
+﻿Imports System.ComponentModel
+Imports System.Reflection
 Imports System.Reflection.Emit
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Linq
 
 ''' <summary>
 ''' 
@@ -13,9 +15,16 @@ Public Class DynamicType
     Public ReadOnly Property GeneratedType As Type
 
     ReadOnly inheritsFrom As Type
-    ReadOnly properties As NamedValue(Of Type)()
+    ReadOnly properties As PropertyInfo()
 
-    Sub New(ParamArray properties As NamedValue(Of Type)())
+    Public Structure PropertyInfo
+        Dim Name As String
+        Dim PropertyType As Type
+        Dim Description As String
+        Dim DisplayName As String
+    End Structure
+
+    Sub New(ParamArray properties As PropertyInfo())
         Me.properties = properties
     End Sub
 
@@ -37,16 +46,18 @@ Public Class DynamicType
                                                   MethodAttributes.SpecialName Or
                                                   MethodAttributes.RTSpecialName)
 
-        For Each [property] As NamedValue(Of Type) In properties
-            Call AddProperty(dynamicType, [property].Name, [property].Value)
+        For Each [property] As PropertyInfo In properties
+            Call AddProperty(dynamicType, [property].Name, [property].PropertyType,
+                             desc:=[property].Description,
+                             display:=[property].DisplayName)
         Next
 
         _GeneratedType = dynamicType.CreateType()
     End Function
 
-    Private Shared Sub AddProperty(typeBuilder As TypeBuilder, propertyName As String, propertyType As Type)
+    Private Shared Sub AddProperty(typeBuilder As TypeBuilder, propertyName As String, propertyType As Type, desc As String, display As String)
         Dim fieldBuilder = typeBuilder.DefineField("_" & propertyName, propertyType, FieldAttributes.Private)
-        Dim propertyBuilder = typeBuilder.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null)
+        Dim propertyBuilder As PropertyBuilder = typeBuilder.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null)
         Dim getMethod = typeBuilder.DefineMethod("get_" & propertyName,
         MethodAttributes.Public Or
         MethodAttributes.SpecialName Or
@@ -72,7 +83,66 @@ Public Class DynamicType
         setMethodIL.MarkLabel(exitSet)
         setMethodIL.Emit(OpCodes.Ret)
 
-        propertyBuilder.SetGetMethod(getMethod)
-        propertyBuilder.SetSetMethod(setMethod)
+        If Not desc.StringEmpty Then
+            Call AddDescription(propertyBuilder, desc)
+        End If
+        If Not display.StringEmpty Then
+            Call AddDisplayName(propertyBuilder, display)
+        End If
+
+        Call propertyBuilder.SetGetMethod(getMethod)
+        Call propertyBuilder.SetSetMethod(setMethod)
     End Sub
+
+    Private Shared Sub AddDisplayName(propertyBuilder As PropertyBuilder, display As String)
+        Dim ctorSig = New Type() {GetType(String)}
+        Dim classInfo As ConstructorInfo = GetType(DisplayNameAttribute).GetConstructor(ctorSig)
+        Dim attr As New CustomAttributeBuilder(classInfo, New Object() {display})
+
+        Call propertyBuilder.SetCustomAttribute(attr)
+    End Sub
+
+    Private Shared Sub AddDescription(propertyBuilder As PropertyBuilder, desc As String)
+        Dim ctorSig = New Type() {GetType(String)}
+        Dim classInfo As ConstructorInfo = GetType(DescriptionAttribute).GetConstructor(ctorSig)
+        Dim attr As New CustomAttributeBuilder(classInfo, New Object() {desc})
+
+        Call propertyBuilder.SetCustomAttribute(attr)
+    End Sub
+
+    Public Shared Function Create(metadata As Dictionary(Of String, Object)) As Object
+        Dim properties As New List(Of PropertyInfo)
+
+        For Each meta In metadata
+            Dim symbol As String = meta.Key.NormalizePathString()
+            Dim type As Type
+            Dim value As Object = meta.Value
+
+            If value Is Nothing Then
+                type = GetType(String)
+                value = "NULL"
+            Else
+                type = value.GetType
+            End If
+
+            properties.Add(New PropertyInfo With {
+                .Name = symbol,
+                .PropertyType = type,
+                .Description = "",
+                .DisplayName = meta.Key
+            })
+        Next
+
+        Dim obj As Object = New DynamicType(properties.ToArray).Create.GeneratedType.DoCall(AddressOf Activator.CreateInstance)
+        Dim schema = DataFramework.Schema(obj.GetType, flag:=PropertyAccess.Writeable, nonIndex:=True)
+
+        For Each meta In properties
+            Dim value As Object = metadata(meta.DisplayName)
+            Dim prop = schema(meta.Name)
+
+            Call prop.SetValue(obj, value)
+        Next
+
+        Return obj
+    End Function
 End Class
