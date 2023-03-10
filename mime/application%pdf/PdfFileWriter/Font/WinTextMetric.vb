a9abb3d06a644af937e2545c69505164c681eaf8
guigang xie <xie.guigang@gcmodeller.org>
2023-03-10 17:00:55 +0800

    Update WinTextMetric.vb

--- mime/application%pdf/PdfFileWriter/Font/WinTextMetric.vb
+++ mime/application%pdf/PdfFileWriter/Font/WinTextMetric.vb
@@ -8,89 +8,13 @@
 ''' that is, they depend on the current mapping mode of the
 ''' display context.
 ''' </remarks>
-
 Public Class WinTextMetric
-    ''' <summary>
-    ''' TextMetric height
-    ''' </summary>
 
-    ''' <summary>
-    ''' TextMetric ascent
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric descent
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric internal leading
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric external leading
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric average character width
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric maximum character width
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric height
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric overhang
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric digitize aspect X
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric digitize aspect Y
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric first character
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric last character
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric default character
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric break character
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric italic
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric underlined
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric struck out
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric pitch and family
-    ''' </summary>
-
-    ''' <summary>
-    ''' TextMetric character set
-    ''' </summary>
     Private _tmHeight As Integer, _tmAscent As Integer, _tmDescent As Integer, _tmInternalLeading As Integer, _tmExternalLeading As Integer, _tmAveCharWidth As Integer, _tmMaxCharWidth As Integer, _tmWeight As Integer, _tmOverhang As Integer, _tmDigitizedAspectX As Integer, _tmDigitizedAspectY As Integer, _tmFirstChar As UShort, _tmLastChar As UShort, _tmDefaultChar As UShort, _tmBreakChar As UShort, _tmItalic As Byte, _tmUnderlined As Byte, _tmStruckOut As Byte, _tmPitchAndFamily As Byte, _tmCharSet As Byte
 
+    ''' <summary>
+    ''' TextMetric height
+    ''' </summary>
     Public Property tmHeight As Integer
         Get
             Return _tmHeight
@@ -99,7 +23,9 @@ Public Class WinTextMetric
             _tmHeight = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric ascent
+    ''' </summary>
     Public Property tmAscent As Integer
         Get
             Return _tmAscent
@@ -108,7 +34,9 @@ Public Class WinTextMetric
             _tmAscent = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric descent
+    ''' </summary>
     Public Property tmDescent As Integer
         Get
             Return _tmDescent
@@ -117,7 +45,9 @@ Public Class WinTextMetric
             _tmDescent = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric internal leading
+    ''' </summary>
     Public Property tmInternalLeading As Integer
         Get
             Return _tmInternalLeading
@@ -126,7 +56,9 @@ Public Class WinTextMetric
             _tmInternalLeading = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric external leading
+    ''' </summary>
     Public Property tmExternalLeading As Integer
         Get
             Return _tmExternalLeading
@@ -135,7 +67,9 @@ Public Class WinTextMetric
             _tmExternalLeading = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric average character width
+    ''' </summary>
     Public Property tmAveCharWidth As Integer
         Get
             Return _tmAveCharWidth
@@ -144,7 +78,9 @@ Public Class WinTextMetric
             _tmAveCharWidth = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric maximum character width
+    ''' </summary>
     Public Property tmMaxCharWidth As Integer
         Get
             Return _tmMaxCharWidth
@@ -153,7 +89,9 @@ Public Class WinTextMetric
             _tmMaxCharWidth = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric height
+    ''' </summary>
     Public Property tmWeight As Integer
         Get
             Return _tmWeight
@@ -162,7 +100,9 @@ Public Class WinTextMetric
             _tmWeight = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric overhang
+    ''' </summary>
     Public Property tmOverhang As Integer
         Get
             Return _tmOverhang
@@ -172,6 +112,9 @@ Public Class WinTextMetric
         End Set
     End Property
 
+    ''' <summary>
+    ''' TextMetric digitize aspect X
+    ''' </summary>
     Public Property tmDigitizedAspectX As Integer
         Get
             Return _tmDigitizedAspectX
@@ -180,6 +123,9 @@ Public Class WinTextMetric
             _tmDigitizedAspectX = value
         End Set
     End Property
+    ''' <summary>
+    ''' TextMetric digitize aspect Y
+    ''' </summary>
 
     Public Property tmDigitizedAspectY As Integer
         Get
@@ -189,7 +135,9 @@ Public Class WinTextMetric
             _tmDigitizedAspectY = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric first character
+    ''' </summary>
     Public Property tmFirstChar As UShort
         Get
             Return _tmFirstChar
@@ -198,7 +146,9 @@ Public Class WinTextMetric
             _tmFirstChar = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric last character
+    ''' </summary>
     Public Property tmLastChar As UShort
         Get
             Return _tmLastChar
@@ -207,7 +157,9 @@ Public Class WinTextMetric
             _tmLastChar = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric default character
+    ''' </summary>
     Public Property tmDefaultChar As UShort
         Get
             Return _tmDefaultChar
@@ -216,7 +168,9 @@ Public Class WinTextMetric
             _tmDefaultChar = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric break character
+    ''' </summary>
     Public Property tmBreakChar As UShort
         Get
             Return _tmBreakChar
@@ -225,7 +179,9 @@ Public Class WinTextMetric
             _tmBreakChar = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric italic
+    ''' </summary>
     Public Property tmItalic As Byte
         Get
             Return _tmItalic
@@ -234,7 +190,9 @@ Public Class WinTextMetric
             _tmItalic = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric underlined
+    ''' </summary>
     Public Property tmUnderlined As Byte
         Get
             Return _tmUnderlined
@@ -243,7 +201,9 @@ Public Class WinTextMetric
             _tmUnderlined = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric struck out
+    ''' </summary>
     Public Property tmStruckOut As Byte
         Get
             Return _tmStruckOut
@@ -252,7 +212,9 @@ Public Class WinTextMetric
             _tmStruckOut = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric pitch and family
+    ''' </summary>
     Public Property tmPitchAndFamily As Byte
         Get
             Return _tmPitchAndFamily
@@ -261,7 +223,9 @@ Public Class WinTextMetric
             _tmPitchAndFamily = value
         End Set
     End Property
-
+    ''' <summary>
+    ''' TextMetric character set
+    ''' </summary>
     Public Property tmCharSet As Byte
         Get
             Return _tmCharSet
