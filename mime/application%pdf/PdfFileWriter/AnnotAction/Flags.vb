1e7a42ba2b68e6281945159d0499c27983a8bc21
guigang xie <xie.guigang@gcmodeller.org>
2023-03-10 16:43:58 +0800

    split source file of AnnotAction

---
+++ mime/application%pdf/PdfFileWriter/AnnotAction/Flags.vb
@@ -0,0 +1,64 @@
+﻿
+''' <summary>
+''' File attachement icon
+''' </summary>
+Public Enum FileAttachIcon
+    ''' <summary>
+    ''' Graph
+    ''' </summary>
+    Graph
+
+    ''' <summary>
+    ''' Paperclip
+    ''' </summary>
+    Paperclip
+
+    ''' <summary>
+    ''' PushPin (default)
+    ''' </summary>
+    PushPin
+
+    ''' <summary>
+    ''' Tag
+    ''' </summary>
+    Tag
+
+    ''' <summary>
+    ''' no icon 
+    ''' </summary>
+    NoIcon
+End Enum
+
+''' <summary>
+''' Sticky note icon
+''' </summary>
+Public Enum StickyNoteIcon
+    ''' <summary>
+    ''' Comment (note: no icon)
+    ''' </summary>
+    Comment
+    ''' <summary>
+    ''' Key
+    ''' </summary>
+    Key
+    ''' <summary>
+    ''' Note (default)
+    ''' </summary>
+    Note
+    ''' <summary>
+    ''' Help
+    ''' </summary>
+    Help
+    ''' <summary>
+    ''' New paragraph
+    ''' </summary>
+    NewParagraph
+    ''' <summary>
+    ''' Paragraph
+    ''' </summary>
+    Paragraph
+    ''' <summary>
+    ''' Insert
+    ''' </summary>
+    Insert
+End Enum
