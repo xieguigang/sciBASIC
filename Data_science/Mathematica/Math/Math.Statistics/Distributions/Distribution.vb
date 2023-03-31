601aa527c2dc7898d9e797168bdb3f91cb0e1108
xieguigang <xie.guigang@gcmodeller.org>
2023-04-01 00:01:08 +0800

    Update Distribution.vb

--- Data_science/Mathematica/Math/Math.Statistics/Distributions/Distribution.vb
+++ Data_science/Mathematica/Math/Math.Statistics/Distributions/Distribution.vb
@@ -55,32 +55,26 @@
 Imports Microsoft.VisualBasic.Language
 
 Namespace Distributions
+
     Public Class Distribution
-        Private Shared ReadOnly Z_MAX As Double = 6.0
-        Private Shared ReadOnly Z_EPSILON As Double = 0.000001
-        Private Shared ReadOnly CHI_EPSILON As Double = 0.000001
-        Private Shared ReadOnly CHI_MAX As Double = 99999.0
-        Private Shared ReadOnly LOG_SQRT_PI As Double = 0.57236494292470008
-        Private Shared ReadOnly I_SQRT_PI As Double = 0.56418958354775628
-        Private Shared ReadOnly BIGX As Double = 200.0
-        Private Shared ReadOnly I_PI As Double = 0.31830988618379069
-        Private Shared ReadOnly F_EPSILON As Double = 0.000001
-        Private Shared ReadOnly F_MAX As Double = 9999.0
 
-        ' *
-        '  
-        ' <summary>The ex method takes a double x as an input, if x is less than -BIGX it returns 0, otherwise it returns Euler's number
-        '  <i>e</i> raised to the power of x.</summary>
-        ' 
-        ' 
-        '  
-        ' <param name="x">double input.</param>
-        ' 
-        '  
-        ' <returns>0 if input is less than -BIGX, Euler's number <i>e</i> raised to the power of x otherwise.</returns>
-        ' 
-        ' 
+        Public Const Z_MAX As Double = 6.0
+        Public Const Z_EPSILON As Double = 0.000001
+        Public Const CHI_EPSILON As Double = 0.000001
+        Public Const CHI_MAX As Double = 99999.0
+        Public Const LOG_SQRT_PI As Double = 0.57236494292470008
+        Public Const I_SQRT_PI As Double = 0.56418958354775628
+        Public Const BIGX As Double = 200.0
+        Public Const I_PI As Double = 0.31830988618379069
+        Public Const F_EPSILON As Double = 0.000001
+        Public Const F_MAX As Double = 9999.0
 
+        ''' <summary>
+        ''' The ex method takes a double x as an input, if x is less than -BIGX it returns 0, otherwise it returns Euler's number
+        ''' <i>e</i> raised to the power of x.
+        ''' </summary>
+        ''' <param name="x"></param>
+        ''' <returns>0 if input is less than -BIGX, Euler's number <i>e</i> raised to the power of x otherwise.</returns>
         Private Shared Function Ex(x As Double) As Double
             If x < -BIGX Then
                 Return 0
@@ -89,20 +83,12 @@ Namespace Distributions
             Return System.Math.Exp(x)
         End Function
 
-        ' *
-        '  
-        ' <summary>The beta method takes a double {@link java.lang.reflect.Array} x as an input. It loops through x and accumulates
-        '  the value of gammaLn(x), also it sums up the items of x and returns (accumulated result - gammaLn of this summation).</summary>
-        ' 
-        ' 
-        '  
-        ' <param name="x">double {@link java.lang.reflect.Array} input.</param>
-        ' 
-        '  
-        ' <returns>gammaLn(sum).</returns>
-        ' 
-        ' 
-
+        ''' <summary>
+        ''' The beta method takes a double {@link java.lang.reflect.Array} x as an input. It loops through x and accumulates
+        ''' the value of gammaLn(x), also it sums up the items of x and returns (accumulated result - gammaLn of this summation).
+        ''' </summary>
+        ''' <param name="x"></param>
+        ''' <returns>gammaLn(sum).</returns>
         Public Shared Function Beta(x As Double()) As Double
             Dim sum = 0.0, result = 0.0
             Dim i As Integer
@@ -115,19 +101,11 @@ Namespace Distributions
             Return result
         End Function
 
-        ' *
-        '  
-        ' <summary>The gammaLn method takes a double x as an input and returns the logarithmic result of the gamma distribution at point x.</summary>
-        ' 
-        ' 
-        '  
-        ' <param name="x">double input.</param>
-        ' 
-        '  
-        ' <returns>the logarithmic result of the gamma distribution at point x.</returns>
-        ' 
-        ' 
-
+        ''' <summary>
+        ''' The gammaLn method takes a double x as an input and returns the logarithmic result of the gamma distribution at point x.
+        ''' </summary>
+        ''' <param name="x"></param>
+        ''' <returns>the logarithmic result of the gamma distribution at point x.</returns>
         Public Shared Function GammaLn(x As Double) As Double
             Dim cof = {76.180091729471457, -86.505320329416776, 24.014098240830911, -1.231739572450155, 0.001208650973866179, -0.000005395239384953}
             Dim j As Integer
@@ -142,20 +120,12 @@ Namespace Distributions
             Return -tmp + System.Math.Log(2.5066282746310007 * ser / x)
         End Function
 
-        ' *
-        '  
-        ' <summary>The zNormal method performs the Z-Normalization. It ensures, that all elements of the input vector are transformed
-        '  into the output vector whose mean is approximately 0 while the standard deviation is in a range close to 1.</summary>
-        ' 
-        ' 
-        '  
-        ' <param name="z">double input.</param>
-        ' 
-        '  
-        ' <returns>normalized value of given input.</returns>
-        ' 
-        ' 
-
+        ''' <summary>
+        ''' The zNormal method performs the Z-Normalization. It ensures, that all elements of the input vector are transformed
+        ''' into the output vector whose mean is approximately 0 while the standard deviation is in a range close to 1.
+        ''' </summary>
+        ''' <param name="z"></param>
+        ''' <returns>normalized value of given input.</returns>
         Public Shared Function ZNormal(z As Double) As Double
             Dim x As Double
             If z = 0.0 Then
@@ -182,19 +152,11 @@ Namespace Distributions
             Return (1.0 - x) * 0.5
         End Function
 
-        ' *
-        '  
-        ' <summary>he zInverse method returns the Z-Inverse of given probability value.</summary>
-        ' 
-        ' 
-        '  
-        ' <param name="p">double probability.</param>
-        ' 
-        '  
-        ' <returns>the Z-Inverse of given probability.</returns>
-        ' 
-        ' 
-
+        ''' <summary>
+        ''' the zInverse method returns the Z-Inverse of given probability value.
+        ''' </summary>
+        ''' <param name="p">double probability.</param>
+        ''' <returns>the Z-Inverse of given probability.</returns>
         Public Shared Function ZInverse(p As Double) As Double
             Dim minZ = -Z_MAX
             Dim maxZ = Z_MAX
@@ -276,22 +238,12 @@ Namespace Distributions
             Return s
         End Function
 
-        ' *
-        '  
-        ' <summary>The chiSquareInverse method returns the Chi Square-Inverse of given probability value with given degree of freedom.</summary>
-        ' 
-        ' 
-        '  
-        ' <param name="p">      double probability.</param>
-        ' 
-        '  
-        ' <param name="freedom">integer input for degrees of freedom.</param>
-        ' 
-        '  
-        ' <returns>the chiSquare-Inverse of given probability.</returns>
-        ' 
-        ' 
-
+        ''' <summary>
+        ''' The chiSquareInverse method returns the Chi Square-Inverse of given probability value with given degree of freedom.
+        ''' </summary>
+        ''' <param name="p">double probability.</param>
+        ''' <param name="freedom">integer input for degrees of freedom.</param>
+        ''' <returns>the chiSquare-Inverse of given probability.</returns>
         Public Shared Function ChiSquareInverse(p As Double, freedom As Integer) As Double
             Dim minChiSquare = 0.0
             Dim maxChiSquare = CHI_MAX
@@ -317,26 +269,14 @@ Namespace Distributions
             Return chiSquareValue
         End Function
 
-        ' *
-        '  
-        ' <summary>The fDistribution method is used to observe whether two samples have the same variance. It takes a double input F
-        '  and two integer freedom1 and freedom2 for degrees of freedom as inputs. It returns the F-Distribution result.</summary>
-        ' 
-        ' 
-        '  
-        ' <param name="fValue">       double input.</param>
-        ' 
-        '  
-        ' <param name="freedom1">integer input for degrees of freedom.</param>
-        ' 
-        '  
-        ' <param name="freedom2">integer input for degrees of freedom.</param>
-        ' 
-        '  
-        ' <returns>the F-Distribution result.</returns>
-        ' 
-        ' 
-
+        ''' <summary>
+        ''' The fDistribution method is used to observe whether two samples have the same variance. It takes a double input F
+        ''' and two integer freedom1 and freedom2 for degrees of freedom as inputs. It returns the F-Distribution result.
+        ''' </summary>
+        ''' <param name="fValue"></param>
+        ''' <param name="freedom1">integer input for degrees of freedom.</param>
+        ''' <param name="freedom2">integer input for degrees of freedom.</param>
+        ''' <returns>the F-Distribution result.</returns>
         Public Shared Function FDistribution(fValue As Double, freedom1 As Integer, freedom2 As Integer) As Double
             Dim i, j As Integer
             Dim y, d, p As Double
@@ -401,25 +341,13 @@ Namespace Distributions
             Return 1.0 - p
         End Function
 
-        ' *
-        '  
-        ' <summary>The fDistributionInverse method returns the F-Distribution Inverse of given probability value.</summary>
-        ' 
-        ' 
-        '  
-        ' <param name="p">       double probability.</param>
-        ' 
-        '  
-        ' <param name="freedom1">integer input for degrees of freedom.</param>
-        ' 
-        '  
-        ' <param name="freedom2">integer input for degrees of freedom.</param>
-        ' 
-        '  
-        ' <returns>the F-Distribution Inverse of given probability.</returns>
-        ' 
-        ' 
-
+        ''' <summary>
+        ''' The fDistributionInverse method returns the F-Distribution Inverse of given probability value.
+        ''' </summary>
+        ''' <param name="p">double probability.</param>
+        ''' <param name="freedom1">integer input for degrees of freedom.</param>
+        ''' <param name="freedom2">integer input for degrees of freedom.</param>
+        ''' <returns>the F-Distribution Inverse of given probability.</returns>
         Public Shared Function FDistributionInverse(p As Double, freedom1 As Integer, freedom2 As Integer) As Double
             Dim maxF = F_MAX
             Dim minF = 0.0
@@ -445,23 +373,13 @@ Namespace Distributions
             Return fValue
         End Function
 
-        ' *
-        '  
-        ' <summary>The tDistribution method is used instead of the normal distribution when there is small samples. It takes a double input T
-        '  and an integer freedom for degree of freedom as inputs. It returns the T-Distribution result by using F-Distribution method.</summary>
-        ' 
-        ' 
-        '  
-        ' <param name="T">      double input.</param>
-        ' 
-        '  
-        ' <param name="freedom">integer input for degrees of freedom.</param>
-        ' 
-        '  
-        ' <returns>the T-Distribution result.</returns>
-        ' 
-        ' 
-
+        ''' <summary>
+        ''' The tDistribution method is used instead of the normal distribution when there is small samples. It takes a double input T
+        ''' and an integer freedom for degree of freedom as inputs. It returns the T-Distribution result by using F-Distribution method.
+        ''' </summary>
+        ''' <param name="T"></param>
+        ''' <param name="freedom">integer input for degrees of freedom.</param>
+        ''' <returns>the T-Distribution result.</returns>
         Public Shared Function TDistribution(T As Double, freedom As Integer) As Double
             If T >= 0 Then
                 Return FDistribution(T * T, 1, freedom) / 2
@@ -470,22 +388,12 @@ Namespace Distributions
             End If
         End Function
 
-        ' *
-        '  
-        ' <summary>The tDistributionInverse method returns the T-Distribution Inverse of given probability value.</summary>
-        ' 
-        ' 
-        '  
-        ' <param name="p">      double probability.</param>
-        ' 
-        '  
-        ' <param name="freedom">integer input for degrees of freedom.</param>
-        ' 
-        '  
-        ' <returns>the T-Distribution Inverse of given probability.</returns>
-        ' 
-        ' 
-
+        ''' <summary>
+        ''' The tDistributionInverse method returns the T-Distribution Inverse of given probability value.
+        ''' </summary>
+        ''' <param name="p">double probability.</param>
+        ''' <param name="freedom">integer input for degrees of freedom.</param>
+        ''' <returns>the T-Distribution Inverse of given probability.</returns>
         Public Shared Function TDistributionInverse(p As Double, freedom As Integer) As Double
             If p < 0.5 Then
                 Return System.Math.Sqrt(FDistributionInverse(p * 2, 1, freedom))
