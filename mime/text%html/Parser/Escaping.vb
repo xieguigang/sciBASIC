d1a330a5ac88b9d1380a880f76964e467dfa5aac
guigang xie <xie.guigang@gcmodeller.org>
2023-04-19 13:42:21 +0800

    make bugs fixed for parse the javascript text from html document

--- mime/text%html/Parser/Escaping.vb
+++ mime/text%html/Parser/Escaping.vb
@@ -1,54 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::3de1dcd3c85a4ff5f4aa502544b62980, sciBASIC#\mime\text%html\Parser\Escaping.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 10
-    '    Code Lines: 7
-    ' Comment Lines: 0
-    '   Blank Lines: 3
-    '     File Size: 182 B
+' Summaries:
 
 
-    '     Class Escaping
-    ' 
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 10
+'    Code Lines: 7
+' Comment Lines: 0
+'   Blank Lines: 3
+'     File Size: 182 B
+
+
+'     Class Escaping
+' 
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
+
+Imports Microsoft.VisualBasic.Text.Parser
 
 Namespace Language
 
@@ -57,6 +59,12 @@ Namespace Language
         Public [string] As Boolean
         Public quote As Char
         Public tagOpen As Boolean
+        ''' <summary>
+        ''' the html script tag is a special tag 
+        ''' </summary>
+        Public scriptOpen As Boolean
+
+        Public checkScriptEnd As New CharBuffer
 
     End Class
 End Namespace
--- mime/text%html/Parser/TokenIcer.vb
+++ mime/text%html/Parser/TokenIcer.vb
@@ -1,53 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::a8baf821610ad8baf3062dfaaf1a5818, sciBASIC#\mime\text%html\Parser\TokenIcer.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 101
-    '    Code Lines: 83
-    ' Comment Lines: 0
-    '   Blank Lines: 18
-    '     File Size: 3.31 KB
+' Summaries:
 
 
-    '     Class TokenIcer
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: GetTokens, MeasureToken, WalkChar
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 101
+'    Code Lines: 83
+' Comment Lines: 0
+'   Blank Lines: 18
+'     File Size: 3.31 KB
+
+
+'     Class TokenIcer
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: GetTokens, MeasureToken, WalkChar
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -61,6 +61,7 @@ Namespace Language
         Dim buf As New CharBuffer
         Dim text As CharPtr
         Dim escape As New Escaping
+        Dim lastToken As Token
 
         Sub New(document As CharPtr)
             Me.text = document
@@ -70,6 +71,7 @@ Namespace Language
             Do While text
                 For Each t As Token In WalkChar(++text)
                     If Not t Is Nothing Then
+                        lastToken = t
                         Yield t
                     End If
                 Next
@@ -92,6 +94,26 @@ Namespace Language
                     escape.string = False
                     escape.quote = Nothing
                 Else
+                    buf += c
+                End If
+            ElseIf escape.scriptOpen Then
+                If c = "<"c Then
+                    If escape.checkScriptEnd = 0 Then
+                        escape.checkScriptEnd += c
+                    End If
+                ElseIf c = "/"c Then
+                    If escape.checkScriptEnd = "<" Then
+                        escape.checkScriptEnd *= 0
+                        escape.scriptOpen = False
+
+                        If buf > 0 Then
+                            Yield MeasureToken(New String(buf.PopAllChars))
+                            Yield New Token(HtmlTokens.openTag, "<")
+                            Yield New Token(HtmlTokens.splash, "/")
+                        End If
+                    End If
+                Else
+                    escape.checkScriptEnd.Clear()
                     buf += c
                 End If
             ElseIf c = "<"c Then
@@ -139,11 +161,15 @@ Namespace Language
         End Function
 
         Private Function MeasureToken(text As String) As Token
+            If lastToken IsNot Nothing AndAlso lastToken = (HtmlTokens.openTag, "<") AndAlso text.TextEquals("script") Then
+                escape.scriptOpen = True
+            End If
+
             If text = "=" Then
                 Return New Token(HtmlTokens.equalsSymbol, "=")
             ElseIf text = vbCr OrElse text = vbLf OrElse text = vbCrLf Then
                 Return Nothing
-            ElseIf text.Trim(" "c, ASCII.CR, Ascii.LF) = "" Then
+            ElseIf text.Trim(" "c, ASCII.CR, ASCII.LF) = "" Then
                 Return Nothing
             Else
                 Return New Token(HtmlTokens.text, text)
