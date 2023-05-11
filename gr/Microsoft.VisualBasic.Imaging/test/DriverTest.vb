ed8378932c91e729a55f29be4ae052e6b4fb1196
guigang xie <xie.guigang@gcmodeller.org>
2023-05-11 15:03:23 +0800

    config for run test

--- gr/Microsoft.VisualBasic.Imaging/test/DriverTest.vb
+++ gr/Microsoft.VisualBasic.Imaging/test/DriverTest.vb
@@ -102,7 +102,6 @@ Module DriverTest
     ''' <param name="A!"></param>
     ''' <param name="b!"></param>
     ''' <returns></returns>
-   ' <Driver("test.plot")>
     Public Function testPlot(A!, b!, Optional bg$ = "1234", Optional testFont$ = CSSFont.Win7LargerBold) As GraphicsData
         Call Console.WriteLine(A)
         Call Console.WriteLine(b)
