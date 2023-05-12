6cdc245b52ebc1a4e2669dd4fa20c3b9e5e9ba4b
guigang xie <xie.guigang@gcmodeller.org>
2023-05-12 17:14:35 +0800

    fix of the image library reference error

--- gr/Microsoft.VisualBasic.Imaging/Drivers/Exif/JpegMetadataAdapter.vb
+++ gr/Microsoft.VisualBasic.Imaging/Drivers/Exif/JpegMetadataAdapter.vb
@@ -1,63 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::a07d5bf90e33773db4dfd97fd079ffd8, sciBASIC#\gr\Microsoft.VisualBasic.Imaging\Drivers\Exif\JpegMetadataAdapter.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 165
-    '    Code Lines: 113
-    ' Comment Lines: 16
-    '   Blank Lines: 36
-    '     File Size: 6.33 KB
+' Summaries:
 
 
-    '     Class JpegMetadataAdapter
-    ' 
-    '         Properties: Metadata
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: CreateJpegBitmapEncoderWithMetadata, CreateMetadata, ReadMetadata, Save, TryPadAndSave
-    '                   TrySave
-    ' 
-    '         Sub: PadAndSave, SetMetadata
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 165
+'    Code Lines: 113
+' Comment Lines: 16
+'   Blank Lines: 36
+'     File Size: 6.33 KB
+
+
+'     Class JpegMetadataAdapter
+' 
+'         Properties: Metadata
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: CreateJpegBitmapEncoderWithMetadata, CreateMetadata, ReadMetadata, Save, TryPadAndSave
+'                   TrySave
+' 
+'         Sub: PadAndSave, SetMetadata
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-#If netcore5=0
+#If NET48 Then
 
 Imports System.Collections.ObjectModel
 Imports System.IO
@@ -221,4 +221,4 @@ Namespace Driver
         End Sub
     End Class
 End Namespace
-#end if
+#End If
