e28da5eb6da6fdfeccbcd4845d7a8e66c4671ef2
guigang xie <xie.guigang@gcmodeller.org>
2023-05-26 11:03:23 +0800

    create axis maps

---
+++ gr/Microsoft.VisualBasic.Imaging/d3js/scale/MapperTypes.vb
@@ -0,0 +1,21 @@
+﻿
+Namespace d3js.scale
+
+    ''' <summary>
+    ''' 从graphics的属性值到相应的图形属性(大小，颜色，字体，形状)的映射操作类型
+    ''' </summary>
+    Public Enum MapperTypes
+        ''' <summary>
+        ''' 连续的数值型的映射
+        ''' </summary>
+        Continuous
+        ''' <summary>
+        ''' 离散的分类映射
+        ''' </summary>
+        Discrete
+        ''' <summary>
+        ''' 直接映射
+        ''' </summary>
+        Passthrough
+    End Enum
+End Namespace
--- gr/network-visualization/Visualizer/Styling/Expression/MapExpression.vb
+++ gr/network-visualization/Visualizer/Styling/Expression/MapExpression.vb
@@ -65,24 +65,6 @@ Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace Styling
 
-    ''' <summary>
-    ''' 从graph的属性值到相应的图形属性(节点大小，颜色，字体，形状)的映射操作类型
-    ''' </summary>
-    Public Enum MapperTypes
-        ''' <summary>
-        ''' 连续的数值型的映射
-        ''' </summary>
-        Continuous
-        ''' <summary>
-        ''' 离散的分类映射
-        ''' </summary>
-        Discrete
-        ''' <summary>
-        ''' 直接映射
-        ''' </summary>
-        Passthrough
-    End Enum
-
     Public Structure MapExpression
 
         Dim propertyName As String
