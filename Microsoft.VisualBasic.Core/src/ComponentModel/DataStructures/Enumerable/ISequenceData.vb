a0c175ab85c23429334ac8544305300b429d99f8
guigang xie <xie.guigang@gcmodeller.org>
2023-06-06 16:58:48 +0800

    make improvements of the sequence data model

---
+++ Microsoft.VisualBasic.Core/src/ComponentModel/DataStructures/Enumerable/ISequenceData.vb
@@ -0,0 +1,18 @@
+﻿Namespace ComponentModel.Collection.Generic
+
+    ''' <summary>
+    ''' The sequence analysis data model, example as biological sequence, 
+    ''' time signal, time sequence anaysis
+    ''' </summary>
+    ''' <typeparam name="T"></typeparam>
+    Public Interface ISequenceData(Of T, List As IEnumerable(Of T))
+
+        ''' <summary>
+        ''' the sequence data provider, example like time serial signals, 
+        ''' logging data, biological sequence
+        ''' </summary>
+        ''' <returns></returns>
+        ReadOnly Property SequenceData As List
+
+    End Interface
+End Namespace
