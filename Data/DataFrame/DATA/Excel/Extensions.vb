fe5a3ace7948f6a8a2b6fbeb1c6e92d58ec63e46
guigang xie <xie.guigang@gcmodeller.org>
2023-06-08 21:53:34 +0800

    Update Extensions.vb

--- Data/DataFrame/DATA/Excel/Extensions.vb
+++ Data/DataFrame/DATA/Excel/Extensions.vb
@@ -1,61 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::a1b5f7e83286635a53c219e7f658f7a4, sciBASIC#\Data\DataFrame\DATA\Excel\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 93
-    '    Code Lines: 48
-    ' Comment Lines: 34
-    '   Blank Lines: 11
-    '     File Size: 3.58 KB
+' Summaries:
 
 
-    '     Module Extensions
-    ' 
-    '         Function: LoadDataSet, LoadEntitySet, (+2 Overloads) ReadXlsx
-    ' 
-    '         Sub: doUpdateMaps
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 93
+'    Code Lines: 48
+' Comment Lines: 34
+'   Blank Lines: 11
+'     File Size: 3.58 KB
+
+
+'     Module Extensions
+' 
+'         Function: LoadDataSet, LoadEntitySet, (+2 Overloads) ReadXlsx
+' 
+'         Sub: doUpdateMaps
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+#If Not NETCOREAPP Then
+
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.csv.IO
-
-#If Not NETCOREAPP Then
 
 Namespace Excel
 
