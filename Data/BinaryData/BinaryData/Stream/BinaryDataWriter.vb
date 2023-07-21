5a8048cfd56070cb92fb8da514706647a10fcb20
guigang xie <xie.guigang@gcmodeller.org>
2023-07-21 17:45:01 +0800

    Update BinaryDataWriter.vb

--- Data/BinaryData/BinaryData/Stream/BinaryDataWriter.vb
+++ Data/BinaryData/BinaryData/Stream/BinaryDataWriter.vb
@@ -1,63 +1,62 @@
 ﻿#Region "Microsoft.VisualBasic::026e10a66b9a31243c1d7cfbe508d1e4, sciBASIC#\Data\BinaryData\BinaryData\Stream\BinaryDataWriter.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 592
-    '    Code Lines: 282
-    ' Comment Lines: 252
-    '   Blank Lines: 58
-    '     File Size: 24.82 KB
+' Summaries:
 
 
-    ' Class BinaryDataWriter
-    ' 
-    '     Properties: ByteOrder, Encoding, Position, RerouteInt32ToUnsigned
-    ' 
-    '     Constructor: (+5 Overloads) Sub New
-    ' 
-    '     Function: DecimalToBytes, ReserveOffset, (+2 Overloads) Seek, (+2 Overloads) TemporarySeek, ToString
-    '               (+2 Overloads) Write, WriteByteLengthPrefixString, WriteDwordLengthPrefixString, WriteNoPrefixOrTerminationString, WriteWordLengthPrefixString
-    '               WriteZeroTerminatedString
-    ' 
-    '     Sub: Align, Finalize, (+22 Overloads) Write, WriteMultiple, WriteReversed
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 592
+'    Code Lines: 282
+' Comment Lines: 252
+'   Blank Lines: 58
+'     File Size: 24.82 KB
+
+
+' Class BinaryDataWriter
+' 
+'     Properties: ByteOrder, Encoding, Position, RerouteInt32ToUnsigned
+' 
+'     Constructor: (+5 Overloads) Sub New
+' 
+'     Function: DecimalToBytes, ReserveOffset, (+2 Overloads) Seek, (+2 Overloads) TemporarySeek, ToString
+'               (+2 Overloads) Write, WriteByteLengthPrefixString, WriteDwordLengthPrefixString, WriteNoPrefixOrTerminationString, WriteWordLengthPrefixString
+'               WriteZeroTerminatedString
+' 
+'     Sub: Align, Finalize, (+22 Overloads) Write, WriteMultiple, WriteReversed
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Diagnostics
 Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
