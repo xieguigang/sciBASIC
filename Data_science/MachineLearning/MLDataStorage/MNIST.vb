d7aa42db73de8a4b3e9d0943247216d8728fe2b8
guigang xie <xie.guigang@gcmodeller.org>
2023-08-08 14:42:10 +0800

    try to extract the raw dataset

--- Data_science/MachineLearning/MLDataStorage/MNIST.vb
+++ Data_science/MachineLearning/MLDataStorage/MNIST.vb
@@ -5,11 +5,19 @@ Imports System.Runtime.InteropServices
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports std = System.Math
 
-Public Module MNIST
+Public Class MNIST : Implements IDisposable
 
-    Public Iterator Function ExtractImages(imagesFile As String, labelsFile As String) As IEnumerable(Of NamedValue(Of Image))
-        Dim imageReader As BinaryReader = New BinaryReader(New FileStream(imagesFile, FileMode.Open))
-        Dim labelReader As BinaryReader = New BinaryReader(New FileStream(labelsFile, FileMode.Open))
+    Private disposedValue As Boolean
+
+    Dim imageReader As BinaryReader
+    Dim labelReader As BinaryReader
+    Dim count As Integer
+    Dim rows, columns As Integer
+    Dim bytes As Integer
+
+    Sub New(imagesFile As String, labelsFile As String)
+        imageReader = New BinaryReader(New FileStream(imagesFile, FileMode.Open))
+        labelReader = New BinaryReader(New FileStream(labelsFile, FileMode.Open))
 
         If ReadInt(imageReader) <> 2051 OrElse ReadInt(labelReader) <> 2049 Then
             Throw New Exception("Invalid magic number.")
@@ -20,26 +28,23 @@ Public Module MNIST
 
         If count1 <> count2 Then
             Throw New Exception("Images and Labels count mismatched.")
+        Else
+            count = count1
         End If
 
-        Dim count As Integer = count1
-        Dim rows = ReadInt(imageReader)
-        Dim columns = ReadInt(imageReader)
+        rows = ReadInt(imageReader)
+        columns = ReadInt(imageReader)
+    End Sub
 
+    Public Iterator Function ExtractImages(imagesFile As String, labelsFile As String) As IEnumerable(Of NamedValue(Of Image))
         For i As Integer = 0 To count - 1
-            Dim label As Byte = labelReader.ReadByte()
             Dim image As Bitmap = New Bitmap(columns, rows)
             Dim rect As Rectangle = New Rectangle(0, 0, image.Width, image.Height)
             Dim data = image.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb)
             Dim ptr = data.Scan0
             Dim bytes = std.Abs(data.Stride) * image.Height
-            Dim rgbValues = New Byte(bytes - 1) {}
+
             Marshal.Copy(ptr, rgbValues, 0, bytes)
-            For j = 0 To rows * columns - 1
-                rgbValues(j * 3 + 2) = imageReader.ReadByte()
-                rgbValues(j * 3 + 1) = rgbValues(j * 3 + 2)
-                rgbValues(j * 3) = rgbValues(j * 3 + 1)
-            Next
             Marshal.Copy(rgbValues, 0, ptr, bytes)
             image.UnlockBits(data)
 
@@ -51,11 +56,50 @@ Public Module MNIST
         Next
     End Function
 
-    Private Function ReadInt(reader As BinaryReader) As Integer
+    Private Function ExtractRaw() As NamedCollection(Of Byte)
+        Dim label As Byte = labelReader.ReadByte()
+        Dim rgbValues = New Byte(rows * columns - 1) {}
+        Dim labelStr As String = $"{label}-{labelReader.BaseStream.Position}"
+
+        For j As Integer = 0 To rows * columns - 1
+            rgbValues(j) = imageReader.ReadByte()
+        Next
+
+        Return New NamedCollection(Of Byte)(labelStr, rgbValues, description:=label.ToString)
+    End Function
+
+    Private Shared Function ReadInt(reader As BinaryReader) As Integer
         Dim value As Byte() = reader.ReadBytes(4)
         If BitConverter.IsLittleEndian Then
             Call Array.Reverse(value)
         End If
         Return BitConverter.ToInt32(value, 0)
     End Function
-End Module
+
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: 释放托管状态(托管对象)
+                imageReader.Dispose()
+                labelReader.Dispose()
+            End If
+
+            ' TODO: 释放未托管的资源(未托管的对象)并重写终结器
+            ' TODO: 将大型字段设置为 null
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: 仅当“Dispose(disposing As Boolean)”拥有用于释放未托管资源的代码时才替代终结器
+    ' Protected Overrides Sub Finalize()
+    '     ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
+End Class
