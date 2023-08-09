df75d92b82ba63afac7fe8a2d3c624bd6ff7ac2a
guigang xie <xie.guigang@gcmodeller.org>
2023-08-09 16:52:02 +0800

    add linear model

---
+++ Data_science/MachineLearning/DeepLearning/Layers/Linear.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
+
+Namespace Math
+
+    ''' <summary>
+    ''' implements torch.nn.Linear
+    ''' </summary>
+    Public Class Linear
+
+        Dim w As NumericMatrix
+        Dim b As Double
+
+        Sub New(in_features As Integer, out_features As Integer, Optional bias As Boolean = True)
+            w = New NumericMatrix(in_features, out_features)
+        End Sub
+
+        Public Function Fit(x As Vector) As Vector
+            Return w.DotMultiply(x) + b
+        End Function
+    End Class
+End Namespace
---
+++ Data_science/MachineLearning/VAE/Class1.vb
@@ -0,0 +1,4 @@
+﻿Public Class Class1
+
+
+End Class
