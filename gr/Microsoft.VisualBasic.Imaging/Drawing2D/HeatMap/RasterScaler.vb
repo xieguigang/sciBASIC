6aa08cc5e37d954afe7289b7f1a1e7321d6b22c3
guigang xie <xie.guigang@gcmodeller.org>
2023-08-14 11:37:57 +0800

    raster helper

--- gr/Microsoft.VisualBasic.Imaging/Drawing2D/HeatMap/RasterScaler.vb
+++ gr/Microsoft.VisualBasic.Imaging/Drawing2D/HeatMap/RasterScaler.vb
@@ -1,56 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::704df0d2f8dd6d2ea82cf98f24382d31, sciBASIC#\gr\Microsoft.VisualBasic.Imaging\Drawing2D\HeatMap\RasterScaler.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 139
-    '    Code Lines: 83
-    ' Comment Lines: 33
-    '   Blank Lines: 23
-    '     File Size: 5.35 KB
+' Summaries:
 
 
-    '     Class RasterScaler
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    ' 
-    '         Function: (+2 Overloads) Scale
-    ' 
-    '         Sub: (+2 Overloads) Dispose, Scale, ScaleTo
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 139
+'    Code Lines: 83
+' Comment Lines: 33
+'   Blank Lines: 23
+'     File Size: 5.35 KB
+
+
+'     Class RasterScaler
+' 
+'         Constructor: (+2 Overloads) Sub New
+' 
+'         Function: (+2 Overloads) Scale
+' 
+'         Sub: (+2 Overloads) Dispose, Scale, ScaleTo
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -91,6 +91,30 @@ Namespace Drawing2D.HeatMap
         Public Sub ScaleTo(g As IGraphics, region As Rectangle)
             Call Scale(g, region.Size, region.Location)
         End Sub
+
+        ''' <summary>
+        ''' Get grayscale raster data
+        ''' </summary>
+        ''' <returns></returns>
+        Public Iterator Function GetRasterData() As IEnumerable(Of PixelData)
+            Dim w = buffer.Width
+            Dim h = buffer.Height
+            Dim c As Color
+            Dim l As Double
+
+            For x As Integer = 0 To buffer.Width - 1
+                For y As Integer = 0 To buffer.Height - 1
+                    c = buffer.GetPixel(x, y)
+                    l = c.GetBrightness
+
+                    Yield New PixelData With {
+                        .Scale = l,
+                        .X = x + 1,
+                        .Y = y + 1
+                    }
+                Next
+            Next
+        End Function
 
         Public Function Scale(newWidth As Integer, newHeight As Integer) As Bitmap
             Dim canvas As IGraphics = New Size(newWidth, newHeight).CreateGDIDevice(filled:=Color.Transparent)
