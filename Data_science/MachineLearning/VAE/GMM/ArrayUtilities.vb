80b5e2dff8a30f84ecbb4779ea169e0041b37418
guigang xie <xie.guigang@gcmodeller.org>
2023-08-15 11:22:14 +0800

    make improvements of the vector math supports

--- Data_science/MachineLearning/VAE/GMM/ArrayUtilities.vb
+++ Data_science/MachineLearning/VAE/GMM/ArrayUtilities.vb
@@ -1,54 +1,32 @@
-﻿Imports ext = Microsoft.VisualBasic.Linq.Extensions
+﻿Imports Microsoft.VisualBasic.Math.Correlations
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports ext = Microsoft.VisualBasic.Linq.Extensions
 Imports std = System.Math
 
 Namespace GMM.EMGaussianMixtureModel
 
     Public Module ArrayUtilities
 
-        Public Function sumList(X As IList(Of Double), Y As IList(Of Double)) As IList(Of Double)
-            If X Is Nothing OrElse Y Is Nothing Then
-                Throw New NullReferenceException("one of the submitted Lists is null, cannot add Lists")
-            End If
-            ' Debug.Assert(X.Count == Y.Count);
-            Dim results As IList(Of Double) = New List(Of Double)()
-            For i = 0 To X.Count - 1
-                results.Add(X(i) + Y(i))
-            Next
-            Return results
-        End Function
-
-        Public Function multiplicationScalar(X As IList(Of Double), j As Double) As IList(Of Double)
-            Dim results As IList(Of Double) = New List(Of Double)()
-            For Each xi In X
-                results.Add(xi * j)
-            Next
-
-            Return results
-        End Function
-
-        Public Function multiplicationScalar(X As Double(), j As Double) As Double()
-            Dim results = New Double(X.Length - 1) {}
-            For i = 0 To X.Length - 1
-                results(i) = X(i) * j
-            Next
-            Return results
-        End Function
-
-
-        Public Function divisionScalar(X As IList(Of Double), j As Double) As IList(Of Double)
+        Public Function divisionScalar(X As IList(Of Double), j As Double) As Double()
             If j = 0.0 Then
                 Throw New ArithmeticException("divisionScalar failed, scalar cannot be 0.0")
             Else
-                Return multiplicationScalar(X, 1 / j)
+                Return New Vector(X) * (1 / j)
             End If
         End Function
 
-        'idea is that we have a list of lists (inner list is like a row), and we sum over all columns.
+        ''' <summary>
+        ''' idea is that we have a list of lists (inner list is like a row), and we sum over all columns.
+        ''' </summary>
+        ''' <param name="input"></param>
+        ''' <returns></returns>
         Public Function columnSum(input As IList(Of IList(Of Double))) As IList(Of Double)
-            Dim results As IList(Of Double) = New List(Of Double)(ext.Repeats(0.0, input(0).Count)) '(Collections.nCopies(input[0].Count, 0.0));
-            For Each i In input
-                results = sumList(results, i)
+            Dim results As IList(Of Double) = New List(Of Double)(ext.Repeats(0.0, input(0).Count))
+
+            For Each i As IList(Of Double) In input
+                results = New Vector(results) + New Vector(i)
             Next
+
             Return results
         End Function
 
@@ -68,19 +46,10 @@ Namespace GMM.EMGaussianMixtureModel
             Return l1Norm(results)
         End Function
 
-        Public Function euclidDist(x As Double(), y As Double()) As Double
-            Debug.Assert(x.Length = y.Length)
-            Dim result = 0.0
-            For i = 0 To x.Length - 1
-                result += std.Pow(x(i) - y(i), 2)
-            Next
-            Return std.Sqrt(result)
-        End Function
-
         Public Function distToCenterL1(x As Double(), centers As IList(Of Double())) As IList(Of Double)
             Dim results As IList(Of Double) = New List(Of Double)()
-            For Each center In centers
-                results.Add(euclidDist(x, center))
+            For Each center As Double() In centers
+                results.Add(x.EuclideanDistance(center))
             Next
             Return l1Norm(results)
         End Function
