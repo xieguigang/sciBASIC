e080b829879dbe266e8e242763d8dcf8bd41a0b7
guigang xie <xie.guigang@gcmodeller.org>
2023-08-21 11:04:15 +0800

    create spatial mapping formula

--- Microsoft.VisualBasic.Core/src/Extensions/Image/Math/Polygon2D.vb
+++ Microsoft.VisualBasic.Core/src/Extensions/Image/Math/Polygon2D.vb
@@ -1,65 +1,66 @@
 ﻿#Region "Microsoft.VisualBasic::ea2c286f5c5ebd639fa5d65e593dbd7a, sciBASIC#\Microsoft.VisualBasic.Core\src\Extensions\Image\Math\Polygon2D.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 326
-    '    Code Lines: 216
-    ' Comment Lines: 59
-    '   Blank Lines: 51
-    '     File Size: 11.64 KB
+' Summaries:
 
 
-    '     Class Polygon2D
-    ' 
-    '         Properties: height, length, width, xpoints, ypoints
-    ' 
-    '         Constructor: (+8 Overloads) Sub New
-    ' 
-    '         Function: boundingInside, checkInside, GenericEnumerator, GetArea, GetEnumerator
-    '                   GetRandomPoint, GetRectangle, GetShoelaceArea, (+4 Overloads) inside
-    ' 
-    '         Sub: calculateBounds
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 326
+'    Code Lines: 216
+' Comment Lines: 59
+'   Blank Lines: 51
+'     File Size: 11.64 KB
+
+
+'     Class Polygon2D
+' 
+'         Properties: height, length, width, xpoints, ypoints
+' 
+'         Constructor: (+8 Overloads) Sub New
+' 
+'         Function: boundingInside, checkInside, GenericEnumerator, GetArea, GetEnumerator
+'                   GetRandomPoint, GetRectangle, GetShoelaceArea, (+4 Overloads) inside
+' 
+'         Sub: calculateBounds
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Drawing
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math
 Imports randf = Microsoft.VisualBasic.Math.RandomExtensions
 Imports stdNum = System.Math
 
@@ -145,6 +146,7 @@ Namespace Imaging.Math2D
             Me.xpoints = New Double(length - 1) {}
             Me.ypoints = New Double(length - 1) {}
 
+            ' try to break the clr class object reference at here
             Array.Copy(x, 0, Me.xpoints, 0, length)
             Array.Copy(y, 0, Me.ypoints, 0, length)
 
@@ -448,6 +450,19 @@ Namespace Imaging.Math2D
         Public Shared Widening Operator CType(points As PointF()) As Polygon2D
             Dim x As Double() = points.Select(Function(p) CDbl(p.X)).ToArray
             Dim y As Double() = points.Select(Function(p) CDbl(p.Y)).ToArray
+
+            Return New Polygon2D(x, y)
+        End Operator
+
+        ''' <summary>
+        ''' move current polygon object by a given offset
+        ''' </summary>
+        ''' <param name="p"></param>
+        ''' <param name="offset"></param>
+        ''' <returns></returns>
+        Public Shared Operator +(p As Polygon2D, offset As PointF) As Polygon2D
+            Dim x = SIMD.Add.f64_op_add_f64_scalar(p.xpoints, offset.X)
+            Dim y = SIMD.Add.f64_op_add_f64_scalar(p.ypoints, offset.Y)
 
             Return New Polygon2D(x, y)
         End Operator
