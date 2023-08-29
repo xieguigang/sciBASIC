f57fbab9902167198e32cd4d0ea5a088d8f46bab
guigang xie <xie.guigang@gcmodeller.org>
2023-08-29 13:57:35 +0800

    add stream helper objects for save/load machine learning model

---
+++ Microsoft.VisualBasic.Core/src/Serialization/BinaryDumping/Stream/ObjectInputStream.vb
@@ -0,0 +1,52 @@
+﻿Imports System.IO
+
+Namespace Serialization.BinaryDumping
+
+    Public Class ObjectInputStream : Implements IDisposable
+
+        Dim disposedValue As Boolean
+        Dim stream As Stream
+
+        Sub New(s As Stream)
+            stream = s
+        End Sub
+
+        Public Function ReadObject() As Object
+
+        End Function
+
+        Public Sub Close()
+            stream.Close()
+        End Sub
+
+        Protected Overridable Sub Dispose(disposing As Boolean)
+            If Not disposedValue Then
+                If disposing Then
+                    ' TODO: 释放托管状态(托管对象)
+                    Try
+                        Call stream.Close()
+                    Catch ex As Exception
+
+                    End Try
+                End If
+
+                ' TODO: 释放未托管的资源(未托管的对象)并重写终结器
+                ' TODO: 将大型字段设置为 null
+                disposedValue = True
+            End If
+        End Sub
+
+        ' ' TODO: 仅当“Dispose(disposing As Boolean)”拥有用于释放未托管资源的代码时才替代终结器
+        ' Protected Overrides Sub Finalize()
+        '     ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+        '     Dispose(disposing:=False)
+        '     MyBase.Finalize()
+        ' End Sub
+
+        Public Sub Dispose() Implements IDisposable.Dispose
+            ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+            Dispose(disposing:=True)
+            GC.SuppressFinalize(Me)
+        End Sub
+    End Class
+End Namespace
