f44794730a3eef6fc8f42e4d7500fa584ff4c3ab
guigang xie <xie.guigang@gcmodeller.org>
2023-08-31 11:21:30 +0800

    Create Class1.vb

---
+++ Data_science/MachineLearning/DeepLearning/test/Class1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Class1
+
+    Public Function forward(inX As Double()()()(), in_weight As Double()()()(), in_bias, Optional padding As Integer = 0, Optional stride As Integer = 1)
+        Dim nOutCh, nInCh, nKnRows, nKnCols As Integer
+        nKnRows = in_weight.Length
+        nKnCols = in_weight(0).Length
+        nOutCh = in_weight(0)(0).Length
+        nInCh = in_weight(0)(0)(0).Length
+
+        Dim nImgSamples, nInImgRows, nInImgCols As Integer
+        nImgSamples = inX.Length
+        nInCh = inX(0).Length
+        nInImgRows = inX(0)(0).Length
+        nInImgCols = inX(0)(0)(0).Length
+
+        Dim paddedX = New Double(nImgSamples - 1)(nInCh-1)(nInImgRows+2*padding-1)(nInImgCols+2*padding-1) {}
+
+    End Function
+
+End Class
