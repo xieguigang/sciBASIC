d05834bb16f9c5d7d74cfa89e5ad3042256ff81d
guigang xie <xie.guigang@gcmodeller.org>
2023-09-07 11:24:29 +0800

    add BipartiteMatching

---
+++ Microsoft.VisualBasic.Core/src/ComponentModel/Algorithm/BipartiteMatching.vb
@@ -0,0 +1,44 @@
+﻿Namespace ComponentModel.Algorithm
+
+    Public NotInheritable Class BipartiteMatching
+        Private ReadOnly _v As Integer
+        Private ReadOnly _g As List(Of Integer)()
+
+        Public Sub New(ByVal v As Integer)
+            _v = v
+            _g = Enumerable.Repeat(0, v).[Select](Function(__) New List(Of Integer)()).ToArray()
+        End Sub
+
+        Public Sub AddEdge(ByVal u As Integer, ByVal v As Integer)
+            _g(u).Add(v)
+            _g(v).Add(u)
+        End Sub
+
+        Public Function Match() As Integer
+            Dim res As Integer = 0
+            Dim matches = Enumerable.Repeat(-1, _v).ToArray()
+            For v As Integer = 0 To _v - 1
+                If matches(v) < 0 Then
+                    Dim used = New Boolean(_v - 1) {}
+                    If Dfs(v, used, matches) Then
+                        res += 1
+                    End If
+                End If
+            Next
+            Return res
+        End Function
+
+        Private Function Dfs(ByVal v As Integer, ByVal used As Boolean(), ByVal matches As Integer()) As Boolean
+            used(v) = True
+            For i As Integer = 0 To _g(v).Count - 1
+                Dim u = _g(v)(i), w = matches(u)
+                If w < 0 OrElse Not used(w) AndAlso Dfs(w, used, matches) Then
+                    matches(v) = u
+                    matches(u) = v
+                    Return True
+                End If
+            Next
+            Return False
+        End Function
+    End Class
+End Namespace
