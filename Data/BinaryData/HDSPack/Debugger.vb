58eabb579f654f1e85f8002c91363014b0af3103
guigang xie <xie.guigang@gcmodeller.org>
2023-09-21 12:45:07 +0800

    Update Debugger.vb

--- Data/BinaryData/HDSPack/Debugger.vb
+++ Data/BinaryData/HDSPack/Debugger.vb
@@ -1,53 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::b502105277aa6e6562ee3c266cd7a3c3, sciBASIC#\Data\BinaryData\HDSPack\Debugger.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 89
-    '    Code Lines: 64
-    ' Comment Lines: 13
-    '   Blank Lines: 12
-    '     File Size: 3.37 KB
+' Summaries:
 
 
-    ' Module Debugger
-    ' 
-    '     Function: (+2 Overloads) ListFiles
-    ' 
-    '     Sub: Tree, treeInternal
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 89
+'    Code Lines: 64
+' Comment Lines: 13
+'   Blank Lines: 12
+'     File Size: 3.37 KB
+
+
+' Module Debugger
+' 
+'     Function: (+2 Overloads) ListFiles
+' 
+'     Sub: Tree, treeInternal
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -61,8 +61,8 @@ Public Module Debugger
 
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     <Extension>
-    Public Function ListFiles(hds As StreamPack) As IEnumerable(Of StreamObject)
-        Return New StreamObject() {hds.superBlock}.JoinIterates(hds.superBlock.ListFiles)
+    Public Function ListFiles(hds As StreamPack, Optional recursive As Boolean = True) As IEnumerable(Of StreamObject)
+        Return New StreamObject() {hds.superBlock}.JoinIterates(hds.superBlock.ListFiles(recursive:=recursive))
     End Function
 
     ''' <summary>
@@ -76,7 +76,10 @@ Public Module Debugger
     ''' get all the files/dirs inside current dir object and its child dirs
     ''' </remarks>
     <Extension>
-    Public Iterator Function ListFiles(dir As StreamGroup, Optional safe As Boolean = True) As IEnumerable(Of StreamObject)
+    Public Iterator Function ListFiles(dir As StreamGroup,
+                                       Optional safe As Boolean = True,
+                                       Optional recursive As Boolean = True) As IEnumerable(Of StreamObject)
+
         If dir Is Nothing AndAlso safe Then
             ' empty collection will be generates if safe mode is turn on
             Return
@@ -88,9 +91,11 @@ Public Module Debugger
             Else
                 Yield file
 
-                For Each child As StreamObject In DirectCast(file, StreamGroup).ListFiles
-                    Yield child
-                Next
+                If recursive Then
+                    For Each child As StreamObject In DirectCast(file, StreamGroup).ListFiles
+                        Yield child
+                    Next
+                End If
             End If
         Next
     End Function
