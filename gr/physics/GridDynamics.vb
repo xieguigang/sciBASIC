6fee3de8f4089241b1f607a2ca6f8e50023bd68e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-10-05 10:48:02 +0800

    Create GridDynamics.vb

---
+++ gr/physics/GridDynamics.vb
@@ -0,0 +1,9 @@
+﻿Imports Microsoft.VisualBasic.Data.GraphTheory.GridGraph
+
+Public Module GridDynamics
+
+    Public Function EncodeGrid(Of T)(field As Field, radius As Single) As Grid(Of T)
+
+    End Function
+
+End Module
