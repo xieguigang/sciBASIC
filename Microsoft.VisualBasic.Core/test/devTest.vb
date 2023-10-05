1e7fdb43369e27f6014fa3e7c8be3dac7c43fdba
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-10-05 10:45:44 +0800

    make a grid model for physic simulation?

--- Microsoft.VisualBasic.Core/test/devTest.vb
+++ Microsoft.VisualBasic.Core/test/devTest.vb
@@ -1,55 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::51a188a4442e2f304f2947a1cc5519a4, sciBASIC#\Microsoft.VisualBasic.Core\test\devTest.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 31
-    '    Code Lines: 22
-    ' Comment Lines: 0
-    '   Blank Lines: 9
-    '     File Size: 857 B
+' Summaries:
 
 
-    ' Module devTest
-    ' 
-    '     Sub: logfileTest, Main1
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 31
+'    Code Lines: 22
+' Comment Lines: 0
+'   Blank Lines: 9
+'     File Size: 857 B
+
+
+' Module devTest
+' 
+'     Sub: logfileTest, Main1
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports Microsoft.VisualBasic.ApplicationServices.Development.NetCore5
+Imports Microsoft.VisualBasic.ApplicationServices.Development.NetCoreApp
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 Module devTest
