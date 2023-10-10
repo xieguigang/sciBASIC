222f71f56848fdfe09914088fb5da930342a6067
guigang xie <xie.guigang@gcmodeller.org>
2023-10-10 14:40:42 +0800

    handling of the column width problem

--- Data_science/Mathematica/Math/Math/Algebra/Matrix.NET/SparseMatrix.vb
+++ Data_science/Mathematica/Math/Math/Algebra/Matrix.NET/SparseMatrix.vb
@@ -1,56 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::c13807830971ee41b766524797f373bd, sciBASIC#\Data_science\Mathematica\Math\Math\Algebra\Matrix.NET\SparseMatrix.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 211
-    '    Code Lines: 157
-    ' Comment Lines: 20
-    '   Blank Lines: 34
-    '     File Size: 8.15 KB
+' Summaries:
 
 
-    '     Class SparseMatrix
-    ' 
-    '         Properties: ColumnDimension, RowDimension
-    ' 
-    '         Constructor: (+4 Overloads) Sub New
-    '         Function: ArrayPack, GetMatrix, Resize, RowVectors, ToString
-    '                   Transpose, UnpackData
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 211
+'    Code Lines: 157
+' Comment Lines: 20
+'   Blank Lines: 34
+'     File Size: 8.15 KB
+
+
+'     Class SparseMatrix
+' 
+'         Properties: ColumnDimension, RowDimension
+' 
+'         Constructor: (+4 Overloads) Sub New
+'         Function: ArrayPack, GetMatrix, Resize, RowVectors, ToString
+'                   Transpose, UnpackData
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -240,7 +240,7 @@ Namespace LinearAlgebra.Matrix
             Dim matrix As New Dictionary(Of Integer, Dictionary(Of Integer, Double))
             Dim i As i32 = Scan0
 
-            If maxColumns < 0 Then
+            If maxColumns <= 0 Then
                 maxColumns = xindices.Max + 1
             End If
 
