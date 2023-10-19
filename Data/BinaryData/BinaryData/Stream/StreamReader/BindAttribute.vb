074da23e597e80c9fbbb18ad9f28448b745ed4f5
guigang xie <xie.guigang@gcmodeller.org>
2023-10-19 12:32:30 +0800

    split source files

---
+++ Data/BinaryData/BinaryData/Stream/StreamReader/BindAttribute.vb
@@ -0,0 +1,17 @@
+﻿''' <summary>
+''' bind data type to read/write
+''' </summary>
+<AttributeUsage(AttributeTargets.Method, AllowMultiple:=False, Inherited:=True)>
+Public Class BindAttribute : Inherits Attribute
+
+    Public ReadOnly Property Type As TypeCode
+
+    Sub New(type As TypeCode)
+        Me.Type = type
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return Type.Description
+    End Function
+End Class
+
--- Data/BinaryData/BinaryData/Stream/StreamReader/FieldAttribute.vb
+++ Data/BinaryData/BinaryData/Stream/StreamReader/FieldAttribute.vb
@@ -1,23 +1,6 @@
 ﻿Imports System.Reflection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
 
-''' <summary>
-''' bind data type to read/write
-''' </summary>
-<AttributeUsage(AttributeTargets.Method, AllowMultiple:=False, Inherited:=True)>
-Public Class BindAttribute : Inherits Attribute
-
-    Public ReadOnly Property Type As TypeCode
-
-    Sub New(type As TypeCode)
-        Me.Type = type
-    End Sub
-
-    Public Overrides Function ToString() As String
-        Return Type.Description
-    End Function
-End Class
-
 Public Class FieldAttribute : Inherits Field
 
     Public Property N As Integer
