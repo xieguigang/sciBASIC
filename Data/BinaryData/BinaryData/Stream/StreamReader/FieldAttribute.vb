3496dca7af1a0e2c1348e4fa81c7318487827503
guigang xie <xie.guigang@gcmodeller.org>
2023-10-19 14:11:25 +0800

    make improvements of the debug view

--- Data/BinaryData/BinaryData/Stream/StreamReader/FieldAttribute.vb
+++ Data/BinaryData/BinaryData/Stream/StreamReader/FieldAttribute.vb
@@ -59,4 +59,14 @@ Public Class FieldAttribute : Inherits Field
             Return ReaderProvider.ReadScalar(code)(buf)
         End If
     End Function
+
+    Public Overrides Function ToString() As String
+        Dim offset As String = If(Me.offset >= 0, $"offset:{StringFormats.Lanudry(Me.offset)}", "")
+
+        If ReadArray Then
+            Return $"#{Index} vec; {offset}".Trim
+        Else
+            Return $"#{Index} scalar; {offset}".Trim
+        End If
+    End Function
 End Class
