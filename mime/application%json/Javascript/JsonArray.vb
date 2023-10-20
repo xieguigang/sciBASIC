8de8b228bdaf92300348dab629a0cded42339f4d
guigang xie <xie.guigang@gcmodeller.org>
2023-10-20 13:19:17 +0800

    Update JsonArray.vb

--- mime/application%json/Javascript/JsonArray.vb
+++ mime/application%json/Javascript/JsonArray.vb
@@ -1,61 +1,62 @@
 ﻿#Region "Microsoft.VisualBasic::3eae61b032ae9c4df42eb9ecbb922ce8, sciBASIC#\mime\application%json\Javascript\JsonArray.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 98
-    '    Code Lines: 72
-    ' Comment Lines: 9
-    '   Blank Lines: 17
-    '     File Size: 3.23 KB
+' Summaries:
 
 
-    '     Class JsonArray
-    ' 
-    '         Properties: Length, UnderlyingType
-    ' 
-    '         Constructor: (+4 Overloads) Sub New
-    ' 
-    '         Function: ContainsElement, GetEnumerator, IEnumerable_GetEnumerator, ToString
-    ' 
-    '         Sub: Add, Insert, Remove
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 98
+'    Code Lines: 72
+' Comment Lines: 9
+'   Blank Lines: 17
+'     File Size: 3.23 KB
+
+
+'     Class JsonArray
+' 
+'         Properties: Length, UnderlyingType
+' 
+'         Constructor: (+4 Overloads) Sub New
+' 
+'         Function: ContainsElement, GetEnumerator, IEnumerable_GetEnumerator, ToString
+' 
+'         Sub: Add, Insert, Remove
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Linq
 
 Namespace Javascript
@@ -65,6 +66,10 @@ Namespace Javascript
 
         Friend ReadOnly list As New List(Of JsonElement)
 
+        ''' <summary>
+        ''' the element count in current json array 
+        ''' </summary>
+        ''' <returns></returns>
         Public ReadOnly Property Length As Integer
             Get
                 Return list.Count
@@ -94,25 +99,36 @@ Namespace Javascript
             End Get
         End Property
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Sub New()
         End Sub
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Sub New(objs As IEnumerable(Of JsonElement))
             list = objs.SafeQuery.ToList
         End Sub
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Sub New(values As IEnumerable(Of String))
             Call Me.New(values.SafeQuery.Select(Function(str) New JsonValue(str)))
         End Sub
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Sub New(values As IEnumerable(Of Double))
             Call Me.New(values.Select(Function(d) New JsonValue(d)))
         End Sub
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Sub New(values As IEnumerable(Of Integer))
+            Call Me.New(values.Select(Function(d) New JsonValue(d)))
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Sub Add(element As JsonElement)
             Call list.Add(element)
         End Sub
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Sub Insert(index As Integer, element As JsonElement)
             Call list.Insert(index, element)
         End Sub
