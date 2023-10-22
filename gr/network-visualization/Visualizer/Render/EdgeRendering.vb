6af8ede07a46d1f1f89c5e7a2baf96b536a27d37
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-10-22 10:57:01 +0800

    fix of hte symbol reference error

--- gr/network-visualization/Visualizer/Render/EdgeRendering.vb
+++ gr/network-visualization/Visualizer/Render/EdgeRendering.vb
@@ -1,56 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::e63e010c8aed261b862cdeb48301f785, sciBASIC#\gr\network-visualization\Visualizer\Render\EdgeRendering.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 191
-    '    Code Lines: 152
-    ' Comment Lines: 12
-    '   Blank Lines: 27
-    '     File Size: 7.27 KB
+' Summaries:
 
 
-    ' Class EdgeRendering
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: drawEdges, renderEdge, rendering
-    ' 
-    ' Class LineSegmentRender
-    ' 
-    '     Function: Render
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 191
+'    Code Lines: 152
+' Comment Lines: 12
+'   Blank Lines: 27
+'     File Size: 7.27 KB
+
+
+' Class EdgeRendering
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: drawEdges, renderEdge, rendering
+' 
+' Class LineSegmentRender
+' 
+'     Function: Render
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -58,7 +58,7 @@ Imports System.Drawing
 Imports System.Drawing.Drawing2D
 Imports Microsoft.VisualBasic.ComponentModel.Algorithm.base
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
-Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts.EdgeBundling
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph.EdgeBundling
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.d3js.Layout
 Imports Microsoft.VisualBasic.Imaging.Math2D
