3e0ef3ffec305fb5432c44252cae3a61869de337
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-05 13:19:54 +0800

    removes un-used code

--- Data_science/DataMining/BinaryTree/test/Program.vb
+++ Data_science/DataMining/BinaryTree/test/Program.vb
@@ -1,16 +1,16 @@
-Imports System
-Imports Microsoft.VisualBasic.Data.csv.IO
-Imports Microsoft.VisualBasic.DataMining.BinaryTree
-Imports Microsoft.VisualBasic.DataMining.KMeans
+'Imports System
+'Imports Microsoft.VisualBasic.Data.csv.IO
+'Imports Microsoft.VisualBasic.DataMining.BinaryTree
+'Imports Microsoft.VisualBasic.DataMining.KMeans
 
 Module Program
     Sub Main(args As String())
-        Dim ds As DataSet() = DataSet.LoadDataSet("G:\GCModeller\src\runtime\sciBASIC#\Data_science\DataMining\data\f5dfc234eafd0716742e3b14b72a70c0_umap3.csv").ToArray
-        Dim data As EntityClusterModel() = EntityClusterModel.FromModel(ds).ToArray
-        Dim knn As New KNNGraph(data)
-        Dim test = knn.BuildClusterGraph(k:=16)
+        '        Dim ds As DataSet() = DataSet.LoadDataSet("G:\GCModeller\src\runtime\sciBASIC#\Data_science\DataMining\data\f5dfc234eafd0716742e3b14b72a70c0_umap3.csv").ToArray
+        '        Dim data As EntityClusterModel() = EntityClusterModel.FromModel(ds).ToArray
+        '        Dim knn As New KNNGraph(data)
+        '        Dim test = knn.BuildClusterGraph(k:=16)
 
 
-        Pause()
+        '        Pause()
     End Sub
 End Module
