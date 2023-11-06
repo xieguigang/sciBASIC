04790a5321f097a56d5c55e82e478c80369e9dae
guigang xie <xie.guigang@gcmodeller.org>
2023-11-06 14:40:51 +0800

    make improvements of tghe aggregate

--- Data_science/Mathematica/SignalProcessing/SignalProcessing/Sampler/BinSampler.vb
+++ Data_science/Mathematica/SignalProcessing/SignalProcessing/Sampler/BinSampler.vb
@@ -22,18 +22,29 @@ Public Class BinSampler
         Me.signal = New Signal(time.Select(Function(ti, i) New TimeSignal(ti, intensity(i))))
     End Sub
 
-    Public Function AggregateSignal(dt As Double) As Signal
-        Return New Signal(GetTicks(dt))
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="dt"></param>
+    ''' <param name="aggregate">
+    ''' default is sum intensity data
+    ''' </param>
+    ''' <returns></returns>
+    Public Function AggregateSignal(dt As Double, Optional aggregate As Func(Of IEnumerable(Of Double), Double) = Nothing) As Signal
+        Static sum As Func(Of IEnumerable(Of Double), Double) = AddressOf Enumerable.Sum
+        Return New Signal(GetTicks(dt, If(aggregate, sum)))
     End Function
 
-    Private Iterator Function GetTicks(dt As Double) As IEnumerable(Of TimeSignal)
+    Private Iterator Function GetTicks(dt As Double, aggregate As Func(Of IEnumerable(Of Double), Double)) As IEnumerable(Of TimeSignal)
         For Each box In CutBins.FixedWidthBins(signal, width:=dt, range:=Range, eval:=Function(ti) ti.time)
             If box.Count = 0 Then
                 Continue For
             End If
 
             Dim time As Double = Aggregate ti As TimeSignal In box.Raw Into Average(ti.time)
-            Dim into As Double = Aggregate ti As TimeSignal In box.Raw Into Sum(ti.intensity)
+            Dim into As Double = aggregate(From ti As TimeSignal
+                                           In box.Raw
+                                           Select ti.intensity)
 
             Yield New TimeSignal(time, into)
         Next
