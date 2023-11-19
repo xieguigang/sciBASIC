4c66bb64024af380040625417f8a4e34b7ba0338
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-20 00:32:03 +0800

    fix of the symbol name conflicts

--- Data_science/DataMining/DataMining/Clustering/BisectingKMeans/Cluster.vb
+++ Data_science/DataMining/DataMining/Clustering/BisectingKMeans/Cluster.vb
@@ -10,69 +10,39 @@ Namespace BisectingKMeans
 	Public Class Cluster
 		Private Const CENTROID_THRESHOLD As Double = 0.005
 
-
-		Private cx_Conflict, cy_Conflict As Double
-		Private dataPoints_Conflict As List(Of DataPoint)
-
 		Public Sub New(ByVal cx As Double, ByVal cy As Double)
-			Me.cx_Conflict = cx
-			Me.cy_Conflict = cy
-			dataPoints_Conflict = New List(Of DataPoint)()
+			Me.Cx = cx
+			Me.Cy = cy
+			DataPoints = New List(Of DataPoint)()
 		End Sub
 
 		Public Sub New(ByVal centroid As DataPoint)
-			Me.cx_Conflict = centroid.Dx
-			Me.cy_Conflict = centroid.Dy
-			dataPoints_Conflict = New List(Of DataPoint)()
+			Me.Cx = centroid.Dx
+			Me.Cy = centroid.Dy
+			DataPoints = New List(Of DataPoint)()
 		End Sub
 
 		Public Sub New(ByVal cx As Double, ByVal cy As Double, ByVal dataPoints As List(Of DataPoint))
-			Me.cx_Conflict = cx
-			Me.cy_Conflict = cy
-			Me.dataPoints_Conflict = dataPoints
+			Me.Cx = cx
+			Me.Cy = cy
+			Me.DataPoints = dataPoints
 		End Sub
 
 		Public Overridable Property Cx As Double
-			Get
-				Return cx_Conflict
-			End Get
-			Set(ByVal cx As Double)
-				Me.cx_Conflict = cx
-			End Set
-		End Property
-
-
 		Public Overridable Property Cy As Double
-			Get
-				Return cy_Conflict
-			End Get
-			Set(ByVal cy As Double)
-				Me.cy_Conflict = cy
-			End Set
-		End Property
-
-
 		Public Overridable Property DataPoints As List(Of DataPoint)
-			Get
-				Return dataPoints_Conflict
-			End Get
-			Set(ByVal dataPoints As List(Of DataPoint))
-				Me.dataPoints_Conflict = dataPoints
-			End Set
-		End Property
-
 
 		Public Overridable Sub addPoint(ByVal p As DataPoint)
-			Me.dataPoints_Conflict.Add(p)
+			Me.DataPoints.Add(p)
 		End Sub
 
 
 		Public Overridable ReadOnly Property SSE As Double
 			Get
 				Dim sse_d As Double = 0.0
-				For Each p As DataPoint In dataPoints_Conflict
-					Dim dx As Double = cx_Conflict - p.Dx
-					Dim dy As Double = cy_Conflict - p.Dy
+				For Each p As DataPoint In DataPoints
+					Dim dx As Double = Cx - p.Dx
+					Dim dy As Double = Cy - p.Dy
 					sse_d += (dx * dx + dy * dy)
 				Next p
 				Return sse_d
@@ -80,32 +50,32 @@ Namespace BisectingKMeans
 		End Property
 
 		Public Overridable Function getDistSq(ByVal p As DataPoint) As Double
-			Dim dx As Double = p.Dx - cx_Conflict
-			Dim dy As Double = p.Dy - cy_Conflict
+			Dim dx As Double = p.Dx - Cx
+			Dim dy As Double = p.Dy - Cy
 			Return dx * dx + dy * dy
 		End Function
 
 		Public Overrides Function ToString() As String
-			Return "Cluster{" & "cx=" & cx_Conflict & ", cy=" & cy_Conflict & ", dataPoints=" & dataPoints_Conflict.JoinBy(", ") & "}"c
+			Return "Cluster{" & "cx=" & Cx & ", cy=" & Cy & ", dataPoints=" & DataPoints.JoinBy(", ") & "}"c
 		End Function
 
 		Public Overridable Function updateCentroid() As Boolean
 			Dim sumX As Double = 0.0
 			Dim sumY As Double = 0.0
-			For Each p As DataPoint In dataPoints_Conflict
+			For Each p As DataPoint In DataPoints
 				sumX += p.Dx
 				sumY += p.Dy
 			Next p
-			Dim size As Integer = dataPoints_Conflict.Count
+			Dim size As Integer = DataPoints.Count
 			If size = 0 Then
 				size = 1
 			End If
-			Dim tcx As Double = cx_Conflict
-			cx_Conflict = sumX / size
-			Dim tcy As Double = cy_Conflict
-			cy_Conflict = sumY / size
+			Dim tcx As Double = Cx
+			Cx = sumX / size
+			Dim tcy As Double = Cy
+			Cy = sumY / size
 
-			Return Not (std.Abs(tcx - cx_Conflict) < CENTROID_THRESHOLD AndAlso std.Abs(tcy - cy_Conflict) < CENTROID_THRESHOLD)
+			Return Not (std.Abs(tcx - Cx) < CENTROID_THRESHOLD AndAlso std.Abs(tcy - Cy) < CENTROID_THRESHOLD)
 		End Function
 	End Class
 
--- Data_science/DataMining/DataMining/Clustering/BisectingKMeans/DataPoint.vb
+++ Data_science/DataMining/DataMining/Clustering/BisectingKMeans/DataPoint.vb
@@ -6,52 +6,25 @@
 	''' </summary>
 	Public Class DataPoint
 
-		Private dx_Conflict, dy_Conflict As Double
-		Private clusterNo_Conflict As Integer
-
 		Public Sub New(ByVal dx As Double, ByVal dy As Double)
-			Me.dx_Conflict = dx
-			Me.dy_Conflict = dy
+			Me.Dx = dx
+			Me.Dy = dy
 		End Sub
 
 		Public Sub New(ByVal dx As Double, ByVal dy As Double, ByVal clusterNo As Integer)
-			Me.dx_Conflict = dx
-			Me.dy_Conflict = dy
-			Me.clusterNo_Conflict = clusterNo
+			Me.Dx = dx
+			Me.Dy = dy
+			Me.ClusterNo = clusterNo
 		End Sub
 
 		Public Overridable Property ClusterNo As Integer
-			Get
-				Return clusterNo_Conflict
-			End Get
-			Set(ByVal clusterNo As Integer)
-				Me.clusterNo_Conflict = clusterNo
-			End Set
-		End Property
-
 
 		Public Overridable Property Dx As Double
-			Get
-				Return dx_Conflict
-			End Get
-			Set(ByVal dx As Double)
-				Me.dx_Conflict = dx
-			End Set
-		End Property
-
 
 		Public Overridable Property Dy As Double
-			Get
-				Return dy_Conflict
-			End Get
-			Set(ByVal dy As Double)
-				Me.dy_Conflict = dy
-			End Set
-		End Property
-
 
 		Public Overrides Function ToString() As String
-			Return "DataPoint{" & "dx=" & dx_Conflict & ", dy=" & dy_Conflict & ", clusterNo=" & clusterNo_Conflict & "}"c
+			Return "DataPoint{" & "dx=" & Dx & ", dy=" & Dy & ", clusterNo=" & ClusterNo & "}"c
 		End Function
 	End Class
 
