69d4d3c9e1dcedae5cfaba6e69380a0c058d9301
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-20 00:00:27 +0800

    make improvements of the matrix helper

--- Data_science/DataMining/hierarchical-clustering/hierarchical-clustering/ClusteringAlgorithm/DoCluster.vb
+++ Data_science/DataMining/hierarchical-clustering/hierarchical-clustering/ClusteringAlgorithm/DoCluster.vb
@@ -52,11 +52,13 @@
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language.Default
 Imports Microsoft.VisualBasic.Linq
-Imports Microsoft.VisualBasic.Math.DataFrame
+Imports Microsoft.VisualBasic.Math.Correlations
+Imports Microsoft.VisualBasic.Parallel
 
 Public Module DoCluster
 
@@ -75,16 +77,59 @@ Public Module DoCluster
                                Optional algorithm As ClusteringAlgorithm = Nothing,
                                Optional linkageStrategy As LinkageStrategy = Nothing) As Cluster
 
-        Dim dist As DistanceMatrix = objects.Euclidean
-        Dim distances As Double()() = dist.PopulateRows.Select(Function(r) r.ToArray).ToArray
+        Dim rawdata = objects.ToArray
+        Dim distances As Double()() = rawdata.CreateDistanceMatrix
+        Dim keys As String() = rawdata.Keys
 
         linkageStrategy = linkageStrategy Or DefaultLinkageStrategy
 
         ' with (algorithm or new DefaultClusteringAlgorithm as default) if algorithm is nothing
         With algorithm Or DefaultClusteringAlgorithm  ' (Function(alg) alg Is Nothing)
             ' using (linkageStrategy or new AverageLinkageStrategy as default) if linkageStrategy is nothing
-            Dim cluster As Cluster = .performClustering(distances, dist.keys, linkageStrategy)
+            Dim cluster As Cluster = .performClustering(distances, keys, linkageStrategy)
             Return cluster
         End With
     End Function
+
+    <Extension>
+    Private Function CreateDistanceMatrix(Of DataSet As {INamedValue, DynamicPropertyBase(Of Double)})(objects As DataSet()) As Double()()
+        Dim rawdata As New List(Of Double())
+        Dim keys As String() = objects _
+            .Select(Function(a) a.Properties.Keys) _
+            .IteratesALL _
+            .Distinct _
+            .ToArray
+
+        For Each r As DataSet In objects
+            Call rawdata.Add(r(keys))
+        Next
+
+        Dim par As New EuclideanTask(rawdata.ToArray)
+
+        Return DirectCast(par.Run, EuclideanTask).dist
+    End Function
+
+    Private Class EuclideanTask : Inherits VectorTask
+
+        Public dist As Double()()
+        Public rawdata As Double()()
+
+        Sub New(rawdata As Double()())
+            Call MyBase.New(rawdata.Length)
+
+            Me.dist = New Double(rawdata.Length - 1)() {}
+            Me.rawdata = rawdata
+        End Sub
+
+        Protected Overrides Sub Solve(start As Integer, ends As Integer, cpu_id As Integer)
+            Dim row As Double()
+
+            For i As Integer = start To ends
+                row = rawdata(i)
+                dist(i) = rawdata _
+                    .Select(Function(r) row.EuclideanDistance(r)) _
+                    .ToArray
+            Next
+        End Sub
+    End Class
 End Module
