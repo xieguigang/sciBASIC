bfb5022776e4f8a0e235b552d06ca523111ed665
guigang xie <xie.guigang@gcmodeller.org>
2023-12-14 17:10:59 +0800

    source file renames

---
+++ Data/BinaryData/netCDF/Data/VLen.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Runtime.InteropServices
+
+Namespace Data
+
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure vlen_t
+
+        ''' <summary>
+        ''' size_t
+        ''' </summary>
+        Public len As Integer
+        ''' <summary>
+        ''' void *
+        ''' </summary>
+        Public data As IntPtr
+
+    End Structure
+
+End Namespace
