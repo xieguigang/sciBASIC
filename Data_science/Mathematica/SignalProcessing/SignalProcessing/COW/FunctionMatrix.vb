6e6d3872b71937346e9a93dd6a3ac71fe683a74b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-16 20:51:48 +0800

    disable parallel

--- Data_science/Mathematica/SignalProcessing/SignalProcessing/COW/FunctionMatrix.vb
+++ Data_science/Mathematica/SignalProcessing/SignalProcessing/COW/FunctionMatrix.vb
@@ -1,5 +1,4 @@
 ﻿Imports Microsoft.VisualBasic.ComponentModel.Collection
-Imports Microsoft.VisualBasic.Parallel
 
 Namespace COW
 
@@ -26,34 +25,17 @@ Namespace COW
         End Sub
 
         Public Sub Initialize(segmentNumber As Integer, enabledLength As Integer)
-            Call New InitializeTask(Me).Run()
+            For i = 0 To segmentNumber
+                For j = 0 To enabledLength
+                    Me(i, j) = New FunctionElement(Double.MinValue, 0)
+                Next
+            Next
+
             Me(segmentNumber, enabledLength).Score = 0
         End Sub
 
         Public Overrides Function ToString() As String
             Return $"[{dims.row} x {dims.col}]"
         End Function
-
-        Private Class InitializeTask : Inherits VectorTask
-
-            ReadOnly mat As FunctionMatrix
-
-            Public Sub New(m As FunctionMatrix)
-                MyBase.New(m.funcBeanMatrix.Length)
-                Me.mat = m
-            End Sub
-
-            Protected Overrides Sub Solve(start As Integer, ends As Integer, cpu_id As Integer)
-                For i As Integer = start To ends
-                    Dim v As FunctionElement() = mat.funcBeanMatrix(i)
-
-                    For j As Integer = 0 To v.Length - 1
-                        v(j) = New FunctionElement(Double.MinValue, 0)
-                    Next
-
-                    mat.funcBeanMatrix(i) = v
-                Next
-            End Sub
-        End Class
     End Class
 End Namespace
