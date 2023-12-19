1d84aa303dbbe4c5ec4aa0ecc691d1063d1cb447
guigang xie <xie.guigang@gcmodeller.org>
2023-12-19 11:45:29 +0800

    <DebuggerStepThrough>

--- Data_science/Mathematica/SignalProcessing/SignalProcessing/NDtw/Preprocessing/IPreprocessor.vb
+++ Data_science/Mathematica/SignalProcessing/SignalProcessing/NDtw/Preprocessing/IPreprocessor.vb
@@ -19,18 +19,22 @@ Namespace NDtw.Preprocessing
         Public MustOverride Function Preprocess(data As Double()) As Double()
         Public MustOverride Overrides Function ToString() As String
 
+        <DebuggerStepThrough>
         Public Shared Function Centralization() As CentralizationPreprocessor
             Return New CentralizationPreprocessor
         End Function
 
+        <DebuggerStepThrough>
         Public Shared Function None() As NonePreprocessor
             Return New NonePreprocessor
         End Function
 
+        <DebuggerStepThrough>
         Public Shared Function Normalization() As NormalizationPreprocessor
             Return New NormalizationPreprocessor
         End Function
 
+        <DebuggerStepThrough>
         Public Shared Function Standardization() As StandardizationPreprocessor
             Return New StandardizationPreprocessor
         End Function
