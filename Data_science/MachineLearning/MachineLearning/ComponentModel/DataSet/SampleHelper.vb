ba14e393b36f5e3bc81ca27f1d70da283d0fcad5
guigang xie <xie.guigang@gcmodeller.org>
2023-12-22 15:02:52 +0800

    Create SampleHelper.vb

---
+++ Data_science/MachineLearning/MachineLearning/ComponentModel/DataSet/SampleHelper.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace ComponentModel.StoreProcedure
+
+    <HideModuleName>
+    Public Module SampleHelper
+
+        ''' <summary>
+        ''' get feature dimension
+        ''' </summary>
+        ''' <param name="samples"></param>
+        ''' <param name="[dim]"></param>
+        ''' <returns></returns>
+        <Extension>
+        Public Function dimension(samples As IEnumerable(Of SampleData), [dim] As Integer) As IEnumerable(Of Double)
+            Return samples.Select(Function(si) si.features([dim]))
+        End Function
+
+    End Module
+End Namespace
