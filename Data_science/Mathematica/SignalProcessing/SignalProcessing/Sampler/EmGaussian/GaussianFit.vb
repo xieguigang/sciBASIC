9cbe6913104fbaec30dd768d8ec29d73c2e3cb1f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-25 14:59:18 +0800

    add xml dev notes

--- Data_science/Mathematica/SignalProcessing/SignalProcessing/Sampler/EmGaussian/GaussianFit.vb
+++ Data_science/Mathematica/SignalProcessing/SignalProcessing/Sampler/EmGaussian/GaussianFit.vb
@@ -1,15 +1,22 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Math.Distributions
+Imports std = System.Math
 
 Namespace EmGaussian
 
+    ''' <summary>
+    ''' em-gaussian fit of a single data
+    ''' </summary>
     Public Class GaussianFit
 
+        Dim membership As Double()
+
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
-        Public Function fit(samples, ParamArray components As Double()())
+        Public Function fit(samples As Double(), ParamArray components As Double()()) As Variable()
             Return fit(samples, New Opts With {.components = components})
         End Function
 
-        Public Function fit(samples, Optional opts As Opts = Nothing)
+        Public Function fit(samples As Double(), Optional opts As Opts = Nothing) As Variable()
             Dim detectComponents As Boolean = opts.components.IsNullOrEmpty
             ' initialize components
             Dim cn As Integer = opts.components.Length
@@ -24,10 +31,121 @@ Namespace EmGaussian
                 .ToArray
 
             ' optimize components
+            Dim lastLikelihood As Double = Double.NegativeInfinity
 
+            membership = New Double(components.Length * samples.Length - 1) {}
+
+            For i As Integer = 0 To opts.maxIterations - 1
+                Dim lh = likelihood(samples, components)
+
+                If std.Abs(lh - lastLikelihood) < opts.tolerance Then
+                    Exit For
+                End If
+
+                components = optimize(samples, components)
+                lastLikelihood = lh
+            Next
+
+            'If Not detectComponents Then
+            Return components
+            'End If
+        End Function
+
+        ''' <summary>
+        ''' calculate likelihood
+        ''' </summary>
+        ''' <param name="samples"></param>
+        ''' <param name="components"></param>
+        ''' <returns></returns>
+        Private Function likelihood(samples As Double(), components As Variable()) As Double
+            Dim l As Double = 0
+            Dim p As Double = 0
+            Dim n = samples.Length
+            Dim comp As Variable
+
+            For i As Integer = 0 To samples.Length - 1
+                p = 0
+                For c As Integer = 0 To components.Length - 1
+                    comp = components(c)
+                    p += samples(i) * comp.weight * pnorm.normal_pdf(i / n, comp.mean, comp.variance)
+                Next
+                If p = 0.0 Then
+                    Return Double.NegativeInfinity
+                Else
+                    l += std.Log(p)
+                End If
+            Next
+
+            Return l
+        End Function
+
+        ''' <summary>
+        ''' single iteration of em algorithm
+        ''' </summary>
+        ''' <param name="samples"></param>
+        ''' <param name="components"></param>
+        ''' <returns></returns>
+        Private Function optimize(samples As Double(), components As Variable()) As Variable()
+            For i As Integer = 0 To samples.Length - 1
+                Dim x = i / samples.Length
+                Dim p = New Double(components.Length - 1) {}
+
+                ' total probability at the point
+                Dim sump As Double = 0
+
+                For c As Integer = 0 To components.Length - 1
+                    p(c) = components(c).weight * pnorm.normal_pdf(x, components(c).mean, components(c).variance)
+                    sump += p(c)
+                Next
+
+                ' [c0, c1, c2, c0, c1, c2, ...]
+                For c As Integer = 0 To components.Length - 1
+                    membership(i * components.Length + c) = samples(i) * p(c) / sump
+                Next
+            Next
+
+            ' M-step: update components to better cover member samples
+            Dim w = New Double(components.Length - 1) {}
+            Dim sumw As Double = 0
+
+            For c As Integer = 0 To components.Length - 1
+                For i As Integer = 0 To samples.Length - 1
+                    w(c) += membership(i * components.Length + c)
+                Next
+                sumw += w(c)
+            Next
+
+            Dim n = samples.Length
+
+            Return components _
+                .Select(Function(component, c)
+                            ' get new amp as ratio of the total weight
+                            component.weight = w(c) / sumw
+                            ' get new mean as weighted by ratios value
+                            Dim sumu As Double = 0.0
+
+                            For i As Integer = 0 To samples.Length - 1
+                                sumu += (i / n) * membership(i * components.Length + c)
+                            Next
+                            component.mean = sumu / w(c)
+
+                            ' get new variations as weighted by ratios stdev
+                            Dim sumv As Double = 0
+                            For i As Integer = 0 To samples.Length - 1
+                                sumv += membership(i * components.Length + c) * (1 / n - component.mean) ^ 2
+                            Next
+
+                            component.variance = std.Max(sumv / w(c), 0.00001)
+
+                            Return component
+                        End Function) _
+                .ToArray
         End Function
     End Class
 
+    ''' <summary>
+    ''' A possible signle peak
+    ''' </summary>
     Public Class Variable
 
         Public Property weight As Double
