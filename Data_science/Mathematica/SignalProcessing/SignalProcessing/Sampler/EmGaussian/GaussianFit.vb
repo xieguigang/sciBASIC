9fdfaef1e4106749088c3f1919ffc06232733008
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-25 14:38:25 +0800

    create variable data model

--- Data_science/Mathematica/SignalProcessing/SignalProcessing/Sampler/EmGaussian/GaussianFit.vb
+++ Data_science/Mathematica/SignalProcessing/SignalProcessing/Sampler/EmGaussian/GaussianFit.vb
@@ -1,8 +1,38 @@
-﻿Namespace EmGaussian
+﻿Imports System.Runtime.CompilerServices
+
+Namespace EmGaussian
 
     Public Class GaussianFit
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function fit(samples, ParamArray components As Double()())
+            Return fit(samples, New Opts With {.components = components})
+        End Function
 
+        Public Function fit(samples, Optional opts As Opts = Nothing)
+            Dim detectComponents As Boolean = opts.components.IsNullOrEmpty
+            ' initialize components
+            Dim cn As Integer = opts.components.Length
+            Dim components = opts.components _
+                .Select(Function(v, i)
+                            If v.IsNullOrEmpty Then
+                                Return New Variable With {.weight = 1 / cn, .mean = i / cn, .variance = 1 / cn}
+                            Else
+                                Return New Variable With {.weight = v(0), .mean = v(1), .variance = v(2)}
+                            End If
+                        End Function) _
+                .ToArray
+
+            ' optimize components
+
+        End Function
+    End Class
+
+    Public Class Variable
+
+        Public Property weight As Double
+        Public Property mean As Double
+        Public Property variance As Double
 
     End Class
 End Namespace
--- Data_science/Mathematica/SignalProcessing/SignalProcessing/Sampler/EmGaussian/Opts.vb
+++ Data_science/Mathematica/SignalProcessing/SignalProcessing/Sampler/EmGaussian/Opts.vb
@@ -2,7 +2,17 @@
 
     Public Class Opts
 
+        ''' <summary>
+        ''' time points that possible has a peak
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property components As Double()()
+        Public Property maxNumber As Integer = 100
+        Public Property maxIterations As Integer = 100
+        Public Property tolerance As Double = 0.00001
 
-
+        Public Shared Function GetDefault() As Opts
+            Return New Opts
+        End Function
     End Class
 End Namespace
