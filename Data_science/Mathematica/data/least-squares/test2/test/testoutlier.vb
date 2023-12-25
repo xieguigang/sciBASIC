33d8d39ed3e9d30266d5a9ef590ed17e0b5da1fe
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-25 11:48:42 +0800

    make bugs fixed for ruyn test

--- Data_science/Mathematica/data/least-squares/test2/test/testoutlier.vb
+++ Data_science/Mathematica/data/least-squares/test2/test/testoutlier.vb
@@ -1,57 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::4ba37d35a89a53e730191af6f9a56639, sciBASIC#\Data_science\Mathematica\data\least-squares\test2\test\testoutlier.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 46
-    '    Code Lines: 24
-    ' Comment Lines: 4
-    '   Blank Lines: 18
-    '     File Size: 1.56 KB
+' Summaries:
 
 
-    ' Module testoutlier
-    ' 
-    '     Sub: Main, orderSeq
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 46
+'    Code Lines: 24
+' Comment Lines: 4
+'   Blank Lines: 18
+'     File Size: 1.56 KB
+
+
+' Module testoutlier
+' 
+'     Sub: Main, orderSeq
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Drawing
-Imports Microsoft.VisualBasic.Data.Bootstrapping.Outlier
-Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
 Imports Microsoft.VisualBasic.Serialization.JSON
 
@@ -59,7 +56,7 @@ Module testoutlier
 
     Sub Main()
 
-        Call orderSeq()
+        ' Call orderSeq()
 
         Dim x As Vector = {0.010228592, 2.278282642, 0.922615588, 0.472233653, 0.234864831, 0.117762581, 0.051605649}
 
@@ -80,18 +77,18 @@ Module testoutlier
 
     End Sub
 
-    Sub orderSeq()
-        Dim x As Vector = {10, -3, 0.010228592, 2.278282642, 0.922615588, 0.472233653, 0.234864831, 0.117762581, 0.051605649, -10, 1}
-        Dim Y As Vector = {9, -5, 10.010228592, 2.578282642, 0.922615588, 0.472233653, 0.234864831, 0.117762581, 0.051605649, -11, 1.2222}
+    'Sub orderSeq()
+    '    Dim x As Vector = {10, -3, 0.010228592, 2.278282642, 0.922615588, 0.472233653, 0.234864831, 0.117762581, 0.051605649, -10, 1}
+    '    Dim Y As Vector = {9, -5, 10.010228592, 2.578282642, 0.922615588, 0.472233653, 0.234864831, 0.117762581, 0.051605649, -11, 1.2222}
 
-        Dim index = x.OrderSequenceOutlierIndex.ToArray
-        Dim line = x.Select(Function(p, i) New PointF(p, Y(i))).ToArray
+    '    Dim index = x.OrderSequenceOutlierIndex.ToArray
+    '    Dim line = x.Select(Function(p, i) New PointF(p, Y(i))).ToArray
 
-        Dim xy = line.DeleteOutlier.ToArray
+    '    Dim xy = line.DeleteOutlier.ToArray
 
-        Call xy.SaveTo("./line.csv")
-        Call line.SaveTo("./line-raw.csv")
+    '    Call xy.SaveTo("./line.csv")
+    '    Call line.SaveTo("./line-raw.csv")
 
-        Pause()
-    End Sub
+    '    Pause()
+    'End Sub
 End Module
