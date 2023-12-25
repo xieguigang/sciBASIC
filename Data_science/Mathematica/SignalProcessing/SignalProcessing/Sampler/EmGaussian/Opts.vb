cad124705e0621421f0ec4c312c9c47bbf65074f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-25 15:02:40 +0800

    add xml dev notes

--- Data_science/Mathematica/SignalProcessing/SignalProcessing/Sampler/EmGaussian/Opts.vb
+++ Data_science/Mathematica/SignalProcessing/SignalProcessing/Sampler/EmGaussian/Opts.vb
@@ -7,8 +7,20 @@
         ''' </summary>
         ''' <returns></returns>
         Public Property components As Double()()
+        ''' <summary>
+        ''' max number of components in case of auto-detection
+        ''' </summary>
+        ''' <returns></returns>
         Public Property maxNumber As Integer = 100
+        ''' <summary>
+        ''' max number of iterations
+        ''' </summary>
+        ''' <returns></returns>
         Public Property maxIterations As Integer = 100
+        ''' <summary>
+        ''' min difference of likelihood
+        ''' </summary>
+        ''' <returns></returns>
         Public Property tolerance As Double = 0.00001
 
         Public Shared Function GetDefault() As Opts
