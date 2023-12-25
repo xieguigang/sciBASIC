ff45b39da8f63a6f3f39d19098041ee7c05cb09d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-25 15:00:10 +0800

    split source file into multiple parts

--- Data_science/Mathematica/SignalProcessing/SignalProcessing/Sampler/EmGaussian/GaussianFit.vb
+++ Data_science/Mathematica/SignalProcessing/SignalProcessing/Sampler/EmGaussian/GaussianFit.vb
@@ -142,15 +142,4 @@ Namespace EmGaussian
                 .ToArray
         End Function
     End Class
-
-    ''' <summary>
-    ''' A possible signle peak
-    ''' </summary>
-    Public Class Variable
-
-        Public Property weight As Double
-        Public Property mean As Double
-        Public Property variance As Double
-
-    End Class
 End Namespace
---
+++ Data_science/Mathematica/SignalProcessing/SignalProcessing/Sampler/EmGaussian/Variable.vb
@@ -0,0 +1,13 @@
+﻿Namespace EmGaussian
+
+    ''' <summary>
+    ''' A possible signle peak
+    ''' </summary>
+    Public Class Variable
+
+        Public Property weight As Double
+        Public Property mean As Double
+        Public Property variance As Double
+
+    End Class
+End Namespace
