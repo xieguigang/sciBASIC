c26d7e3697530a0b1e2b11de78e1c76dcaf26446
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-28 16:27:34 +0800

    split source file into multiple parts

--- Data_science/DataMining/DataMining/Clustering/KMeans/Extensions.vb
+++ Data_science/DataMining/DataMining/Clustering/KMeans/Extensions.vb
@@ -56,55 +56,8 @@ Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
-Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace KMeans
-
-    Public Class DataSetConvertor
-
-        ReadOnly maps As String()
-
-        Sub New(rawInput As EntityClusterModel())
-            maps = rawInput _
-                .Select(Function(a) a.Properties.Keys) _
-                .IteratesALL _
-                .Distinct _
-                .ToArray
-        End Sub
-
-        Public Iterator Function GetPoints(rawInput As EntityClusterModel()) As IEnumerable(Of Lloyds.Point)
-            For Each xi As EntityClusterModel In rawInput
-                Yield New Lloyds.Point(xi(maps)) With {.uid = xi.ID}
-            Next
-        End Function
-
-        Public Iterator Function GetVectors(rawInput As EntityClusterModel()) As IEnumerable(Of ClusterEntity)
-            For Each xi As EntityClusterModel In rawInput
-                Yield xi.ToModel(projection:=maps)
-            Next
-        End Function
-
-        Public Iterator Function GetObjects(Of T As ClusterEntity)(cluster As IEnumerable(Of T)) As IEnumerable(Of EntityClusterModel)
-            For Each xi As ClusterEntity In cluster
-                Yield xi.ToDataModel(maps)
-            Next
-        End Function
-
-        Public Iterator Function GetObjects(Of T As ClusterEntity)(cluster As IEnumerable(Of T), setClass As Integer) As IEnumerable(Of EntityClusterModel)
-            Dim yi As EntityClusterModel
-
-            For Each xi As ClusterEntity In cluster
-                yi = xi.ToDataModel(maps)
-                yi.Cluster = setClass
-
-                Yield yi
-            Next
-        End Function
-
-        Public Overrides Function ToString() As String
-            Return maps.GetJson
-        End Function
-    End Class
 
     <HideModuleName>
     Public Module Extensions
