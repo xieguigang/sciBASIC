21c91c1d58b4b46ba0c9d2d884954ce056024e96
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-11 14:40:37 +0800

    make improvements of the object debug view

--- Data/Trinity/Model/TokenCounter.vb
+++ Data/Trinity/Model/TokenCounter.vb
@@ -1,61 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::4e23cd0fd15bffb54ec401d29d33dca3, sciBASIC#\Data\word2vec\utils\Counter.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 96
-    '    Code Lines: 53
-    ' Comment Lines: 27
-    '   Blank Lines: 16
-    '     File Size: 2.96 KB
+' Summaries:
 
 
-    '     Class Counter
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    ' 
-    '         Function: [get], keySet, size, ToString
-    ' 
-    '         Sub: (+2 Overloads) add, remove
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 96
+'    Code Lines: 53
+' Comment Lines: 27
+'   Blank Lines: 16
+'     File Size: 2.96 KB
+
+
+'     Class Counter
+' 
+'         Constructor: (+2 Overloads) Sub New
+' 
+'         Function: [get], keySet, size, ToString
+' 
+'         Sub: (+2 Overloads) add, remove
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Text
 Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Serialization.JSON
 Imports int = Microsoft.VisualBasic.ComponentModel.Counter
 
 Namespace Model
@@ -134,19 +134,7 @@ Namespace Model
         ''' 将计数器转换为字符串 </summary>
         ''' <returns> 字符串 </returns>
         Public Overrides Function ToString() As String
-            Dim iterator As IEnumerator(Of KeyValuePair(Of tT, int)) = SetOfKeyValuePairs(hm).GetEnumerator()
-            Dim sb As New StringBuilder()
-            Dim [next] As KeyValuePair(Of tT, int) = Nothing
-
-            While iterator.MoveNext()
-                [next] = iterator.Current
-                sb.Append([next].Key)
-                sb.Append(vbTab)
-                sb.Append([next].Value)
-                sb.Append(vbLf)
-            End While
-
-            Return sb.ToString()
+            Return hm.ToDictionary(Function(w) w.Key, Function(w) CInt(w.Value)).GetJson
         End Function
     End Class
 End Namespace
