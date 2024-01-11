cc9e7f8889e81b79d287bbe6d5591c0eed20773d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-11 15:08:34 +0800

    make improvements of the test code

--- Data/word2vec/test/TestWord2Vec.vb
+++ Data/word2vec/test/TestWord2Vec.vb
@@ -1,59 +1,57 @@
 ﻿#Region "Microsoft.VisualBasic::47091a61152c0fd0a678546a386fd839, sciBASIC#\Data\word2vec\test\TestWord2Vec.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 54
-    '    Code Lines: 39
-    ' Comment Lines: 4
-    '   Blank Lines: 11
-    '     File Size: 1.95 KB
+' Summaries:
 
 
-    '     Class TestWord2Vec
-    ' 
-    '         Sub: Main, readByJava, testVector
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 54
+'    Code Lines: 39
+' Comment Lines: 4
+'   Blank Lines: 11
+'     File Size: 1.95 KB
+
+
+'     Class TestWord2Vec
+' 
+'         Sub: Main, readByJava, testVector
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.IO
 Imports Microsoft.VisualBasic.Data.NLP.Model
 Imports Microsoft.VisualBasic.Data.NLP.Word2Vec.NlpVec
-Imports Microsoft.VisualBasic.Data.NLP.Word2Vec.utils
 
 Namespace test
 
@@ -62,7 +60,7 @@ Namespace test
     ''' @author siegfang
     ''' </summary>
     Public Class TestWord2Vec
-        Public Shared Sub readByJava(ByVal textFilePath As String, ByVal modelFilePath As String)
+        Public Shared Function readByJava(ByVal textFilePath As String, ByVal modelFilePath As String) As VectorModel
             Dim wv As Word2Vec = (New Word2VecFactory()).setMethod(TrainMethod.Skip_Gram).setNumOfThread(1).build()
             Dim data As Paragraph() = Paragraph.Segmentation(textFilePath.ReadAllText).ToArray
 
@@ -73,11 +71,10 @@ Namespace test
             Next
 
             wv.training()
-            wv.saveModel(New FileStream(modelFilePath, FileMode.OpenOrCreate))
-        End Sub
+            Return wv.outputVector
+        End Function
 
-        Public Shared Sub testVector(ByVal modelFilePath As String)
-            Dim vm = VectorModel.loadFromFile(modelFilePath)
+        Public Shared Sub testVector(vm As VectorModel)
             Dim result1 As New SortedSet(Of WordScore)(vm.similar("亲"))
 
             For Each we In result1
@@ -88,8 +85,8 @@ Namespace test
         Public Shared Sub Main(ByVal args As String())
             Dim textFilePath = "E:\GCModeller\src\runtime\sciBASIC#\Data\TextRank\Rapunzel.txt"
             Dim modelFilePath = "./swresult_withoutnature.vec"
-            readByJava(textFilePath, modelFilePath)
-            testVector(modelFilePath)
+
+            testVector(readByJava(textFilePath, modelFilePath))
         End Sub
     End Class
 End Namespace
