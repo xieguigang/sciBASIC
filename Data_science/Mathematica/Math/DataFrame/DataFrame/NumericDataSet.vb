1d908fb2e8eb29ac5eb160056c5d0c2e21d74924
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-15 14:54:18 +0800

    cast a dataframe to numeric matrix

---
+++ Data_science/Mathematica/Math/DataFrame/DataFrame/NumericDataSet.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+Public Module NumericDataSet
+
+    <Extension>
+    Public Function NumericGetter(v As FeatureVector) As Func(Of Integer, Double)
+
+    End Function
+
+    <Extension>
+    Public Iterator Function NumericMatrix(df As DataFrame) As IEnumerable(Of NamedCollection(Of Double))
+        Dim colnames As String() = df.featureNames
+        Dim fieldGetters As Func(Of Integer, Double)() = colnames _
+            .Select(Function(s) df(s).NumericGetter) _
+            .ToArray
+        Dim nrow As Integer = df.nsamples
+        Dim rownames As String() = df.rownames
+        Dim offset As Integer
+        Dim row As Double()
+
+        For i As Integer = 0 To nrow - 1
+            offset = i
+            row = fieldGetters _
+                .Select(Function(v) v(offset)) _
+                .ToArray
+
+            Yield New NamedCollection(Of Double)(rownames(i), row)
+        Next
+    End Function
+
+End Module
