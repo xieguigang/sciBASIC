360cd9fb43af5bbad3fff19f307a6f29c06dd7b1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-16 15:57:39 +0800

    Update SymbolicTest.vb

--- Data_science/Mathematica/Math/MathLambda/test/SymbolicTest.vb
+++ Data_science/Mathematica/Math/MathLambda/test/SymbolicTest.vb
@@ -1,51 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::e498b2e5d8f26283d46709dbdc85a999, sciBASIC#\Data_science\Mathematica\Math\MathLambda\test\SymbolicTest.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 49
-    '    Code Lines: 34
-    ' Comment Lines: 0
-    '   Blank Lines: 15
-    '     File Size: 1.52 KB
+' Summaries:
 
 
-    ' Module SymbolicTest
-    ' 
-    '     Sub: expands, Main
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 49
+'    Code Lines: 34
+' Comment Lines: 0
+'   Blank Lines: 15
+'     File Size: 1.52 KB
+
+
+' Module SymbolicTest
+' 
+'     Sub: expands, Main
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -59,8 +59,13 @@ Module SymbolicTest
 
     Sub Main()
 
-        Call expands()
+        ' Call expands()
+        Call test_Simplify()
 
+        Pause()
+    End Sub
+
+    Sub test_Simplify()
         Dim symbols = ScriptEngine.ParseExpression("(5+5) * (2*x + x / 5 + x ^ 3)")
         Dim result = symbols.DoCall(AddressOf Symbolic.Simplify)
 
@@ -71,7 +76,15 @@ Module SymbolicTest
         Console.WriteLine($"{symbols} = {symbols.Evaluate(ScriptEngine.Expression)}")
         Console.WriteLine($"{result} = {result.Evaluate(ScriptEngine.Expression)}")
 
-        Pause()
+        symbols = ScriptEngine.ParseExpression("(2 * x ^ 2 - 1 + 0 * a) ^ -1 * (2 * x ^ 2  - 1 * 1) ^ -1")
+        result = symbols.DoCall(AddressOf Symbolic.Simplify)
+
+        Console.WriteLine($"{symbols} -> {result}")
+
+        ScriptEngine.SetVariable("x", 33)
+
+        Console.WriteLine($"{symbols} = {symbols.Evaluate(ScriptEngine.Expression)}")
+        Console.WriteLine($"{result} = {result.Evaluate(ScriptEngine.Expression)}")
     End Sub
 
     Sub expands()
