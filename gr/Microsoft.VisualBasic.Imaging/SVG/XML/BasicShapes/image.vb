63e582c15990b29b6fdccf427bd65f543b240481
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-29 10:50:50 +0800

    needs merge the image module

--- gr/Microsoft.VisualBasic.Imaging/SVG/XML/BasicShapes/image.vb
+++ gr/Microsoft.VisualBasic.Imaging/SVG/XML/BasicShapes/image.vb
@@ -2,17 +2,17 @@
 Imports Microsoft.VisualBasic.Text.Xml
 
 Namespace SVG.XML
-    Public NotInheritable Class SvgImage
-        Inherits SvgBasicShape
-        Private Sub New(element As XmlElement)
-            MyBase.New(element)
-        End Sub
 
-        Friend Shared Function Create(parent As XmlElement) As SvgImage
-            Dim element = parent.OwnerDocument.CreateElement("image")
-            parent.AppendChild(element)
-            Return New SvgImage(element)
-        End Function
+    ''' <summary>
+    ''' The &lt;image> SVG element includes images inside SVG documents. It can display raster image files or other SVG files.
+    '''
+    ''' The only image formats SVG software must support are JPEG, PNG, And other SVG files. Animated GIF behavior Is undefined.
+    '''
+    ''' SVG files displayed With &lt;image> are treated As an image: external resources aren't loaded, :visited styles aren't
+    ''' applied, and they cannot be interactive. To include dynamic SVG elements, try &lt;use> with an external URL. To include 
+    ''' SVG files and run scripts inside them, try &lt;object> inside of &lt;foreignObject>.
+    ''' </summary>
+    Public NotInheritable Class SvgImage : Inherits SvgBasicShape
 
         Public Property X As Double
             Get
@@ -58,5 +58,15 @@ Namespace SVG.XML
                 Element.SetAttribute("xlink:href", value)
             End Set
         End Property
+
+        Private Sub New(element As XmlElement)
+            MyBase.New(element)
+        End Sub
+
+        Friend Shared Function Create(parent As XmlElement) As SvgImage
+            Dim element = parent.OwnerDocument.CreateElement("image")
+            parent.AppendChild(element)
+            Return New SvgImage(element)
+        End Function
     End Class
 End Namespace
