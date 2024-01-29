ebe920435816a8dd77e2a1ea157dd3ba249d7060
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-29 10:57:42 +0800

    svg title element

--- gr/Microsoft.VisualBasic.Imaging/SVG/XML/BasicShapes/title.vb
+++ gr/Microsoft.VisualBasic.Imaging/SVG/XML/BasicShapes/title.vb
@@ -1,10 +1,26 @@
-﻿Imports System.Xml.Serialization
+﻿Imports System.Xml
 
 Namespace SVG.XML
 
-    Public Class title : Inherits node
+    Public Class SvgTitle : Inherits SvgElement
 
-        <XmlText>
-        Public Property innerHTML As String
+        Public Property Text As String
+            Get
+                Return Element.InnerText
+            End Get
+            Set(value As String)
+                Element.InnerText = value
+            End Set
+        End Property
+
+        Private Sub New(element As XmlElement)
+            MyBase.New(element)
+        End Sub
+
+        Friend Shared Function Create(parent As XmlElement) As SvgTitle
+            Dim element = parent.OwnerDocument.CreateElement("title")
+            parent.AppendChild(element)
+            Return New SvgTitle(element)
+        End Function
     End Class
 End Namespace
