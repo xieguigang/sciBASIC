b2e49be4bd0104bf25ddb21bbcdd8f7f5feea2fb
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-03 20:44:53 +0800

    add bitmap file handler

--- Microsoft.VisualBasic.Core/src/Extensions/ValueTypes/TupleHelper.vb
+++ Microsoft.VisualBasic.Core/src/Extensions/ValueTypes/TupleHelper.vb
@@ -13,6 +13,13 @@ Public Module TupleHelper
     End Sub
 
     <Extension>
+    Public Sub [Set](Of T1, T2, T3)(t As (T1, T2, T3), <Out> ByRef a As T1, <Out> ByRef b As T2, <Out> ByRef c As T3)
+        a = t.Item1
+        b = t.Item2
+        c = t.Item3
+    End Sub
+
+    <Extension>
     Public Sub [Set](Of T1, T2, T3, T4)(t As (T1, T2, T3, T4), <Out> ByRef a As T1, <Out> ByRef b As T2, <Out> ByRef c As T3, <Out> ByRef d As T4)
         a = t.Item1
         b = t.Item2
