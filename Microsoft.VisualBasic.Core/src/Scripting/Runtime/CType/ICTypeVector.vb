1fca421350b13e15c4ff95613eb95af00799e992
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-04 19:49:23 +0800

    move abstract vector conversion model to base project

---
+++ Microsoft.VisualBasic.Core/src/Scripting/Runtime/CType/ICTypeVector.vb
@@ -0,0 +1,12 @@
+﻿Namespace Scripting.Runtime
+
+    Public Interface ICTypeVector
+
+        Function ToNumeric() As Double()
+        Function ToFloat() As Single()
+        Function ToFactors() As String()
+        Function ToInteger() As Integer()
+        Function ToLong() As Long()
+
+    End Interface
+End Namespace
