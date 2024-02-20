cd849eb5582df195b43c2453f22783db23ab551e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-20 16:31:33 +0800

    implements the line editor

---
+++ Microsoft.VisualBasic.Core/src/ApplicationServices/Terminal/InteractiveIODevice/LineEdit/LineReader.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ApplicationServices.Terminal.STDIO__
+
+Namespace ApplicationServices.Terminal.LineEdit
+
+    Public Class LineReader : Implements IShellDevice
+
+        Dim prompt As String
+        Dim line As LineEditor
+
+        Sub New(reader As LineEditor)
+            line = reader
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Sub SetPrompt(s As String) Implements IShellDevice.SetPrompt
+            prompt = s
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function ReadLine() As String Implements IShellDevice.ReadLine
+            Return line.Edit(prompt, "")
+        End Function
+    End Class
+End Namespace
