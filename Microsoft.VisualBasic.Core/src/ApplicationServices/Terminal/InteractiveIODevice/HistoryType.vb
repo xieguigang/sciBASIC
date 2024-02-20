c7dac17496d85a4e10aa15e475f1460b88bba16a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-20 15:41:04 +0800

    split source file into multiple parts

---
+++ Microsoft.VisualBasic.Core/src/ApplicationServices/Terminal/InteractiveIODevice/HistoryType.vb
@@ -0,0 +1,162 @@
+﻿Namespace ApplicationServices.Terminal
+
+    '
+    ' Emulates the bash-like behavior, where edits done to the
+    ' history are recorded
+    '
+    Friend Class HistoryType
+        Private history As String()
+        Private head, tail As Integer
+        Private cursor, count As Integer
+        Private histfile As String
+
+        Public Sub New(app As String, size As Integer)
+            If size < 1 Then Throw New ArgumentException("size")
+
+            If Not Equals(app, Nothing) Then
+                Dim dir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+                'Console.WriteLine (dir);
+                If Not Directory.Exists(dir) Then
+                    Try
+                        Directory.CreateDirectory(dir)
+                    Catch
+                        app = Nothing
+                    End Try
+                End If
+                If Not Equals(app, Nothing) Then histfile = Path.Combine(dir, app) & ".history"
+            End If
+
+            history = New String(size - 1) {}
+            cursor = 0
+            tail = 0
+            head = 0
+
+            If File.Exists(histfile) Then
+                Using sr = File.OpenText(histfile)
+                    Dim line As Value(Of String) = ""
+
+                    While Not (line = sr.ReadLine) Is Nothing
+                        If Not Equals(line, "") Then Append(line)
+                    End While
+                End Using
+            End If
+        End Sub
+
+        Public Sub Close()
+            If Equals(histfile, Nothing) Then Return
+
+            Try
+                Using sw = File.CreateText(histfile)
+                    Dim start = If(count = history.Length, head, tail)
+                    For i = start To start + count - 1
+                        Dim p = i Mod history.Length
+                        sw.WriteLine(history(p))
+                    Next
+                End Using
+
+            Catch
+                ' ignore
+            End Try
+        End Sub
+
+        '
+        ' Appends a value to the history
+        '
+        Public Sub Append(s As String)
+            'Console.WriteLine ("APPENDING {0} head={1} tail={2}", s, head, tail);
+            history(head) = s
+            head = (head + 1) Mod history.Length
+            If head = tail Then tail = tail + 1 Mod history.Length
+            If count <> history.Length Then count += 1
+            'Console.WriteLine ("DONE: head={1} tail={2}", s, head, tail);
+        End Sub
+
+        '
+        ' Updates the current cursor location with the string,
+        ' to support editing of history items.   For the current
+        ' line to participate, an Append must be done before.
+        '
+        Public Sub Update(s As String)
+            history(cursor) = s
+        End Sub
+
+        Public Sub RemoveLast()
+            head = head - 1
+            If head < 0 Then head = history.Length - 1
+        End Sub
+
+        Public Sub Accept(s As String)
+            Dim t = head - 1
+            If t < 0 Then t = history.Length - 1
+
+            history(t) = s
+        End Sub
+
+        Public Function PreviousAvailable() As Boolean
+            'Console.WriteLine ("h={0} t={1} cursor={2}", head, tail, cursor);
+            If count = 0 Then Return False
+            Dim [next] = cursor - 1
+            If [next] < 0 Then [next] = count - 1
+
+            If [next] = head Then Return False
+
+            Return True
+        End Function
+
+        Public Function NextAvailable() As Boolean
+            If count = 0 Then Return False
+            Dim [next] = (cursor + 1) Mod history.Length
+            If [next] = head Then Return False
+            Return True
+        End Function
+
+
+        '
+        ' Returns: a string with the previous line contents, or
+        ' nul if there is no data in the history to move to.
+        '
+        Public Function Previous() As String
+            If Not PreviousAvailable() Then Return Nothing
+
+            cursor -= 1
+            If cursor < 0 Then cursor = history.Length - 1
+
+            Return history(cursor)
+        End Function
+
+        Public Function [Next]() As String
+            If Not NextAvailable() Then Return Nothing
+
+            cursor = (cursor + 1) Mod history.Length
+            Return history(cursor)
+        End Function
+
+        Public Sub CursorToEnd()
+            If head = tail Then Return
+
+            cursor = head
+        End Sub
+
+        Public Sub Dump()
+            Console.WriteLine("Head={0} Tail={1} Cursor={2} count={3}", head, tail, cursor, count)
+            For i = 0 To history.Length - 1
+                Console.WriteLine(" {0} {1}: {2}", If(i = cursor, "==>", "   "), i, history(i))
+            Next
+            'log.Flush ();
+        End Sub
+
+        Public Function SearchBackward(term As String) As String
+            For i = 0 To count - 1
+                Dim slot = cursor - i - 1
+                If slot < 0 Then slot = history.Length + slot
+                If slot >= history.Length Then slot = 0
+                If Not Equals(history(slot), Nothing) AndAlso history(slot).IndexOf(term) <> -1 Then
+                    cursor = slot
+                    Return history(slot)
+                End If
+            Next
+
+            Return Nothing
+        End Function
+    End Class
+End Namespace
