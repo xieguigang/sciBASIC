a720801848f46dc86fee15c42acd81a0f0a31f6f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-27 10:50:25 +0800

    json async parser

--- mime/application%json/Javascript/JsonElement.vb
+++ mime/application%json/Javascript/JsonElement.vb
@@ -1,52 +1,52 @@
 ﻿#Region "Microsoft.VisualBasic::2c2a9046b1db4922fce76570dd40864c, sciBASIC#\mime\application%json\Javascript\JsonElement.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 29
-    '    Code Lines: 16
-    ' Comment Lines: 8
-    '   Blank Lines: 5
-    '     File Size: 923 B
+' Summaries:
 
 
-    '     Class JsonElement
-    ' 
-    '         Function: [As], ParseJSON, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 29
+'    Code Lines: 16
+' Comment Lines: 8
+'   Blank Lines: 5
+'     File Size: 923 B
+
+
+'     Class JsonElement
+' 
+'         Function: [As], ParseJSON, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -77,6 +77,10 @@ Namespace Javascript
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Shared Function ParseJSON(jsonStr As String) As JsonElement
             Return New JsonParser().OpenJSON(jsonStr)
+        End Function
+
+        Public Shared Function Parse(json_str As String) As Task(Of JsonElement)
+            Return Task(Of JsonElement).Run(Function() ParseJSON(json_str))
         End Function
 
         Public Shared Narrowing Operator CType(js As JsonElement) As JavaScriptObject
