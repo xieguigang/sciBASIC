8e52f1d552e7c044b8e0e817ace8e8d49276fb1a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-28 09:42:55 +0800

    parse json and apply the logic code

---
+++ mime/application%json/JSONLogic/jsonLogic.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic.MIME.application.json.Javascript
+
+Namespace JSONLogic
+
+    Public Module jsonLogic
+
+        Public Function apply(logic As JsonElement, data As JsonElement) As JsonElement
+
+        End Function
+
+        Public Function apply(logic As String, data As String) As Object
+            Dim logic_json As JsonElement = JsonParser.Parse(logic)
+            Dim data_json As JsonElement = JsonParser.Parse(logic)
+            Dim result_json As JsonElement = jsonLogic.apply(logic_json, data_json)
+
+            If result_json Is Nothing Then
+                Return Nothing
+            ElseIf TypeOf result_json Is JsonValue Then
+                Return DirectCast(result_json, JsonValue).value
+            Else
+                Throw New NotImplementedException(result_json.GetType.FullName)
+            End If
+        End Function
+
+    End Module
+End Namespace
