ea89099e5b8e281bcd8d83a4f4b4ca7543f7fddb
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-28 10:00:09 +0800

    configs run logical test

---
+++ mime/application%json/JSONtest/logicTest.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic.MIME.application.json.JSONLogic
+
+Module logicTest
+
+    Sub Main()
+        Dim out = jsonLogic.apply(
+            "{'var': ['a'] }",  ' Logic
+            "{'a': 1, 'b': 2 }" ' Data
+        )   ' 1
+
+        Console.WriteLine(out)
+
+        Pause()
+    End Sub
+End Module
