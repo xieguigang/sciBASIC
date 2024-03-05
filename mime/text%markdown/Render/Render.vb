41adeb8cc61fa9e5c70c83320df9b29e3a68a9f7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-05 13:26:58 +0800

    fix of the paragraph syntax in latax

--- mime/text%markdown/Render/Render.vb
+++ mime/text%markdown/Render/Render.vb
@@ -7,6 +7,7 @@ Public MustInherit Class Render
     Protected Shared ReadOnly _leadingWhitespace As New Regex("^[ ]*", RegexOptions.Compiled)
 
     Public MustOverride Function Paragraph(text As String, CreateParagraphs As Boolean) As String
+    Public MustOverride Function Header(text As String) As String
     Public MustOverride Function Document(text As String) As String
 
 End Class
