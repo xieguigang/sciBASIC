6da4cded063cc6bad4ab2dbab2eeb06f3ce38b4c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-16 23:38:14 +0800

    dataframe schema data

--- Data/BinaryData/DataStorage/Tabular/FrameWriter.vb
+++ Data/BinaryData/DataStorage/Tabular/FrameWriter.vb
@@ -1,3 +1,18 @@
-﻿Module FrameWriter
+﻿Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Math.DataFrame
+
+Public Module FrameWriter
+
+    ''' <summary>
+    ''' write dataframe object as the binary file
+    ''' </summary>
+    ''' <param name="df"></param>
+    ''' <param name="file"></param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function WriteFrame(df As DataFrame, file As Stream) As Boolean
+
+    End Function
 
 End Module
---
+++ Data/BinaryData/DataStorage/Tabular/Schema.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualBasic.Math.DataFrame
+
+Public Class Schema
+
+    Public Property rownames As String()
+    Public Property cols As Dictionary(Of String, Type)
+    Public Property dims As Integer()
+
+    Sub New(df As DataFrame)
+        rownames = df.rownames
+        dims = {df.dims.Height, df.dims.Width}
+        cols = df.features _
+            .ToDictionary(Function(f) f.Key,
+                          Function(f)
+                              Return f.Value.type
+                          End Function)
+    End Sub
+
+End Class
