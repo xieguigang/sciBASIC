dfe9e117fd134cf1fa8d823641af2e1852966134
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-18 14:01:47 +0800

    configs for run json parser test

---
+++ mime/application%json/JSONtest/simple_jsonParserTest.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic.MIME.application.json
+Imports Microsoft.VisualBasic.MIME.application.json.Javascript
+
+Module simple_jsonParserTest
+
+    Sub Main()
+        Call test1()
+    End Sub
+
+    Sub test1()
+        Dim str As JsonValue = JsonParser.Parse("'abc'")
+        Dim null As JsonValue = JsonParser.Parse("null")
+
+    End Sub
+End Module
