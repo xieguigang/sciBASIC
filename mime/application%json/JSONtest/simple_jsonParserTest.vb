645ce1a2ff832f0f47d4c003e239c8f463cfc98f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-18 14:47:46 +0800

    add more demo test

--- mime/application%json/JSONtest/simple_jsonParserTest.vb
+++ mime/application%json/JSONtest/simple_jsonParserTest.vb
@@ -11,7 +11,23 @@ Module simple_jsonParserTest
         ' Dim str As JsonValue = JsonParser.Parse("'abc'")
         ' Dim null As JsonValue = JsonParser.Parse("null")
         ' Dim vec1 As JsonArray = JsonParser.Parse("[-1,1,2,3,4,5]")
-        Dim obj As JsonObject = JsonParser.Parse("{'a': true, b: [3,3,4]}")
+        ' Dim obj As JsonObject = JsonParser.Parse("{'a': true, b: [3,3,4]}")
+        Dim obj_no_comment = JsonParser.Parse("
+        {
+            'a': true,
+            // is an integer vector
+            'v': [1,1,1,3,4,5],
+            // is a string
+            'str': 'hello ""world""!',
+            // string in multiple lines
+            ""text"": '
+                line1
+                line2
+                line3
+            '
+        }
+
+        ")
 
         Pause()
     End Sub
