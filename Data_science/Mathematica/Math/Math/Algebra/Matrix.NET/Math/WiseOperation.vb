5db23d7b39d14042f5aee7d25da1b3945493be0a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-19 16:29:24 +0800

    make improvements of the matrix model

---
+++ Data_science/Mathematica/Math/Math/Algebra/Matrix.NET/Math/WiseOperation.vb
@@ -0,0 +1,32 @@
+﻿
+Imports Microsoft.VisualBasic.Linq
+
+Namespace LinearAlgebra.Matrix
+
+    Public Class WiseOperation
+
+        Dim matrix_wise As Vector()
+
+        Public Function Sum() As Vector
+            Return matrix_wise.Select(Function(xi) xi.Sum).AsVector
+        End Function
+
+        Public Shared Function RowWise(m As GeneralMatrix) As WiseOperation
+            Return New WiseOperation With {
+                .matrix_wise = m.RowVectors.ToArray
+            }
+        End Function
+
+        Public Shared Function ColWise(m As GeneralMatrix) As WiseOperation
+            Return New WiseOperation With {
+                .matrix_wise = m.ColumnDimension _
+                    .Sequence _
+                    .Select(Function(i)
+                                Return m.ColumnVector(i)
+                            End Function) _
+                    .ToArray
+            }
+        End Function
+
+    End Class
+End Namespace
