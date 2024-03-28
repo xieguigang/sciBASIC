5e2ce2d72e8fd5f2f222cb798a120198045ac944
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-28 14:22:24 +0800

    hhow to stream the clr object vector to fill a data table

---
+++ Data/DataFrame.Extensions/DataTableStream.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module DataTableStream
+
+    <Extension>
+    Public Sub StreamTo(Of T As Class)(list As IEnumerable(Of T), table As DataTable)
+
+    End Sub
+End Module
