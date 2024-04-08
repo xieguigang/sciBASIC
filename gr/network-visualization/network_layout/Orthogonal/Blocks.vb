ed2d57d99ccca4cea2a064a02af968bb2d7558cf
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-08 15:59:53 +0800

    make code style improvements

--- gr/network-visualization/network_layout/Orthogonal/Blocks.vb
+++ gr/network-visualization/network_layout/Orthogonal/Blocks.vb
@@ -14,26 +14,26 @@ Namespace Orthogonal
     ''' @author santi
     ''' </summary>
     Public Class Blocks
-        Public Shared DEBUG As Integer = 0
 
-        Private Shared Function single_node(graph As Integer()()) As Pair(Of Dictionary(Of Integer, IList(Of Integer)), Dictionary(Of Integer, IList(Of Integer)))
-            If DEBUG >= 2 Then
-                Console.WriteLine("Blocks: special case of a graph with a single node.")
-            End If
+        Private Shared Function single_node() As Pair(Of Dictionary(Of Integer, IList(Of Integer)), Dictionary(Of Integer, IList(Of Integer)))
             ' special case:
-            Dim blocks As Dictionary(Of Integer, IList(Of Integer)) = New Dictionary(Of Integer, IList(Of Integer))()
-            Dim cutNodes As Dictionary(Of Integer, IList(Of Integer)) = New Dictionary(Of Integer, IList(Of Integer))()
-            Dim l As IList(Of Integer) = New List(Of Integer)()
-            l.Add(1)
+            Dim blocks As New Dictionary(Of Integer, IList(Of Integer))()
+            Dim cutNodes As New Dictionary(Of Integer, IList(Of Integer))()
+            Dim l As New List(Of Integer)() From {1}
+            VBDebugger.EchoLine("Blocks: special case of a graph with a single node.")
             blocks(0) = l
             Return New Pair(Of Dictionary(Of Integer, IList(Of Integer)), Dictionary(Of Integer, IList(Of Integer)))(blocks, cutNodes)
         End Function
 
         Public Shared Function blocks(graph As Integer()()) As Pair(Of Dictionary(Of Integer, IList(Of Integer)), Dictionary(Of Integer, IList(Of Integer)))
             If graph.Length = 1 Then
-                Return single_node(graph)
+                Return single_node()
+            Else
+                Return blocksInternal(graph)
             End If
+        End Function
 
+        Private Shared Function blocksInternal(graph As Integer()()) As Pair(Of Dictionary(Of Integer, IList(Of Integer)), Dictionary(Of Integer, IList(Of Integer)))
             ' the following code assumes that nodes are numbered 1,2,... (i.e. they start at 1 not at 0!)
             ' this is because the algorithms uses node numebers and their negative to determine
             ' certain properties of the graph, and 0 and its negative are the same, so it wouldn't work
@@ -62,12 +62,8 @@ Namespace Orthogonal
             While U.Count > 0
                 '            for(int i = 1;i<=n;i++) A[i] = false;
 
-                Dim toAddInU As IList(Of Integer) = New List(Of Integer)()
-
+                Dim toAddInU As New List(Of Integer)()
                 Dim v = U.PopAt(U.Count - 1) ' get the last element of U
-                If DEBUG >= 2 Then
-                    Console.WriteLine("  v = " & v.ToString())
-                End If
                 Dim lL = 0
                 For z = 1 To n
                     If graph(v - 1)(z - 1) = 1 Then ' we have to subtract 1, since in 'graph' nodes start from 0
@@ -78,12 +74,6 @@ Namespace Orthogonal
                             p(z) = v
                             d(z) = d(v) + 1
                             b(z) = -z
-                            If DEBUG >= 2 Then
-                                Console.WriteLine("  p[" & z.ToString() & "] = " & v.ToString())
-                            End If
-                            If DEBUG >= 2 Then
-                                Console.WriteLine("  (" & z.ToString() & "," & p(z).ToString() & ") = " & b(z).ToString())
-                            End If
                         End If
                     End If
                 Next
@@ -91,50 +81,32 @@ Namespace Orthogonal
                     If graph(v - 1)(z - 1) = 1 Then ' we have to subtract 1, since in 'graph' nodes start from 0
                         If U.Contains(z) Then
                             ' fundamental cycle detected:
-                            If DEBUG >= 2 Then
-                                Console.WriteLine("  Cycle: " & v.ToString() & " -> " & z.ToString() & " -> " & p(z).ToString() & " + Q")
-                            End If
                             Dim q = b(z)
                             b(z) = v
-                            If DEBUG >= 2 Then
-                                Console.WriteLine("  (" & z.ToString() & "," & p(z).ToString() & ") = " & b(z).ToString())
-                            End If
+
                             If q > 0 Then
                                 A(q) = True
                             End If
                             lL = std.Max(lL, d(v) - d(p(z)))
-                            If DEBUG >= 2 Then
-                                Console.WriteLine("  L = " & lL.ToString())
-                            End If
                         End If
                     End If
                 Next
                 If lL > 0 Then
-                    If DEBUG >= 2 Then
-                        Console.WriteLine("  Consolidating...")
-                    End If
+                    VBDebugger.EchoLine("  Consolidating...")
+
                     Dim k = v
                     Dim l = 0
                     While k <> treeroot AndAlso l < lL
-                        If DEBUG >= 2 Then
-                            Console.WriteLine("  edge: (" & k.ToString() & "," & p(k).ToString() & ") with b[" & k.ToString() & "]=" & b(k).ToString())
-                        End If
                         If b(k) > 0 Then
                             A(b(k)) = True
                         End If
                         b(k) = v
-                        If DEBUG >= 2 Then
-                            Console.WriteLine("  (" & k.ToString() & "," & p(k).ToString() & ") = " & b(k).ToString())
-                        End If
                         k = p(k)
                         l += 1
                     End While
                     For t = 1 To n
                         If b(t) > 0 AndAlso A(b(t)) Then
                             b(t) = v
-                            If DEBUG >= 2 Then
-                                Console.WriteLine("  (" & t.ToString() & "," & p(t).ToString() & ") = " & b(t).ToString())
-                            End If
                         End If
                     Next
                 End If
@@ -198,9 +170,7 @@ Namespace Orthogonal
                 End If
             Next
 
-
             Return New Pair(Of Dictionary(Of Integer, IList(Of Integer)), Dictionary(Of Integer, IList(Of Integer)))(lBlocks, cutNodes)
         End Function
     End Class
-
 End Namespace
