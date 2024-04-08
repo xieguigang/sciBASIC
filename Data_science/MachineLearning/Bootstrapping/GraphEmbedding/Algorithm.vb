9bd5a05ae4dd3d49968919628fc76bef26e75596
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-08 23:50:15 +0800

    add dev notes

--- Data_science/MachineLearning/Bootstrapping/GraphEmbedding/Algorithm.vb
+++ Data_science/MachineLearning/Bootstrapping/GraphEmbedding/Algorithm.vb
@@ -1,5 +1,39 @@
 ﻿Namespace GraphEmbedding
 
+    ''' <summary>
+    ''' "Improving Knowledge Graph Embedding Using Simple Constraints" (ACL-2018)
+    ''' 
+    ''' ### Parameters
+    ''' 
+    ''' You can changes parameter when training the model
+    ''' 
+    ''' ```
+    ''' k = number of dimensions
+    ''' lmbda = L2 regularization coffecient
+    ''' neg = number of negative samples
+    ''' mu = AER regularization coffecient
+    ''' ```
+    ''' 
+    ''' ### Citation
+    ''' 
+    ''' ```
+    ''' @inproceedings{boyang2018:aer,
+    ''' 	author = {Ding, Boyang and Wang, Quan and Wang, Bin and Guo, Li},
+    ''' 	booktitle = {56th Annual Meeting of the Association for Computational Linguistics},
+    ''' 	title = {Improving Knowledge Graph Embedding Using Simple Constraints},
+    ''' 	year = {2018}
+    ''' }
+    ''' ```
+    ''' 
+    ''' ### Contact
+    ''' 
+    ''' For all remarks or questions please contact Quan Wang: wangquan (at) iie (dot) ac (dot) cn .
+    ''' </summary>
+    ''' <remarks>
+    ''' * [Improving Knowledge Graph Embedding Using Simple Constraints](https://arxiv.org/abs/1805.02408). Boyang Ding, Quan Wang, Bin Wang and Li Guo. ACL 2018.
+    ''' * [Complex Embeddings for Simple Link Prediction](http://proceedings.mlr.press/v48/trouillon16.pdf). Théo Trouillon, Johannes Welbl, Sebastian Riedel, Éric Gaussier and Guillaume Bouchard. ICML 2016.
+    ''' * [Regularizing Knowledge Graph Embeddings via Equivalence And Inversion Axioms](https://luca.costabello.info/docs/ECML_PKDD_2017_embeddings.pdf). Pasquale Minervini, Luca Costabello, Emir Muñoz, Vít Nováček And Pierre-Yves Vandenbussche. ECML 2017. 
+    ''' </remarks>
     Public MustInherit Class Algorithm
 
         Public m_NumFactor As Integer = 50
