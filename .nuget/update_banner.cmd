4f57f1e8232709a9b85c10281d65b51e0781bab2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-27 21:09:16 +0800

    Create update_banner.cmd

---
+++ .nuget/update_banner.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+SET R_proj=../../../R-sharp
+SET R_HOME=%R_proj%/App/net6.0
+SET REnv="%R_HOME%/R#.exe"
+SET updater=%R_proj%/studio/code_banner.R
+
+%REnv% %updater% --banner-xml ../../../../gpl3.xml --proj-folder ../
+
+git add -A
+git commit -m "update source file banner headers!"
+
+pause
