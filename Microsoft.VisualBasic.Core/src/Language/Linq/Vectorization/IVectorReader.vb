8b0ff1c2a1e597edb7d3a77bd0d4df9e0efca03c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-28 16:28:10 +0800

    not workign as expected

---
+++ Microsoft.VisualBasic.Core/src/Language/Linq/Vectorization/IVectorReader.vb
@@ -0,0 +1,9 @@
+﻿Namespace Language.Vectorization
+
+    Public Interface IVectorReader
+
+        ReadOnly Property vector As Array
+        ReadOnly Property length As Integer
+
+    End Interface
+End Namespace
