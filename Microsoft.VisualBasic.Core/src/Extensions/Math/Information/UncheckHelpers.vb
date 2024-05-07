c8f06eb0edbf857099d6db913a54c477a5f6615a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-07 14:44:49 +0800

    fix of the method reference

--- Microsoft.VisualBasic.Core/src/Extensions/Math/Information/UncheckHelpers.vb
+++ Microsoft.VisualBasic.Core/src/Extensions/Math/Information/UncheckHelpers.vb
@@ -52,21 +52,18 @@
 
 Imports System.Numerics
 Imports System.Runtime.CompilerServices
-Imports System.Runtime.InteropServices
+Imports Microsoft.VisualBasic.ApplicationServices.Debugging
 Imports Microsoft.VisualBasic.Language
 
 Namespace Math.Information
 
     Public Module UncheckHelpers
 
-        ReadOnly sizeOfInt64% = Marshal.SizeOf(Long.MaxValue)
-        ReadOnly sizeOfInt32% = Marshal.SizeOf(Integer.MaxValue)
-
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         <Extension>
         Public Function ToTruncateInt64(bi As BigInteger) As Long
             With bi.ToByteArray
-                If .Length < sizeOfInt64 Then
+                If .Length < HeapSizeOf.long Then
                     Return CType(bi, Long)
                 Else
                     Return BitConverter.ToInt64(.ByRef, Scan0)
@@ -78,7 +75,7 @@ Namespace Math.Information
         <Extension>
         Public Function ToTruncateInt32(bi As BigInteger) As Integer
             With bi.ToByteArray
-                If .Length < sizeOfInt32 Then
+                If .Length < HeapSizeOf.int Then
                     Return CType(bi, Long)
                 Else
                     Return BitConverter.ToInt32(.ByRef, Scan0)
