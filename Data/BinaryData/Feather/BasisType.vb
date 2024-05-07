fd6c3a77a950b37e01bac2cb515291ae2c319d84
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-07 17:37:21 +0800

    update namespace value

--- Data/BinaryData/Feather/BasisType.vb
+++ Data/BinaryData/Feather/BasisType.vb
@@ -1,16 +1,15 @@
 ﻿
-Namespace FeatherDotNet
+
+''' <summary>
+''' Represents whether a dataframe is using 0-based or 1-based indexing.
+''' </summary>
+Public Enum BasisType
     ''' <summary>
-    ''' Represents whether a dataframe is using 0-based or 1-based indexing.
+    ''' 1-based indexing - intended for ports from 1-based languages
     ''' </summary>
-    Public Enum BasisType
-        ''' <summary>
-        ''' 1-based indexing - intended for ports from 1-based languages
-        ''' </summary>
-        One = 1
-        ''' <summary>
-        ''' 0-based indexing - the C# standard
-        ''' </summary>
-        Zero = 2
-    End Enum
-End Namespace
+    One = 1
+    ''' <summary>
+    ''' 0-based indexing - the C# standard
+    ''' </summary>
+    Zero = 2
+End Enum
