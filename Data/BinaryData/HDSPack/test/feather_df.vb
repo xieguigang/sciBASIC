4956ac0083e9a3545dbfaba7017f37f8ccbed8de
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-08 11:32:01 +0800

    configs run test

--- Data/BinaryData/HDSPack/test/feather_df.vb
+++ Data/BinaryData/HDSPack/test/feather_df.vb
@@ -3,7 +3,7 @@
 Module feather_df
 
     Sub Main222222()
-        Using untyped = FeatherReader.ReadFromFile("G:\GCModeller\src\runtime\sciBASIC#\Data\BinaryData\Feather\examples\r-feather-test.feather", BasisType.Zero)
+        Using untyped = FeatherReader.ReadFromFile("\GCModeller\src\runtime\sciBASIC#\Data\BinaryData\Feather\examples\r-feather-test.feather", BasisType.Zero)
             Dim typed = untyped.Map(Of Boolean, Integer, Double, String, DateTimeOffset, TimeSpan, DateTime, String)()
 
             Pause()
