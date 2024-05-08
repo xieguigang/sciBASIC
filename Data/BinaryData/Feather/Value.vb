0cf82c1dd0b18b9bc113415dda2aad79482148d3
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-08 12:54:59 +0800

    cast the element data

--- Data/BinaryData/Feather/Value.vb
+++ Data/BinaryData/Feather/Value.vb
@@ -125,6 +125,136 @@ Public Class Value
                     Return Nothing
                 End If
 
+            Case GetType(Integer)
+
+                Dim val As Integer = Nothing
+
+                If TryConvert(val) Then
+                    Return val
+                Else
+                    Return Nothing
+                End If
+
+            Case GetType(Single)
+
+                Dim val As Single = Nothing
+
+                If TryConvert(val) Then
+                    Return val
+                Else
+                    Return Nothing
+                End If
+
+            Case GetType(Long)
+
+                Dim val As Long = Nothing
+
+                If TryConvert(val) Then
+                    Return val
+                Else
+                    Return Nothing
+                End If
+
+            Case GetType(Double)
+
+                Dim val As Double = Nothing
+
+                If TryConvert(val) Then
+                    Return val
+                Else
+                    Return Nothing
+                End If
+
+            Case GetType(String)
+
+                Dim val As String = Nothing
+
+                If TryConvert(val) Then
+                    Return val
+                Else
+                    Return Nothing
+                End If
+
+            Case GetType(UInteger)
+
+                Dim val As UInteger = Nothing
+
+                If TryConvert(val) Then
+                    Return val
+                Else
+                    Return Nothing
+                End If
+
+            Case GetType(ULong)
+
+                Dim val As ULong = Nothing
+
+                If TryConvert(val) Then
+                    Return val
+                Else
+                    Return Nothing
+                End If
+
+            Case GetType(Date)
+
+                Dim val As Date = Nothing
+
+                If TryConvert(val) Then
+                    Return val
+                Else
+                    Return Nothing
+                End If
+
+            Case GetType(TimeSpan)
+
+                Dim val As TimeSpan = Nothing
+
+                If TryConvert(val) Then
+                    Return val
+                Else
+                    Return Nothing
+                End If
+
+            Case GetType(Byte)
+
+                Dim val As Byte = Nothing
+
+                If TryConvert(val) Then
+                    Return val
+                Else
+                    Return Nothing
+                End If
+
+            Case GetType(SByte)
+
+                Dim val As SByte = Nothing
+
+                If TryConvert(val) Then
+                    Return val
+                Else
+                    Return Nothing
+                End If
+
+            Case GetType(Short)
+
+                Dim val As Short = Nothing
+
+                If TryConvert(val) Then
+                    Return val
+                Else
+                    Return Nothing
+                End If
+
+            Case GetType(UShort)
+
+                Dim val As UShort = Nothing
+
+                If TryConvert(val) Then
+                    Return val
+                Else
+                    Return Nothing
+                End If
+
             Case Else
                 Throw New NotImplementedException(type.FullName)
         End Select
