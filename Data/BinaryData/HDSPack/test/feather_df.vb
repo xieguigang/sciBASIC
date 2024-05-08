bc1f977f1b102fd02411cab264c7b771b554de3d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-08 13:52:40 +0800

    configs for run debug

--- Data/BinaryData/HDSPack/test/feather_df.vb
+++ Data/BinaryData/HDSPack/test/feather_df.vb
@@ -1,8 +1,21 @@
 ﻿Imports Microsoft.VisualBasic.DataStorage.FeatherFormat
+Imports Microsoft.VisualBasic.Math.Quantile
 
 Module feather_df
 
     Sub Main222222()
+        Call writeTest()
+    End Sub
+
+    Sub writeTest()
+        Using writer As New FeatherWriter("./aaa.fea1", WriteMode.Eager)
+            writer.AddColumn("row.names", {"1", "2", "3"})
+            writer.AddColumn("x", {2, 4, 8})
+            writer.AddColumn("y", {3.1, 6.2, 9.3})
+        End Using
+    End Sub
+
+    Sub readTest()
         Using untyped = FeatherReader.ReadFromFile("\GCModeller\src\runtime\sciBASIC#\Data\BinaryData\Feather\examples\r-feather-test.feather", BasisType.Zero)
             Dim typed = untyped.Map(Of Boolean, Integer, Double, String, DateTimeOffset, TimeSpan, DateTime, String)()
 
