65afc5e46d8fa407f7edc15d95ebd7148e26c89a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-13 15:03:54 +0800

    try to draw the circle size legend

---
+++ Data_science/Visualization/Plots/g/Legends/CircleSizeLegend.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging
+
+Namespace Graphic.Legend
+
+    Public Class CircleSizeLegend
+
+        Public Property radius As Integer()
+        Public Property title As String
+        Public Property titleFont As Font
+        Public Property radiusFont As Font
+        Public Property CircleStroke As Pen
+
+        Public Sub Draw(g As IGraphics, layout As Rectangle)
+
+        End Sub
+    End Class
+End Namespace
