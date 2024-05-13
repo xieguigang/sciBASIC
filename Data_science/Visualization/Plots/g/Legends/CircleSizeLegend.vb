e06cf9c4a72dfc08411d6ed40fa62f3aee926ade
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-13 15:34:55 +0800

    tweaks of the legend plot layout

--- Data_science/Visualization/Plots/g/Legends/CircleSizeLegend.vb
+++ Data_science/Visualization/Plots/g/Legends/CircleSizeLegend.vb
@@ -6,7 +6,6 @@ Namespace Graphic.Legend
     Public Class CircleSizeLegend
 
         Public Property radius As Integer()
-        Public Property radius_scale As d3js.scale.LinearScale
         Public Property title As String
         Public Property titleFont As Font
         Public Property radiusFont As Font
@@ -17,18 +16,18 @@ Namespace Graphic.Legend
             Dim tickSize As SizeF = g.MeasureString("0", radiusFont)
             Dim y As Single = layout.Top + titleSize.Height * 2
             Dim r As Single
-            Dim max_r As Single = radius_scale(radius.Max)
+            Dim max_r As Single = radius.Max
             Dim tick_left As Single = layout.Left + max_r * 3
             Dim left As Single = layout.Left
-            Dim dy As Single = radius_scale(radius.Min) / 5
+            Dim dy As Single = radius.Average
 
             Call g.DrawString(title, titleFont, Brushes.Black, layout.Left, layout.Top)
 
             For Each radius As Integer In Me.radius
-                r = radius_scale(radius)
+                r = radius * 2
                 y += r
 
-                Call g.DrawEllipse(circleStroke, New RectangleF(left + r, y - r, r, r))
+                Call g.DrawEllipse(circleStroke, New RectangleF(left, y - r, r, r))
                 Call g.DrawString(radius, radiusFont, Brushes.Black, New PointF(tick_left, y - tickSize.Height / 2))
 
                 y += dy
