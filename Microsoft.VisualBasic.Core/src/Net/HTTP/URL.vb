b40ba14441dca9fc82b0dcd486c217e1abbf6805
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-21 19:27:16 +0800

    make imp[rovements of the url model

--- Microsoft.VisualBasic.Core/src/Net/HTTP/URL.vb
+++ Microsoft.VisualBasic.Core/src/Net/HTTP/URL.vb
@@ -1,62 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::3bc74fb830b377ea381d36695e990f9d, Microsoft.VisualBasic.Core\src\Net\HTTP\URL.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 179
-    '    Code Lines: 132
-    ' Comment Lines: 25
-    '   Blank Lines: 22
-    '     File Size: 6.62 KB
+' Summaries:
 
 
-    '     Class URL
-    ' 
-    '         Properties: hashcode, Host, hostName, path, port
-    '                     protocol, query
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    ' 
-    '         Function: BuildUrl, GetValues, Parse, ToString, UrlQueryString
-    ' 
-    '         Sub: Parser
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 179
+'    Code Lines: 132
+' Comment Lines: 25
+'   Blank Lines: 22
+'     File Size: 6.62 KB
+
+
+'     Class URL
+' 
+'         Properties: hashcode, Host, hostName, path, port
+'                     protocol, query
+' 
+'         Constructor: (+2 Overloads) Sub New
+' 
+'         Function: BuildUrl, GetValues, Parse, ToString, UrlQueryString
+' 
+'         Sub: Parser
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language
@@ -130,12 +131,27 @@ Namespace Net.Http
             Return query.Select(Function(q) q.Value.Select(Function(val) $"{q.Key}={UrlEncode(val)}")).IteratesALL.JoinBy("&")
         End Function
 
-        Public Function Url() As String
-            Return $"/{path}?{UrlQueryString()}#{hashcode}"
+        Public Overloads Function ToString(addHostName As Boolean) As String
+            Dim host_prefix As String = $"{protocol}{hostName}:{port}"
+            Dim url As String = $"/{path}?{UrlQueryString()}#{hashcode}".Trim("#"c, "?"c)
+            Dim is_file As Boolean = (Not path.StringEmpty) AndAlso
+                (path.Last <> "/"c) AndAlso
+                (Not path.ExtensionSuffix.StringEmpty)
+
+            If is_file Then
+                url = "/" & url.Trim("/"c)
+            End If
+
+            If addHostName Then
+                Return host_prefix & url
+            Else
+                Return url
+            End If
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Overrides Function ToString() As String
-            Return $"{protocol}{hostName}:{port}/{path}?{UrlQueryString()}#{hashcode}"
+            Return ToString(addHostName:=True)
         End Function
 
         Private Shared Sub Parser(url As String,
