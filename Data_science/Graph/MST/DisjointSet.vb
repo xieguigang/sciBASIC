1b3a061261fcb9896558e431745481f4de1875d9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-22 22:04:34 +0800

    add MST source code

---
+++ Data_science/Graph/MST/DisjointSet.vb
@@ -0,0 +1,158 @@
+﻿Imports System
+Imports System.Collections.Generic
+
+Namespace MinimumSpanningTree
+    Friend Class DisjointSet
+        Private parents As Integer()
+        Private rank As Integer()
+
+        Public Sub New(Optional max_heap As Integer = 4096)
+            parents = New Integer(max_heap - 1) {}
+            rank = New Integer(max_heap - 1) {}
+        End Sub
+
+        Public Sub makeset([set] As Integer)
+            parents([set]) = [set]
+            rank([set]) = 0
+        End Sub
+
+        Public Function findset(node As Integer) As Integer
+            Dim root = node
+            While root <> parents(root)
+                root = parents(root)
+            End While
+            Dim j = parents(node) ' j == parent of 'node'
+            While j <> root
+                parents(node) = root
+                node = j
+                j = parents(node)
+            End While
+            Return root
+        End Function
+
+        Public Sub union(i As Integer, j As Integer)
+            mergetrees(findset(i), findset(j))
+        End Sub
+
+        Private Sub mergetrees(firstTree As Integer, secondTree As Integer)
+            If rank(firstTree) < rank(secondTree) Then
+                parents(firstTree) = secondTree
+            ElseIf rank(firstTree) > rank(secondTree) Then
+                parents(secondTree) = firstTree
+            Else
+                parents(firstTree) = secondTree
+                rank(secondTree) += 1
+            End If
+        End Sub
+
+        Public Sub printSets()
+            Dim djSets As DJSets = New DJSets()
+
+            Dim i = 0
+
+            While i < parents.Length
+                If parents(i) <> 0 Then
+                    Dim root = findset(i)
+                    If Not djSets.inDJSets(root) Then
+                        djSets.addSet(root)
+                        djSets.insertIntoSet(root, i)
+                    Else
+                        djSets.insertIntoSet(root, i)
+                    End If
+                End If
+
+                Threading.Interlocked.Increment(i)
+            End While
+            djSets.printSets()
+        End Sub
+
+        Public Sub printArray()
+            For i = 0 To parents.Length - 1
+                Console.Write("{0} ", parents(i))
+            Next
+            Console.Write(Microsoft.VisualBasic.Constants.vbLf)
+        End Sub
+    End Class
+
+    Friend Class DJSet
+        Private rootField As Integer
+        Private [set] As HashSet(Of Integer)
+
+        Public Sub New(root As Integer)
+            [set] = New HashSet(Of Integer)()
+            rootField = root
+        End Sub
+
+        Public Sub add(i As Integer)
+            [set].Add(i)
+        End Sub
+
+
+        Public Property Root As Integer
+            Get
+                Return rootField
+            End Get
+            Set(value As Integer)
+                rootField = value
+            End Set
+        End Property
+
+        Public Sub print()
+            Dim firstTime = True
+
+            Console.Write("{")
+            For Each i In [set]
+                If firstTime Then
+                    firstTime = False
+                    Console.Write(i)
+                Else
+                    Console.Write(",{0}", i)
+                End If
+            Next
+            Console.Write("}")
+        End Sub
+    End Class
+
+    Friend Class DJSets
+        Private djSets As List(Of DJSet)
+
+        Public Sub New()
+            djSets = New List(Of DJSet)()
+        End Sub
+
+        Public Sub printSets()
+            Dim firstTime = True
+            For Each djSet In djSets
+                If firstTime Then
+                    firstTime = False
+                    djSet.print()
+                Else
+                    Console.Write(",  ")
+                    djSet.print()
+                End If
+            Next
+            Console.WriteLine()
+        End Sub
+
+        Public Sub addSet(root As Integer)
+            djSets.Add(New DJSet(root))
+        End Sub
+
+        Public Function inDJSets(root As Integer) As Boolean
+            For Each djSet In djSets
+                If djSet.Root = root Then
+                    Return True
+                End If
+            Next
+            Return False
+        End Function
+
+        Public Sub insertIntoSet(root As Integer, i As Integer)
+            For Each djSet In djSets
+                If djSet.Root = root Then
+                    djSet.add(i)
+                End If
+            Next
+        End Sub
+    End Class
+End Namespace
---
+++ Data_science/Graph/MST/Kruskal.vb
@@ -0,0 +1,59 @@
+﻿Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Scripting.Expressions
+
+Namespace MinimumSpanningTree
+
+    ''' <summary>
+    ''' Minimum spanning tree via Kruskal algorithm
+    ''' </summary>
+    Public Class Kruskal
+
+        Private djset As New DisjointSet()
+        Private edge As New List(Of VertexEdge)
+        Private numNodes As Integer
+
+        Public Sub New(edges As IEnumerable(Of VertexEdge))
+            edge = edges.AsList
+            edge.Sort(Function(e1, e2)
+                          If e1.weight > e2.weight Then Return 1
+                          If e1.weight < e2.weight Then Return -1
+                          Return 0
+                      End Function)
+
+            numNodes = calculateNumNodes(edge)
+        End Sub
+
+        Public Function calculateNumNodes(edges As IEnumerable(Of VertexEdge)) As Integer
+            Return (From i In edges.AsEnumerable Select NodeIdTuple(i)).IteratesALL.Distinct.Count
+        End Function
+
+        Private Iterator Function NodeIdTuple(i As VertexEdge) As IEnumerable(Of Integer)
+            Yield i.U.ID
+            Yield i.V.ID
+        End Function
+
+        Public Iterator Function findMinTree() As IEnumerable(Of VertexEdge)
+            Dim i = 1
+
+            While i <= numNodes
+                djset.makeset(i)
+                i += 1
+            End While
+
+            Dim count = 0
+            Dim idx = 0
+
+            While count < numNodes - 1
+                If djset.findset(edge(idx).V.ID) <> djset.findset(edge(idx).U.ID) Then
+                    Yield edge(idx)
+
+                    count += 1
+                    djset.union(edge(idx).V.ID, edge(idx).U.ID)
+                End If
+
+                idx += 1
+            End While
+        End Function
+    End Class
+End Namespace
