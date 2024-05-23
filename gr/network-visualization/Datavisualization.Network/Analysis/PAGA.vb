be03b9ce13033c352578f504961bf3f2b306f840
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-23 17:01:01 +0800

    make graph abstraction

--- gr/network-visualization/Datavisualization.Network/Analysis/PAGA.vb
+++ gr/network-visualization/Datavisualization.Network/Analysis/PAGA.vb
@@ -2,6 +2,7 @@
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream.Generic
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
 
 Namespace Analysis
 
@@ -22,11 +23,29 @@ Namespace Analysis
         <Extension>
         Public Function Abstraction(manifolds As NetworkGraph) As NetworkGraph
             ' split the nodes by node type
-            Dim clusters = manifolds.vertex.GroupBy(Function(v) v(NamesOf.REFLECTION_ID_MAPPING_NODETYPE)).ToArray
+            Dim clusters = manifolds.vertex _
+                .GroupBy(Function(v)
+                             Return v(NamesOf.REFLECTION_ID_MAPPING_NODETYPE)
+                         End Function) _
+                .ToArray
             Dim abstract As New NetworkGraph
 
             For Each cluster As IGrouping(Of String, Node) In clusters
+                Dim node_group = cluster.ToArray
+                Dim x As Double = Aggregate v In node_group Into Sum(v.data.initialPostion.x)
+                Dim y As Double = Aggregate v In node_group Into Sum(v.data.initialPostion.y)
 
+                Call abstract.CreateNode(cluster.Key, New NodeData With {
+                    .initialPostion = New FDGVector2(x, y),
+                    .label = cluster.Key,
+                    .mass = node_group.Length,
+                    .origID = cluster.Key,
+                    .size = {node_group.Length},
+                    .weights = {node_group.Length},
+                    .Properties = New Dictionary(Of String, String) From {
+                        {NamesOf.REFLECTION_ID_MAPPING_NODETYPE, cluster.Key}
+                    }
+                })
             Next
 
             Return abstract
