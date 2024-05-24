e3577e5eb7416f2390bc4553534d7a13cf91bff8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-24 16:19:04 +0800

    query docuiments

--- Data/FullTextSearch/InvertedIndex.vb
+++ Data/FullTextSearch/InvertedIndex.vb
@@ -1,4 +1,5 @@
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
 
 ''' <summary>
 ''' The core of FTS is a data structure called Inverted Index. 
@@ -6,7 +7,8 @@ Imports Microsoft.VisualBasic.Language
 ''' </summary>
 Public Class InvertedIndex
 
-    ReadOnly index As Dictionary(Of String, List(Of Integer))
+    ReadOnly index As New Dictionary(Of String, List(Of Integer))
+    ReadOnly rawDocs As New List(Of String)
 
     Dim id As i32 = 0
 
@@ -16,7 +18,7 @@ Public Class InvertedIndex
         Next
     End Sub
 
-    Private Sub add(doc As String)
+    Public Sub Add(doc As String)
         Dim id As Integer
         Dim tokens As String() = split(doc)
 
@@ -33,6 +35,8 @@ Public Class InvertedIndex
 
             Call index(str).Add(id)
         Next
+
+        Call rawDocs.Add(doc)
     End Sub
 
     Private Function split(doc As String) As String()
@@ -46,22 +50,60 @@ Public Class InvertedIndex
     End Function
 
     ''' <summary>
-    ''' Querying
+    ''' Boolean queries
     ''' </summary>
     ''' <param name="text"></param>
     ''' <returns></returns>
-    Public Iterator Function Search(text As String) As IEnumerable(Of IReadOnlyCollection(Of Integer))
+    Public Iterator Function Search(text As String) As IEnumerable(Of SeqValue(Of String))
         Dim tokens As String() = split(text)
 
         If tokens.IsNullOrEmpty Then
             Return
         End If
 
+        Dim r As List(Of Integer) = Nothing
+
         For Each token As String In tokens
             If index.ContainsKey(token) Then
-                Yield DirectCast(index(token), IReadOnlyCollection(Of Integer))
+                If r Is Nothing Then
+                    r = New List(Of Integer)(index(token))
+                Else
+                    r = intersection(r, index(token))
+                End If
+            Else
+                ' Token doesn't exist.
+                Return
             End If
         Next
+
+        For Each i As Integer In r
+            Yield New SeqValue(Of String)(i, rawDocs(i))
+        Next
+    End Function
+
+    Private Shared Function intersection(a As List(Of Integer), b As List(Of Integer)) As List(Of Integer)
+        Dim maxLen = a.Count
+
+        If b.Count > maxLen Then
+            maxLen = b.Count
+        End If
+
+        Dim r As New List(Of Integer)
+        Dim i, j As Integer
+
+        Do While i < a.Count AndAlso j < b.Count
+            If a(i) < b(j) Then
+                i += 1
+            ElseIf a(i) > b(j) Then
+                j += 1
+            Else
+                r.Add(a(i))
+                i += 1
+                j += 1
+            End If
+        Loop
+
+        Return r
     End Function
 
 End Class
