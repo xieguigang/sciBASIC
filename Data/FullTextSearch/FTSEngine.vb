c3abd0508163fae885043da655c65df9f83a8a2b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-24 17:14:38 +0800

    call document save and close

--- Data/FullTextSearch/FTSEngine.vb
+++ Data/FullTextSearch/FTSEngine.vb
@@ -1,10 +1,13 @@
 ﻿Imports System.IO
 Imports Microsoft.VisualBasic.Linq
 
-Public Class FTSEngine
+Public Class FTSEngine : Implements IDisposable
 
     ReadOnly index As InvertedIndex
     ReadOnly documents As FileStorage
+    ReadOnly repo_dir As String
+
+    Private disposedValue As Boolean
 
     Sub New(repo_dir As String)
         Dim offsets As Long() = Nothing
@@ -42,4 +45,31 @@ Public Class FTSEngine
             Yield New SeqValue(Of String)(id, documents.GetDocument(id))
         Next
     End Function
+
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: 释放托管状态(托管对象)
+                Call FileStorage.WriteIndex(index, documents.AsEnumerable.ToArray, $"{repo_dir}/index.dat".Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False))
+                Call documents.Dispose()
+            End If
+
+            ' TODO: 释放未托管的资源(未托管的对象)并重写终结器
+            ' TODO: 将大型字段设置为 null
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: 仅当“Dispose(disposing As Boolean)”拥有用于释放未托管资源的代码时才替代终结器
+    ' Protected Overrides Sub Finalize()
+    '     ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
 End Class
--- Data/FullTextSearch/FileStorage.vb
+++ Data/FullTextSearch/FileStorage.vb
@@ -8,11 +8,13 @@ Imports Microsoft.VisualBasic.Linq
 ''' <summary>
 ''' document text file 
 ''' </summary>
-Public Class FileStorage
+Public Class FileStorage : Implements Enumeration(Of Long), IDisposable
 
     ReadOnly doc_stream As BinaryDataReader
     ReadOnly file As Stream
     ReadOnly offsets As New List(Of Long)
+
+    Private disposedValue As Boolean
 
     Sub New(offsets As Long(), doc As Stream)
         Me.offsets = New List(Of Long)(offsets)
@@ -85,4 +87,37 @@ Public Class FileStorage
         Call bin.Dispose()
     End Sub
 
+    Public Iterator Function GenericEnumerator() As IEnumerator(Of Long) Implements Enumeration(Of Long).GenericEnumerator
+        For Each offset_l As Long In offsets
+            Yield offset_l
+        Next
+    End Function
+
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: 释放托管状态(托管对象)
+                Call file.Flush()
+                Call file.Close()
+                Call file.Dispose()
+            End If
+
+            ' TODO: 释放未托管的资源(未托管的对象)并重写终结器
+            ' TODO: 将大型字段设置为 null
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: 仅当“Dispose(disposing As Boolean)”拥有用于释放未托管资源的代码时才替代终结器
+    ' Protected Overrides Sub Finalize()
+    '     ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
 End Class
