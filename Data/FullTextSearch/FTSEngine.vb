1db5308ca9b920b4f9a2be1e7a217d45224e6b7f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-24 16:59:32 +0800

    test create index

--- Data/FullTextSearch/FTSEngine.vb
+++ Data/FullTextSearch/FTSEngine.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.IO
+Imports Microsoft.VisualBasic.Linq
 
 Public Class FTSEngine
 
@@ -12,6 +13,12 @@ Public Class FTSEngine
         documents = New FileStorage(offsets, $"{repo_dir}/documents.dat".Open(FileMode.OpenOrCreate, doClear:=False, [readOnly]:=False))
     End Sub
 
+    Public Sub Indexing(doc As IEnumerable(Of String))
+        For Each par As String In doc
+            Call Indexing(par)
+        Next
+    End Sub
+
     ''' <summary>
     ''' 
     ''' </summary>
@@ -23,4 +30,16 @@ Public Class FTSEngine
         Call index.Add(doc)
         Call documents.Save(doc)
     End Sub
+
+    Public Iterator Function Search(text As String) As IEnumerable(Of SeqValue(Of String))
+        Dim ids = index.Search(text)
+
+        If ids Is Nothing Then
+            Return
+        End If
+
+        For Each id As Integer In ids
+            Yield New SeqValue(Of String)(id, documents.GetDocument(id))
+        Next
+    End Function
 End Class
--- Data/FullTextSearch/test/Program.vb
+++ Data/FullTextSearch/test/Program.vb
@@ -1,19 +1,38 @@
 Imports System
 Imports Microsoft.VisualBasic.Data.FullTextSearch
+Imports Microsoft.VisualBasic.Linq
 
 Module Program
 
-    Sub Main(args As String())
-        Dim doc = "E:\GCModeller\src\runtime\sciBASIC#\Data\TextRank\Beauty_and_the_Beast.txt".ReadAllLines
-        Dim index As New InvertedIndex
+    ReadOnly demo_repo As String = $"{App.HOME}/demo_data/"
 
-        Call index.Add(doc)
+    Sub Main(args As String())
+        Call test_create()
+        Call test_read()
+    End Sub
+
+    Sub test_read()
+        Dim index As New FTSEngine(demo_repo)
+
+        For Each hit As SeqValue(Of String) In index.Search("princess  walking")
+            Call Console.WriteLine(hit.value)
+        Next
+    End Sub
+
+    Sub test_create()
+        Dim doc_1 = "E:\GCModeller\src\runtime\sciBASIC#\Data\TextRank\Beauty_and_the_Beast.txt".ReadAllLines
+        Dim doc_2 = "E:\GCModeller\src\runtime\sciBASIC#\Data\TextRank\Rapunzel.txt".ReadAllLines
+
+        Dim index As New FTSEngine(demo_repo)
+
+        Call index.Indexing(doc_1)
+        Call index.Indexing(doc_2)
 
         'For Each hit In index.Search("baby girl")
         '    Call Console.WriteLine(hit.value)
         'Next
 
-        For Each hit In index.Search("princess  walking")
+        For Each hit As SeqValue(Of String) In index.Search("princess  walking")
             Call Console.WriteLine(hit.value)
         Next
     End Sub
