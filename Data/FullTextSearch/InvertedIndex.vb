c9bd1b80ad63808e4ecb5576cd3f1c09c4b42605
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-24 16:09:40 +0800

    query index

--- Data/FullTextSearch/InvertedIndex.vb
+++ Data/FullTextSearch/InvertedIndex.vb
@@ -1,5 +1,3 @@
-
-Imports Microsoft.VisualBasic.Data.NLP.Model
 Imports Microsoft.VisualBasic.Language
 
 ''' <summary>
@@ -20,17 +18,9 @@ Public Class InvertedIndex
 
     Private Sub add(doc As String)
         Dim id As Integer
-        Dim tokens As String()
+        Dim tokens As String() = split(doc)
 
-        doc = Strings.Trim(doc).ToLower
-
-        If doc.StringEmpty Then
-            Return
-        Else
-            tokens = doc.Split({"+"c, "-"c, "*"c, "/"c, "."c, ","c})
-        End If
-
-        If tokens.Length = 0 Then
+        If tokens.IsNullOrEmpty Then
             Return
         Else
             id = ++Me.id
@@ -44,5 +34,34 @@ Public Class InvertedIndex
             Call index(str).Add(id)
         Next
     End Sub
+
+    Private Function split(doc As String) As String()
+        doc = Strings.Trim(doc).ToLower
+
+        If doc.StringEmpty Then
+            Return Nothing
+        Else
+            Return doc.Split({"+"c, "-"c, "*"c, "/"c, "."c, ","c})
+        End If
+    End Function
+
+    ''' <summary>
+    ''' Querying
+    ''' </summary>
+    ''' <param name="text"></param>
+    ''' <returns></returns>
+    Public Iterator Function Search(text As String) As IEnumerable(Of IReadOnlyCollection(Of Integer))
+        Dim tokens As String() = split(text)
+
+        If tokens.IsNullOrEmpty Then
+            Return
+        End If
+
+        For Each token As String In tokens
+            If index.ContainsKey(token) Then
+                Yield DirectCast(index(token), IReadOnlyCollection(Of Integer))
+            End If
+        Next
+    End Function
 
 End Class
