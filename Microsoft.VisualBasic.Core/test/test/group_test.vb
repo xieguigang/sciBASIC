e74dd3928934fa7e365b1b61c310451d24d16e66
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-27 08:58:14 +0800

    config run group test

---
+++ Microsoft.VisualBasic.Core/test/test/group_test.vb
@@ -0,0 +1,27 @@
+﻿Imports Microsoft.VisualBasic.Text
+Imports randf = Microsoft.VisualBasic.Math.RandomExtensions
+
+Public Module group_test
+
+    Sub RunGroup()
+        Dim asc_chars = ASCII.AlphaNumericTable.Keys.Select(Function(c) c.ToString).ToArray
+        Dim chars = Enumerable.Range(0, 700000).Select(Function(a) randf.Next(asc_chars)).ToArray
+
+        Dim groupBy = chars.GroupBy(Function(s) s).ToDictionary(Function(s) s.Key, Function(s) s.Count)
+        Dim dict_group As New Dictionary(Of String, List(Of String))
+
+        For Each c As String In chars
+            If Not dict_group.ContainsKey(c) Then
+                dict_group.Add(c, New List(Of String))
+            End If
+
+            dict_group(c).Add(c)
+        Next
+
+        Dim groupByDict = dict_group.ToDictionary(Function(c) c.Key, Function(c) c.Value.Count)
+
+
+        Pause()
+    End Sub
+
+End Module
