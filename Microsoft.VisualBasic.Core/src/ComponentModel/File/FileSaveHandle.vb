35ff3234ea2351aaed63d01ce8636b0e810f92ca
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-28 15:02:33 +0800

    make improvements of the file save handler

--- Microsoft.VisualBasic.Core/src/ComponentModel/File/FileSaveHandle.vb
+++ Microsoft.VisualBasic.Core/src/ComponentModel/File/FileSaveHandle.vb
@@ -1,61 +1,62 @@
 ﻿#Region "Microsoft.VisualBasic::011fa2291fe5dd6f7fc89410a001045d, Microsoft.VisualBasic.Core\src\ComponentModel\File\FileSaveHandle.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 40
-    '    Code Lines: 13 (32.50%)
-    ' Comment Lines: 22 (55.00%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 5 (12.50%)
-    '     File Size: 1.51 KB
+' Summaries:
 
 
-    '     Interface ISaveHandle
-    ' 
-    '         Function: (+2 Overloads) Save
-    ' 
-    '     Interface IFileReference
-    ' 
-    '         Properties: FilePath, MimeType
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 40
+'    Code Lines: 13 (32.50%)
+' Comment Lines: 22 (55.00%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 5 (12.50%)
+'     File Size: 1.51 KB
+
+
+'     Interface ISaveHandle
+' 
+'         Function: (+2 Overloads) Save
+' 
+'     Interface IFileReference
+' 
+'         Properties: FilePath, MimeType
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.IO
 Imports System.Text
 Imports Microsoft.VisualBasic.Net.Protocols.ContentTypes
 Imports Microsoft.VisualBasic.Text
@@ -63,26 +64,29 @@ Imports Microsoft.VisualBasic.Text
 Namespace ComponentModel
 
     ''' <summary>
-    ''' This is a file object which have a handle to save its data to the filesystem.(这是一个带有文件数据保存方法的文件模型)
+    ''' This is a file object which have a handle to save its data to the filesystem.
     ''' </summary>
-    ''' <remarks></remarks>
+    ''' <remarks>(这是一个带有文件数据保存方法的文件模型)</remarks>
     Public Interface ISaveHandle
 
         ''' <summary>
-        ''' Handle for saving the file data.(保存文件的方法)
+        ''' Handle for saving the file data.
         ''' </summary>
         ''' <param name="path">The file path that will save data to.(进行文件数据保存的文件路径)</param>
         ''' <param name="encoding">The text encoding value for the text document.(文本文档的编码格式)</param>
         ''' <returns></returns>
-        ''' <remarks></remarks>
+        ''' <remarks>(保存文件的方法)</remarks>
         Function Save(path$, encoding As Encoding) As Boolean
         Function Save(path$, Optional encoding As Encodings = Encodings.UTF8) As Boolean
+        Function Save(file As Stream, encoding As Encoding) As Boolean
+
     End Interface
 
     ''' <summary>
     ''' 表示一个对文件的引用接口
     ''' </summary>
     Public Interface IFileReference
+
         ''' <summary>
         ''' 进行文件引用的路径
         ''' </summary>
@@ -94,5 +98,6 @@ Namespace ComponentModel
         ''' </summary>
         ''' <returns></returns>
         ReadOnly Property MimeType As ContentType()
+
     End Interface
 End Namespace
