cd180e1cebda8c1099adfd1bf93891f71cf59f14
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-29 21:02:55 +0800

    split source file into multiple parts

---
+++ Microsoft.VisualBasic.Core/src/ApplicationServices/Terminal/Utility/UnixMan/ManFile/ManIndex.vb
@@ -0,0 +1,20 @@
+﻿
+Namespace ApplicationServices.Terminal.Utility
+
+    ''' <summary>
+    ''' the document index
+    ''' </summary>
+    Public Class ManIndex
+
+        Public Property index As String
+        Public Property category As Integer
+        Public Property [date] As Date = Now
+        Public Property keyword As String
+        Public Property title As String
+
+        Public Overrides Function ToString() As String
+            Return $".TH {Strings.UCase(index)} {category} {[date].ToString("yyyy-MMM")} ""{keyword}"" ""{title}"""
+        End Function
+
+    End Class
+End Namespace
