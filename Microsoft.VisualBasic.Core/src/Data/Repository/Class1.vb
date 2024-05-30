08d20ab08c338be195c58820a2ad6f356fa10d18
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-30 09:45:18 +0800

    a unique Snowflake ID generator

---
+++ Microsoft.VisualBasic.Core/src/Data/Repository/Class1.vb
@@ -0,0 +1,27 @@
+﻿Namespace Data.Repository
+
+    ''' <summary>
+    ''' a unique Snowflake ID generator
+    ''' </summary>
+    Public Class SnowflakeIdGenerator
+
+        ReadOnly _epoch As Long = 1288834974657L
+        ReadOnly _machineId As Long
+
+        Dim _sequence As Long = 0L
+
+        Public Sub New(machineId As Long, Optional epoch As Long = 1288834974657L, Optional seqId As Long = 0L)
+            _machineId = machineId
+            _epoch = epoch
+            _sequence = seqId
+        End Sub
+
+        Public Function GenerateId() As Long
+            SyncLock Me
+                Dim currentTimestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
+                _sequence = _sequence + 1 And 4095
+                Return currentTimestamp - _epoch << 22 Or _machineId << 12 Or _sequence
+            End SyncLock
+        End Function
+    End Class
+End Namespace
