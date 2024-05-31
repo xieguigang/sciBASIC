8344614d25976a7ee29653f90fadbd39ea1b7e82
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-31 13:54:44 +0800

    make improvements of the size model

--- mime/text%html/CSS/Size.vb
+++ mime/text%html/CSS/Size.vb
@@ -1,63 +1,86 @@
 ﻿#Region "Microsoft.VisualBasic::8abc330ba1f0750b87805c735deb2a13, mime\text%html\CSS\Size.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 9
-    '    Code Lines: 6 (66.67%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 3 (33.33%)
-    '     File Size: 158 B
+' Summaries:
 
 
-    '     Class CSSsize
-    ' 
-    '         Properties: height, width
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 9
+'    Code Lines: 6 (66.67%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 3 (33.33%)
+'     File Size: 158 B
+
+
+'     Class CSSsize
+' 
+'         Properties: height, width
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Drawing
+
 Namespace CSS
 
+    ''' <summary>
+    ''' parse the width and height data from the css style string
+    ''' </summary>
     Public Class CSSsize
 
-        Public Property width As Double
-        Public Property height As Double
+        Public Property width As String
+        Public Property height As String
+
+        Sub New()
+        End Sub
+
+        Sub New(size As CSSsize)
+            width = size.width
+            height = size.height
+        End Sub
+
+        Sub New(size As Size)
+            width = size.Width
+            height = size.Height
+        End Sub
+
+        Sub New(size As SizeF)
+            width = size.Width
+            height = size.Height
+        End Sub
 
     End Class
 End Namespace
