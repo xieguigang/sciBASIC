cbc1a71b0743494871ce61491e669d48e901d51f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-08 00:55:51 +0800

    fix of the incorrect netmask

--- www/Microsoft.VisualBasic.NETProtocol/IPv4/IPUtils.vb
+++ www/Microsoft.VisualBasic.NETProtocol/IPv4/IPUtils.vb
@@ -1,56 +1,57 @@
 ﻿#Region "Microsoft.VisualBasic::4ba72bc2b8ec6fbd1b54905d9cd2330e, www\Microsoft.VisualBasic.NETProtocol\IPv4\IPUtils.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 90
-    '    Code Lines: 27 (30.00%)
-    ' Comment Lines: 54 (60.00%)
-    '    - Xml Docs: 75.93%
-    ' 
-    '   Blank Lines: 9 (10.00%)
-    '     File Size: 2.92 KB
+' Summaries:
 
 
-    ' Module IPUtils
-    ' 
-    '     Function: GetGeoAddress, ValidateIPAddress
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 90
+'    Code Lines: 27 (30.00%)
+' Comment Lines: 54 (60.00%)
+'    - Xml Docs: 75.93%
+' 
+'   Blank Lines: 9 (10.00%)
+'     File Size: 2.92 KB
+
+
+' Module IPUtils
+' 
+'     Function: GetGeoAddress, ValidateIPAddress
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Net
 Imports System.Runtime.CompilerServices
 Imports System.Text.RegularExpressions
 
@@ -140,4 +141,23 @@ Public Module IPUtils
 
         Return Dot2LongIP
     End Function
+
+    Public Function GetSubnetMaskFromPrefixLength(prefixLength As Integer) As IPAddress
+        ' 确保前缀长度在0到32之间
+        If prefixLength < 0 Or prefixLength > 32 Then
+            Throw New ArgumentOutOfRangeException("prefixLength", "前缀长度必须在0到32之间。")
+        End If
+
+        ' 将前缀长度转换为子网掩码
+        Dim mask As UInteger = UInteger.MaxValue << (32 - prefixLength)
+        Dim maskBytes() As Byte = BitConverter.GetBytes(mask)
+
+        ' 由于BitConverter可能会返回一个长度大于4的字节数组，我们需要将其截断或反转
+        Array.Reverse(maskBytes)
+        Dim subnetMaskBytes(3) As Byte
+        Array.Copy(maskBytes, subnetMaskBytes, 4)
+
+        Return New IPAddress(subnetMaskBytes)
+    End Function
+
 End Module
