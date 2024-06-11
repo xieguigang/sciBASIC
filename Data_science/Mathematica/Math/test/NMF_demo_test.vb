0e9f8357b7040986c7af056851f41231c4a7c5ca
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-11 22:34:01 +0800

    config run nmf demo test

---
+++ Data_science/Mathematica/Math/test/NMF_demo_test.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
+
+Module NMF_demo_test
+
+    Sub Main()
+        Dim a As New NumericMatrix({{423, 4, 23}, {2342, 3, 4}, {2, 4, 23}, {2, 423, 4}, {24, 22, 2}, {24, 2, 4.0}})
+        Dim result = NMF.Factorisation(a)
+
+        Pause()
+    End Sub
+End Module
