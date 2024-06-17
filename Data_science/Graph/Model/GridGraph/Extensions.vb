967fdce6fe8f1861ebb3370f80609bd9748ba477
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-17 11:01:17 +0800

    get layer intensuity value

---
+++ Data_science/Graph/Model/GridGraph/Extensions.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Scripting.Expressions
+
+Namespace GridGraph
+
+    <HideModuleName>
+    Public Module Extensions
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        <Extension>
+        Public Function CreateReadOnlyGrid(points As IEnumerable(Of Point)) As Grid(Of Point)
+            Return Grid(Of Point).CreateReadOnly(points.Select(Function(pt) New GridCell(Of Point)(pt, pt)))
+        End Function
+    End Module
+End Namespace
