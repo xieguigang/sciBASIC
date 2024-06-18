1a86324436e9f60a1c45a4e68b88d94f3fea239f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-18 14:17:30 +0800

    try to make improvements of the data set api

--- Data/word2vec/VectorModel.vb
+++ Data/word2vec/VectorModel.vb
@@ -1,61 +1,62 @@
 ﻿#Region "Microsoft.VisualBasic::ca2fadfb98bd62bb591752f6f03c675f, Data\word2vec\VectorModel.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 197
-    '    Code Lines: 113 (57.36%)
-    ' Comment Lines: 45 (22.84%)
-    '    - Xml Docs: 95.56%
-    ' 
-    '   Blank Lines: 39 (19.80%)
-    '     File Size: 6.55 KB
+' Summaries:
 
 
-    ' Class VectorModel
-    ' 
-    '     Properties: vectorSize, wordMap, words
-    ' 
-    '     Constructor: (+2 Overloads) Sub New
-    '     Function: analogy, GenericEnumerator, getWordVector, (+2 Overloads) similar, ToString
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 197
+'    Code Lines: 113 (57.36%)
+' Comment Lines: 45 (22.84%)
+'    - Xml Docs: 95.56%
+' 
+'   Blank Lines: 39 (19.80%)
+'     File Size: 6.55 KB
+
+
+' Class VectorModel
+' 
+'     Properties: vectorSize, wordMap, words
+' 
+'     Constructor: (+2 Overloads) Sub New
+'     Function: analogy, GenericEnumerator, getWordVector, (+2 Overloads) similar, ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.DataStructures
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Serialization.JSON
 
--- Data_science/Graph/Model/Tree/HuffmanTree/HuffmanTree.vb
+++ Data_science/Graph/Model/Tree/HuffmanTree/HuffmanTree.vb
@@ -1,58 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::43f72a1625a3fcb0f4ddeab5f9677b6b, Data_science\Graph\Model\Tree\HuffmanTree\HuffmanTree.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 34
-    '    Code Lines: 23 (67.65%)
-    ' Comment Lines: 4 (11.76%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 7 (20.59%)
-    '     File Size: 983 B
+' Summaries:
 
 
-    '     Module HuffmanTreeTools
-    ' 
-    '         Function: getPath
-    ' 
-    '         Sub: make
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 34
+'    Code Lines: 23 (67.65%)
+' Comment Lines: 4 (11.76%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 7 (20.59%)
+'     File Size: 983 B
+
+
+'     Module HuffmanTreeTools
+' 
+'         Function: getPath
+' 
+'         Sub: make
+' 
+' 
+' /********************************************************************************/
 
 #End Region
+
+Imports Microsoft.VisualBasic.ComponentModel.DataStructures
 
 Namespace HuffmanTree
 
