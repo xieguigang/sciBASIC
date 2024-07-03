418dca244193cdf4016eca01ec458eb36cd5e1c5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-03 12:30:51 +0800

    show progress

--- Data_science/Mathematica/Math/Math/Algebra/Matrix.NET/Decomposition/NMF.vb
+++ Data_science/Mathematica/Math/Math/Algebra/Matrix.NET/Decomposition/NMF.vb
@@ -1,58 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::f24ceb0cce6a16ae86cfb61092852ad2, Data_science\Mathematica\Math\Math\Algebra\Matrix.NET\Decomposition\NMF.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 96
-    '    Code Lines: 42 (43.75%)
-    ' Comment Lines: 41 (42.71%)
-    '    - Xml Docs: 85.37%
-    ' 
-    '   Blank Lines: 13 (13.54%)
-    '     File Size: 4.55 KB
+' Summaries:
 
 
-    '     Class NMF
-    ' 
-    '         Properties: cost, errors, H, W
-    ' 
-    '         Function: Factorisation
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 96
+'    Code Lines: 42 (43.75%)
+' Comment Lines: 41 (42.71%)
+'    - Xml Docs: 85.37%
+' 
+'   Blank Lines: 13 (13.54%)
+'     File Size: 4.55 KB
+
+
+'     Class NMF
+' 
+'         Properties: cost, errors, H, W
+' 
+'         Function: Factorisation
+' 
+' 
+' /********************************************************************************/
 
 #End Region
+
+Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar
 
 Namespace LinearAlgebra.Matrix
 
@@ -118,7 +120,7 @@ Namespace LinearAlgebra.Matrix
             Dim cost As Double
             Dim errors As New List(Of Double)
 
-            For i As Integer = 0 To max_iterations
+            For Each i As Integer In Tqdm.Range(0, max_iterations)
                 Dim HN = W.Transpose.Dot(A)
                 Dim HD = W.Transpose.Dot(W)
 
