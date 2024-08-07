4027566a3a7c469fec863cd8c3bc1c51a409e1b6
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-07 11:27:30 +0800

    Update ZipPackage.vb

--- mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel/XLSX/FileIO/ZipPackage.vb
+++ mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel/XLSX/FileIO/ZipPackage.vb
@@ -1,58 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::dd0fcbcf3ff09d4fc8f47fde38361761, mime\application%vnd.openxmlformats-officedocument.spreadsheetml.sheet\Excel\XLSX\FileIO\ZipPackage.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 91
-    '    Code Lines: 57 (62.64%)
-    ' Comment Lines: 23 (25.27%)
-    '    - Xml Docs: 73.91%
-    ' 
-    '   Blank Lines: 11 (12.09%)
-    '     File Size: 3.05 KB
+' Summaries:
 
 
-    '     Class ZipPackage
-    ' 
-    '         Properties: data, Err, Retry, xlsx
-    ' 
-    '         Function: ExtractZip
-    ' 
-    '         Sub: ExtractZipInternal, requestDataURI, requestHttpFile
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 91
+'    Code Lines: 57 (62.64%)
+' Comment Lines: 23 (25.27%)
+'    - Xml Docs: 73.91%
+' 
+'   Blank Lines: 11 (12.09%)
+'     File Size: 3.05 KB
+
+
+'     Class ZipPackage
+' 
+'         Properties: data, Err, Retry, xlsx
+' 
+'         Function: ExtractZip
+' 
+'         Sub: ExtractZipInternal, requestDataURI, requestHttpFile
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
