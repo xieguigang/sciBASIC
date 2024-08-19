4cb948ae6c061fdd932115b8b148e3ca6369b106
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-19 21:43:26 +0800

    make bugs fixed for the xml parser error

--- mime/application%xml/XmlGeneric/XmlDocumentParser.vb
+++ mime/application%xml/XmlGeneric/XmlDocumentParser.vb
@@ -1,53 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::1579e9b4720fe88fdb969f34aab8ffa9, mime\application%xml\XmlGeneric\XmlDocumentParser.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 56
-    '    Code Lines: 42 (75.00%)
-    ' Comment Lines: 5 (8.93%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 9 (16.07%)
-    '     File Size: 1.94 KB
+' Summaries:
 
 
-    ' Module XmlParser
-    ' 
-    '     Function: (+2 Overloads) ParseXml, PopulateChilds
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 56
+'    Code Lines: 42 (75.00%)
+' Comment Lines: 5 (8.93%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 9 (16.07%)
+'     File Size: 1.94 KB
+
+
+' Module XmlParser
+' 
+'     Function: (+2 Overloads) ParseXml, PopulateChilds
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -76,10 +76,15 @@ Public Module XmlParser
         }
 
         If root.HasAttributes Then
+            Dim xmlns As String
+            Dim attr_name As String
+
             rootElement.attributes = New Dictionary(Of String, String)
 
             For Each attr In root.Attributes
-                rootElement.attributes.Add(attr.Name.Namespace.ToString & "!" & attr.Name.LocalName, attr.Value)
+                xmlns = attr.Name.Namespace.ToString
+                attr_name = If(xmlns.StringEmpty, "", xmlns & "!") & attr.Name.LocalName
+                rootElement.attributes.Add(attr_name, attr.Value)
             Next
         End If
 
