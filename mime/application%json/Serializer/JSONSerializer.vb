e1b4b8a203449d6841c74f430d83d788bd771430
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-30 21:12:37 +0800

    fix of config xic mass tolernece error

--- mime/application%json/Serializer/JSONSerializer.vb
+++ mime/application%json/Serializer/JSONSerializer.vb
@@ -1,60 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::797367acc97ccc8605a547cb3d38f582, mime\application%json\Serializer\JSONSerializer.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 186
-    '    Code Lines: 129 (69.35%)
-    ' Comment Lines: 33 (17.74%)
-    '    - Xml Docs: 87.88%
-    ' 
-    '   Blank Lines: 24 (12.90%)
-    '     File Size: 6.63 KB
+' Summaries:
 
 
-    ' Module JSONSerializer
-    ' 
-    '     Function: (+2 Overloads) BuildJsonString, encodeString, GetJson, jsonArrayString, jsonObjectString
-    '               jsonValueString
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 186
+'    Code Lines: 129 (69.35%)
+' Comment Lines: 33 (17.74%)
+'    - Xml Docs: 87.88%
+' 
+'   Blank Lines: 24 (12.90%)
+'     File Size: 6.63 KB
+
+
+' Module JSONSerializer
+' 
+'     Function: (+2 Overloads) BuildJsonString, encodeString, GetJson, jsonArrayString, jsonObjectString
+'               jsonValueString
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports System.Text
 Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.MIME.application.json.BSON
@@ -90,6 +91,21 @@ Public Module JSONSerializer
             .unixTimestamp = unixTimestamp
         }.DoCall(Function(opts)
                      Return obj.GetType.GetJsonElement(obj, opts).BuildJsonString(opts)
+                 End Function)
+    End Function
+
+    <Extension>
+    Public Function CreateJSONElement(Of T)(obj As T,
+                                            Optional maskReadonly As Boolean = False,
+                                            Optional enumToStr As Boolean = True,
+                                            Optional unixTimestamp As Boolean = True) As JsonElement
+
+        Return New JSONSerializerOptions With {
+            .maskReadonly = maskReadonly,
+            .enumToString = enumToStr,
+            .unixTimestamp = unixTimestamp
+        }.DoCall(Function(opts)
+                     Return obj.GetType.GetJsonElement(obj, opts)
                  End Function)
     End Function
 
