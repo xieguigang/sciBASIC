5247ed8c33835dd3723e7f830eb9e3468ddf225e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-07 23:35:35 +0800

    make extract of the sample region via raster filter

--- Data_science/Graph/Model/GridGraph/Grid2D.vb
+++ Data_science/Graph/Model/GridGraph/Grid2D.vb
@@ -247,6 +247,24 @@ Namespace GridGraph
         End Function
 
         ''' <summary>
+        ''' check of the given point is existed?
+        ''' </summary>
+        ''' <param name="x"></param>
+        ''' <param name="y"></param>
+        ''' <returns></returns>
+        Public Function Check(x As Integer, y As Integer) As Boolean
+            Dim xkey = CLng(x), ykey = CLng(y)
+
+            If Not matrix2D.ContainsKey(xkey) Then
+                Return False
+            ElseIf Not matrix2D(xkey).ContainsKey(ykey) Then
+                Return False
+            End If
+
+            Return True
+        End Function
+
+        ''' <summary>
         ''' get a range of nearby cell data via a given pixel point data 
         ''' and query size of the cell block rectangle.
         ''' </summary>
