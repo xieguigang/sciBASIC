e8b0ce47e7071733a84a11c03bc9c4efc820859b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-20 18:23:33 +0800

    make improvements of the graphics drawing model for .net 8.0

--- Data_science/Mathematica/SignalProcessing/SignalProcessing/COW/CowAlignment.vb
+++ Data_science/Mathematica/SignalProcessing/SignalProcessing/COW/CowAlignment.vb
@@ -1,55 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::b549b730fcdef7cd2ec7fc2b6ca2a62f, Data_science\Mathematica\SignalProcessing\SignalProcessing\COW\CowAlignment.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 319
-    '    Code Lines: 231 (72.41%)
-    ' Comment Lines: 39 (12.23%)
-    '    - Xml Docs: 76.92%
-    ' 
-    '   Blank Lines: 49 (15.36%)
-    '     File Size: 17.11 KB
+' Summaries:
 
 
-    '     Class CowAlignment
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: Coefficient, CorrelationOptimizedWarping, cowFunctionCalculation, LinearAlignment, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 319
+'    Code Lines: 231 (72.41%)
+' Comment Lines: 39 (12.23%)
+'    - Xml Docs: 76.92%
+' 
+'   Blank Lines: 49 (15.36%)
+'     File Size: 17.11 KB
+
+
+'     Class CowAlignment
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: Coefficient, CorrelationOptimizedWarping, cowFunctionCalculation, LinearAlignment, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -123,8 +123,8 @@ Namespace COW
                     ' GaussianFunction(normalizedValue, mean, standardDeviation, variable)
                     '    normalizedValue * std.Exp(-1 * std.Pow(variable - mean, 2) / (2 * std.Pow(standardDeviation, 2)))
                     slack.Add(CInt(Gaussian.Gaussian(
-                              a:=maxSlack - minSlack,
-                              b:=segmentNumber / 2,
+                              A:=maxSlack - minSlack,
+                              mu:=segmentNumber / 2,
                               sigma:=segmentNumber / 4,
                               x:=i)) + minSlack)
                 End If
---
+++ Microsoft.VisualBasic.Core/src/Extensions/Image/DrawingGraphics.vb
@@ -0,0 +1,57 @@
+﻿Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Imaging.Math2D
+
+Namespace Imaging
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <remarks>
+    ''' An abstract interface model for hide the .NET Framework gdi+ on linux for .net 8 application
+    ''' </remarks>
+    Public MustInherit Class DrawingGraphics
+
+        Protected ReadOnly w As Integer
+        Protected ReadOnly h As Integer
+
+        Public ReadOnly Property Width As Integer
+            Get
+                Return w
+            End Get
+        End Property
+
+        Public ReadOnly Property Height As Integer
+            Get
+                Return h
+            End Get
+        End Property
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Sub New(width As Integer, height As Integer)
+            w = width
+            h = height
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public MustOverride Sub Clear(fill As ArgbColor)
+
+        Public MustOverride Sub DrawString(s As String, fontName As String, fontSize As Single, color As ArgbColor, x As Single, y As Single)
+        Public MustOverride Sub DrawLine(x1 As Single, y1 As Single, x2 As Single, y2 As Single, color As ArgbColor, width As Single, Optional dash As Single() = Nothing)
+        Public MustOverride Sub DrawPath(path As Polygon2D, color As ArgbColor, width As Single, Optional fill As ArgbColor? = Nothing, Optional dash As Single() = Nothing)
+        Public MustOverride Function MeasureString(text As String, fontName As String, fontSize As Single) As (Width As Single, Height As Single)
+
+        ''' <summary>
+        ''' save default image file
+        ''' </summary>
+        ''' <param name="file"></param>
+        Public Sub Save(file As String)
+            Using s As Stream = file.Open(FileMode.OpenOrCreate, doClear:=True,)
+                Call Save(s)
+            End Using
+        End Sub
+
+        Public MustOverride Sub Save(file As Stream)
+
+    End Class
+End Namespace
