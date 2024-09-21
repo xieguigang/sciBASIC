cf71f637729c0cf8706458d7b7b2b8386b659e8d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-21 12:34:00 +0800

    fix of the namespace reference error

--- Microsoft.VisualBasic.Core/src/Extensions/Image/Bitmap/BitmapScale.vb
+++ Microsoft.VisualBasic.Core/src/Extensions/Image/Bitmap/BitmapScale.vb
@@ -76,6 +76,12 @@ Namespace Imaging.BitmapImage
     ''' </summary>
     Public Module BitmapScale
 
+        ''' <summary>
+        ''' A, R, G, B
+        ''' </summary>
+        Friend Const PixelSize% = 4
+        Friend Const RGBSize% = 3
+
         <Extension>
         Public Function GetBinaryBitmap(res As Image, Optional style As BinarizationStyles = BinarizationStyles.Binary) As Bitmap
             Dim bmp As New Bitmap(DirectCast(res.Clone, Image))
@@ -176,41 +182,6 @@ Namespace Imaging.BitmapImage
                 ' move forward this memory pointer by a specific offset.
                 byts += style
             Loop
-        End Sub
-
-        ''' <summary>
-        ''' 
-        ''' </summary>
-        ''' <param name="originalImage"></param>
-        ''' <param name="brightness"></param>
-        ''' <param name="contrast"></param>
-        ''' <param name="gamma"></param>
-        ''' <remarks>
-        ''' 1 means no changed
-        ''' </remarks>
-        Public Sub Adjust(ByRef originalImage As Bitmap, Optional brightness As Single = 1, Optional contrast As Single = 1, Optional gamma As Single = 1)
-            Dim size As New Size(originalImage.Width, originalImage.Height)
-            Dim adjustedImage As New Bitmap(size.Width, size.Height)
-            Dim adjustedBrightness As Single = brightness - 1.0F
-            ' create matrix that will brighten and contrast the image
-            Dim ptsArray()() As Single = {
-                New Single() {contrast, 0, 0, 0, 0}, ' scale red
-                New Single() {0, contrast, 0, 0, 0}, ' scale green
-                New Single() {0, 0, contrast, 0, 0}, ' scale blue
-                New Single() {0, 0, 0, 1.0F, 0}, ' don't scale alpha
-                New Single() {adjustedBrightness, adjustedBrightness, adjustedBrightness, 0, 1}
-            }
-
-            Dim imageAttributes As New ImageAttributes()
-            imageAttributes.ClearColorMatrix()
-            imageAttributes.SetColorMatrix(New ColorMatrix(ptsArray), ColorMatrixFlag.Default, ColorAdjustType.Bitmap)
-            imageAttributes.SetGamma(gamma, ColorAdjustType.Bitmap)
-
-            Using g As Graphics = Graphics.FromImage(adjustedImage)
-                Call g.DrawImage(originalImage, New Rectangle(0, 0, size.Width, size.Height), 0, 0, size.Width, size.Height, GraphicsUnit.Pixel, imageAttributes)
-            End Using
-
-            originalImage = adjustedImage
         End Sub
 
         ''' <summary>
--- gr/Drawing-net4.8/Utils.vb
+++ gr/Drawing-net4.8/Utils.vb
@@ -1,54 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::6004c228798a107fdd01c9739bc50627, Microsoft.VisualBasic.Core\src\Extensions\Image\Bitmap\Utils.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 259
-    '    Code Lines: 166 (64.09%)
-    ' Comment Lines: 44 (16.99%)
-    '    - Xml Docs: 61.36%
-    ' 
-    '   Blank Lines: 49 (18.92%)
-    '     File Size: 9.51 KB
+' Summaries:
 
 
-    '     Module Utils
-    ' 
-    '         Function: (+2 Overloads) ColorReplace, CorpBlank, ResizeUnscaled, ResizeUnscaledByHeight, TrimRoundAvatar
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 259
+'    Code Lines: 166 (64.09%)
+' Comment Lines: 44 (16.99%)
+'    - Xml Docs: 61.36%
+' 
+'   Blank Lines: 49 (18.92%)
+'     File Size: 9.51 KB
+
+
+'     Module Utils
+' 
+'         Function: (+2 Overloads) ColorReplace, CorpBlank, ResizeUnscaled, ResizeUnscaledByHeight, TrimRoundAvatar
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -57,6 +57,8 @@ Imports System.Drawing.Drawing2D
 Imports System.Drawing.Text
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.BitmapImage
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 Namespace Imaging.BitmapImage
@@ -273,12 +275,6 @@ Namespace Imaging.BitmapImage
                 Return res
             End If
         End Function
-
-        ''' <summary>
-        ''' A, R, G, B
-        ''' </summary>
-        Friend Const PixelSize% = 4
-        Friend Const RGBSize% = 3
 
         ''' <summary>
         ''' Color replace using memory pointer
