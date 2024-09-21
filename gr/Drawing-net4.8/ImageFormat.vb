add56d4caf3d4fb8d16efb4920139fa8945f855f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-21 11:27:35 +0800

    move image formats wrapper

--- gr/Drawing-net4.8/ImageFormat.vb
+++ gr/Drawing-net4.8/ImageFormat.vb
@@ -1,62 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::7a496ec2b87c7d6ac6014cd105ecf21d, Microsoft.VisualBasic.Core\src\Extensions\Image\ImageFormat.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 137
-    '    Code Lines: 90 (65.69%)
-    ' Comment Lines: 32 (23.36%)
-    '    - Xml Docs: 93.75%
-    ' 
-    '   Blank Lines: 15 (10.95%)
-    '     File Size: 5.32 KB
+' Summaries:
 
 
-    '     Module ImageFormatExtensions
-    ' 
-    '         Properties: Png
-    ' 
-    '         Function: GetFormat, GetSaveImageFormat, ParseImageFormat, SaveAs
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 137
+'    Code Lines: 90 (65.69%)
+' Comment Lines: 32 (23.36%)
+'    - Xml Docs: 93.75%
+' 
+'   Blank Lines: 15 (10.95%)
+'     File Size: 5.32 KB
+
+
+'     Module ImageFormatExtensions
+' 
+'         Properties: Png
+' 
+'         Function: GetFormat, GetSaveImageFormat, ParseImageFormat, SaveAs
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Drawing
 Imports System.Drawing.Imaging
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Net.Http
 Imports Microsoft.VisualBasic.Text
 Imports defaultFormat = Microsoft.VisualBasic.Language.Default.Default(Of System.Drawing.Imaging.ImageFormat)
@@ -126,7 +127,7 @@ Namespace Imaging
         End Function
 
         ReadOnly formatsEnumMapping As New SortedDictionary(Of ImageFormats, ImageFormat) From {
- _
+                                                                                                _
             {ImageFormats.Bmp, ImageFormat.Bmp},
             {ImageFormats.Emf, ImageFormat.Emf},
             {ImageFormats.Exif, ImageFormat.Exif},
