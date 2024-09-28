7f5445a6d13f963ed788cec7c2c2718d384e7bd5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-28 17:35:22 +0800

    Create GdiRasterGraphics.vb

---
+++ Microsoft.VisualBasic.Core/src/Extensions/Image/GDI+/GdiRasterGraphics.vb
@@ -0,0 +1,14 @@
+﻿#If NET48 Then
+Imports System.Drawing
+#Else
+Imports Microsoft.VisualBasic.Imaging
+#End If
+
+Namespace Imaging.Driver
+
+    Public Interface GdiRasterGraphics
+
+        ReadOnly Property ImageResource As Image
+
+    End Interface
+End Namespace
