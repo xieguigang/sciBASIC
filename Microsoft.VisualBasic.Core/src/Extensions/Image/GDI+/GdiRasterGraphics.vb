6dc37f30adc134243fba94e416e4decaeca5f86d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-03 13:23:13 +0800

    abstract graphics data model

--- Microsoft.VisualBasic.Core/src/Extensions/Image/GDI+/GdiRasterGraphics.vb
+++ Microsoft.VisualBasic.Core/src/Extensions/Image/GDI+/GdiRasterGraphics.vb
@@ -1,4 +1,7 @@
-﻿#If NET48 Then
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.Net.Http
+
+#If NET48 Then
 Imports System.Drawing
 #Else
 Imports Microsoft.VisualBasic.Imaging
@@ -6,9 +9,66 @@ Imports Microsoft.VisualBasic.Imaging
 
 Namespace Imaging.Driver
 
+    ''' <summary>
+    ''' 
+    ''' </summary>
     Public Interface GdiRasterGraphics
 
         ReadOnly Property ImageResource As Image
 
     End Interface
+
+    Public MustInherit Class IGraphicsData
+
+        ''' <summary>
+        ''' The graphics engine driver type indicator, 
+        ''' 
+        ''' + for <see cref="Drivers.GDI"/> -> <see cref="ImageData"/>(<see cref="Drawing.Image"/>, <see cref="Bitmap"/>)
+        ''' + for <see cref="Drivers.SVG"/> -> <see cref="SVGData"/>(<see cref="SVGDocument"/>)
+        ''' 
+        ''' (驱动程序的类型)
+        ''' </summary>
+        ''' <returns></returns>
+        Public MustOverride ReadOnly Property Driver As Drivers
+
+        ''' <summary>
+        ''' http content type
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property content_type As String
+            Get
+                Select Case Driver
+                    Case Drivers.GDI
+                        Return "image/png"
+                    Case Drivers.PS
+                        Return "application/postscript"
+                    Case Drivers.SVG
+                        Return "image/svg+xml"
+                    Case Drivers.WMF
+                        Return "application/x-wmf"
+                    Case Else
+                        Return "application/octet-stream"
+                End Select
+            End Get
+        End Property
+
+        Public MustOverride ReadOnly Property Width As Integer
+        Public MustOverride ReadOnly Property Height As Integer
+
+        Public MustOverride Function GetDataURI() As DataURI
+
+        ''' <summary>
+        ''' Save the image graphics to file
+        ''' </summary>
+        ''' <param name="path$"></param>
+        ''' <returns></returns>
+        Public MustOverride Function Save(path$) As Boolean
+        ''' <summary>
+        ''' Save the image graphics to a specific output stream
+        ''' </summary>
+        ''' <param name="out"></param>
+        ''' <returns></returns>
+        Public MustOverride Function Save(out As Stream) As Boolean
+
+    End Class
 End Namespace
