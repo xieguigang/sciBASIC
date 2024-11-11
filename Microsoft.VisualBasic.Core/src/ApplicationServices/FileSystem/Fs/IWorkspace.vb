25c783df9eb00b53f209680b3a2110479632bddf
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-11 21:05:26 +0800

    Create IWorkspace.vb

---
+++ Microsoft.VisualBasic.Core/src/ApplicationServices/FileSystem/Fs/IWorkspace.vb
@@ -0,0 +1,15 @@
+﻿Namespace ApplicationServices
+
+    ''' <summary>
+    ''' A abstract reference to a workdir
+    ''' </summary>
+    Public Interface IWorkspace
+
+        ''' <summary>
+        ''' the workdir path of current workspace object
+        ''' </summary>
+        ''' <returns></returns>
+        ReadOnly Property Workspace As String
+
+    End Interface
+End Namespace
