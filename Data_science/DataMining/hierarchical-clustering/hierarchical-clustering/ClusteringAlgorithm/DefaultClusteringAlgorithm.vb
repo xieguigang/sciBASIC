c6a6935d5a69281021b351d4b453a7eb05240c13
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-02-13 18:24:02 +0800

    make code style improvements

--- Data_science/DataMining/hierarchical-clustering/hierarchical-clustering/ClusteringAlgorithm/DefaultClusteringAlgorithm.vb
+++ Data_science/DataMining/hierarchical-clustering/hierarchical-clustering/ClusteringAlgorithm/DefaultClusteringAlgorithm.vb
@@ -172,30 +172,13 @@ Public Class DefaultClusteringAlgorithm : Implements ClusteringAlgorithm
             Return linkages
         Else
             ' 当数量很大的时候，这里也是一个限速步骤，需要使用并行
+            Dim copy As Cluster() = clusters.ToArray
             Dim LQuery = From c As SeqValue(Of Cluster)
                          In clusters.SeqIterator.AsParallel
                          Let col As Integer = c.i
                          Let lCluster As Cluster = c.value
-                         Let factory =
-                             Function()
-                                 Dim list As New List(Of HierarchyTreeNode)
-                                 Dim n = clusters.Count
-                                 Dim copy = clusters.ToArray
-
-                                 For row As Integer = col + 1 To n - 1
-                                     Dim rCluster As Cluster = copy(row)
-                                     Dim link As New HierarchyTreeNode With {
-                                         .LinkageDistance = distances(col)(row),
-                                         .Left = (lCluster),
-                                         .Right = (rCluster)
-                                     }
-
-                                     Call list.Add(link)
-                                 Next
-
-                                 Return list
-                             End Function
-                         Select factory()
+                         Let list = alignRow(lCluster, col, distances, copy)
+                         Select list.ToArray
 
             Dim links = LQuery _
                 .IteratesALL _
@@ -203,6 +186,21 @@ Public Class DefaultClusteringAlgorithm : Implements ClusteringAlgorithm
 
             Return New DistanceMap(links)
         End If
+    End Function
+
+    Private Iterator Function alignRow(lCluster As Cluster, col As Integer, distances As Double()(), clusters As Cluster()) As IEnumerable(Of HierarchyTreeNode)
+        Dim n = clusters.Length
+
+        For row As Integer = col + 1 To n - 1
+            Dim rCluster As Cluster = clusters(row)
+            Dim link As New HierarchyTreeNode With {
+                .LinkageDistance = distances(col)(row),
+                .Left = lCluster,
+                .Right = rCluster
+            }
+
+            Yield link
+        Next
     End Function
 
     ''' <summary>
