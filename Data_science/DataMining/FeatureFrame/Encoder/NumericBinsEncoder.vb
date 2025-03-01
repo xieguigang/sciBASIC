42d7e6a58cc17e7179959af6e10ac8954ef2a503
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-01 22:30:40 +0800

    fix of the namespace reference error

--- Data_science/DataMining/FeatureFrame/Encoder/NumericBinsEncoder.vb
+++ Data_science/DataMining/FeatureFrame/Encoder/NumericBinsEncoder.vb
@@ -1,59 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::086220f969fdd10c6274126670277338, Data_science\DataMining\FeatureFrame\Encoder\NumericBinsEncoder.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 49
-    '    Code Lines: 40 (81.63%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 9 (18.37%)
-    '     File Size: 1.86 KB
+' Summaries:
 
 
-    ' Class NumericBinsEncoder
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: Encode, NumericBinsEncoder
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 49
+'    Code Lines: 40 (81.63%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 9 (18.37%)
+'     File Size: 1.86 KB
+
+
+' Class NumericBinsEncoder
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: Encode, NumericBinsEncoder
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports Microsoft.VisualBasic.Data.Framework
 Imports Microsoft.VisualBasic.DataMining.ComponentModel.Discretion
-Imports Microsoft.VisualBasic.Math.DataFrame
 
 Public Class NumericBinsEncoder : Inherits FeatureEncoder
 
--- Data_science/DataMining/FeatureFrame/FeatureEncoder.vb
+++ Data_science/DataMining/FeatureFrame/FeatureEncoder.vb
@@ -1,59 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::6e48861c59c395a6a0fffe4251eb582a, Data_science\DataMining\FeatureFrame\FeatureEncoder.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 18
-    '    Code Lines: 12 (66.67%)
-    ' Comment Lines: 3 (16.67%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 3 (16.67%)
-    '     File Size: 590 B
+' Summaries:
 
 
-    ' Class FeatureEncoder
-    ' 
-    '     Function: IndexNames
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 18
+'    Code Lines: 12 (66.67%)
+' Comment Lines: 3 (16.67%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 3 (16.67%)
+'     File Size: 590 B
+
+
+' Class FeatureEncoder
+' 
+'     Function: IndexNames
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Data.Framework
 Imports Microsoft.VisualBasic.Linq
-Imports Microsoft.VisualBasic.Math.DataFrame
 
 ''' <summary>
 ''' the base abstract data type of the vector encoder
