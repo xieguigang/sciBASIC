37a732172920b9fdafd0fbf05b4b9b6ac585a143
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-15 23:25:26 +0800

    make code style improvements

--- Data_science/Graph/Analysis/PQDijkstra/Finder.vb
+++ Data_science/Graph/Analysis/PQDijkstra/Finder.vb
@@ -1,57 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::30e5c911930e258ea4058e0de5192ced, Data_science\Graph\Analysis\PQDijkstra\Example.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 73
-    '    Code Lines: 52 (71.23%)
-    ' Comment Lines: 8 (10.96%)
-    '    - Xml Docs: 75.00%
-    ' 
-    '   Blank Lines: 13 (17.81%)
-    '     File Size: 2.54 KB
+' Summaries:
 
 
-    '     Class Finder
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: getInternodeTraversalCost, GetNearbyNodes, GetRelativePosition
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 73
+'    Code Lines: 52 (71.23%)
+' Comment Lines: 8 (10.96%)
+'    - Xml Docs: 75.00%
+' 
+'   Blank Lines: 13 (17.81%)
+'     File Size: 2.54 KB
+
+
+'     Class Finder
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: getInternodeTraversalCost, GetNearbyNodes, GetRelativePosition
+' 
+' 
+' /********************************************************************************/
 
 #End Region
+
+Imports rv = Microsoft.VisualBasic.Math.RandomExtensions
 
 Namespace Dijkstra.PQDijkstra
 
@@ -60,19 +62,18 @@ Namespace Dijkstra.PQDijkstra
         Private totalNodes As Integer = 100000
         Private cost As Single(,)
         Private dijkstra As DijkstraFast
-        Private rv As Random
 
         Sub New(totalNodes As Integer)
             Call MyBase.New(totalNodes)
 
             cost = New Single(totalNodes - 1, 3) {}
+
             ' initialize the cost matrix
-            rv = New Random()
             For i As Integer = 0 To totalNodes - 1
-                cost(i, 0) = CSng(rv.[Next](1000)) * 0.01F
-                cost(i, 1) = CSng(rv.[Next](1000)) * 0.01F
-                cost(i, 2) = CSng(rv.[Next](1000)) * 0.01F
-                cost(i, 3) = CSng(rv.[Next](1000)) * 0.01F
+                cost(i, 0) = CSng(rv.Next(1000)) * 0.01F
+                cost(i, 1) = CSng(rv.Next(1000)) * 0.01F
+                cost(i, 2) = CSng(rv.Next(1000)) * 0.01F
+                cost(i, 3) = CSng(rv.Next(1000)) * 0.01F
             Next
         End Sub
 
