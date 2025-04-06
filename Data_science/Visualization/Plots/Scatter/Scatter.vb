6f59653c99d5cc47481382d2b16a159162c2781c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-06 22:40:43 +0800

    make code style improvements fo the pixel property charting plot

--- Data_science/Visualization/Plots/Scatter/Scatter.vb
+++ Data_science/Visualization/Plots/Scatter/Scatter.vb
@@ -303,7 +303,10 @@ Public Module Scatter
         Dim plot As Plot
 
         If drawLine Then
-            plot = New Plots.LinePlot2D(data:=c, theme:=theme, fill:=fill, interplot:=interplot) With {
+            plot = New Plots.LinePlot2D(data:=c, theme:=theme,
+                                        fill:=fill,
+                                        fillPie:=fillPie,
+                                        interplot:=interplot) With {
                 .xlabel = Xlabel,
                 .ylabel = Ylabel,
                 .main = title,
