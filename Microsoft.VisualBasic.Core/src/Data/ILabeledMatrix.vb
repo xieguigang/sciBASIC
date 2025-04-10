973681a62545db2c8b4e5fe98c34cee1158b4674
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-10 23:22:17 +0800

    Create ILabeledMatrix.vb

---
+++ Microsoft.VisualBasic.Core/src/Data/ILabeledMatrix.vb
@@ -0,0 +1,15 @@
+﻿Namespace ComponentModel.DataSourceModel
+
+    ''' <summary>
+    ''' dataframe helper
+    ''' </summary>
+    Public Interface ILabeledMatrix
+
+        ''' <summary>
+        ''' get the labels of each row data
+        ''' </summary>
+        ''' <returns></returns>
+        Function GetLabels() As IEnumerable(Of String)
+
+    End Interface
+End Namespace
