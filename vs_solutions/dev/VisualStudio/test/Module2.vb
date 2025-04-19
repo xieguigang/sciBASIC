5acac86ebb0c127d4782790963eae9cfea36d032
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-19 17:26:28 +0800

    conbfigs for the il test

--- vs_solutions/dev/VisualStudio/test/Module2.vb
+++ vs_solutions/dev/VisualStudio/test/Module2.vb
@@ -1,6 +1,23 @@
-﻿Module Module2
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+Module Module2
 
     Sub Main()
+        Dim method = GetType(aaaa).GetMethod(NameOf(aaaa.bbbb))
+        Dim result = New MethodAnalyzer().AnalyzeMethod(method)
 
+        Call Console.WriteLine(result.ToString)
+        Call Pause()
     End Sub
 End Module
+
+Public Class aaaa
+
+    Public Function bbbb(a As String, b As Double) As Object
+        Dim rand As New Random
+        Dim d As Long = rand.NextDouble * b
+        Return New NamedValue(Of Single)(a & Now.ToShortDateString, CDbl(d))
+    End Function
+
+End Class
