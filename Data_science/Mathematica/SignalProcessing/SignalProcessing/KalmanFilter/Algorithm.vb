04d6b781fa04fc3f8585f5a2bc74d2cac3a81ce4
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-24 10:08:12 +0800

    source file renames

---
+++ Data_science/Mathematica/SignalProcessing/SignalProcessing/KalmanFilter/Algorithm.vb
@@ -0,0 +1,144 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace KalmanFilter
+
+    Public Module Algorithm
+
+        ' 
+        '  @brief   
+        '    Init fields of structure @kalman1_state.
+        '    I make some defaults in this init function:
+        '      A = 1;
+        '      H = 1; 
+        '    and @q,@r are valued after prior tests.
+        ' 
+        '    NOTES: Please change A,H,q,r according to your application.
+        ' 
+        '  @inputs  
+        '    state - Klaman filter structure
+        '    init_x - initial x state value   
+        '    init_p - initial estimated error convariance
+        '  @outputs 
+        '  @retval  
+        ' 
+
+        <Extension>
+        Public Sub kalman1_init(state As kalman1_state, init_x As Single, init_p As Single)
+            state.x = init_x
+            state.p = init_p
+            state.A = 1.0F
+            state.H = 1.0F
+            state.q = 200.0F '10e-6;  /* predict noise convariance 
+            state.r = 500.0F '10e-5;  /* measure error convariance 
+        End Sub
+
+        ' 
+        '  @brief   
+        '    1 Dimension Kalman filter
+        '  @inputs  
+        '    state - Klaman filter structure
+        '    z_measure - Measure value
+        '  @outputs 
+        '  @retval  
+        '    Estimated result
+        ' 
+        <Extension>
+        Public Function kalman1_filter(state As kalman1_state, z_measure As Single) As Single
+            ' Predict 
+            state.x = state.A * state.x
+            state.p = state.A * state.A * state.p + state.q ' p(n|n-1)=A^2*p(n-1|n-1)+q
+
+            ' Measurement 
+            state.gain = state.p * state.H / (state.p * state.H * state.H + state.r)
+            state.x = state.x + state.gain * (z_measure - state.H * state.x)
+            state.p = (1 - state.gain * state.H) * state.p
+
+            Return state.x
+        End Function
+
+        ' 
+        '  @brief   
+        '    Init fields of structure @kalman1_state.
+        '    I make some defaults in this init function:
+        '      A = {{1, 0.1}, {0, 1}};
+        '      H = {1,0}; 
+        '    and @q,@r are valued after prior tests. 
+        ' 
+        '    NOTES: Please change A,H,q,r according to your application.
+        ' 
+        '  @inputs  
+        '  @outputs 
+        '  @retval  
+        ' 
+        <Extension>
+        Public Sub kalman2_init(state As kalman2_state, init_x As Single(), init_p As Single()())
+            state.x(0) = init_x(0)
+            state.x(1) = init_x(1)
+            state.p(0)(0) = init_p(0)(0)
+            state.p(0)(1) = init_p(0)(1)
+            state.p(1)(0) = init_p(1)(0)
+            state.p(1)(1) = init_p(1)(1)
+            'state->A       = {{1, 0.1}, {0, 1}};
+            state.A(0)(0) = 1.0F
+            state.A(0)(1) = 0.1F
+            state.A(1)(0) = 0F
+            state.A(1)(1) = 1.0F
+            'state->H       = {1,0};
+            state.H(0) = 1.0F
+            state.H(1) = 0F
+            'state->q       = {{10e-6,0}, {0,10e-6}};  /* measure noise convariance 
+            state.q(0) = 0.000001F
+            state.q(1) = 0.000001F
+            state.r = 0.000001F ' estimated error convariance
+        End Sub
+
+        ' 
+        '  @brief   
+        '    2 Dimension kalman filter
+        '  @inputs  
+        '    state - Klaman filter structure
+        '    z_measure - Measure value
+        '  @outputs 
+        '    state->x[0] - Updated state value, Such as angle,velocity
+        '    state->x[1] - Updated state value, Such as diffrence angle, acceleration
+        '    state->p    - Updated estimated error convatiance matrix
+        '  @retval  
+        '    Return value is equals to state->x[0], so maybe angle or velocity.
+        ' 
+        <Extension>
+        Public Function kalman2_filter(state As kalman2_state, z_measure As Single) As Single
+            Dim temp0 = 0.0F
+            Dim temp1 = 0.0F
+            Dim temp = 0.0F
+
+            ' Step1: Predict 
+            state.x(0) = state.A(0)(0) * state.x(0) + state.A(0)(1) * state.x(1)
+            state.x(1) = state.A(1)(0) * state.x(0) + state.A(1)(1) * state.x(1)
+            ' p(n|n-1)=A^2*p(n-1|n-1)+q 
+            state.p(0)(0) = state.A(0)(0) * state.p(0)(0) + state.A(0)(1) * state.p(1)(0) + state.q(0)
+            state.p(0)(1) = state.A(0)(0) * state.p(0)(1) + state.A(1)(1) * state.p(1)(1)
+            state.p(1)(0) = state.A(1)(0) * state.p(0)(0) + state.A(0)(1) * state.p(1)(0)
+            state.p(1)(1) = state.A(1)(0) * state.p(0)(1) + state.A(1)(1) * state.p(1)(1) + state.q(1)
+
+            ' Step2: Measurement 
+            ' gain = p * H^T * [r + H * p * H^T]^(-1), H^T means transpose. 
+            temp0 = state.p(0)(0) * state.H(0) + state.p(0)(1) * state.H(1)
+            temp1 = state.p(1)(0) * state.H(0) + state.p(1)(1) * state.H(1)
+            temp = state.r + state.H(0) * temp0 + state.H(1) * temp1
+            state.gain(0) = temp0 / temp
+            state.gain(1) = temp1 / temp
+            ' x(n|n) = x(n|n-1) + gain(n) * [z_measure - H(n)*x(n|n-1)]
+            temp = state.H(0) * state.x(0) + state.H(1) * state.x(1)
+            state.x(0) = state.x(0) + state.gain(0) * (z_measure - temp)
+            state.x(1) = state.x(1) + state.gain(1) * (z_measure - temp)
+
+            ' Update @p: p(n|n) = [I - gain * H] * p(n|n-1) 
+            state.p(0)(0) = (1 - state.gain(0) * state.H(0)) * state.p(0)(0)
+            state.p(0)(1) = (1 - state.gain(0) * state.H(1)) * state.p(0)(1)
+            state.p(1)(0) = (1 - state.gain(1) * state.H(0)) * state.p(1)(0)
+            state.p(1)(1) = (1 - state.gain(1) * state.H(1)) * state.p(1)(1)
+
+            Return state.x(0)
+        End Function
+    End Module
+End Namespace
--- Data_science/Mathematica/SignalProcessing/SignalProcessing/KalmanFilter/kalman_filter.vb
+++ Data_science/Mathematica/SignalProcessing/SignalProcessing/KalmanFilter/kalman_filter.vb
@@ -1,46 +1,49 @@
-﻿' 
-'  FileName : kalman_filter.c
-'  Author   : xiahouzuoxin @163.com
-'  Version  : v1.0
-'  Date     : 2014/9/24 20:36:51
-'  Brief    : 
-'  
-'  Copyright (C) MICL,USTB
-' 
+﻿Namespace KalmanFilter
 
-' 
-'  FileName : kalman_filter.h
-'  Author   : xiahouzuoxin @163.com
-'  Version  : v1.0
-'  Date     : 2014/9/24 20:37:01
-'  Brief    : 
-'  
-'  Copyright (C) MICL,USTB
-' 
+    ' 
+    '  FileName : kalman_filter.c
+    '  Author   : xiahouzuoxin @163.com
+    '  Version  : v1.0
+    '  Date     : 2014/9/24 20:36:51
+    '  Brief    : 
+    '  
+    '  Copyright (C) MICL,USTB
+    ' 
 
-' 
-'  NOTES: n Dimension means the state is n dimension, 
-'  measurement always 1 dimension 
-' 
+    ' 
+    '  FileName : kalman_filter.h
+    '  Author   : xiahouzuoxin @163.com
+    '  Version  : v1.0
+    '  Date     : 2014/9/24 20:37:01
+    '  Brief    : 
+    '  
+    '  Copyright (C) MICL,USTB
+    ' 
 
-' 1 Dimension 
-Public Class kalman1_state
-    Public x As Single ' state
-    Public A As Single ' x(n)=A*x(n-1)+u(n),u(n)~N(0,q)
-    Public H As Single ' z(n)=H*x(n)+w(n),w(n)~N(0,r)
-    Public q As Single ' process(predict) noise convariance
-    Public r As Single ' measure noise convariance
-    Public p As Single ' estimated error convariance
-    Public gain As Single
-End Class
+    ' 
+    '  NOTES: n Dimension means the state is n dimension, 
+    '  measurement always 1 dimension 
+    ' 
 
-' 2 Dimension 
-Public Class kalman2_state
-    Public x As Single() = New Single(1) {} ' state: [0]-angle [1]-diffrence of angle, 2x1
-    Public A As Single()() = {New Single(1) {}, New Single(1) {}}
-    Public H As Single() = New Single(1) {} ' Z(n)=H*X(n)+W(n),W(n)~N(0,r), 1x2
-    Public q As Single() = New Single(1) {} ' process(predict) noise convariance,2x1 [q0,0; 0,q1]
-    Public r As Single ' measure noise convariance
-    Public p As Single()() = {New Single(1) {}, New Single(1) {}}
-    Public gain As Single() = New Single(1) {} ' 2x1
-End Class
+    ' 1 Dimension 
+    Public Class kalman1_state
+        Public x As Single ' state
+        Public A As Single ' x(n)=A*x(n-1)+u(n),u(n)~N(0,q)
+        Public H As Single ' z(n)=H*x(n)+w(n),w(n)~N(0,r)
+        Public q As Single ' process(predict) noise convariance
+        Public r As Single ' measure noise convariance
+        Public p As Single ' estimated error convariance
+        Public gain As Single
+    End Class
+
+    ' 2 Dimension 
+    Public Class kalman2_state
+        Public x As Single() = New Single(1) {} ' state: [0]-angle [1]-diffrence of angle, 2x1
+        Public A As Single()() = {New Single(1) {}, New Single(1) {}}
+        Public H As Single() = New Single(1) {} ' Z(n)=H*X(n)+W(n),W(n)~N(0,r), 1x2
+        Public q As Single() = New Single(1) {} ' process(predict) noise convariance,2x1 [q0,0; 0,q1]
+        Public r As Single ' measure noise convariance
+        Public p As Single()() = {New Single(1) {}, New Single(1) {}}
+        Public gain As Single() = New Single(1) {} ' 2x1
+    End Class
+End Namespace
