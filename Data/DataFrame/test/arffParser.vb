0e01ca3e8f95538c9821dff312b29ec8659d6088
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-09 17:42:53 +0800

    config run test

---
+++ Data/DataFrame/test/arffParser.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.VisualBasic.Data.Framework
+
+Module arffParser
+
+    Sub Main()
+        Dim df As DataFrame = DataFrame.read_arff("G:\GCModeller\src\runtime\sciBASIC#\Data\Example\dataframe\Smile.arff")
+
+        Pause()
+    End Sub
+End Module
