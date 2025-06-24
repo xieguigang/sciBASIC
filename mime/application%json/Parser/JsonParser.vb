be433d73fcede9eb8df45ffee4412106b94b6ded
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-24 17:50:13 +0800

    make improvements of the larhge json strem parser

--- mime/application%json/Parser/JsonParser.vb
+++ mime/application%json/Parser/JsonParser.vb
@@ -92,22 +92,7 @@ Public Class JsonParser
     'Const INVALID_KEY As Integer = 6
     'Const INVALID_RPC_CALL As Integer = 7
 
-    Dim psErrors As String
-    ''' <summary>
-    ''' a clean json string
-    ''' </summary>
-    Dim json_str As CharPtr
-    Dim buffer As CharBuffer
-    Dim escape As Char = ASCII.NUL
-    ''' <summary>
-    ''' single line comment
-    ''' </summary>
-    Dim comment_escape As Boolean
-    Dim comments As New Dictionary(Of String, String)
-    Dim comment_key As String = ""
-
-    Dim lastToken As Token
-    Dim line As Integer = 0
+    Dim ps As TokenIcer
     Dim lineText As String()
 
     ''' <summary>
@@ -128,20 +113,16 @@ Public Class JsonParser
     ''' <param name="json_str">the json text content</param>
     Sub New(json_str As String, Optional strictVectorSyntax As Boolean = True)
         Me.strictVectorSyntax = strictVectorSyntax
-        Me.buffer = ""
         Me.lineText = Strings _
             .Trim(json_str) _
             .Trim(ASCII.TAB, ASCII.CR, ASCII.LF, " "c) _
             .LineTokens
-        Me.json_str = lineText.JoinBy(vbLf)
+        Me.ps = New StringTokenIcer(lineText.JoinBy(vbLf), strictVectorSyntax)
     End Sub
 
-    Public Function GetParserErrors() As String
-        Return psErrors
-    End Function
-
-    Public Sub ClearParserError()
-        psErrors = "*"
+    Sub New(s As StreamReader, Optional strictVectorSyntax As Boolean = True)
+        Me.strictVectorSyntax = strictVectorSyntax
+        Me.ps = New StreamTokenIcer(s, strictVectorSyntax)
     End Sub
 
     ''' <summary>
@@ -165,11 +146,7 @@ Public Class JsonParser
     ''' </returns>
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function OpenJSON() As JsonElement
-        If json_str Is Nothing Then
-            Return Nothing
-        ElseIf json_str Like "null" Then
-            Return Nothing
-        ElseIf json_str = "" Then
+        If ps.CheckError Then
             Return Nothing
         Else
             _JSONvalue = _parse()
@@ -200,10 +177,7 @@ Public Class JsonParser
     ''' </summary>
     ''' <returns></returns>
     Private Function _parse() As JsonElement
-        Dim tokens As IEnumerator(Of Token) = GetTokenSequence() _
-            .ToArray _
-            .AsEnumerable _
-            .GetEnumerator
+        Dim tokens As IEnumerator(Of Token) = ps.GetTokenSequence().GetEnumerator
 
         If Not tokens.MoveNext Then
             ' empty collection 
@@ -363,122 +337,6 @@ Public Class JsonParser
         Loop
 
         Return array
-    End Function
-
-    Private Iterator Function GetTokenSequence() As IEnumerable(Of Token)
-        Do While Not json_str.EndRead
-            For Each t As Token In walkChar(++json_str)
-                If Not t Is Nothing Then
-                    lastToken = t.SetLine(line)
-                    Yield t
-                End If
-            Next
-        Loop
-
-        If buffer > 0 Then
-            If comment_escape Then
-                comments(comment_key) = New String(buffer.PopAllChars)
-            ElseIf Not strictVectorSyntax Then
-                Yield MeasureToken()
-            Else
-                Throw New Exception("unknow parser error at the end of the json document stream!")
-            End If
-        End If
-    End Function
-
-    Private Iterator Function walkChar(c As Char) As IEnumerable(Of Token)
-        If c = ASCII.LF Then
-            line += 1
-        End If
-
-        If comment_escape Then
-            If c = ASCII.LF Then
-                comment_escape = False
-                comments(comment_key) = New String(buffer.PopAllChars)
-                comment_key = ""
-            Else
-                Call buffer.Add(c)
-            End If
-        ElseIf escape <> ASCII.NUL Then
-            ' is string escape
-            If c = escape Then
-                If buffer.StartEscaping Then
-                    ' continute
-                    buffer += c
-                Else
-                    ' end of the string escape
-                    escape = ASCII.NUL
-                    Yield New Token(Token.JSONElements.String, buffer.PopAllChars)
-                End If
-            Else
-                buffer += c
-            End If
-        ElseIf c = "/"c Then
-            ' check hjson single comment line
-            If buffer > 0 Then
-                If buffer = "/" Then
-                    ' is single comment line
-                    buffer.Pop()
-                    comment_escape = True
-                Else
-                    Yield MeasureToken()
-                    buffer.Add("/"c)
-                End If
-            Else
-                buffer.Add(c)
-            End If
-        ElseIf c = "'"c OrElse c = """"c Then
-            escape = c
-            Return
-        ElseIf c = ":"c Then
-            ' end previous token
-            ' key: value
-            If buffer > 0 Then
-                Yield New Token(Token.JSONElements.Key, buffer.PopAllChars)
-            End If
-
-            Yield New Token(Token.JSONElements.Colon, ":")
-        ElseIf c = "," Then
-            ' end previous token
-            Yield MeasureToken()
-            Yield New Token(Token.JSONElements.Delimiter, ",")
-        ElseIf c = " "c OrElse c = ASCII.TAB OrElse c = ASCII.LF Then
-            Yield MeasureToken()
-        ElseIf c = "{"c OrElse c = "["c Then
-            ' end previous token
-            Yield MeasureToken()
-            Yield New Token(Token.JSONElements.Open, CStr(c))
-        ElseIf c = "}"c OrElse c = "]"c Then
-            ' end previous token
-            Yield MeasureToken()
-            Yield New Token(Token.JSONElements.Close, CStr(c))
-        Else
-            buffer += c
-        End If
-    End Function
-
-    ''' <summary>
-    ''' the entire <see cref="buffer"/> will be clear in this function
-    ''' </summary>
-    ''' <returns></returns>
-    Private Function MeasureToken() As Token
-        If buffer = 0 Then
-            Return Nothing
-        End If
-
-        Dim str As New String(buffer.PopAllChars)
-
-        Static [boolean] As Index(Of String) = {"true", "false"}
-
-        If str.IsInteger Then
-            Return New Token(Token.JSONElements.Integer, str)
-        ElseIf str.IsNumeric Then
-            Return New Token(Token.JSONElements.Double, str)
-        ElseIf str.ToLower Like [boolean] Then
-            Return New Token(Token.JSONElements.Boolean, str)
-        Else
-            Return New Token(Token.JSONElements.String, str)
-        End If
     End Function
 
     ''' <summary>
---
+++ mime/application%json/Parser/TokenIcer.vb
@@ -0,0 +1,226 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Scripting.TokenIcer
+Imports Microsoft.VisualBasic.Text
+Imports Microsoft.VisualBasic.Text.Parser
+
+Public MustInherit Class TokenIcer
+
+    Protected buffer As CharBuffer
+    Protected escape As Char = ASCII.NUL
+    Protected lastToken As Token
+    Protected line As Integer = 0
+
+    ''' <summary>
+    ''' single line comment
+    ''' </summary>
+    Protected comment_escape As Boolean
+    Protected comments As New Dictionary(Of String, String)
+    Protected comment_key As String = ""
+
+    ''' <summary>
+    ''' set this option to value false will enable syntax of array like: [1 2 3], 
+    ''' without comma symbol as the element value delimiter!
+    ''' </summary>
+    Protected strictVectorSyntax As Boolean = True
+
+    Sub New(strictVectorSyntax As Boolean)
+        Me.buffer = ""
+    End Sub
+
+    Public MustOverride Iterator Function GetTokenSequence() As IEnumerable(Of Token)
+    Public MustOverride Function CheckError() As Boolean
+
+    Protected Iterator Function walkChar(c As Char) As IEnumerable(Of Token)
+        If c = ASCII.LF Then
+            line += 1
+        End If
+
+        If comment_escape Then
+            If c = ASCII.LF Then
+                comment_escape = False
+                comments(comment_key) = New String(buffer.PopAllChars)
+                comment_key = ""
+            Else
+                Call buffer.Add(c)
+            End If
+        ElseIf escape <> ASCII.NUL Then
+            ' is string escape
+            If c = escape Then
+                If buffer.StartEscaping Then
+                    ' continute
+                    buffer += c
+                Else
+                    ' end of the string escape
+                    escape = ASCII.NUL
+                    Yield New Token(Token.JSONElements.String, buffer.PopAllChars)
+                End If
+            Else
+                buffer += c
+            End If
+        ElseIf c = "/"c Then
+            ' check hjson single comment line
+            If buffer > 0 Then
+                If buffer = "/" Then
+                    ' is single comment line
+                    buffer.Pop()
+                    comment_escape = True
+                Else
+                    Yield MeasureToken()
+                    buffer.Add("/"c)
+                End If
+            Else
+                buffer.Add(c)
+            End If
+        ElseIf c = "'"c OrElse c = """"c Then
+            escape = c
+            Return
+        ElseIf c = ":"c Then
+            ' end previous token
+            ' key: value
+            If buffer > 0 Then
+                Yield New Token(Token.JSONElements.Key, buffer.PopAllChars)
+            End If
+
+            Yield New Token(Token.JSONElements.Colon, ":")
+        ElseIf c = "," Then
+            ' end previous token
+            Yield MeasureToken()
+            Yield New Token(Token.JSONElements.Delimiter, ",")
+        ElseIf c = " "c OrElse c = ASCII.TAB OrElse c = ASCII.LF Then
+            Yield MeasureToken()
+        ElseIf c = "{"c OrElse c = "["c Then
+            ' end previous token
+            Yield MeasureToken()
+            Yield New Token(Token.JSONElements.Open, CStr(c))
+        ElseIf c = "}"c OrElse c = "]"c Then
+            ' end previous token
+            Yield MeasureToken()
+            Yield New Token(Token.JSONElements.Close, CStr(c))
+        Else
+            buffer += c
+        End If
+    End Function
+
+    ''' <summary>
+    ''' the entire <see cref="buffer"/> will be clear in this function
+    ''' </summary>
+    ''' <returns></returns>
+    Protected Function MeasureToken() As Token
+        If buffer = 0 Then
+            Return Nothing
+        End If
+
+        Dim str As New String(buffer.PopAllChars)
+
+        Static [boolean] As Index(Of String) = {"true", "false"}
+
+        If str.IsInteger Then
+            Return New Token(Token.JSONElements.Integer, str)
+        ElseIf str.IsNumeric Then
+            Return New Token(Token.JSONElements.Double, str)
+        ElseIf str.ToLower Like [boolean] Then
+            Return New Token(Token.JSONElements.Boolean, str)
+        Else
+            Return New Token(Token.JSONElements.String, str)
+        End If
+    End Function
+
+End Class
+
+Public Class StringTokenIcer : Inherits TokenIcer
+
+    ''' <summary>
+    ''' a clean json string
+    ''' </summary>
+    Dim json_str As CharPtr
+
+    Sub New(s As String, strictVectorSyntax As Boolean)
+        Call MyBase.New(strictVectorSyntax)
+        json_str = s
+    End Sub
+
+    Public Overrides Iterator Function GetTokenSequence() As IEnumerable(Of Token)
+        Do While Not json_str.EndRead
+            For Each t As Token In walkChar(++json_str)
+                If Not t Is Nothing Then
+                    lastToken = t.SetLine(line)
+                    Yield t
+                End If
+            Next
+        Loop
+
+        If buffer > 0 Then
+            If comment_escape Then
+                comments(comment_key) = New String(buffer.PopAllChars)
+            ElseIf Not strictVectorSyntax Then
+                Yield MeasureToken()
+            Else
+                Throw New Exception("unknow parser error at the end of the json document stream!")
+            End If
+        End If
+    End Function
+
+    Public Overrides Function CheckError() As Boolean
+        If json_str Is Nothing Then
+            Return True
+        ElseIf json_str Like "null" Then
+            Return True
+        ElseIf json_str = "" Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+End Class
+
+Public Class StreamTokenIcer : Inherits TokenIcer
+
+    ''' <summary>
+    ''' a clean json string
+    ''' </summary>
+    Dim json_str As CharStream
+
+    Sub New(s As Stream, strictVectorSyntax As Boolean)
+        Call MyBase.New(strictVectorSyntax)
+        json_str = New CharStream(New StreamReader(s))
+    End Sub
+
+    Sub New(s As StreamReader, strictVectorSyntax As Boolean)
+        Call MyBase.New(strictVectorSyntax)
+        json_str = New CharStream(s)
+    End Sub
+
+    Public Overrides Iterator Function GetTokenSequence() As IEnumerable(Of Token)
+        Do While Not json_str.EndRead
+            For Each t As Token In walkChar(++json_str)
+                If Not t Is Nothing Then
+                    lastToken = t.SetLine(line)
+                    Yield t
+                End If
+            Next
+        Loop
+
+        If buffer > 0 Then
+            If comment_escape Then
+                comments(comment_key) = New String(buffer.PopAllChars)
+            ElseIf Not strictVectorSyntax Then
+                Yield MeasureToken()
+            Else
+                Throw New Exception("unknow parser error at the end of the json document stream!")
+            End If
+        End If
+    End Function
+
+    Public Overrides Function CheckError() As Boolean
+        If json_str Is Nothing Then
+            Return True
+        ElseIf json_str Like "null" Then
+            Return True
+        ElseIf json_str = "" Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+End Class
