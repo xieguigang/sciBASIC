49687c3f32615a671ac90548e4d0987b93fc1dc3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-07-12 17:12:15 +0800

    make the model file be truncated at first

--- Microsoft.VisualBasic.Core/src/Extensions/IO/Extensions/IO.vb
+++ Microsoft.VisualBasic.Core/src/Extensions/IO/Extensions/IO.vb
@@ -382,15 +382,23 @@ Public Module IOExtensions
     ''' <param name="buf">The binary bytes data of the target package's data.(目标二进制数据)</param>
     ''' <param name="path">The saved file path of the target binary data chunk.(目标二进制数据包所要进行保存的文件名路径)</param>
     ''' <returns></returns>
-    ''' <remarks>(保存二进制数据包值文件系统)</remarks>
+    ''' <remarks>this function will truncates the target file and then save binary data into the file.
+    ''' (保存二进制数据包值文件系统)</remarks>
     '''
     <ExportAPI("FlushStream")>
     <Extension>
     Public Function FlushStream(buf As IEnumerable(Of Byte), path$) As Boolean
+        ' make target file truncated
         Using write As New BinaryWriter(path.Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False))
-            For Each b As Byte In buf
-                Call write.Write(b)
-            Next
+            If TypeOf buf Is Byte() Then
+                Call write.Write(DirectCast(buf, Byte()))
+            Else
+                For Each block As Byte() In buf.SplitIterator(partitionSize:=4096)
+                    Call write.Write(block)
+                Next
+            End If
+
+            Call write.Flush()
         End Using
 
         Return True
