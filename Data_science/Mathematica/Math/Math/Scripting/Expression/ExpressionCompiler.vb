caabe2b0476c6d57638338b5b76ff16148911792
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-07-13 12:11:41 +0800

    implements of the compiler for call abs method

--- Data_science/Mathematica/Math/Math/Scripting/Expression/ExpressionCompiler.vb
+++ Data_science/Mathematica/Math/Math/Scripting/Expression/ExpressionCompiler.vb
@@ -1,65 +1,67 @@
 ﻿#Region "Microsoft.VisualBasic::4467fbc2496521ab61e69b72ae1e53f9, Data_science\Mathematica\Math\Math\Scripting\Expression\ExpressionCompiler.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 95
-    '    Code Lines: 76 (80.00%)
-    ' Comment Lines: 5 (5.26%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 14 (14.74%)
-    '     File Size: 4.96 KB
+' Summaries:
 
 
-    '     Delegate Function
-    ' 
-    ' 
-    '     Module ExpressionCompiler
-    ' 
-    '         Function: CreateExpression, CreateLambda, MakeSymbolReference
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 95
+'    Code Lines: 76 (80.00%)
+' Comment Lines: 5 (5.26%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 14 (14.74%)
+'     File Size: 4.96 KB
+
+
+'     Delegate Function
+' 
+' 
+'     Module ExpressionCompiler
+' 
+'         Function: CreateExpression, CreateLambda, MakeSymbolReference
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Linq.Expressions
+Imports System.Reflection
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Algorithm.DynamicProgramming.Levenshtein
 Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Math.Scripting.MathExpression.Impl
 Imports ScriptExpression = Microsoft.VisualBasic.Math.Scripting.MathExpression.Impl.Expression
 
@@ -91,12 +93,16 @@ Namespace Scripting.MathExpression
                                           applied As Index(Of String)) As Expressions.Expression
             Select Case model.GetType
                 Case GetType(Literal)
+                    ' a constant
                     Return Expressions.Expression.Constant(DirectCast(model, Literal).Evaluate(Nothing), GetType(Double))
                 Case GetType(Factorial)
+                    ' a! factorial expression
                     Return Expressions.Expression.Constant(DirectCast(model, Factorial).Evaluate(Nothing), GetType(Double))
                 Case GetType(SymbolExpression)
+                    ' x
                     Return DirectCast(model, SymbolExpression).MakeSymbolReference(parameters, applied)
                 Case GetType(Impl.BinaryExpression)
+                    ' a - b
                     Dim bin As Impl.BinaryExpression = DirectCast(model, Impl.BinaryExpression)
                     Dim left As Expressions.Expression = CreateExpression(parameters, bin.left, applied)
                     Dim right As Expressions.Expression = CreateExpression(parameters, bin.right, applied)
@@ -111,6 +117,23 @@ Namespace Scripting.MathExpression
                         Case Else
                             Throw New InvalidCastException(bin.ToString)
                     End Select
+                Case GetType(Impl.FunctionInvoke)
+                    Dim fx As Impl.FunctionInvoke = DirectCast(model, Impl.FunctionInvoke)
+                    Dim name As String = fx.funcName
+                    Dim args As Expressions.Expression() = fx.parameters _
+                        .SafeQuery _
+                        .Select(Function(r) CreateExpression(parameters, r, applied)) _
+                        .ToArray
+
+                    Select Case name.ToLower
+                        Case "abs"
+                            Dim absMethod As MethodInfo = GetType(System.Math).GetMethod("Abs", {GetType(Double)})
+                            Dim callAbs As MethodCallExpression = Expressions.Expression.Call(absMethod, args(0))
+                            Return callAbs
+                        Case Else
+                            Throw New NotImplementedException($"the function call of '{name}' is not implemented!")
+                    End Select
+
 
                 Case Else
                     Throw New InvalidProgramException(model.GetType.FullName)
