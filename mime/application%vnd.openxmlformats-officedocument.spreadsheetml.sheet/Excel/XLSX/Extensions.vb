268fbc82f20bd4f0b31a6f2b746e12875368f2a3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-07-28 18:12:15 +0800

    export result archive file

--- mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel/XLSX/Extensions.vb
+++ mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel/XLSX/Extensions.vb
@@ -1,61 +1,62 @@
 ﻿#Region "Microsoft.VisualBasic::f096ded3302ef4c4695a0e3accc70ad3, mime\application%vnd.openxmlformats-officedocument.spreadsheetml.sheet\Excel\XLSX\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 105
-    '    Code Lines: 72 (68.57%)
-    ' Comment Lines: 20 (19.05%)
-    '    - Xml Docs: 95.00%
-    ' 
-    '   Blank Lines: 13 (12.38%)
-    '     File Size: 3.92 KB
+' Summaries:
 
 
-    '     Module Extensions
-    ' 
-    '         Properties: Sheet1
-    ' 
-    '         Function: EnumerateTables, (+2 Overloads) FirstSheet, GetSheetNames, ReadTableAuto
-    ' 
-    '         Sub: WriteSheetTable
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 105
+'    Code Lines: 72 (68.57%)
+' Comment Lines: 20 (19.05%)
+'    - Xml Docs: 95.00%
+' 
+'   Blank Lines: 13 (12.38%)
+'     File Size: 3.92 KB
+
+
+'     Module Extensions
+' 
+'         Properties: Sheet1
+' 
+'         Function: EnumerateTables, (+2 Overloads) FirstSheet, GetSheetNames, ReadTableAuto
+' 
+'         Sub: WriteSheetTable
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.Framework
@@ -163,7 +164,7 @@ Namespace XLSX
         End Sub
 
         <Extension>
-        Public Sub SaveToExcel(Of T)(data As IEnumerable(Of T), file As String, sheetName As String, Optional metadata As String()() = Nothing)
+        Public Sub SaveToExcel(Of T)(data As IEnumerable(Of T), file As Stream, sheetName As String, Optional metadata As String()() = Nothing)
             Dim str As csv = data.ToCsvDoc()
             Dim workbook As New Workbook(sheetName)
             Dim sheet As Worksheet = workbook.CurrentWorksheet
@@ -179,7 +180,14 @@ Namespace XLSX
             End If
 
             Call workbook.WriteSheetTable(str)
-            Call workbook.SaveAs(file)
+            Call workbook.SaveAsStream(file)
+        End Sub
+
+        <Extension>
+        Public Sub SaveToExcel(Of T)(data As IEnumerable(Of T), file As String, sheetName As String, Optional metadata As String()() = Nothing)
+            Using s As Stream = file.Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False)
+                Call data.SaveToExcel(s, sheetName, metadata)
+            End Using
         End Sub
     End Module
 End Namespace
