870b31c001fd146db28300b5432e8c155dfc2c08
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-11 15:16:53 +0800

    run test success

--- Data_science/Mathematica/SignalProcessing/SignalProcessing/test/labeltest.vb
+++ Data_science/Mathematica/SignalProcessing/SignalProcessing/test/labeltest.vb
@@ -5,11 +5,11 @@ Imports Microsoft.VisualBasic.Math.MachineVision.CCL
 Public Module labeltest
 
     Sub Main()
-        Dim img = "Z:\Untitled.bmp".LoadImage
+        Dim img = "Z:\aaa.bmp".LoadImage
         Dim CELLS = CCLabeling.Process(BitmapBuffer.FromImage(img))
 
         For Each item In CELLS
-            Call item.Value.Save($"Z:/ccl2/{item.Key}.bmp")
+            Call item.Value.Save($"Z:/ccl/{item.Key}.bmp")
         Next
     End Sub
 End Module
