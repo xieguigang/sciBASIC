9805661e8bf06d34e143b6a8ad0911cad9e19dda
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-21 16:35:40 +0800

    Create IstatPvalue.vb

---
+++ Microsoft.VisualBasic.Core/src/Extensions/Math/StatisticsMathExtensions/IstatPvalue.vb
@@ -0,0 +1,16 @@
+﻿Namespace Math.Statistics
+
+    ''' <summary>
+    ''' a general abstract object model for statistics result which 
+    ''' contains p value information
+    ''' </summary>
+    Public Interface IstatPvalue
+
+        ''' <summary>
+        ''' p value of current sample statistics result
+        ''' </summary>
+        ''' <returns></returns>
+        ReadOnly Property pValue As Double
+
+    End Interface
+End Namespace
