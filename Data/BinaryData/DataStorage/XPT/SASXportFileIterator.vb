3941d8b86fa1be66719d84312d357c213f886f3d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-25 13:48:38 +0800

    add sas xpt file reader code

---
+++ Data/BinaryData/DataStorage/XPT/SASXportFileIterator.vb
@@ -0,0 +1,67 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.Data.IO.Xpt.Types
+
+Namespace Xpt
+
+    Public Class SASXportFileIterator
+        Inherits SASXportConverter
+
+        Private crecord As IList(Of String) = Nothing
+        Private cPrimitiveRecord As IList(Of ReadstatValue) = Nothing
+        Private crow As Byte() = Nothing
+
+        Public Sub New(fileName As String)
+            MyBase.New(fileName)
+            MyBase.init()
+        End Sub
+
+        Public Sub New([is] As Stream)
+            MyBase.New([is])
+            MyBase.init()
+        End Sub
+
+        Public Sub New(fileName As String, offset As Integer)
+            Me.New(fileName)
+            MyBase.seek(offset)
+        End Sub
+
+        Public Overridable Function hasNext() As Boolean
+            Return Not MyBase.Done
+        End Function
+
+        Public Overridable Function [next]() As IList(Of String)
+            crecord = MyBase.Record
+            cPrimitiveRecord = MyBase.PrimitiveRecord
+            crow = MyBase.Row
+
+            MyBase.readNextRecord()
+
+            Return crecord
+        End Function
+
+        Public Overridable Function nextPrimitive() As IList(Of ReadstatValue)
+            [next]()
+            Return cPrimitiveRecord
+        End Function
+
+        Public Overridable Function nextRaw() As Byte()
+            [next]()
+            Return crow
+        End Function
+
+        Public Shared Sub Main(args As String())
+            Dim iterator As SASXportFileIterator = New SASXportFileIterator("/grid/data/xpt/test3.sasxpt")
+            While iterator.hasNext()
+                Dim row As IList(Of String) = iterator.next()
+            End While
+            Console.WriteLine("Total Rows: " & iterator.RowCount.ToString())
+            iterator.Dispose()
+
+            Dim cal As Date = New DateTime()
+            cal = New DateTime(1960, 1, 1)
+            cal.AddDays(19778)
+            Console.WriteLine(cal.ToString())
+        End Sub
+    End Class
+
+End Namespace
---
+++ Data/BinaryData/DataStorage/XPT/XPTReaderUtils.vb
@@ -0,0 +1,91 @@
+﻿Imports System.Globalization
+Imports System.Text
+
+Namespace Xpt
+
+    Public Class SimpleDateFormat
+
+        Private formatStr As String
+
+        Public Sub New(format As String)
+            formatStr = format
+        End Sub
+
+        Public Function format(dt As Date) As String
+            Return dt.ToString(formatStr, CultureInfo.InvariantCulture)
+        End Function
+    End Class
+
+
+    Public Class XPTReaderUtils
+
+        Public Shared DATE5_FORMAT As SimpleDateFormat = New SimpleDateFormat("ddMMM ")
+        Public Shared DATE6_FORMAT As SimpleDateFormat = New SimpleDateFormat(" ddMMM ")
+        Public Shared DATE7_FORMAT As SimpleDateFormat = New SimpleDateFormat("ddMMMYY ")
+        Public Shared DATE8_FORMAT As SimpleDateFormat = New SimpleDateFormat(" ddMMMYY ")
+        Public Shared DATE9_FORMAT As SimpleDateFormat = New SimpleDateFormat("ddMMMYYYY")
+        Public Shared DATE11_FORMAT As SimpleDateFormat = New SimpleDateFormat("dd-MMM-YYYY")
+
+        Public Shared Function getString(line As Byte(), offset As Integer, len As Integer) As String
+
+            Dim data = New Byte(len - 1) {}
+            For i = 0 To len - 1
+                data(i) = line(offset + i)
+            Next
+            Return Encoding.UTF8.GetString(data).Trim()
+        End Function
+
+        Public Shared Function getInteger(line As Byte(), offset As Integer, len As Integer) As Integer
+            Dim val = getString(line, offset, len)
+            If val.Length <= 0 Then
+                Return 0
+            End If
+            Return Integer.Parse(val)
+        End Function
+
+        Public Shared Function getShort(line As Byte(), offset As Integer, len As Integer) As Short
+            Dim val = getString(line, offset, 2)
+            If val.Length <= 0 Then
+                Return 0
+            End If
+            Return Short.Parse(val)
+        End Function
+
+        Public Shared Function getPrimitiveInteger(buffer As Byte(), offset As Integer) As Integer
+
+            Dim val = (buffer(offset + 0) And &HFF) << 24 Or (buffer(offset + 1) And &HFF) << 16 Or (buffer(offset + 2) And &HFF) << 8 Or (buffer(offset + 3) And &HFF) << 0
+            Return val
+        End Function
+
+        Public Shared Function getPrimitiveShort(buffer As Byte(), offset As Integer) As Short
+            Dim val As Short = (buffer(offset + 0) And &HFF) << 8 Or (buffer(offset + 1) And &HFF) << 0
+            Return val
+        End Function
+
+        Public Shared Function convertSASDate9ToString(dtformat As String, [date] As Double) As String
+            Dim num As Integer = [date]
+            Dim format = DATE9_FORMAT
+            If "date5".Equals(dtformat) Then
+                format = DATE5_FORMAT
+            ElseIf "date6".Equals(dtformat) Then
+                format = DATE6_FORMAT
+            ElseIf "date7".Equals(dtformat) Then
+                format = DATE7_FORMAT
+            ElseIf "date8".Equals(dtformat) Then
+                format = DATE8_FORMAT
+            ElseIf "date9".Equals(dtformat) Then
+                format = DATE9_FORMAT
+            ElseIf "date11".Equals(dtformat) Then
+                format = DATE11_FORMAT
+            End If
+
+            Dim cal As Date = New DateTime()
+            cal = New DateTime(1960, 0, 1)
+            cal.AddDays(num)
+
+            Dim formatted = format.format(cal)
+            Return formatted
+        End Function
+    End Class
+
+End Namespace
