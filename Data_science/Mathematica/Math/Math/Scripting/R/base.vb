9dd8685cb4a417bec2b5cd4230c3721de14f88da
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-29 14:32:16 +0800

    merge vector data

--- Data_science/Mathematica/Math/Math/Scripting/R/base.vb
+++ Data_science/Mathematica/Math/Math/Scripting/R/base.vb
@@ -1,6 +1,12 @@
-﻿Namespace BasicR.Helpers
+﻿Imports Microsoft.VisualBasic.Linq
 
-    Public Module Module1
+Namespace Scripting.BasicR
+
+    Public Module base
+
+        Public Function c(Of T)(ParamArray v As IEnumerable(Of T)()) As T()
+            Return v.IteratesALL.ToArray
+        End Function
 
     End Module
 End Namespace
