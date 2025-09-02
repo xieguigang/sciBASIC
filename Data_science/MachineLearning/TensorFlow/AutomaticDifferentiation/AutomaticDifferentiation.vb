e446c814e9d054851bdfa09cff7f3d2ae75c932d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-09-02 14:53:18 +0800

    add AutomaticDifferentiation model

---
+++ Data_science/MachineLearning/TensorFlow/AutomaticDifferentiation/AutomaticDifferentiation.vb
@@ -0,0 +1,19 @@
+﻿Public MustInherit Class AutomaticDifferentiation(Of Tlhs, Trhs)
+
+    Protected ReadOnly lhs As Tlhs
+    Protected ReadOnly rhs As Trhs
+
+    Sub New(lhs As Tlhs, rhs As Trhs)
+        Me.lhs = lhs
+        Me.rhs = rhs
+    End Sub
+
+    Public MustOverride Sub Differentiation(dx As Double)
+
+End Class
+
+''' <summary>
+''' dy
+''' </summary>
+''' <param name="dx"></param>
+Public Delegate Sub Differentiation(dx As Double)
