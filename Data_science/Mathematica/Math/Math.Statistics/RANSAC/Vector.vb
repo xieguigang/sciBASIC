d1eaa349363d98a49cb241b0fd532cee65b9e679
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-08 15:44:35 +0800

    symbol renames

--- Data_science/Mathematica/Math/Math.Statistics/RANSAC/Vector.vb
+++ Data_science/Mathematica/Math/Math.Statistics/RANSAC/Vector.vb
@@ -1,61 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::5ec43e2bf1bfde68d6f70224173da2cc, Data_science\Mathematica\Math\Math.Statistics\RANSAC\Vector.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 37
-    '    Code Lines: 27 (72.97%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 10 (27.03%)
-    '     File Size: 1.06 KB
+' Summaries:
 
 
-    '     Class Vector
-    ' 
-    '         Properties: magnitude, normalized
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Operators: *, +
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 37
+'    Code Lines: 27 (72.97%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 10 (27.03%)
+'     File Size: 1.06 KB
+
+
+'     Class Vector
+' 
+'         Properties: magnitude, normalized
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Operators: *, +
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports stdNum = System.Math
+Imports std = System.Math
 
 Namespace RANSAC
 
@@ -63,7 +63,7 @@ Namespace RANSAC
 
         Public ReadOnly Property magnitude As Double
             Get
-                Return stdNum.Sqrt(stdNum.Pow(x, 2) + stdNum.Pow(y, 2) + stdNum.Pow(z, 2))
+                Return std.Sqrt(std.Pow(x, 2) + std.Pow(y, 2) + std.Pow(z, 2))
             End Get
         End Property
 
