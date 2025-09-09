4f5b2e220dcb07c2df9b91e06a4cfcacb50cfa27
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-09 10:02:44 +0800

    Create Transform.vb

---
+++ Microsoft.VisualBasic.Core/src/Drawing/Math/Geometry/Transform.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Imaging.Math2D
+
+    Public Class Transform
+
+        ''' <summary>
+        ''' angle for rotation
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property theta As Double
+        ''' <summary>
+        ''' translate x
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property tx As Double
+        ''' <summary>
+        ''' translate y
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property ty As Double
+        ''' <summary>
+        ''' scale x
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property scalex As Double
+        ''' <summary>
+        ''' scale y
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property scaley As Double
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Widening Operator CType(args As (theta As Double, tx As Double, ty As Double, scalex As Double, scaley As Double)) As Transform
+            Return New Transform With {
+                .theta = args.theta,
+                .tx = args.tx,
+                .ty = args.ty,
+                .scalex = args.scalex,
+                .scaley = args.scaley
+            }
+        End Operator
+
+    End Class
+End Namespace
