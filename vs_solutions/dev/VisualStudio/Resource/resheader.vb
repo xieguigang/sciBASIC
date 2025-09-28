441f4cb8810194536be87e5b246f9e94bb834559
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-28 11:35:06 +0800

    add resource resx xml file model

---
+++ vs_solutions/dev/VisualStudio/Resource/resheader.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Xml.Serialization
+
+Namespace Resource
+
+    Public Class resheader
+
+        <XmlAttribute>
+        Public Property name As String
+        Public Property value As String
+
+    End Class
+
+    Public Class assembly
+
+        <XmlAttribute> Public Property [alias] As String
+        <XmlAttribute> Public Property name As String
+
+    End Class
+
+    Public Class data
+
+        <XmlAttribute> Public Property name As String
+        <XmlAttribute> Public Property type As String
+        Public Property value As String
+
+    End Class
+
+
+End Namespace
