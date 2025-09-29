55dacdaa278d496929039cf185a641104f51fb00
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-09-29 14:04:34 +0800

    configs of the tqqmd options

--- Microsoft.VisualBasic.Core/src/ApplicationServices/Terminal/Utility/ProgressBar/Tqdm/ProgressBar.vb
+++ Microsoft.VisualBasic.Core/src/ApplicationServices/Terminal/Utility/ProgressBar/Tqdm/ProgressBar.vb
@@ -1,62 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::c8fdce19e374981bdca028a24b38d5cc, Microsoft.VisualBasic.Core\src\ApplicationServices\Terminal\Utility\ProgressBar\Tqdm\ProgressBar.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 347
-    '    Code Lines: 203 (58.50%)
-    ' Comment Lines: 92 (26.51%)
-    '    - Xml Docs: 59.78%
-    ' 
-    '   Blank Lines: 52 (14.99%)
-    '     File Size: 14.29 KB
+' Summaries:
 
 
-    '     Class ProgressBar
-    ' 
-    '         Properties: CurrentTicks, ElapsedSeconds, FormatTaskCounter, UpdateDynamicConfigs
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Sub: [Step], Finish, PrintLine, (+2 Overloads) Progress, Reset
-    '              SetLabel, SetTheme, SetThemeAscii, SetThemeBasic, SetThemePython
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 347
+'    Code Lines: 203 (58.50%)
+' Comment Lines: 92 (26.51%)
+'    - Xml Docs: 59.78%
+' 
+'   Blank Lines: 52 (14.99%)
+'     File Size: 14.29 KB
+
+
+'     Class ProgressBar
+' 
+'         Properties: CurrentTicks, ElapsedSeconds, FormatTaskCounter, UpdateDynamicConfigs
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Sub: [Step], Finish, PrintLine, (+2 Overloads) Progress, Reset
+'              SetLabel, SetTheme, SetThemeAscii, SetThemeBasic, SetThemePython
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Text
+Imports Microsoft.VisualBasic.CommandLine.InteropService.Pipeline
 Imports std = System.Math
 
 Namespace ApplicationServices.Terminal.ProgressBar.Tqdm
@@ -240,6 +241,16 @@ Namespace ApplicationServices.Terminal.ProgressBar.Tqdm
         Public Sub Progress(current As Integer, total As Integer)
             _total = total
             Progress(current)
+        End Sub
+
+        ''' <summary>
+        ''' adapter interface for <see cref="RunSlavePipeline.SetProgressEventHandler"/>
+        ''' </summary>
+        ''' <param name="current"></param>
+        ''' <param name="message"></param>
+        Public Sub Progress(current As Integer, message As String)
+            Call Me.Progress(current)
+            Call Me.SetLabel(message)
         End Sub
 
         ''' <summary>
