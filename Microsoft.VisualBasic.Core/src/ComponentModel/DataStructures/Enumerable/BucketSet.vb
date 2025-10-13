d440aa8fe82b98b58c257e5eacac6337bbb9a858
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-13 11:57:29 +0800

    Create BucketSet.vb

---
+++ Microsoft.VisualBasic.Core/src/ComponentModel/DataStructures/Enumerable/BucketSet.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace ComponentModel.Collection.Generic
+
+    Public Class BucketSet(Of T) : Implements IEnumerable(Of T)
+
+        ReadOnly buckets As New List(Of T())
+
+        Public ReadOnly Property Count As Long
+            Get
+                Return Aggregate block As T()
+                       In buckets
+                       Let lngSize As Long = CLng(block.Length)
+                       Into Sum(lngSize)
+            End Get
+        End Property
+
+        Sub New(buckets As IEnumerable(Of IEnumerable(Of T)))
+            For Each block As IEnumerable(Of T) In buckets
+                Call Me.buckets.Add(block.ToArray)
+            Next
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Sub Add(block As IEnumerable(Of T))
+            Call buckets.Add(block.ToArray)
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Sub Add(getData As Func(Of IEnumerable(Of T)))
+            Call Add(getData())
+        End Sub
+
+        Public Iterator Function GetEnumerator() As IEnumerator(Of T) Implements IEnumerable(Of T).GetEnumerator
+            For Each block As T() In buckets
+                For Each item As T In block
+                    Yield item
+                Next
+            Next
+        End Function
+
+        Private Iterator Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+            Yield GetEnumerator()
+        End Function
+    End Class
+End Namespace
