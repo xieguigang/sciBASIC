7a798272bef35e1c1924cfd7ce9fd6cab6b6c9ae
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-26 18:13:58 +0800

    add point descriptor models

---
+++ Data_science/Mathematica/SignalProcessing/MachineVision/RANSAC/FPFHDescriptor.vb
@@ -0,0 +1,143 @@
+﻿Imports System.Drawing
+Imports std = System.Math
+
+''' <summary>
+''' 一个带法向量的点
+''' </summary>
+Public Structure PointWithNormal
+    Public Pt As PointF
+    ''' <summary>
+    ''' 法向量，应为单位向量
+    ''' </summary>
+    Public Normal As PointF
+End Structure
+
+''' <summary>
+''' FPFH (Fast Point Feature Histograms)
+''' </summary>
+Public Class FPFHDescriptor
+    Public Property Point As PointWithNormal
+    ''' <summary>
+    ''' FPFH 直方图，例如，将每个角度特征分成11个bin，共33维
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Histogram As Double()
+
+    Public Sub New(point As PointWithNormal, featureBins As Integer)
+        Me.Point = point
+        Me.Histogram = New Double(featureBins * 3 - 1) {} ' 假设3个特征
+    End Sub
+End Class
+
+Public NotInheritable Class FPFHCalculator
+
+    Private Sub New()
+    End Sub
+
+    ''' <summary>
+    ''' 为一组点计算 FPFH 描述子
+    ''' </summary>
+    Public Shared Function ComputeDescriptors(points As List(Of PointWithNormal),
+                                             Optional searchRadius As Double = 0.5,
+                                             Optional featureBins As Integer = 11) As List(Of FPFHDescriptor)
+        If points Is Nothing OrElse points.Count < 2 Then Return New List(Of FPFHDescriptor)()
+
+        Dim spfhHistograms(points.Count - 1)() As Double
+        Dim n = points.Count
+
+        ' 1. 计算每个点的 SPFH
+        For i As Integer = 0 To n - 1
+            spfhHistograms(i) = ComputeSPFH(points(i), points, searchRadius, featureBins)
+        Next
+
+        ' 2. 组合 SPFH 以创建 FPFH
+        Dim fpfhDescriptors As New List(Of FPFHDescriptor)
+        For i As Integer = 0 To n - 1
+            Dim fpfhHist = New Double(featureBins * 3 - 1) {}
+            Dim neighbors = FindNeighbors(points(i), points, searchRadius)
+
+            ' 加权平均
+            Dim weightSum As Double = 1.0 ' 自身权重为1
+            For j As Integer = 0 To fpfhHist.Length - 1
+                fpfhHist(j) = spfhHistograms(i)(j) ' 初始化为自身的SPFH
+            Next
+
+            For Each neighbor In neighbors
+                Dim dist = Distance(points(i).Pt, neighbor.Pt)
+                Dim weight = 1.0 / dist ' 简单的距离权重
+                weightSum += weight
+
+                For j As Integer = 0 To fpfhHist.Length - 1
+                    fpfhHist(j) += weight * spfhHistograms(points.IndexOf(neighbor))(j)
+                Next
+            Next
+
+            ' 归一化
+            If weightSum > 0 Then
+                For j As Integer = 0 To fpfhHist.Length - 1
+                    fpfhHist(j) /= weightSum
+                Next
+            End If
+
+            fpfhDescriptors.Add(New FPFHDescriptor(points(i), featureBins) With {.Histogram = fpfhHist})
+        Next
+
+        Return fpfhDescriptors
+    End Function
+
+    ''' <summary>
+    ''' 计算单个点的简化点特征直方图 (SPFH)
+    ''' </summary>
+    Private Shared Function ComputeSPFH(queryPoint As PointWithNormal, allPoints As List(Of PointWithNormal), radius As Double, bins As Integer) As Double()
+        Dim hist = New Double(bins * 3 - 1) {} ' 3个特征: alpha, phi, theta
+        Dim neighbors = FindNeighbors(queryPoint, allPoints, radius)
+
+        For Each neighbor In neighbors
+            ' 计算三个角度特征 (这里是2D简化版)
+            Dim u = queryPoint.Normal
+            Dim v = New PointF(neighbor.Pt.X - queryPoint.Pt.X, neighbor.Pt.Y - queryPoint.Pt.Y)
+            Dim w = neighbor.Normal
+
+            Dim alpha = AngleBetween(u, v)
+            Dim phi = AngleBetween(u, w)
+            ' theta 在2D中可以简化或省略
+
+            ' 将角度量化并加入直方图
+            AddToHistogram(hist, alpha, 0, bins, std.PI)
+            AddToHistogram(hist, phi, bins, bins, std.PI)
+            ' AddToHistogram(hist, theta, 2 * bins, bins, std.PI)
+        Next
+
+        ' 归一化
+        Dim sum = hist.Sum()
+        If sum > 0 Then
+            For i As Integer = 0 To hist.Length - 1
+                hist(i) /= sum
+            Next
+        End If
+        Return hist
+    End Function
+
+    ' --- 辅助函数 ---
+    Private Shared Function FindNeighbors(query As PointWithNormal, points As List(Of PointWithNormal), radius As Double) As List(Of PointWithNormal)
+        Return points.Where(Function(p) p.Pt <> query.Pt AndAlso Distance(query.Pt, p.Pt) <= radius).ToList()
+    End Function
+
+    Private Shared Function Distance(p1 As PointF, p2 As PointF) As Double
+        Dim dx = p1.X - p2.X
+        Dim dy = p1.Y - p2.Y
+        Return std.Sqrt(dx * dx + dy * dy)
+    End Function
+
+    Private Shared Function AngleBetween(v1 As PointF, v2 As PointF) As Double
+        Dim dot = v1.X * v2.X + v1.Y * v2.Y
+        Dim det = v1.X * v2.Y - v1.Y * v2.X
+        Return std.Atan2(det, dot) ' 返回 [-PI, PI]
+    End Function
+
+    Private Shared Sub AddToHistogram(hist As Double(), value As Double, startIndex As Integer, binCount As Integer, range As Double)
+        Dim bin = CInt(std.Floor((value + range / 2) * binCount / range))
+        bin = std.Max(0, std.Min(binCount - 1, bin))
+        hist(startIndex + bin) += 1
+    End Sub
+End Class
---
+++ Data_science/Mathematica/SignalProcessing/MachineVision/RANSAC/ShapeContextDescriptor.vb
@@ -0,0 +1,106 @@
+﻿Imports System.Drawing
+Imports std = System.Math
+
+''' <summary>
+''' 一个点的 Shape Context 描述子
+''' </summary>
+Public Class ShapeContextDescriptor
+    Public Property Point As PointF
+    ''' <summary>
+    ''' 对数极坐标直方图，例如，5个径向bin * 12个角度bin = 60维向量
+    ''' </summary>
+    Public Property Histogram As Double()
+
+    Public Sub New(point As PointF, radialBins As Integer, angularBins As Integer)
+        Me.Point = point
+        Me.Histogram = New Double(radialBins * angularBins - 1) {}
+    End Sub
+End Class
+
+Public NotInheritable Class ShapeContextCalculator
+
+    Private Sub New()
+    End Sub
+
+    ''' <summary>
+    ''' 为一组点计算 Shape Context 描述子
+    ''' </summary>
+    ''' <param name="points">输入点集</param>
+    ''' <param name="radialBins">径向bin数量</param>
+    ''' <param name="angularBins">角度bin数量</param>
+    ''' <returns>描述子列表</returns>
+    Public Shared Function ComputeDescriptors(points As List(Of PointF),
+                                            Optional radialBins As Integer = 5,
+                                            Optional angularBins As Integer = 12) As List(Of ShapeContextDescriptor)
+        If points Is Nothing OrElse points.Count < 2 Then Return New List(Of ShapeContextDescriptor)()
+
+        Dim descriptors As New List(Of ShapeContextDescriptor)
+        Dim n = points.Count
+
+        ' 1. 预计算所有点对之间的距离和角度
+        Dim logDistances(n - 1)() As Double
+        Dim angles(n - 1)() As Double
+        For i As Integer = 0 To n - 1
+            logDistances(i) = New Double(n - 1) {}
+            angles(i) = New Double(n - 1) {}
+            For j As Integer = 0 To n - 1
+                If i = j Then Continue For
+                Dim dx = points(j).X - points(i).X
+                Dim dy = points(j).Y - points(i).Y
+                Dim dist = std.Sqrt(dx * dx + dy * dy)
+                ' 使用对数距离，并加1避免log(0)
+                logDistances(i)(j) = std.Log(dist + 1)
+                angles(i)(j) = std.Atan2(dy, dx)
+            Next
+        Next
+
+        ' 2. 确定直方图的边界
+        Dim minLogR As Double = Double.PositiveInfinity
+        Dim maxLogR As Double = Double.NegativeInfinity
+        For i As Integer = 0 To n - 1
+            For j As Integer = 0 To n - 1
+                If i <> j Then
+                    If logDistances(i)(j) < minLogR Then minLogR = logDistances(i)(j)
+                    If logDistances(i)(j) > maxLogR Then maxLogR = logDistances(i)(j)
+                End If
+            Next
+        Next
+        Dim logRStep = If(maxLogR > minLogR, (maxLogR - minLogR) / radialBins, 1.0)
+        Dim angleStep = 2 * std.PI / angularBins
+
+        ' 3. 为每个点计算直方图
+        For i As Integer = 0 To n - 1
+            Dim descriptor = New ShapeContextDescriptor(points(i), radialBins, angularBins)
+            For j As Integer = 0 To n - 1
+                If i = j Then Continue For
+
+                Dim r = logDistances(i)(j)
+                Dim theta = angles(i)(j)
+
+                ' 找到对应的 bin
+                Dim rBin = CInt(std.Floor((r - minLogR) / logRStep))
+                Dim thetaBin = CInt(std.Floor((theta + std.PI) / angleStep))
+
+                ' 边界检查
+                rBin = std.Max(0, std.Min(radialBins - 1, rBin))
+                thetaBin = std.Max(0, std.Min(angularBins - 1, thetaBin))
+
+                ' 累加到直方图
+                descriptor.Histogram(rBin * angularBins + thetaBin) += 1
+            Next
+
+            ' 4. 归一化直方图（使其总和为1）
+            Dim sum = descriptor.Histogram.Sum()
+            If sum > 0 Then
+                For k As Integer = 0 To descriptor.Histogram.Length - 1
+                    descriptor.Histogram(k) /= sum
+                Next
+            End If
+
+            descriptors.Add(descriptor)
+        Next
+
+        Return descriptors
+    End Function
+End Class
+
