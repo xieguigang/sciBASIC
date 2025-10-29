48cdfa6345ef654f4f8e4de1ed0eb0c3f6308732
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-29 11:41:00 +0800

    make imrpcoements of the debug view

--- Data_science/Mathematica/SignalProcessing/MachineVision/RANSAC/PointWithDescriptor.vb
+++ Data_science/Mathematica/SignalProcessing/MachineVision/RANSAC/PointWithDescriptor.vb
@@ -1,5 +1,8 @@
 ﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Math2D
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Serialization.JSON
 Imports std = System.Math
 
 ''' <summary>
@@ -8,10 +11,20 @@ Imports std = System.Math
 Public Structure PointWithDescriptor
 
     Public Pt As PointF
+
     ''' <summary>
     ''' (distance to centroid, angle)
     ''' </summary>
     Public Descriptor As (r As Double, theta As Double)
+
+    ''' <summary>
+    ''' other property that used for weight score which is generated from the raw data
+    ''' </summary>
+    Public [properties] As Double()
+
+    Public Overrides Function ToString() As String
+        Return $"({Pt.X},{Pt.Y}) radius:{Descriptor.r}, theta:{Descriptor.theta}, properties:{properties.GetJson}"
+    End Function
 
     ''' <summary>
     ''' Computes a simple (distance, angle) descriptor for each point relative to the polygon's centroid.
@@ -36,6 +49,33 @@ Public Structure PointWithDescriptor
 
             Yield New PointWithDescriptor With {
                 .Pt = pt, .Descriptor = (r, theta)
+            }
+        Next
+    End Function
+
+    Public Shared Iterator Function ComputeDescriptors(Of T As Layout2D)(shape As IEnumerable(Of T), getMetadata As Func(Of T, Double())) As IEnumerable(Of PointWithDescriptor)
+        Dim pool As T() = shape.SafeQuery.ToArray
+
+        If pool.Length = 0 Then
+            Return
+        End If
+
+        Dim poly As New Polygon2D(pool.X, pool.Y)
+        ' Calculate centroid (using only valid points)
+        Dim centroid As PointF = poly.centroid
+
+        ' Compute descriptor for each point
+        For i As Integer = 0 To poly.length - 1
+            Dim pt As PointF = poly(i)
+            Dim dx = pt.X - centroid.X
+            Dim dy = pt.Y - centroid.Y
+            Dim r = std.Sqrt(dx * dx + dy * dy)
+            Dim theta = std.Atan2(dy, dx)
+
+            Yield New PointWithDescriptor With {
+                .Pt = pt,
+                .Descriptor = (r, theta),
+                .properties = getMetadata(pool(i))
             }
         Next
     End Function
