bd8f8a097cca0f7a795a946485d61736c10a5a38
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-12 03:37:25 +0800

    add PrincipalCurve code

---
+++ Data_science/Mathematica/Math/Math/Spline/PrincipalCurve.vb
@@ -0,0 +1,313 @@
+﻿Imports std = System.Math
+
+Namespace Interpolation
+
+    Namespace PrincipalCurveAnalysis
+        ' 二维数据点结构
+        Public Structure DataPoint
+            Public X As Double
+            Public Y As Double
+
+            Public Sub New(x As Double, y As Double)
+                Me.X = x
+                Me.Y = y
+            End Sub
+
+            Public Shared Operator -(a As DataPoint, b As DataPoint) As DataPoint
+                Return New DataPoint(a.X - b.X, a.Y - b.Y)
+            End Operator
+
+            Public Shared Operator +(a As DataPoint, b As DataPoint) As DataPoint
+                Return New DataPoint(a.X + b.X, a.Y + b.Y)
+            End Operator
+
+            Public Shared Operator *(scalar As Double, point As DataPoint) As DataPoint
+                Return New DataPoint(scalar * point.X, scalar * point.Y)
+            End Operator
+
+            Public Function DistanceTo(other As DataPoint) As Double
+                Return std.Sqrt((X - other.X) ^ 2 + (Y - other.Y) ^ 2)
+            End Function
+
+            Public Function Magnitude() As Double
+                Return std.Sqrt(X ^ 2 + Y ^ 2)
+            End Function
+
+            Public Function Normalize() As DataPoint
+                Dim mag = Magnitude()
+                If mag = 0 Then Return New DataPoint(0, 0)
+                Return New DataPoint(X / mag, Y / mag)
+            End Function
+        End Structure
+
+        ' 数学计算工具类
+        Public Class MathUtils
+            ' 计算两点之间的欧氏距离
+            Public Shared Function EuclideanDistance(p1 As DataPoint, p2 As DataPoint) As Double
+                Return std.Sqrt((p1.X - p2.X) ^ 2 + (p1.Y - p2.Y) ^ 2)
+            End Function
+
+            ' 计算点到线段的距离
+            Public Shared Function DistanceToSegment(point As DataPoint, segStart As DataPoint, segEnd As DataPoint) As Double
+                Dim l2 = EuclideanDistance(segStart, segEnd) ^ 2
+                If l2 = 0 Then Return EuclideanDistance(point, segStart)
+
+                Dim t = std.Max(0, std.Min(1, ((point.X - segStart.X) * (segEnd.X - segStart.X) +
+                                   (point.Y - segStart.Y) * (segEnd.Y - segStart.Y)) / l2))
+
+                Dim projection As New DataPoint(
+                segStart.X + t * (segEnd.X - segStart.X),
+                segStart.Y + t * (segEnd.Y - segStart.Y)
+            )
+
+                Return EuclideanDistance(point, projection)
+            End Function
+
+            ' 高斯核函数
+            Public Shared Function GaussianKernel(distance As Double, bandwidth As Double) As Double
+                Return std.Exp(-0.5 * (distance / bandwidth) ^ 2)
+            End Function
+        End Class
+    End Namespace
+
+    Public Class PrincipalCurve
+        Private _dataPoints As List(Of DataPoint)
+        Private _curvePoints As List(Of DataPoint)
+        Private _bandwidth As Double
+        Private _maxIterations As Integer
+        Private _tolerance As Double
+
+        Public Sub New(dataPoints As IEnumerable(Of DataPoint),
+                  Optional bandwidth As Double = 1.0,
+                  Optional maxIterations As Integer = 100,
+                  Optional tolerance As Double = 0.001)
+            Me._dataPoints = dataPoints.ToList()
+            Me._bandwidth = bandwidth
+            Me._maxIterations = maxIterations
+            Me._tolerance = tolerance
+            Me._curvePoints = New List(Of DataPoint)()
+        End Sub
+
+        ' 获取主曲线点
+        Public ReadOnly Property CurvePoints As List(Of DataPoint)
+            Get
+                Return _curvePoints
+            End Get
+        End Property
+
+        ' 初始化曲线（使用线性主成分）
+        Private Function InitializeCurve() As List(Of DataPoint)
+            Dim points = _dataPoints.ToArray()
+            Dim n = points.Length
+
+            ' 计算数据中心点
+            Dim meanX = points.Average(Function(p) p.X)
+            Dim meanY = points.Average(Function(p) p.Y)
+            Dim center As New DataPoint(meanX, meanY)
+
+            ' 计算协方差矩阵
+            Dim covXX = 0.0, covXY = 0.0, covYY = 0.0
+            For Each p In points
+                covXX += (p.X - meanX) * (p.X - meanX)
+                covXY += (p.X - meanX) * (p.Y - meanY)
+                covYY += (p.Y - meanY) * (p.Y - meanY)
+            Next
+
+            covXX /= n : covXY /= n : covYY /= n
+
+            ' 计算主成分方向（特征向量）
+            Dim trace = covXX + covYY
+            Dim determinant = covXX * covYY - covXY * covXY
+            Dim discriminant = Sqrt((trace / 2) ^ 2 - determinant)
+
+            Dim lambda1 = trace / 2 + discriminant
+            Dim lambda2 = trace / 2 - discriminant
+
+            ' 主特征向量
+            Dim eigenVectorX As Double, eigenVectorY As Double
+            If Abs(covXY) > 0.0001 Then
+                eigenVectorX = lambda1 - covYY
+                eigenVectorY = covXY
+            Else
+                eigenVectorX = 1
+                eigenVectorY = 0
+            End If
+
+            Dim magnitude = Sqrt(eigenVectorX ^ 2 + eigenVectorY ^ 2)
+            eigenVectorX /= magnitude
+            eigenVectorY /= magnitude
+
+            ' 生成初始曲线点（沿主成分方向）
+            Dim stdX = Sqrt(covXX)
+            Dim stdY = Sqrt(covYY)
+            Dim curveLength = 3 * Max(stdX, stdY) ' 3倍标准差
+
+            Dim initialCurve = New List(Of DataPoint)()
+            Dim steps = 20
+            For i = 0 To steps - 1
+                Dim t = (i - steps / 2) * curveLength / steps
+                Dim point As New DataPoint(
+                center.X + t * eigenVectorX,
+                center.Y + t * eigenVectorY
+            )
+                initialCurve.Add(point)
+            Next
+
+            Return initialCurve
+        End Function
+
+        ' 主曲线拟合算法[6,7](@ref)
+        Public Sub Fit()
+            ' 初始化曲线
+            _curvePoints = InitializeCurve()
+
+            Dim previousError As Double = Double.MaxValue
+            Dim currentError As Double = 0
+
+            For iter = 0 To _maxIterations - 1
+                ' 投影步骤：将每个数据点投影到曲线上
+                Dim projections = ProjectDataToCurve()
+
+                ' 重构步骤：根据投影重新估计曲线
+                ReconstructCurve(projections)
+
+                ' 平滑曲线
+                SmoothCurve()
+
+                ' 计算重构误差
+                currentError = CalculateReconstructionError(projections)
+
+                ' 检查收敛
+                If iter > 0 AndAlso Abs(previousError - currentError) < _tolerance Then
+                    Exit For
+                End If
+
+                previousError = currentError
+            Next
+        End Sub
+
+        ' 将数据点投影到曲线上[7](@ref)
+        Private Function ProjectDataToCurve() As List(Of ProjectionInfo)
+            Dim projections = New List(Of ProjectionInfo)()
+
+            For Each dataPoint In _dataPoints
+                Dim minDistance = Double.MaxValue
+                Dim closestSegmentIndex = -1
+                Dim closestT As Double = 0
+                Dim closestPoint As DataPoint
+
+                ' 找到曲线上最近的点
+                For i = 0 To _curvePoints.Count - 2
+                    Dim segStart = _curvePoints(i)
+                    Dim segEnd = _curvePoints(i + 1)
+
+                    ' 计算点到线段的距离和投影参数
+                    Dim segmentVector = segEnd - segStart
+                    Dim segmentLength = segmentVector.Magnitude()
+
+                    If segmentLength = 0 Then Continue For
+
+                    Dim toPoint = dataPoint - segStart
+                    Dim segmentDirection = segmentVector.Normalize()
+
+                    Dim t = (toPoint.X * segmentDirection.X + toPoint.Y * segmentDirection.Y) / segmentLength
+                    t = Max(0, Min(1, t)) ' 限制在[0,1]范围内
+
+                    Dim projectionPoint = segStart + t * segmentVector
+                    Dim distance = dataPoint.DistanceTo(projectionPoint)
+
+                    If distance < minDistance Then
+                        minDistance = distance
+                        closestSegmentIndex = i
+                        closestT = t
+                        closestPoint = projectionPoint
+                    End If
+                Next
+
+                If closestSegmentIndex >= 0 Then
+                    projections.Add(New ProjectionInfo(
+                    dataPoint, closestPoint, closestSegmentIndex + closestT, minDistance))
+                End If
+            Next
+
+            Return projections
+        End Function
+
+        ' 根据投影重新构建曲线[6](@ref)
+        Private Sub ReconstructCurve(projections As List(Of ProjectionInfo))
+            If projections.Count = 0 Then Return
+
+            ' 按投影位置排序
+            Dim sortedProjections = projections.OrderBy(Function(p) p.CurveParameter).ToList()
+
+            ' 为每个曲线点计算加权平均
+            For i = 0 To _curvePoints.Count - 1
+                Dim curveParam = i ' 曲线参数化位置
+
+                Dim totalWeight = 0.0
+                Dim weightedSumX = 0.0
+                Dim weightedSumY = 0.0
+
+                For Each proj In sortedProjections
+                    ' 使用高斯核计算权重（基于参数距离）
+                    Dim paramDistance = Abs(proj.CurveParameter - curveParam)
+                    Dim weight = MathUtils.GaussianKernel(paramDistance, _bandwidth)
+
+                    ' 也考虑数据点到当前曲线点的距离
+                    Dim spatialDistance = _curvePoints(i).DistanceTo(proj.DataPoint)
+                    Dim spatialWeight = MathUtils.GaussianKernel(spatialDistance, _bandwidth * 2)
+
+                    Dim totalWeightHere = weight * spatialWeight
+
+                    totalWeight += totalWeightHere
+                    weightedSumX += totalWeightHere * proj.DataPoint.X
+                    weightedSumY += totalWeightHere * proj.DataPoint.Y
+                Next
+
+                If totalWeight > 0 Then
+                    _curvePoints(i) = New DataPoint(weightedSumX / totalWeight, weightedSumY / totalWeight)
+                End If
+            Next
+        End Sub
+
+        ' 平滑曲线（防止过拟合）
+        Private Sub SmoothCurve()
+            If _curvePoints.Count < 3 Then Return
+
+            Dim smoothedPoints = New List(Of DataPoint)()
+            smoothedPoints.Add(_curvePoints(0)) ' 保持起点不变
+
+            For i = 1 To _curvePoints.Count - 2
+                ' 简单移动平均平滑
+                Dim smoothedX = (_curvePoints(i - 1).X + _curvePoints(i).X + _curvePoints(i + 1).X) / 3
+                Dim smoothedY = (_curvePoints(i - 1).Y + _curvePoints(i).Y + _curvePoints(i + 1).Y) / 3
+                smoothedPoints.Add(New DataPoint(smoothedX, smoothedY))
+            Next
+
+            smoothedPoints.Add(_curvePoints(_curvePoints.Count - 1)) ' 保持终点不变
+            _curvePoints = smoothedPoints
+        End Sub
+
+        ' 计算重构误差
+        Private Function CalculateReconstructionError(projections As List(Of ProjectionInfo)) As Double
+            If projections.Count = 0 Then Return 0
+            Return projections.Average(Function(p) p.Distance)
+        End Function
+
+        ' 投影信息辅助类
+        Private Class ProjectionInfo
+            Public Property DataPoint As DataPoint
+            Public Property ProjectionPoint As DataPoint
+            Public Property CurveParameter As Double
+            Public Property Distance As Double
+
+            Public Sub New(dataPoint As DataPoint, projectionPoint As DataPoint,
+                      curveParameter As Double, distance As Double)
+                Me.DataPoint = dataPoint
+                Me.ProjectionPoint = projectionPoint
+                Me.CurveParameter = curveParameter
+                Me.Distance = distance
+            End Sub
+        End Class
+    End Class
+End Namespace
---
+++ Data_science/Visualization/Plots/PrincipalCurveVisualizer.vb
@@ -0,0 +1,88 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Scripting.Expressions
+
+Public Class PrincipalCurveVisualizer
+    Private _dataPoints As List(Of DataPoint)
+    Private _curvePoints As List(Of DataPoint)
+    Private _bounds As RectangleF
+
+    Public Sub New(dataPoints As List(Of DataPoint), curvePoints As List(Of DataPoint))
+        Me._dataPoints = dataPoints
+        Me._curvePoints = curvePoints
+        CalculateBounds()
+    End Sub
+
+    Private Sub CalculateBounds()
+        If _dataPoints.Count = 0 Then
+            _bounds = New RectangleF(0, 0, 100, 100)
+            Return
+        End If
+
+        Dim minX = _dataPoints.Min(Function(p) p.X)
+        Dim maxX = _dataPoints.Max(Function(p) p.X)
+        Dim minY = _dataPoints.Min(Function(p) p.Y)
+        Dim maxY = _dataPoints.Max(Function(p) p.Y)
+
+        ' 添加一些边距
+        Dim marginX = (maxX - minX) * 0.1
+        Dim marginY = (maxY - minY) * 0.1
+
+        _bounds = New RectangleF(CSng(minX - marginX), CSng(minY - marginY),
+                                CSng(maxX - minX + 2 * marginX), CSng(maxY - minY + 2 * marginY))
+    End Sub
+
+    ' 在Graphics对象上绘制[3,5](@ref)
+    Public Sub Draw(g As Graphics, width As Integer, height As Integer)
+        If _dataPoints.Count = 0 Then Return
+
+        ' 计算缩放和偏移
+        Dim scaleX = width / _bounds.Width
+        Dim scaleY = height / _bounds.Height
+        Dim scale = Math.Min(scaleX, scaleY) * 0.9 ' 保留边距
+
+        Dim offsetX = -_bounds.Left + (width / scale - _bounds.Width) / 2
+        Dim offsetY = -_bounds.Top + (height / scale - _bounds.Height) / 2
+
+        ' 设置高质量渲染
+        g.SmoothingMode = SmoothingMode.AntiAlias
+
+        ' 绘制数据点[5](@ref)
+        Dim pointBrush As New SolidBrush(Color.Blue)
+        Dim pointRadius As Single = 3
+
+        For Each point In _dataPoints
+            Dim screenX = CSng((point.X + offsetX) * scale)
+            Dim screenY = CSng((point.Y + offsetY) * scale)
+            g.FillEllipse(pointBrush, screenX - pointRadius, screenY - pointRadius,
+                         pointRadius * 2, pointRadius * 2)
+        Next
+
+        ' 绘制主曲线[3](@ref)
+        If _curvePoints.Count > 1 Then
+            Dim curvePen As New Pen(Color.Red, 2)
+            Dim curvePoints = _curvePoints.Select(Function(p)
+                                                      Return New PointF(CSng((p.X + offsetX) * scale), CSng((p.Y + offsetY) * scale))
+                                                  End Function).ToArray()
+
+            g.DrawLines(curvePen, curvePoints)
+            curvePen.Dispose()
+        End If
+
+        pointBrush.Dispose()
+    End Sub
+
+    ' 在PictureBox中显示结果
+    Public Sub DrawToPictureBox(pictureBox As PictureBox)
+        If pictureBox.Width = 0 OrElse pictureBox.Height = 0 Then Return
+
+        Using bmp As New Bitmap(pictureBox.Width, pictureBox.Height)
+            Using g As Graphics = Graphics.FromImage(bmp)
+                g.Clear(Color.White)
+                Draw(g, pictureBox.Width, pictureBox.Height)
+            End Using
+            pictureBox.Image = bmp.Clone()
+        End Using
+    End Sub
+End Class
