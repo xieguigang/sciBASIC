2f7f039ef98a1402093e809be4a4db4ea35dfc4d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-16 01:50:44 +0800

    move the gdi+ bridge model source files outside the folder

---
+++ Microsoft.VisualBasic.Core/src/Drawing/Bitmap/BitmapTools.vb
@@ -0,0 +1,70 @@
+﻿Imports System.Drawing
+Imports std = System.Math
+
+Namespace Imaging.BitmapImage
+
+    Public Class BitmapTools
+
+        ''' <summary>
+        ''' 裁剪BitmapBuffer的指定区域
+        ''' </summary>
+        ''' <param name="sourceBuffer">源BitmapBuffer</param>
+        ''' <param name="cropRect">裁剪区域</param>
+        ''' <returns>裁剪后的新BitmapBuffer</returns>
+        Public Shared Function CropBitmapBuffer(sourceBuffer As BitmapBuffer, cropRect As Rectangle) As BitmapBuffer
+            ' 验证输入参数
+            If sourceBuffer Is Nothing Then
+                Throw New ArgumentNullException("sourceBuffer")
+            End If
+
+            If sourceBuffer.RawBuffer Is Nothing OrElse sourceBuffer.RawBuffer.Length = 0 Then
+                Throw New ArgumentException("源缓冲区为空")
+            End If
+
+            If cropRect.Width <= 0 OrElse cropRect.Height <= 0 Then
+                Throw New ArgumentException("裁剪区域尺寸无效")
+            End If
+
+            ' 确保裁剪区域在源图像范围内[4,5](@ref)
+            cropRect = GetValidCropRectangle(cropRect, sourceBuffer.Width, sourceBuffer.Height)
+
+            If cropRect.Width <= 0 OrElse cropRect.Height <= 0 Then
+                Throw New ArgumentException("裁剪区域超出图像范围")
+            End If
+
+            ' 创建新的BitmapBuffer来存储裁剪结果
+            Dim newWidth As Integer = cropRect.Width
+            Dim newHeight As Integer = cropRect.Height
+            Dim newBuffer As Byte() = New Byte(newWidth * newHeight * 4 - 1) {}
+
+            ' 计算源图像和目标图像的行字节数
+            Dim sourceStride As Integer = sourceBuffer.Width * 4
+            Dim destStride As Integer = newWidth * 4
+
+            ' 执行像素数据复制
+            For y As Integer = 0 To newHeight - 1
+                Dim sourceY As Integer = cropRect.Y + y
+                ' 计算源和目标行的起始索引
+                Dim sourceStartIndex As Integer = sourceY * sourceStride + cropRect.X * 4
+                Dim destStartIndex As Integer = y * destStride
+
+                ' 复制整行数据
+                Call Array.Copy(sourceBuffer.RawBuffer, sourceStartIndex, newBuffer, destStartIndex, destStride)
+            Next
+
+            Return newBuffer
+        End Function
+
+        ''' <summary>
+        ''' 确保裁剪矩形在图像有效范围内
+        ''' </summary>
+        Private Shared Function GetValidCropRectangle(rect As Rectangle, maxWidth As Integer, maxHeight As Integer) As Rectangle
+            Dim x As Integer = std.Max(0, std.Min(rect.X, maxWidth - 1))
+            Dim y As Integer = std.Max(0, std.Min(rect.Y, maxHeight - 1))
+            Dim width As Integer = std.Min(rect.Width, maxWidth - x)
+            Dim height As Integer = std.Min(rect.Height, maxHeight - y)
+
+            Return New Rectangle(x, y, width, height)
+        End Function
+    End Class
+End Namespace
---
+++ Microsoft.VisualBasic.Core/src/Drawing/GDI+/ImageTools.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+
+#If NET48 Then
+Imports Bitmap = System.Drawing.Bitmap
+#Else
+Imports Microsoft.VisualBasic.Imaging.BitmapImage
+#End If
+
+Namespace Imaging
+
+    Public Module ImageTools
+
+        ''' <summary>
+        ''' Crop an image 
+        ''' </summary>
+        ''' <param name="img">image to crop</param>
+        ''' <param name="cropArea">rectangle to crop</param>
+        ''' <returns>resulting image</returns>
+        ''' 
+        <Extension>
+        Public Function ImageCrop(img As Image, cropArea As Rectangle) As Image
+            Dim bmpImage As New Bitmap(img)
+            Dim bmpCrop As Bitmap
+
+#If NET48 Then
+            bmpCrop = bmpImage.Clone(cropArea, bmpImage.PixelFormat)
+#Else
+            Dim buffer As BitmapBuffer = BitmapBuffer.FromBitmap(bmpImage)
+            Dim crop As BitmapBuffer = BitmapTools.CropBitmapBuffer(buffer, cropArea)
+
+            bmpCrop = New Bitmap(crop)
+#End If
+            Return bmpCrop
+        End Function
+
+
+    End Module
+End Namespace
