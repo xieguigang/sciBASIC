66ba578fa692d4eb0717cc32f8c1a7b71c87b2a3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-28 03:34:38 +0800

    create graphology json model

---
+++ gr/network-visualization/Network.IO.Extensions/SigmaJs.vb
@@ -0,0 +1,10 @@
+﻿''' <summary>
+''' Create json for sigma.js
+''' </summary>
+Public Module SigmaJs
+
+    Public Function AsGraphology() As graphology.graph
+
+    End Function
+
+End Module
---
+++ gr/network-visualization/Network.IO.Extensions/graphology.vb
@@ -0,0 +1,31 @@
+﻿Namespace graphology
+
+    Public Class node
+
+        Public Property id As String
+        Public Property label As String
+        Public Property x As Double
+        Public Property y As Double
+        Public Property size As Double
+        Public Property color As String
+
+    End Class
+
+    Public Class edge
+
+        Public Property id As String
+        Public Property source As String
+        Public Property target As String
+        Public Property size As Double
+        Public Property color As String
+
+    End Class
+
+    Public Class graph
+
+        Public Property nodes As node()
+        Public Property edges As edge()
+
+    End Class
+
+End Namespace
