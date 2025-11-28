fc0cde5137fdbcb806645f00dc62587484d18798
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-28 18:30:42 +0800

    make improvements of the data model

--- Data_science/Graph/Analysis/Dijkstra/Route.vb
+++ Data_science/Graph/Analysis/Dijkstra/Route.vb
@@ -1,65 +1,67 @@
 ﻿#Region "Microsoft.VisualBasic::e594b8b64c5b14f252096dea7cf45b15, Data_science\Graph\Analysis\Dijkstra\Route.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 67
-    '    Code Lines: 50 (74.63%)
-    ' Comment Lines: 3 (4.48%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 14 (20.90%)
-    '     File Size: 1.99 KB
+' Summaries:
 
 
-    '     Class Route
-    ' 
-    '         Properties: Connections, Cost, Count, id
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: ContainsNode, ToString
-    ' 
-    '         Sub: Add, Clear, SetValue
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 67
+'    Code Lines: 50 (74.63%)
+' Comment Lines: 3 (4.48%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 14 (20.90%)
+'     File Size: 1.99 KB
+
+
+'     Class Route
+' 
+'         Properties: Connections, Cost, Count, id
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: ContainsNode, ToString
+' 
+'         Sub: Add, Clear, SetValue
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.Linq
 Imports Connection = Microsoft.VisualBasic.Data.GraphTheory.VertexEdge
 
 Namespace Analysis.Dijkstra
@@ -67,7 +69,7 @@ Namespace Analysis.Dijkstra
     ''' <summary>
     ''' 从出发点到终点所经过的路径
     ''' </summary>
-    Public Class Route : Implements IReadOnlyId
+    Public Class Route : Implements IReadOnlyId, Enumeration(Of Connection)
 
         Public ReadOnly Property id As String Implements IReadOnlyId.Identity
 
@@ -89,8 +91,16 @@ Namespace Analysis.Dijkstra
             End Set
         End Property
 
+        ''' <summary>
+        ''' sum edge weight as cost
+        ''' </summary>
+        ''' <returns></returns>
         Public Property Cost As Double
 
+        ''' <summary>
+        ''' size/length of the route path
+        ''' </summary>
+        ''' <returns></returns>
         Public ReadOnly Property Count As Integer
             Get
                 Return route.Count
@@ -116,12 +126,19 @@ Namespace Analysis.Dijkstra
             Call route.Add(item)
         End Sub
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Sub Clear()
             Call route.Clear()
         End Sub
 
         Public Overrides Function ToString() As String
             Return "Id: " & id & ", cost: " & Cost
+        End Function
+
+        Public Iterator Function GenericEnumerator() As IEnumerator(Of Connection) Implements Enumeration(Of Connection).GenericEnumerator
+            For Each con As Connection In Connections
+                Yield con
+            Next
         End Function
     End Class
 End Namespace
