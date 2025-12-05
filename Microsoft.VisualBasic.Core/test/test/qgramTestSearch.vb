25bbf929bb70fcdc41cf960b97a511a17c6ef180
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-05 18:54:23 +0800

    Create qgramTestSearch.vb

---
+++ Microsoft.VisualBasic.Core/test/test/qgramTestSearch.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+Imports Microsoft.VisualBasic.Language
+
+Module qgramTestSearch
+
+    Sub Run()
+        Dim q As New QGramIndex(3)
+        Dim i As i32 = 0
+
+        Call q.AddString("", ++i)
+        Call q.AddString("", ++i)
+        Call q.AddString("", ++i)
+        Call q.AddString("test ATP token", ++i)
+        Call q.AddString("", ++i)
+        Call q.AddString("", ++i)
+        Call q.AddString("ATP", ++i)
+        Call q.AddString("test acid", ++i)
+        Call q.AddString("ATP", ++i)
+        Call q.AddString("ATP+", ++i)
+        Call q.AddString("Hello world", ++i)
+        Call q.AddString("test", ++i)
+
+        Dim find1 = q.FindSimilar("ATP").ToArray
+        Dim find2 = q.FindSimilar("atp").ToArray
+
+        Pause()
+    End Sub
+End Module
