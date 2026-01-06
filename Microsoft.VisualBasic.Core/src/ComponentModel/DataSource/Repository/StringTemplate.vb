f3ab36e0c35ebe26e182d9cda498be1aad799039
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-06 11:55:33 +0800

    Create StringTemplate.vb

---
+++ Microsoft.VisualBasic.Core/src/ComponentModel/DataSource/Repository/StringTemplate.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Text
+
+Namespace ComponentModel.DataSourceModel.Repository
+
+    Public Interface IKeyDataReader
+
+        Function GetData(key As String) As String
+
+    End Interface
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <remarks>
+    ''' template syntax example:
+    ''' 
+    ''' &lt;locus_tag>|taxid|&lt;product>
+    ''' </remarks>
+    Public Class StringTemplate
+
+        ReadOnly template As String
+        ReadOnly defaults As Dictionary(Of String, String)
+        ReadOnly keys As String()
+
+        Sub New(template As String, Optional defaults As Dictionary(Of String, String) = Nothing)
+            Me.template = template
+            Me.defaults = If(defaults, New Dictionary(Of String, String))
+            Me.keys = ParseKeys(template)
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Private Shared Function ParseKeys(template As String) As IEnumerable(Of String)
+            Return template.Matches("[<].*?[>]", RegexICSng)
+        End Function
+
+        ''' <summary>
+        ''' Create a string based on the given template
+        ''' </summary>
+        ''' <typeparam name="T"></typeparam>
+        ''' <param name="i"></param>
+        ''' <returns></returns>
+        Public Function CreateString(Of T As IKeyDataReader)(i As T) As String
+            Dim str As New StringBuilder(template)
+            Dim value As String
+
+            For Each key As String In keys
+                value = i.GetData(key)
+
+                If value.StringEmpty Then
+                    value = defaults.TryGetValue(key, default:="unknown")
+                End If
+
+                Call str.Replace($"<{key}>", value)
+            Next
+
+            Return str.ToString
+        End Function
+
+    End Class
+End Namespace
