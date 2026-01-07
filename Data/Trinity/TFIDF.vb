bf6f98d214861d6504b566a08763de35d34968e1
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-07 13:50:33 +0800

    start to implements TF-IDF framework

---
+++ Data/Trinity/TFIDF.vb
@@ -0,0 +1,31 @@
+﻿Public Class TFIDF
+
+    ReadOnly vecs As New Dictionary(Of String, Dictionary(Of String, Integer))
+
+    ''' <summary>
+    ''' get N sequence
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property N As Integer
+        Get
+            Return vecs.Count
+        End Get
+    End Property
+
+    Sub New()
+    End Sub
+
+    Public Sub Add(id As String, seq As IEnumerable(Of String))
+        Dim counter As Dictionary(Of String, Integer) = seq _
+            .GroupBy(Function(a) a) _
+            .ToDictionary(Function(a) a.Key,
+                          Function(a)
+                              Return a.Count
+                          End Function)
+
+        Call vecs.Add(id, seq)
+    End Sub
+
+
+
+End Class
