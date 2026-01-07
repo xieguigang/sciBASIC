318e0814e2606db2a5b66f3eb07146a08bcfdfc1
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-07 16:41:52 +0800

    create CartesianProduct helper

---
+++ Microsoft.VisualBasic.Core/src/ComponentModel/Algorithm/base/Combination/CartesianProduct.vb
@@ -0,0 +1,66 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace ComponentModel.Algorithm.base
+
+    ''' <summary>
+    ''' 笛卡尔积是集合论中的一个基本概念，它描述了如何从两个或多个集合中生成所有可能的有序组合。
+    ''' </summary>
+    Public Module CartesianProduct
+
+        ''' <summary>
+        ''' 生成两个序列的两两组合 ``{<paramref name="seq_1"/> -> <paramref name="seq_2"/>}[]``
+        ''' </summary>
+        ''' <typeparam name="TA"></typeparam>
+        ''' <typeparam name="TB"></typeparam>
+        ''' <param name="seq_1"></param>
+        ''' <param name="seq_2"></param>
+        ''' <returns></returns>
+        <Extension>
+        Public Iterator Function CreateCombos(Of TA, TB)(seq_1 As IEnumerable(Of TA), seq_2 As IEnumerable(Of TB)) As IEnumerable(Of (a As TA, b As TB))
+            Dim b As TB() = seq_2.ToArray
+
+            For Each i As TA In seq_1
+                For Each j As TB In b
+                    Yield (i, j)
+                Next
+            Next
+        End Function
+
+        ''' <summary>
+        ''' 生成三个序列的笛卡尔积（所有可能的三元组组合）
+        ''' </summary>
+        ''' <typeparam name="TA">第一个序列的元素类型</typeparam>
+        ''' <typeparam name="TB">第二个序列的元素类型</typeparam>
+        ''' <typeparam name="TC">第三个序列的元素类型</typeparam>
+        ''' <param name="seq1">第一个序列</param>
+        ''' <param name="seq2">第二个序列</param>
+        ''' <param name="seq3">第三个序列</param>
+        ''' <returns>包含所有可能三元组的序列</returns>
+        <Extension>
+        Public Iterator Function CreateTripleCartesianProduct(Of TA, TB, TC)(
+            seq1 As IEnumerable(Of TA),
+            seq2 As IEnumerable(Of TB),
+            seq3 As IEnumerable(Of TC)) As IEnumerable(Of (First As TA, Second As TB, Third As TC))
+
+            ' 将第二和第三个序列转换为数组，避免多次枚举
+            Dim array2 As TB() = seq2.ToArray()
+            Dim array3 As TC() = seq3.ToArray()
+
+            ' 三重嵌套循环生成所有组合
+            For Each item1 In seq1
+                For Each item2 In array2
+                    For Each item3 In array3
+                        Yield (item1, item2, item3)
+                    Next
+                Next
+            Next
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        <Extension>
+        Public Function FullCombination(Of T)(seq As IEnumerable(Of T)) As IEnumerable(Of (a As T, b As T))
+            Dim all = seq.ToArray
+            Return all.CreateCombos(all)
+        End Function
+    End Module
+End Namespace
---
+++ Microsoft.VisualBasic.Core/src/ComponentModel/Algorithm/base/Combination/NDimensionCartesianProduct.vb
@@ -0,0 +1,53 @@
+﻿Namespace ComponentModel.Algorithm.base
+
+    ''' <summary>
+    ''' Create a vs b vs c ...
+    ''' </summary>
+    Public NotInheritable Class NDimensionCartesianProduct
+
+        Private Sub New()
+        End Sub
+
+        Private Class InternalLambda(Of T)
+
+            Public sequences As T()()
+
+            ' 递归辅助函数
+            Public Function helper(depth As Integer, currentCombination As List(Of T)) As IEnumerable(Of T())
+                If depth = sequences.Length Then
+                    Return {currentCombination.ToArray()}
+                Else
+                    Dim result As New List(Of T())()
+
+                    For Each item As T In sequences(depth)
+                        Dim newCombination As New List(Of T)(currentCombination)
+
+                        Call newCombination.Add(item)
+                        Call result.AddRange(helper(depth + 1, newCombination))
+                    Next
+
+                    Return result
+                End If
+            End Function
+        End Class
+
+        ''' <summary>
+        ''' 生成任意数量集合的笛卡尔积（递归实现）
+        ''' </summary>
+        Public Shared Iterator Function CreateMultiCartesianProduct(Of T)(ParamArray sequences As IEnumerable(Of T)()) As IEnumerable(Of T())
+            If sequences Is Nothing OrElse sequences.Length = 0 Then
+                Return
+            End If
+
+            Dim recursive As New InternalLambda(Of T) With {
+                .sequences = sequences _
+                    .Select(Function(a) a.ToArray) _
+                    .ToArray
+            }
+
+            For Each combo As T() In recursive.helper(0, New List(Of T)())
+                Yield combo
+            Next
+        End Function
+    End Class
+End Namespace
