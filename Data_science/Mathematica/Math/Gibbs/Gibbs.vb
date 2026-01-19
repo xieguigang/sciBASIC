4f8d221c094d6fcc1c6e446119cc2d346c013704
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-19 14:59:14 +0800

    split spiurce file into multiple parts

--- Data_science/Mathematica/Math/Gibbs/Gibbs.vb
+++ Data_science/Mathematica/Math/Gibbs/Gibbs.vb
@@ -60,24 +60,6 @@
 
 Imports randf2 = Microsoft.VisualBasic.Math.RandomExtensions
 
-Public Class Score
-
-    Public ReadOnly Property pwm As String
-        Get
-            Return Mid(seq, start + 1, len)
-        End Get
-    End Property
-
-    Public Property seq As String
-    Public Property start As Integer
-    Public Property len As Integer
-
-    Public Overrides Function ToString() As String
-        Return pwm
-    End Function
-
-End Class
-
 ''' <summary>
 ''' Gibb's Sampling Steps:<br/>
 ''' 
---
+++ Data_science/Mathematica/Math/Gibbs/Score.vb
@@ -0,0 +1,17 @@
+﻿Public Class Score
+
+    Public ReadOnly Property pwm As String
+        Get
+            Return Mid(seq, start + 1, len)
+        End Get
+    End Property
+
+    Public Property seq As String
+    Public Property start As Integer
+    Public Property len As Integer
+
+    Public Overrides Function ToString() As String
+        Return pwm
+    End Function
+
+End Class
