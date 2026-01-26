e945df3435c754aec882c62ff6fbf74876def41e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-26 11:26:00 +0800

    Create JsonIgnoreAttribute.vb

---
+++ Microsoft.VisualBasic.Core/src/Serialization/JSON/JsonIgnoreAttribute.vb
@@ -0,0 +1,10 @@
+﻿Namespace Serialization.JSON
+
+    ' fix for the missing attribute from .NET core System.Text.Json.Serialization
+
+#If NET48 Then
+Public Class JsonIgnoreAttribute : Inherits Attribute
+End Class
+#End If
+
+End Namespace
