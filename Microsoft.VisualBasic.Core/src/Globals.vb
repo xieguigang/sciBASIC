f637811bb78fb3c768963dd754d59b3261ccedb3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-27 15:26:42 +0800

    fix of the LLMs proxy

--- Microsoft.VisualBasic.Core/src/Globals.vb
+++ Microsoft.VisualBasic.Core/src/Globals.vb
@@ -64,7 +64,7 @@ Imports any = Microsoft.VisualBasic.Scripting
 ''' <summary>
 ''' global constants
 ''' </summary>
-Public Module Globals
+Partial Public Module Globals
 
     ''' <summary>
     ''' The first element in a collection.
@@ -101,25 +101,4 @@ Public Module Globals
 
         Call strMsg.info
     End Sub
-
-    Dim ollama As Func(Of String, String)
-
-    Public Sub HookOllama(chat As Func(Of String, String))
-        ollama = chat
-    End Sub
-
-    ''' <summary>
-    ''' A proxy function for LLMs talk in framework runtime
-    ''' </summary>
-    ''' <param name="msg"></param>
-    ''' <returns>
-    ''' this proxy function will returns nothing if there is no ollama client is hooked
-    ''' </returns>
-    Public Function LLMsTalk(msg As String) As String
-        If ollama Is Nothing Then
-            Return Nothing
-        Else
-            Return ollama(msg)
-        End If
-    End Function
 End Module
