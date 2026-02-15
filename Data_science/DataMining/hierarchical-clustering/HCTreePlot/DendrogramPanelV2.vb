199d471a2b0c2d8c27eb3309610690df052834f4
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-15 16:02:53 +0800

    config of the plot styles

--- Data_science/DataMining/hierarchical-clustering/HCTreePlot/DendrogramPanelV2.vb
+++ Data_science/DataMining/hierarchical-clustering/HCTreePlot/DendrogramPanelV2.vb
@@ -74,6 +74,7 @@ Imports std = System.Math
 Public Class DendrogramPanelV2 : Inherits DendrogramPanel
 
     Protected labels As New List(Of NamedValue(Of PointF))
+    Protected legendWidth As Single = 20
 
     Public Sub New(hist As Cluster, theme As Theme,
                    Optional classes As ColorClass() = Nothing,
@@ -200,7 +201,7 @@ Public Class DendrogramPanelV2 : Inherits DendrogramPanel
             Dim d As Double = std.Max(charWidth / 2, theme.pointSize)
             Dim layout As New Rectangle With {
                 .Location = New Point(x + d, y - unitWidth / 2),
-                .Size = New Size(labelPadding - d * 1.25, unitWidth)
+                .Size = New Size(legendWidth, unitWidth)
             }
 
             Call g.FillRectangle(color, layout)
