d3ea72d4796f5abaa4afca893fe5695ce1599309
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-23 22:52:44 +0800

    fix of the product ion display

--- Data_science/Visualization/Plots/BarPlot/Plots/PlotAlignmentGroup.vb
+++ Data_science/Visualization/Plots/BarPlot/Plots/PlotAlignmentGroup.vb
@@ -1,63 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::8b697b48c58e4e85c1abb95d49c75937, Data_science\Visualization\Plots\BarPlot\Plots\PlotAlignmentGroup.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 587
-    '    Code Lines: 412 (70.19%)
-    ' Comment Lines: 72 (12.27%)
-    '    - Xml Docs: 11.11%
-    ' 
-    '   Blank Lines: 103 (17.55%)
-    '     File Size: 25.74 KB
+' Summaries:
 
 
-    '     Class PlotAlignmentGroup
-    ' 
-    '         Properties: bw, displayX, highlightMargin, hitsHightLights, idTag
-    '                     labelPlotStrength, legendLayout, queryName, subjectName, XAxisLabelCss
-    '                     xError
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: Hit
-    ' 
-    '         Sub: DrawAlignmentBars, DrawLegendTitleRegion, DrawLegendTopRight, DrawLegeneds, DrawTextLabels
-    '              PlotInternal
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 587
+'    Code Lines: 412 (70.19%)
+' Comment Lines: 72 (12.27%)
+'    - Xml Docs: 11.11%
+' 
+'   Blank Lines: 103 (17.55%)
+'     File Size: 25.74 KB
+
+
+'     Class PlotAlignmentGroup
+' 
+'         Properties: bw, displayX, highlightMargin, hitsHightLights, idTag
+'                     labelPlotStrength, legendLayout, queryName, subjectName, XAxisLabelCss
+'                     xError
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: Hit
+' 
+'         Sub: DrawAlignmentBars, DrawLegendTitleRegion, DrawLegendTopRight, DrawLegeneds, DrawTextLabels
+'              PlotInternal
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -440,7 +440,7 @@ Namespace BarPlot
 
                     ' Call textCloud.add_label(New TextRectangle("", rect))
 
-                    If displayX AndAlso o.value / yrange.Max >= labelPlotStrength Then
+                    If displayX AndAlso ((o.value / yrange.Max >= labelPlotStrength) OrElse checkHighlight.yes) Then
                         xlabel = o.x.ToString(theme.tagFormat)
 
                         If checkHighlight.yes AndAlso Not checkHighlight.X.Name.StringEmpty(, True) Then
@@ -511,7 +511,7 @@ Namespace BarPlot
 
                     ' Call textCloud.add_label(New TextRectangle("", rect))
 
-                    If displayX AndAlso o.value / yrange.Max >= labelPlotStrength Then
+                    If displayX AndAlso ((o.value / yrange.Max >= labelPlotStrength) OrElse checkHighlight.yes) Then
                         xlabel = o.x.ToString(theme.tagFormat)
 
                         If checkHighlight.yes AndAlso Not checkHighlight.X.Name.StringEmpty(, True) Then
