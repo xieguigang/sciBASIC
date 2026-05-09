13776f41f693d74731b84dd9969cc8fe81af0e98
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-10 00:14:43 +0800

    make code style improvements

--- Data_science/Graph/MST/Kruskal.vb
+++ Data_science/Graph/MST/Kruskal.vb
@@ -1,58 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::c9f0a238f5b0db9ae13dec5f0fac5332, Data_science\Graph\MST\Kruskal.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 59
-    '    Code Lines: 43 (72.88%)
-    ' Comment Lines: 3 (5.08%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 13 (22.03%)
-    '     File Size: 1.87 KB
+' Summaries:
 
 
-    '     Class Kruskal
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: calculateNumNodes, findMinTree, NodeIdTuple
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 59
+'    Code Lines: 43 (72.88%)
+' Comment Lines: 3 (5.08%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 13 (22.03%)
+'     File Size: 1.87 KB
+
+
+'     Class Kruskal
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: calculateNumNodes, findMinTree, NodeIdTuple
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.Expressions
@@ -79,8 +80,11 @@ Namespace MinimumSpanningTree
             numNodes = calculateNumNodes(edge)
         End Sub
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Function calculateNumNodes(edges As IEnumerable(Of VertexEdge)) As Integer
-            Return (From i In edges.AsEnumerable Select NodeIdTuple(i)).IteratesALL.Distinct.Count
+            Return (From i As VertexEdge
+                    In edges
+                    Select NodeIdTuple(i)).IteratesALL.Distinct.Count
         End Function
 
         Private Iterator Function NodeIdTuple(i As VertexEdge) As IEnumerable(Of Integer)
