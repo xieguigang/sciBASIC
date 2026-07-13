f5229be8694215b1b11b2eae0f92fbcdf1f16d30
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-13 11:08:05 +0800

    removes obsolete code

--- gr/Landscape/3DBuilder/Project.vb
+++ gr/Landscape/3DBuilder/Project.vb
@@ -1,60 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::c62d482cdccddd8af53a7e42e791c69d, gr\Landscape\3DBuilder\Project.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 54
-    '    Code Lines: 33 (61.11%)
-    ' Comment Lines: 13 (24.07%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 8 (14.81%)
-    '     File Size: 1.80 KB
+' Summaries:
 
 
-    '     Class Project
-    ' 
-    '         Properties: model, Thumbnail
-    ' 
-    '         Function: FromZipDirectory, GetMatrix, GetSurfaces
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 54
+'    Code Lines: 33 (61.11%)
+' Comment Lines: 13 (24.07%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 8 (14.81%)
+'     File Size: 1.80 KB
+
+
+'     Class Project
+' 
+'         Properties: model, Thumbnail
+' 
+'         Function: FromZipDirectory, GetMatrix, GetSurfaces
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Drawing
 Imports Microsoft.VisualBasic.Imaging.Drawing3D
 Imports Microsoft.VisualBasic.Imaging.Drawing3D.Math3D
 Imports Microsoft.VisualBasic.Imaging.Landscape.Vendor_3mf.XML
--- gr/Landscape/Surface.vb
+++ gr/Landscape/Surface.vb
@@ -1,60 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::5b04740f9e115f48cc1b36ad30215d4f, gr\Landscape\Surface.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 62
-    '    Code Lines: 26 (41.94%)
-    ' Comment Lines: 28 (45.16%)
-    '    - Xml Docs: 71.43%
-    ' 
-    '   Blank Lines: 8 (12.90%)
-    '     File Size: 2.33 KB
+' Summaries:
 
 
-    '     Class Surface
-    ' 
-    '         Properties: Brush, paint, vertices
-    ' 
-    '         Function: CreateObject, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 62
+'    Code Lines: 26 (41.94%)
+' Comment Lines: 28 (45.16%)
+'    - Xml Docs: 71.43%
+' 
+'   Blank Lines: 8 (12.90%)
+'     File Size: 2.33 KB
+
+
+'     Class Surface
+' 
+'         Properties: Brush, paint, vertices
+' 
+'         Function: CreateObject, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Drawing
 Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.Imaging.Drawing3D
 Imports Microsoft.VisualBasic.Linq
@@ -104,15 +103,5 @@ Namespace Data
         Public Overrides Function ToString() As String
             Return paint
         End Function
-
-        'Public Iterator Function GetEnumerator() As IEnumerator(Of Point3D) Implements IEnumerable(Of Point3D).GetEnumerator
-        '    For Each x As Point3D In vertices
-        '        Yield x
-        '    Next
-        'End Function
-
-        'Private Iterator Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
-        '    Yield GetEnumerator()
-        'End Function
     End Class
 End Namespace
