d7088408da271ff15c44b8d7e3ce0c136a7dd8b6
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-29 03:33:13 +0800

    fix of the missing nuget package referemnce

--- vs_solutions/dev/VisualStudio/VBProject/Project/Extensions.vb
+++ vs_solutions/dev/VisualStudio/VBProject/Project/Extensions.vb
@@ -88,20 +88,22 @@ Namespace VBProj
         ''' <returns></returns>
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         <Extension>
-        Public Function EnumerateSourceFiles(vbproj As Project,
+        Public Function EnumerateSourceFiles(vbproj As VBProject,
                                              Optional skipAssmInfo As Boolean = False,
                                              Optional fullName As Boolean = False) As IEnumerable(Of String)
 
             Dim sourceFolder As String = DirectCast(vbproj, IFileReference).FilePath.ParentPath
-            Dim sourceList As IEnumerable(Of String)
+            Dim sourceList As IEnumerable(Of String) = vbproj.CompileFiles _
+                .SafeQuery _
+                .Select(Function(c)
+                            Return c.FileName
+                        End Function)
 
-            If vbproj.IsDotNetCoreSDK Then
-                sourceList = vbproj.newDotNetSDKProjectSource
+            If fullName Then
+                Return sourceList.Select(Function(rel) $"{sourceFolder}/{rel}".GetFullPath)
             Else
-                sourceList = vbproj.legacyProjectSource
+                Return sourceList
             End If
-
-            Return sourceList.vbfileFilter(sourceFolder, fullName, skipAssmInfo)
         End Function
 
         ''' <summary>
