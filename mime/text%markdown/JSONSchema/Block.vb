c405a7e2f69ed5d365c17b42a9d4cf243000bdd2
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-30 02:58:57 +0800

    create json schema for llm markdown response output

---
+++ mime/text%markdown/JSONSchema/Block.vb
@@ -0,0 +1,55 @@
+﻿Namespace JSONSchema
+
+    ''' <summary>
+    ''' 针对markdown格式有限的支持
+    ''' </summary>
+    Public Class Block
+
+        ''' <summary>
+        ''' table/heading/paragraph/code/list/blockquote
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property type As String
+        ''' <summary>
+        ''' heading level if type = heading
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property level As Integer
+        ''' <summary>
+        ''' the text content of heading/paragraph/code/blockquote
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property content As String
+        ''' <summary>
+        ''' the language code if type = code, example as bash/r/vbnet/c-sharp/python/php
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property language As String
+        ''' <summary>
+        ''' is ordered list if type = list
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property ordered As Boolean
+        ''' <summary>
+        ''' the list items for type = list
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property items As String()
+        ''' <summary>
+        ''' the table headers for type = table
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property headers As String()
+        ''' <summary>
+        ''' the table header alignments, value could be left|right|center
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property alignments As String()
+        ''' <summary>
+        ''' the table rows for type = table, each block elements inside this array should be list type, list items will be used as table row cells
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property rows As String()()
+
+    End Class
+End Namespace
---
+++ mime/text%markdown/JSONSchema/JSONRender.vb
@@ -0,0 +1,77 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.MIME.application.json
+Imports Microsoft.VisualBasic.MIME.application.json.Javascript
+Imports Microsoft.VisualBasic.MIME.application.json.LenientJson
+
+Namespace JSONSchema
+
+    ''' <summary>
+    ''' Markdown render helper for LLM outputs
+    ''' </summary>
+    Public Module JSONRender
+
+        Public Iterator Function Parse(jsonstr As String) As IEnumerable(Of Block)
+            Dim json As JsonArray = LenientJsonParser.ParseJSON(jsonstr)
+
+            For Each item As JsonObject In json
+                Dim type As String = item!type.AsString(False)
+
+                Select Case Strings.LCase(type)
+                    Case "table"
+                        Yield New Block With {
+                            .type = "table",
+                            .headers = item!headers.AsStringVector(True),
+                            .alignments = item!alignments.AsStringVector(False),
+                            .rows = DirectCast(item!rows, JsonArray) _
+                                .Select(Function(r) r.AsStringVector(True)) _
+                                .ToArray
+                        }
+                    Case "heading", "h"
+                        Yield New Block With {
+                            .type = "paragraph",
+                            .content = item!content.AsString(True),
+                            .level = item!level.AsString(True).ParseInteger
+                        }
+                    Case "paragraph", "p"
+                        Yield New Block With {
+                            .type = "paragraph",
+                            .content = item!content.AsString(True)
+                        }
+                    Case "code"
+                        Yield New Block With {
+                            .type = "code",
+                            .content = item!content.AsString(True),
+                            .language = item!language.AsString(True)
+                        }
+                    Case "list", "li"
+                        Yield New Block With {
+                            .type = "list",
+                            .ordered = item!ordered.AsString(False).ParseBoolean,
+                            .items = item!items.AsStringVector(True)
+                        }
+                    Case "blockquote"
+                        Yield New Block With {
+                            .type = "blockquote",
+                            .content = item!content.AsString(True)
+                        }
+                    Case Else
+                        ' 默认当作纯文本
+                        Yield New Block With {
+                            .type = "paragraph",
+                            .content = item!content.AsString(True)
+                        }
+                End Select
+            Next
+        End Function
+
+        <Extension>
+        Public Function ToMarkdown(docBlocks As IEnumerable(Of Block)) As String
+
+        End Function
+
+        Public Function ToHtml(docBlocks As IEnumerable(Of Block)) As String
+
+        End Function
+
+    End Module
+End Namespace
