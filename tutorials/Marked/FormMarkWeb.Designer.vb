ffb422de282eda277740f078d0e5c1c9f98d4e2e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-30 01:52:19 +0800

    edit and render

--- tutorials/Marked/FormMarkWeb.Designer.vb
+++ tutorials/Marked/FormMarkWeb.Designer.vb
@@ -26,8 +26,15 @@ Partial Class FormMarkWeb
         WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
         ToolStrip1 = New ToolStrip()
         ToolStripButton1 = New ToolStripButton()
+        TabControl1 = New TabControl()
+        TabPage1 = New TabPage()
+        TabPage2 = New TabPage()
+        TextBox1 = New TextBox()
         CType(WebView21, ComponentModel.ISupportInitialize).BeginInit()
         ToolStrip1.SuspendLayout()
+        TabControl1.SuspendLayout()
+        TabPage1.SuspendLayout()
+        TabPage2.SuspendLayout()
         SuspendLayout()
         ' 
         ' WebView21
@@ -36,9 +43,9 @@ Partial Class FormMarkWeb
         WebView21.CreationProperties = Nothing
         WebView21.DefaultBackgroundColor = Color.White
         WebView21.Dock = DockStyle.Fill
-        WebView21.Location = New Point(0, 25)
+        WebView21.Location = New Point(3, 3)
         WebView21.Name = "WebView21"
-        WebView21.Size = New Size(710, 548)
+        WebView21.Size = New Size(1447, 886)
         WebView21.TabIndex = 0
         WebView21.ZoomFactor = 1R
         ' 
@@ -47,7 +54,7 @@ Partial Class FormMarkWeb
         ToolStrip1.Items.AddRange(New ToolStripItem() {ToolStripButton1})
         ToolStrip1.Location = New Point(0, 0)
         ToolStrip1.Name = "ToolStrip1"
-        ToolStrip1.Size = New Size(710, 25)
+        ToolStrip1.Size = New Size(1461, 25)
         ToolStrip1.TabIndex = 1
         ToolStrip1.Text = "ToolStrip1"
         ' 
@@ -60,18 +67,67 @@ Partial Class FormMarkWeb
         ToolStripButton1.Size = New Size(23, 22)
         ToolStripButton1.Text = "打开"
         ' 
+        ' TabControl1
+        ' 
+        TabControl1.Controls.Add(TabPage1)
+        TabControl1.Controls.Add(TabPage2)
+        TabControl1.Dock = DockStyle.Fill
+        TabControl1.Location = New Point(0, 25)
+        TabControl1.Name = "TabControl1"
+        TabControl1.SelectedIndex = 0
+        TabControl1.Size = New Size(1461, 920)
+        TabControl1.TabIndex = 2
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.Controls.Add(WebView21)
+        TabPage1.Location = New Point(4, 24)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3)
+        TabPage1.Size = New Size(1453, 892)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "渲染结果"
+        TabPage1.UseVisualStyleBackColor = True
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.Controls.Add(TextBox1)
+        TabPage2.Location = New Point(4, 24)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3)
+        TabPage2.Size = New Size(1453, 892)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "Markdown原始输入"
+        TabPage2.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BorderStyle = BorderStyle.None
+        TextBox1.Dock = DockStyle.Fill
+        TextBox1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(3, 3)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.ScrollBars = ScrollBars.Both
+        TextBox1.Size = New Size(1447, 886)
+        TextBox1.TabIndex = 0
+        ' 
         ' FormMarkWeb
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(710, 573)
-        Controls.Add(WebView21)
+        ClientSize = New Size(1461, 945)
+        Controls.Add(TabControl1)
         Controls.Add(ToolStrip1)
         Name = "FormMarkWeb"
         Text = "Markdown文档查看"
         CType(WebView21, ComponentModel.ISupportInitialize).EndInit()
         ToolStrip1.ResumeLayout(False)
         ToolStrip1.PerformLayout()
+        TabControl1.ResumeLayout(False)
+        TabPage1.ResumeLayout(False)
+        TabPage2.ResumeLayout(False)
+        TabPage2.PerformLayout()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -79,5 +135,9 @@ Partial Class FormMarkWeb
     Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
     Friend WithEvents ToolStrip1 As ToolStrip
     Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TextBox1 As TextBox
 
 End Class
--- tutorials/Marked/FormMarkWeb.vb
+++ tutorials/Marked/FormMarkWeb.vb
@@ -7,12 +7,24 @@ Public Class FormMarkWeb
     Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
         Using file As New OpenFileDialog With {.Filter = "Markdown 文档(*.md)|*.md"}
             If file.ShowDialog = DialogResult.OK Then
-                Call WebViewLoader.NavigateToLargeString(WebView21, render.Transform(file.FileName.ReadAllText))
+                Dim md As String = file.FileName.ReadAllText
+                Dim html As String = render.Transform(md)
+
+                Call TextBox1.Clear()
+                Call TextBox1.AppendText(md)
+
+                Call WebViewLoader.NavigateToLargeString(WebView21, html)
             End If
         End Using
     End Sub
 
     Private Async Sub FormMarkWeb_Load(sender As Object, e As EventArgs) Handles Me.Load
         Await WebViewLoader.Init(WebView21)
+    End Sub
+
+    Private Sub TabControl1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles TabControl1.SelectedIndexChanged
+        If TabControl1.SelectedTab Is TabPage1 Then
+            Call WebViewLoader.NavigateToLargeString(WebView21, render.Transform(TextBox1.Text))
+        End If
     End Sub
 End Class
