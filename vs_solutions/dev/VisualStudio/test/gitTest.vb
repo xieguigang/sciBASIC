58509a639e4ef73823897653dbe0214de9b5c332
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-31 01:55:35 +0800

    test diff on specifci workspace success

--- vs_solutions/dev/VisualStudio/test/gitTest.vb
+++ vs_solutions/dev/VisualStudio/test/gitTest.vb
@@ -3,7 +3,7 @@
 Public Module gitTest
 
     Sub Run()
-        Dim diff = Git.diff.GetDiff("G:\GCModeller\src\runtime\sciBASIC#\vs_solutions\dev\VisualStudio\test")
+        Dim diff = Git.diff.GetDiff("G:\GCModeller\src\runtime\sciBASIC#\vs_solutions\dev\VisualStudio")
 
         Pause()
     End Sub
