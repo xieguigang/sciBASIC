dbfeff6f16bc055928f8d06b78c3676110f2b437
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-31 02:36:28 +0800

    dump git diff for demo

--- vs_solutions/dev/VisualStudio/test/gitTest.vb
+++ vs_solutions/dev/VisualStudio/test/gitTest.vb
@@ -1,9 +1,13 @@
 ﻿Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio.VersionControl
+Imports Microsoft.VisualBasic.Serialization.JSON
 
 Public Module gitTest
 
     Sub Run()
-        Dim diff = Git.diff.GetDiff("G:\GCModeller\src\runtime\sciBASIC#\vs_solutions\dev\VisualStudio")
+        Dim diff = Git.diff.GetDiff("G:\GCModeller\src\runtime\sciBASIC#\vs_solutions\dev")
+
+        Call Console.WriteLine(diff.GetJson)
+        Call diff.GetJson.SaveTo("G:\GCModeller\src\runtime\sciBASIC#\vs_solutions\dev\data\git_diff.json")
 
         Pause()
     End Sub
