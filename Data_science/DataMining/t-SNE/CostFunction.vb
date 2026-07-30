7017eadf644e03ac121a902abce792db164faf80
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-31 06:39:30 +0800

    movbe t-sne source file into dataminging folder

--- Data_science/DataMining/t-SNE/CostFunction.vb
+++ Data_science/DataMining/t-SNE/CostFunction.vb
@@ -1,60 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::b14575111acb769e54ce30377d1c624d, Data_science\MachineLearning\t-SNE\CostFunction.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 88
-    '    Code Lines: 59 (67.05%)
-    ' Comment Lines: 8 (9.09%)
-    '    - Xml Docs: 50.00%
-    ' 
-    '   Blank Lines: 21 (23.86%)
-    '     File Size: 2.37 KB
+' Summaries:
 
 
-    ' Class CostFunction
-    ' 
-    '     Properties: mN
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Sub: CostGrad
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 88
+'    Code Lines: 59 (67.05%)
+' Comment Lines: 8 (9.09%)
+'    - Xml Docs: 50.00%
+' 
+'   Blank Lines: 21 (23.86%)
+'     File Size: 2.37 KB
+
+
+' Class CostFunction
+' 
+'     Properties: mN
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Sub: CostGrad
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports stdNum = System.Math
+Imports std = System.Math
 
 Friend Class CostFunction
 
@@ -107,7 +107,7 @@ Friend Class CostFunction
         Dim lQ = zeros(NN)
 
         For q = 0 To NN - 1
-            lQ(q) = stdNum.Max(lQu(q) / qsum, 1.0E-100)
+            lQ(q) = std.Max(lQu(q) / qsum, 1.0E-100)
         Next
 
         Dim cost = 0.0
@@ -122,7 +122,7 @@ Friend Class CostFunction
 
             For j = 0 To N - 1
                 ' accumulate cost (the non-constant portion at least...)
-                cost += -P(i * N + j) * stdNum.Log(lQ(i * N + j))
+                cost += -P(i * N + j) * std.Log(lQ(i * N + j))
                 Dim premult = 4 * (pmul * P(i * N + j) - lQ(i * N + j)) * lQu(i * N + j)
 
                 For d = 0 To [dim] - 1
