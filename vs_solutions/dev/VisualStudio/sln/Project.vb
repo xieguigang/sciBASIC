d80f9f70760a7d0502bd47fb94f5aec4bbf69134
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-02 01:09:41 +0800

    Move Project model out and add Solution.Load helper
    本次改动对 `Solution.vb`进行了重构，简化了解决方案模型文件，并新增了便捷的加载入口。
    
    具体变更如下：
    
    -在 `Solution`类中新增静态方法 `Load(sln As String)`，内部委托给 `Parser.Parse`完成解决方案文件的解析，为调用方提供更简洁的入口。
    -将 `TypeId`枚举与 `Project`类从 `Solution.vb`中移除（已迁移至其他文件），使该文件只保留 `Solution`核心模型；相应地删除了不再需要的 `System.ComponentModel`命名空间引用。
    -对文件顶部的 GPL许可证头注释做了缩进格式化调整，不涉及逻辑变化。
    
    动机与影响：将项目类型枚举与项目节点模型从解决方案模型文件中拆分出去，职责划分更加清晰，有利于后续在 Parser等模块中直接复用 `Project`/`TypeId`；同时 `Solution.Load`为外部调用方提供了语义明确的加载入口，简化了使用方式。

---
+++ vs_solutions/dev/VisualStudio/sln/Project.vb
@@ -0,0 +1,68 @@
+﻿Imports System.ComponentModel
+
+Namespace sln
+
+    ''' <summary>
+    ''' Known Visual Studio project type GUIDs.
+    ''' </summary>
+    Public Enum TypeId
+        <Description("2150E333-8FDC-42A3-9474-1A3956D46DE8")> FolderGroup
+        <Description("F184B08F-C81C-45F6-A57F-5ABD9991F28F")> VBProject
+        <Description("9092AA53-FB77-4645-B42D-1CCCA6BD08BD")> NjsProject
+        <Description("FAE04EC0-301F-11D3-BF4B-00C04F79EFBC")> CSharpProject
+        <Description("8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942")> CppProject
+        <Description("F135691A-BF7E-435D-8960-F99683D2D49C")> WebApplication
+        <Description("E24C65DC-7377-472B-9ABA-BC803B73C61A")> WebSite
+        <Description("3AC096D0-A1C2-E12C-1390-A8335801FDAB")> TestProject
+        <Description("VC60C7D7-84AE-47E4-8DED-D1B4E0554DBB")> SolutionFolderAlt
+        <Description("Unknown")> Unknown
+    End Enum
+
+    ''' <summary>
+    ''' A project or solution folder inside the solution.
+    ''' </summary>
+    Public Class Project
+        ''' <summary>
+        ''' The project type, derived from the project type GUID.
+        ''' </summary>
+        Public Property NodeType As TypeId
+        ''' <summary>
+        ''' The project type GUID as it appears in the solution file.
+        ''' </summary>
+        Public Property TypeGuid As String
+        ''' <summary>
+        ''' The project GUID.
+        ''' </summary>
+        Public Property Guid As String
+        ''' <summary>
+        ''' The node display name.
+        ''' </summary>
+        Public Property Name As String
+        ''' <summary>
+        ''' Includes virtual solution folder and project file path.
+        ''' </summary>
+        Public Property TreePath As String
+        ''' <summary>
+        ''' Relative path of the project file (e.g. a ``.vbproj``) to the solution file.
+        ''' Empty for solution folders.
+        ''' </summary>
+        Public Property RelativePath As String
+        ''' <summary>
+        ''' Resolved full path of the project file. Empty for solution folders.
+        ''' </summary>
+        Public Property FullPath As String
+        ''' <summary>
+        ''' The GUID of the parent solution folder, or empty for top-level nodes.
+        ''' </summary>
+        Public Property ParentGuid As String
+
+        ''' <summary>
+        ''' True when this node is a solution folder (no physical project file).
+        ''' </summary>
+        Public ReadOnly Property IsFolder As Boolean
+            Get
+                Return NodeType = TypeId.FolderGroup OrElse NodeType = TypeId.SolutionFolderAlt
+            End Get
+        End Property
+    End Class
+End Namespace
--- vs_solutions/dev/VisualStudio/sln/Solution.vb
+++ vs_solutions/dev/VisualStudio/sln/Solution.vb
@@ -1,69 +1,67 @@
 ﻿#Region "Microsoft.VisualBasic::0110500a1a13881ab3099e5d683e5f1d, vs_solutions\dev\VisualStudio\sln\Solution.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 37
-    '    Code Lines: 20 (54.05%)
-    ' Comment Lines: 11 (29.73%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 6 (16.22%)
-    '     File Size: 1.14 KB
+' Summaries:
 
 
-    '     Class Solution
-    ' 
-    '         Properties: FormatVersion, MinimumVisualStudioVersion, Projects, VisualStudioVersion
-    ' 
-    '     Enum TypeId
-    ' 
-    ' 
-    '  
-    ' 
-    ' 
-    ' 
-    '     Class Project
-    ' 
-    '         Properties: Guid, Name, NodeType, TreePath
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 37
+'    Code Lines: 20 (54.05%)
+' Comment Lines: 11 (29.73%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 6 (16.22%)
+'     File Size: 1.14 KB
+
+
+'     Class Solution
+' 
+'         Properties: FormatVersion, MinimumVisualStudioVersion, Projects, VisualStudioVersion
+' 
+'     Enum TypeId
+' 
+' 
+'  
+' 
+' 
+' 
+'     Class Project
+' 
+'         Properties: Guid, Name, NodeType, TreePath
+' 
+' 
+' /********************************************************************************/
 
 #End Region
-
-Imports System.ComponentModel
 
 Namespace sln
 
@@ -120,6 +118,10 @@ Namespace sln
 
             Return IO.Path.GetFullPath(IO.Path.Combine(IO.Path.GetDirectoryName(FilePath), p.RelativePath))
         End Function
+
+        Public Shared Function Load(sln As String) As Solution
+            Return Parser.Parse(path:=sln)
+        End Function
     End Class
 
     ''' <summary>
@@ -156,67 +158,5 @@ Namespace sln
         End Sub
     End Class
 
-    ''' <summary>
-    ''' Known Visual Studio project type GUIDs.
-    ''' </summary>
-    Public Enum TypeId
-        <Description("2150E333-8FDC-42A3-9474-1A3956D46DE8")> FolderGroup
-        <Description("F184B08F-C81C-45F6-A57F-5ABD9991F28F")> VBProject
-        <Description("9092AA53-FB77-4645-B42D-1CCCA6BD08BD")> NjsProject
-        <Description("FAE04EC0-301F-11D3-BF4B-00C04F79EFBC")> CSharpProject
-        <Description("8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942")> CppProject
-        <Description("F135691A-BF7E-435D-8960-F99683D2D49C")> WebApplication
-        <Description("E24C65DC-7377-472B-9ABA-BC803B73C61A")> WebSite
-        <Description("3AC096D0-A1C2-E12C-1390-A8335801FDAB")> TestProject
-        <Description("VC60C7D7-84AE-47E4-8DED-D1B4E0554DBB")> SolutionFolderAlt
-        <Description("Unknown")> Unknown
-    End Enum
 
-    ''' <summary>
-    ''' A project or solution folder inside the solution.
-    ''' </summary>
-    Public Class Project
-        ''' <summary>
-        ''' The project type, derived from the project type GUID.
-        ''' </summary>
-        Public Property NodeType As TypeId
-        ''' <summary>
-        ''' The project type GUID as it appears in the solution file.
-        ''' </summary>
-        Public Property TypeGuid As String
-        ''' <summary>
-        ''' The project GUID.
-        ''' </summary>
-        Public Property Guid As String
-        ''' <summary>
-        ''' The node display name.
-        ''' </summary>
-        Public Property Name As String
-        ''' <summary>
-        ''' Includes virtual solution folder and project file path.
-        ''' </summary>
-        Public Property TreePath As String
-        ''' <summary>
-        ''' Relative path of the project file (e.g. a ``.vbproj``) to the solution file.
-        ''' Empty for solution folders.
-        ''' </summary>
-        Public Property RelativePath As String
-        ''' <summary>
-        ''' Resolved full path of the project file. Empty for solution folders.
-        ''' </summary>
-        Public Property FullPath As String
-        ''' <summary>
-        ''' The GUID of the parent solution folder, or empty for top-level nodes.
-        ''' </summary>
-        Public Property ParentGuid As String
-
-        ''' <summary>
-        ''' True when this node is a solution folder (no physical project file).
-        ''' </summary>
-        Public ReadOnly Property IsFolder As Boolean
-            Get
-                Return NodeType = TypeId.FolderGroup OrElse NodeType = TypeId.SolutionFolderAlt
-            End Get
-        End Property
-    End Class
 End Namespace
