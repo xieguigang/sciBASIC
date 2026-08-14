b1aca9601029566ab478b505d7805f8794731799
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-14 16:14:35 +0800

    add spatial expression analusis algorithm module code

--- Data/BinaryData/HDF5/HDF5File.vb
+++ Data/BinaryData/HDF5/HDF5File.vb
@@ -59,7 +59,6 @@
 
 Imports System.IO
 Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Data.IO.HDF5.device
 Imports Microsoft.VisualBasic.Data.IO.HDF5.struct
 Imports Microsoft.VisualBasic.Data.IO.HDF5.struct.messages
