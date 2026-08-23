c8ae41d00edba99cb2df8d1a173d2cd1d3d57a70
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-23 16:58:07 +0800

    read/write of the binary data matrix file

---
+++ Data_science/Mathematica/Math/DataFrame/MatrixMarket/MatrixFormat.vb
@@ -0,0 +1,38 @@
+﻿Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Serialization
+Imports Microsoft.VisualBasic.Serialization.BinaryDumping
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Public Module MatrixFormat
+
+    ReadOnly netorder As New NetworkByteOrderBuffer
+
+    <Extension>
+    Public Function WriteData(m As DataMatrix, file As Stream) As Boolean
+        Using wr As New BinaryWriter(file)
+            Call wr.Write(m.GetLabels.GetJson)
+
+            For Each row As Double() In m.matrix
+                Call wr.Write(netorder.GetBytes(row))
+            Next
+        End Using
+
+        Return True
+    End Function
+
+    Public Function ReadData(file As Stream) As DataMatrix
+        Using rd As New BinaryReader(file)
+            Dim labels As String() = rd.ReadString.LoadJSON(Of String())
+            Dim mat As Double()() = New Double(labels.Length - 1)() {}
+            Dim width As Integer = labels.Length * RawStream.DblFloat
+
+            For i As Integer = 0 To labels.Length - 1
+                mat(i) = netorder.ParseDouble(rd.ReadBytes(width))
+            Next
+
+            Return New DataMatrix(labels, mat)
+        End Using
+    End Function
+
+End Module
