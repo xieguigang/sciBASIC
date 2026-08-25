054b9841383b9fee59da7e910fd4277057dd5da4
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-25 17:52:12 +0800

    split source file into multiple parts

---
+++ gr/network-visualization/Network.IO.Extensions/Gephi/node.vb
@@ -0,0 +1,10 @@
+﻿Namespace Gephi
+
+    Public Class node
+
+        Public Property id As Integer
+        Public Property label As String
+        Public Property graphics As graphics
+
+    End Class
+End Namespace
