183e7aa41e8c8cf7400295a92409610a9c105464
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-27 05:53:20 +0800

    fix of the namespace reference errpr

--- mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel/XLS/MsHtml.vb
+++ mime/application%vnd.openxmlformats-officedocument.spreadsheetml.sheet/Excel/XLS/MsHtml.vb
@@ -1,61 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::d6c0e0874eb971f7235080c232ba4f9c, mime\application%vnd.openxmlformats-officedocument.spreadsheetml.sheet\Excel\XLS\MsHtml.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 56
-    '    Code Lines: 42 (75.00%)
-    ' Comment Lines: 6 (10.71%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 8 (14.29%)
-    '     File Size: 2.15 KB
+' Summaries:
 
 
-    '     Module MsHtml
-    ' 
-    '         Function: (+2 Overloads) ToExcel
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 56
+'    Code Lines: 42 (75.00%)
+' Comment Lines: 6 (10.71%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 8 (14.29%)
+'     File Size: 2.15 KB
+
+
+'     Module MsHtml
+' 
+'         Function: (+2 Overloads) ToExcel
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports System.Text
 Imports Microsoft.VisualBasic.Data.Framework
-Imports Microsoft.VisualBasic.Data.Framework.DATA
+Imports Microsoft.VisualBasic.Data.Framework.DataSetExtensions
 Imports Table = Microsoft.VisualBasic.Data.Framework.IO.File
 
 Namespace XLS
