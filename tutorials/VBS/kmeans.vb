a8932a35bb16db6fd747e7e5326dd4be39359017
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-08 17:50:15 +0800

    create cmeans demo

---
+++ tutorials/VBS/kmeans.vb
@@ -0,0 +1,5 @@
+#imports "G:\GCModeller\src\runtime\sciBASIC#\.nuget\net10.0\Microsoft.VisualBasic.DataMining.Framework.dll"
+
+dim file = "G:\GCModeller\src\R-sharp\REnv\data\bezdekIris.csv"
+dim k = 3
+
