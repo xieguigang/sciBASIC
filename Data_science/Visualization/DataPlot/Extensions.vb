7008c8089c13fdb399f87db307656303724b14c3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-08 22:40:09 +0800

    Add DataSerials helper to build grouped scatter series
    本次改动为 DataPlot绘图库新增一个「按类别分组构造散点序列」的辅助扩展，并把 KMeans教程从“聚类+PCA后只存表”升级为“聚类→PCA投影→按簇分类着色的散点图”的完整可视化示例。
    
    1.Data_science/Visualization/DataPlot/Extensions.vb：新增 `DataSerials(x As Double(), y As Double(), class_id As String())`迭代器扩展，返回 `IEnumerable(Of Series)`。其实现先将 x/y/class_id按索引三元组打包，再按类别 id分组；对每一组生成一个 Series，颜色取自 `Designer.GetColors("paper")`调色板并按组递增取用，线型为点线、标记为圆形、名称取类别 id，并把组内 x、y分别填成数组。同时该文件补齐所需导入：System.Drawing、Microsoft.VisualBasic.ComponentModel.DataStructures（LoopArray）、Microsoft.VisualBasic.Imaging.Drawing2D.Colors（Designer），头部版权注释统一去缩进排版。
    
    2.tutorials/VBS/kmeans.vb：脚本新增 `microsoft.visualbasic.datamining`与 `microsoft.visualbasic.data.plots`命名空间导入，移除原先对 PC1/PC2的局部变量写法，改为直接以 `pca!PC1`、`pca!PC2`作为 x/y调用 `DataSerials(...)`，得到按 `result.ClassId()`类别分组的序列列表后传入 ScatterPlot绘图：标题 “PCA group of bezdek-Iris”，X/Y轴分别为 PC1/PC2，并以300dpi输出 PNG至 Z:/bezdekIris-pca-groups.png。
    
    整体影响：DataPlot因此获得“数值坐标+类别标签 →多序列”的标准拼接入口，上层无需再手工构造 Series；教程展示了用 PCA降维后按 KMeans类别分组着色的完整数据可视化闭环。

--- Data_science/Visualization/DataPlot/Extensions.vb
+++ Data_science/Visualization/DataPlot/Extensions.vb
@@ -1,58 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::c2e7f9f520c52023adffa1c611e82d86, Data_science\Visualization\DataPlot\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 14
-    '    Code Lines: 11 (78.57%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 3 (21.43%)
-    '     File Size: 449 B
+' Summaries:
 
 
-    ' Module Extensions
-    ' 
-    '     Sub: SavePng
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 14
+'    Code Lines: 11 (78.57%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 3 (21.43%)
+'     File Size: 449 B
+
+
+' Module Extensions
+' 
+'     Sub: SavePng
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Drawing
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataStructures
 Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
 Imports Microsoft.VisualBasic.Imaging.Driver
 
 Public Module Extensions
@@ -64,4 +67,21 @@ Public Module Extensions
 
         Call res.SaveAs(filepath)
     End Sub
+
+    Public Iterator Function DataSerials(x As Double(), y As Double(), class_id As String()) As IEnumerable(Of Series)
+        Dim groups = class_id.Select(Function(cid, i) (x(i), y(i), cid)).GroupBy(Function(a) a.cid)
+        Dim colors As LoopArray(Of Color) = Designer.GetColors("paper")
+
+        For Each serial_group In groups
+            Yield New Series With {
+                .Color = ++colors,
+                .LineStyle = DashStyle.Dot,
+                .MarkerShape = MarkerShape.Circle,
+                .Name = serial_group.Key,
+                .Visible = True,
+                .X = serial_group.Select(Function(a) a.Item1).ToArray,
+                .Y = serial_group.Select(Function(a) a.Item2).ToArray
+            }
+        Next
+    End Function
 End Module
--- tutorials/VBS/kmeans.vb
+++ tutorials/VBS/kmeans.vb
@@ -5,8 +5,10 @@
 
 imports microsoft.visualbasic.data.framework.storageprovider
 imports microsoft.visualbasic.datamining.kmeans
+imports microsoft.visualbasic.datamining
 imports microsoft.visualbasic.data.framework
 imports Microsoft.VisualBasic.Math.Statistics.Hypothesis.ANOVA
+imports microsoft.visualbasic.data.plots
 
 dim file = "G:\GCModeller\src\R-sharp\REnv\data\bezdekIris.csv"
 dim k = 3
@@ -14,8 +16,15 @@ dim k = 3
 dim dataset = DataFrameResolver.LoadDataSet(file, cols := {"D1","D2","D3","D4"}).ToKMeansModels()
 dim result = dataset.kmeans( expected := k).toarray()
 dim pca = result.CommonDataSet.PrincipalComponentAnalysis(maxPC := 2).GetPCAScore()
-dim x = pca!PC1
-dim y = pca!PC2
-dim class_id = result.ClassId.ToArray()
+dim class_id = result.ClassId().ToArray()
+
+Using plt As New ScatterPlot(800, 600, PlotTheme.Nature())
+    plt.Title = "PCA group of bezdek-Iris"
+    plt.SubTitle = "Two groups with linear trend"
+    plt.XLabel = "PC1"
+    plt.YLabel = "PC2"
+    plt.Plot(DataSerials(x:=pca!PC1,y:=pca!PC2, class_id).tolist())
+    plt.SavePng("Z:/bezdekIris-pca-groups.png", 300)
+End Using
 
 call result.saveto("Z:/test.csv")
