b7f51f8ff9ef92b31f946aa742dc8ad9bccec39b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-09 16:29:49 +0800

    Update word2vector.vb

--- tutorials/VBS/word2vector.vb
+++ tutorials/VBS/word2vector.vb
@@ -1,22 +1,25 @@
 #include "Microsoft.VisualBasic.Data.NLP.Word2Vec.dll"
 #include "Microsoft.VisualBasic.Data.NLP.dll"
 #include "Microsoft.VisualBasic.DataMining.Framework.dll"
-#include "Microsoft.VisualBasic.Math.Statistics.ANOVA.dll"
 #include "Microsoft.VisualBasic.Drawing.dll"
 #include "Microsoft.VisualBasic.Data.DataPlot.dll"
 #include "Microsoft.VisualBasic.Data.Framework.dll"
+#include "Microsoft.VisualBasic.Math.Randomizer.dll"
+#include "Microsoft.VisualBasic.DataMining.UMAP.dll"
 
 Imports Microsoft.VisualBasic.Data.NLP.Word2Vec
 Imports Microsoft.VisualBasic.Data.NLP.Model
 Imports Microsoft.VisualBasic.DataMining
 Imports Microsoft.VisualBasic.DataMining.Kmeans
-Imports Microsoft.VisualBasic.Math.Statistics.Hypothesis.ANOVA
 imports microsoft.visualbasic.data.plots
 imports microsoft.visualbasic.drawing
 Imports Microsoft.VisualBasic.Data.Framework
+imports Microsoft.VisualBasic.linq
+imports Microsoft.VisualBasic.scripting.runtime
+imports Microsoft.VisualBasic.DataMining.UMAP
 
 dim textfile = "G:\GCModeller\src\runtime\sciBASIC#\Data\TextRank\Rapunzel.txt"
-dim wv As Word2Vec = Word2VecFactory().setVectorSize(100).setMethod(TrainMethod.Skip_Gram).setNumOfThread(1).setFreqThresold(1).build()
+dim wv As Word2Vec = BuildWord2VecFactory().setVectorSize(30).setMethod(TrainMethod.CBow).setNumOfThread(1).setFreqThresold(1).build()
 Dim data As Paragraph() = Paragraph.Segmentation(textFile.ReadAllText).ToArray
 
 For Each p As Paragraph In data
@@ -27,21 +30,27 @@ Next
 
 call wv.training()
 
-dim result = wv.outputVector.AsEnumerable().AsDataSet().CommonDataSet().PrincipalComponentAnalysis(maxPC:= 20).GetPCAScore()
-dim clusters = result.Kmeans(k:=6).toarray()
+dim vector = wv.outputVector()
+dim umap as new umap(dimensions := 9,numberOfNeighbors := 64 )
+dim n_epochs = umap.InitializeFit( vector.AsEnumerable().asdataset().toarray())
+
+Call umap.Step(n_epochs)
+
+dim clusters = umap.AsDataSet(labels:=vector.tokens).Kmeans(k:=9).toarray()
 dim class_id = clusters.ClassId().ascharacter().toarray()
 dim x = clusters.feature(offset:= 0).toarray()
 dim y = clusters.feature(offset:= 1).toarray()
+dim result = clusters.as_dataframe(colnames:= fieldname("v", n:=vector.words).toarray())
 
-' scatter plot with PC1 and PC2
+' scatter plot with UMAP1 and UMAP2
 call SkiaDriver.Register()
 call result.add("class_id", class_id)
 
 Using plt As New ScatterPlot(800, 600, PlotTheme.Nature())
-    plt.Title = "PCA group of Rapunzel"
-    plt.SubTitle = "PCA score scatter of the 'Rapunzel' word2vector embedding result"
-    plt.XLabel = "PC1"
-    plt.YLabel = "PC2"
+    plt.Title = "UMAP group of Rapunzel"
+    plt.SubTitle = "UMAP scatter of the 'Rapunzel' word2vector embedding result"
+    plt.XLabel = "UMAP1"
+    plt.YLabel = "UMAP2"
     plt.Plot(DataSerials(x,y, class_id).tolist())
     plt.SavePng("Z:/rapunzel-pca-groups.png", 300)
 End Using
