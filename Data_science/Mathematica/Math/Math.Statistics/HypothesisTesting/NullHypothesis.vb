dfd1f4942e9e3f48317fa8d3e685e08fd34552e0
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-09 17:58:59 +0800

    make code style improvements

--- Data_science/Mathematica/Math/Math.Statistics/HypothesisTesting/NullHypothesis.vb
+++ Data_science/Mathematica/Math/Math.Statistics/HypothesisTesting/NullHypothesis.vb
@@ -82,28 +82,28 @@ Namespace Hypothesis
                 Case Hypothesis.Greater
                     ' mu > mu0
                     n = Aggregate x As T
-                    In zero.AsParallel
-                    Let per_score As Double = Me.Score(x)
-                    Where per_score >= score
-                    Into Count
+                        In zero.AsParallel
+                        Let per_score As Double = Me.Score(x)
+                        Where per_score >= score
+                        Into Count
                 Case Hypothesis.Less
                     ' mu < mu0
                     n = Aggregate x As T
-                    In zero.AsParallel
-                    Let per_score As Double = Me.Score(x)
-                    Where per_score <= score
-                    Into Count
+                        In zero.AsParallel
+                        Let per_score As Double = Me.Score(x)
+                        Where per_score <= score
+                        Into Count
                 Case Hypothesis.TwoSided
                     ' 计算观测统计量的绝对值，用于双侧检验判断极端性
                     Dim abs_score As Double = std.Abs(score)
 
                     ' 双侧检验：置换统计量的绝对值 >= 观测统计量的绝对值
                     n = Aggregate x As T
-                    In zero.AsParallel
-                    Let per_score As Double = Me.Score(x)
-                    Let abs_per_score As Double = std.Abs(per_score)
-                    Where abs_per_score >= abs_score
-                    Into Count
+                        In zero.AsParallel
+                        Let per_score As Double = Me.Score(x)
+                        Let abs_per_score As Double = std.Abs(per_score)
+                        Where abs_per_score >= abs_score
+                        Into Count
                 Case Else
                     Throw New InvalidProgramException($"unknown alternative hypothesis: {alternative}!")
             End Select
--- nlp/KnowledgeGraph/SimilarityMetrics.vb
+++ nlp/KnowledgeGraph/SimilarityMetrics.vb
@@ -1,59 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::84fc4beb32df7a08630b1764a02325e5, nlp\KnowledgeGraph\SimilarityMetrics.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 200
-    '    Code Lines: 104 (52.00%)
-    ' Comment Lines: 66 (33.00%)
-    '    - Xml Docs: 51.52%
-    ' 
-    '   Blank Lines: 30 (15.00%)
-    '     File Size: 8.07 KB
+' Summaries:
 
 
-    ' Class SimilarityResult
-    ' 
-    '     Properties: AdamicAdar, CommonNeighbors, CosineSimilarity, EntityAId, EntityBId
-    '                 InclusionAB, InclusionBA, Jaccard, MaxInclusion, SharedAttributeIds
-    ' 
-    ' Module SimilarityMetrics
-    ' 
-    '     Function: AdamicAdarIndex, CommonNeighborsCount, ComputeAllPairs, ComputeSimilarity, CosineSim
-    '               InclusionRatio, JaccardSimilarity
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 200
+'    Code Lines: 104 (52.00%)
+' Comment Lines: 66 (33.00%)
+'    - Xml Docs: 51.52%
+' 
+'   Blank Lines: 30 (15.00%)
+'     File Size: 8.07 KB
+
+
+' Class SimilarityResult
+' 
+'     Properties: AdamicAdar, CommonNeighbors, CosineSimilarity, EntityAId, EntityBId
+'                 InclusionAB, InclusionBA, Jaccard, MaxInclusion, SharedAttributeIds
+' 
+' Module SimilarityMetrics
+' 
+'     Function: AdamicAdarIndex, CommonNeighborsCount, ComputeAllPairs, ComputeSimilarity, CosineSim
+'               InclusionRatio, JaccardSimilarity
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -81,7 +81,6 @@
 '    补偿了实体度数差异的影响。
 ' ============================================================================
 
-Imports System.Collections.Generic
 Imports std = System.Math
 
 ''' <summary>
