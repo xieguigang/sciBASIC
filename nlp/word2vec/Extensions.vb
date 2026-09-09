7ba97080bf09cc62ed23f494851c3895e5815f6d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-09 15:31:34 +0800

    create word2vec result dataset

---
+++ nlp/word2vec/Extensions.vb
@@ -0,0 +1,7 @@
+﻿<HideModuleName>
+Public Module Extensions
+
+    Public Function Word2VecFactory() As Word2VecFactory
+        Return New Word2VecFactory
+    End Function
+End Module
--- tutorials/VBS/word2vector.vb
+++ tutorials/VBS/word2vector.vb
@@ -1,11 +1,13 @@
 #include "Microsoft.VisualBasic.Data.NLP.Word2Vec.dll"
 #include "Microsoft.VisualBasic.Data.NLP.dll"
+#include "Microsoft.VisualBasic.DataMining.Framework.dll"
 
 Imports Microsoft.VisualBasic.Data.NLP.Word2Vec
 Imports Microsoft.VisualBasic.Data.NLP.Model
+Imports Microsoft.VisualBasic.DataMining
 
 dim textfile = "G:\GCModeller\src\runtime\sciBASIC#\Data\TextRank\Rapunzel.txt"
-dim wv As Word2Vec = (New Word2VecFactory()).setVectorSize(100).setMethod(TrainMethod.Skip_Gram).setNumOfThread(1).setFreqThresold(1).build()
+dim wv As Word2Vec = Word2VecFactory().setVectorSize(100).setMethod(TrainMethod.Skip_Gram).setNumOfThread(1).setFreqThresold(1).build()
 Dim data As Paragraph() = Paragraph.Segmentation(textFile.ReadAllText).ToArray
 
 For Each p As Paragraph In data
@@ -16,4 +18,4 @@ Next
 
 call wv.training()
 
-dim result = wv.outputVector
+dim result = wv.outputVector.AsEnumerable().AsDataSet()
