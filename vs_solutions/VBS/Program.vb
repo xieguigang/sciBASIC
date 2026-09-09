71c77bf923cdcf478c1ef5fdb5908601654523ed
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-09 16:47:30 +0800

    add verbose debug options

--- vs_solutions/VBS/Program.vb
+++ vs_solutions/VBS/Program.vb
@@ -1,3 +1,4 @@
+Imports Microsoft.VisualBasic.CommandLine
 Imports VBScriptHost.Script
 
 Module Program
@@ -7,10 +8,12 @@ Module Program
     ''' </summary>
     ''' <param name="args"></param>
     Public Function Main(args As String()) As Integer
+        Dim cmdl As CommandLine = CommandLine.BuildFromArguments(args, NoSubCommand:=False)
         Dim scriptFile As String = args(0)
-        Dim vbs As ScriptParseResult = VBScript.ParseScript(scriptFile)
+        Dim verbose As Boolean = cmdl("--verbose")
+        Dim vbs As ScriptParseResult = VBScript.ParseScript(scriptFile, verbose:=verbose)
 
-        Using script As ScriptRuntime = vbs.CompileScript
+        Using script As ScriptRuntime = vbs.CompileScript(debug:=verbose)
             Return script.Run(args)
         End Using
     End Function
