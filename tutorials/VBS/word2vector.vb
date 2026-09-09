24a758842e2b73a429c626d22a8cabf952660f05
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-09 15:43:54 +0800

    Update word2vector.vb

--- tutorials/VBS/word2vector.vb
+++ tutorials/VBS/word2vector.vb
@@ -1,10 +1,13 @@
 #include "Microsoft.VisualBasic.Data.NLP.Word2Vec.dll"
 #include "Microsoft.VisualBasic.Data.NLP.dll"
 #include "Microsoft.VisualBasic.DataMining.Framework.dll"
+#include "Microsoft.VisualBasic.Math.Statistics.ANOVA.dll"
 
 Imports Microsoft.VisualBasic.Data.NLP.Word2Vec
 Imports Microsoft.VisualBasic.Data.NLP.Model
 Imports Microsoft.VisualBasic.DataMining
+Imports Microsoft.VisualBasic.DataMining.Kmeans
+Imports Microsoft.VisualBasic.Math.Statistics.Hypothesis.ANOVA
 
 dim textfile = "G:\GCModeller\src\runtime\sciBASIC#\Data\TextRank\Rapunzel.txt"
 dim wv As Word2Vec = Word2VecFactory().setVectorSize(100).setMethod(TrainMethod.Skip_Gram).setNumOfThread(1).setFreqThresold(1).build()
@@ -18,4 +21,5 @@ Next
 
 call wv.training()
 
-dim result = wv.outputVector.AsEnumerable().AsDataSet()
+dim result = wv.outputVector.AsEnumerable().AsDataSet().CommonDataSet().PrincipalComponentAnalysis(maxPC:= 20).GetPCAScore()
+dim clusters = result.Kmeans(k:=6).toarray()
