227e80e42cdf054aaed28887dc7998600ad9acb3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-11 17:25:20 +0800

    sqlite3 writer

---
+++ Data/BinaryData/SQLite3/Writer/Sqlite3TableWriter.vb
@@ -0,0 +1,331 @@
+Imports System.Collections.Generic
+Imports System.Linq
+
+Namespace Writer
+
+    ''' <summary>
+    ''' 单张数据表的可写模型, 提供链式的行级增删改查(CRUD)。所有修改在 <see cref="Sqlite3Writer.Commit"/> 时落盘。
+    ''' </summary>
+    Public Class Sqlite3TableWriter
+
+        Private ReadOnly _writer As Sqlite3Writer
+        Private ReadOnly _columns As New List(Of Sqlite3Column)()
+        Private ReadOnly _rows As New SortedDictionary(Of Long, Object())()
+
+        Private _originalSql As String
+        Private _nextRowId As Long = 1
+
+        Friend Sub New(writer As Sqlite3Writer, name As String, columns As IEnumerable(Of Sqlite3Column))
+            Me._writer = writer
+            Me.TableName = name
+
+            For Each c As Sqlite3Column In columns
+                _columns.Add(c)
+            Next
+        End Sub
+
+        ''' <summary>表名</summary>
+        Public ReadOnly Property TableName As String
+
+        ''' <summary>列定义</summary>
+        Public ReadOnly Property Columns As Sqlite3Column()
+            Get
+                Return _columns.ToArray
+            End Get
+        End Property
+
+        ''' <summary>当前内存之中的数据行数</summary>
+        Public ReadOnly Property RowCount As Integer
+            Get
+                Return _rows.Count
+            End Get
+        End Property
+
+        ''' <summary>rowid 别名列(INTEGER PRIMARY KEY)的序号, 不存在时为 -1</summary>
+        Public ReadOnly Property RowIdAliasOrdinal As Integer
+            Get
+                For i As Integer = 0 To _columns.Count - 1
+                    If _columns(i).IsRowIdAlias() Then
+                        Return i
+                    End If
+                Next
+
+                Return -1
+            End Get
+        End Property
+
+        ''' <summary>主键列序号, 不存在时为 -1</summary>
+        Public ReadOnly Property PrimaryKeyOrdinal As Integer
+            Get
+                For i As Integer = 0 To _columns.Count - 1
+                    If _columns(i).PrimaryKey Then
+                        Return i
+                    End If
+                Next
+
+                Return -1
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' 写入磁盘时使用的 CREATE TABLE 语句。由本模块创建的表为自动生成; 打开已有库时原样保留。
+        ''' </summary>
+        Public ReadOnly Property Sql As String
+            Get
+                If Not String.IsNullOrEmpty(_originalSql) Then
+                    Return _originalSql
+                End If
+
+                Return GenerateSql()
+            End Get
+        End Property
+
+        Friend Sub SetOriginalSql(sql As String)
+            _originalSql = sql
+        End Sub
+
+        ''' <summary>按列名获取列序号(忽略大小写), 找不到返回 -1</summary>
+        Public Function GetOrdinal(name As String) As Integer
+            For i As Integer = 0 To _columns.Count - 1
+                If String.Equals(_columns(i).Name, name, StringComparison.OrdinalIgnoreCase) Then
+                    Return i
+                End If
+            Next
+
+            Return -1
+        End Function
+
+        ''' <summary>
+        ''' 追加一行数据。当表存在 INTEGER PRIMARY KEY 列时, 若该列提供了整数值则作为 rowid, 否则自增分配。
+        ''' </summary>
+        ''' <returns>该行的 rowid</returns>
+        Public Function AddRow(values As Object()) As Long
+            Dim row As Object() = NormalizeValues(values)
+            Dim rowId As Long
+
+            If RowIdAliasOrdinal >= 0 Then
+                Dim key As Object = row(RowIdAliasOrdinal)
+
+                If IsIntegerValue(key) Then
+                    rowId = Convert.ToInt64(key)
+                ElseIf key Is Nothing Then
+                    rowId = _nextRowId
+                Else
+                    Throw New ArgumentException($"表 [{TableName}] 的 INTEGER PRIMARY KEY 列必须为整数或 Nothing")
+                End If
+            Else
+                rowId = _nextRowId
+            End If
+
+            Return AddRow(rowId, row)
+        End Function
+
+        ''' <summary>以指定 rowid 追加一行数据</summary>
+        Public Function AddRow(rowId As Long, values As Object()) As Long
+            If _rows.ContainsKey(rowId) Then
+                Throw New ArgumentException($"rowid {rowId} 已经存在于表 [{TableName}] 之中")
+            End If
+
+            Dim row As Object() = NormalizeValues(values)
+
+            If RowIdAliasOrdinal >= 0 Then
+                row(RowIdAliasOrdinal) = rowId
+            End If
+
+            _rows(rowId) = row
+
+            If rowId >= _nextRowId Then
+                _nextRowId = rowId + 1
+            End If
+
+            Call _writer.MarkDirty()
+            Return rowId
+        End Function
+
+        ''' <summary>按 rowid 整体更新一行</summary>
+        Public Function UpdateRow(rowId As Long, values As Object()) As Boolean
+            If Not _rows.ContainsKey(rowId) Then
+                Return False
+            End If
+
+            Dim row As Object() = NormalizeValues(values)
+
+            If RowIdAliasOrdinal >= 0 Then
+                row(RowIdAliasOrdinal) = rowId
+            End If
+
+            _rows(rowId) = row
+            Call _writer.MarkDirty()
+            Return True
+        End Function
+
+        ''' <summary>按 rowid 更新指定列</summary>
+        Public Function UpdateRow(rowId As Long, columnName As String, value As Object) As Boolean
+            If Not _rows.ContainsKey(rowId) Then
+                Return False
+            End If
+
+            Dim ordinal As Integer = GetOrdinal(columnName)
+
+            If ordinal < 0 Then
+                Throw New ArgumentException($"表 [{TableName}] 之中不存在列 [{columnName}]")
+            End If
+
+            ' INTEGER PRIMARY KEY 是 rowid 的别名, 不允许通过列更新改变 rowid
+            _rows(rowId)(ordinal) = If(ordinal = RowIdAliasOrdinal, CObj(rowId), value)
+            Call _writer.MarkDirty()
+            Return True
+        End Function
+
+        ''' <summary>按主键值更新一行</summary>
+        Public Function UpdateRowByPrimaryKey(key As Object, values As Object()) As Boolean
+            Dim pk As Integer = PrimaryKeyOrdinal
+
+            If pk < 0 Then
+                Throw New InvalidOperationException($"表 [{TableName}] 没有定义主键")
+            End If
+
+            For Each kv As KeyValuePair(Of Long, Object()) In _rows
+                If ValuesEqual(kv.Value(pk), key) Then
+                    Return UpdateRow(kv.Key, values)
+                End If
+            Next
+
+            Return False
+        End Function
+
+        ''' <summary>按 rowid 删除一行</summary>
+        Public Function DeleteRow(rowId As Long) As Boolean
+            If Not _rows.Remove(rowId) Then
+                Return False
+            End If
+
+            Call _writer.MarkDirty()
+            Return True
+        End Function
+
+        ''' <summary>按主键值删除一行</summary>
+        Public Function DeleteRowByPrimaryKey(key As Object) As Boolean
+            Dim pk As Integer = PrimaryKeyOrdinal
+
+            If pk < 0 Then
+                Throw New InvalidOperationException($"表 [{TableName}] 没有定义主键")
+            End If
+
+            For Each kv As KeyValuePair(Of Long, Object()) In _rows
+                If ValuesEqual(kv.Value(pk), key) Then
+                    Return DeleteRow(kv.Key)
+                End If
+            Next
+
+            Return False
+        End Function
+
+        ''' <summary>清空全部数据行</summary>
+        Public Sub Clear()
+            If _rows.Count > 0 Then
+                _rows.Clear()
+                Call _writer.MarkDirty()
+            End If
+        End Sub
+
+        ''' <summary>按 rowid 读取一行</summary>
+        Public Function GetRow(rowId As Long) As Sqlite3DataRow
+            Dim values As Object() = Nothing
+
+            If Not _rows.TryGetValue(rowId, values) Then
+                Return Nothing
+            End If
+
+            Return New Sqlite3DataRow(Me, rowId, values)
+        End Function
+
+        ''' <summary>按 rowid 升序枚举全部数据行</summary>
+        Public Iterator Function EnumerateRows() As IEnumerable(Of Sqlite3DataRow)
+            For Each kv As KeyValuePair(Of Long, Object()) In _rows
+                Yield New Sqlite3DataRow(Me, kv.Key, kv.Value)
+            Next
+        End Function
+
+        ''' <summary>构建提交快照(记录体编码)</summary>
+        Friend Function BuildSnapshot() As Internal.WriterTableSnapshot
+            Dim entries As New List(Of Internal.LeafEntry)()
+            Dim aliasOrdinal As Integer = RowIdAliasOrdinal
+
+            For Each kv As KeyValuePair(Of Long, Object()) In _rows
+                Dim record As Byte() = Internal.RecordEncoder.EncodeRecord(kv.Value, aliasOrdinal)
+                entries.Add(New Internal.LeafEntry With {.RowId = kv.Key, .Record = record})
+            Next
+
+            Return New Internal.WriterTableSnapshot With {
+                .Name = TableName,
+                .Sql = Sql,
+                .Entries = entries
+            }
+        End Function
+
+        ''' <summary>载入一行已有数据(打开已有库时使用)</summary>
+        Friend Sub LoadRow(rowId As Long, values As Object())
+            _rows(rowId) = values
+
+            If rowId >= _nextRowId Then
+                _nextRowId = rowId + 1
+            End If
+        End Sub
+
+        Private Function NormalizeValues(values As Object()) As Object()
+            Dim result(_columns.Count - 1) As Object
+
+            If values IsNot Nothing Then
+                If values.Length > _columns.Count Then
+                    Throw New ArgumentException($"行数据的列数({values.Length})多于表 [{TableName}] 的列数({_columns.Count})")
+                End If
+
+                Call Array.Copy(values, result, values.Length)
+            End If
+
+            Return result
+        End Function
+
+        Private Function GenerateSql() As String
+            Dim parts As New List(Of String)()
+
+            For Each c As Sqlite3Column In _columns
+                parts.Add(c.ToSql())
+            Next
+
+            Dim primaryKeys As Sqlite3Column() = _columns.Where(Function(c) c.PrimaryKey).ToArray()
+
+            ' 单个 INTEGER PRIMARY KEY 已在列定义之中表达(rowid 别名), 不需要额外的表级约束
+            Dim expressedInline As Boolean = primaryKeys.Length = 1 AndAlso primaryKeys(0).IsRowIdAlias()
+
+            If primaryKeys.Length > 0 AndAlso Not expressedInline Then
+                parts.Add("PRIMARY KEY (" & String.Join(", ", primaryKeys.Select(Function(c) Sqlite3Column.EscapeName(c.Name))) & ")")
+            End If
+
+            Return "CREATE TABLE " & Sqlite3Column.EscapeName(TableName) & " (" & String.Join(", ", parts) & ")"
+        End Function
+
+        Private Shared Function IsIntegerValue(value As Object) As Boolean
+            Return TypeOf value Is Long OrElse TypeOf value Is Integer OrElse TypeOf value Is Short OrElse TypeOf value Is Byte
+        End Function
+
+        Private Shared Function ValuesEqual(a As Object, b As Object) As Boolean
+            If a Is Nothing AndAlso b Is Nothing Then
+                Return True
+            End If
+
+            If a Is Nothing OrElse b Is Nothing Then
+                Return False
+            End If
+
+            Return Equals(a, b)
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return $"[{TableName}] {RowCount} rows"
+        End Function
+
+    End Class
+
+End Namespace
---
+++ Data/BinaryData/SQLite3/Writer/Sqlite3Writer.vb
@@ -0,0 +1,251 @@
+Imports System.Collections.Generic
+Imports System.IO
+Imports System.Linq
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Data.IO.ManagedSqlite
+Imports Microsoft.VisualBasic.Data.IO.ManagedSqlite.Core.Tables
+Imports Microsoft.VisualBasic.Data.IO.ManagedSqlite.Writer.Internal
+
+Namespace Writer
+
+    ''' <summary>
+    ''' SQLite3 数据库写入器。
+    ''' 
+    ''' 采用"内存模型 + 提交时整文件重建"的策略: 全部增删改在内存之中完成,
+    ''' 调用 <see cref="Commit"/> 时一次性序列化重建整个数据库文件, 并以临时文件原子替换目标文件,
+    ''' 保证写入失败不会损坏原库。构建之后可以被本模块的读取器重新打开。
+    ''' </summary>
+    Public Class Sqlite3Writer : Implements IDisposable
+
+        Private ReadOnly _tables As New List(Of Sqlite3TableWriter)()
+
+        Private _path As String
+        Private _pageSize As Integer = 4096
+        Private _reservedSpace As Integer = 0
+        Private _changeCounter As Long = 1
+        Private _dirty As Boolean
+        Private _disposed As Boolean
+
+        Private Sub New()
+        End Sub
+
+        ''' <summary>目标数据库文件路径</summary>
+        Public ReadOnly Property FilePath As String
+            Get
+                Return _path
+            End Get
+        End Property
+
+        ''' <summary>页大小(字节)</summary>
+        Public ReadOnly Property PageSize As Integer
+            Get
+                Return _pageSize
+            End Get
+        End Property
+
+        ''' <summary>Dispose 时是否自动提交(默认 True)</summary>
+        Public Property AutoCommitOnDispose As Boolean = True
+
+        ''' <summary>
+        ''' 从零创建一个新的数据库文件(默认页大小 4096), 并立即写入一个合法的空库。
+        ''' </summary>
+        Public Shared Function CreateFile(path As String, Optional pageSize As Integer = 4096) As Sqlite3Writer
+            If String.IsNullOrWhiteSpace(path) Then
+                Throw New ArgumentException("数据库文件路径不能为空", NameOf(path))
+            End If
+
+            Dim writer As New Sqlite3Writer()
+            writer._path = path
+            writer._pageSize = If(pageSize = 65536, 65536, pageSize)
+            writer._reservedSpace = 0
+            writer._changeCounter = 1
+            writer.Commit()
+
+            Return writer
+        End Function
+
+        ''' <summary>
+        ''' 打开一个已有的数据库文件, 把它加载为内存模型后即可执行增删改查。
+        ''' </summary>
+        Public Shared Function OpenFile(path As String) As Sqlite3Writer
+            If Not File.Exists(path) Then
+                Throw New FileNotFoundException("数据库文件不存在: " & path, path)
+            End If
+
+            Dim writer As New Sqlite3Writer()
+            writer._path = path
+
+            Using db As Sqlite3Database = Sqlite3Database.OpenFile(path, New Sqlite3Settings())
+                writer._pageSize = db.Header.PageSize
+                writer._reservedSpace = db.Header.ReservedSpaceAtEndOfPage
+                writer._changeCounter = CLng(db.Header.ChangeCounter)
+
+                For Each schemaRow As Sqlite3SchemaRow In db.GetTables
+                    If Not String.Equals(schemaRow.type, "table", StringComparison.OrdinalIgnoreCase) Then
+                        Continue For
+                    End If
+
+                    Dim table As Sqlite3Table = db.GetTable(schemaRow.tableName)
+                    Dim primaryKeys As String() = table.schema.PrimaryKeys
+                    Dim columns As New List(Of Sqlite3Column)()
+
+                    For Each column As NamedValue(Of String) In table.schema.columns
+                        Dim name As String = column.Name
+                        Dim declared As String = column.Value
+                        Dim isPrimaryKey As Boolean =
+                            primaryKeys.Any(Function(p) String.Equals(p, name, StringComparison.OrdinalIgnoreCase))
+
+                        columns.Add(New Sqlite3Column With {
+                            .Name = name,
+                            .Type = declared,
+                            .PrimaryKey = isPrimaryKey
+                        })
+                    Next
+
+                    Dim tableWriter As New Sqlite3TableWriter(writer, schemaRow.tableName, columns)
+                    Call tableWriter.SetOriginalSql(schemaRow.Sql)
+
+                    For Each row As Sqlite3Row In table.EnumerateRows()
+                        Dim values As Object() = DirectCast(row.ColumnData.Clone(), Object())
+                        Call tableWriter.LoadRow(row.RowId, values)
+                    Next
+
+                    writer._tables.Add(tableWriter)
+                Next
+            End Using
+
+            Return writer
+        End Function
+
+        ''' <summary>创建一个新的数据表并返回其可写模型</summary>
+        Public Function CreateTable(name As String, columns As IEnumerable(Of Sqlite3Column)) As Sqlite3TableWriter
+            If String.IsNullOrWhiteSpace(name) Then
+                Throw New ArgumentException("表名不能为空", NameOf(name))
+            End If
+
+            If GetTableNames().Any(Function(x) String.Equals(x, name, StringComparison.OrdinalIgnoreCase)) Then
+                Throw New InvalidOperationException("数据表已经存在: " & name)
+            End If
+
+            Dim columnList As Sqlite3Column() = columns.ToArray()
+
+            If columnList.Length = 0 Then
+                Throw New ArgumentException("数据表至少需要定义一列", NameOf(columns))
+            End If
+
+            Dim tableWriter As New Sqlite3TableWriter(Me, name, columnList)
+            _tables.Add(tableWriter)
+            _dirty = True
+
+            Return tableWriter
+        End Function
+
+        ''' <summary>获取指定名称的表</summary>
+        Public Function GetTable(name As String) As Sqlite3TableWriter
+            For Each table As Sqlite3TableWriter In _tables
+                If String.Equals(table.TableName, name, StringComparison.OrdinalIgnoreCase) Then
+                    Return table
+                End If
+            Next
+
+            Throw New InvalidOperationException("找不到数据表: " & name)
+        End Function
+
+        ''' <summary>按创建顺序返回全部表名</summary>
+        Public Function GetTableNames() As IEnumerable(Of String)
+            Return _tables.Select(Function(t) t.TableName).ToArray()
+        End Function
+
+        ''' <summary>删除指定名称的表</summary>
+        Public Function DropTable(name As String) As Boolean
+            For i As Integer = 0 To _tables.Count - 1
+                If String.Equals(_tables(i).TableName, name, StringComparison.OrdinalIgnoreCase) Then
+                    _tables.RemoveAt(i)
+                    _dirty = True
+                    Return True
+                End If
+            Next
+
+            Return False
+        End Function
+
+        ''' <summary>
+        ''' 把当前内存模型整体提交到磁盘。可重复调用, 结果一致。
+        ''' </summary>
+        Public Sub Commit()
+            If _disposed Then
+                Throw New ObjectDisposedException(NameOf(Sqlite3Writer))
+            End If
+
+            Dim snapshots As New List(Of WriterTableSnapshot)()
+
+            For Each table As Sqlite3TableWriter In _tables
+                snapshots.Add(table.BuildSnapshot())
+            Next
+
+            _changeCounter += 1
+
+            Dim serializer As New SqliteFileSerializer(_pageSize, _reservedSpace, _changeCounter)
+            Dim bytes As Byte() = serializer.Serialize(snapshots)
+
+            Call WriteAtomic(_path, bytes)
+            _dirty = False
+        End Sub
+
+        ''' <summary>标记存在未提交的修改(供表模型回调)</summary>
+        Friend Sub MarkDirty()
+            _dirty = True
+        End Sub
+
+        ''' <summary>是否存在未提交的修改</summary>
+        Public ReadOnly Property IsDirty As Boolean
+            Get
+                Return _dirty
+            End Get
+        End Property
+
+        Public Sub Dispose() Implements IDisposable.Dispose
+            If _disposed Then
+                Return
+            End If
+
+            _disposed = True
+
+            If AutoCommitOnDispose AndAlso _dirty Then
+                Call Commit()
+            End If
+        End Sub
+
+        Private Shared Sub WriteAtomic(path As String, bytes As Byte())
+            Dim directory As String = Path.GetDirectoryName(path)
+
+            If Not String.IsNullOrEmpty(directory) Then
+                Call Directory.CreateDirectory(directory)
+            End If
+
+            Dim tempPath As String = path & ".tmp"
+
+            Try
+                File.WriteAllBytes(tempPath, bytes)
+
+                If File.Exists(path) Then
+                    File.Replace(tempPath, path, Nothing)
+                Else
+                    File.Move(tempPath, path)
+                End If
+            Catch
+                If File.Exists(tempPath) Then
+                    Try
+                        File.Delete(tempPath)
+                    Catch
+                        ' 忽略清理失败
+                    End Try
+                End If
+
+                Throw
+            End Try
+        End Sub
+
+    End Class
+
+End Namespace
