5630e7eb3878195003c72197ccb64ba96a646cef
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-11 17:24:47 +0800

    btree writer

---
+++ Data/BinaryData/SQLite3/Writer/Internal/BTreeWriter.vb
@@ -0,0 +1,242 @@
+Imports System.Collections.Generic
+Imports System.IO
+
+Namespace Writer.Internal
+
+    ''' <summary>
+    ''' 表 B 树叶页之中的一条记录(按 rowid 升序排列)
+    ''' </summary>
+    Friend Structure LeafEntry
+        Public RowId As Long
+        Public Record As Byte()
+    End Structure
+
+    ''' <summary>
+    ''' 表 B 树的节点(叶节点或内部节点), 页号在结构构建完成之后单独分配。
+    ''' </summary>
+    Friend Class BTreeNode
+
+        Public Property IsLeaf As Boolean
+        Public Property LeafCells As List(Of LeafEntry)
+        Public Property Children As List(Of BTreeNode)
+        Public Property Page As Long
+
+        Friend Shared Function CreateLeaf(cells As List(Of LeafEntry)) As BTreeNode
+            Return New BTreeNode With {
+                .IsLeaf = True,
+                .LeafCells = cells,
+                .Children = New List(Of BTreeNode)()
+            }
+        End Function
+
+        Friend Shared Function CreateInterior(children As List(Of BTreeNode)) As BTreeNode
+            Return New BTreeNode With {
+                .IsLeaf = False,
+                .LeafCells = New List(Of LeafEntry)(),
+                .Children = children
+            }
+        End Function
+
+        ''' <summary>
+        ''' 子树之中的最大 rowid(用于内部页的 key)
+        ''' </summary>
+        Public ReadOnly Property MaxRowId As Long
+            <System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)>
+            Get
+                If IsLeaf Then
+                    Return LeafCells(LeafCells.Count - 1).RowId
+                Else
+                    Return Children(Children.Count - 1).MaxRowId
+                End If
+            End Get
+        End Property
+    End Class
+
+    ''' <summary>
+    ''' 表 B 树写入器: 负责容量计算、自底向上建树、cell 编码与页号分配。
+    ''' </summary>
+    Friend Class BTreeWriter
+
+        Public ReadOnly Property PageSize As Integer
+        Public ReadOnly Property ReservedSpace As Integer
+        ''' <summary>可用页大小 U = PageSize - ReservedSpace</summary>
+        Public ReadOnly Property UsableSize As Integer
+
+        ''' <summary>table leaf 页在无溢出时可以内联的最大 payload(X = U - 35)</summary>
+        Private ReadOnly _x As Integer
+        ''' <summary>溢出时保证内联的最小 payload(M = ((U-12)*32/255)-23)</summary>
+        Private ReadOnly _m As Integer
+
+        Public Sub New(pageSize As Integer, reservedSpace As Integer)
+            Me.PageSize = pageSize
+            Me.ReservedSpace = reservedSpace
+            Me.UsableSize = pageSize - reservedSpace
+            _x = UsableSize - 35
+            _m = (UsableSize - 12) * 32 \ 255 - 23
+        End Sub
+
+        ''' <summary>
+        ''' 计算 payload 之中内联保存在 cell 内部的字节数(与读取侧 <c>ParseCellInternal</c> 严格对称)。
+        ''' </summary>
+        Public Function GetInlineSize(payloadSize As Long) As Integer
+            If payloadSize <= _x Then
+                ' 全部内联, 无溢出
+                Return CInt(payloadSize)
+            End If
+
+            Dim k As Integer = CInt(_m + ((payloadSize - _m) Mod (UsableSize - 4)))
+
+            If k <= _x Then
+                Return k
+            End If
+
+            Return _m
+        End Function
+
+        ''' <summary>
+        ''' 计算叶 cell 在页内所占的字节数(含可能的 4 字节溢出页指针)
+        ''' </summary>
+        Public Function GetLeafCellSize(entry As LeafEntry) As Integer
+            Dim payload As Long = entry.Record.Length
+            Dim inlineSize As Integer = GetInlineSize(payload)
+
+            Dim size As Integer = RecordEncoder.GetVarIntLength(payload) +
+                                  RecordEncoder.GetVarIntLength(entry.RowId) +
+                                  inlineSize
+
+            If payload > inlineSize Then
+                size += 4
+            End If
+
+            Return size
+        End Function
+
+        ''' <summary>
+        ''' 自底向上把有序记录构建为表 B 树。
+        ''' </summary>
+        Public Function BuildTree(entries As List(Of LeafEntry)) As BTreeNode
+            Dim nodes As New List(Of BTreeNode)()
+            Dim current As New List(Of LeafEntry)()
+            Dim used As Long = 0
+
+            For Each e As LeafEntry In entries
+                Dim cellSize As Long = GetLeafCellSize(e)
+
+                If current.Count > 0 AndAlso
+                   (PageBuffer.LeafHeaderSize + used + cellSize + PageBuffer.CellPointerSize) > UsableSize Then
+
+                    nodes.Add(BTreeNode.CreateLeaf(current))
+                    current = New List(Of LeafEntry)()
+                    used = 0
+                End If
+
+                current.Add(e)
+                used += cellSize
+            Next
+
+            If current.Count > 0 OrElse nodes.Count = 0 Then
+                nodes.Add(BTreeNode.CreateLeaf(current))
+            End If
+
+            While nodes.Count > 1
+                nodes = BuildInteriorLevel(nodes)
+            End While
+
+            Return nodes(0)
+        End Function
+
+        Private Function BuildInteriorLevel(children As List(Of BTreeNode)) As List(Of BTreeNode)
+            Dim parents As New List(Of BTreeNode)()
+            Dim current As New List(Of BTreeNode)()
+            Dim used As Long = 0
+
+            For Each child As BTreeNode In children
+                Dim addCell As Long = 0
+                Dim addPointer As Long = 0
+
+                If current.Count > 0 Then
+                    ' 追加一个子节点会在末尾新增一个 cell(左指针=当前最后一个子节点, key=其最大 rowid)
+                    addCell = 4 + RecordEncoder.GetVarIntLength(current(current.Count - 1).MaxRowId)
+                    addPointer = PageBuffer.CellPointerSize
+                End If
+
+                If current.Count > 0 AndAlso
+                   (PageBuffer.InteriorHeaderSize + used + addCell + addPointer) > UsableSize Then
+
+                    parents.Add(BTreeNode.CreateInterior(current))
+                    current = New List(Of BTreeNode)()
+                    used = 0
+                End If
+
+                current.Add(child)
+                used += addCell + addPointer
+            Next
+
+            If current.Count > 0 Then
+                parents.Add(BTreeNode.CreateInterior(current))
+            End If
+
+            Return parents
+        End Function
+
+        ''' <summary>
+        ''' 按 pre-order(父页先于子页)分配页号, 保证父页可以引用已确定的子页号。
+        ''' </summary>
+        Public Shared Sub AssignPages(node As BTreeNode, ByRef nextPage As Long)
+            node.Page = nextPage
+            nextPage += 1
+
+            If Not node.IsLeaf Then
+                For Each child As BTreeNode In node.Children
+                    AssignPages(child, nextPage)
+                Next
+            End If
+        End Sub
+
+        ''' <summary>
+        ''' 构建叶 cell 字节: varint(P) + varint(rowid) + 内联 payload [+ 4 字节溢出页指针]
+        ''' </summary>
+        ''' <param name="allocateOverflow">
+        ''' 溢出页分配回调, 参数为(完整记录体, 内联字节数), 返回首个溢出页号。
+        ''' </param>
+        Public Function BuildLeafCell(entry As LeafEntry, allocateOverflow As Func(Of Byte(), Integer, Long)) As Byte()
+            Dim payload As Long = entry.Record.Length
+            Dim inlineSize As Integer = GetInlineSize(payload)
+
+            Using ms As New MemoryStream()
+                Call RecordEncoder.WriteVarInt(ms, payload)
+                Call RecordEncoder.WriteVarInt(ms, entry.RowId)
+
+                If payload <= inlineSize Then
+                    ms.Write(entry.Record, 0, entry.Record.Length)
+                Else
+                    ms.Write(entry.Record, 0, inlineSize)
+                    Dim firstOverflow As Long = allocateOverflow(entry.Record, inlineSize)
+                    Call WriteUInt32BigEndian(ms, firstOverflow)
+                End If
+
+                Return ms.ToArray()
+            End Using
+        End Function
+
+        ''' <summary>
+        ''' 构建内部 cell 字节: 4 字节左子页号 + varint(key)
+        ''' </summary>
+        Public Function BuildInteriorCell(leftChildPage As Long, key As Long) As Byte()
+            Using ms As New MemoryStream()
+                Call WriteUInt32BigEndian(ms, leftChildPage)
+                Call RecordEncoder.WriteVarInt(ms, key)
+                Return ms.ToArray()
+            End Using
+        End Function
+
+        Public Shared Sub WriteUInt32BigEndian(stream As Stream, value As Long)
+            stream.WriteByte(CByte((value >> 24) And &HFF))
+            stream.WriteByte(CByte((value >> 16) And &HFF))
+            stream.WriteByte(CByte((value >> 8) And &HFF))
+            stream.WriteByte(CByte(value And &HFF))
+        End Sub
+
+    End Class
+
+End Namespace
---
+++ Data/BinaryData/SQLite3/Writer/Internal/SqliteFileSerializer.vb
@@ -0,0 +1,240 @@
+Imports System.Collections.Generic
+Imports System.IO
+Imports System.Text
+
+Namespace Writer.Internal
+
+    ''' <summary>
+    ''' 一张表在提交时的快照数据
+    ''' </summary>
+    Friend Class WriterTableSnapshot
+        Public Property Name As String
+        Public Property Sql As String
+        ''' <summary>按 rowid 升序排列的记录</summary>
+        Public Property Entries As List(Of LeafEntry)
+    End Class
+
+    ''' <summary>
+    ''' 把内存模型序列化为一个完整的 SQLite3 数据库文件。
+    ''' 
+    ''' 页号分配策略: 用户表从第 2 页起按 pre-order 分配; <c>sqlite_master</c> 的根页固定为第 1 页,
+    ''' 其子页在所有用户表页之后分配; 溢出页最后分配。这样父页始终可以引用已经确定的子页号。
+    ''' </summary>
+    Friend Class SqliteFileSerializer
+
+        Private ReadOnly _btree As BTreeWriter
+        Private ReadOnly _pages As New Dictionary(Of Long, Byte())
+        Private _nextPage As Long
+        Private _changeCounter As Long
+
+        Public Sub New(pageSize As Integer, Optional reservedSpace As Integer = 0, Optional changeCounter As Long = 1)
+            _btree = New BTreeWriter(pageSize, reservedSpace)
+            _changeCounter = changeCounter
+        End Sub
+
+        Public ReadOnly Property PageSize As Integer
+            Get
+                Return _btree.PageSize
+            End Get
+        End Property
+
+        Public Property ChangeCounter As Long
+            Get
+                Return _changeCounter
+            End Get
+            Set(value As Long)
+                _changeCounter = value
+            End Set
+        End Property
+
+        ''' <summary>
+        ''' 序列化整库, 返回完整的文件字节。
+        ''' </summary>
+        Public Function Serialize(tables As IList(Of WriterTableSnapshot)) As Byte()
+            _pages.Clear()
+            _nextPage = 1
+
+            ' 1. 用户表建树
+            Dim trees As New List(Of BTreeNode)()
+            For Each t As WriterTableSnapshot In tables
+                trees.Add(_btree.BuildTree(t.Entries))
+            Next
+
+            ' 2. 用户表页号从第 2 页开始分配
+            Dim counter As Long = 2
+            For Each tree As BTreeNode In trees
+                BTreeWriter.AssignPages(tree, counter)
+            Next
+
+            ' 3. 构建 sqlite_master, 根页固定为第 1 页
+            Dim masterEntries As New List(Of LeafEntry)()
+            Dim masterRowId As Long = 1
+
+            For i As Integer = 0 To tables.Count - 1
+                Dim t As WriterTableSnapshot = tables(i)
+                Dim record As Byte() = RecordEncoder.EncodeRecord(
+                    New Object() {"table", t.Name, t.Name, trees(i).Page, t.Sql}, -1)
+
+                masterEntries.Add(New LeafEntry With {.RowId = masterRowId, .Record = record})
+                masterRowId += 1
+            Next
+
+            Dim masterTree As BTreeNode = _btree.BuildTree(masterEntries)
+            masterTree.Page = 1
+
+            If Not masterTree.IsLeaf Then
+                For Each child As BTreeNode In masterTree.Children
+                    BTreeWriter.AssignPages(child, counter)
+                Next
+            End If
+
+            ' 4. 序列化所有页(过程中可能分配溢出页)
+            _nextPage = counter
+            Call SerializeNode(masterTree, PageBuffer.Page1ContentOffset)
+
+            For Each tree As BTreeNode In trees
+                Call SerializeNode(tree, 0)
+            Next
+
+            ' 5. 组装文件内容
+            Dim totalPages As Long = _nextPage - 1
+            Dim header As Byte() = BuildDatabaseHeader(totalPages)
+
+            If _pages.ContainsKey(1) Then
+                Call Array.Copy(header, 0, _pages(1), 0, header.Length)
+            Else
+                _pages(1) = header
+            End If
+
+            Dim ms As New MemoryStream()
+
+            For p As Long = 1 To totalPages
+                Dim page As Byte()
+
+                If _pages.ContainsKey(p) Then
+                    page = _pages(p)
+                Else
+                    page = New Byte(_btree.PageSize - 1) {}
+                End If
+
+                ms.Write(page, 0, page.Length)
+            Next
+
+            Return ms.ToArray()
+        End Function
+
+        Private Sub SerializeNode(node As BTreeNode, baseOffset As Integer)
+            Dim cells As New List(Of Byte())()
+
+            If node.IsLeaf Then
+                For Each entry As LeafEntry In node.LeafCells
+                    cells.Add(_btree.BuildLeafCell(entry, AddressOf AllocateOverflow))
+                Next
+
+                _pages(node.Page) = PageBuffer.BuildBTreePage(
+                    _btree.PageSize, PageBuffer.PageTypeLeafTable, cells,
+                    rightMostPointer:=0, baseOffset:=baseOffset, usableSize:=_btree.UsableSize)
+                Return
+            End If
+
+            For i As Integer = 0 To node.Children.Count - 2
+                Dim child As BTreeNode = node.Children(i)
+                cells.Add(_btree.BuildInteriorCell(child.Page, child.MaxRowId))
+            Next
+
+            Dim rightMost As Long = node.Children(node.Children.Count - 1).Page
+
+            _pages(node.Page) = PageBuffer.BuildBTreePage(
+                _btree.PageSize, PageBuffer.PageTypeInteriorTable, cells,
+                rightMostPointer:=rightMost, baseOffset:=baseOffset, usableSize:=_btree.UsableSize)
+
+            For Each child As BTreeNode In node.Children
+                Call SerializeNode(child, 0)
+            Next
+        End Sub
+
+        ''' <summary>
+        ''' 为超出页内可容纳范围的记录分配溢出页链, 返回首个溢出页号。
+        ''' 每个溢出页 = 4 字节下一页指针 + 最多 (U - 4) 字节数据。
+        ''' </summary>
+        Private Function AllocateOverflow(record As Byte(), inlineSize As Integer) As Long
+            Dim capacity As Integer = _btree.UsableSize - 4
+            Dim remaining As Integer = record.Length - inlineSize
+            Dim pageCount As Integer = (remaining + capacity - 1) \ capacity
+
+            Dim firstPage As Long = _nextPage
+            _nextPage += pageCount
+
+            Dim offset As Integer = inlineSize
+
+            For i As Integer = 0 To pageCount - 1
+                Dim pageNo As Long = firstPage + i
+                Dim page As New PageBuffer(_btree.PageSize)
+                Dim nextNo As Long = If(i < pageCount - 1, pageNo + 1, 0L)
+
+                page.WriteUInt32(0, nextNo)
+
+                Dim length As Integer = Math.Min(capacity, record.Length - offset)
+                Dim chunk(length - 1) As Byte
+                Call Array.Copy(record, offset, chunk, 0, length)
+                page.WriteBytes(4, chunk)
+
+                _pages(pageNo) = page.Data
+                offset += length
+            Next
+
+            Return firstPage
+        End Function
+
+        ''' <summary>
+        ''' 构建 100 字节的数据库文件头(字段顺序与读取侧 DatabaseHeader.Parse 严格对齐)。
+        ''' </summary>
+        Private Function BuildDatabaseHeader(totalPages As Long) As Byte()
+            Dim pageSize As Integer = _btree.PageSize
+            Dim header As Byte() = New Byte(99) {}
+
+            Dim magic As Byte() = Encoding.ASCII.GetBytes("SQLite format 3")
+            Call Array.Copy(magic, 0, header, 0, magic.Length)
+            header(15) = 0
+
+            Call WriteUInt16(header, 16, If(pageSize = 65536, 1, pageSize))
+            header(18) = 1                       ' WriteVersion = Legacy
+            header(19) = 1                       ' ReadVersion  = Legacy
+            header(20) = 0                       ' ReservedSpaceAtEndOfPage
+            header(21) = 64                      ' MaximumEmbeddedPayloadFraction
+            header(22) = 32                      ' MinimumEmbeddedPayloadFraction
+            header(23) = 32                      ' LeafPayloadFraction
+            Call WriteUInt32(header, 24, _changeCounter)
+            Call WriteUInt32(header, 28, totalPages)
+            Call WriteUInt32(header, 32, 0)      ' FirstFreelistTrunkPage
+            Call WriteUInt32(header, 36, 0)      ' FreeListPages
+            Call WriteUInt32(header, 40, _changeCounter)  ' SchemaCookie
+            Call WriteUInt32(header, 44, 4)      ' SchemaFormat
+            Call WriteUInt32(header, 48, 0)      ' DefaultPageCacheSize
+            Call WriteUInt32(header, 52, 0)      ' LargestRootBTreePage
+            Call WriteUInt32(header, 56, 1)      ' TextEncoding = UTF8
+            Call WriteUInt32(header, 60, 0)      ' UserVersion
+            Call WriteUInt32(header, 64, 0)      ' IncrementalVacuumMode
+            Call WriteUInt32(header, 68, 0)      ' ApplicationId
+            ' 偏移 72..91 为保留区, 保持为 0
+            Call WriteUInt32(header, 92, _changeCounter)  ' VersionValidFor
+            Call WriteUInt32(header, 96, 3045000)         ' SQLite version number
+
+            Return header
+        End Function
+
+        Private Shared Sub WriteUInt16(buffer As Byte(), offset As Integer, value As Integer)
+            buffer(offset) = CByte((value >> 8) And &HFF)
+            buffer(offset + 1) = CByte(value And &HFF)
+        End Sub
+
+        Private Shared Sub WriteUInt32(buffer As Byte(), offset As Integer, value As Long)
+            buffer(offset) = CByte((value >> 24) And &HFF)
+            buffer(offset + 1) = CByte((value >> 16) And &HFF)
+            buffer(offset + 2) = CByte((value >> 8) And &HFF)
+            buffer(offset + 3) = CByte(value And &HFF)
+        End Sub
+
+    End Class
+
+End Namespace
