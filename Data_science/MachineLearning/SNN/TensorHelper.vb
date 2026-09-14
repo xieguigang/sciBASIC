52e5297a6cdb03bfbced4800793969079131c593
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-15 03:53:27 +0800

    config run test

---
+++ Data_science/MachineLearning/SNN/TensorHelper.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic.Data
+Imports Microsoft.VisualBasic.MachineLearning.TensorFlow
+
+Public Module TensorHelper
+
+    Public Function BatchTensor(data As Double()(), idx As Integer(), offset As Integer, count As Integer) As Tensor
+        Dim f = data(0).Length
+        Dim flat(f * count - 1) As Double
+        For b = 0 To count - 1
+            Array.Copy(data(idx(offset + b)), 0, flat, b * f, f)
+        Next
+        Return New Tensor(flat, count, f)
+    End Function
+
+    Public Function AllTensor(d As NumericTable) As Tensor
+        Dim n = d.nsamples
+        Dim idx(n - 1) As Integer
+        For i = 0 To n - 1
+            idx(i) = i
+        Next
+        Return BatchTensor(d.features, idx, 0, n)
+    End Function
+End Module
--- Data_science/MachineLearning/SNN/test/test1.vb
+++ Data_science/MachineLearning/SNN/test/test1.vb
@@ -39,24 +39,6 @@ Module test1
         Return New NumericTable(xs, ys.ToArray())
     End Function
 
-    Private Function BatchTensor(data As Double()(), idx As Integer(), offset As Integer, count As Integer) As Tensor
-        Dim f = data(0).Length
-        Dim flat(f * count - 1) As Double
-        For b = 0 To count - 1
-            Array.Copy(data(idx(offset + b)), 0, flat, b * f, f)
-        Next
-        Return New Tensor(flat, count, f)
-    End Function
-
-    Private Function AllTensor(d As NumericTable) As Tensor
-        Dim n = d.nsamples
-        Dim idx(n - 1) As Integer
-        For i = 0 To n - 1
-            idx(i) = i
-        Next
-        Return BatchTensor(d.features, idx, 0, n)
-    End Function
-
     Public Sub SupervisedDemo()
         Console.WriteLine("  Spiking Neural Network  ·  LIF + SurrogateGrad + STDP")
         Console.WriteLine()
@@ -166,7 +148,7 @@ Module test1
                 u(tt) = h1.UHistory(tt).Data(n)
             Next
             Console.WriteLine($"    h{n}: {Sparkline(u)}")
-            Console.WriteLine($"    " & New String("-"c, 40))
+            Console.WriteLine($"    " & New String("-"c, 36))
             Console.WriteLine()
         Next
 
