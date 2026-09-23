54137cd42ba59d27af8e44521ed8526787d6df7e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-23 09:04:21 +0800

    create h264 encoder

---
+++ gr/MPEG4/H264IntraPrediction.vb
@@ -0,0 +1,305 @@
+' Author:
+' 
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+''' <summary>
+''' 帧内预测的模式编号（与 H.264 规范一致）。
+''' </summary>
+''' <remarks>
+''' 亮度 I_16x16 与色度的 4 种模式编号相同（<c>Vertical=0, Horizontal=1, DC=2, Plane=3</c>），
+''' 写入码流时：I_16x16 的模式直接进 mb_type；色度的模式则用 unary 编码单独传输。
+''' </remarks>
+Friend Enum H264IntraMode
+    Vertical = 0
+    Horizontal = 1
+    DC = 2
+    Plane = 3
+End Enum
+
+''' <summary>
+''' I_16x16 亮度与 8x8 色度的帧内预测。
+''' </summary>
+''' <remarks>
+''' 公式与 ffmpeg libavcodec/h264pred_template.c 的对应实现一致：
+''' 
+''' + I_16x16 DC：<c>(Σleft + Σtop + 16) &gt;&gt; 5</c>；只有一侧可用时 <c>(Σ + 8) &gt;&gt; 4</c>；都不可用时为 128
+''' + I_16x16 Plane：<c>H = (5*H + 32) &gt;&gt; 6</c>、<c>V = (5*V + 32) &gt;&gt; 6</c>，
+'''   <c>a0 = 16*(left[15] + top[15] + 1) - 7*(V + H)</c>，<c>pred = (a0 + y*V + x*H) &gt;&gt; 5</c>
+''' + 色度 DC 按 4 个 4x4 子块分别求均值；色度 Plane 的系数为 17/16 与 3
+''' 
+''' 相邻样点不可用时，规范要求把某些模式回退成"只用一侧求均值"的 DC 变体：
+''' 顶部不可用时 Vertical/Plane 非法、左侧不可用时 Horizontal/Plane 非法，
+''' 因此编码器在选模式时必须先按可用性收窄候选集合（<see cref="allowedModes"/>）。
+''' </remarks>
+Friend NotInheritable Class H264IntraPrediction
+
+    Private Sub New()
+    End Sub
+
+    ''' <summary>
+    ''' 给定左、上相邻样点是否可用，返回合法的模式集合
+    ''' </summary>
+    Friend Shared Function allowedModes(leftAvailable As Boolean, topAvailable As Boolean) As Boolean()
+        Dim allowed As Boolean() = New Boolean(3) {}
+
+        allowed(CInt(H264IntraMode.DC)) = True
+        allowed(CInt(H264IntraMode.Vertical)) = topAvailable
+        allowed(CInt(H264IntraMode.Horizontal)) = leftAvailable
+        allowed(CInt(H264IntraMode.Plane)) = leftAvailable AndAlso topAvailable
+
+        Return allowed
+    End Function
+
+    ''' <summary>
+    ''' 生成 I_16x16 的预测样点（16x16）
+    ''' </summary>
+    ''' <param name="plane">重建平面（左邻与上邻样点已就绪）</param>
+    ''' <param name="x0">当前宏块左上角 x（像素）</param>
+    ''' <param name="y0">当前宏块左上角 y（像素）</param>
+    ''' <param name="mode">预测模式</param>
+    ''' <param name="leftAvailable">左邻是否可用</param>
+    ''' <param name="topAvailable">上邻是否可用</param>
+    ''' <param name="pred">输出 16x16 预测样点，按行优先</param>
+    Friend Shared Sub predict16x16(plane As H264Plane, x0 As Integer, y0 As Integer,
+                                   mode As H264IntraMode,
+                                   leftAvailable As Boolean, topAvailable As Boolean,
+                                   pred As Integer())
+
+        Select Case mode
+            Case H264IntraMode.Vertical
+                For x As Integer = 0 To 15
+                    Dim v As Integer = plane.at(x0 + x, y0 - 1)
+
+                    For y As Integer = 0 To 15
+                        pred(y * 16 + x) = v
+                    Next
+                Next
+            Case H264IntraMode.Horizontal
+                For y As Integer = 0 To 15
+                    Dim v As Integer = plane.at(x0 - 1, y0 + y)
+
+                    For x As Integer = 0 To 15
+                        pred(y * 16 + x) = v
+                    Next
+                Next
+            Case H264IntraMode.DC
+                Dim sum As Integer = 0
+
+                For i As Integer = 0 To 15
+                    If topAvailable Then sum += plane.at(x0 + i, y0 - 1)
+                    If leftAvailable Then sum += plane.at(x0 - 1, y0 + i)
+                Next
+
+                Dim dc As Integer
+
+                If topAvailable AndAlso leftAvailable Then
+                    dc = (sum + 16) >> 5
+                ElseIf topAvailable OrElse leftAvailable Then
+                    dc = (sum + 8) >> 4
+                Else
+                    dc = 128
+                End If
+
+                For i As Integer = 0 To pred.Length - 1
+                    pred(i) = dc
+                Next
+            Case Else
+                Call predictPlane16x16(plane, x0, y0, pred)
+        End Select
+    End Sub
+
+    ''' <summary>
+    ''' I_16x16 的 plane 模式
+    ''' </summary>
+    Private Shared Sub predictPlane16x16(plane As H264Plane, x0 As Integer, y0 As Integer, pred As Integer())
+        Dim top As Integer() = New Integer(15) {}
+        Dim left As Integer() = New Integer(15) {}
+
+        For i As Integer = 0 To 15
+            top(i) = plane.at(x0 + i, y0 - 1)
+            left(i) = plane.at(x0 - 1, y0 + i)
+        Next
+
+        Dim h As Integer = top(8) - top(6)
+
+        For k As Integer = 2 To 8
+            h += k * (top(7 + k) - top(7 - k))
+        Next
+
+        Dim v As Integer = left(8) - left(6)
+
+        For k As Integer = 2 To 8
+            v += k * (left(8 + k) - left(6 - k))
+        Next
+
+        h = (5 * h + 32) >> 6
+        v = (5 * v + 32) >> 6
+
+        Dim a0 As Integer = 16 * (left(15) + top(15) + 1) - 7 * (v + h)
+
+        For y As Integer = 0 To 15
+            For x As Integer = 0 To 15
+                pred(y * 16 + x) = clip((a0 + y * v + x * h) >> 5)
+            Next
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' 生成色度 8x8 的预测样点
+    ''' </summary>
+    Friend Shared Sub predict8x8(plane As H264Plane, x0 As Integer, y0 As Integer,
+                                 mode As H264IntraMode,
+                                 leftAvailable As Boolean, topAvailable As Boolean,
+                                 pred As Integer())
+
+        Select Case mode
+            Case H264IntraMode.Vertical
+                For x As Integer = 0 To 7
+                    Dim v As Integer = plane.at(x0 + x, y0 - 1)
+
+                    For y As Integer = 0 To 7
+                        pred(y * 8 + x) = v
+                    Next
+                Next
+            Case H264IntraMode.Horizontal
+                For y As Integer = 0 To 7
+                    Dim v As Integer = plane.at(x0 - 1, y0 + y)
+
+                    For x As Integer = 0 To 7
+                        pred(y * 8 + x) = v
+                    Next
+                Next
+            Case H264IntraMode.DC
+                Call predictDc8x8(plane, x0, y0, leftAvailable, topAvailable, pred)
+            Case Else
+                Call predictPlane8x8(plane, x0, y0, pred)
+        End Select
+    End Sub
+
+    ''' <summary>
+    ''' 色度 DC：8x8 拆成 4 个 4x4 子块，各自取左/上均值
+    ''' </summary>
+    Private Shared Sub predictDc8x8(plane As H264Plane, x0 As Integer, y0 As Integer,
+                                    leftAvailable As Boolean, topAvailable As Boolean,
+                                    pred As Integer())
+        Dim dcs As Integer() = New Integer(3) {}
+
+        If leftAvailable AndAlso topAvailable Then
+            Dim sumTop%, sumLeft%
+
+            For i As Integer = 0 To 3
+                sumLeft += plane.at(x0 - 1, y0 + i)
+                sumTop += plane.at(x0 + i, y0 - 1)
+            Next
+
+            dcs(0) = (sumLeft + sumTop + 4) >> 3
+
+            Dim sumTopRight%, sumLeftBottom%
+
+            For i As Integer = 0 To 3
+                sumTopRight += plane.at(x0 + 4 + i, y0 - 1)
+                sumLeftBottom += plane.at(x0 - 1, y0 + 4 + i)
+            Next
+
+            dcs(1) = (sumTopRight + 2) >> 2
+            dcs(2) = (sumLeftBottom + 2) >> 2
+            dcs(3) = (dcs(1) + dcs(2) + 4) >> 3
+        ElseIf leftAvailable Then
+            Dim sumLeftTop%, sumLeftBottom%
+
+            For i As Integer = 0 To 3
+                sumLeftTop += plane.at(x0 - 1, y0 + i)
+                sumLeftBottom += plane.at(x0 - 1, y0 + 4 + i)
+            Next
+
+            dcs(0) = (sumLeftTop + 2) >> 2
+            dcs(1) = dcs(0)
+            dcs(2) = (sumLeftBottom + 2) >> 2
+            dcs(3) = dcs(2)
+        ElseIf topAvailable Then
+            Dim sumTopLeft%, sumTopRight%
+
+            For i As Integer = 0 To 3
+                sumTopLeft += plane.at(x0 + i, y0 - 1)
+                sumTopRight += plane.at(x0 + 4 + i, y0 - 1)
+            Next
+
+            dcs(0) = (sumTopLeft + 2) >> 2
+            dcs(1) = (sumTopRight + 2) >> 2
+            dcs(2) = dcs(0)
+            dcs(3) = dcs(1)
+        Else
+            For i As Integer = 0 To 3
+                dcs(i) = 128
+            Next
+        End If
+
+        For y As Integer = 0 To 7
+            For x As Integer = 0 To 7
+                pred(y * 8 + x) = dcs(If(y < 4, 0, 2) + If(x < 4, 0, 1))
+            Next
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' 色度 plane 模式（系数为 17/16 与 3，与亮度的 5/6 与 7 不同）
+    ''' </summary>
+    Private Shared Sub predictPlane8x8(plane As H264Plane, x0 As Integer, y0 As Integer, pred As Integer())
+        Dim top As Integer() = New Integer(7) {}
+        Dim left As Integer() = New Integer(7) {}
+
+        For i As Integer = 0 To 7
+            top(i) = plane.at(x0 + i, y0 - 1)
+            left(i) = plane.at(x0 - 1, y0 + i)
+        Next
+
+        Dim h As Integer = top(4) - top(2)
+
+        For k As Integer = 2 To 4
+            h += k * (top(3 + k) - top(3 - k))
+        Next
+
+        Dim v As Integer = left(4) - left(2)
+
+        For k As Integer = 2 To 4
+            v += k * (left(4 + k) - left(2 - k))
+        Next
+
+        h = (17 * h + 16) >> 5
+        v = (17 * v + 16) >> 5
+
+        Dim a0 As Integer = 16 * (left(7) + top(7) + 1) - 3 * (v + h)
+
+        For y As Integer = 0 To 7
+            For x As Integer = 0 To 7
+                pred(y * 8 + x) = clip((a0 + y * v + x * h) >> 5)
+            Next
+        Next
+    End Sub
+
+    Private Shared Function clip(value As Integer) As Integer
+        If value < 0 Then Return 0
+        If value > 255 Then Return 255
+        Return value
+    End Function
+
+End Class
---
+++ gr/MPEG4/H264Plane.vb
@@ -0,0 +1,196 @@
+' Author:
+' 
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+''' <summary>
+''' 单个 8bit 图像平面（亮度或色度），带独立步长。
+''' </summary>
+''' <remarks>
+''' 编码器需要频繁按 4x4 块读写样点，加上重建帧还要作为后续预测的参考，
+''' 因此这里用连续的一维数组加步长来承载，避免二维数组的边界检查开销。
+''' 平面尺寸已按宏块对齐，右侧/下方的多余样点用于预测时越界的相邻样点。
+''' </remarks>
+Friend Class H264Plane
+
+    Friend ReadOnly Property width As Integer
+    Friend ReadOnly Property height As Integer
+    Friend ReadOnly Property stride As Integer
+    Friend ReadOnly Property data As Byte()
+
+    Friend Sub New(width As Integer, height As Integer)
+        Me.width = width
+        Me.height = height
+        Me.stride = width
+        Me.data = New Byte(width * height - 1) {}
+    End Sub
+
+    ''' <summary>
+    ''' 把整幅平面填充为同一个值
+    ''' </summary>
+    Friend Sub fill(value As Integer)
+        Dim v As Byte = CByte(value And &HFF)
+
+        For i As Integer = 0 To data.Length - 1
+            data(i) = v
+        Next
+    End Sub
+
+    Friend Function at(x As Integer, y As Integer) As Integer
+        Return data(y * stride + x)
+    End Function
+
+    Friend Sub setAt(x As Integer, y As Integer, value As Integer)
+        If value < 0 Then value = 0
+        If value > 255 Then value = 255
+
+        data(y * stride + x) = CByte(value)
+    End Sub
+
+    ''' <summary>
+    ''' 用左/上边界的样点复制来补齐到宏块对齐区域之外的部分
+    ''' </summary>
+    ''' <param name="visibleWidth">实际可见宽度</param>
+    ''' <param name="visibleHeight">实际可见高度</param>
+    Friend Sub padEdges(visibleWidth As Integer, visibleHeight As Integer)
+        For y As Integer = 0 To height - 1
+            Dim last As Integer = at(Math.Max(0, visibleWidth - 1), Math.Min(y, visibleHeight - 1))
+
+            For x As Integer = visibleWidth To width - 1
+                setAt(x, y, last)
+            Next
+        Next
+
+        For y As Integer = visibleHeight To height - 1
+            For x As Integer = 0 To width - 1
+                setAt(x, y, at(x, Math.Max(0, visibleHeight - 1)))
+            Next
+        Next
+    End Sub
+
+End Class
+
+''' <summary>
+''' 一幅 4:2:0 YUV 图像（亮度 + 两个色度平面，均已按宏块对齐）。
+''' </summary>
+''' <remarks>
+''' 由 RGBA 帧转换而来：BT.601 的 studio range 整数公式，色度用 2x2 平均下采样。
+''' 转换结果同时作为「原始帧」参与残差计算与「重建帧」被逐块覆盖，
+''' 因此 <see cref="clone"/> 用于保留原始样点。
+''' </remarks>
+Friend Class H264Yuv420
+
+    Friend ReadOnly Property codedWidth As Integer
+    Friend ReadOnly Property codedHeight As Integer
+
+    ''' <summary>实际可见宽度（用于边界补齐与裁剪）</summary>
+    Friend ReadOnly Property width As Integer
+
+    ''' <summary>实际可见高度</summary>
+    Friend ReadOnly Property height As Integer
+
+    Friend ReadOnly Property y As H264Plane
+    Friend ReadOnly Property u As H264Plane
+    Friend ReadOnly Property v As H264Plane
+
+    Friend Sub New(codedWidth As Integer, codedHeight As Integer, width As Integer, height As Integer)
+        Me.codedWidth = codedWidth
+        Me.codedHeight = codedHeight
+        Me.width = width
+        Me.height = height
+        Me.y = New H264Plane(codedWidth, codedHeight)
+        Me.u = New H264Plane(codedWidth \ 2, codedHeight \ 2)
+        Me.v = New H264Plane(codedWidth \ 2, codedHeight \ 2)
+    End Sub
+
+    ''' <summary>
+    ''' 复制一份（用于保留原始帧，编码过程中的重建会就地覆盖）
+    ''' </summary>
+    Friend Function clone() As H264Yuv420
+        Dim copy As New H264Yuv420(codedWidth, codedHeight, width, height)
+
+        Call Array.Copy(y.data, copy.y.data, y.data.Length)
+        Call Array.Copy(u.data, copy.u.data, u.data.Length)
+        Call Array.Copy(v.data, copy.v.data, v.data.Length)
+
+        Return copy
+    End Function
+
+    ''' <summary>
+    ''' 由扁平的 BGRA 字节数组（每像素 4 字节，b/g/r/a 顺序）载入画面。
+    ''' </summary>
+    Friend Sub loadBgra(bgra As Byte(), pixelWidth As Integer)
+        For j As Integer = 0 To height - 1
+            For i As Integer = 0 To width - 1
+                Dim p As Integer = (j * pixelWidth + i) * 4
+                Dim b As Integer = bgra(p)
+                Dim g As Integer = bgra(p + 1)
+                Dim r As Integer = bgra(p + 2)
+
+                Call y.setAt(i, j, clip((66 * r + 129 * g + 25 * b + 128) >> 8) + 16)
+            Next
+        Next
+
+        For j As Integer = 0 To height \ 2 - 1
+            For i As Integer = 0 To width \ 2 - 1
+                Dim sumB%, sumG%, sumR%
+                Dim n% = 0
+
+                For dy As Integer = 0 To 1
+                    For dx As Integer = 0 To 1
+                        Dim x As Integer = i * 2 + dx
+                        Dim yy As Integer = j * 2 + dy
+
+                        If x < width AndAlso yy < height Then
+                            Dim p As Integer = (yy * pixelWidth + x) * 4
+
+                            sumB += bgra(p)
+                            sumG += bgra(p + 1)
+                            sumR += bgra(p + 2)
+                            n += 1
+                        End If
+                    Next
+                Next
+
+                Dim bAvg As Integer = sumB \ n
+                Dim gAvg As Integer = sumG \ n
+                Dim rAvg As Integer = sumR \ n
+
+                Call u.setAt(i, j, clip((-38 * rAvg - 74 * gAvg + 112 * bAvg + 128) >> 8) + 128)
+                Call v.setAt(i, j, clip((112 * rAvg - 94 * gAvg - 18 * bAvg + 128) >> 8) + 128)
+            Next
+        Next
+
+        Call y.padEdges(width, height)
+        Call u.padEdges(Math.Max(1, width \ 2), Math.Max(1, height \ 2))
+        Call v.padEdges(Math.Max(1, width \ 2), Math.Max(1, height \ 2))
+    End Sub
+
+    ''' <summary>
+    ''' 按规格把样点限制到 0 - 255
+    ''' </summary>
+    Private Shared Function clip(value As Integer) As Integer
+        If value < 0 Then Return 0
+        If value > 255 Then Return 255
+        Return value
+    End Function
+
+End Class
